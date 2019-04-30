61fb6fa0c1aa86ee4f15e22f9accad6118b4251f
Meg Stang <mstang15@gmail.com>
2019-04-30 12:46:38 -0600

    Update test to use symbols for category

--- test/card_test.rb
+++ test/card_test.rb
@@ -5,26 +5,26 @@ require './lib/card'
 class CardTest < Minitest::Test
 
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", "Geography")
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_instance_of Card, card
   end
 
   def test_it_has_a_question
-    card = Card.new("What is the capital of Alaska?", "Juneau", "Geography")
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "What is the capital of Alaska?", card.question
   end
 
   def test_it_has_an_answer
-    card = Card.new("What is the capital of Alaska?", "Juneau", "Geography")
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "Juneau", card.answer
   end
 
   def test_it_has_a_category
-    card = Card.new("What is the capital of Alaska?", "Juneau", "Geography")
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    assert_equal "Geography", card.category
+    assert_equal :Geography, card.category
   end
 end
