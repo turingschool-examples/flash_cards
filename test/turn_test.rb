1892f343a7a1601e8065bc6773693455e21d1cdc
Fenton Taylor <fentontaylor@gmail.com>
2019-06-25 13:17:10 -0600

    Run test_turn.rb without failure

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,16 +1,52 @@
+require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/turn'
 require './lib/card'
 
-card = Card.new("What's the airspeed velocity of an unladen swallow?",
-                "I don't know",
-                :Biology)
+class TurnTest < Minitest::Test
 
-turn = Turn.new("25", card)
+  def test_it_exists
+    card = Card.new("What's the airspeed velocity of an unladen swallow?",
+              "I don't know",
+              :Biology)
+    turn = Turn.new("25", card)
 
-turn.card
+    assert_instance_of Turn, turn
+  end
 
-turn.guess
 
-turn.correct?
+  def test_card_in_turn
+    card = Card.new("What's the airspeed velocity of an unladen swallow?",
+              "I don't know",
+              :Biology)
+    turn = Turn.new("25", card)
+    actual_card = turn.card
+    assert_equal actual_card, card
+  end
 
-turn.feedback
+  def test_guess_in_turn
+    card = Card.new("What's the airspeed velocity of an unladen swallow?",
+              "I don't know",
+              :Biology)
+    turn = Turn.new("25", card)
+    assert_equal "25", turn.guess
+  end
+
+  def test_correct
+    card = Card.new("What's the airspeed velocity of an unladen swallow?",
+              "I don't know",
+              :Biology)
+    turn = Turn.new("25", card)
+    expected_correct = false
+    assert_equal expected_correct, turn.correct?
+  end
+
+  def test_feedback
+    card = Card.new("What's the airspeed velocity of an unladen swallow?",
+              "I don't know",
+              :Biology)
+    turn = Turn.new("25", card)
+    assert_equal "Incorrect.", turn.feedback
+  end
+
+end
