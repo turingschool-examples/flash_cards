2bc153f396700f7cc29cf9c76a8ed3637d16427a
Fenton Taylor <fentontaylor@gmail.com>
2019-06-25 13:40:38 -0600

    Rewrite with setup method for deck and card instance variables

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,65 +6,35 @@ require "pry"
 
 class DeckTest < Minitest::Test
 
-  def make_cards
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    [card_1, card_2, card_3]
-  end
-
-  def make_deck
-    Deck.new(make_cards)
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
   end
 
   def test_it_exists
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    assert_instance_of Deck, deck
+    assert_instance_of Deck, @deck
   end
 
-  def test_deck_cards
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    assert_equal cards, deck.cards
+  def test_cards_method
+    assert_equal @cards, @deck.cards
   end
 
   def test_deck_count
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    assert_equal 3, deck.count
+    assert_equal 3, @deck.count
   end
 
   def test_cards_in_category
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
-    actual_cards = deck.cards_in_category(:STEM)
-    expected_cards = [card_2, card_3]
+    actual_cards = @deck.cards_in_category(:STEM)
+    expected_cards = [@card_2, @card_3]
     assert_equal expected_cards, actual_cards
   end
 
   def test_no_cards_in_category
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
     expected_cards = []
-    actual_cards = deck.cards_in_category(:Science)
+    actual_cards = @deck.cards_in_category(:Science)
     assert_equal expected_cards, actual_cards
   end
 end
