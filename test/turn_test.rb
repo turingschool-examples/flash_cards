54ed661b9e118335ac1dc655e06f511e189ebdd3
Evette Telyas <etelyas@gmail.com>
2019-06-25 16:16:08 -0600

    Iteration 1

---
+++ test/turn_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn'
+require './lib/card'
+
+class TurnTest < Minitest::Test
+
+  def test_turn_exist
+    card = Card.new("What is the capital of New York?", "Albany", :Geography)
+    turn = Turn.new("Albany", card)
+    assert_instance_of Turn, turn
+  end
+
+  def test_guess_exist
+    card = Card.new("What is the capital of New York?", "Albany", :Geography)
+    turn = Turn.new("Albany", card)
+    assert_equal "Albany", turn.guess
+  end
+
+  def test_card_exist
+    card = Card.new("What is the capital of New York?", "Albany", :Geography)
+    turn = Turn.new("Albany", card)
+    assert_equal "What is the capital of New York?", card.question
+  end
+
+  def test_correct
+    card = Card.new("What is the capital of New York?", "Albany", :Geography)
+    turn = Turn.new("Albany", card)
+    assert_equal turn.guess, card.answer
+  end
+
+  def test_feedback
+    card = Card.new("What is the capital of New York?", "Albany", :Geography)
+    turn = Turn.new("Albany", card)
+    assert_equal "Correct!", turn.feedback
+
+    turn = Turn.new("Manhattan", card)
+    assert_equal "Incorrect.", turn.feedback
+  end
+end
