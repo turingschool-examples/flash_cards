ee85f284868c75b132321cbc67ddb78d39209bca
Corina Allen <StarPerfecrt@gmail.com>
2019-06-25 13:46:23 -0600

    Clarify initialize arguements

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,9 @@
 class Card
   attr_reader :question, :answer, :category
 
-  def initialize(q, a, c)
-    @question = q
-    @answer = a
-    @category = c
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category 
   end
 end
