5cbb9e543b0536eec9a461a4e4c7e2312c58b32d
Corina Allen <StarPerfecrt@gmail.com>
2019-06-25 16:15:37 -0600

    Edit Turn Testing

---
+++ test/turn_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+
+
+class TurnTest < Minitest::Test
+
+  def test_does_turn_exist
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
+  end
+
+  def test_does_turn_have_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_does_turn_have_a_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal card, turn.card
+  end
+
+  def test_turn_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Correct!", turn.feedback
+
+    turn = Turn.new("Denver", card)
+
+    assert_equal "Incorrect!", turn.feedback
+  end
+
+  def test_turn_correct
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal turn.guess, card.answer 
+  end
+end
