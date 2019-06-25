0348f4776b3e498730228739034e6a4a1e3cea28
Tyler Schaffer <tpschaffer@gmail.com>
2019-06-25 13:25:44 -0600

    Add count method and test

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,4 +3,8 @@ attr_reader :cards
   def initialize(cards)
     @cards = cards
   end
+
+  def count
+    cards.size
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -13,4 +13,24 @@ class DeckTest < MiniTest::Test
 
     assert_instance_of Deck, deck
   end
+
+  def test_it_can_reference_its_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal cards, deck.cards
+  end
+
+  def test_it_can_count_the_number_of_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+  end
 end
