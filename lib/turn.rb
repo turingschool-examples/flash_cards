768735963abef3d4a3a3300cc99490fed1356e07
Leiya Kenney <leiyakenney@gmail.com>
2019-06-26 08:23:14 -0600

    Add turn class

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+class Turn
+  attr_accessor :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    @guess == @card.answer
+  end
+
+  def feedback
+    if @guess == @card.answer
+      "Correct!"
+    else
+      "Incorrect."
+  end
+  end
+end
