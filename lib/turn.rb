2eb308dde31636dbbd5de6545f7a4d3044d3a1ef
Evette Telyas <etelyas@gmail.com>
2019-06-26 09:26:59 -0600

    Add instance variable to turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,11 +7,11 @@ class Turn
   end
 
   def correct?
-    guess == card.answer
+    @guess == card.answer
   end
 
   def feedback
-    if guess == card.answer
+    if @guess == card.answer
       return "Correct!"
     else
       return "Incorrect."
