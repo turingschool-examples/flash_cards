c5cfe336daceae9a461db295519256f2ee7134c3
Alec Wells“git config --global user.email alec.wells47@gmail.comgit config --global user.name Alec <alec.wells47@gmail.com>
2019-06-26 13:00:08 -0600

    Make changes to round

--- lib/round.rb
+++ lib/round.rb
@@ -1,23 +1,25 @@
+
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns, :current_card
 
   def initialize (deck)
     @deck = deck
+    @turns = []
+    @current_card = @deck.cards[0]
+    #@guess = ""
   end
 
   def take_turn (string)
-    i = 0
-    @guess = string
-    @turn = Turn.new(@guess, @deck[i])
-    i += 1
+    #@guess = string
+    turn = Turn.new(string, @deck.cards[0])
+    @turns << turn
+    @deck.cards.unshift
+    return turn
   end
 
-  def turns
+end
 
-  end
-
-  def currrent_card
-
-  end
+#deck.cards is an array
+@deck.cards.each do |card|
 
 end
