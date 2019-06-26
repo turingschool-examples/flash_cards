14ced514bb992a57e0ea92ce35deda88e1663220
Corina Allen <StarPerfecrt@gmail.com>
2019-06-26 16:15:26 -0600

    Almost complete Round class and test file

--- lib/round.rb
+++ lib/round.rb
@@ -1,15 +1,34 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    @deck.cards[0]
   end
 
   def take_turn(guess)
-    # this method needs to create a turn, removes the card from the deck, and moves on through
-    # deck.cards is an array BUT deck is NOT 
-    deck[0]
+    created_turn = Turn.new(guess, current_card)
+    @turns << created_turn
+    @deck.cards.shift
+    created_turn
   end
 
+  def number_correct
+    num_correct = 0
+    @turns.each do |turn|
+      if turn.correct?
+        num_correct += 1
+      end
+    end
+    num_correct
+  end
+
+  def number_correct_by_category(category)
+    @turns.select{|turn| turn.card.category == category && turn.correct?}.count
+  end
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -16,4 +16,75 @@ class RoundTest < Minitest::Test
 
     assert_instance_of Round, round
   end
-end 
+
+  def test_does_deck_exist_in_round
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal deck, round.deck
+  end
+
+  def test_turns
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal [], round.turns
+  end
+
+    def test_current_card
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+  end
+
+  def test_take_turn
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = Turn.new("Rome", card_1)
+
+    assert_equal new_turn.guess, round.take_turn("Rome").guess
+  end
+
+  def test_number_correct
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Rome")
+
+    assert_equal 1, round.number_correct
+  end
+
+  def test_number_correct_by_category
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Rome")
+    round.number_correct_by_category(:Geography)
+
+    assert_equal 1, round.number_correct_by_category(:Geography)
+    assert_equal 0, round.number_correct_by_category(:STEM)
+  end
+end
