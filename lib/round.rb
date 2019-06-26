e3add58136f15769a703a2f6374450a8700c2e33
Fenton Taylor <fentontaylor@gmail.com>
2019-06-25 21:27:49 -0600

    Add all necessary (empty) methods

--- lib/round.rb
+++ lib/round.rb
@@ -7,17 +7,33 @@ class Round
   end
 
   def current_card
-    deck.cards[0]
+    #deck.cards[0]
   end
 
   def next_card
-    deck = deck.cards.rotate
+    #deck = deck.cards.rotate
   end
 
   def take_turn(guess)
-    this_turn = Turn.new(guess, current_card)
-    turns << this_turn
-    next_card
-    this_turn
+    # this_turn = Turn.new(guess, current_card)
+    # turns << this_turn
+    # next_card
+    # this_turn
+  end
+
+  def number_correct
+
+  end
+
+  def number_correct_by_category(category)
+
+  end
+
+  def percent_correct
+
+  end
+
+  def percent_correct_by_category(category)
+
   end
 end
