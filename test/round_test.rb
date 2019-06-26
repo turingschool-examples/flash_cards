492382e2ac2acdfa6621ea58b5290888fba3154a
Tyler Schaffer <tpschaffer@gmail.com>
2019-06-26 08:38:30 -0600

    Add turns to round class

--- test/round_test.rb
+++ test/round_test.rb
@@ -14,4 +14,24 @@ class RoundTest < MiniTest::Test
 
     assert_instance_of Round, round
   end
+
+  def test_it_has_a_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_equal deck, round.deck
+  end
+
+  def test_it_has_no_turns_to_start
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_equal [], round.turns
+  end
 end
