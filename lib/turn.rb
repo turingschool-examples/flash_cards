926bff7b1c7c106ef93049a3c70e6a163218e78c
MackHalliday <mackenzie.halliday@gmail.com>
2019-06-25 22:18:05 -0600

    Update with error notes for tomorrow

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,3 +26,9 @@ class Turn
     end
   end
 end
+
+#2019-06-25 having issues with pry test starting on pry test
+#pry(main)> turn = Turn.new("Juneau", card)
+
+# pry(main)> pry(main)> turn.card
+# NameError: undefined local variable or method `main' for main:Object
