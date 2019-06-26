78d815c565a84e386b306aaa698479dbcf8e5f37
Fenton Taylor <fentontaylor@gmail.com>
2019-06-25 21:39:35 -0600

    Finish round test template

--- test/round_test.rb
+++ test/round_test.rb
@@ -27,13 +27,65 @@ class RoundTest < Minitest::Test
     assert_equal [], @round.turns
   end
 
-  def test_inital_current_card
+  def test_current_card_1
+    skip
     assert_equal @card_1, @round.current_card
   end
 
   def test_take_turn_1
-    new_turn = @round.take_turn("Juneau")
-    assert_equal Turn.new("Juneau", @card_1), new_turn
+
+  end
+
+  def test_turn_1_class
+    skip
+  end
+
+  def test_turn_1_correct
+    skip
+  end
+
+  def test_turns_array_1
+    skip
+  end
+
+  def test_number_correct_1
+    skip
+  end
+
+  def test_current_card_2
+    skip
+  end
+
+  def test_turns_count
+    skip
+  end
+
+  def test_turns_last_feedback
+    skip
+  end
+
+  def test_number_correct_2
+    skip
+  end
+
+  def test_number_correct_by_category_1
+    skip
+  end
+
+  def test_number_correct_by_category_2
+    skip
+  end
+
+  def test_percent_correct
+    skip
+  end
+
+  def test_percent_correct_by_category_1
+    skip
+  end
+
+  def test_current_3
+    skip
   end
 
 end
