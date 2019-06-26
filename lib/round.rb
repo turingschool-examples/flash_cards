df27b8e680f90fd62543a3ab8311c8dcbebbd459
Tylor Schafer <tylorschafer@gmail.com>
2019-06-26 15:15:02 -0600

    Add current_card and take_turn functionality

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,24 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @current_card = 0
   end
-  
+
+  def current_card
+    @deck.cards[@current_card]
+  end
+
+  def take_turn(guess)
+     record_turn = Turn.new(guess, current_card)
+     @turns << record_turn
+     deck.cards.shift
+     record_turn
+  end
+
+
+
+
 end
