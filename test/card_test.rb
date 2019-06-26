72938b3c88e5c9f037b87dde6dc724d0bebb3d1f
Fenton Taylor <fentontaylor@gmail.com>
2019-06-25 21:07:19 -0600

    Simplify tests with @card instance variable

--- test/card_test.rb
+++ test/card_test.rb
@@ -4,27 +4,23 @@ require './lib/card'
 
 class CardTest < Minitest::Test
 
-  def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  end
 
-    assert_instance_of Card, card
+  def test_it_exists
+    assert_instance_of Card, @card
   end
 
   def test_it_has_a_question
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_equal "What is the capital of Alaska?", card.question
+    assert_equal "What is the capital of Alaska?", @card.question
   end
 
   def test_it_has_an_answer
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_equal "Juneau", card.answer
+    assert_equal "Juneau", @card.answer
   end
 
   def test_it_has_a_category
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_equal :Geography, card.category
+    assert_equal :Geography, @card.category
   end
 end
