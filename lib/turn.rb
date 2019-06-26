a4309e3bb2d9cbbf1986a70f3dfbe2e851c21136
MackHalliday <mackenzie.halliday@gmail.com>
2019-06-25 20:49:22 -0600

    Update Turn class with rough guess of method structure

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,19 +1,30 @@
 
 class Turns
 
-  def initialize
+  def initialize (answer, card)
+    @answer = answer
+    @card = card
   end
 
   def guess
+    @guess
   end
 
   def card
+    @card
   end
 
   def correct?
+    guess == answer
   end
 
   def feedback
+    if quess == answer
+      puts "Correct!"
+    else
+      puts "Incorrect!"
   end
 
 end
+
+guess
