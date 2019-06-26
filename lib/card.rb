1001e5d9e0bf51260bf4cdf40809fb3ac2aee6b6
Nancy Lee <srlee713@gmail.com>
2019-06-26 14:32:54 -0600

    Edit parameter names

--- lib/card.rb
+++ lib/card.rb
@@ -1,21 +1,11 @@
 class Card
+
   attr_reader :question, :answer, :category
 
-  def initialize(q, a, c)
-    @question = q
-    @answer = a
-    @category = c
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
   end
 
-  # def question
-  #   @question
-  # end
-  # 
-  # def answer
-  #   @answer
-  # end
-  #
-  # def category
-  #   @category
-  # end
 end
