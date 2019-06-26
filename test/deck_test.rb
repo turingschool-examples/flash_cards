28123e9d64bb200534b14968acf2ff8f3ef71665
Corina Allen <StarPerfecrt@gmail.com>
2019-06-26 08:47:17 -0600

    Add Cards Array Test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,13 +6,44 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
   def test_does_deck_exit
-    card = Card.new("What is the capital of Italy?", "Rome", :Geography)
-    turn = Turn.new("Rome", card)
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
     assert_instance_of Deck, deck
   end
 
+  def test_cards_exists
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal cards, deck.cards
+  end
+
   def test_count
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal deck.count, cards.count
+  end
+
+  def test_cards_in_category
+    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    deck.cards_in_category(:Geography)
+
+    assert_equal [card_1, card_2], deck.cards_in_category(:Geography)
+  end
 
 end
