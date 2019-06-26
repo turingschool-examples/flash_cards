6206190ddfe1f7c24f919ea436a00cab4178c5e0
Tylor Schafer <tylorschafer@gmail.com>
2019-06-26 15:17:57 -0600

    Add round_deck, turns, current_card, and take_turn functionality

--- test/round_test.rb
+++ test/round_test.rb
@@ -3,6 +3,8 @@ require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/turn'
+
 
 class RoundTest < Minitest::Test
 
@@ -16,14 +18,50 @@ class RoundTest < Minitest::Test
     assert_instance_of Round, round
   end
 
-  def test_take_turn
+  def test_round_deck
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("Music is composed of what?", "Notes", :STEM)
     cards = [card_1, card_2]
     deck = Deck.new(cards)
     round = Round.new(deck)
 
-    take_turn
+    assert_equal deck, round.deck
+  end
+
+  def test_turns
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("Music is composed of what?", "Notes", :STEM)
+    cards = [card_1, card_2]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal [], round.turns
+  end
+
+  def test_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("Music is composed of what?", "Notes", :STEM)
+    cards = [card_1, card_2]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+  end
 
 
+  def test_take_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("Music is composed of what?", "Notes", :STEM)
+    cards = [card_1, card_2]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_instance_of Turn , new_turn
+
+    assert_equal [new_turn], round.turns
+
+
+
+  end
 end
