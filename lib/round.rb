fd433b5b9b4604d36b3051bdefd2b6a4a716c425
Fenton Taylor <fentontaylor@gmail.com>
2019-06-26 09:30:33 -0600

    Method take_turn functions properly

--- lib/round.rb
+++ lib/round.rb
@@ -1,28 +1,39 @@
+require 'pry'
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
 class Round
   attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
+    @current_card_index = 0
     @turns = []
   end
 
   def current_card
-    #deck.cards[0]
+    deck.cards[@current_card_index]
+  end
+
+  def current_card_index
+    @current_card_index
   end
 
   def next_card
-    #deck = deck.cards.rotate
+    unless @current_card_index == (deck.count - 1)
+      @current_card_index += 1
+    end
   end
 
   def take_turn(guess)
-    # this_turn = Turn.new(guess, current_card)
-    # turns << this_turn
-    # next_card
-    # this_turn
+    this_turn = Turn.new(guess, current_card)
+    turns << this_turn
+    next_card
+    return this_turn
   end
 
   def number_correct
-
+    
   end
 
   def number_correct_by_category(category)
@@ -37,3 +48,10 @@ class Round
 
   end
 end
+card1 = Card.new("5 + 5 = ", "10", :Math)
+card2 = Card.new("7 * 6 = ", "42", :Math)
+card3 = Card.new("H2O is", "water", :Science)
+cards = [card1, card2, card3]
+deck = Deck.new(cards)
+round = Round.new(deck)
+binding.pry
