b5f11159be6c0f6852015d16bf0e6a8e52a0173c
Alec Wells“git config --global user.email alec.wells47@gmail.comgit config --global user.name Alec <alec.wells47@gmail.com>
2019-06-26 17:37:53 -0600

    Add runner

---
+++ flashcard_runner.rb
@@ -0,0 +1,9 @@
+require './lib'
+require './test'
+require 'pry'
+
+card_1 = Card.new("What type of meat is chashu?", "pork", :Food)
+card_2 = Card.new("Are heirloom tomatoes determinate or indeterminate?", "indeterminate", :Garden)
+card_3 = Card.new("What type of plant uses bacterial nodes to fix nitrogen in the soil?", "legume", :Garden)
+card_4 = Card.new("What type of bones are used to make ramen broth?", "neck bones", :Food)
+card_5 = Card.new("What is the name for the cooking method that involves cooking at a consistent temperture in a water bath", "sous vide", :Food)
