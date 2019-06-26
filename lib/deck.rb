f0bb834dc593d972aa631b495e7b7e8e313fb2e7
MackHalliday <mackenzie.halliday@gmail.com>
2019-06-26 08:55:00 -0600

    Set up template for new Deck class

---
+++ lib/deck.rb
@@ -0,0 +1,29 @@
+# Desk class outline template
+
+
+class Deck
+
+#creates an array of Card objects
+  def initialize (x, y)
+
+  end
+
+#returns the number of cards in a deck
+  def count
+  end
+
+#returns a number of cards in a category
+  def cards_in_category
+  end
+
+  def
+  end
+
+  def
+    if
+
+    else
+
+    end
+  end
+end
