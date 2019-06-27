c62788abaeeb1e994d103800fc2cdb425f3fe83e
Scott Payton <spayton.c@gmail.com>
2019-06-27 13:43:33 -0600

    Update round_test.rb

--- test/round_test.rb
+++ test/round_test.rb
@@ -6,30 +6,72 @@ require './lib/round'
 
 class DeckTest < Minitest::Test
 
-  def test_it_exists
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
        and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5°
        clockwise from due north?", "North north west", :STEM)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @round = Round.new(@deck,@turns)
 
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new(deck)
+  end
 
-    assert_instance_of Round, round
+  def test_if_round_and_deck_exists
+    @card_1
+    @card_2
+    @card_3
+    @deck
+    @round
+    assert_instance_of Round, @round
+    assert_instance_of Deck, @deck
+  end
+
+  def test_if_round_returns_a_deck
+    @card_1
+    @card_2
+    @card_3
+    @deck
+    @round
+    assert_equal @deck, @round.deck
+  end
+
+  def test_if_round_starts_mt_array
+    @card_1
+    @card_2
+    @card_3
+    @deck
+    @round
+    assert_equal [], @round.turns
+  end
+
+  def test_if_round_has_current_card
+    @card_1
+    @card_2
+    @card_3
+    @deck
+    @round
+    
+    assert_equal @card_1, @round.current_card
+
   end
   #end class
-
-def test_has_a_deck
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-     and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5°
-     clockwise from due north?", "North north west", :STEM)
-
-  deck = Deck.new([card_1, card_2, card_3])
-  round = Round.new(deck)
-end
-
-
-end
+#
+# def test_has_a_deck
+#   @card_1
+#   @card_2
+#   @card_3
+#   @deck
+#   @round
+#
+#   assert_equal deck, round.deck
+#
+# end
+#
+# def test_has_a_turn
+#
+#
+#
+#
+# end
+ end
