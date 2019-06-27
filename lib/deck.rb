88715c66911286bafdf10d8eae2737abe118cc2c
Scott Payton <spayton.c@gmail.com>
2019-06-26 21:33:58 -0600

    Add tests to deck_test.rb && add function to deck.rb to get category

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,16 +10,17 @@ class Deck
     @cards.length
   end
 
-  def cards_in_category(category)
-    category = @cards.card_2
-    category
-  end
+  def cards_in_category(category_stem)
+  cards.select {|card| card.category == category_stem}
+    #returning
+end
 end #end class
 
-
-deck = Deck.new(cards)
-card_1 = Card.new("what is the capital of Alaska?","Juneau",:Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
-                  reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5°
-                  clockwise from due north?", "North north west", :STEM)
+# card_1 = Card.new("what is the capital of Alaska?","Juneau",:Geography)
+# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
+#                   reports about the surface of which planet?", "Mars", :STEM)
+# card_3 = Card.new("Describe in words the exact direction that is 697.5°
+#                   clockwise from due north?", "North north west", :STEM)
+#
+# cards = [card_1,card_2,card_3]
+# deck = Deck.new(cards)
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
+
 class DeckTest < Minitest::Test
   def test_it_exists
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -10,21 +11,47 @@ class DeckTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
       from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
+    deck = Deck.new(cards) #7th pry
     assert_instance_of Deck, deck
+  end #end first test
+
+  def test_it_has_card
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+        and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
+        from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      assert_equal cards, deck.cards
+      assert_equal card_1, deck.cards[0]
+      assert_equal card_2, deck.cards[1]
+      assert_equal card_3, deck.cards[2]
   end
 
-  def test_it_has_cards
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_3 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    assert_equal cards, deck.cards
-    assert_equal card_1, deck.card
-    assert_equal card_2, deck.card
-    assert_equal card_3, deck.card
-
-  end
+def test_count
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+    and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
+    from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
+  deck = Deck.new(cards)
+  assert_equal 3, deck.count
 end
+
+# def test_category
+#   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+#   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+#     and reports about the surface of which planet?", "Mars", :STEM)
+#   card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
+#     from due north?", "North north west", :STEM)
+#   cards = [card_1, card_2, card_3]
+#   deck = Deck.new(cards)
+#   stem_cards = [card_1, card_2]
+#
+#   assert_equal stem_cards, deck.cards_in_category(:STEM)
+#
+# end#end test
+
+end #class
