2d25d2df5ed7d4f499d68d922b19b85f20197142
Nancy Lee <srlee713@gmail.com>
2019-06-27 15:49:34 -0600

    Add another test case in Card test

--- test/card_test.rb
+++ test/card_test.rb
@@ -16,6 +16,12 @@ class CardTest < Minitest::Test
     assert_equal "What is the capital of Alaska?", card.question
   end
 
+  def test_it_can_have_another_question
+    card = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+
+    assert_equal "What is the capital of Colorado?", card.question
+  end
+
   def test_it_has_an_answer
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
