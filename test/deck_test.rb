4eb3521afe9fad5b693555f11c6304da61f1c00c
Leiya Kenney <leiyakenney@gmail.com>
2019-06-27 08:35:54 -0600

    Finish storing cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -24,6 +24,20 @@ class DeckTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?","North north west",:STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    
+
+    assert_equal cards.count, deck.count
+  end
+
+  def test_cards_in_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars",:STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?","North north west",:STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    deck.cards_in_category(:Geography)
+    deck.cards_in_category(:STEM)
+
+    assert_equal [card_1], deck.cards_in_category(:Geography)
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
   end
 end
