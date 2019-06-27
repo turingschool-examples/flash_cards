7e7323ccf84d85c77b88885f1088f22accfe231b
Scott Payton <spayton.c@gmail.com>
2019-06-27 08:11:47 -0600

    Update deck_test.rb with cateogry algo

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -40,18 +40,43 @@ def test_count
   assert_equal 3, deck.count
 end
 
-# def test_category
-#   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-#     and reports about the surface of which planet?", "Mars", :STEM)
-#   card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
-#     from due north?", "North north west", :STEM)
-#   cards = [card_1, card_2, card_3]
-#   deck = Deck.new(cards)
-#   stem_cards = [card_1, card_2]
-#
-#   assert_equal stem_cards, deck.cards_in_category(:STEM)
-#
-# end#end test
+def test_category_are_stem
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+    and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
+    from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
+  deck = Deck.new(cards)
+  actual = [card_2, card_3]
+  expected = deck.cards_in_category(:STEM)
+  assert_equal actual,expected
 
+end#end test
+
+def test_category_are_category
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+    and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
+    from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
+  deck = Deck.new(cards)
+  actual = [card_1]
+  expected = deck.cards_in_category(:Geography)
+  assert_equal actual,expected
+end
+
+def  test_category_are_popculture
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+    and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
+    from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
+  deck = Deck.new(cards)
+  actual = []
+  expected = deck.cards_in_category(:Popculture)
+  assert_equal actual,expected
+end 
 end #class
