a8d9542e870a051d640ff614b7d6dd0bdf0b1b67
Leiya Kenney <leiyakenney@gmail.com>
2019-06-27 16:52:45 -0600

    Edit deck_test, Update round_test through current_card

--- test/round_test.rb
+++ test/round_test.rb
@@ -3,52 +3,53 @@ require 'minitest/pride'
 require './lib/turn'
 require './lib/card'
 require './lib/deck'
+require './lib/round'
 require 'pry'
 
-class DeckTest < Minitest::Test
+class RoundTest < Minitest::Test
 
-  def test_cards_exist
+  def test_round_exists
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars",:STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?","North north west",:STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+    round = Round.new(deck)
 
-    assert_instance_of Deck, deck
+    assert_instance_of Round, round
   end
 
-  def test_card_array_exists
+  def test_deck_in_round
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars",:STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?","North north west",:STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+    round = Round.new(deck)
 
-    assert_equal cards, deck.cards
+    assert_equal deck, round.deck
   end
 
-  def test_deck_count
+  def test_turns_in_round
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars",:STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?","North north west",:STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+    round = Round.new(deck)
 
-    assert_equal cards.count, deck.count
+    assert_equal [], round.turns
   end
 
-  def test_cards_in_category
+  def test_current_card
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars",:STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?","North north west",:STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    deck.cards_in_category(:Geography)
-    deck.cards_in_category(:STEM)
-    deck.cards_in_category("Pop Culture")
+    round = Round.new(deck)
 
-    assert_equal [card_1], deck.cards_in_category(:Geography)
-    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
-    assert_equal [], deck.cards_in_category("Pop Culture")
+    assert_equal [card_1], round.current_card
   end
+
 end
