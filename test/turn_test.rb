fe70d50da6927655d47c517d61c69efdea693a08
Tyler Schaffer <tpschaffer@gmail.com>
2019-06-27 16:02:58 -0600

    Clean up turn test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,52 +4,34 @@ require './lib/card'
 require './lib/turn'
 
 class TurnTest < MiniTest::Test
-  def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
 
-    assert_instance_of Turn, turn
+  def test_it_exists
+    assert_instance_of Turn, @turn
   end
 
   def test_it_can_reference_the_card
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal card, turn.card
+    assert_equal @card, @turn.card
   end
 
   def test_it_has_a_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal "Juneau", turn.guess
+    assert_equal "Juneau", @turn.guess
   end
 
-  def test_if_the_guess_is_correct_when_it_should_be
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+  def test_if_the_guess_is_correct?
+    assert @turn.correct?
 
-    assert turn.correct?
+    wrong_turn = Turn.new("Anchorage", @card)
+    refute wrong_turn.correct?
   end
 
-  def test_if_the_guess_is_incorrect_when_it_should_be
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Anchorage", card)
+  def test_feedback
+    assert_equal "Correct!", @turn.feedback
 
-    refute turn.correct?
-  end
-
-  def test_correct_feedback
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal "Correct!", turn.feedback
-  end
-
-  def test_incorrect_feedback
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Anchorage", card)
-
-    assert_equal "Incorrect.", turn.feedback
+    wrong_turn = Turn.new("Anchorage", @card)
+    assert_equal "Incorrect.", wrong_turn.feedback
   end
 end
