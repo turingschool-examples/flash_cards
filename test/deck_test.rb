3379a899eeaaa64864b87c90e4fd4fe2e5672c0d
Evette Telyas <etelyas@gmail.com>
2019-06-27 15:42:17 -0600

    added methods to deck and round

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,9 +7,7 @@ class DeckTest < Minitest::Test
 
   def test_deck_exist
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     cards = [card_1, card_2, card_3]
@@ -21,9 +19,7 @@ class DeckTest < Minitest::Test
 
   def test_deck_cards_exist
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     cards = [card_1, card_2, card_3]
@@ -35,9 +31,7 @@ class DeckTest < Minitest::Test
 
   def test_deck_count
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     cards = [card_1, card_2, card_3]
