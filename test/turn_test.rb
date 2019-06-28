640895ff65b72fd97e6f3001342d0d33c30452f1
Scott Payton <spayton.c@gmail.com>
2019-06-27 23:32:16 -0600

    Cleaned up turn_test.rb File

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,59 +5,42 @@ require './lib/card'
 require './lib/turn'
 class TurnTest < Minitest::Test
 
-  def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of Turn, turn
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
   end
 
-def test_has_a_card
-  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  turn = Turn.new("Juneau", card)
+  def test_turn_exists
+    @card
+    @turn
 
-  assert_equal card, turn.card
+    assert_instance_of Turn, @turn
+  end
+
+  def test_turn_has_a_card
+  @card
+  @turn
+
+  assert_equal @card, @turn.card
 end
 
 def test_has_guess
-  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  turn = Turn.new("Juneau", card)
+  @card
+  @turn
 
-  assert_equal "Juneau", turn.guess
+  assert_equal "Juneau", @turn.guess
 end
 
 def test_is_correct?
-  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  turn = Turn.new("Juneau", card)
-  assert_equal true, turn.correct?
+  @card
+  @turn
+  assert_equal true, @turn.correct?
 end
 
 def test_feedback
-  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  turn = Turn.new("Juneau", card)
-  assert_equal "Correct!", turn.feedback
+  @card
+  @turn
+  assert_equal "Correct!", @turn.feedback
 end
 
-
-
-
-
-
-#   def test_it_has_a_question
-#     card = Turn.new("What is the capital of Alaska?", "Juneau", :Geography)
-#
-#     assert_equal "What is the capital of Alaska?", card.question
-#   end
-#
-#   def test_it_has_an_answer
-#     card = Turn.new("What is the capital of Alaska?", "Juneau", :Geography)
-#
-#     assert_equal "Juneau", card.answer
-#   end
-#
-#   def test_it_has_a_category
-#     card = Turn.new("What is the capital of Alaska?", "Juneau", :Geography)
-#
-#     assert_equal :Geography, card.category
-#   end
-end
+end#end class
