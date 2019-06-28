e0be575b548e5109996c732230666d6677de7f38
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-06-28 07:17:10 -0600

    Update round and round test files

--- test/round_test.rb
+++ test/round_test.rb
@@ -1,7 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card.rb'
+require './lib/deck'
 require './lib/turn.rb'
+require './lib/round.rb'
 require 'pry'
 
 class RoundTest < Minitest::Test
@@ -9,7 +11,23 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @cards = [card_1, card_2, card_3]
-    @deck = Deck.new(cards)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
   end
+
+  def test_that_round_exists
+
+  assert_instance_of Round, @round
+  end
+
+  def test_that_round_has_deck
+
+    assert_instance_of Deck, @round.the_deck
+  end
+  def test_that_round_has_turns
+
+    assert_equal [], @round.turns
+  end
+
 end
