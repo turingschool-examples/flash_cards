659378401372c91478b1a1f6b096ae10b6597a00
Scott Payton <spayton.c@gmail.com>
2019-06-27 22:59:05 -0600

    Cleaned up code for deck.rb File V2

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,16 +11,7 @@ class Deck
   end
 
   def cards_in_category(category_)
-  cards.select {|card| card.category == category_}
-    #returning
-end
-end #end class
+    cards.select {|card| card.category == category_}
+  end
 
-# card_1 = Card.new("what is the capital of Alaska?","Juneau",:Geography)
-# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
-#                   reports about the surface of which planet?", "Mars", :STEM)
-# card_3 = Card.new("Describe in words the exact direction that is 697.5°
-#                   clockwise from due north?", "North north west", :STEM)
-#
-# cards = [card_1,card_2,card_3]
-# deck = Deck.new(cards)
+end #end class
