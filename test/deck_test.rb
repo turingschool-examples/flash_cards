a3c699e6b96e9e2628e4e9afb17db46423b7ee17
Corina Allen <StarPerfecrt@gmail.com>
2019-06-28 16:22:19 -0600

    Refactor Deck Test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,46 +5,32 @@ require './lib/deck'
 
 
 class DeckTest < Minitest::Test
-  def test_does_deck_exist
-    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
-    card_2 = Card.new("What is the capital of England?", "London", :Geography)
-    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
 
-    assert_instance_of Deck, deck
+  def setup
+    @card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
+    @card_2 = Card.new("What is the capital of England?", "London", :Geography)
+    @card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+  end
+
+  def test_does_deck_exist
+    assert_instance_of Deck, @deck
   end
 
   def test_cards_exists
-    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
-    card_2 = Card.new("What is the capital of England?", "London", :Geography)
-    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
-    assert_equal cards, deck.cards
+    assert_equal @cards, @deck.cards
   end
 
   def test_count
-    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
-    card_2 = Card.new("What is the capital of England?", "London", :Geography)
-    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
-    assert_equal cards.count, deck.count
+    assert_equal @cards.count, @deck.count
   end
 
   def test_cards_in_category
-    card_1 = Card.new("What is the capital of Italy?", "Rome", :Geography)
-    card_2 = Card.new("What is the capital of England?", "London", :Geography)
-    card_3 = Card.new("What is Japan's form of currency?", "Yen", :Finance)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    deck.cards_in_category(:Geography)
+    @deck.cards_in_category(:Geography)
 
-    assert_equal [card_1, card_2], deck.cards_in_category(:Geography)
-    assert_equal [card_3], deck.cards_in_category(:Finance)
+    assert_equal [@card_1, @card_2], @deck.cards_in_category(:Geography)
+    assert_equal [@card_3], @deck.cards_in_category(:Finance)
   end
 
 end
