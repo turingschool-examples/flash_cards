339d12b285bd49231cc4c97e313106e87b5b313f
Scott Payton <spayton.c@gmail.com>
2019-06-27 23:31:29 -0600

    Cleaned up turn.rb File

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,24 +6,14 @@ class Turn
       @guess = guess
       @card = card
     end
-=begin multilinecomment
-    def card
-      puts @card
-    end
-
-  def guess guess
-    puts "#{question}"
-    guess = gets.chomp
-  end
-=end
 
   def correct?
-    if @guess == card.answer
+     if @guess == card.answer
        true
      else
       false
-   end
-end
+     end
+ end
 
   def feedback
     if @guess ==  card.answer
@@ -34,16 +24,3 @@ end
   end
 
 end#end class
-
-
-=begin multilinecomment
-card1 = Card.new("what is the element W?", "Tungsten", :science) #creates new card
---to test fail answer
-turn1 = Turn.new("Helium", card1)
---to test correct answer
-turn1 = Turn.new("Tungsten", card1)
-turn1.card
-turn1.guess
-turn1.correct?
-turn1.feedback
-=end
