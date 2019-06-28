763715eed31cd1922d0dac9fa97530b506755923
Scott Payton <spayton.c@gmail.com>
2019-06-27 22:56:35 -0600

    Cleaned up code for deck_test.rb File

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,79 +4,93 @@ require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
-  def test_it_exists
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-      and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
-      from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards) #7th pry
-    assert_instance_of Deck, deck
-  end #end first test
 
-  def test_it_has_card
-      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-        and reports about the surface of which planet?", "Mars", :STEM)
-      card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+      and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
         from due north?", "North north west", :STEM)
-      cards = [card_1, card_2, card_3]
-      deck = Deck.new(cards)
-      assert_equal cards, deck.cards
-      assert_equal card_1, deck.cards[0]
-      assert_equal card_2, deck.cards[1]
-      assert_equal card_3, deck.cards[2]
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
   end
 
-def test_count
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-    and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
-    from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
-  deck = Deck.new(cards)
-  assert_equal 3, deck.count
+  def test_deck_exists
+
+    @card_1
+    @card_2
+    @card_3
+    @cards
+    @deck
+
+    assert_instance_of Deck, @deck
+  end
+
+  def test_deck_has_cards
+
+      @card_1
+      @card_2
+      @card_3
+      @cards
+      @deck
+
+      assert_equal @cards, @deck.cards
+      assert_equal @card_1, @deck.cards[0]
+      assert_equal @card_2, @deck.cards[1]
+      assert_equal @card_3, @deck.cards[2]
+  end
+
+def test_count_of_cards_in_deck
+
+  @card_1
+  @card_2
+  @card_3
+  @cards
+  @deck
+
+  assert_equal @cards.count, @deck.count
 end
 
-def test_category_are_stem
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-    and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
-    from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
-  deck = Deck.new(cards)
-  actual = [card_2, card_3]
-  expected = deck.cards_in_category(:STEM)
+def test_category_is_stem
+
+  @card_1
+  @card_2
+  @card_3
+  @cards
+  @deck
+
+  actual = [@card_2, @card_3]
+  expected = @deck.cards_in_category(:STEM)
+
   assert_equal actual,expected
+end
 
-end#end test
+def test_category_is_geography
 
-def test_category_are_category
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-    and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
-    from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
-  deck = Deck.new(cards)
-  actual = [card_1]
-  expected = deck.cards_in_category(:Geography)
+  @card_1
+  @card_2
+  @card_3
+  @cards
+  @deck
+
+  actual = [@card_1]
+  expected = @deck.cards_in_category(:Geography)
+
   assert_equal actual,expected
 end
 
 def  test_category_are_popculture
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-    and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5 degress clockwise
-    from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
-  deck = Deck.new(cards)
+
+  @card_1
+  @card_2
+  @card_3
+  @cards
+  @deck
+
   actual = []
-  expected = deck.cards_in_category(:Popculture)
+  expected = @deck.cards_in_category(:Popculture)
+
   assert_equal actual,expected
-end 
-end #class
+end
+
+end #end class
