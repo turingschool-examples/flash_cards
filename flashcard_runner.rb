cf2be01b23fda947b389a0a1d46a2383b1c1dad1
Evette Telyas <etelyas@gmail.com>
2019-06-28 08:26:57 -0600

    Edit questions

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,7 +6,7 @@ require 'pry'
 
 card_1 = Card.new("What is 5 + 5", "10", :STEM)
 card_2 = Card.new("What is Rachel's favorite animal?", "dog", :"Turing Staff")
-card_3 = Card.new("What is Mike's Middle Name", "nobody knows", :"Turing Staff")
+card_3 = Card.new("What is Mike's Middle Name?", "nobody knows", :"Turing Staff")
 card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :"Pop Culture")
 deck = Deck.new([card_1, card_2, card_3, card_4])
 round_1 = Round.new(deck)
