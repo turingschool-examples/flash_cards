c7facd0b4e14c13fbd35cbd71b397ba361424578
Leiya Kenney <leiyakenney@gmail.com>
2019-06-28 09:18:29 -0600

    Update through test_turn_correct

--- test/round_test.rb
+++ test/round_test.rb
@@ -49,7 +49,31 @@ class RoundTest < Minitest::Test
     deck = Deck.new(cards)
     round = Round.new(deck)
 
-    assert_equal [card_1], round.current_card
+    assert_equal [card_1], [round.current_card]
+  end
+
+  def test_turn_class
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars",:STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?","North north west",:STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal Turn, round.take_turn("Juneau").class
+  end
+
+  def test_turn_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars",:STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?","North north west",:STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert round.take_turn("Juneau").correct?
+    refute round.take_turn("Juneau").correct?
+
   end
 
 end
