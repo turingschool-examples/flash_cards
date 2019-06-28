3cc2ae66aa1ace629721276f04198e977b4ed001
Evette Telyas <etelyas@gmail.com>
2019-06-28 10:11:58 -0600

    fix categories

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -9,10 +9,10 @@ card_2 = Card.new("What is Rachel's favorite animal?", "dog", :"Turing Staff")
 card_3 = Card.new("What is Mike's Middle Name?", "nobody knows", :"Turing Staff")
 card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :"Pop Culture")
 card_5 = Card.new("Who is the best pug ever?", "Larry", :"Pugs")
-card_6 = Card.new("Which planet is red?", "Mars", :"STEM")
+card_6   = Card.new("Which planet is red?", "Mars", :"STEM")
 deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
 round_1 = Round.new(deck)
-
+categories = []
 
   def start(round, deck)
     puts "Welcome! You're playing with #{deck.cards.count} cards!"
@@ -37,7 +37,9 @@ start(round_1, deck)
 
 puts "******** GAME OVER ********"
 puts "You had #{round_1.number_correct} correct answers out of #{deck.cards.count} for a total score of #{round_1.percent_correct}%!"
-puts "STEM - #{round_1.percent_correct_category(:STEM)}% correct"
-puts "Turing Staff - #{round_1.percent_correct_category(:"Turing Staff")}% correct"
-puts "Pop Culture - #{round_1.percent_correct_category(:"Pop Culture")}% correct"
-puts "Pugs - #{round_1.percent_correct_category(:"Pugs")}% correct"
+
+round_1.deck.cards.uniq {|card| categories << card.category}
+
+categories.uniq.each do |category|
+  puts "#{category} - #{round_1.percent_correct_category(category)}% correct"
+end
