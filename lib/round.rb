d46c13dc31f0b26b8a91ce57e4f68d92e3942aef
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-06-28 08:15:12 -0600

    Add more round and round test information

--- lib/round.rb
+++ lib/round.rb
@@ -4,5 +4,18 @@ class Round
   def initialize(the_deck)
     @the_deck = the_deck
     @turns = []
+    @number_correct = 0
+  end
+
+  def current_card
+    num = @turns.count
+    @the_deck.cards[num]
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    @number_correct += 1 if new_turn.correct?
+    return new_turn
   end
 end
