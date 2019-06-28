396f29134fb80df33b70ba091a3bc6adfe045b92
Scott Payton <spayton.c@gmail.com>
2019-06-27 22:44:41 -0600

    Cleaned up code for Card.rb File

--- lib/card.rb
+++ lib/card.rb
@@ -5,35 +5,9 @@ attr_reader :question,
             :answer,
             :category
 
-def initialize(question, answer, category) #init variables for card class
-  @question = question #assigning inits
+def initialize(question, answer, category)
+  @question = question 
   @answer = answer
   @category = category
 end #end init method
-
-
-  # def question
-  #   puts "ian was here"
-  #   @question
-  # end #end method
-#
-#   def answer
-#     @answer
-#   end
-#
-#   def category
-#     @category
-#   end
-end
-#
-# #to test initial
-# card1 = Card.new("What is the element W?", "Tungsten", :science ) #creates new card
-
-#to test failure
-
-
-
-#begin calling methods
-# card1.question
-# card1.answer
-# card1.category
+end#end class
