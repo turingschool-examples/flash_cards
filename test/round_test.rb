fa3da16bd2282914f994d407e69b6a02a2319856
Daniel <danmoran.pro@gmail.com>
2019-06-29 13:39:36 -0600

    add round and round test

---
+++ test/round_test.rb
@@ -0,0 +1,77 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+class Round_test < Minitest::Test
+
+  def setup
+
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+  require "pry"; binding.pry
+  end
+
+  def test_round_exists
+    round = Round.new(@deck)
+  end
+# #=> #<Round:0x00007f972a1c7960...>,
+#
+# pry(main)> round.deck
+# #=> #<Deck:0x00007fa160a38ed8...>
+#
+# pry(main)> round.turns
+# #=> []
+#
+# pry(main)> round.current_card
+# #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
+#
+# pry(main)> new_turn = round.take_turn("Juneau")
+# #=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
+#
+# pry(main)> new_turn.class
+# #=> Turn
+#
+# pry(main)> new_turn.correct?
+# #=> true
+#
+# pry(main)> round.turns
+# #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
+#
+# pry(main)> round.number_correct
+# #=> 1
+#
+# pry(main)> round.current_card
+# #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
+#
+# pry(main)> round.take_turn("Venus")
+# #=> #<Turn:0x00007f972a215b38...>
+#
+# pry(main)> round.turns.count
+# #=> 2
+#
+# pry(main)> round.turns.last.feedback
+# #=> "Incorrect."
+#
+# pry(main)> round.number_correct
+# #=> 1
+#
+# pry(main)> round.number_correct_by_category(:Geography)
+# #=> 1
+#
+# pry(main)> round.number_correct_by_category(:STEM)
+# #=> 0
+#
+# pry(main)> round.percent_correct
+# #=> 50.0
+#
+# pry(main)> round.percent_correct_by_category(:Geography)
+# #=> 100.0
+#
+# pry(main)> round.current_card
+#=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
+end
