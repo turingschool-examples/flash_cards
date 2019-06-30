08321c1a6e49b3731f0b8b30a9404336ec118d54
Scott Payton <spayton.c@gmail.com>
2019-06-29 20:41:37 -0600

    Updates to runner.rb file, still need tweeking

--- runner.rb
+++ runner.rb
@@ -4,10 +4,38 @@ require './lib/deck'
 require './lib/round'
 require 'pry'
 
-#create sopme cards
-card_1 = ("Name the main Charecter in FLCL.", "Naota", :ANIME)
-card_2 = ("Name the robot in FLCL.", "Kanti", :ANIME)
-card_3 = ("Name the protagonist in FLCL.", "Haruko", :ANIME)
+
+
+#create some cards
+card_1 = Card.new("Name the main Charecter in FLCL.", "Naota", :ANIME)
+card_2 = Card.new("Name the robot in FLCL.", "Kanti", :ANIME)
+card_3 = Card.new("Name the protagonist in FLCL.", "Haruko", :ANIME)
+card_4 = Card.new("Who is the Astro Pirate King?", "Atomisk", :ANIME)
 
 #put those cards into a deck
-card_stack = [card_1,card_2,card_3]
+@card_stack_anime = [card_1,card_2,card_3,card_4] #first put the cards into an array
+@anime_deck = Deck.new([card_stack_anime]) #put anime card stack array into new deck obj
+@new_round = Round.new(anime_deck) #make new round with anime deck
+
+def start(round)
+
+  puts " "
+  puts " "
+  puts "***F L A S H ***C A R D***G A M E***"
+  puts " "
+  puts " "
+  puts "Welcome! You are playing with 4 cards."
+  puts "Good Luck and Enjoy! : ) "
+  puts "This is card number 1 out of 4..."
+  puts "--------------------------------------"
+
+  @round.deck.each do |card|
+    puts "Question: #{@card.question}"
+    puts "Type your answer -> "
+    @guess = gets.chomp #gets user answer
+    @round.take_turn(guess)
+    puts round.turns.last.feedback
+  end
+end
+
+start(new_round) #calls start method
