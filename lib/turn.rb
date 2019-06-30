3a8227996f366583ee099b738b239d3e016e6307
Corina Allen <StarPerfecrt@gmail.com>
2019-06-30 12:28:19 -0600

    Add downcase functionality to both answer and guess in correct? method

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,15 +7,10 @@ class Turn
   end
 
   def correct?
-    @guess == @card.answer
+    @guess.downcase == @card.answer.downcase
   end
 
   def feedback
     correct? ? "Correct." : "Incorrect."
-    # if @guess == @card.answer
-    #   return "Correct."
-    # else
-    #   return "Incorrect."
-    # end
   end
 end
