1e3e66b5801196f178fe8b6e4eb82fb23e2947f5
Christopher Kelling <cj.kelling88@gmail.com>
2019-06-30 16:46:24 -0600

    Complete iteration 3

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,10 +6,13 @@ require './lib/round'
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+card_4 = Card.new("Who was the first president of the United States?", "George Washington", :History)
 
-cards = [card_1, card_2, card_3]
+cards = [card_1, card_2, card_3, card_4]
 deck = Deck.new(cards)
 round = Round.new(deck)
+
+category = {}
 
 def start (deck, round)
   index = 1
@@ -18,7 +21,7 @@ def start (deck, round)
   round.deck.cards.each do |card|
     puts "This is card number #{index} out of #{round.deck.cards.count}"
     puts "Question: #{round.current_card.question}"
-    guess = gets.chomp.capitalize
+    guess = gets.chomp
     puts round.take_turn(guess).feedback
     index += 1
   end
@@ -28,6 +31,10 @@ start(deck, round)
 
 puts "****** Game Over! ******"
 puts "You have #{round.number_correct} correct guesses out of #{round.deck.cards.count} for a total score of #{round.percent_correct.round(2)}."
-# puts "#{round.percent_correct_by_category(category[0])} - correct"
-# puts "#{round.percent_correct_by_category(category[1])} - correct"
-# puts "#{round.percent_correct_by_category(category[2])} - correct"
+round.deck.cards.each do |card|
+  category [card.category] = nil
+end
+
+category.each do |key, value|
+  puts "#{key} - #{round.percent_correct_by_category(key)} correct"
+end
