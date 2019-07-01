d2b9e6b625b73318b923ea21f3402fdc6c76f457
MackHalliday <mackenzie.halliday@gmail.com>
2019-06-30 20:35:37 -0600

    take_turn test working

--- test/round_test.rb
+++ test/round_test.rb
@@ -8,7 +8,7 @@ require 'pry'
 
 class RoundTest < Minitest::Test
 
-  def varibles
+  def setup
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -19,40 +19,34 @@ class RoundTest < Minitest::Test
   end
 
   def test_round_new
+
       assert_instance_of Round, @round
   end
 
   def test_round_deck_method
-    skip
+
     assert_equal @deck, @round.deck
   end
 
   def test_round_turns_method
-    skip
+
     assert_equal [], @round.turns
   end
 
   def test_round_current_card_method
-    skip
-    assert_equal @deck.cards.first, @round.current_card
+
+    assert_equal @card_1, @round.current_card
   end
 
   def test_round_take_turn_method
-    skip
-    @new_turn = round.take_turn("Juneau")
 
-    assert_equal Turn, @new_turn.class
-  end
+    new_turn = @round.take_turn("Juneau")
 
-  def test_round_take_turn_method
-    skip
-    new_turn = round.take_turn("Juneau")
-
-    assert_equal Turn, @new_turn.class
+    assert_equal Turn, new_turn.class
   end
 
   def test_round_take_turn_correct_method
-    skip
+
     new_turn = round.take_turn("Juneau")
 
     assert_equal true, new_turn.correct?
