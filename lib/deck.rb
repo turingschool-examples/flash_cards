8ea203809d55a6721a80fed3b2cf23682b84063b
MackHalliday <mackenzie.halliday@gmail.com>
2019-06-30 19:53:53 -0600

    Cleaned up code, created instance variables

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,21 +5,18 @@ class Deck
 
   attr_reader :cards
 #creates an array of Card objects with attributes
-#create a new class called deck that takes in an array when initialize
-#assuming cards is array.
   def initialize (cards)
     @cards = cards
   end
 
-
 #returns the number of cards in a deck
   def count
-    cards.count
+    @cards.count
   end
 
 #returns a number of cards in a category
   def cards_in_category(category)
-    cards.select {|card| category == card.category}
+    @cards.select {|card| category == card.category}
   end
 
 end
