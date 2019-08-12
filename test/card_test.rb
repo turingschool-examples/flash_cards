8ca8ee556d931df76c7675814de125e213e64c56
Daniel <danmoran.pro@gmail.com>
2019-08-12 15:55:24 -0600

    Initial commit

--- test/card_test.rb
+++ test/card_test.rb
@@ -17,12 +17,14 @@ class CardTest < Minitest::Test
   end
 
   def test_it_has_an_answer
+
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "Juneau", card.answer
   end
 
   def test_it_has_a_category
+    
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal :Geography, card.category
