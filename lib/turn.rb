d5b7140556b2924fd46a99f9c377fd02d448d45a
John Travers <johnk.travers@gmail.com>
2019-08-12 18:59:55 -0600

    Add correct? method to turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,20 @@
 class Turn
     # Allow turn attributes to be read but not written
-    attr_reader :string, :card
+    attr_reader :guess, :card
 
     # Initialize guess and card attributes
     def initialize(string, card)
         @guess = string
         @card = card
+    end
+
+    # Define method that indicates if guess is correct
+    def correct?
+        # Ignore differences in capitalization
+        if guess.casecmp(card.answer) == 0
+            return true
+        else
+            return false
+        end
     end
 end
