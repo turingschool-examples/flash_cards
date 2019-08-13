38eb714878452d10aadeb93fd2c474405417f7f4
John Travers <johnk.travers@gmail.com>
2019-08-12 19:47:17 -0600

    Modify initialize method so that count method works in deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,6 +4,17 @@ class Deck
 
     # Initialize deck using array of cards
     def initialize(cards)
-        @cards = cards
+        # If more than one card exists in the deck (i.e. cards are an array)
+        if cards.class == Array
+            @cards = cards
+        # If only one card exists in the deck (cards.class == Card)
+        else
+            @cards = Array.new([cards])
+        end
+    end
+
+    # Define method to count the number of cards in deck
+    def count
+        return cards.length
     end
 end
