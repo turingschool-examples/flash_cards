5c219cbed438130afef791450ecd26be30b8aa84
Sebastian Sloan <sebastiansloan94@gmail.com>
2019-08-13 08:59:39 -0600

    Corrections on indentation

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,12 @@
 class Card
-  attr_accessor :answer, :question, :category
-
-  def initialize
-    @answer = answer
+  attr_accessor :question, :answer, :category
+  def initialize(question, answer, category)
     @question = question
+    @answer = answer
     @category = category
   end
 end
+
+
+
+card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
