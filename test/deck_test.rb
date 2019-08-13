918ad4cfb751042939b4d1bef7b769c1327ee299
Michael Cooper <macoop2363@mgmail.com>
2019-08-13 13:12:44 -0600

    Finished initial test cases

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,39 +5,46 @@ require './lib/deck'
 
 class CardTest < Minitest::Test
 
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecrafte sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west,", :STEM)
-    cards = [card_1, card_2, card_3]
+  CARD_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  CARD_2 = Card.new("The Viking spacecrafte sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  CARD_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west,", :STEM)
+  CARDS = [CARD_1, CARD_2, CARD_3]
 
   def test_it_exists
-    deck = Deck.new(cards)
+    deck = Deck.new(CARDS)
 
     assert_instance_of Deck, deck
   end
 
   def test_it_has_cards
-    deck = Deck.new(cards)
+    deck = Deck.new(CARDS)
 
-    assert_equal cards, deck.cards 
+    assert_equal CARDS, deck.cards 
   end
 
   def test_correct_card_count
-    deck = Deck.new(cards)
+    deck = Deck.new(CARDS)
 
-    assert_equal 3, deck.count
+    assert_equal CARDS.length, deck.count
   end
 
   def test_correct_valid_category_output
-    deck = Deck.new(cards)
+    pass = false
+    deck = Deck.new(CARDS)
 
-    assert_equal true, turn.correct?
+    test_1 = deck.cards_in_category(:Geography)
+    test_2 = deck.cards_in_category(:STEM)
+
+    if test_1.eql?([CARD_1]) && test_2.eql?([CARD_2, CARD_3])
+      pass = true
+    end
+    
+    assert_equal true, pass
   end
 
   def test_correct_invalid_category_output
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Denver", card)
+    deck = Deck.new(CARDS)
 
-    assert_equal false, turn.correct?
+    assert_equal [], deck.cards_in_category(:empty)
   end
 end
