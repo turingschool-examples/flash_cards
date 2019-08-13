b553211f4b7244c1ab15fe9de7f47632531388ea
Alice Post <ap2322@gmail.com>
2019-08-12 19:47:31 -0600

    Add first iteration of a turn.

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+class Turn
+  def initialize(guess_string, card)
+    @guess = guess_string
+    @card = card
+  end
+
+  def guess
+    @guess
+  end
+
+  def card
+    @card
+  end
+
+  def correct?
+    if @guess == @card.answer
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if correct?
+      "Great job!"
+    else
+      "Incorrect"
+    end
+  end
+
+end
