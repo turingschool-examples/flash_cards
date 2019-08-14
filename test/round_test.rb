268e9ed59b63911b1e39048721552c5f2e95fec0
Michael Cooper <macoop2363@mgmail.com>
2019-08-14 13:59:21 -0600

    Completed tests

--- test/round_test.rb
+++ test/round_test.rb
@@ -72,6 +72,7 @@ class RoundTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
     round.take_turn("Juneau")
     round.take_turn("I don't know")
     round.take_turn("North north west")
@@ -79,13 +80,19 @@ class RoundTest < Minitest::Test
     assert_equal 2, round.number_correct
   end
 
-  def test_number_correct_by_category(:Geography)
+  def test_number_correct_by_category
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.take_turn("I don't know")
+    round.take_turn("North north west")
+
+    assert_equal 1, round.number_correct_by_category(:STEM)
   end
 
   def test_percent_correct
@@ -95,15 +102,27 @@ class RoundTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.take_turn("I don't know")
+    round.take_turn("North north west")
+
+    assert_equal 66.6667, round.percent_correct
   end
 
-  def test_percent_correct_by_category(:Geography)
+  def test_percent_correct_by_category
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.take_turn("I don't know")
+    round.take_turn("North north west")
+
+    assert_equal 50.0, round.percent_correct_by_category(:STEM)
   end
 
 end
