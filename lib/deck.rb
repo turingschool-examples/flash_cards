25cf8da653f65a9bdc062e3f26af5df0a89423a5
Brian Bower <brian@brianbower.com>
2019-08-14 13:51:29 -0600

    Add deck.rb and deck_test.rb

---
+++ lib/deck.rb
@@ -0,0 +1,20 @@
+class Deck
+
+  def initialize(cards)
+    @Card = []
+    @cards = cards
+  end
+
+  def cards
+    @Card << @cards
+  end
+
+  def count
+    @count = @cards.count
+  end
+
+
+  def cards_in_category()
+  end
+
+end
