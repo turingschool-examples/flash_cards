5c1f8f51026528121b83f6a72d4ee8558fd5977c
David Lopez <davidclopez1@live.com>
2019-08-14 14:51:56 -0600

    Refinalize Deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,15 +4,11 @@ class Deck
     @cards = cards
   end
 
-  def cards
-    @cards
-  end
-
   def count
     @cards.count
   end
-  
+
   def cards_in_category(category)
-    cards.select{|cat| cat.category == category}
+    cards.select{|card| card.category == category}
   end
 end
