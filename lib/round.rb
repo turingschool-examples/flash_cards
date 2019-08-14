37aa76b1f20acbdd3ea418d9d6d6f39c653383e0
Michael Cooper <macoop2363@mgmail.com>
2019-08-14 13:48:29 -0600

    Filled out methods

--- lib/round.rb
+++ lib/round.rb
@@ -21,11 +21,22 @@ class Round
   end
 
   def number_correct_by_category(category)
+    turns_in_category = turns_by_category(category)
+    turns_in_category.count { |turn| turn.correct? == true }
   end
 
   def percent_correct
+    (number_correct.to_f / @turns.length * 100).round(4)
   end
 
-  def percent_correct_by_category(categoy)
+  def percent_correct_by_category(category)
+    turns_in_category = turns_by_category(category)
+    number_correct = turns_in_category.count { |turn| turn.correct? == true }
+    (number_correct.to_f / turns_in_category.length * 100).round(4)
+  end
+
+  # Helper methods
+  def turns_by_category(category)
+    @turns.select { |turn| turn.card.category == category }
   end
 end
