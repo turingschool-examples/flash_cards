5cf567e2c9c133363265fdb3b61b6cf8b0bf739a
sasloan <sebastiansloan94@gmail.com>
2019-08-13 23:51:21 -0600

    Ran test

--- test/card_test.rb
+++ test/card_test.rb
@@ -10,20 +10,20 @@ class CardTest < Minitest::Test
     assert_instance_of Card, card
   end
 
-  def test_it_has_a_question
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   def test_it_has_a_question
+     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    assert_equal "What is the capital of Alaska?", card.question
-  end
+     assert_equal "What is the capital of Alaska?", card.question
+   end
 
-  def test_it_has_an_answer
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   def test_it_has_an_answer
+     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    assert_equal "Juneau", card.answer
-  end
+     assert_equal "Juneau", card.answer
+   end
 
-  def test_it_has_a_category
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   def test_it_has_a_category
+     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal :Geography, card.category
   end
