20ee45f5d54cedb6e254ea5dacebce4cc8298ef1
Rachel Lew <rlew07@gmail.com>
2019-08-14 17:37:41 -0600

    Add Turn class

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+class Turn
+
+attr_reader :string, :card
+
+  def initialize(string, card)
+    @string = string
+    @card = card
+  end
+
+  def guess
+    @string
+  end
+
+  def correct?
+    @string == card.answer
+  end
+
+  def feedback
+    if correct?
+      "Correct!"
+    else
+      "Incorrect!"
+    end
+  end
+end
