79aab2b446692b9c3f950eb2a1efa4b100a9d4cc
John Travers <johnk.travers@gmail.com>
2019-08-14 14:36:54 -0600

    Clean up game statistics using enumerables

--- lib/round.rb
+++ lib/round.rb
@@ -37,39 +37,26 @@ class Round
 
     # Define method to calculate number of correct turns by category
     def number_correct_by_category(category)
-        # Initialize a counting variable and count the number of correct turns
-        correct = 0
-        @turns.each do |turn|
-            if turn.card.category == category && turn.correct?
-                correct += 1
-            end
-        end
-
-        # Return that number
-        return correct
+        # Count the number of correct turns where input == category
+        @turns.count { |turn| turn.card.category == category && turn.correct? }
     end
 
     # Define method to calculate percent correct
     def percent_correct
-        return self.number_correct.to_f / turns.length.to_f * 100
+        number_correct.to_f / turns.length.to_f * 100
     end
 
     # Define method to calculate percent correct by category
     def percent_correct_by_category(category)
         # Count the number of cards used within the given category
-        cards_in_cat = 0
-        @turns.each do |turn|
-            if turn.card.category == category
-                cards_in_cat += 1
-            end
-        end
+        cards_in_cat = @turns.count { |turn| turn.card.category == category }
 
         # Return the number correct in given category divided by total cards
         if cards_in_cat != 0
-            return self.number_correct_by_category(category).to_f / cards_in_cat.to_f * 100
+            return number_correct_by_category(category).to_f / cards_in_cat.to_f * 100
         # Avoid dividing by 0
         else
-            return 0
+            return 0.0
         end
     end
 
@@ -88,7 +75,7 @@ class Round
             puts "Question: #{card.question}"
 
             # Allow user to input a guess and check correctness
-            turn = self.take_turn(gets.chomp)
+            turn = take_turn(gets.chomp)
             puts turn.feedback
 
             # Add new card categories to the array of categories
@@ -99,11 +86,11 @@ class Round
 
         # Display general game statistics
         puts "****** Game over! ******"
-        puts "You had #{self.number_correct} correct guesses out of #{@deck.cards.length} for a total score of #{self.percent_correct.round}%."
+        puts "You had #{number_correct} correct guesses out of #{@deck.cards.length} for a total score of #{percent_correct.round}%."
 
         # Display category-specific statistics
         categories.each do |cat|
-            puts "#{cat} - #{self.percent_correct_by_category(cat).round}% correct"
+            puts "#{cat} - #{percent_correct_by_category(cat).round}% correct"
         end
     end
 end
