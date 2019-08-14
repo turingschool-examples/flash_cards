61c538c907d31f7b2787abf57fc37c992b7bf872
John Travers <johnk.travers@gmail.com>
2019-08-13 21:43:01 -0600

    Initialize card generator class

---
+++ lib/card_generator.rb
@@ -0,0 +1,12 @@
+class CardGenerator
+    # Allow user to read file name
+    attr_reader :filename
+
+    # Initialize instance variable
+    def initialize(filename)
+        @filename = filename
+    end
+
+    # 
+
+end
