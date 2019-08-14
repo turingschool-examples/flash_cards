403e95330b9303155bf57551f2be2ee4931c9f95
Michael Cooper <macoop2363@mgmail.com>
2019-08-14 10:32:10 -0600

    Added template test methods

--- test/round_test.rb
+++ test/round_test.rb
@@ -75,7 +75,35 @@ class RoundTest < Minitest::Test
     round.take_turn("Juneau")
     round.take_turn("I don't know")
     round.take_turn("North north west")
-
+    
     assert_equal 2, round.number_correct
   end
+
+  def test_number_correct_by_category(:Geography)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+  end
+
+  def test_percent_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+  end
+
+  def test_percent_correct_by_category(:Geography)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+  end
+
 end
