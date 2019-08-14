9733eb267b41bea722b5499f1a1c78b5baded688
Michael Cooper <macoop2363@mgmail.com>
2019-08-14 10:13:01 -0600

    Moved constants to each test method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,37 +5,53 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
 
-  CARD_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  CARD_2 = Card.new("The Viking spacecrafte sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  CARD_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west,", :STEM)
-  CARDS = [CARD_1, CARD_2, CARD_3]
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
 
   def test_it_exists
-    deck = Deck.new(CARDS)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
     assert_instance_of Deck, deck
   end
 
   def test_it_has_cards
-    deck = Deck.new(CARDS)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
-    assert_equal CARDS, deck.cards 
+    assert_equal cards, deck.cards 
   end
 
   def test_correct_card_count
-    deck = Deck.new(CARDS)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
-    assert_equal CARDS.length, deck.count
+    assert_equal cards.length, deck.count
   end
 
   def test_correct_valid_category_output
     pass = false
-    deck = Deck.new(CARDS)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
     test_1 = deck.cards_in_category(:Geography)
     test_2 = deck.cards_in_category(:STEM)
 
-    if test_1.eql?([CARD_1]) && test_2.eql?([CARD_2, CARD_3])
+    if test_1.eql?([card_1]) && test_2.eql?([card_2, card_3])
       pass = true
     end
 
@@ -43,7 +59,11 @@ class DeckTest < Minitest::Test
   end
 
   def test_correct_invalid_category_output
-    deck = Deck.new(CARDS)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
     assert_equal [], deck.cards_in_category(:empty)
   end
