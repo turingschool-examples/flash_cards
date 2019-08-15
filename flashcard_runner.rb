aa232d0037ecda46cf89be18f61fcf087a9d9034
Michael Cooper <macoop2363@mgmail.com>
2019-08-15 14:51:50 -0600

    Moved some sections to helper methods for easier reading

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -15,24 +15,17 @@ def start
   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
   card_4 = Card.new("Where is Turing based out of?", "Denver", :Geography)
   card_5 = Card.new("What is 5 + 5?", "10", :STEM)
-  card_6 = Card.new("What is Rachel's favorite animal?", "red panda", :Turing_Staff)
-  card_7 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
-  card_8 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+  card_6 = Card.new("What is Rachel's favorite animal?", "red panda", "Turing Staff".to_sym)
+  card_7 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff".to_sym)
+  card_8 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture".to_sym)
   cards = [card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8]
   deck = Deck.new(cards)
   round = Round.new(deck)
   total_cards = round.deck.count
   
-  # message setups
-  welcome_message   = "Welcome! You're playing with #{total_cards} cards."
-  welcome_break     = "-" * welcome_message.length
-  game_over_message = " Game Over! "
-  game_over_message = game_over_message.center(game_over_message.length + 12, "*")
-  
-  turn_number = 1
+  puts welcome_message(total_cards)
 
-  puts welcome_message
-  puts welcome_break
+  turn_number = 1
   while turn_number <= total_cards
     puts "This card is number #{turn_number} of #{total_cards}."
     puts "Question: #{round.current_card.question}"
@@ -43,16 +36,31 @@ def start
 
   puts game_over_message
   puts "You had #{round.number_correct} correct guesses out of #{total_cards} for a total score of #{round.percent_correct.round.to_i}%"
-  turns_in_categories = {}
-  round.turns.each do |turn|
-    if turns_in_categories.has_key?(turn.card.category)
-      turns_in_categories[turn.card.category].push(turn)
+  turns_by_categories_hash = categories_turn_hash(round.turns)
+  turns_by_categories_hash.each_key { |key| puts "#{key} - #{round.percent_correct_by_category(key).round.to_i}% correct" }
+end
+
+def welcome_message(total_cards)
+  message       = "Welcome! You're playing with #{total_cards} cards."
+  message_break = "-" * message.length
+  message + "\n" + message_break
+end
+
+def game_over_message
+  message = " Game Over! "
+  message = message.center(message.length + 12, "*")
+end
+
+def categories_turn_hash(turns)
+  return_hash = {}
+  turns.each do |turn|
+    if return_hash.has_key?(turn.card.category)
+      return_hash[turn.card.category].push(turn)
     else
-      turns_in_categories[turn.card.category] = [turn]
+      return_hash[turn.card.category] = [turn]
     end
   end
-  turns_in_categories.each_key { |key| puts "#{key} - #{round.percent_correct_by_category(key).round.to_i}% correct" }
-
+  return_hash
 end
 
 start
