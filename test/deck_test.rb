eb54a78f1247e41cd46baa555199581fc02b4bd2
David Lopez <davidclopez1@live.com>
2019-08-15 08:30:24 -0600

    Finish deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,42 +2,34 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/turn'
+require './lib/deck'
 require 'pry'
 
-class TurnTest < Minitest::Test
+class DeckTest < Minitest::Test
 
   def setup
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card2 = Card.new("What is my name?", "David", :Personal)
+    @card3 = Card.new("What is 30 * 30", "900", :Math)
+    @card4 = Card.new("What is the mile high city", "Denver", :Geography)
+    @cards = [@card1, @card2, @card3, @card4]
+    @deck1 = Deck.new(@cards)
     @turn = Turn.new("Juneau", @card)
   end
 
   def test_it_exists
-    assert_instance_of Turn, @turn
+    assert_instance_of Deck, @deck1
   end
 
-  def test_it_has_a_guess
-    assert_equal "Juneau", @turn.guess
+  def test_if_it_has_cards
+    assert_equal @deck1.cards, @cards
   end
 
-  def test_it_has_a_card
-    assert_instance_of Card, @turn.card
+  def test_if_it_counts
+    assert_equal 4, @deck1.count
   end
 
-  def test_if_it_has_correct_feedback
-    assert_equal "Correct!", @turn.feedback
-  end
-
-  def test_it_has_incorrect_feedback
-    turn = Turn.new("Denver", @card)
-    assert_equal "Incorrect.", turn.feedback
-  end
-
-  def test_it_is_correct?
-    assert_equal true, @turn.correct?
-  end
-
-  def test_is_it_isnt_correct?
-    turn = Turn.new("Denver", @card)
-    assert_equal false, turn.correct?
+  def test_if_it_does_category
+    assert_equal [@card1, @card4], @deck1.cards_in_category(:Geography)
   end
 end
