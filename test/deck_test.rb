0395a0a2447bc3e6cd352f4fe2fc4289873354c9
Daniel Selph <daniel.p.selph@gmail.com>
2019-08-15 09:00:13 -0600

    Updates for iteration 1 and 2

---
+++ test/deck_test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require 'pry'
+
+class DeckTest < Minitest::Test
+
+  def test_deck_exists
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_deck_has_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal [card_1, card_2, card_3], deck.cards
+  end
+
+  def test_count
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal 3, deck.count
+  end
+
+  def test_cards_in_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal [card_2, card_3],deck.cards_in_category(:STEM)
+    assert_equal [card_1],deck.cards_in_category(:Geography)
+    assert_equal [],deck.cards_in_category("Pop Culture")
+  end
+end
