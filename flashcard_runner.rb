f055d41d6b77f395b8ca7985a5ceace21933d67f
Alice Post <ap2322@gmail.com>
2019-08-14 18:16:44 -0600

    Add a runner file to start playing the game. Not yet functional.

---
+++ flashcard_runner.rb
@@ -0,0 +1,24 @@
+require 'pry'
+require './lib/turn'
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+
+card_1 = Card.new("What percent of the universe is dark matter?", "27", :STEM)
+card_2 = Card.new("Which United States President was the only one to serve two non-consecutive terms?", "Grover Cleveland", :HISTORY)
+card_3 = Card.new("Rounded to the nearest ten, approximately how many bones is a human born with?", "270", :HEALTH)
+card_4 = Card.new("Who was the clinical doctor who developed the hormonal birth control pill?", "John Rock", :HISTORY)
+card_5 = Card.new("What area of psychology incorporates the body?", "Somatic Psychology", :HEALTH)
+card_deck = [card_1, card_2, card_3, card_4, card_5]
+
+deck = Deck.new(card_deck)
+start_deck = Deck.new(card_deck)
+
+round = Round.new(start_deck)
+
+def start(round)
+  puts "Welcome! You're playing with #{ round.deck.cards.count } cards."
+  puts "---------------------------------------------------------------"
+end
+
+binding.pry
