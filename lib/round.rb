e26051d14d0c57b1ada3e7261047754cf4cc802d
David Lopez <davidclopez1@live.com>
2019-08-15 16:16:40 -0600

    Finalize round

---
+++ lib/round.rb
@@ -0,0 +1,46 @@
+class Round
+  attr_reader :deck, :turns
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    @deck.cards.shift
+    new_turn
+  end
+
+  def correct?
+    turns.last.correct?
+  end
+
+  def number_correct
+    correct = []
+    @turns.each do |turn|
+      if turn.correct?
+        correct << turn
+      else
+      end
+    end
+    correct.count
+  end
+
+  def number_correct_in_category(category)
+    @turns.find_all { |turn| turn.correct? && turn.card.category == category}.count
+  end
+
+  def percent_correct
+    (number_correct.to_f / @turns.count.to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    (number_correct_in_category(category).to_f / \
+    @turns.find_all {|turn| turn.card.category == category}.count.to_f) * 100
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+end
