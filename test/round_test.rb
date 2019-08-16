9bca678263baad12336f6d78c4fd668a70c88639
Scott Regenthal <scottregenthal@gmail.com>
2019-08-16 06:17:11 -0600

    Add tests

---
+++ test/round_test.rb
@@ -0,0 +1,31 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+class RoundTest < Minitest::Test
+
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @round = Round.new(@deck)
+  end
+
+  def test_round_exists
+    assert_instance_of Round, @round
+  end
+
+  def test_turns_method
+    assert_equal [], @round.turns
+  end
+
+  def test_current_card_method
+    assert_equal @card_1, @round.current_card
+  end
+
+end
