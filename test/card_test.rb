31b47aaee9f28229d20058861a3b5b2020c071f9
Laura Schulz <lrs8810@gmail.com>
2019-08-16 11:32:04 -0600

    Add setup method

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,28 +3,23 @@ require 'minitest/pride'
 require './lib/card'
 
 class CardTest < Minitest::Test
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  end
 
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_instance_of Card, card
+    assert_instance_of Card, @card
   end
 
   def test_it_has_a_question
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_equal "What is the capital of Alaska?", card.question
+    assert_equal "What is the capital of Alaska?", @card.question
   end
 
   def test_it_has_an_answer
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_equal "Juneau", card.answer
+    assert_equal "Juneau", @card.answer
   end
 
   def test_it_has_a_category
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_equal :Geography, card.category
+    assert_equal :Geography, @card.category
   end
 end
