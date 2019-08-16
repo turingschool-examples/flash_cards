a52bc5fa97e8d400e02e624da5578a949589d899
Michael Cooper <macoop2363@mgmail.com>
2019-08-16 16:44:56 -0600

    Created initializer and a few method templates

--- lib/card_manager.rb
+++ lib/card_manager.rb
@@ -1,7 +1,24 @@
 class CardManager
+  attr_reader :cards_by_file
 
   def initialize(file_name=nil)
+    @cards_by_file = {}
     @file_name = file_name
   end
+
+  def load_from_file(file_name) 
+    if @cards_by_file.has_key?
+      puts "File already loaded!"
+    else
+      # TODO Open file and load
+    end
+  end
+
+  # def valid?(line)
+  #   data = line.split(',')
+  #   return data if data.length == 3
+  #     puts "Invalid line format."
+  # end
+
 
 end
