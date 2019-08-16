35ae1b4ca72f7776eaf691836fa70efd179398b4
Laura Schulz <lrs8810@gmail.com>
2019-08-15 20:58:26 -0600

    Add entire turn file

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    @guess == @card.answer
+  end
+
+  def feedback
+    if correct?
+      p "Correct!"
+    else
+      p "Incorrect!"
+    end
+  end
+end
