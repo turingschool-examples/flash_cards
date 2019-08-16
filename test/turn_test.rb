0ee5fcb2607624b2916d4e64bdf2cac8ff16e34e
John Travers <johnk.travers@gmail.com>
2019-08-16 08:50:31 -0600

    Add test for case insensitivity

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -9,6 +9,7 @@ class TurnTest < Minitest::Test
         @card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
         @turn_1 = Turn.new("Saturn", @card)
         @turn_2 = Turn.new("Mercury", @card)
+        @turn_3 = Turn.new("MErCurY", @card)
     end
 
     def test_it_exists
@@ -26,6 +27,10 @@ class TurnTest < Minitest::Test
     def test_correct_method_returns_boolean
         refute @turn_1.correct?
         assert @turn_2.correct?
+    end
+
+    def test_correct_method_is_case_insensitive
+        assert @turn_3.correct?
     end
 
     def test_feedback_method_returns_string
