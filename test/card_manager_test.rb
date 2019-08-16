d9c73b7708322238b55d35b0b9b629eb8c159a9f
Michael Cooper <macoop2363@mgmail.com>
2019-08-16 12:29:11 -0600

    Added additional skeleton tests

--- test/card_manager_test.rb
+++ test/card_manager_test.rb
@@ -14,4 +14,19 @@ class CardManagerTest < Minitest::Test
     assert_instance_of CardManager, @card_manager
   end
 
+  def test_validate_card_data 
+    # make sure data is allowable to make a card
+  end
+
+  def test_create_card
+    assert_instance_of Card, true
+  end
+
+  def test_load_cards_from_file
+    # load cards from file
+  end
+
+  def test_save_cards_to_file
+  end
+
 end
