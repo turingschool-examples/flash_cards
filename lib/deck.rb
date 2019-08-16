836026d29c256c7f7b765c79e2565d3781605166
Laura Schulz <lrs8810@gmail.com>
2019-08-15 20:59:52 -0600

    Add entire deck file

---
+++ lib/deck.rb
@@ -0,0 +1,25 @@
+require './lib/card'
+require './lib/turn'
+
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @cards.length
+  end
+
+  def cards_in_category(category)
+    @category_cards =[]
+    @cards.each do |card|
+        if card.category == category
+          @category_cards << card
+        end
+    end
+    @category_cards
+  end
+
+end
