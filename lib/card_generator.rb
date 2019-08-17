649b9e2ea8a5978bf35027d26e3630b5b7e3a8bf
Alice Post <ap2322@gmail.com>
2019-08-16 23:01:56 -0600

    Testing CardGenerator's ability to map the text file into an array of hashes. Next step is to instantiate Cards using each hash.

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -2,7 +2,28 @@ class CardGenerator
   attr_reader :card_array
 
   def initialize(filename)
-    @card_array  = File.open(filename).map {|line| line.split("\n") }
+    @card_array1  = File.open(filename).map {|line| line.split("\n") }
+    @card_array = @card_array1.flatten
+    # I have an array of strings separated by commas
   end
 
+    # I want each string in the array to be split on the commas
+    def string_split_card
+      @card_array.map {|card| card.split(",")}
+    end
+
+    # then each item is mapped to a Card class
+    # question, answer, category
+    def questions
+      self.string_split_card.map {|string| string[0]}
+    end
+
+    # OR map the string_split_card array to a hash with question, answer, cards_in_category
+    def card_hash
+      card_hash_indv_keys = [:question, :answer, :category]
+      @card_hash_array = self.string_split_card.map do |card_array|
+        card_hash_indv_keys.zip(card_array).to_h
+      end
+      @card_hash_array
+    end
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -18,7 +18,36 @@ class CardGeneratorTest < Minitest::Test
   end
 
   def test_cards_generated_is_array_of_4
-    assert_equal 4, @cards.card_array
+    assert_equal 4, @cards.card_array.length
   end
 
+  def test_first_string_in_array
+    assert_equal "What is 5 + 5?,10,STEM", @cards.card_array[0]
+  end
+
+  def test_map_first_string_to_array
+    assert_equal ["What is 5 + 5?","10","STEM"], @cards.string_split_card[0]
+  end
+
+  def test_find_card_question
+    assert_equal "What is 5 + 5?", @cards.questions[0]
+  end
+
+  def test_card_hash_keys
+    assert_equal [:question, :answer, :category], @cards.card_hash[0].keys
+    assert_instance_of Hash, @cards.card_hash[0]
+  end
+
+  # each array of strings needs to be mapped to the hash_keys
+  # for each array, value[0] = question, value[1] answer, and value[2], cards_in_category
+  def test_assign_question_to_hash
+    #found an index
+    assert_equal "What is Mike's middle name?", @cards.string_split_card[2][0]
+    assert_equal "nobody knows", @cards.card_hash[2][:answer]
+  end
+
+  # def test_card_array_becomes_card_hash
+  #   # mapping the stirng_split_card array to a hash with question, answer, and category keys
+  #   assert_equal {:question "What is 5 + 5?", :answer "10", :category "STEM"}, @cards
+  # end
 end
