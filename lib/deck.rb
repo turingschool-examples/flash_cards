73ab8da8f72d60b4c493dd77878eeb4b3ce59f88
Scott Regenthal <scottregenthal@gmail.com>
2019-08-17 15:03:24 -0600

    Change method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,22 +1,22 @@
 class Deck
-
   attr_reader :cards
+
   def initialize(cards)
     @cards = cards
   end
 
   def count
-    @cards.length
+    @cards.count
   end
 
   def cards_in_category(category)
     return_array = []
-    @cards.each do |card|
+    cards.each do |card|
       if card.category == category
         return_array.push(card)
       end
     end
-    return return_array
+    return_array
   end
 
 end
