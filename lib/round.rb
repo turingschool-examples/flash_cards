14e9c851c7e9065355f1f9870fe9febc4a485538
sasloan <sebastiansloan94@gmail.com>
2019-08-17 14:48:28 -0600

    Reset to correct test

--- lib/round.rb
+++ lib/round.rb
@@ -4,21 +4,16 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
+    @current_card = @deck.cards[0]
     @number_correct = 0
   end
 
-  def current_card
-    @deck.cards[0]
-  end
-
-
-
   def take_turn(guess)
-    new_turn = Turn.new(guess, current_card)
+    new_turn = Turn.new(guess, @current_card)
     @turns << new_turn
     if new_turn.correct?
       @number_correct += 1
     end
-    new_turn  
+    new_turn
   end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -11,24 +11,13 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
     @round = Round.new(@deck)
     @new_turn = @round.take_turn("Juneau")
   end
 
   def test_it_exists
     assert_instance_of Round, @round
-  end
-
-  def test_turns_exist
-    assert_equal  @round.turns
-  end
-
-  def test_can_call_current_card
-    assert_equal @round.current_card
-  end
-
-  def test_you_can_take_another_turn
-    assert_equal "Juneau", @round.take_turn("Juneau")
   end
 end
