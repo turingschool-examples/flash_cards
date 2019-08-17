779b1ea37eca100065001614a9096e02a0f7a9ea
Laura Schulz <lrs8810@gmail.com>
2019-08-16 19:50:09 -0600

    Add working current_card method

---
+++ lib/round.rb
@@ -0,0 +1,16 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+
+class Round
+  attr_reader :deck, :turns
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    @deck.cards.shift
+  end
+
+end
