bba406b5086b1621a1fc2cdd02c65371652a6664
Scott Regenthal <scottregenthal@gmail.com>
2019-08-17 15:05:20 -0600

    Change style

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,18 +1,17 @@
 class Turn
-
   attr_reader :guess, :card
+
   def initialize(guess, card)
     @guess = guess
     @card = card
   end
 
   def correct?
-    @guess == card.answer
+    guess == card.answer
   end
 
   def feedback
     return "Correct!" if correct?
     "Incorrect."
   end
-
 end
