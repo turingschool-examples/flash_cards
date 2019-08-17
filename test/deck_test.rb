878bcca2aaa04bd59eee14acf60657210beea000
Kayden Drake <root@localhost.localdomain>
2019-08-17 14:58:38 -0600

    Adds skeleton deck test and deck file

---
+++ test/deck_test.rb
@@ -0,0 +1,20 @@
+require 'minitest'
+require 'minitest/autorun'
+
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+  def setup
+    @card_1 = Card.new("Who is the cutest cat in the whole world?", "Crona", :Pop_Culture)
+    @card_2 = Card.new("What is the corp that cares?", "Evo", :Corporate_Knowledge)
+    @card_3 = Card.new("Which corp will kill you if you fail a run?", "Mitsuhama", :Corporate_Knowledge)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+  end
+
+  #test exists
+  #test holds cards
+  #test count
+  #test cards_in_category with several categories
+end
