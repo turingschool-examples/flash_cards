86217628f7d11950bb133b0bc27f08df0cb02e9e
Brian Bower <brian@brianbower.com>
2019-08-16 23:06:06 -0600

    A good copy before making number_correct and array and the Number_correct method to count the items in the array.

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,7 @@
 #  Round will be the object that processes responses and records guesses. A Round is initialized with a Deck. The idea is that when we start a Round, the current card is the first in the deck (the first in the Deck’s array of Cards). When we make a guess, the guess is recorded, and the next card in the deck becomes the current card.
 
 # The take_turn method is the crux of this problem. The take_turn method takes a string representing the guess. It should create a new Turn object with the appropriate guess and Card. It should store this new Turn, as well as return it from the take_turn method. Also, when the take_turn method is called, the Round should move on to the next card in the deck.
+require 'pry'
 
 class Round
   attr_reader :deck, :turns, :number_correct
@@ -16,7 +17,6 @@ class Round
   end
 
   def take_turn(guess)
-    #attr_reader :guess
   # Create new Turn object with guess and card. It should store this new Turn, as well as return it from the take_turn method.
     new_turn = Turn.new(guess, current_card)
     @turns << new_turn
@@ -26,11 +26,15 @@ class Round
      new_turn
   end
 
-  def number_correct
-    if # my guess is correct
-      @number_correct += 1
-    end
-  end
+
+
+  # HERE IS WHERE I STOPPED FOR THE NIGHT BUT THE METHOD DOEST WORK...
+  # def number_correct
+  #   # if my guess from the take_turn matches the
+  #   # if @round.take_turn(guess) == @deck.current_card.answer
+  #     @number_correct += 1
+  #   end
+  # end
 
   # def number_correct_by_category(category_string)
   #
