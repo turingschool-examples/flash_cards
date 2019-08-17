140394511a02da649acb8d39e9803e0a7f4fe895
Laura Schulz <lrs8810@gmail.com>
2019-08-16 19:51:47 -0600

    Add test_current_card method

---
+++ test/round_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+require 'pry'
+
+class RoundTest < Minitest::Test
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5 clockwise from due north?", "North north west", :STEM)
+
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+
+    @round = Round.new(@deck)
+  end
+
+  def test_round_exits
+    assert_instance_of Round, @round
+  end
+
+  def test_turns
+    assert_equal [], @round.turns
+    binding.pry
+  end
+
+  def test_current_card
+    assert_equal @card_1, @round.current_card
+  end
+
+end
