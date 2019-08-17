2f3d565f3ebc550b3350b17d6f0388f643367836
Zac Isaacson <isaacson.zm@gmail.com>
2019-08-17 11:25:15 -0600

    add skeletal round class

---
+++ test/round_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+class RoundTest < Minitest::Test
+
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @round = Round.new(@deck)
+  end
+
+  def test_it_exists
+    assert_instance_of Round, @round
+  end
+
+  # def test_round_has_deck
+  #   assert_equal @deck, @round.deck
+  # end
+
+end
