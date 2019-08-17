b333b431126dc557ffb4716e9a7b8a8a9fd552bb
Scott Regenthal <scottregenthal@gmail.com>
2019-08-17 15:07:50 -0600

    Add method tests

--- test/round_test.rb
+++ test/round_test.rb
@@ -12,6 +12,8 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @turn_1 = Turn.new("Juneau", @card_1)
+    @turn_2 = Turn.new("Saturn", @card_2)
     @deck = Deck.new([@card_1, @card_2, @card_3])
     @round = Round.new(@deck)
   end
@@ -24,8 +26,55 @@ class RoundTest < Minitest::Test
     assert_equal [], @round.turns
   end
 
-  def test_current_card_method
+  def test_current_card
     assert_equal @card_1, @round.current_card
+    assert_equal @card_2, @round.current_card
+    assert_equal @card_3, @round.current_card
   end
 
+  # def test_take_turn_method
+  #   assert_equal @turn_1, @round.take_turn("Juneau")
+  #   assert_equal @turn_2, @round.take_turn("Venus")
+  # end
+
+  def test_class
+    new_turn_1 = @round.take_turn("Juneau")
+
+    assert Turn, new_turn_1.class
+  end
+
+  def test_correct
+    new_turn_1 = @round.take_turn("Juneau")
+
+    assert new_turn_1.correct?
+  end
+
+  def test_turns
+    assert_equal [], @round.turns
+  end
+
+  def test_number_correct
+    new_turn_1 = @round.take_turn("Juneau")
+    new_turn_2 = @round.take_turn("Venus")
+
+    assert 1, @round.number_correct
+  end
+
+  def test_turns_count
+    new_turn_1 = @round.take_turn("Juneau")
+    new_turn_2 = @round.take_turn("Venus")
+
+    assert_equal 2, @round.turns.count
+  end
+
+  def test_turns_last_feedback
+    new_turn_1 = @round.take_turn("Juneau")
+    new_turn_2 = @round.take_turn("Venus")
+
+    assert_equal "Incorrect.", @round.turns.last.feedback
+  end
+
+  def test_number_correct_by_category
+    
+  end
 end
