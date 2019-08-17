821cfcbd83883e53574a8bc0c7cb26086129017e
Kayden Drake <root@localhost.localdomain>
2019-08-17 14:22:03 -0600

    Adds test that turn.correct? works

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,6 +8,7 @@ class TurnTest < Minitest::Test
   def setup
     @card = Card.new("Who is the cutest cat in the whole world?", "Crona", :Pop_Culture)
     @turn = Turn.new("Crona", @card)
+    @turn_wrong_answer = Turn.new("A butt", @card)
   end
 
   def test_turn_exists
@@ -17,6 +18,11 @@ class TurnTest < Minitest::Test
   def test_turn_inits_and_exposes_attributes
     assert_equal @card, @turn.card
     assert_equal "Crona", @turn.guess
+  end
+
+  def test_turn_correct_method
+    assert_equal true, @turn.correct?
+    assert_equal false, @turn_wrong_answer.correct?
   end
 
 end
