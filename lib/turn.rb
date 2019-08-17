ade866e7820c00e11a4b18436abe4b4f11eecfd1
Kayden Drake <root@localhost.localdomain>
2019-08-17 14:23:54 -0600

    Adds turn.correct? code

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,5 @@
 require './lib/card'
+require 'pry'
 
 class Turn
   attr_reader :guess, :card
@@ -6,5 +7,9 @@ class Turn
   def initialize(new_guess, new_card)
     @guess = new_guess
     @card = new_card
+  end
+
+  def correct?
+    @guess == @card.answer
   end
 end
