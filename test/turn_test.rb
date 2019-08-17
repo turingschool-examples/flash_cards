585c61ca0ac1fbbdd8ac87de8272797bf21437af
Kayden Drake <root@localhost.localdomain>
2019-08-17 13:53:08 -0600

    Adds turn test setup

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,4 +5,8 @@ require './lib/card'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
+  def setup
+    @card = Card.new("Who is the cutest cat in the whole world?", "Crona", :Pop_Culture)
+    @turn = Turn.new("Crona", @card)
+  end
 end
