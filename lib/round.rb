06a3ba9b0fc461d1ea4ca551a9eef1427248ba38
Scott Regenthal <scottregenthal@gmail.com>
2019-08-17 15:04:00 -0600

    Add methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,16 +1,42 @@
 class Round
+  attr_reader :deck, :turns, :current_card, :correct_turns
 
-  attr_reader :deck
   def initialize(deck)
     @deck = deck
-  end
-
-  def turns
-    []
+    @turns = []
+    @correct_turns = []
+    @current_card = deck.cards[0]
   end
 
   def current_card
-    
+    deck.cards.shift
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    turns << new_turn
+    deck.cards.shift
+    new_turn
+  end
+
+  def class
+  end
+
+  def correct?
+    guess == new_card.answer
+  end
+
+  def number_correct
+    turns.find_all do |turn|
+      if turn.correct?
+        correct_turns << turns
+      end
+      correct_turns.count
+    end
+  end
+
+  def count
+    @round.turns.count
   end
 
 end
