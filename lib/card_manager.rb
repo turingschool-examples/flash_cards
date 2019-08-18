add1d51e8bbcc66b8db292bcdcea74a632dd4545
Michael Cooper <macoop2363@gmail.com>
2019-08-18 10:52:08 -0600

    Completed loading from file

--- lib/card_manager.rb
+++ lib/card_manager.rb
@@ -1,3 +1,4 @@
+#require 
 class CardManager
   attr_reader :cards_by_file
 
@@ -7,10 +8,20 @@ class CardManager
   end
 
   def load_from_file(file_name) 
-    if @cards_by_file.has_key?
+    if @cards_by_file.has_key?(file_name)
       puts "File already loaded!"
     else
-      # TODO Open file and load
+      if File.exist?(file_name)
+        @cards_by_file[file_name] = []
+        file_data = File.read(file_name).split("\n")
+        file_data.map! { |data| data.split(',') }
+        file_data.keep_if { |data| data.length == 3}
+        file_data.each do |data| 
+          @cards_by_file[file_name].push(Card.new(data[0], data[1], data[2]))
+        end
+      else
+        puts "File does not exist!"
+      end
     end
   end
 
