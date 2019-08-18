b753ba982eab6017e2a78b0a8f600ecb613a7847
Michael Cooper <macoop2363@mgmail.com>
2019-08-18 12:13:55 -0600

    Removed static cards and now uses CardManager

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,19 +1,13 @@
 require './lib/card'
+require './lib/card_manager'
 require './lib/deck'
 require './lib/turn'
 require './lib/round'
 
 def start
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  card_4 = Card.new("Where is Turing based out of?", "Denver", :Geography)
-  card_5 = Card.new("What is 5 + 5?", "10", :STEM)
-  card_6 = Card.new("What is Rachel's favorite animal?", "red panda", "Turing Staff".to_sym)
-  card_7 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff".to_sym)
-  card_8 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture".to_sym)
-  cards = [card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8]
-  deck = Deck.new(cards)
+  file_name = './data/deck_1.txt'
+  card_manager = CardManager.new(file_name)
+  deck = Deck.new(card_manager.cards_by_file[file_name])
   round = Round.new(deck)
   total_cards = round.deck.count
   
