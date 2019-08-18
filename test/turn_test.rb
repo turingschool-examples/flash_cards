2deff28192cad6c5776b330e05737503d1612d46
John Travers <johnk.travers@gmail.com>
2019-08-18 10:22:49 -0600

    Update instance variable names in turn test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -7,35 +7,35 @@ class TurnTest < Minitest::Test
 
     def setup
         @card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-        @turn_1 = Turn.new("Saturn", @card)
-        @turn_2 = Turn.new("Mercury", @card)
-        @turn_3 = Turn.new("MErCurY", @card)
+        @turn_wrong = Turn.new("Saturn", @card)
+        @turn_right = Turn.new("Mercury", @card)
+        @turn_case = Turn.new("MErCurY", @card)
     end
 
     def test_it_exists
-        assert_instance_of Turn, @turn_1
+        assert_instance_of Turn, @turn_wrong
     end
 
     def test_it_has_a_card
-        assert_equal @card, @turn_1.card
+        assert_equal @card, @turn_wrong.card
     end
 
     def test_it_has_a_guess
-        assert_equal "Saturn", @turn_1.guess
+        assert_equal "Saturn", @turn_wrong.guess
     end
 
     def test_correct_method_returns_boolean
-        refute @turn_1.correct?
-        assert @turn_2.correct?
+        refute @turn_wrong.correct?
+        assert @turn_right.correct?
     end
 
     def test_correct_method_is_case_insensitive
-        assert @turn_3.correct?
+        assert @turn_case.correct?
     end
 
     def test_feedback_method_returns_string
-        assert_equal "Incorrect.", @turn_1.feedback
-        assert_equal "Correct!", @turn_2.feedback
+        assert_equal "Incorrect.", @turn_wrong.feedback
+        assert_equal "Correct!", @turn_right.feedback
     end
 
 end
