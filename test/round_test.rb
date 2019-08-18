911253461a0a1db1c5bf74a0766fe6d5ac7bb401
John Travers <johnk.travers@gmail.com>
2019-08-18 11:15:49 -0600

    Add testing file for round class

--- test/round_test.rb
+++ test/round_test.rb
@@ -1,51 +1,92 @@
+require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+class RoundTest < Minitest::Test
 
-deck = Deck.new([card_1, card_2, card_3])
-round = Round.new(deck)
+    def setup
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-puts round.deck
-p round.turns
-p round.current_card
+        @deck = Deck.new([@card_1, @card_2, @card_3])
+        @round = Round.new(@deck)
+    end
 
-puts '-' * 10
+    def test_it_exists
+        assert_instance_of Round, @round
+    end
 
-turn_1 = round.take_turn("Juneau")
+    def test_it_has_a_deck
+        assert_equal @deck, @round.deck
+    end
 
-puts turn_1.class
-puts turn_1.correct?
+    def test_user_can_take_a_turn
+        turn_1 = @round.take_turn("Juneau")
 
-puts '-' * 10
+        assert_instance_of Turn, turn_1
+    end
 
-p round.turns
-puts round.number_correct
+    def test_turns_is_empty_at_start_of_round
+        assert_equal [], @round.turns
+    end
 
-puts '-' * 10
+    def test_turns_are_stored_and_returned
+        turn_1 = @round.take_turn("Juneau")
+        turn_2 = @round.take_turn("Jupiter")
 
-p round.current_card
+        assert_equal [turn_1, turn_2], @round.turns
+    end
 
-turn_2 = round.take_turn("Venus")
+    def test_current_card_is_returned
+        assert_equal @card_1, @round.current_card
 
-puts '-' * 10
+        turn_1 = @round.take_turn("Juneau")
+        assert_equal @card_2, @round.current_card
 
-puts round.turns.count
-puts round.turns.last.feedback
-puts round.number_correct
-puts round.number_correct_by_category(:Geography)
-puts round.number_correct_by_category(:STEM)
+        turn_2 = @round.take_turn("Jupiter")
+        assert_equal @card_3, @round.current_card
 
-puts '-' * 10
+        turn_3 = @round.take_turn("North north west")
+        assert_nil @round.current_card
+    end
 
-puts round.percent_correct
-puts round.percent_correct_by_category(:Geography)
-puts round.number_correct_by_category(:STEM)
+    def test_number_of_correct_guesses
+        turn_1 = @round.take_turn("Juneau")
+        turn_2 = @round.take_turn("Jupiter")
+        turn_3 = @round.take_turn("North north west")
 
-puts '-' * 10
+        assert_equal 2, @round.number_correct
+    end
 
-p round.current_card
+    def test_number_of_correct_guesses_by_category
+        turn_1 = @round.take_turn("Juneau")
+        turn_2 = @round.take_turn("Jupiter")
+        turn_3 = @round.take_turn("North north west")
+
+        assert_equal 1, @round.number_correct_by_category(:Geography)
+        assert_equal 1, @round.number_correct_by_category(:STEM)
+        assert_equal 0, @round.number_correct_by_category("Pop Culture")
+    end
+
+    def test_overall_percent_correct
+        turn_1 = @round.take_turn("Juneau")
+        turn_2 = @round.take_turn("Jupiter")
+        turn_3 = @round.take_turn("North north west")
+
+        assert_in_delta 66.6666666, @round.percent_correct, 0.0001
+    end
+
+    def test_percent_correct_by_category
+        turn_1 = @round.take_turn("Juneau")
+        turn_2 = @round.take_turn("Jupiter")
+        turn_3 = @round.take_turn("North north west")
+
+        assert_equal 100.0, @round.percent_correct_by_category(:Geography)
+        assert_equal 50.0, @round.percent_correct_by_category(:STEM)
+        assert_equal 0.0, @round.percent_correct_by_category("Pop Culture")
+    end
+end
