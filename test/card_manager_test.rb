37a25af33361ee9c2b7778302ef0f8c67500b0b1
Michael Cooper <macoop2363@gmail.com>
2019-08-18 10:30:04 -0600

    Fixed test to actually call the method needed to test

--- test/card_manager_test.rb
+++ test/card_manager_test.rb
@@ -6,6 +6,15 @@ require './lib/card_manager'
 class CardManagerTest < Minitest::Test
   
   def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @card_4 = Card.new("Where is Turing based out of?", "Denver", :Geography)
+    @card_5 = Card.new("What is 5 + 5?", "10", :STEM)
+    @card_6 = Card.new("What is Rachel's favorite animal?", "red panda", "Turing Staff".to_sym)
+    @card_7 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff".to_sym)
+    @card_8 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture".to_sym)
+    @cards = [@card_1, @card_2, @card_3, @card_4, @card_5, @card_6, @card_7, @card_8]
     @file_name = "./data/deck_1.txt"
     @card_manager = CardManager.new
   end
@@ -28,8 +37,9 @@ class CardManagerTest < Minitest::Test
     assert_instance_of Card, true
   end
 
-  def test_load_cards_from_file
-    # load cards from file
+  def test_load_from_file
+    @card_manager.load_from_file(@file_name)
+    assert_equal @cards, @card_manager.cards_by_file[@file_name]
   end
 
   def test_save_cards_to_file
