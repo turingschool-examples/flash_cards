85281bfae9d17780aebe96fbe03fcb82d58dedca
Alice Post <ap2322@gmail.com>
2019-08-17 19:51:51 -0600

    Cleaning up indents

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -7,32 +7,32 @@ class CardGenerator
     # I have an array of strings separated by commas
   end
 
-    # I want each string in the array to be split on the commas
-    def string_split_card
-      @card_array.map {|card| card.split(",")}
-    end
+  # I want each string in the array to be split on the commas
+  def string_split_card
+    @card_array.map {|card| card.split(",")}
+  end
 
-    # then each item is mapped to a Card class
-    # question, answer, category
-    def questions
-      self.string_split_card.map {|string| string[0]}
-    end
+  # then each item is mapped to a Card class
+  # question, answer, category
+  def questions
+    self.string_split_card.map {|string| string[0]}
+  end
 
-    # map the string_split_card array to a hash with question, answer, cards_in_category
-    def card_hash
-      card_hash_indv_keys = [:question, :answer, :category]
-      card_hash_array = self.string_split_card.map do |card_array|
-        card_hash_indv_keys.zip(card_array).to_h
-      end
-      card_hash_array
+  # map the string_split_card array to a hash with question, answer, cards_in_category
+  def card_hash
+    card_hash_indv_keys = [:question, :answer, :category]
+    card_hash_array = self.string_split_card.map do |card_array|
+      card_hash_indv_keys.zip(card_array).to_h
     end
+    card_hash_array
+  end
 
-    # make a card from the card_hash
-    def cards
-      @cards_in_array = []
-      self.card_hash.each do |card|
-        @cards_in_array << Card.new(card[:question], card[:answer], card[:category])
-      end
-      @cards_in_array
+  # make a card from the card_hash
+  def cards
+    @cards_in_array = []
+    self.card_hash.each do |card|
+      @cards_in_array << Card.new(card[:question], card[:answer], card[:category])
     end
+    @cards_in_array
+  end
 end
