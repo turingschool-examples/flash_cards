3b4da6a5388eccd9247bc8357a2e1b09ccdab4c7
Tyler Balogh <tyler_balogh@yahoo.com>
2019-08-18 23:03:23 -0600

    updated

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -25,7 +25,7 @@ puts "This is card number 1 out of 4.\n\n"
 @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   puts "#{@card_1.question}"
     @guess = gets.chomp
-      if @guess == @card_1.answer.capitalize
+      if @guess == @card_1.answer.downcase
         puts "Correct!"
       else
         puts "Incorrect!\n\n"
@@ -35,17 +35,17 @@ puts "This is card number 2 out of 4.\n\n"
 @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
   puts "#{@card_2.question}"
     @guess = gets.chomp
-      if @guess == @card_2.answer.capitalize
+      if @guess == @card_2.answer.downcase
         puts "Correct!"
       else
         puts "Incorrect!\n\n"
       end
 
-puts "This is card number 3 out of 4."
+puts "This is card number 3 out of 4.\n\n"
   @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     puts "#{@card_3.question}"
       @guess = gets.chomp
-        if @guess == @card_3.answer.capitalize
+        if @guess == @card_3.answer.downcase
           puts "Correct!"
         else
           puts "Incorrect!\n\n"
@@ -55,11 +55,11 @@ puts "This is card number 3 out of 4.\n\n"
 @card_4 = Card.new("What is the capital of North Dakota?", "Bismark", :Geography)
     puts "#{@card_4.question}"
       @guess = gets.chomp
-        if @guess == @card_4.answer.capitalize
+        if @guess == @card_4.answer.downcase
           puts "Correct!"
         else
           puts "Incorrect!\n\n"
         end
       end
 
-puts "************ Game Over! *************"
+puts "************ Game Over! *************\n\n"
