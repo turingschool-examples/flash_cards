c6276d9d0f11f87986e52ff6f42699934b3e7e95
Scott Regenthal <scottregenthal@gmail.com>
2019-08-19 06:06:39 -0600

    Add start, start_turn, categories, game_over methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,33 +1,30 @@
+require './lib/turn'
+
 class Round
-  attr_reader :deck, :turns, :current_card, :correct_turns
+  attr_reader :deck, :turns, :current_card
 
   def initialize(deck)
     @deck = deck
     @turns = []
-    @correct_turns = []
     @current_card = deck.cards[0]
   end
 
-  def current_card
-    deck.cards.shift
-  end
-
+# make & record a guess
+# create a new Turn object w/ respective guess & card
+# next card in deck becomes current card
+# store & return new Turn object
   def take_turn(guess)
-    new_turn = Turn.new(guess, current_card)
-    turns << new_turn
-    deck.cards.shift
-    new_turn
-  end
-
-  def correct?
-    guess == new_card.answer
+    turn = Turn.new(guess, current_card)
+    current_card = deck.cards.shift
+    turns << turn
+    turn
   end
 
   def number_correct
     turns.find_all do |turn|
       turn.correct?
     end
-    correct_turns.count
+    .count
   end
 
   def count
@@ -38,7 +35,7 @@ class Round
     turns.find_all do |turn|
       turn.correct? && turn.card.category == category
     end
-    turns.count
+    .count
   end
 
   def percent_correct
@@ -49,10 +46,47 @@ class Round
     turns.find_all do |turn|
       turn.card.category == category
     end
-    turns.count
+    .count
   end
 
   def percent_correct_by_category(category)
     (number_correct_by_category(category) / cards_in_category(category).to_f) * 100
+  end
+
+  # starts the round using start method
+  def start
+    total_cards = deck.count + turns.count + 1
+    puts "Welcome! You're playing with #{total_cards} cards."
+    puts "-------------------------------------------------"
+    start_turn
+  end
+
+  def start_turn
+    card_number = turns.count + 1
+    total_cards = deck.count + turns.count + 1
+    puts "This is card number #{card_number} out of #{total_cards}."
+    puts "Question: #{@current_card.question}"
+    guess = gets.chomp
+    puts take_turn(guess).feedback
+    if card_number == total_cards
+      game_over
+    else
+      start_turn
+    end
+  end
+
+  def categories
+    turns.map do |turn|
+      turn.card.category
+    end
+    turns.uniq
+  end
+
+  def game_over
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of #{turns.count} for a total score of #{percent_correct}%."
+    categories.each do |category|
+      puts "#{category} - #{percent_correct_by_category(category)}% correct"
+    end
   end
 end
