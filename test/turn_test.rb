45d055ddd7bb1187ed6761a572fee8b6470e4771
Rachel Lew <rlew07@gmail.com>
2019-08-18 22:46:38 -0600

    Rename string to guess

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -7,36 +7,30 @@ class TurnTest < Minitest::Test
 
   def setup
     @card2 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card2)
   end
 
   def test_it_exists
-    turn = Turn.new("Juneau", @card2)
-
-    assert_instance_of Turn, turn
+    assert_instance_of Turn, @turn
   end
 
   def test_it_has_a_guess
-    turn = Turn.new("Juneau", @card2)
-
-    assert_equal "Juneau", turn.guess
+    assert_equal "Juneau", @turn.guess
   end
 
   def test_it_has_a_card
-    turn = Turn.new("Juneau", @card2)
-
-    assert_equal @card2, turn.card
+    assert_equal @card2, @turn.card
   end
 
   def test_it_is_correct
-    turn = Turn.new("Juneau", @card2)
-
-    assert_equal true, turn.correct?
+    assert_equal true, @turn.correct?
+    assert_equal "Correct!", @turn.feedback
   end
 
-  def test_it_provides_feedback
-    turn_1 = Turn.new("Juneau", @card2)
-    turn_2 = Turn.new("Anchorage", @card2)
+  def test_it_is_incorrect
+    wrong_turn = Turn.new("Anchorage", @card2)
 
-    assert_equal turn_1.feedback, "Correct!"
+    assert_equal false, wrong_turn.correct?
+    assert_equal "Incorrect!", wrong_turn.feedback
   end
 end
