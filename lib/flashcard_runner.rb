cb56c5ade291b347f63a9b557b480ac89d96b0a3
Graham Thompson <grwthomps@gmail.com>
2019-08-18 19:55:20 -0600

    Add comments to flashcard_runner file to make code more readable

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -5,6 +5,7 @@ require './lib/round'
 
 def start
 
+  # Setup
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -15,36 +16,49 @@ def start
 
   round = Round.new(deck)
 
+  # Turn 1
   puts "Welcome! You're playing with 4 cards."
   puts "-------------------------------------------------"
   puts "This is card number 1 out of 4."
   puts "Question: #{card_1.question}"
   print "> "
+
   guess = gets.chomp
   turn = round.take_turn(guess)
   turn.correct?
   puts turn.feedback
+
+  # Turn 2
   puts "This is card number 2 out of 4."
   puts "Question: #{card_2.question}"
   print "> "
+
   guess = gets.chomp
   turn = round.take_turn(guess)
   turn.correct?
   puts turn.feedback
+
+  # Turn 3
   puts "This is card number 3 out of 4."
   puts "Question: #{card_3.question}"
   print "> "
+
   guess = gets.chomp
   turn = round.take_turn(guess)
   turn.correct?
   puts turn.feedback
+
+  # Turn 4
   puts "This is card number 4 out of 4."
   puts "Question: #{card_4.question}"
   print "> "
+
   guess = gets.chomp
   turn = round.take_turn(guess)
   turn.correct?
   puts turn.feedback
+
+  # End of game
   puts "****** Game over! ******"
   puts "You had #{round.number_correct} correct guesses out of #{round.turns.count} for a total score of #{round.percent_correct}%."
   puts "Geography - #{round.percent_correct_by_category(:Geography)}% correct"
@@ -53,6 +67,7 @@ def start
 
 end
 
+# Pre-game welcome and input
 puts "Welcome to Flashcards! Enter 'Start' to begin."
 print "> "
 
