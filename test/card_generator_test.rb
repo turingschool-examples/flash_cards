fecbdae73a56947c5919069eed4e3b563e878ccd
John Travers <johnk.travers@gmail.com>
2019-08-18 22:14:16 -0600

    Add testing file for card generator class

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -12,12 +12,12 @@ class CardGeneratorTest < Minitest::Test
         @filename = "lib/cards.txt"
         @actual_cards = CardGenerator.new(@filename)
 
-        @card_1 = Card.new("India became an independent country in what year?","1947","History")
-        @card_2 = Card.new("What is the name of the strait of water that separates the two main islands of New Zealand?","Cook Strait","Geography")
-        @card_3 = Card.new("What was the first nation in the Western Hemisphere with a woman as head of state?","Argentina","History")
-        @card_4 = Card.new("What distant planet circles the Sun every 84 years?","Uranus","Science")
-        @card_5 = Card.new("What is the only fruit to have its seeds on the outside?","Strawberry","Science")
-        @card_6 = Card.new("Which is the largest country in Africa by area?","Algeria","Geography")
+        @card_1 = Card.new("India became an independent country in what year?", "1947", :History)
+        @card_2 = Card.new("What is the name of the strait of water that separates the two main islands of New Zealand?", "Cook Strait", :Geography)
+        @card_3 = Card.new("What was the first nation in the Western Hemisphere with a woman as head of state?", "Argentina", :History)
+        @card_4 = Card.new("What distant planet circles the Sun every 84 years?", "Uranus", :Science)
+        @card_5 = Card.new("What is the only fruit to have its seeds on the outside?", "Strawberry", :Science)
+        @card_6 = Card.new("Which is the largest country in Africa by area?", "Algeria", :Geography)
 
         @expected_cards = [@card_1, @card_2, @card_3, @card_4, @card_5, @card_6]
     end
@@ -33,10 +33,5 @@ class CardGeneratorTest < Minitest::Test
             assert_equal card.category, @actual_cards.cards[index].category
         end
     end
-
-    def test_it_deals_with_a_wonky_file
-
-    end
-
 
 end
