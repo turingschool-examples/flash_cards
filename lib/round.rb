16833679d9e48e1beac03e9a882ddea6a327c309
Zac Isaacson <isaacson.zm@gmail.com>
2019-08-19 08:22:30 -0600

    Add percent correct functionality and tests

--- lib/round.rb
+++ lib/round.rb
@@ -18,19 +18,19 @@ class Round
   end
 
   def number_correct
-    @turns.select! { |turn| turn.correct? }
-      @turns.count
+    @turns.count { |turn| turn.correct? == true}
   end
 
-
-  def number_correct_by_category
+  def number_correct_by_category(category)
+    @turns.find_all { |turn| turn.correct? && turn.card.category == category}.count
   end
 
   def percent_correct
-    
+    (number_correct.to_f / @turns.count.to_f) * 100
   end
 
-
-
-
+  def percent_correct_by_category(category)
+    correct = @turns.find_all { |turn| turn.correct? == true}
+     (correct.count.to_f / number_correct_by_category(category) * 100
+  end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -19,11 +19,48 @@ class RoundTest < Minitest::Test
     assert_instance_of Round, @round
   end
 
-  # def test_round_has_deck
-  #   assert_equal @deck, @round.deck
-  # end
+  def test_round_has_deck
+    assert_equal @deck, @round.deck
+  end
+
+  def test_current_card_on_top
+    assert_equal @card_1, @round.current_card
+  end
+
+  def test_takes_a_turn
+    @round.take_turn("Juneau")
+    assert_equal @card_2, @round.current_card
+  end
+
+  def test_returns_new_turn
+    assert_instance_of Turn, @round.take_turn("Juneau")
+  end
 
   def test_number_correct
-    assert_equal 1, @round.number_correct
+    @round.take_turn("Juneau")
+    @round.take_turn("Wrong")
+    @round.take_turn("North north west")
+    assert_equal 2, @round.number_correct
   end
+
+  def test_number_correct_by_category
+    @round.take_turn("Juneau")
+    @round.take_turn("Wrong")
+    @round.take_turn("North north west")
+    assert_equal 1, @round.number_correct_by_category(:STEM)
+  end
+
+  def test_percent_correct
+    @round.take_turn("Juneau")
+    @round.take_turn("Incorrect")
+    assert_equal 50.0, @round.percent_correct
+  end
+
+  def test_percent_correct_by_category
+    @round.take_turn("Juneau")
+    @round.take_turn("Wrong")
+    @round.take_turn("North north west")
+    assert_equal 50.0, @round.percent_correct_by_category(:STEM)
+  end
+
 end
