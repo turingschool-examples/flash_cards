67b455c7a5ba20fd8209a41c590a69f65e97389b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-09-30 17:38:43 -0600

    Add initial Turn class that passes first iteration interaction pattern

---
+++ lib/turn.rb
@@ -0,0 +1,26 @@
+class Turn
+  attr_reader :card, :guess
+
+  def initialize(guess, card)
+    @guess    = guess
+    @card     = card
+    @correct  = false
+  end
+
+  def correct?
+    if @guess == @card.answer
+      @correct = true
+    else
+      @correct = false
+    end
+  end
+
+  def feedback
+    if @correct == true
+      puts "Correct!"
+    else
+      puts "Incorrect!"
+    end
+  end
+
+end
