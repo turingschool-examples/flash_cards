99da23422f83da6dd9cce4ba74efd292bb4bfef0
Linda Le <yueling1222@gmail.com>
2019-09-30 21:51:13 -0600

    Added turn.rb and turn_test.rb

---
+++ lib/turn.rb
@@ -0,0 +1,12 @@
+class Turn
+  attr_reader :question, :answer, :category
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer   = answer
+    @category = category
+  end
+
+end
+
+card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
---
+++ test/turn_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require
+
+class TurnTest < Minitest::Test
