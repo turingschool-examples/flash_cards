36eef164be34e48b671e8859a5378716ee719d94
Kevin Krato <kratok08@gmail.com>
2019-10-01 17:34:55 -0600

    Write tests for Turn class

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -14,5 +14,27 @@ class TurnTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Turn, @mock_turn
+  end 
+
+  def test_that_it_returns_the_card
+    assert_equal @mock_card, @mock_turn.card
   end
+
+  def test_it_has_a_correct_guess
+    assert_equal "Rome", @mock_turn.guess
+  end
+
+  def test_it_handles_guess_casing
+    turn = Turn.new("RoMe", @mock_card)
+    assert turn.correct?
+  end
+
+  def test_it_gives_correct_feedback
+    # test for correct guess
+    assert @mock_turn.correct?
+    # test for incorrect guess
+    turn_incorrect = Turn.new("Berlin", @mock_card)
+    assert_equal false, turn_incorrect.correct?
+  end
+
 end
