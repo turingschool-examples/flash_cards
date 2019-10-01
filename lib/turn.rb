87482ef577e3a6e6b33e4e4ba63f3413d9502eab
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-01 17:01:20 -0600

    Add attr_readers for turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,28 +1,22 @@
 require './card.rb'
 
 class Turn
+  attr_reader :card, :guess
+
   def initialize(guess, card)
     @guess = guess
     @card = card
   end
 
-  def guess
-    puts @guess
-  end
-
-  def card
-    puts @card
-  end
-
   def correct?
-    correct = @guess == @card.answer
+    @guess == @card.answer
   end
 
   def feedback
-     if correct
-       puts "Correct!"
+     if correct?
+       "Correct!"
      else
-       puts "Incorrect."
+       "Incorrect."
      end
   end
 end
