46c0449e4d54bedcc84de2c7e7a22b6d1fb0045e
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-01 17:08:09 -0600

    Add cards and count method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,11 @@
 require './card.rb'
 require './turn.rb'
 
-cards = [card1, card2]
+card1 = Card.new("What is my name?", "Dylan", :Names)
+card2 = Card.new("What is the capital of Colorado", "Denver", :Geography)
+card3 = Card.new("What is 2 + 2", "4", :Math)
+
+cards = [card1, card2, card2]
 
 class Deck
   attr_reader :cards
@@ -9,4 +13,11 @@ class Deck
     @cards = cards
   end
 
+  def count
+    cards.count
+  end
+
 end
+
+deck = Deck.new(cards)
+p deck.count
