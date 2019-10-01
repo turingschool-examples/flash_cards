ecb9a976f8b4329c3dbca44dda834d5f516912c6
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-01 17:00:27 -0600

    Change attributes to attr_reader

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,5 @@
 class Card
-  attr_accessor :question, :answer, :category
+  attr_reader :question, :answer, :category
   def initialize(question, answer, category)
     @question = question
     @answer = answer
@@ -8,4 +8,3 @@ class Card
 end
 
 card1 = Card.new("What is the capital of California?", "Sacramento", :Geography)
-card2 = Card.new("What is the capital of Colorado", "Denver", :Geography)
