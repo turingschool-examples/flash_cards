c803bb94b418ac2956c6b9d83604c3a3e03795a1
Phil DeLong <philjdelong@gmail.com>
2019-10-01 10:20:19 -0600

    Identify class attributes

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,21 @@
 #Class: Card
 #Object: FlashCard
 #Attributes: Question (string), Answer (string), Catagory (array)
+
+class Card
+  attr_reader :question, :answer, :catagory
+
+  def
+
+
+
+
+
+
+
+
+
+
+#work on this later..
+  def Card.new("string", "string", array)
+    @
