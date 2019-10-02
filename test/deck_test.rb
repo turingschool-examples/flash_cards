a3df98ec019541e70d3f3cbd64353f330e2c562a
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-02 16:31:57 -0600

    Add more deck test edits

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,18 +7,13 @@ require './lib/turn'
 class DeckTest < MiniTest::Test
 
   def setup
+    card = [@question, @answer, @category]
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(@cards)
-    stem = [card_2, card_3]
-    geography = [card_1]
-    @stem_category = stem
-    @geography_category = geography
-    @category = [@stem_category, @geography_category]
     @cards = cards
-    @deck = deck
   end
 
   def test_it_exists
@@ -32,12 +27,14 @@ class DeckTest < MiniTest::Test
   end
 
   def test_the_stem_category_has_correct_number_of_cards
+skip
     deck = Deck.new(@cards)
-    assert_equal 2, @stem_category.count
+    assert_equal 2, deck.cards_in_category(:STEM).count
   end
 
   def test_the_geography_category_has_correct_number_of_cards
     deck = Deck.new(@cards)
-    assert_equal 1, @geography_category.count
+    assert_equal 1, deck.cards_in_category(:Geography).count
   end
+
 end
