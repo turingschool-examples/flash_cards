29af507989f1e7cb81f76279a8c28104455a585c
Joshua O'Bannon <josho@Joshuas-MacBook-Pro.local>
2019-10-01 18:04:11 -0600

    Add method skeletons to turn.rb.  Start guess method

---
+++ lib/turn.rb
@@ -0,0 +1,21 @@
+require './lib/card'
+
+
+def initialize(string, card)
+  @string = string
+  @card = card
+end
+
+def guess
+  #this method returns the guess
+  @card.answer
+end
+
+def card
+end
+
+def correct?
+end
+
+def feedback
+end
