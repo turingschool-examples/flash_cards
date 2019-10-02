6399dbca8d9635f2794627a5534ce403e4602fb1
Alison Vermeil <alison.minton@gmail.com>
2019-10-02 12:45:17 -0600

    Add methods to round.rb and tests to testing file

--- lib/round.rb
+++ lib/round.rb
@@ -1,15 +1,16 @@
 class Round
-  attr_accessor :deck, :turns
+  attr_accessor :deck, :turns, :player_guess
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @player_guess = player_guess
   end
-
+# I don't need this w/ my accessor
   def deck
     @deck
   end
-
+# I don't need this w/ my accessory
   def turns
     @turns
   end
@@ -18,11 +19,24 @@ class Round
     @deck.cards.first
   end
 
-  def take_turn(guess)
+  def take_turn(player_guess)
     # Takes a string that represents the guess
+    player_guess = @player_guess
+
     # Creates a new Turn object with the appropriate guess and Card
+    card = round.current_card
+    guess = @player_guess
+    new_turn = Turn.new(guess, card)
+
     # Store the new Turn, adn return it from the take_turn method
+    @turns << new_turn
+    new_turn
     # the Round should move on to the next card in the deck.
+    played_cards = []
+    played_cards << round.deck.cards.shift
+
+    round.current_card
+
   end
 
   def number_correct
@@ -36,6 +50,6 @@ class Round
 
   def percent_correct_by_category(category)
   end
-  
+
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -7,13 +7,48 @@ require './lib/round'
 
 
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-deck = Deck.new([card_1, card_2, card_3])
-
-round = Round.new(deck)
+# card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+# card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+#
+# deck = Deck.new([card_1, card_2, card_3])
+#
+# round = Round.new(deck)
 
 class RoundTest < Minitest::Test
+  def test_it_exists
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    assert_instance_of Round, round
+  end
+
+  def test_current_card_is_first_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+
+    # how to refute this?
+  end
+
+  def test_take_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+  end
 end
