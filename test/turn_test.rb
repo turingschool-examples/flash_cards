74e46012a1d63fa45300a21cdcf3c05bb0e93717
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-10-02 14:54:38 -0600

    Add 3 tests

---
+++ test/turn_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require 'pry'
+
+class TurnTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
+  end
+
+  def test_incorrect
+    card = Card.new("What is the capital of Iowa?", "Des Moines", :Geography)
+    turn = Turn.new("Davenport", card)
+
+    assert_equal false, turn.correct?
+  end
+
+  def test_feedback
+    card = Card.new("What is the largest desert on Earth", "Antarctica", :Dumb_trivia)
+    turn = Turn.new("Sahara", card)
+
+    assert_equal "Incorrect.", turn.feedback
+  end
+
+end
