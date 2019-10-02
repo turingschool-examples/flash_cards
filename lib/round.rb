c294b073ec3dc089f4d965c2e3b2f43dcec3a509
Ryan Allen <rallenwasd@gmail.com>
2019-10-02 15:58:51 -0600

    Cleaned code in round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,62 @@
 #round.rb
 
 class Round
+  attr_reader :deck
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+    @x = 0 #place holder
+  end
 
-end 
+  def turns
+    @turns
+  end
+
+  def current_card
+    @deck.cards[@x]
+  end
+
+  def take_turn(guess)
+    @x += 1
+    @turns << Turn.new(guess,@deck.cards[@x-1])
+    Turn.new(guess,@deck.cards[@x-1])
+  end
+
+  def number_correct
+    count = 0
+    turns.each_index do |i|
+      if turns[i].correct?
+        count += 1
+      end
+    end
+    count
+  end
+
+  def number_correct_by_category(subject)
+    count = 0
+    turns.each_index do |i|
+      if turns[i].card.category == subject #check subject
+        if turns[i].correct?               #check correct w/in subject
+          count += 1
+        end
+      end
+    end
+    count
+  end
+
+  def percent_correct
+    number_correct.to_f / @turns.size * 100
+  end
+
+  def percent_correct_by_category(subject)
+    count = 0
+    turns.each_index do |i|
+      if turns[i].card.category == subject
+        count += 1
+      end
+    end
+    number_correct_by_category(subject) / count.to_f * 100
+  end
+end
+
+#require 'pry'; binding.pry
