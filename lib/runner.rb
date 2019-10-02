49c3398202dec74431f2ec92586a54906924a080
Joshua O'Bannon <josho@Joshuas-MacBook-Pro.local>
2019-10-02 11:40:56 -0600

    Minor change to runner file for turn and turn_test

--- lib/runner.rb
+++ lib/runner.rb
@@ -14,28 +14,12 @@ puts card.category
 #runner for the next Turn Class iteration pattern
 turn = Turn.new("Juneau", card)
 
-pry(main)> require './lib/turn'
-#=> true
-
-pry(main)> require './lib/card'
-#=> true
-
-pry(main)> card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-
-pry(main)> turn = Turn.new("Juneau", card)
-#=> #<Turn:0x00007f99842f0998 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @guess="Juneau">
-
-pry(main)> turn.card
-#=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-
-pry(main)> turn.guess
-#=> "Juneau"
-
-pry(main)> turn.correct?
-#=> true
-
-pry(main)> turn.feedback
-#=> "Correct!"
-
+require './lib/turn'
+require './lib/card'
+card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+turn = Turn.new("Juneau", card)
+#turn.card
+#turn.guess
+#turn.correc#t?
+#turn.feedback
 binding.pry
