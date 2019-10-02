8e4b9a5c11226fb1b4a28736ef91bd2ee553dceb
Alison Vermeil <alison.minton@gmail.com>
2019-10-01 20:15:15 -0600

    Add deck class and test file

---
+++ lib/deck.rb
@@ -0,0 +1,39 @@
+class Deck
+  attr_accessor :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def cards
+    @cards
+  end
+
+  def count
+    @cards.count
+  end
+
+
+  #   # Go into the array and pull the category from each entry.
+  #   # We want this to return an array with the cards that are of the same category.
+
+  def cards_in_category(category)
+    category_ary = []
+    cards.each do |cards|
+      if cards.category == category
+        category_ary << cards
+      end
+    end
+    category_ary
+  end
+
+# the next block of code was practicing a simpler example of using each to check the category.
+  def category_of_each_card
+    card_category_ary = []
+    cards.each do |cards|
+     card_category_ary << cards.category
+    end
+    card_category_ary # Having this line was the only way I could get it to return just the categories.
+  end
+
+end
