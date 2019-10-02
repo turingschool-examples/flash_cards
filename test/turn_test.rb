0db12bd4e753b9c4fcd657cd373f0bbf0316aedf
Joshua O'Bannon <josho@Joshuas-MacBook-Pro.local>
2019-10-02 11:38:18 -0600

    add turn_test file. Inital commit

---
+++ test/turn_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn'
+require './lib/card'
+
+class TurnTest < Minitest::Test
+  def setup
+    @turn = Turn.new("Juneau", card)
+  end
+end
