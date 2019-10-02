30ba33a0f78464159f2a82b797fe4484f4f99ac3
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-02 15:52:28 -0600

    Add number and percent correct methods to round class

--- lib/round.rb
+++ lib/round.rb
@@ -22,9 +22,39 @@ class Round
   end
 
   def number_correct
+    correct = 0.0
     @turns.each do |turn|
-      
+      if turn.correct?
+        correct += 1.0
+      end
     end
+    correct
   end
 
+  def number_correct_by_category(cat)
+    correct = 0.0
+    @turns.each do |turn|
+      if turn.card.category == cat
+        if turn.correct?
+          correct += 1.0
+        end
+      end
+    end
+    correct
+  end
+
+  def percent_correct
+    number_correct / @turns.count * 100
+  end
+
+  def percent_correct_by_category(cat)
+    total = 0.0
+    @turns.each do |turn|
+      if turn.card.category == cat
+        total += 1.0
+      end
+    end
+    total
+    number_correct_by_category(cat) / total * 100
+  end
 end
