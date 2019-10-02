6b69b94dce03794faa858e090a7b8d5a30dab8b0
Joshua O'Bannon <josho@Joshuas-MacBook-Pro.local>
2019-10-02 11:42:05 -0600

    Minor changes to turn file.  make class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,22 +1,25 @@
 require './lib/card'
 
+class Turn
 
-def initialize(string, card)
-  @string = string
-  @card = card
-end
+  def initialize(string, card)
+    @string = string
+    @card = card
+  end
 
-def guess
-  #this method returns the guess
-  @card.answer
-end
+  def guess
+    #this method returns the guess
+    @card.answer
+  end
 
-def card
-  @card
-end
+  def card
+    @card
+  end
 
-def correct?
-end
+  def correct?
+  end
 
-def feedback
+  def feedback
+  end
+
 end
