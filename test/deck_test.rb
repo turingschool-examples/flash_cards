14ce0d2cc20f8ca1678b909eb5c6096aedadd128
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-02 14:08:08 -0600

    Add category_test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -11,6 +11,8 @@ class DeckTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     @cards = [card_1, card_2, card_3]
+    @stem_deck = [card_2, card_3]
+    @geo_deck = [card_1]
   end
 
   def test_it_exists
@@ -21,5 +23,12 @@ class DeckTest < Minitest::Test
   def test_count_cards
     deck = Deck.new(@cards)
     assert_equal 3, deck.count
+  end
+
+  def test_categories
+    deck = Deck.new(@cards)
+    assert_equal deck.cards_in_category(:STEM), @stem_deck
+    assert_equal deck.cards_in_category(:Geography), @geo_deck
+    assert_equal deck.cards_in_category(:Pop_culture), []
   end
 end
