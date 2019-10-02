2e6f3b5193939a4e58a0437319d0b991e0bcad8c
Linda Le <yueling1222@gmail.com>
2019-10-02 11:15:55 -0600

    Added outline of methods to run test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,3 +4,28 @@ require './lib/card'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
+
+  # checks to see if the card exists
+  def card_exists
+  end
+
+  #checks to see if the turn exists
+  def turn_exists
+  end
+
+  #checks that a card is displayed during turn
+  def card_displayed
+  end
+
+  #checks that answer is submitted during turn
+  def guess_displayed
+  end
+
+  #checks to see if guess is correct
+  def check_guess
+  end
+
+  #checks to ensure feedback is displayed depending on if guess is correct or not
+  def check_feedback
+  end
+end
