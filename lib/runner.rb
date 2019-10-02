03458ff3a624c6ea9a87a2841c5801f922bdfc60
Joshua O'Bannon <josho@Joshuas-MacBook-Pro.local>
2019-10-01 18:19:11 -0600

    add interaction pattern for runner.rb to Turn card

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,9 +1,41 @@
 require './lib/card'
+require './lib/turn'
 require 'pry'
 
+#runner for the first interation pattern
 card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 puts card.question
 puts card.answer
 puts card.category
+
+# binding.pry commenting out
+
+
+#runner for the next Turn Class iteration pattern
+turn = Turn.new("Juneau", card)
+
+pry(main)> require './lib/turn'
+#=> true
+
+pry(main)> require './lib/card'
+#=> true
+
+pry(main)> card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+#=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
+
+pry(main)> turn = Turn.new("Juneau", card)
+#=> #<Turn:0x00007f99842f0998 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @guess="Juneau">
+
+pry(main)> turn.card
+#=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
+
+pry(main)> turn.guess
+#=> "Juneau"
+
+pry(main)> turn.correct?
+#=> true
+
+pry(main)> turn.feedback
+#=> "Correct!"
 
 binding.pry
