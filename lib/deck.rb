50151a8976f83f85bb68f785e7b827518231a1f7
Ryan Allen <rallenwasd@gmail.com>
2019-10-02 12:08:29 -0600

    Added additional methods to deck.rb' '

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,17 @@
 # deck.rb
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :count
 
   def initialize(cards)
     @cards = cards
+    @count = cards.size
+  end
+
+  def cards_in_category(category)
+    @cards.select {|card| category == card.category}
   end
 
 end
 
-require 'pry'; binding.pry
+#require 'pry'; binding.pry
