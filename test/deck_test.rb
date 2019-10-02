131917f3dc9e4b528337e0ce4f6cf1ae8d550c0a
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-02 13:18:12 -0600

    Add test_count_cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/turn'
+require './lib/deck'
 
 class DeckTest < Minitest::Test
   def setup
@@ -9,12 +10,12 @@ class DeckTest < Minitest::Test
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
-
-    deck = Deck.new(cards)
+    @cards = [card_1, card_2, card_3]
   end
 
   def test_count_cards
-    
+    deck = Deck.new(@cards)
+    deck.count
+    assert_equal 3, deck.count
   end
 end
