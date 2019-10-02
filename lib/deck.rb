9316f0ca96f172b39a4695242a112a1c84770433
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-02 14:09:05 -0600

    Add cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,16 +1,21 @@
 class Deck
-  attr_reader :cards, :count
+  attr_reader :cards
 
   def initialize(cards)
     @cards    = cards
   end
 
   def count
-    @count = @cards.count
+    @cards.count
   end
 
   def cards_in_category(category)
-    @self.group_by { |category| @category == category }
+    cat_sort = []
+    @cards.each do |card|
+      if category == card.category
+        cat_sort << card
+      end
+    end
+    cat_sort
   end
-
 end
