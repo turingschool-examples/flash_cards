b770f4d2720a44ae52ec43c9ff1f3aded4f87d8c
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-02 16:07:03 -0600

    Add turn test corrections

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -2,21 +2,43 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/Turn'
+require './lib/deck'
 
 class TurnTest < Minitest::Test
 
   def test_turn_exists
-    turn = Turn.new("Juneau", "What is the capital of Alaska?")
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
     assert_instance_of Turn, turn
   end
 
   def test_it_has_a_user_guess
-    turn = Turn.new("Juneau", "What is the capital of Alaska?")
-    assert turn.guess
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+    assert_equal "Juneau", turn.guess
   end
 
   def test_the_guess_is_correct
-    turn = Turn.new("Juneau", "What is the capital of Alaska?")
-    assert turn.correct?
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+    assert_equal false, turn.correct?
+  end
+
+  def test_the_guess_is_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Anchorage", card_1)
+    assert_equal false, turn.correct?
+  end
+
+  def test_correct_feedback
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+    assert_equal "Correct!", turn.feedback
+  end
+
+  def test_correct_feedback
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Anchorage", card_1)
+    assert_equal "Incorrect", turn.feedback
   end
 end
