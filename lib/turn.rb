ff3cfce8dd68969905fa641a89ca7480f4364e1e
Linda Le <yueling1222@gmail.com>
2019-10-02 12:35:17 -0600

    Update turn.rb file after project check-in

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,42 +3,28 @@ require_relative 'card'
 
 class Turn
   # takes care of both attr_reader and attr_writer
-  attr_accessor :guess, :card, :correct, :feedback
+  attr_accessor :guess, :card
 
   def initialize(guess, card)
     @guess = guess
     @card = card
   end
 
-  # getter to return value of instance attribute
-  def guess
-    @guess
-  end
-
-  # also a getter
-  def card
-    @card
-  end
-
-  def guess_answer
-    puts @card.question
-    puts "Enter your answer here:"
-    @guess = gets.chomp
-  end
+  # def guess_answer
+  #   puts @card.question
+  #   puts "Enter your answer here:"
+  #   @guess = gets.chomp
+  # end
 
   def correct?
-    if @guess == @card.answer
-      true
-    else
-      false
-    end
+    @guess == @card.answer
   end
 
   def feedback
     if correct?
-      p "Correct!"
+       "Correct!"
     else
-      p "Incorrect, try again!"
+       "Incorrect, try again!"
     end
   end
 end
@@ -47,20 +33,12 @@ card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card2 = Card.new("Who was the first woman to win a Noble Prize?", "Marie Curie", :Science)
 card3 = Card.new("What style of art is characterized by its distinct use of dots to form images?", "Pointillism", :Art)
 
-turn = Turn.new("", card1)
-turn.guess_answer
-turn.feedback
-turn2 = Turn.new("", card2)
-turn2.guess_answer
-turn2.feedback
-turn3 = Turn.new("", card3)
-turn3.guess_answer
-turn3.feedback
+turn = Turn.new("Juneau", card1)
 
-# initialize(string, card) - A turn is initialized with two arguments.
-# The first is a string representing a guess to a card’s question.
-# The second argument is a Card object representing the current flashcard being shown.
-# guess - This method returns the guess
-# card - This method returns the Card
-# correct? - This method returns a boolean indicating if the guess matched the answer on the Card.
-# feedback - This method either returns "Correct!" or "Incorrect."
+turn.feedback
+turn2 = Turn.new("Guess2", card2)
+
+turn2.feedback
+turn3 = Turn.new("Guess3", card3)
+
+turn3.feedback
