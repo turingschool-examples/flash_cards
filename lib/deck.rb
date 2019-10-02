69b4bbe749cbadc36f0e6ad1a8db00ddfd5c50e4
Wren Steitle <lauren.steitle@gmail.com>
2019-10-02 16:42:33 -0600

    Create category tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,6 +2,22 @@ require './lib/card'
 require './lib/turn'
 
 class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
 
+
+  end
+
+  def card_category(category)
+    cat_array = []
+    @cards.each do |card|
+      if card.category == category
+        cat_array << card
+      end
+    end
+      cat_array
+
+  end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,6 +1,5 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/turn'
 require './lib/card'
 require './lib/deck'
 require 'pry'
@@ -13,14 +12,40 @@ class TurnTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     @cards = cards
-    @deck = Deck.new
+    deck = Deck.new(@cards)
+    @deck = deck
+
   end
 
   def test_it_has_cards
     assert_equal 3, @cards.count
   end
 
+  def test_it_has_a_card_array
+    assert_equal @cards, @deck.cards
+  end
+
   def test_it_has_a_deck
     assert_instance_of Deck, @deck
+  end
+
+  def test_deck_has_cards
+    assert_equal @cards, @deck.cards
+  end
+
+
+  def test_cards_in_stem_category
+    x = @deck.card_category(:STEM)
+    assert_equal 2, x.count
+  end
+
+  def test_cards_in_geography_category
+    x = @deck.card_category(:Geography)
+    assert_equal 1, x.count
+  end
+
+  def test_cards_in_non_category
+    x = @deck.card_category(:Popculture)
+    assert_equal 0, x.count
   end
 end
