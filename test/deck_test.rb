6c7017667e2a377bb7d898a8da8d5b1b26a8cb13
Wren Steitle <lauren.steitle@gmail.com>
2019-10-02 13:23:02 -0600

    Create method test_it_has_a_deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -11,12 +11,16 @@ class TurnTest < Minitest::Test
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @cards = [card_1, card_2, card_3]
+    cards = [card_1, card_2, card_3]
+    @cards = cards
+    @deck = Deck.new
   end
 
   def test_it_has_cards
     assert_equal 3, @cards.count
   end
 
-
+  def test_it_has_a_deck
+    assert_instance_of Deck, @deck
+  end
 end
