87b9a4b13ed64f5916356f7da43f79e2e50fbd5f
Phil DeLong <philjdelong@gmail.com>
2019-10-02 14:28:39 -0600

    Test with accessor to make attribute changes

--- lib/runner.rb
+++ lib/runner.rb
@@ -3,12 +3,8 @@ require './lib/turn'
 require 'pry'
 
 #card = Card.new("What is the capital of Alaska?", "Juno", :Geography)
-card_1 = Card.new("What is the capital of Alaska?", "Juno", :Geography)
-card_2 = Card.new([],[],[])
-
-card_2.question = "What is the capital of Ohio?"
-card_2.answer = "Columbus"
-card_2.category = :Geography
+card = Card.new("What is the capital of Alaska?", "Juno", :Geography)
+turn = Turn.new("Columbus", card)
 
 
 binding.pry
