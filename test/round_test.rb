2b5fb08f667d746050c5b6f7777947f41784a4d5
Alison Vermeil <alison.minton@gmail.com>
2019-10-03 17:57:47 -0600

    Add test for percent correct and precent correct by category methods

--- test/round_test.rb
+++ test/round_test.rb
@@ -16,7 +16,7 @@ class RoundTest < Minitest::Test
 
     @round = Round.new(@deck)
   end
-  
+
   def test_it_exists
     assert_instance_of Round, @round
   end
@@ -41,36 +41,32 @@ class RoundTest < Minitest::Test
   end
 
   def test_number_correct
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @round.take_turn("Juneau")
+    assert_equal 1, @round.number_correct
 
-    deck = Deck.new([card_1, card_2, card_3])
+    @round.take_turn("Venus")
+    assert_equal 1, @round.number_correct
 
-    round = Round.new(deck)
-
-    round.take_turn("Juneau")
-    assert_equal 1, round.number_correct
+    @round.take_turn("North north west")
+    assert_equal 2, @round.number_correct
   end
 
   def test_number_correct_by_category
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    #how do you space something like this?
+    @round.take_turn("Juneau")
+    assert_equal 1, @round.number_correct_by_category(:Geography)
 
-    deck = Deck.new([card_1, card_2, card_3])
+    @round.take_turn("Mars")
+    @round.take_turn("North north west")
+    assert_equal 2, @round.number_correct_by_category(:STEM)
 
-    round = Round.new(deck)
-
-    round.take_turn("Juneau")
-
-    assert_equal 1, round.number_correct_by_category(:Geography)
-
-    round.take_turn("Mars")
-
-    round.take_turn("North north west")
-    assert_equal 2, round.number_correct_by_category(:STEM)
-
+    assert_equal 0, @round.number_correct_by_category("pop culture")
   end
+
+  def test_percent_correct
+  end
+
+  def test_percet_correct_by_category
+  end 
 
 end
