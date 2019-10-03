32baa307a636fc7b52de8e1df0b116295e9a4e7c
Alison Vermeil <alison.minton@gmail.com>
2019-10-03 14:15:30 -0600

    Work on start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -5,30 +5,46 @@ require './lib/round'
 require 'pry'
 
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+deck = []
+deck << card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+deck << card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+deck << card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+deck << card_4 = Card.new("What TV show is Jennifer Aniston most famous for?", "Friends", "pop culture")
 
-deck = Deck.new([card_1, card_2, card_3])
 
+deck = Deck.new(deck)
+
+p deck.count
+
+
+
+def start
 round = Round.new(deck)
 
 # Er, what am I doing here?
-def start
-  puts
-end
+# Try turning some of the round stuff into hashes?
 
-puts "Welcome! You're playing with 4 cards."
+
+puts "Welcome! You're playing with #{deck.count} cards."
 puts "-" * 50
+  card_number = 0
+until deck.count = 0
 
-puts "This is card number 1 out of 4."
+
+puts "This is card number #{card_number =+ 1} out of #{deck.count}."
+
 puts "Question: #{round.current_card.question}"
 print ">"
 player_guess = gets.chomp
 puts round.take_turn(player_guess).feedback
 # There should be some sort of loop... keep going until no cards left .. number of turns = number of cards in Deck
-puts "This is card number 2 out of 4."
-puts "Question: #{round.current_card.question}"
-print ">"
-player_guess = gets.chomp
-puts round.take_turn(player_guess).feedback
+end
+#
+# puts "This is card number 2 out of 4."
+# puts "Question: #{round.current_card.question}"
+# print ">"
+# player_guess = gets.chomp
+# puts round.take_turn(player_guess).feedback
+
+end
+start
