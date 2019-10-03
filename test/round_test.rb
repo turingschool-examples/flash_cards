5072581d86dbdacaeef99edb04d01280f635ba5b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-03 13:42:02 -0600

    Add test for turn tracking

--- test/round_test.rb
+++ test/round_test.rb
@@ -10,8 +10,9 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
     @cards = [@card_1, @card_2, @card_3]
+
+    @turn_1 = Turn.new("Juneau", @card_1)
     @deck = Deck.new(@cards)
     @round = Round.new(@deck)
   end
@@ -24,13 +25,36 @@ class RoundTest < Minitest::Test
     assert_instance_of Deck, @deck
   end
 
-  def test_current_card
-    assert_equal @round.current_card, @card_1
+  def test_card_exists
+    assert_instance_of Card, @card_1
   end
 
-  def test_take_turn
-    new_turn = @round.take_turn("Juneau")
-    assert_equal new_turn.class, Turn
-    assert_equal new_turn.correct?, true
+  def test_current_card
+    assert_equal @card_1, @round.current_card
+  end
+
+  def test_take_turn_class
+    assert_equal Turn, @turn_1.class
+  end
+
+  def test_new_turn_correct
+    skip
+    assert_equal true, @turn_1.correct?
+  end
+
+  def test_turns_exists
+    skip
+    assert_instance_of Array, @round.turns
+  end
+
+  def test_turns_gets_card_each_turn
+    @round.take_turn("")
+    assert_equal [@card_1], @round.turns
+
+    @round.take_turn("")
+    assert_equal [@card_1, @card_2], @round.turns
+
+    @round.take_turn("")
+    assert_equal [@card_1, @card_2, @card_3], @round.turns
   end
 end
