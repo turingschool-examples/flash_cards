8c681120082e5d4cc5b93c6fa90877bdf8cc707c
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-03 16:35:40 -0600

    Add 8 tests to  round test

--- test/round_test.rb
+++ test/round_test.rb
@@ -18,4 +18,41 @@ class RoundTest < Minitest::Test
   def test_it_exists
     assert_instance_of Round, @round
   end
+
+  def test_the_deck_exists
+    assert_equal @deck, @round.deck
+  end
+
+  def test_there_are_no_turns_initially
+    assert_equal [], @round.turns
+  end
+
+  def test_current_card_is_first_card
+    assert_equal @card_1, @round.current_card
+  end
+
+  def test_take_turn_class
+    new_turn = @round.take_turn("Juneau")
+    assert_equal Turn, new_turn.class
+  end
+
+  def test_if_guess_is_correct
+    new_turn = @round.take_turn("Juneau")
+    assert_equal true, new_turn.correct?
+  end
+
+  def test_if_each_turn_is_stored_in_array
+    new_turn = @round.take_turn("Juneau")
+    assert_equal [new_turn], @round.turns
+  end
+
+  def test_number_correct
+    new_turn = @round.take_turn("Juneau")
+    assert_equal 1, @round.number_correct
+  end
+
+  def test_current_card_goes_to_second_card_in_deck_after_turn_is_taken
+    new_turn = @round.take_turn("Juneau")
+    assert_equal @card_2, @round.current_card
+  end
 end
