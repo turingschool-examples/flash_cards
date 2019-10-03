67072cc573c90975ce5ec8018d29547ce3013e42
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-02 18:10:36 -0600

    Add current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,15 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
+    @turns = []
+  end
+
+  def take_turn(guess)
+
+  end
+
+  def current_card
+    @deck.cards.shift
   end
 end
