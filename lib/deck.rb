97d25ae8e230fe88b1df98562827a96f8f89175b
Joshua O'Bannon <josho@Joshuas-MacBook-Pro.local>
2019-10-02 21:55:47 -0600

    missed in the last commit

---
+++ lib/deck.rb
@@ -0,0 +1,24 @@
+require './lib/card'
+require './lib/deck'
+require 'pry'
+
+class Deck
+  attr_accessor :cards
+  def initialize(cards)
+    @cards = cards
+
+  end
+
+  def count
+    @cards.length
+  end
+
+  def cards_in_category(sorted_category)
+    category_cards = []
+    @cards.each do |card|
+      if card.category == sorted_category
+        category_cards << card
+      end
+    end
+  end
+end
