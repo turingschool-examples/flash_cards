4d108885c4ec14bacd9c9d9a15b0c2106be52bf4
Wren Steitle <lauren.steitle@gmail.com>
2019-10-02 20:07:15 -0600

    Create round class and test

---
+++ lib/round.rb
@@ -0,0 +1,11 @@
+require './lib/card'
+require './lib/turn'
+require './lib/round'
+
+class Round
+  def initialize(deck)
+    @deck = deck  
+
+  end
+
+end
---
+++ test/round_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+require 'pry'
+
+class RoundTest < Minitest::Test
+  def setup
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [card_1, card_2, card_3]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+  end
+
+  def test_it_has_a_deck
+    assert_instance_of Deck, @deck
+  end
+
+  def test_it_has_cards
+    assert_equal 3, @cards.count
+  end
+
+  def test_it_has_a_round
+    skip
+    assert_instance_of Round, @round
+  end
+end
