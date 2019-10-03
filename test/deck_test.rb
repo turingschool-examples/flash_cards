a0565e5ba154c9e39fb3528d0c5fffc31b800611
Alison Vermeil <alison.minton@gmail.com>
2019-10-03 16:33:11 -0600

    Add setup method to deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,74 +5,46 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
 
-# card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-# card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#
-# cards = [card_1, card_2, card_3]
-# deck = Deck.new(cards)
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
+    @cards = [@card_1, @card_2, @card_3]
+
+    @deck = Deck.new(@cards)
+  end
 
 
   def test_it_exists
 
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-    cards = [card_1, card_2, card_3]
-
-    deck = Deck.new(cards)
-
-    assert_instance_of Deck, deck
+    assert_instance_of Deck, @deck
   end
 
-  def test_card_display
+  def test_the_deck_has_cards
 
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-    cards = [card_1, card_2, card_3]
-
-    deck = Deck.new(cards)
-
-    assert_equal cards, deck.cards
+    assert_equal [@card_1, @card_2, @card_3], @deck.cards
   end
 
   def test_count_the_cards
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
-
-    deck = Deck.new(cards)
-
-    assert_equal 3, deck.count
+    assert_equal 3, @deck.count
   end
 
   def test_cards_in_category
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
+    assert_equal [@card_1], @deck.cards_in_category(:Geography)
 
-    deck = Deck.new(cards)
+    assert_equal true, @deck.cards_in_category(:Geography).include?(@card_1)
+    refute_equal true, @deck.cards_in_category(:Geography).include?(@card_2)
+    refute_equal true, @deck.cards_in_category(:Geography).include?(@card_3)
 
-    assert_equal deck.cards_in_category(:Geography), [card_1]
+    refute_equal true, @deck.cards_in_category(:STEM).include?(@card_1)
+    assert_equal true, @deck.cards_in_category(:STEM).include?(@card_2)
+    assert_equal true, @deck.cards_in_category(:STEM).include?(@card_3)
 
-    assert deck.cards_in_category(:Geography).include?(card_1)
-    refute deck.cards_in_category(:Geography).include?(card_2)
-    refute deck.cards_in_category(:Geography).include?(card_3)
-
-    refute deck.cards_in_category(:STEM).include?(card_1)
-    assert deck.cards_in_category(:STEM).include?(card_2)
-    assert deck.cards_in_category(:STEM).include?(card_3)
-
-    refute deck.cards_in_category("Pop Culture").include?(card_1)
-    refute deck.cards_in_category("Pop Culture").include?(card_2)
-    refute deck.cards_in_category("Pop Culture").include?(card_3)
+    refute_equal true, @deck.cards_in_category("Pop Culture").include?(@card_1)
+    refute_equal true, @deck.cards_in_category("Pop Culture").include?(@card_2)
+    refute_equal true, @deck.cards_in_category("Pop Culture").include?(@card_3)
   end
 end
