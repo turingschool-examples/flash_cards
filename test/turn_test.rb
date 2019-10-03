b87eec6421268a8a6d95e2e6af0d82f8e78139c5
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-03 12:45:20 -0600

    Create first turn test (does it exist)

---
+++ test/turn_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+
+
+
+class TurnTest < Minitest::Test
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", card)
+  end
+
+  def test_it_exists
+    assert_instance_of Turn, @turn
+  end
+
+  def test_it_has_a_card
+
+  end
+end
