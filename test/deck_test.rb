9d438d2362c106ea63de209dad0451bb9460d7da
Nathan Keller <keller.nathan@gmail.com>
2019-10-03 13:03:45 -0600

    updated deck and turn test docs

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,9 +5,27 @@ require './lib/card'
 
 class DeckTest < Minitest::Test
 
-# card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-# card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#
+  def test_card_1_exists
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    assert_instance_of Card, card_1
+  end
+
+  def test_card_2_exists
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    assert_instance_of Card, card_2
+  end
+
+  def test_card_3_exists
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    assert_instance_of Card, card_3
+  end
+
+  def test_if_deck_has_cards
+
+  end
+
 
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -25,9 +25,32 @@ end
     assert_equal true, @turn.correct?
   end
 
+  def test_it_will_provide_feedback
+
+    assert_equal "Correct!", @turn.feedback
+  end
+
+#Incorrect
+
+  # def test_it_returns_the_card
+  #   card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+  #   assert_instance_of Card, card
+  # end
+  #
+  # def  test_it_returns_a_guess
+  #   turn = Turn.new("Saturn", card)
+  #   assert_equal "Saturn", turn.guess
+  # end
+  #
+  # def test_if_the_guess_matches_the_card
+  #
+  #   assert_equal false, @turn.correct?
+  # end
+  #
   # def test_it_will_provide_feedback
   #
-  #   assert_instance_of @turn.correct?
+  #   assert_equal "Incorrect!", @turn.feedback
   # end
+
 
 end
