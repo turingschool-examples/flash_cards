eb1425de5f293816c576b758f03c484ea7bb18e5
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-02 21:22:16 -0600

    Refactor for setup

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,37 +4,26 @@ require './lib/card'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+    @turn2 = Turn.new("Fairbanks", @card)
+  end
 
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of Turn, turn
+    assert_instance_of Turn, @turn
   end
 
   def test_if_guess_is_correct
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    assert_equal true, turn.correct?
+    assert_equal true, @turn.correct?
   end
 
   def test_if_guess_is_incorrect
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Fairbanks", card)
-
-    assert_equal false, turn.correct?
+    assert_equal false, @turn2.correct?
   end
 
   def test_feedback
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    # require "pry"; binding.pry
-    assert_equal "Correct!", turn.feedback
-
-    turn2 = Turn.new("Fairbanks", card)
-    turn2.correct?
-    # require "pry"; binding.pry
-    assert_equal "Incorrect!", turn2.feedback
+    assert_equal "Correct!", @turn.feedback
+    assert_equal "Incorrect!", @turn2.feedback
   end
 end
