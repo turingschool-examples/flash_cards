aefc775ee74ae4739884fdab3f8689020520864e
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-02 18:11:39 -0600

    Add test for current_card

--- test/round_test.rb
+++ test/round_test.rb
@@ -7,21 +7,24 @@ require './lib/round'
 
 class RoundTest < Minitest::Test
   def setup
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
-    @deck = Deck.new(cards)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
   end
 
   def test_round_exists
-    round = Round.new(@deck)
-    assert_instance_of Round, round
+    assert_instance_of Round, @round
   end
 
   def test_deck_exists
-    round = Round.new(@deck)
     assert_instance_of Deck, @deck
+  end
+
+  def test_current_card
+    assert_equal @round.current_card, @card_1
   end
 end
