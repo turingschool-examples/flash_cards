2aeeed3f56ecefba4de69eb624a14481534556a9
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-03 20:02:33 -0600

    Add test for percent_correct_by_cat

--- test/round_test.rb
+++ test/round_test.rb
@@ -74,4 +74,11 @@ class RoundTest < Minitest::Test
     @round.take_turn("North north west")
     assert_equal 66, @round.percent_correct
   end
+
+  def test_percent_correct_by_category
+    @round.take_turn("Juneau")
+    @round.take_turn("Venus")
+    @round.take_turn("North north west")
+    assert_equal 50, @round.percent_correct_by_category(:STEM)
+  end
 end
