40ebcc5a119d91e0adefb2ca43442855a96b149f
Madelyn Romero <m.romero013@gmail.com>
2019-10-03 19:21:15 -0600

    Add Deck and Deck_Test, both working

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,43 +1,27 @@
 class Deck
 
-  attr_reader :cards_array
+  attr_reader :cards
 
-  def initialize(cards_array)
-    @cards_array = cards_array
+  def initialize(cards)
+    @cards = cards
   end
 
   def count
-    @cards_array.length
+    @cards.length
   end
 
-  def cards_in_category
-    
+  def cards_in_category(category)
+
+    temp_array = []
+
+    @cards.each do |card|
+      if category == card.category
+        temp_array << card
+      end
+    end
+
+    temp_array
+
   end
 
 end
-
-
-# class Deck
-#
-#   attr_reader :cards
-#
-#   def initialize(cards)
-#     @cards = cards
-#   end
-#
-#   def count
-#     @cards.length
-#   end
-#
-#   def cards_in_category(category)
-#       collected_cards = []
-#
-#       @cards.each do |card|
-#         if category == card.category
-#           collected_cards << card
-#         end
-#       end
-#       collected_cards
-#   end
-#
-# end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -19,7 +19,7 @@ class DeckTest < Minitest::Test
   #Good practice to put assert_instance_of in setup, or in a test_initialize method?
 
   def test_cards_in_deck
-    assert_equal [@card_1, @card_2, @card_3], @deck.cards_array
+    assert_equal [@card_1, @card_2, @card_3], @deck.cards
   end
 
   def test_number_of_cards_in_deck
@@ -38,53 +38,3 @@ class DeckTest < Minitest::Test
 
 
 end
-
-
-# require 'minitest/autorun'
-# require 'minitest/pride'
-# require './lib/card'
-# require './lib/deck'
-#
-# class DeckTest < Minitest::Test
-#
-#   def setup
-#     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#     card_3 = Card.new("Describe in words the exact direction that is 697.5 clockwise from due north?", "North north west", :STEM)
-#     @cards = [card_1, card_2, card_3]
-#     # cards = [card_1, card_2, card_3]
-#     # @cards = cards
-#     @deck = Deck.new(@cards)
-#
-#     @stem_array = [card_2, card_3]
-#     @geography_array = [card_1]
-#     @pop_culture_array = []
-#
-#   end
-#
-#   def test_it_exists
-#     assert_instance_of Deck, @deck
-#   end
-#
-#   def test_initialize
-#     assert_equal @cards, @deck.cards
-#   end
-#
-#   def test_it_is_an_array
-#   end
-
-#   def test_array_elements_are_objects
-#   end
-#
-#   def test_number_of_cards
-#     assert_equal 3, @cards.length
-#   end
-#
-#   def test_categories
-#
-#     assert_equal @deck.cards_in_category(:STEM), @stem_array
-#     assert_equal @deck.cards_in_category(:Geography), @geography_array
-#     assert_equal @deck.cards_in_category("Pop Culture"), @pop_culture_array
-#   end
-#
-# end
