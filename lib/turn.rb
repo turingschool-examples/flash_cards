ef8b7de38a97e478b46a5d8be71ee71492313042
Madelyn Romero <m.romero013@gmail.com>
2019-10-04 17:09:43 -0600

    Add RoundTest code

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,8 +8,7 @@ class Turn
   end
 
   def correct?
-    if @guess == @card.answer
-      #.upcase, to make sure if someone enters it it works
+    if @guess == @card.answer.upcase
       true
     else
       false
--- test/round_test.rb
+++ test/round_test.rb
@@ -12,6 +12,7 @@ class RoundTest < Minitest::Test
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     @cards = [@card_1, @card_2, @card_3]
+    # @turn = Turn.new(guess, @card_1)
     @deck = Deck.new(@cards)
     @round = Round.new(@deck)
   end
@@ -29,7 +30,9 @@ class RoundTest < Minitest::Test
   end
 
   def test_take_turn
-    assert_equal guess, gets.chomp
+    turn = Turn.new(@guess, @card)
+    # assert_equal "Juneau", @turn.guess
+    # assert_equal
 
     # takes a string representing guess
     # creates new Turn object with appropriate guess and Card
