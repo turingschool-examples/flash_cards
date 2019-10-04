c4586721c3079511d2e6602fb74f583ce33a1190
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-04 17:49:43 -0600

    Refactor to use enumerables and give x/y meaningful names

--- lib/round.rb
+++ lib/round.rb
@@ -17,11 +17,9 @@ class Round
   end
 
   def number_correct
-    sum = 0
-    @turns.each do |turn|
-      sum += 1 if turn.correct?
+    @turns.count do |turn|
+      turn.correct?
     end
-    sum
   end
 
   def percent_correct
@@ -38,35 +36,33 @@ class Round
     sum
   end
 
+  # def number_correct_by_category(category)
+  #   @turns.find_all do |turn|
+  #     turn.card.category == category && turn.correct?
+  #   end
+  # end
+
   def number_of_cards_by_category(category)
-    sum = 0
-    @turns.each do |turn|
-      if category == turn.card.category
-        sum += 1
-      end
+    @turns.count do |turn|
+      category == turn.card.category
     end
-    sum
   end
 
   def cards_in_discard
-    discard = []
-    @turns.each do |turn|
-      discard << turn.card
+    @turns.map do |turn|
+      turn.card
     end
-  discard
   end
 
   def discard_pile_by_category(category)
-    sum = 0
-    cards_in_discard.each do |card|
-      sum += 1 if card.category == category
+    cards_in_discard.count do |card|
+      card.category == category
     end
-    sum
   end
 
   def percent_correct_by_category(category)
-    x = number_correct_by_category(category).to_f
-    y = discard_pile_by_category(category).to_f
-    ((x / y) * 100).to_i
+    correct_cat = number_correct_by_category(category).to_f
+    discard_cat = discard_pile_by_category(category).to_f
+    ((correct_cat/ discard_cat) * 100).to_i
   end
 end
