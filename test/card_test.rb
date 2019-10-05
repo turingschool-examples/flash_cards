13d7c2b1e1b5bf34b9715f3b5f98a096e525da01
Raymond Nguyen <rayknguyen@gmail.com>
2019-10-04 22:52:55 -0600

    Card test cleaned up

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,30 +3,28 @@ require 'minitest/pride'
 require './lib/card'
 
 class CardTest < Minitest::Test
-def setup
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  end
-  def test_it_exists
-    # card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    assert_instance_of Card, @card
+  def setup
+      @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   end
 
-  def test_it_has_a_question #needs the test to requires "test" in the method
-    # card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    def test_it_exists
 
-    assert_equal "What is the capital of Alaska?", @card.question
-  end
+      assert_instance_of Card, @card
+    end
 
-  def test_it_has_an_answer
-    # card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    def test_it_has_a_question
 
-    assert_equal "Juneau", @card.answer
-  end
+      assert_equal "What is the capital of Alaska?", @card.question
+    end
 
-  def test_it_has_a_category
-    # card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    def test_it_has_an_answer
 
-    assert_equal :Geography, @card.category
-  end
+      assert_equal "Juneau", @card.answer
+    end
+
+    def test_it_has_a_category
+
+      assert_equal :Geography, @card.category
+    end
+    
 end
