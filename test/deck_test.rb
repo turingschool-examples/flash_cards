01433593dbabe3f295bf88c7f9532517e2f765a5
Phil DeLong <philjdelong@gmail.com>
2019-10-05 10:17:38 -0600

    Write tests, inlude skips and pry

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,24 +7,69 @@ require 'pry'
 class DeckTest < Minitest::Test
 
   def test_it_exists
-    skip
+    # skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-
-    assert_instance_of Deck, card
+binding.pry
+    assert_instance_of Deck, deck
   end
 
   def test_it_has_cards
-    skip
+    # skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
-    assert_equal cards, deck.cards
+    assert_equal Deck, deck.cards
   end
 
-  def test_it_has_card_1
+  def test_it_can_list_cards_in_geography_category
     skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
-    assert_instance_of card_1, deck.card_
+    assert_instance_of Deck, deck.count
   end
 
+  def test_it_can_list_cards_in_geography_category
+    skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, :Geography
+  end
+
+  def test_it_can_list_cards_in_STEM_category
+    skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, :STEM
+  end
+
+  def test_it_can_list_cards_in_STEM_category
+    skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, :'Pop Culture'
+  end
+# binding.pry
 end
