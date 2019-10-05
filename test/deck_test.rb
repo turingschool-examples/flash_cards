7edefe40770048c8e9741c5cc06d035a01cb8609
David Holtkamp <david.hholtkamp@gmail.com>
2019-10-05 12:20:34 -0600

    Wrote count test and category test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,3 +2,29 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
+
+class DeckTest < Minitest::Test
+
+def setup
+  @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  @cards = [@card_1, @card_2, @card_3]
+
+  @deck = Deck.new(@cards)
+  end
+
+  def test_it_exists
+    assert_instance_of Deck, @deck
+  end
+
+  def test_it_has_a_count
+    assert_equal 3, @deck.count
+  end
+
+  def test_it_has_a_category
+    assert_equal [@card_2, @card_3], @deck.cards_in_category(:STEM)
+  end
+
+
+end
