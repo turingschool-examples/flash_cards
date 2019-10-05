372d8d71a20e8282bd492c018f910810f4281201
Nathan Keller <keller.nathan@gmail.com>
2019-10-05 13:53:40 -0600

    added a lot of information to the round.rb class file. defined take turn and current_card. created 2 tests to check the round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,29 @@
 class Round
-    attr_accessor :round
-                  :take_turn
+    attr_accessor :deck,
+                  :turns
 
-      def initialize
+      def initialize(deck)
+        @deck = deck
+        @turns = []
       end
+
+  def current_card
+    @deck.cards.first
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    @deck.cards.shift
+    new_turn
+  end
+
+# need to check the round against the guess 
+  def number_correct
+    default_guess == 0
+    default_guess.each do |default_guess|
+
+    end
+  end
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
@@ -5,7 +6,38 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-def test_if_there_is_a_deck
+class RoundTest < Minitest::Test
 
-  assert_instance_of Deck, @deck
+  def setup
+    @cards = [
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography),
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM),
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+             ]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+  end
+
+
+  def test_if_there_is_a_round
+
+    assert_instance_of Round, @round
+  end
+
+
+  def test_if_round_is_initialized_with_deck
+    round = Round.new(@deck)
+    assert_equal 3, round.deck.count
+  end
+
+
+  def test_if_round_has_an_array_of_turns
+
+    assert_equal
+  end
+
+  def test_if_
+skip
+  end
+
 end
