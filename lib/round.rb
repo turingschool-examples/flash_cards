4db6bf4d50e3c3b26a1c0a7eef7dd3bf5010df40
Nathan Keller <keller.nathan@gmail.com>
2019-10-05 14:43:50 -0600

    added percent correct method to round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -18,12 +18,44 @@ class Round
     new_turn
   end
 
-# need to check the round against the guess 
-  def number_correct
-    default_guess == 0
-    default_guess.each do |default_guess|
 
+  def number_correct
+    default_guess = 0.0
+    @turns.each do |turn|
+      if turn.correct?
+        default_guess += 1.0
+      end
     end
+    default_guess
   end
+
+  def number_correct_by_category(category)
+    default_cat_guess = 0.0
+    @turns.each do |turn|
+      if turn.card.category == category && turn.correct?
+        default_cat_guess += 1.0
+      end
+    end
+    default_cat_guess
+  end
+
+  def percent_correct
+    (number_correct / @turns.count * 100).round(1)
+  end
+
+def percent_correct_by_category(pcat)
+    default_percent_guess = 0.0
+    @turns.each do |turn|
+      if turn.card.correct?
+        default_percent_guess += 1.0
+      end
+    end
+    default_percent_guess
+        (number_cat_correct / @turns.count * 100).round(1)
+  end
+
+
+
+
 
 end
