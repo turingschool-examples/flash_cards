b7e7918e5d047714c63b1176d32eeca67f612184
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-04 23:15:13 -0600

    Add cards to runner

--- runner.rb
+++ runner.rb
@@ -5,25 +5,34 @@ require_relative 'lib/turn'
 require_relative 'lib/deck'
 require_relative 'lib/round'
 
-card_0 = Card.new("What is 1+1?", "two", :Math)
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+card_0 = Card.new("What is 3.14", "pi", :STEM)
+card_1 = Card.new("What is the capital of Alaska?", "juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "north north west", :STEM)
+card_4 = Card.new("What rock group is best known for the song Bohemian Rhapsody?", "queen", :PopCulture)
+card_5 = Card.new("What is the southernmost continent?", "antarctica", :Geography)
+card_6 = Card.new("What is the name of Jon Snow's direwolf in Game of Thrones?", "ghost", :PopCulture)
+card_7 = Card.new("What is the capital of Russia?", "moscow", :Geography)
+card_8 = Card.new("The number 1.618 is known as the _______ _____.", "golden ratio", :STEM)
+card_9 = Card.new("In 2009, Heath Ledger posthumously won an Oscar for what role?", "the joker", :PopCulture)
 
-@cards = [card_0, card_1, card_2, card_3]
+
+@cards = [card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9]
 @deck = Deck.new(@cards)
 @round = Round.new(@deck)
 @total_cards = @cards.count
+@original_cards = [card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9]
+
+p "Welcome! You're playing with #{@total_cards} cards!"
+p "-" * 50
 
 def start
   while @round.deck.count > 0
-    p "Welcome! You're playing with #{@total_cards} cards!"
-    p "-" * 50
     p "This is card #{@round.turns.length + 1} out of #{@total_cards}"
     p "Question: #{@round.current_card.question}"
-    @round.take_turn(gets.chomp)
-    p @round.turns.first.feedback
-    puts ""
+    @round.take_turn(gets.chomp.downcase)
+    p @round.turns[@round.turns.count - 1].feedback
+    puts "-" * 50
   end
 end
 
@@ -32,22 +41,22 @@ def game_over
   p "You had #{@round.number_correct} correct guesses out of #{@total_cards} for a total of #{@round.percent_correct}%"
 end
 
-def get_categories
-  @cards.map do |card|
-    card.category
-  end
-end
+# def get_categories
+#   @cards.map do |card|
+#     card.category
+#   end
+# end
 
 def print_results_by_category
-  all_cats = get_categories
+  all_cats = @original_cards.map do |card|
+    card.category
+  end
 
   all_cats.uniq.each do |category|
-    # require "pry"; binding.pry
-    p "#{category} - #{@round.percent_correct}"
+    p "#{category} - #{@round.percent_correct_by_category(category)}% correct"
   end
 end
 
-# require "pry"; binding.pry
 start
 game_over
 print_results_by_category
