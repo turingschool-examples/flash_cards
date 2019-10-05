94ce8c57ef005f70e88a764577631be8c6b4606b
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-05 08:18:49 -0600

    Add finished round

--- lib/round.rb
+++ lib/round.rb
@@ -5,38 +5,59 @@ require './lib/turn'
 
 class Round
   attr_reader :deck,
-              :turns,
-              :current_card_index,
-              :number_of_correct_guesses
+              :turns
 
   def initialize(deck)
     @deck = deck
     @turns = []
-    @current_card_index = 0
-    @number_of_correct_guesses = 0
+
   end
 
   def current_card
-    @deck.cards[@current_card_index]
+    @deck.cards.first
   end
 
   def take_turn(guess)
     player_turn = Turn.new(guess,current_card)
     @turns << player_turn
-    @current_card_index += 1
-     if player_turn.correct?
-       @number_of_correct_guesses += 1
-    end
+    @deck.cards.shift
     player_turn
   end
 
   def number_correct
-    @number_of_correct_guesses
+    number_of_correct_guesses = 0
+    @turns.each do |turn|
+      if turn.correct?
+        number_of_correct_guesses += 1
+      end
+    end
+    number_of_correct_guesses
   end
 
+  def number_cat_correct(cat)
+    number_correct_in_category = 0
+    @turns.each do |turn|
+      if turn.card.category == cat
+        if turn.correct?
+          number_correct_in_category += 1
+        end
+      end
+    end
+  number_correct_in_category
+end
 
   def percent_correct
-    number_correct / @turns.count * 100
+    (number_correct.to_f / @turns.count.to_f * 100).round(1)
   end
 
+  def percent_correct_by_category(cat)
+    total_correct_in_category = 0
+      @turns.each do |turn|
+        if turn.card.category == cat
+          total_correct_in_category += 1
+        end
+      end
+      total_correct_in_category
+    (number_cat_correct(cat).to_f / total_correct_in_category.to_f * 100).round(1)
+  end
 end
