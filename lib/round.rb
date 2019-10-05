3f4610ce7fe6a51b77befffe6050f428efeb6a20
David Holtkamp <david.hholtkamp@gmail.com>
2019-10-05 16:21:36 -0600

    Take turn method..hopefully works

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,22 @@
 class Round
-  attr_reader :deck 
+  attr_reader :deck
+
   def initialize(deck)
     @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(current_card, guess)
+    @turns << new_turn
+    new_turn
+  end
+
+  def correct?
   end
 
 end
