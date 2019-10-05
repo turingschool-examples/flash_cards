be0afe877c43133e9e4bd73978e978dce61d8771
David Holtkamp <david.hholtkamp@gmail.com>
2019-10-05 12:18:31 -0600

    Wrote count method and category methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,14 @@ attr_reader :cards
   def initialize (cards)
     @cards = cards
   end
-end 
+
+def count
+  @cards.count
+end
+
+def cards_in_category(category)
+  @cards.select {|card|card.category == category }
+end
+
+
+end
