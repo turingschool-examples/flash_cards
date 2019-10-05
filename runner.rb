d58fdd88f1d01158e0f79c45dcd66f73a80ef6b2
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-05 15:23:33 -0600

    Update cosmetic changes

--- runner.rb
+++ runner.rb
@@ -5,7 +5,7 @@ require_relative 'lib/turn'
 require_relative 'lib/deck'
 require_relative 'lib/round'
 
-card_0 = Card.new("What is 3.14", "pi", :STEM)
+card_0 = Card.new("The number 3.14 is better known as ______?", "pi", :STEM)
 card_1 = Card.new("What is the capital of Alaska?", "juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "north north west", :STEM)
@@ -18,34 +18,30 @@ card_9 = Card.new("In 2009, Heath Ledger posthumously won an Oscar for what role
 
 
 @cards = [card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9]
+@original_cards = [card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9]
 @deck = Deck.new(@cards)
 @round = Round.new(@deck)
 @total_cards = @cards.count
-@original_cards = [card_0, card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8, card_9]
 
-p "Welcome! You're playing with #{@total_cards} cards!"
-p "-" * 50
+puts "-" * 50
+puts "Welcome! You're playing with #{@total_cards} cards!"
+puts "-" * 50
 
 def start
   while @round.deck.count > 0
-    p "This is card #{@round.turns.length + 1} out of #{@total_cards}"
-    p "Question: #{@round.current_card.question}"
+    puts "-" * 50
+    puts "This is card #{@round.turns.length + 1} out of #{@total_cards}"
+    puts "Question: #{@round.current_card.question}"
     @round.take_turn(gets.chomp.downcase)
-    p @round.turns[@round.turns.count - 1].feedback
+    puts @round.turns[@round.turns.count - 1].feedback
     puts "-" * 50
   end
 end
 
 def game_over
-  p "********** GAME OVER! **********"
-  p "You had #{@round.number_correct} correct guesses out of #{@total_cards} for a total of #{@round.percent_correct}%"
+  puts "********** GAME OVER! **********"
+  puts "You had #{@round.number_correct} correct guesses out of #{@total_cards} for a total of #{@round.percent_correct}%"
 end
-
-# def get_categories
-#   @cards.map do |card|
-#     card.category
-#   end
-# end
 
 def print_results_by_category
   all_cats = @original_cards.map do |card|
@@ -53,8 +49,9 @@ def print_results_by_category
   end
 
   all_cats.uniq.each do |category|
-    p "#{category} - #{@round.percent_correct_by_category(category)}% correct"
+    puts "#{category} - #{@round.percent_correct_by_category(category)}% correct"
   end
+  puts ""
 end
 
 start
