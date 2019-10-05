ffc59f6f232908efc98d4069079bd396aba166d5
Carleigh Crockett <capleugh@gmail.com>
2019-10-04 21:53:20 -0600

    Create Round class and test

---
+++ test/round_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+class RoundTest < Minitest::Test
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("What is the tallest mountain in Russia?", "Mount Elbrus", :Geography)
+    @card_3 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_4 = Card.new("Where is the world's largest living aspen grove?", "Utah", :STEM)
+    @card_5 = Card.new("How many paintings did Vincent Van Gogh sell during his lifetime?", "1", :Art)
+    @card_6 = Card.new("What Spanish painter is often referred to as both the last of the Old Masters and the first of the moderns?", "Francisco de Goya", :Art)
+
+    @cards = [@card_1, @card_2, @card_3, @card_4, @card_5, @card_6]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+  end
+
+  def test_it_exists
+    assert_instance_of Round, @round
+  end
+
+  def test_initialize
+    assert_equal @deck, @round.deck
+    assert_equal [], @round.turns
+  end
+end
