55c67ad320fd908532efede2145b15b374fdc4e5
Alison Vermeil <alison.minton@gmail.com>
2019-10-06 09:51:58 -0600

    Adjust spacing for readability and add code to allow player to play one more time

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,31 +4,41 @@ require './lib/turn'
 require './lib/round'
 
 def start
+
   deck = []
 
   deck << card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
   deck << card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
   deck << card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :Geography)
+
   deck << card_4 = Card.new("What TV show is Jennifer Aniston most famous for?", "Friends", "pop culture")
+
   deck << card_5 = Card.new("In what state was the Fresh Prince born and raised?", "Pennsylvania", "pop culture")
+
   deck << card_6 = Card.new("How many legs do spiders have?", "8", :STEM)
 
   original_deck = deck.count
+
   deck = Deck.new(deck)
+
   round = Round.new(deck)
+
   card_number = 0
 
   puts "Welcome! You're playing with #{original_deck} cards."
   puts "-" * 50
 
   until deck.count == 0
+
     card_number += 1
 
-  puts "This is card number #{card_number} out of #{original_deck}."
-  puts "Question: #{round.current_card.question}"
-  print ">"
-  player_guess = gets.chomp.capitalize
-  puts round.take_turn(player_guess).feedback
+    puts "This is card number #{card_number} out of #{original_deck}."
+    puts "Question: #{round.current_card.question}"
+    print ">"
+    player_guess = gets.chomp.capitalize
+    puts round.take_turn(player_guess).feedback
   end
 
   puts "****** Game over! ******"
@@ -40,13 +50,11 @@ def start
 end
 start
 
-#puts "****** Do you want to try again? Enter Y or N. ******"
-#print ">"
-# user_answer = gets.chomp.upcase
-# until user_answer == "N"
-#   if user_answer == "Y"
-#     start
-#   elsif user_answer == "N"
-#     puts "Thanks for playing!"
-#   end
-# end
+puts "****** Do you want to try one more time? Enter Y or N. ******"
+print ">"
+user_answer = gets.chomp.upcase
+  if user_answer == "Y"
+    start
+  elsif user_answer == "N"
+    puts "****** Thanks for playing! ****** "
+  end
