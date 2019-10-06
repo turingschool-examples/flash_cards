5938077859a57af8669c2a17a7a867fba731dbe8
Ana Perez Santos <a.jperez@yahoo.com>
2019-10-06 12:43:47 -0600

    Created turn class, first set of tests, and included comments for self-reference

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,4 +4,46 @@ require './lib/turn'
 require './lib/card'
 
 class TurnTest < Minitest::Test
+# You can add a setup method which includes will call on the instance variables of Turn and Card.
+# before each test you create, so you don't have to type them every single time, as I did below.
+# I did include them every time, just for practice.
+# Setup syntax:
+# def setup
+  # card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  # turn = Turn.new("Juneau", card)
+# end
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
+  end
+
+  def test_it_has_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_it_has_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal card, turn.card
+  end
+
+  def test_if_guess_correct?
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal true, turn.correct?
+  end
+
+  def test_it_has_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Correct!", turn.feedback
+  end
 end
