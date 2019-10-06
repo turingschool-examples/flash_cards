f7b91afda189b4a894008195a33d3d26940c6435
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-06 11:48:55 -0600

    Remove spacing in Card Test

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,9 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-require './lib/turn'
 
 class CardTest < Minitest::Test
+
   def test_it_exists
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     assert_instance_of Card, card
@@ -11,19 +11,16 @@ class CardTest < Minitest::Test
 
   def test_it_has_a_question
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     assert_equal "What is the capital of Alaska?", card.question
   end
 
   def test_it_has_an_answer
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     assert_equal "Juneau", card.answer
   end
 
   def test_it_has_a_category
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     assert_equal :Geography, card.category
   end
 end
