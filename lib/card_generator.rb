385d806c143667f578d4c85f402dfde2125cf02f
msimon42 <msimon@uab.edu>
2019-10-06 17:11:51 -0600

    Add CardGenerator class

---
+++ lib/card_generator.rb
@@ -0,0 +1,14 @@
+class CardGenerator
+  attr_accessor :uploaded_cards
+
+  def initialize
+    @uploaded_cards = Array.new
+  end
+
+  def generate(filename)
+    File.open(filename).each do |line|
+      split_line = line.split(",")
+      @uploaded_cards << Card.new(split_line[0], split_line[1], split_line[2])
+    end
+  end
+end
