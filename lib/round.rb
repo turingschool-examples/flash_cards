bb9ef9a0f7774b5bcda0e07b5b4f396ae0572304
msimon42 <msimon@uab.edu>
2019-10-06 16:21:28 -0600

    Add ability to keep track of correct answers per category

--- lib/round.rb
+++ lib/round.rb
@@ -1,14 +1,16 @@
+#require 'pry'
 require_relative 'turn'
 require_relative 'deck'
 
 class Round
-  attr_accessor :deck, :correct, :total, :score, :turns
+  attr_accessor :deck, :correct, :total, :score, :turns, :correct_by_cat
 
   def initialize(deck)
     @deck = deck
     @correct = 0
     @total = deck.count
     @turns = 0
+    @correct_by_cat = Hash[deck.list_categories.collect {|cat| [cat, 0]}]
   end
 
   def start
@@ -17,7 +19,7 @@ class Round
   end
 
   def select_card
-    @deck.cards.sample
+    @deck.unused_cards.sample
   end
 
   def take_turn(guess, card)
@@ -25,6 +27,7 @@ class Round
 
     if turn.correct?
       @correct += 1
+      @correct_by_cat[card.category.to_s] += 1
     end
 
     @turns += 1
@@ -32,8 +35,22 @@ class Round
     puts turn.feedback
   end
 
+  def get_score_by_category
+    result = Array.new
+
+    @correct_by_cat.each do |cat, val|
+      result << "#{cat.split("_").map{|word| word.capitalize}.join(" ")}: #{self.get_score(val, deck.cards_in_category(cat).length)}%"
+    end
+
+    result
+
+  end
+
   def get_score(correct, total)
+
     (correct.to_f / total.to_f).round(2) * 100
   end
 end
+
+
 
