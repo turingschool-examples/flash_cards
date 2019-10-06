b6e70dee6ebd611b06c5aefef2adbbaeb521b87c
Ana Perez Santos <a.jperez@yahoo.com>
2019-10-06 14:47:56 -0600

    Add setup, reorganized methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,27 +1,41 @@
 class Deck
+  def initialize(cards)
+  end
 
 end
 
-# pry(main)> require './lib/turn'
-# #=> true
-#
+
 # pry(main)> require './lib/card'
 # #=> true
 #
-# pry(main)> card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-# #=> #<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>
+# pry(main)> require './lib/deck'
+# #=> true
 #
-# pry(main)> turn = Turn.new("Saturn", card)
-# #=> #<Turn:0x00007f998413ee60 @card=#<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>, @guess="Saturn">
+# pry(main)> card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
 #
-# pry(main)> turn.card
-# => #<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>
+# pry(main)> card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+# #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
 #
-# pry(main)> turn.guess
-# => "Saturn"
+# pry(main)> card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+# #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
 #
-# pry(main)> turn.correct?
-# => false
+# pry(main)> cards = [card_1, card_2, card_3]
 #
-# pry(main)> turn.feedback
-# => "Incorrect."
+# pry(main)> deck = Deck.new(cards)
+# #=> #<Deck:0x00007fa160a38ed8...>
+#
+# pry(main)> deck.cards
+# #=> [#<Card:0x00007fa16104e160...>, #<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
+#
+# pry(main)> deck.count
+# #=> 3
+#
+# pry(main)> deck.cards_in_category(:STEM)
+# #=> [#<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
+#
+# pry(main)> deck.cards_in_category(:Geography)
+# #=> [#<Card:0x00007fa16104e160...>]
+#
+# pry(main)> deck.cards_in_category("Pop Culture")
+# #=> []
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,37 +5,15 @@ require './lib/deck'
 
 
 class DeckTest < Minitest::Test
-  card_1 = Card.new("In what planet does the game 'Skyrim' take place?", "Nirn", :VideoGames)
-  card_2 = Card.new("What year were the Colorado Rockies founded?", 1991, :SportsHistory)
-  card_3 = Card.new("What famous actor starred in the movies 'The Rocky Horror Picture Show', 'Clue', and 'It'", "Tim Curry", :MovieTrivia)
-  deck = Deck.new(card_1, card_2, card_3)
+  def setup
+    card_1 = Card.new("In what planet does the game 'Skyrim' take place?", "Nirn", :VideoGames)
+    card_2 = Card.new("What year were the Colorado Rockies founded?", 1991, :SportsHistory)
+    card_3 = Card.new("What famous actor starred in the movies 'The Rocky Horror Picture Show', 'Clue', and 'It'", "Tim Curry", :MovieTrivia)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+  end
 
-  assert_instance_of Deck, deck
+  def test_deck_exists
+    assert_instance_of Deck, deck
+  end
 end
-
-# card_1 = Card.new("In what planet does the game 'Skyrim' take place?", "Nirn", :VideoGames)
-# card_2 = Card.new("What year were the Colorado Rockies founded?", 1991, :SportsHistory)
-# card_3 = Card.new("What famous actor starred in the movies 'The Rocky Horror Picture Show', 'Clue', and 'It'", "Tim Curry", :MovieTrivia)
-#
-#
-# deck = Deck.new(card_1, card_2, card_3)
-#
-# assert_instance_of Deck, deck
-
-
-
-
-
-# class DeckTest < Minitest::Test
-#
-# def setup
-#   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#   cards = [card_1, card_2, card_3]
-#   deck = Deck.new(cards)
-# end
-#
-# def test_instance_of_deck_exists
-#   assert_instance_of Deck, deck
-# end
-#
-# end
