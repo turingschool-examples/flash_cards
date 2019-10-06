375d78f482951d7c6d18bdbad953d2f33bbd1a65
Paul Debevec <paulmdebevec@gmail.com>
2019-10-06 12:10:01 -0600

    Created tests for the Round class

--- test/round_test.rb
+++ test/round_test.rb
@@ -22,4 +22,63 @@ class RoundTest < Minitest::Test
     assert_kind_of(Turn, round.turns[0])
   end
 
+  def test_current_card
+    round = Round.new(@deck)
+    assert_equal(@card_1, round.current_card)
+  end
+
+  def test_take_turn
+    round = Round.new(@deck)
+    turn = round.take_turn("Monty Python")
+    assert_kind_of(Turn, turn)
+    assert_equal(1, round.turns.count)
+    assert_equal(2, round.deck.count)
+  end
+
+  def test_number_correct
+    round = Round.new(@deck)
+    turn = round.take_turn("Monty Python")
+    assert_equal(1, round.number_correct)
+  end
+
+  def test_number_incorrect
+    round = Round.new(@deck)
+    turn = round.take_turn("Monty Pythooo")
+    assert_equal(0, round.number_correct)
+  end
+
+  def test_number_correct_by_category
+    round = Round.new(@deck)
+    round.take_turn("Monty Python")
+    round.take_turn("Star Wars")
+    round.take_turn("Rick and Morty")
+    assert_equal(2, round.number_correct_by_category(:Movie))
+    assert_equal(1, round.number_correct_by_category(:TV))
+  end
+
+  def test_number_incorrect_by_category
+    round = Round.new(@deck)
+    round.take_turn("Monty Python")
+    round.take_turn("Star War")
+    round.take_turn("Rick and Mourny")
+    assert_equal(1, round.number_correct_by_category(:Movie))
+    assert_equal(0, round.number_correct_by_category(:TV))
+  end
+
+  def test_percent_correct
+    round = Round.new(@deck)
+    round.take_turn("Monty Python")
+    round.take_turn("Star Wars")
+    round.take_turn("Rick and Morty")
+    assert_equal(100, round.percent_correct)
+  end
+
+  def test_percent_correct_by_category
+    round = Round.new(@deck)
+    round.take_turn("Monty Pycharm")
+    round.take_turn("Star Wars")
+    round.take_turn("Rick and Morty")
+    assert_equal(50, round.percent_correct_by_category(:Movie))
+    assert_equal(100, round.percent_correct_by_category(:TV))
+  end
 end
