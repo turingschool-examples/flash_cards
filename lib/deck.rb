7de5c7477a6dd76ca0c7a8b0682acf44c16d2353
msimon42 <msimon@uab.edu>
2019-10-06 16:19:11 -0600

    Create a separate attribute for unused cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,10 +2,11 @@
 require_relative 'card'
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :unused_cards
 
   def initialize(cards)
     @cards = cards
+    @unused_cards = cards.dup
   end
 
   def count
@@ -17,7 +18,7 @@ class Deck
   end
 
   def remove_from_deck(card)
-    cards.delete(card)
+    @unused_cards.delete(card)
   end
 
   def random_cards(num)
@@ -28,9 +29,12 @@ class Deck
     cards.uniq {|card| card.category}.map {|card| card.category.to_s}
   end
 
-  def cards_in_category(cat)
+  def cards_in_category(category)
+
     list = cards.select do |card|
-      card.category == cat
+
+      card.category.to_s == category
+
     end
     if list == nil
       "Invalid input"
