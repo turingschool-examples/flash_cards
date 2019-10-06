2f625835757d65862b9ccd90670405d0aa622e0c
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-05 18:40:58 -0600

    Add runner file with start method

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -4,41 +4,50 @@ require './lib/card'
 require './lib/deck'
 require './lib/round'
 require './lib/turn'
-require 'pry'
+# require 'pry'
 
-class FlashcardRunner
-  attr_reader :round,
-              :deck
+# class FlashcardRunner
+#   attr_reader :round,
+#               :deck,
+#               :turns
 
-def setup
-  @card  = [@question, @answer, @category]
-  card_1 = Card.new("What is Princess Leia's home planet?", "Alderaan", :Geography)
-  card_2 = Card.new("What planet are Ewoks from?", "Endor", :Geography)
-  card_3 = Card.new("Who was the first American woman in space?", "Sally Ride", :STEM)
-  card_4 = Card.new("What is the fastest land mammal?", "Cheetah", :STEM)
-  card_5 = Card.new("Who was the first man on the moon?", "Neil Armstrong", :STEM)
-  @cards = [card_1, card_2, card_3, card_4, card_5]
-  @deck  = Deck.new(@cards)
-  @round = Round.new(@deck)
-end
+#   def initialize
+#     @deck = deck
+#     @round = round
+#   end
 
-  def initialize(deck,round)
-    @deck = deck
-    @round = round
-  end
+    #@card  = [@question, @answer, @category]
+    @card_1 = Card.new("What is Princess Leia's home planet?", "Alderaan", :Geography)
+    @card_2 = Card.new("What planet are Ewoks from?", "Endor", :Geography)
+    @card_3 = Card.new("Who was the first American woman in space?", "Sally Ride", :STEM)
+    @card_4 = Card.new("What is the fastest land mammal?", "Cheetah", :STEM)
+    @card_5 = Card.new("Who was the first man on the moon?", "Neil Armstrong", :STEM)
+    @cards = [@card_1, @card_2, @card_3, @card_4, @card_5]
+    @card_total = @cards.count
+    @deck  = Deck.new(@cards)
+    @round = Round.new(@deck)
+
+  puts "Welcome! You are playing with #{@round.deck.count} cards."
 
   def start
-    puts "Welcome! You are playing with #{round.deck.count} cards."
-    puts "~" * 50
-
-    while (@round.turns.count + 1) <= @round.deck.count
-      puts "This is card number #{round.turns.count} of 5."
+    while @round.turns.count + 1 <= @card_total
+      puts "~" * 50
+      puts "This is card number #{@round.turns.count + 1} of 5."
       puts "Question: #{@round.current_card.question}"
-      @round.take_turn(gets.chomp.capitalize)
-      player_guess.correct?
-      puts @round.turn.last.feedback
-    #puts "The correct answer is #{deck.card.answer.first}"
+      player_guess = @round.take_turn(gets.chomp.capitalize)
+        # if @round.take_turn.correct?
+        #   puts "#{round.turns.last.feedback}"
+        # #puts "Correct"
+        # else
+        #   puts "The correct answer is #{deck.card.answer.first}"
+        end
     end
-    start
-  end
-end
+#   end
+# end
+
+start
+
+
+# game1 = FlashcardRunner.new
+# game1.start
+# end
