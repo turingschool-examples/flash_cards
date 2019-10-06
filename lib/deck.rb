1bd68a5f1dc74a55175a9064ce3ede9d22389cb5
William Meighan <williamdmeighan@gmail.com>
2019-10-06 13:15:21 -0600

    Finish Deck class and methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,3 +9,15 @@ class Deck
   def count
     @cards.count
   end
+
+  def cards_in_category(category)
+    cards_in_category = []
+    cards.each do |card|
+      if card.category == category
+        cards_in_category << card
+      end
+    end
+    cards_in_category
+  end
+
+end
