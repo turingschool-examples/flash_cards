266782dcca6a047b592ed7684ac1bcc31ec3e2c2
Phil DeLong <philjdelong@gmail.com>
2019-10-07 11:35:14 -0600

    Edit methods, not yet complete

--- lib/round.rb
+++ lib/round.rb
@@ -1,34 +1,39 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :round_counter, :turns, :percent_correct
 
   def initialize(deck)
     @deck = deck
-
-    def turns
-      turns = []
-      turns << @turn.new_turn
-    end
-
+    @round_counter = 0
+    @number_correct = 0
+    @turns = []
+  end
 
     def current_card
-      deck.cards[0]
+      deck.cards[@round_counter]
     end
 
-
-    def take_turn
-      @turn.new_turn
+    def take_turn(guess)
+      new_turn = Turn.new(guess, current_card)
+      turns << new_turn
+      @round_counter += 1
+      new_turn
     end
-
 
     def number_correct
+      if take_turn.guess == take_turn.answer
+        @number_correct +=1
     end
 
 
-    def number_correct_by_category
+    def number_correct_by_category(category)
+      if @turns.guess == @turns.asnwer
+      turns(category)
+      end
     end
 
 
     def percent_correct
+      @number_correct / @round_counter
     end
 
 
