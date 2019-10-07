733980fbce8c8f72593d0b6519fb5ce1a72cc4e0
Phil DeLong <philjdelong@gmail.com>
2019-10-06 19:11:43 -0600

    Define methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,2 +1,39 @@
 class Round
+  attr_reader :deck
+
+  def initialize(deck)
+    @deck = deck
+
+    def turns
+      turns = []
+    end
+
+
+    def current_card
+      deck.cards[0]
+    end
+
+
+    def take_turn
+      turn.new_turn
+    end
+
+
+    def number_correct
+    end
+
+
+    def number_correct_by_category
+    end
+
+
+    def percent_correct
+    end
+
+
+    def percent_correct_by_category
+    end
+
+  end
+
 end
