b7d3978f2859dc07a2fcdd96de17fb8fb09352e5
David Holtkamp <david.hholtkamp@gmail.com>
2019-10-07 06:40:43 -0600

    Testing number correct in round

--- test/round_test.rb
+++ test/round_test.rb
@@ -11,9 +11,7 @@ class RoundTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
     @deck = Deck.new([@card_1, @card_2, @card_3])
-
     @round = Round.new(@deck)
   end
 
@@ -25,6 +23,11 @@ class RoundTest < Minitest::Test
     assert_equal @card_1, @round.current_card
   end
 
+  def test_it_has_turns
+    skip
+    assert_instance_of [], @turns
+  end
+
   def test_it_takes_a_turn
     assert_instance_of Turn, @round.take_turn("Juneau")
   end
@@ -33,4 +36,13 @@ class RoundTest < Minitest::Test
     player_turn = @round.take_turn("Juneau")
     assert_equal player_turn.guess, player_turn.card.answer
   end
+
+  def test_number_correct
+    assert_equal 1, @round.number_correct
+  end
+
+  def test_it_is_new_current_card
+    assert_equal @card_2, @round.current_card
+  end
+
 end
