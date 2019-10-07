585e3cc236a18766eb4ddd1a965be345b6aeb721
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-06 18:12:45 -0600

    Fix spacing in Round Test

--- test/round_test.rb
+++ test/round_test.rb
@@ -6,6 +6,7 @@ require './lib/deck'
 require './lib/turn'
 
 class RoundTest < Minitest::Test
+
   def setup
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
