14ba4c29329b447beb52785be713b067a2ccf4b6
Linda Le <yueling1222@gmail.com>
2019-10-07 00:28:41 -0600

    Fix percent_correct_by_category

--- lib/round.rb
+++ lib/round.rb
@@ -42,16 +42,25 @@ class Round
 
 
   def number_correct_by_category(category)
-    number_correct_by_category = 0
+    number_correct_in_category = 0
     @turns.each do |turn|
-      if turn.card.category == category && turn.correct?
-        number_correct_by_category += 1
-      end
+       if turn.card.category == category && turn.correct?
+        number_correct_in_category += 1
+       end
     end
-    number_correct_by_category
-    end
+    number_correct_in_category
+  end
 
   def percent_correct_by_category(category)
-    (number_correct_by_category.to_f/@turns.count*100).round
+    category_of_cards_in_turn = @turns.find_all do |turn|
+      turn.card.category == category
+    end
+    ((number_correct_by_category(category).to_f/category_of_cards_in_turn.count)*100).round
   end
 end
+
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+card_3 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+#create variable with empty array and shovel in Card instances
