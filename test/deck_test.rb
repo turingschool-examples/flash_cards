5b34c7bd8676dd576b615cb0d6bcff9007f0a46f
Linda Le <yueling1222@gmail.com>
2019-10-07 02:24:28 -0600

    Update tests in deck and turn tests files

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,15 +5,16 @@ require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
-  attr_reader :deck, :card1, :card2, :card3, :card4, :card5
+  attr_reader :deck,
+              :card1,
+              :card2,
+              :card3
 
   def setup
     @card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @card2 = Card.new("Who was the first woman to win a Noble Prize?", "Marie Curie", :STEM)
-    @card3 = Card.new("What style of art is characterized by its distinct use of dots to form images?", "Pointillism", :Art)
-    @card4 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @card5 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    cards = [@card1, @card2, @card3, @card4, @card5]
+    @card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card3 = card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [@card1, @card2, @card3]
     @deck = Deck.new(cards)
   end
 
@@ -21,8 +22,6 @@ class DeckTest < Minitest::Test
     assert_instance_of Card, @card1
     assert_instance_of Card, @card2
     assert_instance_of Card, @card3
-    assert_instance_of Card, @card4
-    assert_instance_of Card, @card5
   end
 
   def test_deck_exists
@@ -30,14 +29,13 @@ class DeckTest < Minitest::Test
   end
 
   def test_card_count
-    assert_equal 5, deck.count
+    assert_equal 3, deck.count
   end
 
   def test_cards_in_category
     assert_equal :Geography, @card1.category
     assert_equal :STEM, @card2.category
-    assert_equal :Art, @card3.category
-    assert_equal :STEM, @card4.category
-    assert_equal :STEM, @card5.category
+    assert_equal :STEM, @card3.category
   end
+  
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -2,19 +2,24 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
 require './lib/card'
+require './lib/deck'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
-  attr_reader :card1, :card2, :card3, :turn1, :turn2, :turn3
+  attr_reader :card1,
+              :card2,
+              :card3,
+              :turn1,
+              :turn2,
+              :turn3
 
-  # sets up variable to use throughout code
   def setup
     @card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @card2 = Card.new("Who was the first woman to win a Noble Prize?", "Marie Curie", :Science)
-    @card3 = Card.new("What style of art is characterized by its distinct use of dots to form images?", "Pointillism", :Art)
+    @card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card3 = card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     @turn1 = Turn.new("Juneau", @card1)
-    @turn2 = Turn.new("Einstein",@card2)
-    @turn3 = Turn.new("Dadaism", @card3)
+    @turn2 = Turn.new("Mercury",@card2)
+    @turn3 = Turn.new("North north west", @card3)
   end
 
   # checks to see if the card exists
@@ -26,36 +31,36 @@ class TurnTest < Minitest::Test
 
   # checks to see if the turn exists
   def test_turn_exists
-    assert_instance_of Turn, @turn1, card1
-    assert_instance_of Turn, @turn2, card2
-    assert_instance_of Turn, @turn3, card3
+    assert_instance_of Turn, @turn1
+    assert_instance_of Turn, @turn2
+    assert_instance_of Turn, @turn3
   end
 
   #checks that a card is displayed during turn
   def test_card_display
     assert_equal "What is the capital of Alaska?", @card1.question
-    assert_equal "Who was the first woman to win a Noble Prize?", @card2.question
-    assert_equal "What style of art is characterized by its distinct use of dots to form images?", @card3.question
+    assert_equal "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @card2.question
+    assert_equal "Describe in words the exact direction that is 697.5° clockwise from due north?", @card3.question
   end
 
   #checks that answer is submitted during turn
   def test_guess_submission
     assert_equal "Juneau", @turn1.guess
-    assert_equal "Einstein", @turn2.guess
-    assert_equal "Dadaism", @turn3.guess
+    assert_equal "Mercury", @turn2.guess
+    assert_equal "North north west", @turn3.guess
   end
 
   #checks to see if guess is correct
   def test_guess_accuracy
     assert_equal true, @turn1.correct?
     assert_equal false, @turn2.correct?
-    assert_equal false, @turn3.correct?
+    assert_equal true, @turn3.correct?
   end
 
   #checks to ensure feedback is displayed depending on if guess is correct or not
   def test_feedback_display
     assert_equal "Correct!", @turn1.feedback
     assert_equal "Incorrect, try again!", @turn2.feedback
-    assert_equal "Incorrect, try again!", @turn3.feedback
+    assert_equal "Correct!", @turn3.feedback
   end
 end
