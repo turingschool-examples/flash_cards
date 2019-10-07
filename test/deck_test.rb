b822da139b14275f542fe695904f4d52790708c6
Melissa Robbins <melissamrobbins@gmail.com>
2019-10-06 18:10:51 -0600

    Refactor Deck Test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,12 +6,10 @@ require './lib/card'
 class TurnTest < Minitest::Test
 
   def setup
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(@cards)
-    @cards = cards
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
   end
 
   def test_it_exists
