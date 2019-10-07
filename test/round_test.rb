4b7d665920fe794b2080528762bfde2bb33d71ba
msimon42 <msimon@uab.edu>
2019-10-06 19:28:41 -0600

    Add test for get_score_by_category method; Remove test for method that requires cmd line input

--- test/round_test.rb
+++ test/round_test.rb
@@ -8,8 +8,9 @@ require_relative '../lib/turn'
 class RoundTest < Minitest::Test
 
   def setup
-    @card = Card.new("Test", "Test", :test)
-    @cards = [@card]
+    @card1 = Card.new("Test", "Test", :test)
+    @card2 = Card.new("test", "test", :other_test)
+    @cards = [@card1, @card2]
     @deck = Deck.new(@cards)
     @round = Round.new(@deck)
   end
@@ -22,15 +23,21 @@ class RoundTest < Minitest::Test
     assert_nil nil, @round.start
   end
 
-  def test_select_card
-    assert_equal @card, @round.select_card
-  end
-
   def test_take_turn
-    assert_equal "Correct", @round.take_turn('Test', @card)
+    assert_equal "Correct", @round.take_turn('Test', @card1)
   end
 
   def test_get_score
     assert_equal 50, @round.get_score(1,2)
   end
+
+  def test_get_score_by_cat
+
+    @round.take_turn('Test', @card1)
+    @round.take_turn('hello', @card2)
+
+    assert_equal ["Test: 100.0%", "Other Test: 0.0%"], @round.get_score_by_category
+
+  end
 end
+
