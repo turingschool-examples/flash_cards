dfac97171cc13c4624c86575d9ef02eac329815f
Nathan Keller <keller.nathan@gmail.com>
2019-11-18 12:41:22 -0700

    Add tests for turn. Testing attributes

---
+++ test/turn_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
+  def test_it_exists
+    assert_instance_of Turn, @turn
+  end
+
+  def test_it_has_attributes
+    assert_equal @card, @turn.card
+    assert_equal "Juneau", @turn.guess
+  end
+
+end
