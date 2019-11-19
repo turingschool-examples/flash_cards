d751a4efdf019cdf081837798639248b5a0585f3
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-19 08:30:06 -0700

    Added methods to the card class

--- lib/card.rb
+++ lib/card.rb
@@ -5,4 +5,17 @@ class Card
     @answer = answer
     @category = category
   end
+
+  def question
+    p @question
+  end
+
+  def answer
+    p @answer
+  end
+
+  def category
+    p @category
+  end
+  
 end
