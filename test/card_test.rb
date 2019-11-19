d036c26d06214e3c815f1631d1f471f886863bca
Ana Perez Santos <a.jperez@yahoo.com>
2019-11-18 17:12:39 -0700

    Add skips for tests

--- test/card_test.rb
+++ test/card_test.rb
@@ -11,18 +11,21 @@ class CardTest < Minitest::Test
   end
 
   def test_it_has_a_question
+    skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "What is the capital of Alaska?", card.question
   end
 
   def test_it_has_an_answer
+    skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "Juneau", card.answer
   end
 
   def test_it_has_a_category
+    skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal :Geography, card.category
