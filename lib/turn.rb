f2fe26e1e144a74db7eb136fcda0b144f11c00ab
Ana Perez Santos <a.jperez@yahoo.com>
2019-11-18 17:59:04 -0700

    Test a turn includes a card object

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,7 @@
 class Turn
+  attr_reader :card
 
   def initialize(guess, card)
+    @card = card
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -11,5 +11,12 @@ class TurnTest < Minitest::Test
 
     assert_instance_of Turn, turn
   end
+
+  def test_it_has_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal card, turn.card
+  end
   
 end
