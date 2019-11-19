e6d66990ee8494efc6a761db0c7f5a09c9f44fa7
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-19 11:29:34 -0700

    Fixed methods in deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,18 +3,18 @@ class Deck
   attr_accessor :cards
 
   def initialize(cards)
-    @cards = []
-  end
-
-  def cards
-    @cards << Card.new()
+    @cards = cards
   end
 
   def count
-
+    return @cards.length
   end
 
-  def cards_in_category
-
+  def cards_in_category(category_name)
+    @cards.each do |card|
+      if category_name == card.category
+        "#{card}"
+      end
+    end
   end
 end
