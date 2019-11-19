d28ec3ab2640433a591b7ee52d5d4efffb0567ed
Chase.Gaddis <cgaddis36@gmail.com>
2019-11-19 08:38:53 -0700

    Update1

--- lib/card.rb
+++ lib/card.rb
@@ -2,9 +2,16 @@
 category = ["Sports", "American History", "Politics", "Entertainment"]
 
 
-class 1Card
+class Card
   attr_accessor :category, :question, :answer
 
-  def category
-    Sports
-  end
+
+
+end
+
+one = Card.new
+one.category = "Sports"
+one.question = "Who won the NHL Stanley Cup last year?"
+one.answer = "Washington Capitals"
+
+puts
