b571b2b55c792811e1b5fb6a6c9607f1d77758ff
Ryan Allen <rcallen89@gmail.com>
2019-11-19 12:54:38 -0700

    Create Turn Class file

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+#class file for Turn
+
+class Turn
+  attr_accessor :user_guess, :card
+  def initialize(user_guess, card)
+    @user_guess = user_guess
+    @card = card
+  end
+
+  def guess
+    @user_guess
+  end
+
+  def card
+    @card
+  end
+
+  def correct?
+    @user_guess == @card.answer
+  end
+
+  def feedback
+    if correct? == true
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
+
+end
