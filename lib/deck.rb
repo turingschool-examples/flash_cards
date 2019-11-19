eb882ccd277823b896280dd6c8bcdb2aff0c8ed4
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-19 13:27:38 -0700

    Added the methods for the test classes and fixed a method in deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,5 @@
+require_relative 'card.rb'
+require_relative 'turn.rb'
 class Deck
 
   attr_reader :cards
@@ -6,16 +8,31 @@ class Deck
     @cards = cards
   end
 
+  def cards
+    @cards << Card.new(question, answer, category)
+  end
+
   def count
     return @cards.length
   end
 
   def cards_in_category(category_name)
+    array = []
     @cards.each do |card|
       if card.category == category_name
-        "#{card}"
+        array.append(card)
       end
     end
+    return array
   end
 
 end
+
+
+first = Card.new("Yes?", "No", "Georgraphy")
+second = Card.new("No?", "Yes", "Stem")
+cards = [first, second]
+deck = Deck.new(cards)
+#x = deck.cards_in_category("Stem")
+deck.cards_in_category("Stem")
+#p first.category
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,10 +4,22 @@ require './lib/turn'
 
 class TurnTest < Minitest::Test
 
-  def test_it_exists
+  def test_card_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    assert_instance_of Card, card
+  end
+
+  def test_turn_exists
     turn = Turn.new("Juneau", card)
 
     assert_instance_of Turn, turn
+  end
+
+  def test_it_has_a_card
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Card, card
   end
 
   def test_it_has_a_guess
@@ -16,9 +28,17 @@ class TurnTest < Minitest::Test
     assert_equal "Juneau", turn.guess
   end
 
-  def test_it_has_a_card
+  def test_correct?
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
-    assert_instance_of Card, card
+    assert_equal true, turn.correct?
+  end
+
+  def test_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Correct!", turn.feedback
   end
 end
