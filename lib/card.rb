b16b77fdd19bf556d249418f8ac080f15c76b30f
Chase.Gaddis <cgaddis36@gmail.com>
2019-11-19 09:02:39 -0700

    Update2

--- lib/card.rb
+++ lib/card.rb
@@ -5,8 +5,6 @@ category = ["Sports", "American History", "Politics", "Entertainment"]
 class Card
   attr_accessor :category, :question, :answer
 
-
-
 end
 
 one = Card.new
@@ -14,4 +12,18 @@ one.category = "Sports"
 one.question = "Who won the NHL Stanley Cup last year?"
 one.answer = "Washington Capitals"
 
-puts
+two = Card.new
+two.category = "American History"
+two.question = "Who was the first President to be impeached?"
+two.answer = "Andrew Johnson"
+
+three = Card.new
+three.category = "Politics"
+three.question = "Who is the Speaker of the House?"
+three.answer = "Nancy Pelosi"
+
+four = Card.new
+four.category = "Entertainment"
+four.question 
+
+puts "Is #{two.category} a variable for a new instance???"
