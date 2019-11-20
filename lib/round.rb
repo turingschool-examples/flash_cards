18b521c7670c142e13099e306f9496bf246ae2f4
Ryan Allen <rcallen89@gmail.com>
2019-11-19 22:18:48 -0700

    Fix percent_correct method Add percen_correct_by_category method

--- lib/round.rb
+++ lib/round.rb
@@ -1,14 +1,15 @@
 #class file for Round class
 
 class Round
-  attr_accessor :deck, :turns, :current_card, :number_correct, :number_correct_by_category
+  attr_accessor :deck, :turns, :current_card, :number_correct, :number_correct_per_category, :number_asked_per_category
 
   def initialize(deck)
     @deck = deck
     @turns = []
     @current_card = 0
     @number_correct = 0
-    @number_correct_by_category = Hash.new(0)
+    @number_correct_per_category = Hash.new(0)
+    @number_asked_per_category = Hash.new(0)
   end
 
   def current_card
@@ -18,17 +19,32 @@ class Round
   def take_turn(user_guess)
     current_turn = Turn.new(user_guess, current_card)
     @turns.push(current_turn)
-    @number_correct_by_category[current_card.category]
+    #Send in initial keys for the hashes that are defaulted to 0
+    @number_correct_per_category[current_card.category]
+    @number_asked_per_category[current_card.category]
+
+    #Check if the guess equals the answer
     if current_turn.correct? == true
+      #Added one to each correct variable
       @number_correct += 1
-      @number_correct_by_category[current_card.category] += 1
+      @number_correct_per_category[current_card.category] += 1
     end
+    #Add one to go to the next card
+    @number_asked_per_category[current_card.category] += 1
     @current_card += 1
     current_turn
   end
 
   def percent_correct
-    (@turns.size.to_f / @number_correct.to_f) * 100
+    (@number_correct.to_f / @turns.size.to_f) * 100
+  end
+
+  def number_correct_by_category(cat)
+    @number_correct_per_category[cat]
+  end
+
+  def percent_correct_by_category(cat)
+    (@number_correct_per_category[cat].to_f / @number_asked_per_category[cat].to_f) * 100
   end
 
 end
