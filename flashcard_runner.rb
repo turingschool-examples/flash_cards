e060795c815c40d626b67889b3a8354ed5e8ec48
Ryan Allen <rcallen89@gmail.com>
2019-11-20 09:40:08 -0700

    Create start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -5,3 +5,24 @@ card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and repo
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 card_4 = Card.new("What is the capital of Colorado?", "Denver", :Geography)
 card_5 = Card.new("What is the website called to store gits?", "GitHub", :Software)
+
+deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+
+round = Round.new(deck)
+
+def start(round)
+  puts "Welcome! You're playing with #{round.deck.count} cards."
+  puts "----------------------------"
+  until round.current_card == nil
+    puts "This is card number #{round.current_card_number + 1} out of #{round.deck.count} cards."
+    puts round.deck.cards[round.current_card_number].question
+    user_guess = gets.chomp
+    round.take_turn(user_guess)
+    puts round.turns.last.feedback
+  end
+
+  puts "****** Game over! ******"
+
+end
+
+start(round)
--- test/round_test.rb
+++ test/round_test.rb
@@ -25,6 +25,7 @@ class RoundTest < MiniTest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+    require "pry"; binding.pry
 
     assert_equal Deck , round.deck
   end
