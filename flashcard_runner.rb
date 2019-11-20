8c0611ef83594b0e07f39fb1cea043794af1d206
Ryan Allen <rcallen89@gmail.com>
2019-11-20 08:08:58 -0700

    Build 5 Card Objects

---
+++ flashcard_runner.rb
@@ -0,0 +1,7 @@
+#Runs the Flashcard Game
+
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+card_4 = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+card_5 = Card.new("What is the website called to store gits?", "GitHub", :Software)
