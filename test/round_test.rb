de0a2a21ae06a13f5566e6a0831ad8c235424f0e
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-20 15:10:18 -0700

    Added the first four methods to the test class

--- test/round_test.rb
+++ test/round_test.rb
@@ -7,6 +7,45 @@ require './lib/round'
 
 class RoundTest < Minitest::Test
 
+  def test_card_1_exists
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    assert_instance_of Card, card_1
+  end
+
+  def test_card_2_exists
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    assert_instance_of Card, card_2
+  end
+
+  def test_card_3_exists
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    assert_instance_of Card, card_3
+  end
+
+  def test_deck_exists
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_round_exists
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal Round, round
+  end
+
   
 
 end
