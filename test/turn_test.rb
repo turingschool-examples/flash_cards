120d0f2504515ef6b7eb23d54887ba8fa0cd0eb9
Matt Holmes <holmesm8@gmail.com>
2019-11-20 15:48:58 -0700

    Add incorrect guess and incorrect feedback

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -32,4 +32,19 @@ class TurnTest < Minitest::Test
 
     assert_equal "Correct!", turn.feedback
   end
+
+  def test_incorrect_guess
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    assert_equal false, turn.correct?
+  end
+
+  def test_incorrect_feedback
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    assert_equal "Incorrect.", turn.feedback
+  end
+
 end
