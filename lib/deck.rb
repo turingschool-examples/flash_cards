81d86c4b0a9b1f83a144d66b660db603b0722885
Matt Holmes <holmesm8@gmail.com>
2019-11-19 22:31:15 -0700

    Add more tests for deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,21 @@
 class Deck
+  attr_reader :cards
 
-  def initialize
-    
-
+  def initialize(cards)
+    @cards = cards
   end
 
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(category)
+    @cards.each do |card|
+      if card.category == card
+        category
+      end
+    end
+  end
 
 
 
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,15 +1,94 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/turn'
 require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
 
-  def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    deck = Deck.new
+  def test_cards_are_valid
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+
+    assert_instance_of Card, card_1
+  end
+
+  def test_array_is_valid
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+
+    assert_equal [card_1, card_2, card_3], cards
+  end
+
+  def test_deck_is_valid
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
     assert_instance_of Deck, deck
   end
+
+  def test_deck_shows_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    deck.cards
+
+    assert_equal [card_1, card_2, card_3], deck.cards
+  end
+
+  def test_deck_counts_number_of_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    deck.count
+
+    assert_equal 3, deck.count
+  end
+
+  def test_deck_show_stem_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    deck.cards_in_category(:STEM)
+
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+  end
+
+
+
+
+end
+
+
+
+#   def number_of_cards_in_deck
+#     skip
+#     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+#     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+#     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+#     # turn = Turn.new("Juneau", card)
+#     cards = [card_1, card_2, card_3]
+#     deck = Deck.new(cards)
+#     deck.count
+#
+#     assert_equal 3, deck.count
+#   end
+#
+#   def cards_in_category
+#
+#
+#
+#
+# end
