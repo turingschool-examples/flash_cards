d69b23c1c72cbc1a0ab219d6be56c244b19bf240
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-20 16:05:05 -0700

    Initial commit

---
+++ test/turn_test.rb
@@ -0,0 +1,18 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn'
+require './lib/card'
+
+class TurnTest < Minitest::Test
+
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+
+    assert_instance_of Turn, turn
+  end
+
+
+end
