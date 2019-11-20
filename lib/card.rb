52e1c5b1aea24427eedbe50c42ca7440b5fac248
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-20 16:02:34 -0700

    Initial commit

---
+++ lib/card.rb
@@ -0,0 +1,19 @@
+class Card
+  attr_reader :question, :answer, :category
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+# require "pry"; binding.pry
+end
+
+# def question
+#  @question
+# end
+
+card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+# require "pry"; binding.pry
+p card.question
