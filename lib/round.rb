137a166c4f20ea19b1c54bc6634778b5fcdfd712
Ana Perez Santos <a.jperez@yahoo.com>
2019-11-20 08:41:24 -0700

    Complete current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -7,15 +7,7 @@ class Round
   end
 
   def current_card
-    @deck
-    # require "pry"; binding.pry
-    # I need the first card in the array of cards that a deck holds
-    # @deck is calling on the instance of my Deck class, however,
-    # the cards are not accessible in the current state
-    # considering that deck is an instance of Deck, I have access to
-    # its methods, one of which is cards, which holds the array
-    # of cards in a deck
-    # will figure out what the next steps are later
+    @deck.cards[0]
   end
 
 end
