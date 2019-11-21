414934e6a09c3a55e02ce82bc9460f7f5ec52c6f
Chase.Gaddis <cgaddis36@gmail.com>
2019-11-20 18:57:54 -0700

    new commit

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+require "./card"
+
+class Turn
+  attr_reader :card
+  attr_accessor :guess
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = Card
+  end
+
+  def correct?(guess, card)
+    if
+
+  end
+end
