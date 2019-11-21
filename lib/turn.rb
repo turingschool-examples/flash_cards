e2a3a7b7d4de587f1ba5993500c63a1fa9f1a001
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-21 02:21:41 -0700

    Code to complete correct answers

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,25 @@
 class Turn
+  attr_reader :guess, :card
 
   def initialize(guess, card)
+    @guess = guess
+    @card = card
   end
 
-  # def card
-  # end
+  def correct?
+    if @guess = card.answer
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if @guess = card.answer
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
 
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -9,10 +9,28 @@ class TurnTest < Minitest::Test
   def test_it_exists
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-
-
+    # assert_instance_of Card, card
     assert_instance_of Turn, turn
   end
 
+  def test_it_has_a_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
+    assert_equal card, turn.card
+  end
+
+  def test_it_can_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_it_is_correct
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal true, turn.correct?
+  end
 end
