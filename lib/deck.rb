69f4ba0fe717943d4f54d6694e50cca022bce34f
Ezekiel Clark <clarkzeke@gmail.com>
2019-11-20 17:37:41 -0700

    First draft of deck and decktest

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,2 +1,23 @@
+
+
 class Deck
-end 
+
+  attr_reader :cards, :deck
+  def initialize(cards)
+    @cards = cards
+    @deck = []
+  end
+
+  def count
+    @cards.length
+  end
+
+  def cards_in_category(category)
+    @cards.find_all do |card|
+      #require "pry"; binding.pry
+      category == card.category
+
+    end
+  end
+
+end
---
+++ test/deck_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+
+class DeckTest < MiniTest::Test
+  def setup
+    @card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
+
+  def test_cards_exist
+    assert_instance_of Deck, @deck
+  end
+
+  def test_cards_method_works
+    assert_equal @cards, @deck.cards
+  end
+
+  def test_if_count_works
+    assert_equal @cards.length, @deck.count
+  end
+
+  def test_cards_in_category
+    assert_equal [@card2, @card3], @deck.cards_in_category(:STEM)
+  require "pry"; binding.pry
+end
