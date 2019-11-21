6da40c4b0e7b872b7d7b45db68537241ea4098ff
Nathan Keller <keller.nathan@gmail.com>
2019-11-20 17:22:43 -0700

    Revert changes to round

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,19 +6,19 @@ require './lib/deck'
 require './lib/round'
 
 
-  @round = Round.new
+  @round = Round.new(
   @deck = Deck.new([
   # @cards = CardGenerator.newCardGenerator.new('.data/cards.txt').cards
   @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography),
   @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM),
   @card_3 = Card.new("What planet is furthest from the sun?", "Uranus", :STEM),
   @card_4 = Card.new("What is the name of the country that encompasses more than 250 islands?", "Indonesia", :Geography)
-  ])
+  ]))
 
 
 def start
   puts "-" *50
-  puts "Welcome! You're playing with #{@deck.count} cards."
+  puts "Welcome! You're playing with #{@round.deck.count} cards."
   until @round.turns.count == 4
     puts "-" *50
     puts "This is card number #{@round.turns.count + 1} out of 4."
