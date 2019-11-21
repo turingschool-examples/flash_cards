e517bd9f587c97118cf226b1c8ff34f95575a535
Nathan Keller <keller.nathan@gmail.com>
2019-11-20 17:32:24 -0700

    Create test and class. Tested it exists.

---
+++ test/cardgenerator_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+require './lib/cardgenerator'
+
+class CardGeneratorTest < Minitest::Test
+
+  def setup
+    @cardgenerator = CardGenerator.new('./data/cards.txt')
+  end
+
+  def test_it_exists
+    assert_instance_of CardGenerator, @cardgenerator
+  end
+
+
+
+end
