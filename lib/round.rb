f56be3f617d4dc0c5fd551936e567a425bb1b1ef
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-21 08:46:25 -0700

    Finished take_turn method and percent_correct method, as well as their test methods

--- lib/round.rb
+++ lib/round.rb
@@ -5,7 +5,7 @@ require './lib/round'
 
 class Round
 
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :total_correct, :num_correct_by_cat, :num_asked_per_cat
 
   def initialize(deck)
     @deck = deck
@@ -20,15 +20,23 @@ class Round
     @deck.cards[@current_card_number]
   end
 
-  def take_turn(guess, current_card)
+  def take_turn(guess)
     new_turn = Turn.new(guess, current_card)
     turns << new_turn
 
     @current_card_number += 1
-    if 
+    if guess == current_card.answer
+      @total_correct += 1
+      @num_correct_by_cat[current_card.category] += 1
+    end
 
+    @num_asked_per_cat[current_card.category] += 1
 
     new_turn
+  end
+
+  def percent_correct
+    (@total_correct.to_f / @turns.count.to_f) * 100
   end
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -57,4 +57,26 @@ class RoundTest < Minitest::Test
     assert_equal card_1, round.current_card
   end
 
+  def test_take_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_instance_of Turn, round.take_turn("Juneau")
+  end
+
+  def test_percent_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_instance_of Float, round.percent_correct
+  end
+
 end
