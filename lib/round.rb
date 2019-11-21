c7bb52e7e350d822b213e5fdd17004c6295bfbc7
Mariana Cid <cidmariana5@gmail.com>
2019-11-20 17:48:26 -0700

    Add method

---
+++ lib/round.rb
@@ -0,0 +1,13 @@
+class Round
+
+  attr_reader :deck, :turns, :current_card
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+end
