573103c27db2b0d14cd341ad7f7360caf25bd900
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-21 20:11:13 -0700

    Completes turn test and turn code pair

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ class Turn
   end
 
   def correct?
-    if @guess = card.answer
+    if @guess == card.answer
       true
     else
       false
@@ -15,7 +15,7 @@ class Turn
   end
 
   def feedback
-    if @guess = card.answer
+    if @guess == card.answer
       "Correct!"
     else
       "Incorrect."
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -32,5 +32,24 @@ class TurnTest < Minitest::Test
     turn = Turn.new("Juneau", card)
 
     assert_equal true, turn.correct?
+
+    turn2 = Turn.new("Atlanta", card)
+
+    assert_equal false, turn2.correct?
   end
+
+  def test_it_gives_positive_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Correct!", turn.feedback
+  end
+
+  def test_it_gives_negative_feedback
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    assert_equal "Incorrect.", turn.feedback
+  end
+
 end
