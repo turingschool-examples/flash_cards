190dc69ef85e1513d595ceda024045580511f0a2
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-21 22:09:47 -0700

    Defines cards as instance variables in setup

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,6 +9,7 @@ class DeckTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
   end
 
   def test_it_exists
@@ -16,4 +17,11 @@ class DeckTest < Minitest::Test
     assert_instance_of Card, @card_2
     assert_instance_of Card, @card_3
   end
+
+  def test_it_has_a_deck
+    deck = Deck.new(@cards)
+
+    assert_instance_of Deck, deck
+  end
+
 end
