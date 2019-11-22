cd45c88deddcec69e8e788d781b886ed7b68eeae
Maria Ronauli <mronauli@gmail.com>
2019-11-22 08:16:11 -0700

    Add test for categories

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -22,6 +22,7 @@ class DeckTest < MiniTest::Test
   binding.pry
 
   assert_instance_of Deck, deck
+
 end
 
   def count_number_of_cards_in_deck
@@ -39,4 +40,37 @@ end
 
   assert_equal 3, deck.count
   end
+
+  def test_to_return_cards_in_category_STEM
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
+                       reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697 .5°
+                       clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = cards
+    binding.pry
+
+    assert_equal
+  end
+
+  def test_to_return_cards_in_category_geography
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
+                       reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697 .5°
+                       clockwise from due north?", "North north west", :STEM)
+
+  cards = [card_1, card_2, card_3]
+
+  deck = cards
+  binding.pry
+
+  assert_equal
+
+  end
+
+
 end
