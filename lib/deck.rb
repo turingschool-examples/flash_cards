2b76babc78525ecbbf05efbfd0411a9ec10a4c71
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-22 06:00:41 -0700

    Can count number of cards per deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,16 @@
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :category
 
   def initialize(cards)
     @cards = cards
   end
 
-
-  # def count
-  #   @cards.length
-  # end
+  def count
+    @cards.length
+  end
+# call back to card file to use @category ???
+  def cards_in_category(category)
+    @category = category
+  end
 
 end
