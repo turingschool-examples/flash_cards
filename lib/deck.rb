eff69ef9158c7fa46b33470929519664dfb7920b
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-23 15:45:24 -0700

    Cleans out extra commenting and junk code

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,24 +8,19 @@ class Deck
   def count
     @cards.length
   end
-# call back to card file to use @category ???
 
-
+  # call back to card file to use @category ???
   def cards_in_category(category)
     @category = category
     @cards_pile = []
 
     @cards.each do |card|
-      # require "pry"; binding.pry
       if card.category == @category
         @cards_pile << card
-        # require "pry"; binding.pry
-        # puts "FUCKKK"
-    # each do OR find_all ????
       end
     end
 
-    @cards_pile # <--- STEM cards captured
+    @cards_pile # <--- cards captured
   end
 
 end
