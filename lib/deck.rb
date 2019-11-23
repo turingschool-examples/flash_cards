340543bb5906dcfdbfe686d3087b49d76b33d639
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-23 15:37:02 -0700

    Initial card category sort established

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,8 +9,23 @@ class Deck
     @cards.length
   end
 # call back to card file to use @category ???
+
+
   def cards_in_category(category)
     @category = category
+    @cards_pile = []
+
+    @cards.each do |card|
+      # require "pry"; binding.pry
+      if card.category == @category
+        @cards_pile << card
+        # require "pry"; binding.pry
+        # puts "FUCKKK"
+    # each do OR find_all ????
+      end
+    end
+
+    @cards_pile # <--- STEM cards captured
   end
 
 end
