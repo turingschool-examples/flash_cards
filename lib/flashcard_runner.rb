aeb1260800185929ee84f22dda17db504bcb78e1
Maria Ronauli <mronauli@gmail.com>
2019-11-24 08:30:11 -0700

    Create instance of cards array

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -4,11 +4,21 @@ require './lib/deck'
 require './lib/round'
 require 'pry'
 
-i = 0
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697 .5° clockwise from due north?", "North north west", :STEM)
+card_4 = Card.new("What is JLO short for?", "Jennifer Lopez", :Pop_culture)
+binding.pry
+cards = [card_1, card_2, card_3, card_4]
 
-p "Welcome! You're playing with #{@cards.count} cards."
-p "-------------------------------------------------"
-p "This is card number #{@deck.card.first} out of #{@cards.count}."
-p "Question: #{@card.question}?"
+deck = Deck.new(cards)
 
-i += 1
+
+# i = 0
+#
+# p "Welcome! You're playing with #{@cards.count} cards."
+# p"-------------------------------------------------"
+# p "This is card number #{@deck.card.first} out of #{@cards.count}."
+# p "Question: #{@card.question}?"
+#
+# i += 1
