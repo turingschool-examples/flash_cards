d33eab962ff265d67f9eddba50b14dfec9631a53
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-23 17:05:21 -0700

    Initial commit

---
+++ test/round_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
+require './lib/round'
+
+class RoundTest < Minitest::Test
+
+ def test_it_exists
+   round = Round.new(@deck)
+
+   assert_instance_of Round, round
+ end
+
+end
