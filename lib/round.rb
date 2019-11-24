ed85df994591db1a47cae56caac201f91f5b9be7
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-24 11:13:34 -0700

    Defines empty turns array

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,11 @@
+# NOTE: plug in actual values instead of variables to get to run, then go find that exact variable
+
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
+    @turns = []
+    # require "pry"; binding.pry
   end
 end
