d27930b3959379f28a2938ef1bac755d83300ed9
Matt Holmes <holmesm8@gmail.com>
2019-11-24 14:38:38 -0700

    Update testing and runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -8,27 +8,29 @@ require './lib/round'
 @card3 = Card.new("What is the state capitol of Michigan?", "Lansing", :Geography)
 @card4 = Card.new("How many teams are currently in the NFL?", "32", :Sports)
 @cards = [@card1, @card2, @card3, @card4]
+@cards_count = @cards.count
 @deck = Deck.new(@cards)
 @round = Round.new(@deck)
 
 def start
 
-  puts "Welcome! You're playing with #{@deck.count} cards."
+  puts "Welcome! You're playing with #{@cards_count} cards."
   puts "---" * 22
 
-  until @deck.count == @round.turns.count + 1
+  until @round.turns.count + 1 == @cards_count
 
-    puts "This is card number #{@round.turns.count + 1} out of #{@deck.count}."
+    puts "This is card number #{@round.turns.count + 1} out of #{@cards_count}."
     puts "Question: #{@deck.cards.first.question}"
 # get user input and check against current cards
-    @round.take_turn(gets.chomp.to_s)
-    puts " #{@round.deck.last.feedback}"
-
-
+    @round.take_turn(gets.chomp)
+    puts " #{@round.turns.last.feedback}"
   end
+
+  puts "****** Game over! ******"
+  puts "You had #{} correct guesses out of #{@cards_count} for a total score of #{@round.percent_correct}%."
+
 
 
 end
-
 
 start
