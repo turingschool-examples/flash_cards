75003d117d76c08fe580ac729dd842450a004c19
Matt Holmes <holmesm8@gmail.com>
2019-11-24 10:11:37 -0700

    Add runner file

---
+++ flashcard_runner.rb
@@ -0,0 +1,24 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+@card1 = Card.new("Who was the first President of the United States?", "George Washington", :History)
+@card2 = Card.new("Who was the NBA regular season MVP in 2011?", "Derrick Rose", :Sports)
+@card3 = Card.new("What is the state capitol of Michigan?", "Lansing", :Geography)
+@card4 = Card.new("How many teams are currently in the NFL?", "32", :Sports)
+@cards = [@card1, @card2, @card3, @card4]
+@deck = Deck.new(@cards)
+@round = Round.new(@deck)
+
+def start
+  puts "Welcome! You're playing with #{@deck.count} cards."
+  puts "---" * 22
+  puts "This is card number #{@round.turns.count + 1} out of #{@deck.count}."
+  puts "Question: #{@deck.cards.first.question}"
+  gets.chomp
+
+
+end
+
+start
