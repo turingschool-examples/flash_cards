e9aecec7f83dc47c431e4c06bc0795b91639fdf8
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-24 09:00:04 -0700

    Passes it exists test

--- test/round_test.rb
+++ test/round_test.rb
@@ -7,9 +7,17 @@ require './lib/round'
 
 class RoundTest < Minitest::Test
 
- def test_it_exists
-   round = Round.new(@deck)
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
+  end
 
+ def test_it_exists
+   deck = Deck.new(@cards)
+   round = Round.new(deck)
+# require "pry"; binding.pry
    assert_instance_of Round, round
  end
 
