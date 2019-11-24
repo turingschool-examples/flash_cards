d002e737ff385bcb7664f18057c1704f98d4f2db
Kevin McGrevey <niv3kmcg@gmail.com>
2019-11-24 12:20:22 -0700

    Finds first card in deck

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,16 @@
 # NOTE: plug in actual values instead of variables to get to run, then go find that exact variable
 
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :cards
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    # @cards = cards
+  end
+
+  def current_card
     # require "pry"; binding.pry
+    deck.cards.first
   end
 end
