6ed3f7bc4473cfa4dd839ffcc310848e4333d357
Maria Ronauli <mronauli@gmail.com>
2019-11-24 12:11:34 -0700

    Fix question variable

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -9,7 +9,6 @@ card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and repo
 card_3 = Card.new("Describe in words the exact direction that is 697 .5° clockwise from due north?", "North north west", :STEM)
 card_4 = Card.new("What is JLO short for?", "Jennifer Lopez", :Pop_culture)
 
-
 cards = [card_1, card_2, card_3, card_4]
 
 @deck = Deck.new(cards)
@@ -22,16 +21,34 @@ def start
   p "-------------------------------------------------"
 
   i = 0
-  while i < @deck.cards.count - 1
+  while i < @deck.cards.count
 
 
 
-# p "This is card number #{deck.cards.first} out of #{cards.count}."
-# p "Question: #{cards.question}?"
+  p "This is card number #{i + 1} out of #{@deck.cards.count}."
+
+  p "Question: #{@round.current_card.question}?"
+
+  answer = gets.chomp()
+
+  turn = @round.take_turn(answer)
+
+  turn.feedback
+
+  #p "#{@turn.cards.feedback}"
+
 
   i += 1
-  p i
   end
+
+  
+
+  p "****** Game over! ******"
+  p "You had #{@round.number_correct} correct guesses out of 4 for a total score of #{@round.percent_correct}."
+  p "#{category} - #{@round.percent_correct_by_category(@category)} correct"
+  p "#{category} - #{@round.percent_correct_by_category(@category)} correct"
+  p "#{category} - #{@round.percent_correct_by_category(@category)} correct"
+
 
 end
 
