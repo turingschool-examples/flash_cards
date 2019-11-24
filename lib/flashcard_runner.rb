fb7c5c7288c03cdf897920af81a71147835158c0
Maria Ronauli <mronauli@gmail.com>
2019-11-23 23:21:38 -0700

    Add referenced files and what player sees at first

---
+++ lib/flashcard_runner.rb
@@ -0,0 +1,12 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+require 'pry'
+
+i = 0
+
+p "Welcome! You're playing with #{n} cards."
+"-------------------------------------------------"
+p "This is card number #{i} out of 4."
+p "Question: #{@card}?"
