b07a1637052ead0bb3b50ec4eb43c09d75732da4
Elom Amouzou <elom1990@yahoo.com>
2019-11-24 23:49:37 -0700

    lib/flashcard_runner.rb

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -3,13 +3,6 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-# card_1 = Card.new("Who is the man that would risk his neck for his brother man?", "Shaft! Can you dig it?", :Movies)
-# card_2 = Card.new("What does it mean when P-Square asks you to chop their money?", "They no fear, cause e no dey empty.", :Music)
-# card_3 = Card.new("Finish this phrase: Kirikou n'est pas grand, ...", "Mais il est vaillant.", :Movies)
-# card_4 = Card.new("What dance did Rihanna confuse American audiences with during her 2018 Grammy performance of Wild Thoughts?", "Gwara Gwara", :Dance)
-# cards = [card_1, card_2, card_3, card_4]
-# deck = Deck.new(cards)
-# round = Round.new(deck)
 
 def start
   card_1 = Card.new("Who is the man that would risk his neck for his brother man?", "Shaft! Can you dig it?", :Movies)
@@ -53,6 +46,12 @@ def start
   user_guess = gets.chomp
   round.take_turn(user_guess)
   round.turns.last.feedback
+
+  puts "****** Game over! ******"
+  puts "You had #{round.number_correct} correct guesses out of 4 for a total score of #{round.percent_correct}."
+  puts "Movies - #{round.percent_correct_by_category(:Movies)}% correct."
+  puts "Music - #{round.percent_correct_by_category(:Music)}% correct."
+  puts "Dance - #{round.percent_correct_by_category(:Dance)} correct."
 
 
 
