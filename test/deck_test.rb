515d83f753401f4244f54630c23acb80d00ccb9e
Tyler Porter <tylerpporter@gmail.com>
2020-01-20 13:30:10 -0700

    Create and pass test_it_counts_number_of_cards_in_deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -19,4 +19,16 @@ class DeckTest < Minitest::Test
     assert_instance_of Deck, deck
   end
 
+  def test_it_counts_number_of_cards_in_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+  end 
+
 end
