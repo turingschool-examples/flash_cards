789b410ae13f6011fa1ed367674f86c81a2ed0c7
Tyler Porter <tylerpporter@gmail.com>
2020-01-20 12:02:50 -0700

    Write and pass test_it_has_a_guess

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -6,10 +6,16 @@ require './lib/turn.rb'
 class TurnTest < Minitest::Test
 
   def test_it_exists
-    card_1 = Card.new("What is the capital?", "Juneau", :Geography)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Anchorage", card_1)
 
     assert_instance_of Turn, turn
   end
 
+  def test_it_has_a_guess
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Anchorage", card_1)
+
+    assert_equal "Anchorage", turn.guess
+  end
 end
