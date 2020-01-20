8b8d87319353839a9c733e69b92d2281bae5d2bc
Tyler Porter <tylerpporter@gmail.com>
2020-01-20 10:55:09 -0700

    Pass test_it_has_an_answer

--- test/card_test.rb
+++ test/card_test.rb
@@ -11,14 +11,14 @@ class CardTest < Minitest::Test
   end
 
   def test_it_has_a_question
-    
+
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "What is the capital of Alaska?", card.question
   end
 
   def test_it_has_an_answer
-    skip
+    
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_equal "Juneau", card.answer
