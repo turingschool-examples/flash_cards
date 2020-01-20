92a8e5d3458e66cd583923f2a3327fbab18242f1
Tyler Porter <tylerpporter@gmail.com>
2020-01-20 14:12:09 -0700

    Create and pass test_it_keeps_track_of_turns

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,12 @@
 require_relative 'deck.rb'
+require_relative 'turn.rb'
 
 class Round
-  attr_accessor :deck
+
+  attr_accessor :deck, :turns
   def initialize(deck)
     @deck = deck
+    @turns = []
   end
 
 end
