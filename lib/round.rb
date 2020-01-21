2ec55d5b543efee945705f6155831ea4fa076b87
Tyler Porter <tylerpporter@gmail.com>
2020-01-20 22:06:52 -0700

    Create and pass test_it_can_track_number_correct_by_category

--- lib/round.rb
+++ lib/round.rb
@@ -4,23 +4,35 @@ require_relative 'turn.rb'
 
 class Round
 
-  attr_accessor :deck, :turns, :current_card, :number_correct
+  attr_accessor :deck, :turns, :current_card
   def initialize(deck)
     @deck = deck
     @turns = []
     @current_card = @deck.cards[0]
-    @number_correct = 0
+    @turns_correct = []
+
   end
 
   def take_turn(guess)
     turn = Turn.new(guess, @current_card)
     @turns << turn
     if turn.guess == @current_card.answer
-      @number_correct += 1
+      @turns_correct << turn
     end
     @deck.cards.shift
     @current_card = @deck.cards[0]
+
     turn
+  end
+
+  def number_correct
+    @turns_correct.size
+  end
+
+  def number_correct_by_category(category)
+     return 0 if @turns_correct.none? {|turn| turn.card.category == category}
+     cat = @turns_correct.select {|turn| turn.card.category == category}
+     cat.size
   end
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -109,9 +109,26 @@ class RoundTest < Minitest::Test
       round = Round.new(deck)
 
       turn_1 = round.take_turn("Juneau")
-      # turn_2 = round.take_turn("Pluto")
 
       assert_equal card_2, round.current_card
+    end
+
+    def test_it_can_track_number_correct_by_category
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      cards = [card_1, card_2, card_3]
+
+      deck = Deck.new(cards)
+
+      round = Round.new(deck)
+
+      turn_1 = round.take_turn("Juneau")
+      turn_2 = round.take_turn("Pluto")
+
+      assert_equal 1, round.number_correct_by_category(:Geography)
+      assert_equal 0, round.number_correct_by_category(:STEM)
     end
 
 end
