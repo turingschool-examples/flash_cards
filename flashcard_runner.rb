02a72b0045389e6c89ded92f80b4497f59c9fcc4
Tyler Porter <tylerpporter@gmail.com>
2020-01-21 11:45:38 -0700

    Complete flashcard_runner.rb

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,10 +2,15 @@ require 'require_all'
 require_all 'lib'
 
 def play_game
+
   card_1 = Card.new("What is the largest mammal in North America?", "Bison", :Nature)
   card_2 = Card.new("What is the largest freshwater fish in North America?", "Sturgeon", :Nature)
   card_3 = Card.new("Which muscle group is antagonistic to the quadriceps?", "Hamstrings", :Anatomy)
   card_4 = Card.new("How many bones are in the human body?", "206", :Anatomy)
+
+  total_cards = 4
+
+  categories = [:Nature, :Anatomy]
 
   cards = [card_1, card_2, card_3, card_4]
 
@@ -13,18 +18,30 @@ def play_game
 
   round = Round.new(deck)
 
-  puts "Welcome! You're playing with 4 cards."
+  puts "Welcome! You're playing with #{total_cards} cards."
   puts '------------------------------------------------'
+
   while cards.count >= 1
-    puts "This is card number #{((cards.length - 4) - 1).abs} out of 4."
+    puts "This is card number #{((cards.length - 4) - 1).abs} out of #{total_cards}."
     puts "Question: #{cards[0].question}"
     guess = gets.chomp()
-    round.take_turn(guess.downcase.capitalize)
+    round.take_turn(guess.downcase.gsub(/[^a-z0-9]/, '').capitalize)
     num = (round.turns.count - 1)
     puts round.turns.fetch(num).feedback
-    require 'pry'; binding.pry
   end
-    puts "****** Game over! ******"
+
+  puts "****** Game over! ******"
+
+  guess_plural = "guesses"
+  if round.number_correct == 1
+    guess_plural = "guess"
+  end
+
+  puts "You had #{round.number_correct} correct #{guess_plural} out of #{total_cards} for a total score of #{round.percent_correct.to_i}%."
+
+  categories.each do |cat|
+    puts "#{cat} - #{round.percent_correct_by_category(cat).to_i}% correct"
+  end
 
 end
 
