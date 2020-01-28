0da1f6188649551a1a6ecdd8765000e06e1a4daf
Mariana Cid <cidmariana5@gmail.com>
2020-01-28 11:33:57 -0700

    Create turn file for turn test.

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,5 +4,15 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class TurnTest < Minitest::Test
-  
+
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
+  end
+
+  def test_it_has_card 
+
+  end
 end
