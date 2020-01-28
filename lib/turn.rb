c358e8a33cdf0d44b9ecc9fe901c5c3bc1b2168f
Steven Meyers <sjjmeyers@gmail.com>
2020-01-27 21:49:08 -0700

    Edit card.rb, add turn.rb and turn_test.rb

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+class Turn
+
+  attr_accessor :guess
+                :card
+
+  def initialize(guess, card)
+    @guess    = guess
+    @card     = card
+    
+  end
+
+  def guess
+  end
+
+  def card
+  end
+
+  def correct?
+  end
+
+  def feedback
+  end
