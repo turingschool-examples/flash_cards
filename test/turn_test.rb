30f0d32ef1d0cc852a6944e1371f1c0bd52cc531
Maxwell Baird <bairdm71@gmail.com>
2020-01-28 12:48:12 -0700

    Added methods to turn_test.rb to test turn.rb methods

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -9,4 +9,40 @@ class Turn_test < Minitest::Test
     turn = Turn.new("Denver", card)
     assert_instance_of Turn, turn
   end
+
+  def test_if_turn_has_a_guess
+    card = Card.new("What is the capital of Colorado","Denver",:Geography)
+    turn = Turn.new("Denver", card)
+    assert_equal "Denver", turn.guess
+  end
+
+  def test_if_turn_has_a_card
+    card = Card.new("What is the capital of Colorado","Denver",:Geography)
+    turn = Turn.new("Denver", card)
+    assert_equal card, turn.card
+  end
+
+  def test_if_turn_correct_returns_if_correct
+    card = Card.new("What is the capital of Colorado","Denver",:Geography)
+    turn = Turn.new("Denver", card)
+    assert_equal true, turn.correct?
+  end
+
+  def test_if_turn_correct_returns_if_incorrect
+    card = Card.new("What is the capital of Colorado","Denver",:Geography)
+    turn = Turn.new("Broomfield", card)
+    assert_equal false, turn.correct?
+  end
+
+  def test_if_turn_feedback_if_correct
+    card = Card.new("What is the capital of Colorado","Denver",:Geography)
+    turn = Turn.new("Denver", card)
+    assert_equal "Correct!", turn.feedback
+  end
+
+  def test_if_turn_feedback_if_incorrect
+    card = Card.new("What is the capital of Colorado","Denver",:Geography)
+    turn = Turn.new("CHEESE!!!!", card)
+    assert_equal "Incorrect.", turn.feedback
+  end
 end
