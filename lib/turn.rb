d10e6df68122049caff5f18bb648414ac03a068f
Mariana Cid <cidmariana5@gmail.com>
2020-01-28 11:44:48 -0700

    Test created to if guess is correct

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,14 @@ class Turn
   attr_reader :guess, :card
   def initialize(guess, card)
     @guess = guess
-    @card = card 
+    @card = card
+  end
+
+  def correct?
+    if @guess == card.answer
+      true
+    else
+      false
+    end 
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -12,7 +12,24 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
-  def test_it_has_card 
+  def test_it_has_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
+    assert_equal card, turn.card
+  end
+
+  def test_it_can_check_answer
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal true, turn.correct?
+  end
+
+  def test_it_can_give_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Correct!", turn.feedback
   end
 end
