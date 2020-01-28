bc6232053d5b63707cacadba38c97e675e0813a5
Mariana Cid <cidmariana5@gmail.com>
2020-01-28 13:21:05 -0700

    Create test to make an array of cards with certain category.

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,15 @@ class Deck
   def count
     @cards.length
   end
+
+  def cards_in_category(category)
+    cards_array = []
+
+    @cards.each do |card|
+      if card.category == category
+        cards_array << card 
+      end
+    end
+    cards_array
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -34,4 +34,16 @@ class DeckTest < Minitest::Test
 
     assert_equal 3, deck.count
   end
+
+  def test_it_can_check_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_1], deck.cards_in_category(:Geography)
+    assert_equal [], deck.cards_in_category("Pop Culture")
+  end
 end
