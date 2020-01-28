a9ff0cd7259dac866c02c3b95d7cba87ca2b5663
Maxwell Baird <bairdm71@gmail.com>
2020-01-28 13:15:18 -0700

    added cards, count, and cards_in_category methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,25 @@
 class Deck
 
   def initialize(array_of_cards_for_the_deck)
-    @array_of_cards = aarray_of_cards_for_the_deck
+    @array_of_cards = array_of_cards_for_the_deck
   end
-  
+
+  def cards
+    return @array_of_cards
+  end
+
+  def count
+    number_of_cards = @array_of_cards.size
+    return number_of_cards
+  end
+
+  def cards_in_category(category)
+    array_of_cards_in_category = []
+    @array_of_cards.each do |card|
+      if card.category == category
+        array_of_cards_in_category << card
+      end
+    end
+    return array_of_cards_in_category
+  end
 end
