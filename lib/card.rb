b95e5022304dc6918532cd5a2f35bdd728892f90
Maxwell Baird <bairdm71@gmail.com>
2020-01-28 08:48:22 -0700

    Initial commit

---
+++ lib/card.rb
@@ -0,0 +1,19 @@
+class Card
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+
+  def question
+    return @question
+  end
+
+  def answer
+    return @answer
+  end
+
+  def category
+    return @category
+  end
+end
