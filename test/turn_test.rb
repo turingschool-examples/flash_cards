89b86de9bf31ed130986f3ee91e81bfe5ac3e3d9
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 19:43:41 -0700

    Pass test_return_card

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -12,4 +12,12 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
+  def test_return_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    turn.card
+
+    assert_instance_of Card, card
+  end
+
 end
