bc29919c01cdb1f0179c3d42c0caae49d9f32a2f
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 19:40:51 -0700

    Pass test_it exists test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -6,7 +6,10 @@ require './lib/turn'
 class TurnTest < Minitest::Test
 
   def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
+    assert_instance_of Turn, turn
   end
 
 end
