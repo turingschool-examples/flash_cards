b74252bee5e06241fc0b1174f12f3c2075887058
Alex Latham <alexlthm@gmail.com>
2020-01-28 21:32:33 -0700

    Update take_turns method to store and return new turn

--- lib/round.rb
+++ lib/round.rb
@@ -1,15 +1,18 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
-  end
-
-  def turns
-    []
+    @turns = []
   end
 
   def current_card
     @deck.cards[0]
+  end
+
+  def take_turn(guess)
+    @turn = Turn.new(guess, current_card)
+    @turns << @turn
+    @turn
   end
 end
