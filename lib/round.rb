fa3235dc97bf839fb70b1c3afd598c2636d682aa
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-28 20:46:17 -0700

    Update round class with number_correct_by_category method

--- lib/round.rb
+++ lib/round.rb
@@ -19,6 +19,10 @@ class Round
   end
 
   def number_correct
-    turns.count {|turn| turn.correct?}
+    @turns.count {|turn| turn.correct?}
+  end
+
+  def number_correct_by_category(category)
+    @turns.count {|turn| turn.correct? && turn.card.category == category}
   end
 end
