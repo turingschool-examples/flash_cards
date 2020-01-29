7b1f0ad78602479e215f07230c150f7566cd5e04
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 21:45:36 -0700

    Pass test_last_turn_feedback

--- test/round_test.rb
+++ test/round_test.rb
@@ -122,7 +122,15 @@ class RoundTest < Minitest::Test
   end
 
   def test_last_turn_feedback
-    skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal "Incorrect.", round.turns.last.feedback 
   end
 
   def test_number_correct_by_category
