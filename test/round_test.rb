7cc8cd4dc93392b1912c05cb8ce66c764c7d83f4
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 21:22:01 -0700

    Pass test_class_of_turn

--- test/round_test.rb
+++ test/round_test.rb
@@ -37,4 +37,28 @@ class RoundTest < Minitest::Test
 
     assert_equal [], round.turns
   end
+
+  def test_take_turn
+    skip
+    # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    # card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    # card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    # deck = Deck.new([card_1, card_2, card_3])
+    # round = Round.new(deck)
+    # turn = Turn.new("Juneau", card_1)
+    # new_turn = round.take_turn("Juneau")
+    #
+    # diff turn, new_turn
+  end
+
+  def test_class_of_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_instance_of Turn, new_turn
+  end
 end
