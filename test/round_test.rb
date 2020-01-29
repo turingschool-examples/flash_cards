790d46760537d8ff0b398b88860aa40c89377889
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 21:41:00 -0700

    Pass test_number_of_turns

--- test/round_test.rb
+++ test/round_test.rb
@@ -93,7 +93,7 @@ class RoundTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
-    new_turn = round.take_turn("Juneau")
+    round.take_turn("Juneau")
 
     assert_equal 1, round.number_correct
   end
@@ -104,8 +104,32 @@ class RoundTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
-    new_turn = round.take_turn("Juneau")
+    round.take_turn("Juneau")
 
     assert_equal card_2, round.current_card
+  end
+
+  def test_number_of_turns
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal 2, round.turns.count
+  end
+
+  def test_number_correct_by_category
+    skip
+  end
+
+  def test_percent_correct
+    skip
+  end
+
+  def test_percent_correct_by_category
+    skip
   end
 end
