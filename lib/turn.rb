b5aef2903e1cb6af1cad76497c9bba37bc174f02
Josh Tukman <josh.tukman@gmail.com>
2020-01-29 15:41:15 -0700

    Add test for incorrect answer to guess

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+class Turn
+  attr_reader :card, :guess
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+  def correct?
+    if @guess == card.answer
+      true
+    else
+      false
+    end
+  end
+  def feedback
+    if correct? == true
+      "Correct!"
+    else
+      "You are wrong"
+    end
+  end
+
+
+end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -17,17 +17,57 @@ class CardTest < Minitest::Test
   def test_turn_has_a_card
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
+
     assert_equal "What is the capital of Alaska?", turn.card.question
     assert_equal "Juneau", turn.card.answer
     assert_equal :Geography, turn.card.category
   end
-end   
 
-# pry(main)> turn.guess
-# #=> "Juneau"
-#
-# pry(main)> turn.correct?
+
+  def test_turn_has_an_answer
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal "Juneau", turn.guess
+  end
+  def test_turn_is_correct
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal true, turn.correct?
+  end
+  def test_turn_feedback_returns_correct_when_correct
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal "Correct!", turn.feedback
+  end
+
+  def test_feedback_returns_incorrect_when_incorrect
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    assert_equal false, turn.correct?
+  end
+
+end
+# pry(main)> require './lib/turn'
 # #=> true
 #
+# pry(main)> require './lib/card'
+# #=> true
+#
+# pry(main)> card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+# #=> #<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>
+#
+# pry(main)> turn = Turn.new("Saturn", card)
+# #=> #<Turn:0x00007f998413ee60 @card=#<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>, @guess="Saturn">
+#
+# pry(main)> turn.card
+# => #<Card:0x007ffdf1820a90 @answer="Mercury", @question="Which planet is closest to the sun?", @category=:STEM>
+#
+# pry(main)> turn.guess
+# => "Saturn"
+#
+# pry(main)> turn.correct?
+# => false
+#
 # pry(main)> turn.feedback
-# #=> "Correct!"
+# => "Incorrect."
+#
