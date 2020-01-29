83450c907e58eadc2f1a52128feb28698268c9c0
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-28 19:50:54 -0700

    Add test for first card drawn when round beings

--- test/round_test.rb
+++ test/round_test.rb
@@ -29,4 +29,15 @@ class RoundTest < Minitest::Test
 
     assert_equal 0, round.turns.count
   end
+
+  def test_when_round_starts_top_card_is_drawn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography),
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM),
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_equal round.current_card, card_1
+
+  end
 end
