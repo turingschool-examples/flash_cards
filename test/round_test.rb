945484b80ac07a35c5852b8c913c8a9d9a7fc27e
Alex Latham <alexlthm@gmail.com>
2020-01-28 22:08:35 -0700

    Add incorrect guess feedback test on second round

--- test/round_test.rb
+++ test/round_test.rb
@@ -108,12 +108,19 @@ class RoundTest < Minitest::Test
 
     assert_equal 2, round.turns.count
   end
-  # pry(main)> round.take_turn("Venus")
-  # #=> #<Turn:0x00007f972a215b38...>
-  #
-  # pry(main)> round.turns.count
-  # #=> 2
-  #
+
+  def test_returns_incorrect_on_incorrect_guess
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal "Incorrect.", round.turns.last.feedback
+  end
   # pry(main)> round.turns.last.feedback
   # #=> "Incorrect."
   #
