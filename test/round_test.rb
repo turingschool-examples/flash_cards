1eb8e63019f6a101e9225bbd4b85149948d55e57
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 21:47:09 -0700

    Pass test_total_correct_guesses

--- test/round_test.rb
+++ test/round_test.rb
@@ -130,7 +130,7 @@ class RoundTest < Minitest::Test
     round.take_turn("Juneau")
     round.take_turn("Venus")
 
-    assert_equal "Incorrect.", round.turns.last.feedback 
+    assert_equal "Incorrect.", round.turns.last.feedback
   end
 
   def test_number_correct_by_category
@@ -144,6 +144,18 @@ class RoundTest < Minitest::Test
 
     assert_equal 1, round.number_correct_by_category(:Geography)
     assert_equal 0, round.number_correct_by_category(:STEM)
+  end
+
+  def test_total_correct_guesses
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal 1, round.number_correct
   end
 
   def test_percent_correct
