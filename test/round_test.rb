11cf30df58669f02dc568450e1c8ddcc270b2300
Alex Latham <alexlthm@gmail.com>
2020-01-28 21:04:31 -0700

    Add correct? method test for guessing through take_turn

--- test/round_test.rb
+++ test/round_test.rb
@@ -48,12 +48,18 @@ class RoundTest < Minitest::Test
 
     assert_instance_of Turn, new_turn
   end
-  # pry(main)> new_turn = round.take_turn("Juneau")
-  # #=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
-  #
-  # pry(main)> new_turn.class
-  # #=> Turn
-  #
+
+  def test_correct_method_returns_true_for_correct_gues
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert new_turn.correct?
+  end
   # pry(main)> new_turn.correct?
   # #=> true
   #
