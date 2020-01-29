6f34d8e53c7d04c7625ecdf14a80b1f94e935761
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-28 19:28:24 -0700

    Create deck test for deck class

---
+++ test/deck_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+
+  def test_cards_can_be_added_to_the_deck
+    deck = Deck.new
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    assert_equal 0, deck.cards.count
+    deck.add_card(card_1)
+    deck.add_card(card_2)
+    deck.add_card(card_3)
+    assert_equal 3, deck.cards.count
+  end
+
+  def test_cards_in_categories
+    deck = Deck.new
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck.add_card(card_1)
+    deck.add_card(card_2)
+    deck.add_card(card_3)
+
+    cards_in_same_category = deck.show_cards_of_category(:STEM)
+  end
+
+end
