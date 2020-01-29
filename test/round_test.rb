48f0292950a19affca7986aa1f9c30c420da92af
Alex Latham <alexlthm@gmail.com>
2020-01-28 22:46:19 -0700

    Add test to check if third card is first after two rounds

--- test/round_test.rb
+++ test/round_test.rb
@@ -85,7 +85,7 @@ class RoundTest < Minitest::Test
     assert_equal 1, round.number_correct
   end
 
-  def test_second_card_is_first_after_new_turn
+  def test_second_card_is_first_after_first_turn
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -175,6 +175,18 @@ class RoundTest < Minitest::Test
     assert_equal 100.0, round.percent_correct_by_category(:Geography)
   end
 
+  def test_third_card_is_first_after_second_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal card_3, round.current_card
+  end
   # pry(main)> round.current_card
   # #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
 end
