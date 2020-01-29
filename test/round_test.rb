63473f713380f796f60089956712472613d75535
Alex Latham <alexlthm@gmail.com>
2020-01-28 22:37:44 -0700

    Add test to calculate percent correct by category

--- test/round_test.rb
+++ test/round_test.rb
@@ -161,12 +161,20 @@ class RoundTest < Minitest::Test
 
     assert_equal 50.0, round.percent_correct
   end
-  # pry(main)> round.percent_correct
-  # #=> 50.0
-  #
-  # pry(main)> round.percent_correct_by_category(:Geography)
-  # #=> 100.0
-  #
+
+  def test_calculate_percent_correct_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal 100.0, round.percent_correct_by_category(:Geography)
+  end
+
   # pry(main)> round.current_card
   # #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
 end
