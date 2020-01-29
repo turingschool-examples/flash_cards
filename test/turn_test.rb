e7dc125e45722de5311a2d3562ede3ea13b6de98
“Krista <stadler.krista@gmail..com>
2020-01-29 16:24:10 -0700

    correct answer method test created

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -30,6 +30,12 @@ class TurnTest < Minitest::Test
     turn = Turn.new("Juneau", card)
     assert_equal "Juneau", turn.guess
   end
+
+  def test_correct_answer_method
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal card.answer, turn.guess
+  end
 end
 
 
