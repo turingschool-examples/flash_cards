97d21437b59a060126a20c69e07120edea5098d0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-01-29 09:10:31 -0700

    Add cards in category test to deck_test.rb

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -8,7 +8,8 @@ class DeckTest < Minitest::Test
     @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @card_2 = Card.new("TheThe Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @cards = [@card_1, @card_2, @card3]
+    @cards = [@card_1, @card_2, @card_3]
+    @cards_in_category = []
     @deck = Deck.new(@cards)
   end
 
@@ -16,14 +17,31 @@ class DeckTest < Minitest::Test
     assert_instance_of  Deck, @deck
   end
 
+  def test_cards_exists
+    assert_equal @cards, @deck.cards
+  end
 
 
   def test_deck_count_cards
-  
     assert_equal 3, @deck.count_cards
   end
 
+  def test_deck_cards_in_category_gegraphy
+    @cards.each do |card|
+      if card.category == :Geography
+        @cards_in_category << card
+      end
+      return @cards_in_category
+      require 'pry'; binding.pry
+    end
+
+    assert_equal @cards_in_category, @deck.cards_in_category(:Geography)
+  end
 end
+
+
+
+
 
 
 # pry(main)> cards = [card_1, card_2, card_3]
