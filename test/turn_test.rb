417a06bc7a9d8b06b1b7a1bca6c2108434ef8a8a
Jesse Gietzen <gietzenjesse@gmail.com>
2020-01-29 15:30:43 -0700

    add negative assertion to test 4

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -21,17 +21,15 @@ class TurnTest < Minitest::Test
   def test_it_has_a_correct?
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-    assert turn.correct?
+    assert_equal true, turn.correct?
   end
 
   def test_it_gives_feedback
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    assert_equal turn.feedback, "Correct!" || "Incorrect."
+    turn = Turn.new("Juneau", card) # i can use turn1 and turn2 if you prefer
+    assert_equal turn.feedback, "Correct!"
+    turn = Turn.new("Anchorage", card) # that to reassigning the variable
+    assert_equal turn.feedback, "Incorrect."
   end
 
 end
-
-=begin
-feedback method?
-=end
