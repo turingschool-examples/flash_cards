15eabaec606c15063a59cf71e313ebf4a2b1127e
Jesse Gietzen <gietzenjesse@gmail.com>
2020-01-28 18:58:18 -0700

    add test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -11,4 +11,18 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
+  def test_it_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal "Juneau", turn.guess
+  end
+
+
 end
+
+=begin
+add string test
+add card
+correct? method?
+feedback method?
+=end
