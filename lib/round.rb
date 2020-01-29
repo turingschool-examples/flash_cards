bb66a40a08151d7d905ac7432387f1c190ee49c6
Maxwell Baird <bairdm71@gmail.com>
2020-01-29 15:46:24 -0700

    created a method that creates a new Turn object

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,17 @@
+require './lib/turn'
 class Round
   attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
     @turns = []
+  end
+
+  def current_card
+    @deck.cards[@turns.size]
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    return turn
   end
 end
