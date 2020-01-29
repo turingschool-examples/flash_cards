cbea9e3bc96d00f974c9b13aa057acaed3372597
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-29 12:59:41 -0700

    Add tests for initial turns and current card

--- test/round_test.rb
+++ test/round_test.rb
@@ -12,7 +12,32 @@ class RoundTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+    round = Round.new(deck)
 
     assert_instance_of Deck, deck
   end
+
+  def test_when_round_starts_no_turns_have_been_taken
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal [], round.turns
+  end
+
+  def test_first_card_of_the_round_is_first_card_of_the_deck_array
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    assert_equal 0, round.turns.count 
+    assert_equal card_1, round.current_card
+  end
+
 end
