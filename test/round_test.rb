440594f9e1676aaa5b2e86a435b23da0d5db75b6
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 20:59:01 -0700

    Pass test_no_turn_taken

--- test/round_test.rb
+++ test/round_test.rb
@@ -17,7 +17,7 @@ class RoundTest < Minitest::Test
 
     assert_instance_of Round, round
   end
-  
+
   def test_has_deck
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -26,5 +26,15 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
 
     assert_equal deck, round.deck
+  end
+
+  def test_no_turn_taken
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_equal [], round.turns
   end
 end
