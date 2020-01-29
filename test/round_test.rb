6466c172ef12a0a339477286d9d89fef08fd25ba
Alex Latham <alexlthm@gmail.com>
2020-01-28 22:14:31 -0700

    Add test to count number correct by category

--- test/round_test.rb
+++ test/round_test.rb
@@ -134,15 +134,21 @@ class RoundTest < Minitest::Test
 
     assert_equal 1, round.number_correct
   end
-  # pry(main)> round.number_correct
-  # #=> 1
-  #
-  # pry(main)> round.number_correct_by_category(:Geography)
-  # #=> 1
-  #
-  # pry(main)> round.number_correct_by_category(:STEM)
-  # #=> 0
-  #
+
+  def test_counts_number_correct_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal 1, round.number_correct_by_category(:Geography)
+    assert_equal 0, round.number_correct_by_category(:STEM)
+  end
+  
   # pry(main)> round.percent_correct
   # #=> 50.0
   #
