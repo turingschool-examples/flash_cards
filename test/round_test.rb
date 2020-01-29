0cb8dd03df6ebc3849278bf698e4f7b20b2f423e
Alex Latham <alexlthm@gmail.com>
2020-01-28 22:10:27 -0700

    Add test to count number of correct guesses after two turns

--- test/round_test.rb
+++ test/round_test.rb
@@ -121,9 +121,19 @@ class RoundTest < Minitest::Test
 
     assert_equal "Incorrect.", round.turns.last.feedback
   end
-  # pry(main)> round.turns.last.feedback
-  # #=> "Incorrect."
-  #
+
+  def test_counts_number_correct_after_second_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal 1, round.number_correct
+  end
   # pry(main)> round.number_correct
   # #=> 1
   #
