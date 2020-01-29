7414174d244484eab8c47f9e29d3626d08215264
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 20:03:42 -0700

    Split test_correct and test_get_feedback methods

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -31,12 +31,26 @@ class TurnTest < Minitest::Test
     turn = Turn.new("Juneau", card)
 
     assert turn.correct?
+  end
 
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Springfield", card)
+  def test_is_incorrect
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
 
     refute turn.correct?
   end
 
+  def test_get_feedback_for_correct_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
+    assert_equal "Correct!", turn.feedback
+  end
+
+  def test_get_feedback_for_incorrect_guess
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    assert_equal "Incorrect.", turn.feedback
+  end
 end
