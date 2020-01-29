c9d8edbc6fe0943e06ad3bf954707766fa122f71
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 21:34:31 -0700

    Pass test_show_number_of_correct_guesses

--- test/round_test.rb
+++ test/round_test.rb
@@ -61,4 +61,29 @@ class RoundTest < Minitest::Test
 
     assert_instance_of Turn, new_turn
   end
+
+  def test_get_turns
+    skip
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    turn = []
+    turn << Turn.new("Juneau", card_1)
+
+    assert_equal turn, round.turns
+  end
+
+  def test_show_number_of_correct_guesses
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+
+    assert_equal 1, round.number_correct
+  end
 end
