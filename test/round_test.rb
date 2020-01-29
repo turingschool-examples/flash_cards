bcb7db0ed6ffb3d14766dfdfd151288b1a0af99c
Alex Latham <alexlthm@gmail.com>
2020-01-28 21:34:36 -0700

    Add number_correct method test for 1 correct guess

--- test/round_test.rb
+++ test/round_test.rb
@@ -73,6 +73,17 @@ class RoundTest < Minitest::Test
     assert_equal [new_turn], round.turns
   end
 
+  def test_number_correct_method_returns_1_after_1_correct_guess
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal 1, round.number_correct
+  end
   # pry(main)> round.number_correct
   # #=> 1
   #
