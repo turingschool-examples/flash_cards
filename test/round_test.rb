a566f4a5b26e1222dbaca2b3914fa2b2ffff0e6b
Javier Aguilar <jvr.agx@gmail.com>
2020-01-28 22:23:36 -0700

    Pass test_show_current_card_again_once_more

--- test/round_test.rb
+++ test/round_test.rb
@@ -177,4 +177,17 @@ class RoundTest < Minitest::Test
 
     assert_equal 100.0, round.percent_correct_by_category(:Geography)
   end
+
+  # Reminder to combine all test_show_current_card methods
+  def test_show_current_card_again_once_more
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal card_3, round.current_card
+  end
 end
