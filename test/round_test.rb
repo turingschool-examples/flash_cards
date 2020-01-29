89a1a471d34b0cd25c9be1538d7c9a874ce03c04
Alex Latham <alexlthm@gmail.com>
2020-01-28 21:31:54 -0700

    Add first turns method test

--- test/round_test.rb
+++ test/round_test.rb
@@ -61,9 +61,18 @@ class RoundTest < Minitest::Test
     assert new_turn.correct?
   end
 
-  # pry(main)> round.turns
-  # #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
-  #
+  def test_turns_method_returns_array_of_one_turn_after_one_take_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal [new_turn], round.turns
+  end
+
   # pry(main)> round.number_correct
   # #=> 1
   #
