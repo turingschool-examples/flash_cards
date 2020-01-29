ec02a9b3bb277d304fed36aa2cc263b9c20d92f1
Alex Latham <alexlthm@gmail.com>
2020-01-28 21:43:56 -0700

    Add test for making second card first after a turn is taken

--- test/round_test.rb
+++ test/round_test.rb
@@ -84,9 +84,18 @@ class RoundTest < Minitest::Test
 
     assert_equal 1, round.number_correct
   end
-  # pry(main)> round.number_correct
-  # #=> 1
-  #
+
+  def test_second_card_is_first_after_new_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal card_2, round.current_card
+  end
   # pry(main)> round.current_card
   # #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
   #
