3deda5b55bde12d156d89ce560adde5cdf2dae6e
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-30 10:23:10 -0700

    Create tests for percentage methods

--- test/round_test.rb
+++ test/round_test.rb
@@ -111,6 +111,33 @@ class RoundTest < Minitest::Test
     assert_equal 0, round.number_correct_by_category(:STEM)
   end
 
+  def test_can_also_show_correct_percentages
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
 
+    assert_equal 50.0, round.percent_correct
+    assert_equal 100.0, round.percent_correct_by_category(:Geography)
+  end
+
+  def test_the_game_continues
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    round.current_card
+
+    assert_equal deck.cards[2], round.current_card
+  end
 
 end
