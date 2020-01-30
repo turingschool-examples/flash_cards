795cf55cd80fda9c949e5d5e8c6b124460d5fae1
“Krista <stadler.krista@gmail..com>
2020-01-29 20:53:31 -0700

    card exists test created and passed

---
+++ lib/deck.rb
@@ -0,0 +1,9 @@
+class Deck
+
+  def initialize
+    @cards = []
+  end
+
+  def add_new_card(card_name)
+    @cards << card_name
+  end
--- lib/runner.rb
+++
@@ -1,8 +0,0 @@
-require './lib/turn'
-require './lib/card'
-
-turn1 = Turn.new("Juneau")
-
-turn1.read_card("What is the capital of Alaska?")
-
-require "pry"; binding.pry
