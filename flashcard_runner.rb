c61256e68372e50f0cf72f5c6bec734f1fb3aad6
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-29 17:07:38 -0700

    Update round class with start functionality

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,43 +3,10 @@ require "./lib/deck"
 require "./lib/turn"
 require "./lib/round"
 
-
-
-
-def create_cards
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   card_2 = Card.new("The Viking spacecraft was sent to which planet?", "Mars", :STEM)
   card_3 = Card.new("What is the airspeed velocity of an unladen swallow?", "African or European?", :STEM)
   cards = [card_1, card_2, card_3]
-end
 
-def create_deck
-  deck = Deck.new(create_cards)
-end
-
-def start
-  round = Round.new(create_deck)
-
-  puts "Welcome! You're playing with #{round.total_cards} cards."
-  puts "------------------------------------------------------------"
-  while round.turns_remaining > 0 do
-    puts "This is card number #{round.turns_remaining} out of #{round.total_cards}"
-    puts "Question: #{round.current_card.question}"
-    round.take_turn(gets.chomp)
-    puts round.turns.last.feedback
-  end
-
-  puts "****** Game over! ******"
-  puts "You had #{round.number_correct} correct guesses out of #{round.total_cards} for a total score of #{round.percent_correct.round}%."
-  list_of_categories = []
-  round.deck.cards.each do |card|
-    list_of_categories << card.category
-    end
-    list_of_categories.uniq.each do |category|
-      puts "#{category} - #{round.percent_correct_by_category(category)}% correct"
-    end
-end
-
-
-
-start
+  round = Round.new(Deck.new(cards))
+  round.start
--- lib/round.rb
+++ lib/round.rb
@@ -40,4 +40,25 @@ class Round
     @deck.count
   end
 
+  def start
+    puts "Welcome! You're playing with #{self.total_cards} cards."
+    puts "------------------------------------------------------------"
+    while self.turns_remaining > 0 do
+      puts "This is card number #{self.turns_remaining} out of #{self.total_cards}"
+      puts "Question: #{self.current_card.question}"
+      self.take_turn(gets.chomp)
+      puts self.turns.last.feedback
+    end
+
+    puts "****** Game over! ******"
+    puts "You had #{self.number_correct} correct guesses out of #{self.total_cards} for a total score of #{self.percent_correct}%."
+    list_of_categories = []
+    self.deck.cards.each do |card|
+      list_of_categories << card.category
+      end
+      list_of_categories.uniq.each do |category|
+        puts "#{category} - #{self.percent_correct_by_category(category)}% correct"
+      end
+  end
+
 end
