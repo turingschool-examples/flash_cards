dbbdabf6f7496f6bcfb38c68904dcc3985993da3
Maxwell Baird <bairdm71@gmail.com>
2020-01-30 11:07:29 -0700

    added the tests for round.number_correct_by_category

--- test/round_test.rb
+++ test/round_test.rb
@@ -66,11 +66,23 @@ class RoundTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
-    new_turn = round.take_turn("Juneau")
+    round.take_turn("Juneau")
     round2 = Round.new(deck)
     round2.take_turn("Juneau")
     round2.take_turn("Venus")
     assert_equal 1, round.number_correct
     assert_equal 1, round2.number_correct
+  end
+
+  def test_round_number_correct_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+    assert_equal 1, round.number_correct_by_category(:Geography)
+    assert_equal 0, round.number_correct_by_category(:STEM)
   end
 end
