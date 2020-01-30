8bbf5dcb6e9aa5edd85a9aa555847706b44c9a6b
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-30 16:29:02 -0700

    Update test_count_of_certain_category with 2 additional categories

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,16 +5,7 @@ require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
-  def test_card_exists
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    assert_instance_of Card, card_1
-    assert_instance_of Card, card_2
-    assert_instance_of Card, card_3
-  end
-
-  def test_deck_exists
+  def test_it_exists
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -30,7 +21,7 @@ class DeckTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
-    assert_equal [card_1, card_2, card_3], deck.cards
+    assert_equal cards, deck.cards
   end
 
   def test_count_of_cards_in_deck
@@ -51,6 +42,7 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
 
     assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_1], deck.cards_in_category(:Geography)
+    assert_equal [], deck.cards_in_category("Pop Culture")
   end
-
 end
