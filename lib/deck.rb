fa2257964bc4be1c38a10e0ff9f58b53fdb83f8d
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-29 20:04:59 -0700

    Add test_count_of_cards_in_deck to test file and refactored cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,18 @@
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :category_array
   def initialize(cards)
     @cards = cards
+    @category_array = []
   end
   def count
     @cards.size
   end
   def cards_in_category(category)
-    cards.map {|card| return card if card.category == category}
+    cards.each do |card|
+      if card.category == category
+        @category_array << card
+      end
+    end
+    return @category_array
   end
 end
