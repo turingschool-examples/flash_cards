cb42e0812b72f81c2dbe7395e9b7c34d921980f7
Maxwell Baird <bairdm71@gmail.com>
2020-01-30 12:29:34 -0700

    added the test to for percent_correct for class Round

--- test/round_test.rb
+++ test/round_test.rb
@@ -85,4 +85,15 @@ class RoundTest < Minitest::Test
     assert_equal 1, round.number_correct_by_category(:Geography)
     assert_equal 0, round.number_correct_by_category(:STEM)
   end
+
+  def test_round_percent_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+    assert_equal 50.0, round.percent_correct
+  end
 end
