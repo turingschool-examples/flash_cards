22bed5c2ff41e6e1e97342a26e5efdf430b59bc6
Colin Alexander <colintalex@gmail.com>
2020-01-29 23:21:27 -0700

    Adds cards method, count method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,13 @@
 class Deck
   def initialize(cards)
-    @cards = cards
+    @cards_in_deck = cards
+  end
+
+  def cards
+    @cards_in_deck
+  end
+
+  def count
+    @cards_in_deck.count
   end
 end
