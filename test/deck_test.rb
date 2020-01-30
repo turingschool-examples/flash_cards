bf3b72386e6d469c4bf98399bf0a32dbc7a42ded
“Krista <stadler.krista@gmail..com>
2020-01-29 21:05:08 -0700

    deck starts empty test created

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,21 +9,21 @@ class TurnTest < Minitest::Test
   def test_card_exists
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    require "pry"; binding.pry
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     assert_instance_of Card, card_1
-    require "pry"; binding.pry
     assert_instance_of Card, card_2
+    assert_instance_of Card, card_3
   end
 
+  def test_deck_starts_empty
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck_1 = Deck.new
+    assert_instance_of [], deck_1
+  end
 end
-#  pry(main)> card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
 
-#  pry(main)> card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
-
-#  pry(main)> card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
 
 #  pry(main)> cards = [card_1, card_2, card_3]
 
