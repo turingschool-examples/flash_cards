90665b99187bde459cf82ce3dd106c496891ebdc
Anthony Gomes <code@tonygom.es>
2020-01-29 20:50:39 -0700

    Add completed Runner File & working program with 4 cards

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -5,37 +5,43 @@ require './lib/round'
 
 
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_1 = Card.new("What country does 45 take orders from?", "Russia", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
+  card_2 = Card.new("What country does 45 take orders from?", "Russia", :Geography)
+  card_3 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_4 = Card.new("What is the largest planet in the solar system", "Jupiter", :STEM)
+  cards = [card_1, card_2, card_3, card_4]
 
   deck = Deck.new(cards)
   round = Round.new(deck)
 
-p "Welcome! You're playing with #{cards.length} cards."
-p "-------------------------------------------------"
+  puts "\n\nWelcome! You're playing with #{cards.length} cards.\n"
+  puts "-------------------------------------------------\n\n"
 
 
-  p "This is card number #{round.flip + 1} out of #{cards.length}"
-  p "Question: " + round.current_card.question
+  puts "This is card number #{round.flip + 1} out of #{cards.length}"
+  puts "Question: " + round.current_card.question
   first_guess = gets.chomp.to_s
-  first_turn = Turn.new(first_guess, round.current_card)
-  p first_turn.feedback
+  first_turn = round.take_turn(first_guess)
+  puts "\n" + first_turn.feedback + "\n\n"
 
-  p "This is card number #{round.flip + 1} out of #{cards.length}"
-  p "Question: " + round.current_card.question
+  puts "This is card number #{round.flip + 1} out of #{cards.length}"
+  puts "Question: " + round.current_card.question
   second_guess = gets.chomp.to_s
-  second_turn = Turn.new(second_guess, round.current_card)
-  p second_turn.feedback
+  second_turn = round.take_turn(second_guess)
+  puts "\n" + second_turn.feedback + "\n\n"
 
-  p "This is card number #{round.flip + 1} out of #{cards.length}"
-  p "Question: " + round.current_card.question
+  puts "This is card number #{round.flip + 1} out of #{cards.length}"
+  puts "Question: " + round.current_card.question
   third_guess = gets.chomp.to_s
-  third_turn = Turn.new(third_guess, round.current_card)
-  p third_turn.feedback
+  third_turn = round.take_turn(third_guess)
+  puts "\n" + third_turn.feedback + "\n\n"
 
-p "****** Game over! ******"
-p "You had #{round.number_correct} correct guesses out of #{cards.length} for a total score of #{round.percent_correct}."
-p "STEM - #{round.percent_correct_by_category(:STEM)} correct"
-p "Geography - #{round.percent_correct_by_category(:geography)} correct"
+  puts "This is card number #{round.flip + 1} out of #{cards.length}"
+  puts "Question: " + round.current_card.question
+  fourth_guess = gets.chomp.to_s
+  fourth_turn = round.take_turn(fourth_guess)
+  puts "\n" + fourth_turn.feedback + "\n\n"
+
+  puts "****** Game over! ******"
+  puts "You had #{round.number_correct} correct guesses out of #{cards.length} for a total score of #{round.percent_correct.round}%."
+  puts "Geography - #{round.percent_correct_by_category(:Geography).round}% correct"
+  puts "STEM - #{round.percent_correct_by_category(:STEM).round}% correct"
