5636f388fe4d636a2116dba97da0811ea205b2b8
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-29 21:04:37 -0700

    Update read_file_into_array test to it_can_read_a_file test

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,25 +1,24 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/card_generator"
+require "./lib/card"
 
 
 class CardGeneratorTest < Minitest::Test
 
   def test_it_exists
-    cards = CardGenerator.new
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename)
 
     assert_instance_of CardGenerator, cards
   end
 
-  def test_it_can_read_a_file_into_array
+  def test_it_can_read_a_file
     filename = "cards.txt"
-    cards = CardGenerator.new(filename)
+    file_contents = CardGenerator.new(filename).read_in_file
 
-    assert_instance_of Array, cards
-    cards.each do |card|
-      assert_instance_of Card, card
-    end
-    
+    refute_equal [] , file_contents
+
   end
 
 end
