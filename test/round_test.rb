1919ca432f288156465ac4cdbdb86bee60d525d6
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-29 17:51:13 -0700

    Create another round test

--- test/round_test.rb
+++ test/round_test.rb
@@ -77,10 +77,25 @@ class RoundTest < Minitest::Test
     new_turn = round.take_turn("Juneau")
 
     assert_equal [new_turn], round.turns
-    assert_equal 1, round.correct
+    assert_equal 1, round.number_correct
     assert_equal deck.cards[1], round.current_card
   end
 
+  def test_can_go_another_round
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    round.take_turn("Venus")
+
+    assert_equal 2, round.turns.count
+    assert_equal "Incorrect.", round.turns.last.feedback
+    assert_equal 1, round.number_correct
+  end
 
 
 end
