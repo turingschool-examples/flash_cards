86b44b4b1a2d344ee8d76e833147a0d4c98c3e3d
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-29 17:32:28 -0700

    Add next card and history tests

--- test/round_test.rb
+++ test/round_test.rb
@@ -62,9 +62,22 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
 
     new_turn = round.take_turn("Juneau")
+
     assert new_turn.correct?
     assert_equal "Correct!", new_turn.feedback
+  end
 
+  def test_a_turn_gets_recorded_and_next_card_gets_drawn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal [new_turn], round.turns
+    assert_equal deck.cards[1], round.current_card
   end
 
 end
