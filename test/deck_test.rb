2749691af1b0d8b0841e19286521453cafb3d6b1
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-30 12:09:35 -0700

    Add test_sort_by_card_category

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,6 +6,17 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
 
+  def setup
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+
+      assert_instance_of Deck, deck
+    end
+
   def test_it_exists
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -34,18 +45,24 @@ class DeckTest < Minitest::Test
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-       cards = [card_1, card_2, card_3]
-       deck = Deck.new(cards)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
 
       assert_equal 3, cards.count
     end
 
   def test_sort_card_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    assert_equal [@card_2, @card_3], @deck.cards_in_category(:STEM)
-    assert_equal @card_1, @deck.cards_in_category(:Geography)
-    assert_equal [], @deck.cards_in_category('Pop Culture')
-  end
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+    # assert_equal [card_1], deck.cards_in_category(:Geography)
+    # assert_equal [], deck.cards_in_category("Pop Culture")
+    end
 end
 
 
