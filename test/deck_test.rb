18ce31675f3a29fdcea525f3fbced68b167bad87
Chris Stankus <chrisstankus99@gmail.com>
2020-01-30 08:46:11 -0700

    Actually add count test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -25,6 +25,16 @@ class DeckTest < Minitest::Test
     assert_equal cards, deck.cards
   end
 
+  def test_if_count_works
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+  end
+
 
 
 
