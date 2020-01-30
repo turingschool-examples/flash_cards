d520f72ba3b9d5ec0668df8ba774259405618eed
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-29 21:10:19 -0700

    Add convert_lines_to_card method to pass file_into_array_of_cards test

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -3,7 +3,7 @@ attr_reader :cards
   def initialize(filename = nil)
     @filename = filename
     @cards = []
-    # @cards = convert_lines_to_cards(self.read_in_file)
+    @cards = convert_lines_to_cards(self.read_in_file)
   end
 
   def read_in_file
@@ -14,4 +14,11 @@ attr_reader :cards
     lines
   end
 
+  def convert_lines_to_cards(lines)
+    lines.each do |line|
+      card_parameters = line.split(",")
+      cards << Card.new(card_parameters[0], card_parameters[1], card_parameters[3])
+    end
+   @cards
+  end
 end
