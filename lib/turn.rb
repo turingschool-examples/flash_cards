4c3fc8b4e3e29ae0ce1911b73b7ab5f34ce54ddb
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-30 16:32:35 -0700

    Add class Turn with methods correct? and feedback

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,11 @@
 class Turn
   attr_reader :guess, :card
+
   def initialize(guess, card)
     @guess = guess
     @card = card
   end
+
   def correct?
     if @guess == card.answer
       true
@@ -11,6 +13,7 @@ class Turn
       false
     end
   end
+
   def feedback
     if @guess == card.answer
       p "Correct!"
