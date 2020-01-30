43d5c1f51a6f19ccf4721817a2586deb06858d0c
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-29 20:14:35 -0700

    Add cards_in_category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,16 +1,28 @@
 class Deck
-  attr_reader :cards, :deck
+  attr_reader :cards, :category
 
   def initialize(cards)
     @card = cards
+    @category = category
   end
-
-
 
   def count
     @cards.count
   end
 
+  # def cards_in_category(category)
+  #   category_array = []
+  #   cards.each do |card|
+  #
+  #     if card.category == category
+  #       category_array << card
+  #     end
+  # end
 
-
+  def cards_in_category(category)
+    category_array = []
+    if card.category == category
+      category_array << card
 end
+end
+  end
