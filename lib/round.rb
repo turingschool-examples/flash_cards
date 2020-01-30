dbf0b84461151c137d58fc317204f5a8be14067a
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-30 16:30:55 -0700

    Refactor code to remove 2 instances variables and save as local variables in method number_correct

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,27 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns, :correct_cards, :incorrect_cards
   def initialize(deck)
     @deck = deck
+    @turns = []
+  end
+  def current_card
+    @deck.cards[0]
+  end
+  def take_turn(guess)
+    #revisit this
+    @turns << Turn.new(guess, self.current_card)
+    @turns[0]
+  end
+  def number_correct
+    incorrect_cards = []
+    correct_cards = []
+    @turns.each do |card|
+      if card.correct? == true
+        correct_cards << card
+      else
+        incorrect_cards << card
+      end
+    end
+    correct_cards.length
   end
 end
