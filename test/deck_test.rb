0882c756073c5d85bbd250aacc2a329a68fd2de3
Colin Alexander <colintalex@gmail.com>
2020-01-29 23:20:16 -0700

    Adds 2nd working test method, test_it_has_cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -10,6 +10,18 @@ class DeckTest < Minitest::Test
     card3 = Card.new("How great is Turing so far?", "Wonderful!", :Turing)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
+
     assert_instance_of Deck, deck
+  end
+
+  def test_it_has_cards
+    card1 = Card.new("What is real?", "You are.", :Philosophy)
+    card2 = Card.new("What is 2+2?", "4", :Math)
+    card3 = Card.new("How great is Turing so far?", "Wonderful!", :Turing)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal deck.cards, cards
+
   end
 end
