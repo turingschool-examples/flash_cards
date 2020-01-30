c50e198d0a8287b1d1f5e2f3ef88cdb70aa957fa
Jesse Gietzen <gietzenjesse@gmail.com>
2020-01-29 19:24:01 -0700

    add count_cards test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -23,6 +23,15 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
     assert_equal deck.cards, cards
   end
+
+  def test_count_the_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal deck.count, cards.count
+  end
 end
 
 # pry(main)> card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
