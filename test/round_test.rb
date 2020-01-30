a35ca41be26ce4b9c3fc08981571996978762908
Steven Meyers <sjjmeyers@gmail.com>
2020-01-29 20:22:29 -0700

    Add round count test to round_test

--- test/round_test.rb
+++ test/round_test.rb
@@ -18,7 +18,7 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     assert_instance_of Round, round
   end
-
+# Might not need this test because it does not test Round class
   def test_if_deck_holds_cards_array
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -38,6 +38,18 @@ class RoundTest < Minitest::Test
     deck = Deck.new(cards)
     round = Round.new(deck)
     assert_equal round.deck, deck
+  end
+
+  def test_if_counts_round_turns
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards  = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    turns = Round.turns(number_of_turns)
+    assert_equal round.turns, turns
   end
 
 end
