dce24b87b4bde6184cc9f6c910db69aa09ec0e95
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-29 20:26:47 -0700

    Add it_can_read_a_file_into_array test

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -11,4 +11,15 @@ class CardGeneratorTest < Minitest::Test
     assert_instance_of CardGenerator, cards
   end
 
+  def test_it_can_read_a_file_into_array
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename)
+
+    assert_instance_of Array, cards
+    cards.each do |card|
+      assert_instance_of Card, card
+    end
+    
+  end
+
 end
