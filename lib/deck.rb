abf757b2459748f906fc6c46966af88f576c2b22
Steven Meyers <sjjmeyers@gmail.com>
2020-01-29 17:07:55 -0700

    Add and pass test_if_returns_cards_from_category

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,8 +6,14 @@ class Deck
   end
 
   def cards_in_category(category)
-    # cards.each do ||
-    # end
+    cards_in_same_category = []
+
+    cards.each do |card|
+      if category == card.category
+        cards_in_same_category << card
+      end
+    end
+      cards_in_same_category
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -36,8 +36,10 @@ class DeckTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards  = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    assert_equal card_1, :Geography
 
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_1], deck.cards_in_category(:Geography)
+    assert_equal [], deck.cards_in_category("Pop Culture")
   end
 end
 
