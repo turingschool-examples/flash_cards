7396eccb04611f21b7e7a797caeb461222a9f38e
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-01-30 12:32:01 -0700

    Added current_card and take_turn method to Round class. Currently passes.

--- lib/round.rb
+++ lib/round.rb
@@ -8,5 +8,13 @@ class Round
     @card_count = 0
   end
 
+  def current_card
+    @deck.cards[@turns.count]
+  end
 
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    new_turn
+  end
 end
