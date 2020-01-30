6c87206fc6b80958537756514176f35bd4906e33
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-01-30 16:33:52 -0700

    Add test for Round class

--- test/round_test.rb
+++ test/round_test.rb
@@ -26,31 +26,86 @@ def test_deck_method_exists
 
   assert_equal deck, round.deck
 end
-#
-# pry(main)> round.deck
-# #=> #<Deck:0x00007fa160a38ed8...>
-#
-# pry(main)> round.turns
-# #=> []
-#
-# pry(main)> round.current_card
-# #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
-#
-# pry(main)> new_turn = round.take_turn("Juneau")
-# #=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
-#
-# pry(main)> new_turn.class
-# #=> Turn
-#
-# pry(main)> new_turn.correct?
-# #=> true
-#
-# pry(main)> round.turns
-# #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
-#
-# pry(main)> round.number_correct
-# #=> 1
-#
+
+def test_turns_method_exists
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+
+  assert_equal [], round.turns
+end
+
+def test_for_current_card_in_deck
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+
+  assert_equal card_1, round.current_card
+  #take another turn
+end
+
+#delete
+def test_if_can_take_a_turn_and_submit_a_guess
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  new_turn = round.take_turn("Juneau")
+
+
+  assert_equal new_turn, round.turns[0]
+  #assert_equal [new_turn], round.turns
+end
+
+def test_new_turn_is_instance_of_class_Turn
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  new_turn = round.take_turn("Juneau")
+
+  assert_instance_of Turn, new_turn
+end
+
+def test_if_guess_is_correct
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  new_turn = round.take_turn("Juneau")
+
+  assert_equal true, new_turn.correct?
+end
+
+def test_which_cards_have_already_taken_a_turn
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  new_turn = round.take_turn("Juneau")
+
+  assert_equal [new_turn], round.turns
+end
+
+def test_number_of_correct_cards
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  new_turn = round.take_turn("Juneau")
+
+  assert_equal 1, round.number_correct
+end
+
 # pry(main)> round.current_card
 # #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
 #
