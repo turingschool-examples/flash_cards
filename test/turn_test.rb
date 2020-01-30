c816038f3ae0386f521452be9368dd67ac209f25
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-01-30 12:06:35 -0700

    Added test for card and guess. Currently passes

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -13,4 +13,12 @@ class TurnTest < Minitest::Test
   def test_it_exists
     assert_instance_of Turn, @turn
   end
+
+  def test_turn_has_a_card
+    assert_equal @card, @turn.card
+  end
+
+  def test_turn_has_a_guess
+    assert_equal "Juneau", @turn.guess
+  end
 end
