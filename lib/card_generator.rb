2943ecf6129d1fb91b859b00fea9c9ee4cf4802c
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-29 21:07:01 -0700

    Add read_in_file method to pass can_read_a_file test

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,8 +1,17 @@
 class CardGenerator
-
-  def initialize()
-    
+attr_reader :cards
+  def initialize(filename = nil)
+    @filename = filename
+    @cards = []
+    # @cards = convert_lines_to_cards(self.read_in_file)
   end
 
+  def read_in_file
+    lines = []
+    File.open(@filename).each do |line|
+      lines << line
+    end
+    lines
+  end
 
 end
