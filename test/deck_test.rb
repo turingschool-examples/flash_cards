d50929aec08cccda763a2214074e7beb6a90b7db
“Krista <stadler.krista@gmail..com>
2020-01-30 20:14:52 -0700

    fixed variable name issue in cards to deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -29,14 +29,13 @@ class DeckTest < Minitest::Test
 #  end
 #end
 
-  def test_card_exists
+  def test_cards_to_deck
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    assert_instance_of Deck, deck_1
-    require "pry"; binding.pry
+    assert_instance_of Deck, deck
   end
 end
 
