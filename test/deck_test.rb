71fcdb6338ff7d9179828be23a8fe687326f382a
“Krista <stadler.krista@gmail..com>
2020-01-31 16:17:56 -0700

    cards in category method test created

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -58,6 +58,13 @@ class DeckTest < Minitest::Test
     assert_equal 3, deck.count
   end
 
+def cards_in_category_method
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
+  deck = Deck.new(cards)
+  assert_equal cards_in_category(:STEM), deck.cards_in_category(:STEM)
 
 end
 
