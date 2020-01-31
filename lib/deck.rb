ec591aa3c1f6949a5cee79c99662fd02f2a40aa8
“Krista <stadler.krista@gmail..com>
2020-01-30 21:41:33 -0700

    Wrote nonworking cards in category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,5 @@
+require './lib/card'
+
 class Deck
   attr_reader :cards
 
@@ -8,5 +10,14 @@ class Deck
   def count
     cards.count
   end
+
+def cards_in_category(category_parameter)
+  cards.each do |card|
+    card.include?(category_parameter) == true
+    puts card
+  end
+end
+
+
 
 end
