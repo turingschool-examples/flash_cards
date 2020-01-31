2f729c27f102dcf98ca0a4139313026d25faeec9
Chris Stankus <chrisstankus99@gmail.com>
2020-01-31 08:09:57 -0700

    Comments removed

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,12 +2,11 @@ class Deck
   attr_reader :cards, :count
   def initialize(cards)
     @cards = cards
-    #avoid methods in initialize
     @count = cards.length
   end
 
 
-#you need to get a category with deck.cards_in_category(:Category) and use that input to find the corresponding array
+
 
   def cards_in_category(card_category)
     card_cate = []
