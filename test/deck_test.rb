7ce59e0409ef65379174eeef9eecd5db4dd69a4f
“Krista <stadler.krista@gmail..com>
2020-01-30 21:03:43 -0700

    Cards method functional and deck count test written

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -45,15 +45,22 @@ class DeckTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     assert_equal deck.cards, cards
+    require "pry"; binding.pry
+  end
+
+  def test_count_method
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal deck.count, 3
   end
 end
 
 #ASK Instructor how you get this to work -- mine returns the iteration of the class AND the cards themselves.
 #pry(main)> deck = Deck.new(cards)
 #=> <Deck:0x00007fa160a38ed8...>
-
-#  pry(main)> deck.cards
-  #=> [#<Card:0x00007fa16104e160...>, #<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
 
 #  pry(main)> deck.count
   #=> 3
