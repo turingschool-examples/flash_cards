6306e471321a0ef483fccc9c3ba150108649b926
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-31 16:30:51 -0700

    Add test_it_can_find_current_card

--- test/round_test.rb
+++ test/round_test.rb
@@ -38,14 +38,22 @@ class RoundTest < Minitest::Test
 
   end
 
+  def test_it_can_find_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+
+  end
 
 
 end
 
 
-# pry(main)> round.turns
-# #=> []
-#
+
 # pry(main)> round.current_card
 # #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
 #
