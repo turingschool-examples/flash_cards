615a0069d8f04c34ca20da4795cfcc607a01bb0c
Zach Holcomb <zachholcombmusic@gmail.com>
2020-01-31 12:19:44 -0700

    Refactor code in round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -31,21 +31,19 @@ class Round
   end
 
   def percent_correct
-    total_turns = @turns.length.to_f
-    percent_correct = ((@number_correct.to_f) / total_turns) * 100.0
-    return percent_correct
+    ((@number_correct.to_f) / @turns.length.to_f) * 100.0
+
   end
 
   def percent_correct_by_category(category)
-    total_number_category = 0.0
+    total_number_category = 0
     @turns.each do |turn|
       if turn.card.category == category
-        total_number_category += 1.0
+        total_number_category += 1
       end
     end
-    correct_by_category = self.number_correct_by_category(category).to_f
-    percent_correct_by_category = (correct_by_category / total_number_category) * 100.0
-    return percent_correct_by_category
+    (number_correct_by_category(category).to_f / total_number_category) * 100.0
+
   end
 
 end
