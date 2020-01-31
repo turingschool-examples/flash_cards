2dc613c43d4866eab13208e0dc8fba1f48b50fc7
“Krista <stadler.krista@gmail..com>
2020-01-30 20:13:19 -0700

    Add cards to deck test and functionality

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,9 @@
 class Deck
   attr_reader :cards
 
-  def initialize(cards)
-    @cards = []
+  def initialize(deck_cards)
+    @deck_cards = deck_cards
   end
-
-  def add_card_to_deck(new_cards)
-    @cards << new_card
 
   #def add_new_card(card_name)
   #  @cards << card_name
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -30,8 +30,13 @@ class DeckTest < Minitest::Test
 #end
 
   def test_card_exists
-    deck_1 = Deck.new(cards)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
     assert_instance_of Deck, deck_1
+    require "pry"; binding.pry
   end
 end
 
