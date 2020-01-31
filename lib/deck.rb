8503c52bdbf90cd851368b49a4cbb568dc2f3085
Jesse Gietzen <gietzenjesse@gmail.com>
2020-01-31 09:00:37 -0700

    finish deck test (yay)

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,14 @@ class Deck
     @cards = deck
     @count = deck.count
   end
+
+  def cards_in_category(category)
+    category_array = []
+    cards.each do |card|
+      if card.category == category
+        category_array << card.to_s
+      end
+    end
+    category_array
+  end
 end
