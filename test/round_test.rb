82b90841fc005e86881601c67ad9e80c6560698d
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-30 19:25:38 -0700

    Add test_it_starts_at_zero

--- test/round_test.rb
+++ test/round_test.rb
@@ -7,8 +7,6 @@ require './lib/round'
 
 class RoundTest < Minitest::Test
 
-
-
   def test_it_exists
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -17,6 +15,27 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
 
     assert_instance_of Round, round
+  end
+
+  def test_it_starts_at_zero
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_instance_of Deck, deck.turn
+
+  end
+
+  def test_it_can_find_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_equal deck, round.deck
   end
 
 # require "pry"; binding.pry
