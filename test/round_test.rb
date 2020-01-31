5ff8563ca2a0e87e9c8905bab1963fb35740d0ba
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-31 16:15:50 -0700

    Add test_it_can_return_deck

--- test/round_test.rb
+++ test/round_test.rb
@@ -17,18 +17,7 @@ class RoundTest < Minitest::Test
     assert_instance_of Round, round
   end
 
-  def test_it_starts_at_zero
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new(deck)
-
-    assert_instance_of Deck, deck.turn
-
-  end
-
-  def test_it_can_find_current_card
+  def test_it_can_return_deck
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -38,6 +27,61 @@ class RoundTest < Minitest::Test
     assert_equal deck, round.deck
   end
 
-# require "pry"; binding.pry
+
 
 end
+
+
+# pry(main)> round.deck
+# #=> #<Deck:0x00007fa160a38ed8...>
+#
+# pry(main)> round.turns
+# #=> []
+#
+# pry(main)> round.current_card
+# #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
+#
+# pry(main)> new_turn = round.take_turn("Juneau")
+# #=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
+#
+# pry(main)> new_turn.class
+# #=> Turn
+#
+# pry(main)> new_turn.correct?
+# #=> true
+#
+# pry(main)> round.turns
+# #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
+#
+# pry(main)> round.number_correct
+# #=> 1
+#
+# pry(main)> round.current_card
+# #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
+#
+# pry(main)> round.take_turn("Venus")
+# #=> #<Turn:0x00007f972a215b38...>
+#
+# pry(main)> round.turns.count
+# #=> 2
+#
+# pry(main)> round.turns.last.feedback
+# #=> "Incorrect."
+#
+# pry(main)> round.number_correct
+# #=> 1
+#
+# pry(main)> round.number_correct_by_category(:Geography)
+# #=> 1
+#
+# pry(main)> round.number_correct_by_category(:STEM)
+# #=> 0
+#
+# pry(main)> round.percent_correct
+# #=> 50.0
+#
+# pry(main)> round.percent_correct_by_category(:Geography)
+# #=> 100.0
+#
+# pry(main)> round.current_card
+# #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
