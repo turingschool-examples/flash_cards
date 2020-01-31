3aa1da12f5b190258be6a977045b133594d8bdae
Alex Latham <alexlthm@gmail.com>
2020-01-30 19:41:39 -0700

    Combine final three tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -3,7 +3,6 @@ require 'minitest/pride'
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
-require 'pry'
 
 class DeckTest < Minitest::Test
 
@@ -40,7 +39,7 @@ class DeckTest < Minitest::Test
     assert_equal 3, deck.count
   end
 
-  def test_can_find_STEM_cards #should include next 3
+  def test_can_find_cards_of_given_category
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -49,28 +48,8 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
 
     assert_equal cards[1..2], deck.cards_in_category(:STEM)
-  end
-
-  def test_can_find_geography_cards
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
     assert_equal [cards[0]], deck.cards_in_category(:Geography)
+    assert_equal [], deck.cards_in_category(:"Pop Culture")
   end
 
-  def test_does_not_find_pop_culture_cards
-
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
-    assert_equal [], deck.cards_in_category("Pop Culture")
-  end
 end
