97b05475b4271c842ecaaa576017d6c5d0d789d3
“Krista <stadler.krista@gmail..com>
2020-01-31 16:38:33 -0700

    wrote cards in category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,4 +11,19 @@ class Deck
     cards.count
   end
 
+  geography_cards = []
+  stem_cards = []
+  pop_culture_cards = []
+
+  def cards_in_category(category_name)
+    cards.each do |card|
+      if card.category = ":STEM"
+        stem_cards << card
+      elsif card.category = ":Geography"
+        geography_cards << card
+      elsif card.category = "Pop Culture"
+        pop_culture_cards << card
+      end
+    end
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -50,6 +50,7 @@ class DeckTest < Minitest::Test
   end
 
   def test_count_method
+    skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -58,13 +59,14 @@ class DeckTest < Minitest::Test
     assert_equal 3, deck.count
   end
 
-def cards_in_category_method
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
-  deck = Deck.new(cards)
-  assert_equal cards_in_category(:STEM), deck.cards_in_category(:STEM)
+  def test_cards_in_category_method
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal deck.stem_cards, deck.cards_in_category(:STEM)
+  end
 
 end
 
