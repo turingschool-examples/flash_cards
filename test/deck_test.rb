d969f150b9c0b65f53d3a9d8e8456b1b4f1bb26f
Colin Alexander <colintalex@gmail.com>
2020-01-30 21:57:36 -0700

    Adds all test methods, functional.

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -19,9 +19,32 @@ class DeckTest < Minitest::Test
     card2 = Card.new("What is 2+2?", "4", :Math)
     card3 = Card.new("How great is Turing so far?", "Wonderful!", :Turing)
     cards = [card1, card2, card3]
+    expected = cards
     deck = Deck.new(cards)
 
-    assert_equal deck.cards, cards
+    assert_equal expected, deck.cards
 
+  end
+
+  def test_it_can_count_cards
+    card1 = Card.new("What is real?", "You are.", :Philosophy)
+    card2 = Card.new("What is 2+2?", "4", :Math)
+    card3 = Card.new("How great is Turing so far?", "Wonderful!", :Turing)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expected = 3
+
+    assert_equal expected, deck.count
+
+  end
+
+  def test_shows_cards_in_category
+    card1 = Card.new("What is real?", "You are.", :Philosophy)
+    card2 = Card.new("What is 2+2?", "4", :Math)
+    card3 = Card.new("How great is Turing so far?", "Wonderful!", :Turing)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal card3, deck.cards_in_category(:Turing)
   end
 end
