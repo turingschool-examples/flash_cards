c7294590664fb8a15978c404b3971e9d16fa548e
“Krista <stadler.krista@gmail..com>
2020-01-30 21:14:11 -0700

    Cards in category test created

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -55,16 +55,24 @@ class DeckTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    assert_equal deck.count, 3
+    assert_equal 3, deck.count
+  end
+
+  def test_cards_in_category_method
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_1], deck.cards_in_category(:Geography)
+    assert_equal [], deck.cards_in_category(:Pop Culture)
   end
 end
 
 #ASK Instructor how you get this to work -- mine returns the iteration of the class AND the cards themselves.
 #pry(main)> deck = Deck.new(cards)
 #=> <Deck:0x00007fa160a38ed8...>
-
-#  pry(main)> deck.count
-  #=> 3
 
 #  pry(main)> deck.cards_in_category(:STEM)
   #=> [#<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
