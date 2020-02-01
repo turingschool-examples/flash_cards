11eaac4b5f36491cebbab705fd31566f12c99287
“Krista <stadler.krista@gmail..com>
2020-01-31 21:50:58 -0700

    Create non working current card method

--- lib/round.rb
+++ lib/round.rb
@@ -2,8 +2,15 @@ class Round
   attr_reader :deck,
               :turns
 
-  def initialize (deck_parameter)
+  def initialize(deck_parameter)
     @deck = deck_parameter
     @turns = []
   end
+
+  def current_card
+    all_cards = cards.find do |card|
+
+  end
+
+#  end
 end
