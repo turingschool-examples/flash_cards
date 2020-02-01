3a06ec46ad797f27889f66ae0c2ef6ab1b09841a
“Krista <stadler.krista@gmail..com>
2020-01-31 20:59:26 -0700

    Cleaned up deck test file

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,31 +6,7 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
 
-#This card functionality has already been tested - this test isn't needed
-
-#  def test_card_exists
-#    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#    require "pry"; binding.pry
-#    assert_instance_of Card, card_1
-#    assert_instance_of Card, card_2
-#    assert_instance_of Card, card_3
-#  end
-
-#created this test just so I could pry / confirm that deck starts out empty. Not part of the interaction pattern, so I'm commenting it out.
-#  def test_deck_starts_empty
-#    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#    deck_1 = Deck.new
-#    require "pry"; binding.pry
-#    assert_instance_of Deck, deck_1
-#  end
-#end
-
   def test_cards_to_deck
-    skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -40,7 +16,6 @@ class DeckTest < Minitest::Test
   end
 
   def test_cards_method
-    skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -50,7 +25,6 @@ class DeckTest < Minitest::Test
   end
 
   def test_count_method
-    skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -68,20 +42,6 @@ class DeckTest < Minitest::Test
     assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
     assert_equal [card_1], deck.cards_in_category(:Geography)
     assert_equal [], deck.cards_in_category("Pop Culture")
-    require "pry"; binding.pry
   end
 
 end
-
-#ASK Instructor how you get this to work -- mine returns the iteration of the class AND the cards themselves.
-#pry(main)> deck = Deck.new(cards)
-#=> <Deck:0x00007fa160a38ed8...>
-
-#  pry(main)> deck.cards_in_category(:STEM)
-  #=> [#<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
-
-#  pry(main)> deck.cards_in_category(:Geography)
-  #=> [#<Card:0x00007fa16104e160...>]
-
-#  pry(main)> deck.cards_in_category("Pop Culture")
-  #=> []
