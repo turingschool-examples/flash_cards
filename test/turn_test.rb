6f42b59c0e3d79e26d9a6d47360ad23de85bab52
Colin Alexander <colintalex@gmail.com>
2020-01-31 19:38:49 -0700

    Adds changes to assert_equal args for multiple methods

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -22,7 +22,7 @@ def test_if_turn_has_guess
   card = Card.new("What is the capital of NM?", "Santa Fe", :Geography)
   turn = Turn.new("Santa Fe", card)
 
-  assert_equal turn.guess, card.answer
+  assert_equal "Santa Fe", turn.guess
 end
 
 def test_correct_returns_value
@@ -30,8 +30,8 @@ def test_correct_returns_value
   turn = Turn.new("Santa Fe", card)
   turn2 = Turn.new("Denver", card)
 
-  assert_equal turn.correct?, true
-  assert_equal turn2.correct?, false
+  assert_equal true, turn.correct?
+  assert_equal false, turn2.correct?
 end
 
 def test_if_feedback_responds
@@ -39,8 +39,8 @@ def test_if_feedback_responds
   turn = Turn.new("Santa Fe", card)
   turn2 = Turn.new("Denver", card)
 
-  assert_equal turn.feedback, "Correct!"
-  assert_equal turn2.feedback, "False!"
+  assert_equal "Correct!", turn.feedback 
+  assert_equal "False!", turn2.feedback
 end
 
 end
