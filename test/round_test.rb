d98d70d0837d82840dbd4ad38863b86eb891d297
“Krista <stadler.krista@gmail..com>
2020-01-31 21:20:50 -0700

    Wrote test deck method test

--- test/round_test.rb
+++ test/round_test.rb
@@ -8,6 +8,7 @@ require './lib/round'
 class RoundTest < Minitest::Test
 
   def test_round_exists
+    skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -15,6 +16,16 @@ class RoundTest < Minitest::Test
     deck = Deck.new(cards)
     round = Round.new(deck)
     assert_instance_of Round, round
+  end
+
+  def test_deck_method_works
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    assert_equal round.deck, deck
   end
 
 end
