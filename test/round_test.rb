ba747b0724cc1ab41c1b83f4bd2f4e727bedcc53
“Krista <stadler.krista@gmail..com>
2020-01-31 21:30:23 -0700

    Wrote current card method test

--- test/round_test.rb
+++ test/round_test.rb
@@ -19,6 +19,7 @@ class RoundTest < Minitest::Test
   end
 
   def test_deck_method_works
+    skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -35,7 +36,16 @@ class RoundTest < Minitest::Test
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
-    assert_equal round.turns, []
+    assert_equal [], round.turns
   end
 
+  def test_current_card_method
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    assert_equal card_1, round.current_card
+  end
 end
