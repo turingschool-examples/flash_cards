582dafc73eb5100fde5aa68aee50d4036b5bb95b
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-31 18:40:07 -0700

    Add test_can_return_number_correct

--- test/round_test.rb
+++ test/round_test.rb
@@ -87,20 +87,26 @@ class RoundTest < Minitest::Test
     assert_equal true, new_turn.correct?
   end
 
+  def test_can_return_number_correct
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card1, card2, card3])
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal 1, round.number_correct
+
+  end
+
 
 end
 
 
 
-# pry(main)> new_turn.correct?
-# #=> true
-#
-# pry(main)> round.turns
-# #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
-#
-# pry(main)> round.number_correct
-# #=> 1
-#
+
 # pry(main)> round.current_card
 # #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
 #
