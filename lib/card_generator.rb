ace2aeaa1276ece1e8d48434613aeff8f1c1799d
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-01 12:10:29 -0700

    Test that card_generator is creating cards from text file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,9 +1,19 @@
 class CardGenerator
 attr_reader :cards
   def initialize(filename)
-    @filename = File.open(filename)
+    @filename = File.read(filename)
     @cards = []
-  end
+    file_lines = @filename.lines
+    split_file_lines = file_lines.map do |line|
+      line.chomp.split(',')
+    end
 
+
+    split_file_lines.each do |line|
+      card = Card.new(line[0], line[1], line[2])
+      @cards << card
+    
+    end
+  end
 
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator.rb'
+require './lib/card'
+require './lib/deck'
 
 class CardGeneratorTest < Minitest::Test
   def setup
@@ -14,10 +16,10 @@ class CardGeneratorTest < Minitest::Test
     assert_instance_of CardGenerator, @cards
   end
 
-  def test_it_has_cards_
+  def test_it_generates_cards
+    deck = Deck.new(@cards.cards)
 
-
-    assert_equal [], @cards.cards
+    assert_equal deck.cards, @cards.cards
   end
 
 
