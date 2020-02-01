04f3a6fcddbd13c1a8189d3389ba18e4181c72c5
Javier Aguilar <jvr.agx@gmail.com>
2020-01-31 21:03:00 -0700

    Pass test_it_generates_cards_from_a_non_file

--- test/flashcard_runner_test.rb
+++ test/flashcard_runner_test.rb
@@ -23,4 +23,14 @@ class CardGeneratorTest < Minitest::Test
     assert_equal deck.cards, cards
   end
 
+  def test_it_generates_cards_from_a_non_file
+    trivia = "What is 5 + 5?,10,STEM
+            What is Rachel's favorite animal?,red panda,Turing Staff
+            What is Mike's middle name?,nobody knows,Turing Staff
+            What cardboard cutout lives at Turing?,Justin bieber,Pop Culture"
+    cards = CardGenerator.new(trivia)
+    deck = Deck.new(cards)
+
+    assert_equal deck.cards, cards
+  end
 end
