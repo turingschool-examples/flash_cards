c16ece69ff193c4500c6b57fea8fedb5138e3549
Chris Stankus <chrisstankus99@gmail.com>
2020-02-01 15:20:58 -0700

    Add correct by category function and tests

--- lib/round.rb
+++ lib/round.rb
@@ -12,8 +12,7 @@ class Round
 #the "!" in .rotate! is basically the same as doing this
 #current = @deck.cards.rotate
   def get_new_card
-    @deck.cards.rotate!
-    # require "pry"; binding.pry
+  @deck.cards.rotate!
   end
 
   def take_turn(guess)
@@ -32,4 +31,37 @@ class Round
     end
     num_correct
   end
+
+  def number_correct_by_category(category)
+    num_correct = 0
+    @turns.each do |turn|
+      if turn.correct? && turn.card.category == category
+        num_correct = num_correct + 1
+      end
+    end
+    num_correct
+  end
 end
+# num_correct
+# card_cate = []
+# @cards.each do |card|
+#   if card.category == card_category
+#     card_cate << card
+
+# num_correct_by_category = []
+# @turns.each do |turn|
+#   require "pry"; binding.pry
+#   turn.number_correct = turn.cards_in_category(category)
+#   num_correct_by_category << turn
+#   end
+#   num_correct_by_category.number_correct
+
+# num_correct_by_category = 0
+# @deck.each do |card|
+#   require "pry"; binding.pry
+#   if card.cards_in_category
+#
+#     num_correct_by_category = num_correct_by_category + 1
+#   end
+# end
+# num_correct_by_category
--- test/round_test.rb
+++ test/round_test.rb
@@ -62,5 +62,24 @@ class RoundTest < Minitest::Test
     assert_equal 1, round.number_correct
     assert_equal 1, round.turns.length
     assert_equal 3, deck.cards.length
+    round.take_turn("Anywhere")
+    assert_equal 2, round.turns.count
+    assert_equal "Incorrect.", round.turns.last.feedback
+    assert_equal 1, round.number_correct
   end
+
+  def test_number_correct_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+
+    assert_equal 1, round.number_correct_by_category(:Geography)
+    assert_equal 0, round.number_correct_by_category(:STEM)
+  end
+
+
+
 end
