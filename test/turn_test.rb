c6a39cdcfa4f40931c806f5e03f63cedb72aeeb5
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-01 14:05:07 -0700

    Add test to test if card method works

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,11 +4,18 @@ require './lib/turn'
 require './lib/card'
 
 class TurnTest < Minitest::Test
-  def test_turn_exists
+  def test_it_exists
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     assert_instance_of Turn, turn
+  end
+
+  def test_it_has_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal card, turn.card
   end
 
   def test_if_has_guess
