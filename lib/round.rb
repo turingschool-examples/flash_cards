19cc96865e2eecfae3af4f2be697b86dc78dd062
Whitney Kidd <whitneymkidd@gmail.com>
2020-02-01 12:59:02 -0700

    Add number_correct_by_category method

--- lib/round.rb
+++ lib/round.rb
@@ -2,16 +2,16 @@ require './lib/turn'
 require './lib/card'
 
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
 
   def initialize(deck)
     @deck = deck
     @turns = []
-
+    @number_correct = 0
   end
 
   def current_card
-    @deck.cards.first
+    @deck.cards[0]
   end
 
   def take_turn(guess)
@@ -19,28 +19,32 @@ class Round
     @deck.cards.rotate
     @turns.last
   end
+#
+#   def guess_correct
+#     @turns.each do |turn|
+#       turn.correct?
+#   end
+# end
+# ^^^ IDK IF I NEED THS OR NOT ^^^
 
-  def guess_correct
-    @turns.each do |turn|
-      turn.correct?
+ def number_correct
+    correct = []
+   @turns.each do |turn|
+      if turn.correct?
+        correct << turn
+      end
+    end
+   correct.count
   end
-end
 
-def number_correct
- correct = []
- @turns.each do |turn|
-   if turn.correct?
-     correct << turn
+
+def number_correct_by_category(category)
+   correct_by_category = []
+   @turns.each do |turn|
+     if turn.correct? && turn.card.category == category
+     correct_by_category << turn
+     end
    end
+   correct_by_category.length
  end
- correct.count
-end
-
-
-
-
-
-
-
-
 end
