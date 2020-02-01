f214707238324ef1c4eee64432dbd70258322bcc
“Krista <stadler.krista@gmail..com>
2020-01-31 21:24:58 -0700

    Wrote turns method returns empty array test

--- test/round_test.rb
+++ test/round_test.rb
@@ -28,4 +28,14 @@ class RoundTest < Minitest::Test
     assert_equal round.deck, deck
   end
 
+  def test_turns_method_returns_empty_array
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    assert_equal round.turns, []
+  end
+
 end
