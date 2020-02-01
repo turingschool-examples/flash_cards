ea42bdf81abb7b4ec1f532a3fc55757720600794
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-01 13:28:41 -0700

    Add start method to round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,9 @@
 class Round
-  attr_reader :deck, :turns, :correct_cards, :incorrect_cards
+  attr_reader :deck, :turns, :correct_cards, :incorrect_cards, :category_array
   def initialize(deck)
     @deck = deck
     @turns = []
+    @category_array = []
   end
 
   def current_card
@@ -28,8 +29,23 @@ class Round
   end
 
   def percent_correct_by_category(category)
-    category_array = []
-    @turns.each { |turn| category_array << turn if turn.card.category == category }
+    @category_array = []
+    @turns.each { |turn| @category_array << turn if turn.card.category == category }
     (number_correct_by_category(category).to_f/category_array.length) * 100.0
+  end
+
+  def start
+    until @deck.cards.length == 0
+      puts @deck.cards[0].question
+      take_turn(gets.chomp)
+
+      @turns[-1].feedback
+      @category_array << @turns[-1].card.category
+    end
+    puts '****** Game over! ******'
+    puts "You had #{number_correct} correct guesses out of #{@turns.length} for a total score of #{self.percent_correct.to_i}%."
+    @category_array.uniq.each do |category|
+      puts "#{category} - #{percent_correct_by_category(category).to_i}% correct"
+    end
   end
 end
