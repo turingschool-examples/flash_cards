ec46369630b62164b615018767b640d506671bec
Chris Stankus <chrisstankus99@gmail.com>
2020-02-01 13:11:28 -0700

    Add take_turn and number_correct methods and tests

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,35 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :guess
   def initialize(deck)
     @deck = deck
     @turns = []
+  end
+
+#this method needs to return the first card in the deck and once a guess is made and recorded the next card in the deck becomes the current card
+  def current_card
+    @deck.cards.first
+  end
+#the "!" in .rotate! is basically the same as doing this
+#current = @deck.cards.rotate
+  def get_new_card
+    @deck.cards.rotate!
+    # require "pry"; binding.pry
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    get_new_card
+    @turns << turn
+    turn
+  end
+
+  def number_correct
+    num_correct = 0
+    @turns.each do |turn|
+      if turn.correct?
+        num_correct = num_correct + 1
+      end
+    end
+    num_correct
   end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -35,4 +35,32 @@ class RoundTest < Minitest::Test
 
     assert_equal [], round.turns
   end
+
+  def test_it_can_get_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_equal card_1, round.current_card
+  end
+
+  def test_take_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+
+    assert_instance_of Turn, turn
+    assert_equal card_1, turn.card
+    assert_equal turn.guess, "Juneau"
+    assert_equal turn.correct?, true
+    assert_equal card_2, round.current_card
+    assert_equal 1, round.number_correct
+    assert_equal 1, round.turns.length
+    assert_equal 3, deck.cards.length
+  end
 end
