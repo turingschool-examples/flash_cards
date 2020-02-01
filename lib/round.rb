7804028d37492190b068d66f7b3d1a7f8496d76f
Josh Tukman <josh.tukman@gmail.com>
2020-01-31 20:41:44 -0700

    Add test to see if a round has a deck

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,8 @@
 class Round
-
+  attr_reader :deck 
   def initialize(deck)
     @deck = deck
-  end 
+  end
 
 
 
--- test/round_test.rb
+++ test/round_test.rb
@@ -19,6 +19,10 @@ def test_round_exists
 
   assert_instance_of Round, @round
 end
+def test_round_has_deck
+
+  assert_equal @deck, @round.deck
+end
 
 
 #   pry(main)> card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
