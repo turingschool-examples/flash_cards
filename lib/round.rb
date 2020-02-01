90dc5cd3828cb17d3636162814359bee1673c610
“Krista <stadler.krista@gmail..com>
2020-01-31 21:18:12 -0700

    Round exists

--- lib/round.rb
+++ lib/round.rb
@@ -1,4 +1,5 @@
 class Round
   def initialize (deck_parameter)
     @deck = deck_parameter
+  end  
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -5,14 +5,16 @@ require './lib/card'
 require './lib/deck'
 require './lib/round'
 
-class DeckTest < Minitest::Test
+class RoundTest < Minitest::Test
 
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
-  deck = Deck.new(cards)
-  round = Round.new(deck)
-  assert_instance_of Round, round
+  def test_round_exists
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    assert_instance_of Round, round
+  end
 
 end
