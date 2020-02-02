3e303fd096d1a9bfa2d93fd112377d63129bde5a
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-01 19:52:26 -0700

    Refactor code

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -2,26 +2,20 @@ require_relative './card.rb'
 require_relative './deck.rb'
 
 class CardGenerator
+  attr_reader :generated_cards
   def initialize(filename)
     @filename = filename
-    @generated_cards = []
   end
 
   def cards
-    file_array = []
-    questions = []
-    answers = []
-    categories = []
+    cards_from_filename = []
+    generated_cards = []
     File.open(@filename).each do |line|
-      file_array << line.chomp.split(',')
+      cards_from_filename << line.chomp.split(',')
     end
-    file_array.each do |card|
-      @generated_cards << Card.new(card[0],card[1],card[2])
+    cards_from_filename.each do |card|
+      generated_cards << Card.new(card[0],card[1],card[2])
     end
-    @generated_cards
+    generated_cards
   end
 end
-
-# filename = 'cards.txt'
-# cards = CardGenerator.new(filename)
-# cards.cards
--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,8 @@
 class Round
-  attr_reader :deck, :turns, :category_array
+  attr_reader :deck, :turns_completed
   def initialize(deck)
     @deck = deck
-    @turns = []
-    @category_array = []
+    @turns_completed = []
   end
 
   def current_card
@@ -11,45 +10,46 @@ class Round
   end
 
   def take_turn(guess)
-    @turns << Turn.new(guess, self.current_card)
+    @turns_completed << Turn.new(guess, self.current_card)
     @deck.cards.shift
-    @turns[-1]
+    @turns_completed[-1]
   end
 
   def number_correct
-    @turns.count { |turn| turn.correct? }
+    @turns_completed.count { |turn| turn.correct? }
   end
 
   def number_correct_by_category(category)
-    @turns.count { |turn| turn.correct? && turn.card.category == category }
+    @turns_completed.count { |turn| turn.correct? && turn.card.category == category }
   end
 
   def percent_correct
-    (number_correct.to_f/turns.length) * 100.0
+    (number_correct.to_f/turns_completed.length) * 100
   end
 
   def percent_correct_by_category(category)
-    @category_array = []
-    @turns.each { |turn| @category_array << turn if turn.card.category == category }
-    (number_correct_by_category(category).to_f/category_array.length) * 100.0
+    cards_of_same_category = []
+    @turns_completed.each { |turn| cards_of_same_category << turn if turn.card.category == category }
+    (number_correct_by_category(category).to_f/cards_of_same_category.length) * 100
   end
 
   def start
     turns_count = 1
+    categories = []
     puts "Welcome! You're playing with #{@deck.count} cards"
     puts '-------------------------------------------------'
     until @deck.cards.length == 0
-      puts "This is card number #{turns_count} out of #{@deck.count + @turns.count}."
+      puts "This is card number #{turns_count} out of #{@deck.count + @turns_completed.count}."
       puts @deck.cards[0].question
       take_turn(gets.chomp)
 
-      @turns[-1].feedback
-      @category_array << @turns[-1].card.category
+      @turns_completed[-1].feedback
+      categories << @turns_completed[-1].card.category
       turns_count += 1
     end
     puts '****** Game over! ******'
-    puts "You had #{number_correct} correct guesses out of #{@turns.length} for a total score of #{self.percent_correct.to_i}%."
-    @category_array.uniq.each do |category|
+    puts "You had #{number_correct} correct guesses out of #{@turns_completed.length} for a total score of #{percent_correct.to_i}%."
+    categories.uniq.each do |category|
       puts "#{category} - #{percent_correct_by_category(category).to_i}% correct"
     end
   end
