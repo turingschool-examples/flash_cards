5ea67324eff114da2a624e6eea7925365bde8b2c
“Krista <stadler.krista@gmail..com>
2020-02-02 09:54:13 -0700

    Wrote number correct by category test

--- test/round_test.rb
+++ test/round_test.rb
@@ -122,6 +122,17 @@ class RoundTest < Minitest::Test
     assert_equal 2, round.number_correct
   end
 
+  def test_number_correct_by_category
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    assert_equal 1, round.number_correct_by_category(:Geography)
+  end
+
   end
 
 #  pry(main)> round.number_correct_by_category(:Geography)
