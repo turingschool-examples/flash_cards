9b693989099111f37ef231e120721b2e706ef07c
Colin Alexander <colintalex@gmail.com>
2020-02-01 18:53:15 -0700

    Adds percent_correct and percent_correct_by_category, fixes bug with number_correct_by_category

--- lib/round.rb
+++ lib/round.rb
@@ -2,38 +2,47 @@ require './lib/deck'
 require './lib/turn'
 
 class Round
-  attr_reader :deck, :turns, :number_correct
+  attr_reader :deck,
+              :turns,
+              :number_correct,
+              :correct_cards,
+              :used_deck
   def initialize(deck)
     @deck = deck
+    @used_cards = []
     @turns = []
     @number_correct = 0
-    @correct_turns = []
+    @correct_cards = []
+
   end
 
-  def current_card_detect
+  def current_card
     @deck.cards.first
   end
 
   def take_turn(guess)
-    new_turn = Turn.new(guess, current_card_detect)
-    @turns << new_turn
-    if guess == current_card_detect.answer
+    current_turn = Turn.new(guess, current_card)
+    if guess == current_card.answer
       @number_correct += 1
-      @correct_turns << new_turn
+      @correct_cards << current_card
     end
-    new_turn
+    @turns << current_turn
+    @used_cards << @deck.cards.shift
+    current_turn
   end
 
   def number_correct_by_category(category)
-    correct_collect = @correct_turns.select{ |turn| category}
-    correct_collect.count
+    correct_by_category = @correct_cards.find_all{|card|card.category == category}
+    correct_by_category.length
   end
 
   def percent_correct
     (@number_correct.to_f / @turns.length.to_f) * 100
   end
 
-  def percent_correct_by_category
+  def percent_correct_by_category(category)
 
+    total = @used_cards.find_all{|used_card| used_card.category ==  category}
+    (number_correct_by_category(category) / total.length.to_f) * 100
   end
 end
