2640f8936c97df8e9a4e97f2f663136591a6633a
Alex Latham <alexlthm@gmail.com>
2020-02-02 15:49:11 -0700

    Add working file reader that processes cards

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -4,20 +4,14 @@ class CardGenerator
   def initialize(filename_parameter)
     file = File.new(filename_parameter, "r")
 
-    cards = []
-    text = file.read
-    array_of_text = text.split("\n")
-    line1 = array_of_text[0].split(",")
-    line2 = array_of_text[1].split(",")
+    card_values = file.read.split("\n")
+    @cards = card_values.map do |card_value|
+      line = card_value.split(",")
+      question = line[0]
+      answer = line[1]
+      category = line[2].to_sym
+      Card.new(question, answer, category)
+    end
     require "pry"; binding.pry
-
-
-    card = line.split(",")
-    question = card[0]
-    answer = card[1]
-    category = card[2].delete("\n").to_sym
-    Card.new
-
   end
-
 end
