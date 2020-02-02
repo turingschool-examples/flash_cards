aea521046101b005deb822e74a583ed6c1df251f
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-01 19:52:54 -0700

    Add test for .card method

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -3,11 +3,20 @@ require 'minitest/pride'
 require './lib/turn'
 require './lib/card'
 require './lib/deck'
+require './lib/card_generator'
 
 class CardGeneratorTest < Minitest::Test
   def test_it_exists
+    filename = 'cards.txt'
+    card = CardGenerator.new(filename)
+
+    assert_instance_of CardGenerator, card
+  end
+
+  def test_card_method_exists
+    filename = 'cards.txt'
     cards = CardGenerator.new(filename).cards
 
-    assert_instance_of CardGenerator, cards
+    assert_equal [cards[0], cards[1], cards[2], cards[3]], cards
   end
 end
