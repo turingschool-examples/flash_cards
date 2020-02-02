4c270a765ad8712ea95332cb333fe05f1c8103e2
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-01 17:33:15 -0700

    Add filename for iteration 4

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,16 +2,14 @@ require './lib/card.rb'
 require './lib/round.rb'
 require './lib/deck.rb'
 require './lib/turn.rb'
+require './lib/card_generator.rb'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-card_4 = Card.new("What is Daniel Atkinson's middle name?", "Patrick", :"Personal Info")
+filename = 'cards.txt'
 
-deck = Deck.new([card_1, card_2, card_3, card_4])
+card = CardGenerator.new(filename).cards
+
+deck = Deck.new(card)
 
 round = Round.new(deck)
 
 round.start
-
-# require "pry"; binding.pry
