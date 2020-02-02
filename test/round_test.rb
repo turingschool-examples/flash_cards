d4f71704c87246ca1c295dd2d6138334cc8cbae2
Colin Alexander <colintalex@gmail.com>
2020-02-01 18:55:08 -0700

    Adds changes to assert_equal variables for more robust testing.Functional, complete for Iteration 2.

--- test/round_test.rb
+++ test/round_test.rb
@@ -48,7 +48,7 @@ class RoundTest < Minitest::Test
     deck = Deck.new(cards)
     round = Round.new(deck)
 
-    assert_equal card1, round.current_card_detect
+    assert_equal card1, round.current_card
   end
 
   def test_can_take_turn
@@ -72,7 +72,23 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     new_turn = round.take_turn("You are.")
 
-    assert_equal [new_turn], round.turns
+    assert_equal 1, round.turns.length
+
+  end
+
+  def test_number_correct_functionality
+    card1 = Card.new("What is real?", "You are.", :Turing)
+    card2 = Card.new("What is 2+2?", "4", :Math)
+    card3 = Card.new("How great is Turing so far?", "Wonderful!", :Turing)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    round.take_turn("You are.")
+    assert_equal 1, round.number_correct
+    round.take_turn("4")
+
+    assert_equal 2, round.number_correct
   end
 
   def test_number_correct_by_category
@@ -82,17 +98,15 @@ class RoundTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     round = Round.new(deck)
-    new_turn = round.take_turn("You are.")
-
-    round.current_card_detect
+    round.take_turn("You are.")
     round.take_turn("4")
-require "pry"; binding.pry
-    assert_equal 1, round.number_correct_by_category(:Math)
-    assert_equal 0, round.number_correct_by_category(:Science)
 
+    assert_equal 1, round.number_correct_by_category(:Turing)
+    assert_equal 0, round.number_correct_by_category(:Science)
   end
 
   def test_percentage_correct
+
     card1 = Card.new("What is real?", "You are.", :Turing)
     card2 = Card.new("What is 2+2?", "4", :Math)
     card3 = Card.new("How great is Turing so far?", "Wonderful!", :Turing)
@@ -101,9 +115,24 @@ require "pry"; binding.pry
     round = Round.new(deck)
     new_turn = round.take_turn("You are.")
 
-    round.current_card_detect
-    round.take_turn("4")
+    round.current_card
+    round.take_turn("3")
 
     assert_equal 50.0, round.percent_correct
+  end
+
+  def test_percentage_correct_by_Category
+    card1 = Card.new("What is real?", "You are.", :Turing)
+    card2 = Card.new("What is 2+2?", "4", :Math)
+    card3 = Card.new("How great is Turing so far?", "Wonderful!", :Turing)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("You are.")
+    round.take_turn("3")
+    round.take_turn("Blue")
+
+    assert_equal 50, round.percent_correct_by_category(:Turing)
+    assert_equal 0, round.percent_correct_by_category(:Math)
   end
 end
