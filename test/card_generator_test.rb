4f5437ad7174b90e6d93a70cdce06d45ae3a9bde
Colin Alexander <colintalex@gmail.com>
2020-02-03 00:19:54 -0700

    Adds correct assert for 3rd test method, 3 working test methods. it_exists, it_has_filename, can_load_cards

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -2,17 +2,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator'
 
-
 class CardGeneratorTest < Minitest::Test
   def test_it_exists
-    skip
     filename = './lib/cards.txt'
     card_gen1 = CardGenerator.new(filename)
     assert_instance_of CardGenerator, card_gen1
   end
 
   def test_it_has_filename
-    skip
     filename = './lib/cards.txt'
     card_gen1 = CardGenerator.new(filename)
     assert_equal './lib/cards.txt', filename
@@ -21,9 +18,6 @@ class CardGeneratorTest < Minitest::Test
   def test_it_can_load_cards
     filename = './lib/cards.txt'
     card_gen1 = CardGenerator.new(filename).cards
-    require "pry"; binding.pry
-    assert_equal 4, 4
-
+    assert_equal 4, card_gen1.length
   end
-
 end
