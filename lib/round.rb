a2effa518c395ffea205917919879c9f82fdefad
Mariana Cid <cidmariana5@gmail.com>
2020-02-02 23:13:06 -0700

    Number correct method completed

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,11 @@
 class Round
+
   attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @correct_answer = 0
   end
 
   def current_card
@@ -14,5 +16,26 @@ class Round
     turn = Turn.new(guess, current_card)
     @turns << turn
     turn
+    # @deck.cards.map do |card|
+    #   if turn.guess == card.answer
+    #     @correct_answer += 1
+    #   end
+    # end
+    # turn 
+    # @correct_answer
+  end
+
+
+  def number_correct
+
+    # require "pry"; binding.pry
+    @turns.map do |turn|
+      if turn.guess == @deck.cards.each do |card|
+                          card.answer
+                        end
+        @correct_answer += 1
+      end
+    end
+    @correct_answer
   end
 end
