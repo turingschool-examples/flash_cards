33da6ee6bb2e488a2df3368a6f14519b5c7642d5
michelle <foleymichelle9@gmail.com>
2020-02-02 21:49:57 -0700

    Test works, fabulous run

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -16,3 +16,36 @@ class TurnTest < Minitest::Test
       turn = Turn.new("Juneau", card)
       assert_equal card, turn.card
     end
+
+    def test_turn_has_an_guess
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+        assert_equal "Juneau", turn.guess
+      end
+
+      def test_turn_is_correct
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+        assert_equal true, turn.correct?
+      end
+
+
+      def test_turn_feedback_returns_correct_when_correct
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+        assert_equal "Correct!", turn.feedback
+      end
+
+      def test_correct_returns_false_when_incorrect
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Saturn", card)
+        assert_equal false, turn.correct?
+      end
+
+      def test_feedback_returns_incorrect_when_incorrect
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Saturn", card)
+        assert_equal "Incorrect.", turn.feedback
+      end
+
+    end
