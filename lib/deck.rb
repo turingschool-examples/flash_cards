895d0f13a2193bedf594be20c63db0d2bfcd1e9a
Colin Alexander <colintalex@gmail.com>
2020-02-03 00:39:17 -0700

    Adds empty space cleanup for submission.

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,9 +3,11 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
   def count
     @cards.count
   end
+  
   def cards_in_category(category)
     @cards.find_all do |card|
       card.category == category
