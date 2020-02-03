05cf7b8ea5ecae4a011ba395dcca1b07241b44ed
Alex Lynch <mlynch5187@gmail.com>
2020-02-02 19:41:50 -0700

    ework on start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,24 +3,33 @@ require_relative './lib/deck.rb'
 require_relative './lib/card.rb'
 require_relative './lib/turn.rb'
 
-@card1 = Card.new("In what year did the United States of America declare independence from Great Britain?", "1776", :History)
+@card_1 = Card.new("In what year did the United States of America declare independence from Great Britain?", "1776", :History)
+@card_2 = Card.new("What is the capital of Canada?", "Ottawa", :Geography)
+@card_3 = Card.new("What is largest country in the world?", "Russia", :History)
+@cards = [@card_1, @card_2, @card_3]
+@deck = Deck.new(@cards)
+@round = Round.new(@deck)
 
-puts "Welcome! You're playing with 4 cards.
-      -------------------------------------------------
-      This is card number 1 out of 4.
-      Question: What is 5 + 5?"
+  def start
+    puts "Welcome! You're playing with #{@cards.count} cards."
+    puts  " -------------------------------------------------"
+    puts   "This is card number #{@cards.find_index(@round.current_card) + 1}."
+    puts  @round.current_card.question
 
-user_answer_1 = gets.chomp
-
-  if user_answer_1 == 10
-    puts "Correct!
-    This is card number 2 out of 4.
-    Question: What is Rachel's favorite animal?"
-  else
-    puts "Incorrect.
-    This is card number 2 out of 4.
-    Question: What is Rachel's favorite animal?"
+    @round.take_turn(gets.chomp)
   end
+
+# user_answer_1 = gets.chomp
+#
+#   if user_answer_1 == 10
+#     puts "Correct!
+#     This is card number 2 out of 4.
+#     Question: What is Rachel's favorite animal?"
+#   else
+#     puts "Incorrect.
+#     This is card number 2 out of 4.
+#     Question: What is Rachel's favorite animal?"
+#   end
 
 # user_answer_2 = gets.chomp
 #
@@ -33,3 +42,5 @@ user_answer_1 = gets.chomp
 #     This is card number 2 out of 4.
 #     Question: What is Rachel's favorite animal?"
 #   end
+
+start
