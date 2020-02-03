65c0cc61bcf2c9d7613e1b0adea2238a5ed9c413
michelle <foleymichelle9@gmail.com>
2020-02-02 22:05:51 -0700

    final edits

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,24 @@
 class Deck
   attr_reader :cards
-
+  #
   def initialize(cards)
     @cards = cards
   end
+
+def count
+  @cards.count
+#
+end
+
+def cards_category(category)
+
+  @cards.each do |card|
+    if card.category == cards_category
+      stem_cards << card
+
+    end
+#
+     stem_cards
+
+   end
+end
