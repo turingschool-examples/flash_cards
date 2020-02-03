30d7ce0cb5d3e8d9ad3c62a172b61d2de8f81f9f
michelle <foleymichelle9@gmail.com>
2020-02-02 21:23:31 -0700

    Added has card test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -11,35 +11,8 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
-  def test_it_has_a_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of Turn, turn
-  end
-
-  def test_correct_is_working
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn1 = Turn.new("Juneau", card1)
-
-    assert_equal true, turn1.correct?
-
-    card2 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn2 = Turn.new("Denver", card2)
-
-    assert_equal false, turn2.correct?
-
-  end
-
-  def test_it_can_give_feedback
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn1 = Turn.new("Juneau", card1)
-
-    assert_equal "Correct!", turn1.feedback
-
-     card2 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     turn2 = Turn.new("Denver", card2)
-
-     assert_equal "Incorrect.", turn2.feedback
-   end
-end
+  def test_turn_has_a_card
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
+      assert_equal card, turn.card
+    end
