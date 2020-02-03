51f04dbc0cfbd7ab71c32cf7885537d83c5a0da9
Colin Alexander <colintalex@gmail.com>
2020-02-03 00:22:15 -0700

    Adds code to return correct return value for cards method.

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -8,11 +8,11 @@ class CardGenerator
   end
 
   def cards
-    file_data = File.read(@filename)
-    file_data_by_line = file_data.split("\n")
+    file_data_by_line = File.read(@filename).split("\n")
     file_data_by_line.each do |line|
       split_line = line.split(",")
       @new_cards << Card.new(split_line[0], split_line[1], split_line[2].to_sym)
     end
+    @new_cards
   end
 end
