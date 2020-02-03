dd62bd7459e5834c783b9510526663f96e55e4e4
michelle <foleymichelle9@gmail.com>
2020-02-02 20:18:15 -0700

    Final Card commit

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,6 @@
 class Card
   attr_reader :question, :answer, :category
-#set the initial values for an object
+#set  values for an object
   def initialize(question, answer, category)
     @question = question
     @answer = answer
