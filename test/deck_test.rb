c6434fbf63109feb1a0af77afe31af2b0819261c
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-14 11:17:58 -0600

    Create tests for iteration 3 - deck tests

---
+++ test/deck_test.rb
@@ -0,0 +1,33 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/deck"
+require "./lib/card"
+
+class DeckTest < Minitest::Test
+
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+  end
+
+  def test_it_exists
+    assert_instance_of Deck, @deck
+  end
+
+  def test_it_has_an_array_of_cards
+    assert_equal [@card_1, @card_2, @card_3], @deck.cards
+  end
+
+  def test_it_can_count_cards
+    assert_equal 3, @cards.size
+  end
+
+  def test_it_can_sort_cards_by_category
+    assert_equal [@card_2, @card_3], @deck.cards_in_category(:STEM)
+    assert_equal [@card1], @deck.cards_in_category(:Geography)
+    assert_equal [], @deck.cards_in_category("Pop Culture")
+  end
+end
