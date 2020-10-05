a7aa6a363b4a9b756c4b6acd800c93ac4cff6f7a
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-05 17:39:13 -0600

    Add Deck class

---
+++ lib/deck.rb
@@ -0,0 +1,21 @@
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @cards.length
+  end
+
+  def cards_in_category(category)
+    same_category = []
+    @cards.map do |card|
+      if card.category == category
+        same_category << card
+      end 
+    end
+    same_category
+  end
+end
