7e5201cdd6584266335fe0d03dfd19bb784f31d1
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-05 17:14:42 -0600

    finish I1

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -26,4 +26,20 @@ class TurnTest < Minitest::Test
 
     assert_equal true, turn.correct?
   end
+
+  def test_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Correct!", turn.feedback
+  end
+
+  def test_it_can_be_incorrect
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    assert_equal "Saturn", turn.guess
+    assert_equal false, turn.correct?
+    assert_equal "Incorrect.", turn.feedback
+  end
 end
