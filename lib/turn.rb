4f08e9b2def1921096847b1f2758967fc6a38060
Ely Hess <ely.hess@me.com>
2020-10-05 20:52:41 -0600

    Add feedback & correct? methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,18 +1,35 @@
-require '../lib/card'
+require '../lib/card'  # => true
 
 class Turn
-  attr_reader :string, :card
+  attr_reader :string, :card  # => nil
 
   def initialize(string, card)
     @string = string
     @card = card
-  end
+  end                           # => :initialize
 
   def guess
     @string
-  end
+  end        # => :guess
 
   def card
     @card
-  end
-end
+  end       # => :card
+
+  def correct?
+    if @string == card.answer
+      true
+    else
+      false
+    end
+  end                          # => :correct?
+
+  def feedback
+    if correct? == true
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end                    # => :feedback
+  
+end  # => :feedback
