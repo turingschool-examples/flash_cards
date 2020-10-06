6ed76732cb44d1227b19948cff729dbdd9049745
Ely Hess <ely.hess@me.com>
2020-10-05 21:00:02 -0600

    Add test_it_has_turn

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -3,5 +3,14 @@ require 'minitest/pride'
 require '../lib/turn'
 
 class TurnTest < Minitest::Test
+  def test_it_has_turn
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Card, card
+    assert_instance_of Turn, turn
+  end
+
+  
 
 end
