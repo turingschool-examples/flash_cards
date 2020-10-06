287f9595e65e063e766f73cddd582a012375b360
Liam Cusack <LCusack8108@gmail.com>
2020-10-06 17:28:20 -0600

    All tests pass fix overcomplication

--- lib/card.rb
+++ lib/card.rb
@@ -1,25 +1,12 @@
 class Card
 
-    attr_reader :question, :answer
-    def initialize(question, answer, card_category)
+    attr_reader :question, :answer, :category
+
+    def initialize(question, answer, category)
         @question = question
         @answer = answer
-        @card_category = []
+        @category = category
+
     end
 
-  def categorize(categorization)
-  @card_category << [categorization]
-  end
-
-  def category
-    print card_category
-  end
-
-    def is_a_card
-        if card.is_a?(Card) == true
-            puts true
-        else
-            puts false
-        end
-end
 end
