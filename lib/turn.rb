7659d33cbd38dddfd1cdc4aa7e38ffbd17a77ea1
Yesi Meza <yesi.meza10@gmail.com>
2020-10-05 21:32:13 -0600

    add to turn class and turn test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,12 @@
 class Turn
-  
+
+attr_reader :guess
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def return_card
+    @card    
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,3 +4,22 @@ require './lib/card'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
+
+  def test_it_exists
+    turn = Turn.new("Juneau", Card)
+
+    assert_instance_of Turn, turn
+  end
+
+  def test_it_can_return_a_guess
+    turn = Turn.new("Juneau", Card)
+
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_it_can_return_a_card
+    turn = Turn.new("Juneau", Card)
+
+    assert_equal card, turn.return_card
+  end
+end
