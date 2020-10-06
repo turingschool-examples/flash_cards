f8e620871c7650fb57526d014bf52cec3e0e743e
Robert Heath <robert.b.heathii@gmail.com>
2020-10-06 11:19:15 -0500

    Write instance and attribute test for Test

---
+++ test/turn_test.rb
@@ -0,0 +1,15 @@
+require './lib/turn'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class TurnTest < MiniTest::Test
+
+  def test_it_exists_and_has_attributes
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
+    assert_equal card, turn.card
+    assert_equal "Juneau", turn.guess
+  end
+end
