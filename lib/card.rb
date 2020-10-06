d7d8f7e77db3b2134ccf951f680290b109a3f61a
Joseph Budina <joebud3@gmail.com>
2020-10-06 16:25:07 -0600

    Initialize card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,1 +1,8 @@
-class Card 
+class Card
+  attr_reader :question, :answer, :category
+   def initialize(question, answer, catogory)
+     @question = question
+     @answer = answer
+     @category = catogory
+  end
+ end
