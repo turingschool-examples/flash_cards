352e475fd0c6c0ab48c6e0fdbbc40256f5b93503
Ely Hess <ely.hess@me.com>
2020-10-05 21:18:26 -0600

    Remove comments

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,30 +1,42 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/card'
+require 'minitest/autorun'  # => true
+require 'minitest/pride'    # => true
+require './lib/card'       # => true
 
-class CardTest < Minitest::Test
+
+class CardTest < Minitest::Test  # => Minitest::Test
 
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007fd6d083ee70 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
 
-    assert_instance_of Card, card
-  end
+    assert_instance_of Card, card  # => true
+  end                              # => :test_it_exists
 
   def test_it_has_a_question
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007fd6d083de58 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
 
-    assert_equal "What is the capital of Alaska?", card.question
-  end
+    assert_equal "What is the capital of Alaska?", card.question  # => true
+  end                                                             # => :test_it_has_a_question
 
   def test_it_has_an_answer
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007fd6d0844a78 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
 
-    assert_equal "Juneau", card.answer
-  end
+    assert_equal "Juneau", card.answer  # => true
+  end                                   # => :test_it_has_an_answer
 
   def test_it_has_a_category
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007fd6d083e650 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
 
-    assert_equal :Geography, card.category
-  end
-end
+    assert_equal :Geography, card.category  # => true
+  end                                       # => :test_it_has_a_category
+
+end  # => :test_it_has_a_category
+
+# >> Run options: --seed 24571
+# >>
+# >> # Running:
+# >>
+# >> ....
+# >>
+# >> Finished in 0.000796s, 5025.1256 runs/s, 5025.1256 assertions/s.
+# >>
+# >> 4 runs, 4 assertions, 0 failures, 0 errors, 0 skips
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,59 +1,49 @@
-require 'minitest/autorun'  # => true
-require 'minitest/pride'    # => true
-require './lib/turn'       # => true
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/turn'
 
-class TurnTest < Minitest::Test                                              # => Minitest::Test
+class TurnTest < Minitest::Test
   def test_it_has_turn
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007ff4c48a00f0 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-    turn = Turn.new("Juneau", card)                                          # => #<Turn:0x00007ff4c48abbd0 @string="Juneau", @card=#<Card:0x00007ff4c48a00f0 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>>
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    assert_instance_of Card, card  # => true
-    assert_instance_of Turn, turn  # => true
-  end                              # => :test_it_has_turn
+    assert_instance_of Card, card
+    assert_instance_of Turn, turn
+  end
 
   def test_it_has_card
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007ff4c48aa8e8 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-    turn = Turn.new("Juneau", card)                                          # => #<Turn:0x00007ff4c48aa528 @string="Juneau", @card=#<Card:0x00007ff4c48aa8e8 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>>
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    assert_instance_of Card, card  # => true
-    assert_equal card, turn.card   # => true
-  end                              # => :test_it_has_card
+    assert_instance_of Card, card
+    assert_equal card, turn.card
+  end
 
   def test_it_has_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007ff4c48a2508 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-    turn = Turn.new("Juneau", card)                                          # => #<Turn:0x00007ff4c48a10b8 @string="Juneau", @card=#<Card:0x00007ff4c48a2508 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>>
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    assert_equal "Juneau", turn.guess  # => true
-  end                                  # => :test_it_has_guess
+    assert_equal "Juneau", turn.guess
+  end
 
   def test_if_answer_correct
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007ff4c48a38b8 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-    turn = Turn.new("Juneau", card)                                          # => #<Turn:0x00007ff4c48a3368 @string="Juneau", @card=#<Card:0x00007ff4c48a38b8 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>>
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    assert turn.correct?  # => true
-  end                     # => :test_if_answer_correct
+    assert turn.correct?
+  end
 
   def test_correct_feedback_response
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007ff4c48a9038 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-    turn = Turn.new("Juneau", card)                                          # => #<Turn:0x00007ff4c48a8c00 @string="Juneau", @card=#<Card:0x00007ff4c48a9038 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>>
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    assert turn.correct?                    # => true
-    assert_equal turn.feedback, "Correct!"  # => true
+    assert turn.correct?
+    assert_equal turn.feedback, "Correct!"
 
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  # => #<Card:0x00007ff4c517be40 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-    turn = Turn.new("Bulgogi", card)                                         # => #<Turn:0x00007ff4c517bb20 @string="Bulgogi", @card=#<Card:0x00007ff4c517be40 @question="What is the capital of Alaska?", @answer="Juneau", @category=:Geography>>
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Bulgogi", card)
 
-    refute turn.correct?                      # => false
-    assert_equal turn.feedback, "Incorrect."  # => true
-  end                                         # => :test_correct_feedback_response
-end                                           # => :test_correct_feedback_response
-
-# >> Run options: --seed 24854
-# >>
-# >> # Running:
-# >>
-# >> .....
-# >>
-# >> Finished in 0.001016s, 4921.2607 runs/s, 9842.5214 assertions/s.
-# >>
-# >> 5 runs, 10 assertions, 0 failures, 0 errors, 0 skips
+    refute turn.correct?
+    assert_equal turn.feedback, "Incorrect."
+  end
+end
