1d90421202d4eae146ed5f80c13bd9111ca04334
Aiden Mendez <aidenmendez@gmail.com>
2020-10-05 20:27:53 -0600

    Create card class Include question, answer, and category attributes.

---
+++ lib/card.rb
@@ -0,0 +1,12 @@
+class Card
+  attr_accessor :question, :answer, :category
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category= category
+  end
+
+
+
+end
