7ff0e1f7ef5b9482876be0ee78038eaa56edf4b4
Aiden Mendez <aidenmendez@gmail.com>
2020-10-06 21:40:52 -0600

    Add tests for incorrect guesses

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -12,15 +12,26 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
-  def test_has_guess
+  def test_has_guess_when_correct
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     assert_equal "Juneau", turn.guess
   end
 
-  def test_has_current_card
+  def test_has_guess_when_incorrect
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    assert_equal "Saturn", turn.guess
+  end
+
+  def test_has_card_when_correct
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
+    assert_instance_of Card, turn.card
+  end
+  def test_has_card_when_incorrect
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
     assert_instance_of Card, turn.card
   end
 
@@ -28,5 +39,25 @@ class TurnTest < Minitest::Test
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     assert turn.correct?
+  end
+
+  def test_guess_is_incorrect
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    refute turn.correct?
+  end
+
+  def test_feedback_for_correct_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    turn.correct?
+    assert_equal "Correct!", turn.feedback
+  end
+
+  def test_feedback_for_incorrect_guess
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    turn.correct?
+    assert_equal "Incorrect.", turn.feedback
   end
 end
