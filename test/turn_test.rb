6648ef76a017feed0c9cd6ef5c97bc074a62ef42
Liam Cusack <LCusack8108@gmail.com>
2020-10-07 15:27:21 -0600

    redoing everything with what I learned today

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,36 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/turn'
+require './lib/card'
 
 class TurnTest < Minitest::Test
-
-  def test_it_has_turn #made it more concise, got rid of _a
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneu", card)
-
-    assert_instance_of Card, card
-    assert_instance_of Turn, turn
-  end
-
-  def test_turn_has_card
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneu", card)
-
-    assert_instance_of Card, card
-    assert_equal_to card, turn.card
-  end
-
-  def test_it_has_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneu", card)
-
-    assert_equal_to "Juneu", turn.answer
-  end
-
-
-
-  #def test_it_has_new_answer
-  #  card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-
-  #  assert_equal "Mercury", card2.answer
-end
