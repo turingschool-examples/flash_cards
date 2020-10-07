318ac345bd08b6cd986592034cc03d15a71dbb12
Cydnee Owens <cks410@yahoo.com>
2020-10-07 17:43:32 -0600

    Add setup method to test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,12 +5,15 @@ require "./lib/turn"
 require "./lib/deck"
 
 class DeckTest < Minitest::Test
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+  end
+
   def test_it_has_attributes
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    assert_equal [card_1, card_2, card_3], deck.cards
-    assert_equal 3, deck.count
+    assert_equal [@card_1, @card_2, @card_3], @deck.cards
+    assert_equal 3, @deck.count
   end
