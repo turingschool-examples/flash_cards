d86177f967c2987357019b25b68714092c0f8892
ninesky00 <jiangzho@gmail.com>
2020-10-06 22:22:57 -0500

    add 5 tests to round_test.rb

--- test/round_test.rb
+++ test/round_test.rb
@@ -68,4 +68,47 @@ class RoundTest < Minitest::Test
 
         assert_equal card_2, round.current_card
     end
+
+    def test_can_take_multiple_turns
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+        round.take_turn("Venus")
+
+        assert_equal 2, round.turns.count
+    end
+
+    def test_can_make_incorrect_guesses
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+        round.take_turn("Venus")
+
+        assert_equal "Incorrect.", round.turns.last.feedback
+        assert_equal 1, round.number_correct
+    end
+
+    def test_can_tally_and_calculate_percentages_by_category
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+        round.take_turn("Venus")
+
+        assert_equal 1, round.number_correct_by_category(:Geography)
+        assert_equal 0, round.number_correct_by_category(:STEM)
+        assert_equal 50, round.percent_correct
+        assert_equal 100, round.percent_correct_by_category(:Geography)
+    end
 end
