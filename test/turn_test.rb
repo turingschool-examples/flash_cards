d243fc12db1b3549d71b484b3e2ea62299e7f757
Liam Cusack <LCusack8108@gmail.com>
2020-10-07 12:51:54 -0600

    Add 2 more tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,13 +4,27 @@ require './lib/turn'
 
 class TurnTest < Minitest::Test
 
-  def test_it_has_a_turn
+  def test_it_has_turn #made it more concise, got rid of _a
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     turn = Turn.new("Juneu", card)
 
     assert_instance_of Card, card
     assert_instance_of Turn, turn
+  end
+
+  def test_turn_has_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneu", card)
+
+    assert_instance_of Card, card
+    assert_equal_to card, turn.card
+  end
+
+  def test_it_has_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneu", card)
+
+    assert_equal_to "Juneu", turn.answer
   end
 
 
