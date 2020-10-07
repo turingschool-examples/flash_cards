796c825425b01da80eff7b94505a1b67da47039c
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-06 19:25:04 -0600

    Add method count and cards_in_category

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,19 +5,27 @@ require '../lib/deck'
 
 class DeckTest < Minitest::Test
 
-  def test_can_add_cards
+  def test_it_can_add_cards
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
-
-    assert_equal [card_1, card_2, card_3], deck.add_card(cards)
+    deck = Deck.new(cards)
+    assert_equal [card_1, card_2, card_3], deck.cards
+    assert_equal 3, deck.count
   end
 
+  def test_it_can_find_card_by_category
 
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
+    assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_1], deck.cards_in_category(:Geography)
+    assert_equal [], deck.cards_in_category("Pop Culture")
 
-
-
-
+  end
 end
