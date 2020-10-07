8f247196bcdedc2bf1f2f8b0045cd51fa8b2f9db
Joseph Budina <joebud3@gmail.com>
2020-10-06 22:17:28 -0600

    Add what card is it test method

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -10,9 +10,11 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
-  #test what card it is
+  def test_what_card_it_is
+    turn = Turn.new("Juneau", Card)
+    assert_equal Card, turn.card
+  end
   #test_what_guess_is
   #test_if_correct
   #test_if_theres_feedback
 end
-    # card = card.new("What is the capital of Alaska?", "Juneau", :Geography)
