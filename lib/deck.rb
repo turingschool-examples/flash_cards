6081e021f2cfcd94b172078471a4442876d28c83
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-06 19:27:22 -0600

    Create variable to collect return value for second test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,11 +1,25 @@
 class Deck
+  attr_reader :cards, :count
 
-  def initialize
-    @cards = []
+  def initialize(cards)
+    @cards = cards
   end
 
+  def count
+    cards.count
+  end
 
+  def cards_in_category(category)
 
+    expected = []
+    cards.find_all do |card|
+      if card.category == category
+        expected << card
+      end
+    end
 
+    # expected 
+    # require "pry"; binding.pry
+  end
 
 end
