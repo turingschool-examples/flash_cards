7e4dee6b4632ee58cac0cb323f73d216a40ee7f5
Grayson Myers <graymyers10@gmail.com>
2020-10-06 20:33:24 -0600

    Add round class with initialize method

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,15 @@
 require './card.rb'
 require './turn.rb'
 require './deck.rb'
+
+class Round
+  attr_reader :deck
+  def initialize(deck)
+    @deck = deck
+  end
+
+  def take_turn
+    
+  end
+
+end
