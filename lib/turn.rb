053ef2fa65f713a7ed6613181a907a75d8d75046
Aiden Mendez <aidenmendez@gmail.com>
2020-10-06 19:32:15 -0600

    Create four tests in turn test file Create turn class and 'correct?' method

---
+++ lib/turn.rb
@@ -0,0 +1,21 @@
+class Turn
+  attr_reader :guess, :card, :feedback
+
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+    @feedback = nil
+  end
+
+  def correct?
+    if guess == card.answer
+      @feedback = "Correct!"
+      true
+    elsif guess != card.answer
+      @feedback = "Incorrect."
+      false
+    end
+  end
+
+end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,8 +1,32 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/turn'
+require './lib/card'
 
 class TurnTest < Minitest::Test
 
   def test_it_exists
-    turn = Turn.new()
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
+  end
+
+  def test_has_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_has_current_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_instance_of Card, turn.card
+  end
+
+  def test_guess_is_correct
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert turn.correct?
+  end
+end
