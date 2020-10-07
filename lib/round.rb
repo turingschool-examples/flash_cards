004d90b3d020326a2397f08de0c54aee0a913e6b
Phil McCarthy <philmcc2631@gmail.com>
2020-10-06 21:31:09 -0600

    Add take_turn functionality to round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,10 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
   end
 
   def current_card
@@ -11,6 +12,17 @@ class Round
   end
 
   def take_turn(guess)
-    Turn.new(guess, current_card)
+    turn = Turn.new(guess, current_card)
+    @turns << turn
+    if turn.correct?
+      @deck.cards.shift
+      add_number_correct
+    else @deck.cards.rotate!
+    end
+    return turn
+  end
+
+  def add_number_correct
+    @number_correct += 1
   end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -48,4 +48,26 @@ class RoundTest < Minitest::Test
     assert_instance_of Turn, new_turn
     assert new_turn.correct?
   end
+
+  def test_deck_shifts_after_new_turn_is_correct_and_stores_turns
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    assert_equal card_2, round.current_card
+    assert_equal 2, round.deck.cards.length
+    assert_includes round.turns, new_turn
+  end
+
+  def test_correct_answer_count_increments
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    assert_equal 1, round.number_correct
+  end
 end
