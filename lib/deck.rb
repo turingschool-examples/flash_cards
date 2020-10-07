39510b7c1df29d4fae164f64771bb914fe24edc0
awosborn <alanturing@example.coclear>
2020-10-07 16:57:51 -0600

    Add deck_test

---
+++ lib/deck.rb
@@ -0,0 +1,22 @@
+class Deck
+attr_reader :cards, :count
+
+  def initialize(cards = [])
+    @cards = cards
+    @round = card
+  end
+
+  def count
+    @cards.length
+  end
+
+  def cards_in_category(category)
+
+    expected = []
+    cards.find_all do |card|
+      if card.category == category
+        expected << card
+      end
+    end
+end 
+end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -3,3 +3,6 @@ require 'minitest/pride'
 require './lib/turn'
 require './lib/card'
 require './lib/deck'
+
+
+class TurnTest < Minitest::Test
