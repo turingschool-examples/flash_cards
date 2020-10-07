5e5397d2d26169d49afdd5d80b96eb415b583dae
Jordan Beck <jordanfbeck@gmail.com>
2020-10-07 09:39:57 -0600

    Update flashcard project

--- test/card_test.rb
+++ test/card_test.rb
@@ -9,22 +9,23 @@ class CardTest < Minitest::Test
 
     assert_instance_of Card, card
   end
-  # 
-  # def test_it_has_a_question
-  #   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #
-  #   assert_equal "What is the capital of Alaska?", card.question
-  # end
-  #
-  # def test_it_has_an_answer
-  #   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #
-  #   assert_equal "Juneau", card.answer
-  # end
-  #
-  # def test_it_has_a_category
-  #   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #
-  #   assert_equal :Geography, card.category
-  # end
+
+
+  def test_it_has_a_question
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    assert_equal "What is the capital of Alaska?", card.question
+  end
+
+  def test_it_has_an_answer
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    assert_equal "Juneau", card.answer
+  end
+
+  def test_it_has_a_category
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    assert_equal :Geography, card.category
+  end
 end
