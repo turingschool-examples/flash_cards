8097a065a63cc74e03a3bc1aa07d888013df982c
Aiden Mendez <aidenmendez@gmail.com>
2020-10-07 17:33:13 -0600

    Add current_card functionality and test it

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,13 @@
 class Round
-attr_reader :deck, :turns
+attr_reader :deck, :turns, :card_tracker
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @card_tracker = 0
+  end
+
+  def current_card
+    deck.cards[card_tracker]
   end
 end
