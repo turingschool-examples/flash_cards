ef88582d92666aaf34908436da9e62ca7cdeef9e
Max Ribbans <ribbans.max@gmail.com>
2020-10-06 20:54:34 -0400

    Add correct? method and tested

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -9,4 +9,25 @@ class TurnTest < Minitest::Test
     turn = Turn.new("guess", card)
     assert_instance_of Turn, turn
   end
+
+  def test_it_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card)
+
+    assert_equal "guess", turn.guess
+  end
+
+  def test_it_has_a_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card)
+
+    assert_equal card, turn.card
+  end
+
+  def test_it_has_correct?
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card)
+
+    assert_equal false, turn.correct?
+  end 
 end
