d5e9511ba7b4f82b0ba05146971f54fd515f44ac
ninesky00 <jiangzho@gmail.com>
2020-10-06 22:16:31 -0500

    add 3 more tests to round_test.rb

--- test/round_test.rb
+++ test/round_test.rb
@@ -53,6 +53,19 @@ class RoundTest < Minitest::Test
         new_turn = round.take_turn("Juneau")
         
         assert_equal "Juneau", new_turn.guess
+        assert_equal true, new_turn.correct?
+        assert_equal 1, round.number_correct
     end
  
+    def test_card_changes_after_turn
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+
+        assert_equal card_2, round.current_card
+    end
 end
