f5334953b08f280be42f74ea70feefb64966c86c
ninesky00 <jiangzho@gmail.com>
2020-10-06 21:14:36 -0500

    change initialize attributes

--- lib/round.rb
+++ lib/round.rb
@@ -1,10 +1,10 @@
 class Round
-    attr_reader :deck 
-    attr_accessor :turn, :number_correct
+    attr_reader :deck, :turns, :correct_by_category
     def initialize(deck)
         @deck = deck
         @turns = []
-        @number_correct = 0
+        @correct_by_category = []
+        @turns_by_category = []
     end
     
     def current_card
@@ -12,13 +12,26 @@ class Round
     end
 
     def take_turn(guess)
-        @number_correct += 1  if guess == current_card.answer
+       if guess == current_card.answer
+        @correct_by_category << current_card.category  
+       end
         @turns << Turn.new(guess, current_card)
+        @turns_by_category << current_card.category
         @turns.last
     end
 
-    def turns 
-        @turns
+    def number_correct
+        @correct_by_category.count
     end
 
+    def number_correct_by_category(category)
+        @correct_by_category.count(category)
+    end
+
+    def percent_correct
+        (@correct_by_category.count * 100 / @turns.count).round(2)
+    end
+    
+    def percent_correct_by_category(category)
+        (@correct_by_category.count(category) * 100 / @turns_by_category.count(category)).round(2)
 end
