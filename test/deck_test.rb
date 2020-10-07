76afdf665d91687b4b8f8fbea41abee95a32d39a
Sam Yeo <yeosamuel95@gmail.com>
2020-10-06 21:05:36 -0700

    Add Incorrect section to deck_test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -58,4 +58,30 @@ class DeckTest < MiniTest::Test
 
   # Incorrects
 
+  def test_deck_incorrect_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
+      reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° cloc
+      kwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    refute_equal [card_2, card_3], deck.cards_in_category(:Geography)
+  end
+
+  def test_deck_count
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
+      reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° cloc
+      kwise from due north?", "North north west", :STEM)
+    card_4 = Card.new("What is the proper name for tradition Japanese tattoos?",
+       "Horimono", :Interest)
+    cards = [card_1, card_2, card_3, card_4]
+    deck = Deck.new(cards)
+
+    refute_equal 3, deck.count
+  end
+
 end
