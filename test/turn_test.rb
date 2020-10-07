1bedfac58e4a406cf7dcd8f358ad330c5d5c0e1f
Liam Cusack <LCusack8108@gmail.com>
2020-10-07 15:31:25 -0600

    Built turn_test.rb test 1

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,3 +4,12 @@ require './lib/turn'
 require './lib/card'
 
 class TurnTest < Minitest::Test
+
+  def test_it_has_turn
+    card = Card.new("What is the capital of Alaska?", "Juneu", :Geography)
+    turn = Turn.new("Junea", card)
+
+    assert_instance_of Card, card
+    assert_instance_of Turn, turn
+  end
+end
