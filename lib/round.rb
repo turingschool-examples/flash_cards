3ded7257d3e675a55d59738f15b462be5cdfae68
Grayson Myers <graymyers10@gmail.com>
2020-10-06 21:15:57 -0600

    Add take_turn and current_card methods to round class

--- lib/round.rb
+++ lib/round.rb
@@ -3,13 +3,20 @@ require './lib/turn.rb'
 require './lib/deck.rb'
 
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turn
   def initialize(deck)
     @deck = deck
+    @turns = []
   end
 
-  def take_turn
+  def current_card
+    @deck.cards[0]
+  end
 
+  def take_turn(guess)
+    turn = Turn.new(guess,@deck.cards.shift)
+    @turns << turn
+    turn
   end
 
 end
