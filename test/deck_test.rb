cf43b824c3dfa520b29f2de03d1e29621cff6cd5
Max Ribbans <ribbans.max@gmail.com>
2020-10-06 23:28:01 -0400

    Add Deck and test it exists

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,5 +5,11 @@ require './lib/turn'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
+  def test_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    cards = [card]
+    deck = Deck.new(cards)
+    assert_instance_of Deck, deck
+  end 
 
-end 
+end
