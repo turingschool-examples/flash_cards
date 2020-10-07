2443fb7565d5b0a72aacae8f562bc0894dff1dff
Ely Hess <ely.hess@me.com>
2020-10-07 17:49:33 -0600

    Add require file paths

---
+++ test/round_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+
+
+class Test < Minitest::Test
+  def test_it_exists_and_has_attributes
+  end
+end
