31469467e78a3786e160aa656ed667158d0aa844
Joseph Budina <joebud3@gmail.com>
2020-10-07 20:42:18 -0600

    Add cards_in_catogory method and test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,12 @@
 class Deck
-
+  attr_reader :cards, :count, :catogory
   def initialize(cards)
+    @cards = cards
+    @count = cards.count
+    @catogory = catogory
+  end
 
+  def cards_in_catogory(type)
+    @category = type
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -16,4 +16,40 @@ class DeckTest < Minitest::Test
 
     assert_instance_of Deck, deck
   end
+
+  def test_it_has_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal cards, deck.cards
+  end
+
+  def test_how_many_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+  end
+
+  def test_cards_in_what_catogory
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_equal :STEM, deck.cards_in_catogory(:STEM)
+  end
 end
