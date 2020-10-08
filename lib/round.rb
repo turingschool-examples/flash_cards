174edd179dfd8c8ac9b5f2ae8309bbae5941502f
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-08 08:12:11 -0700

    added correct/percent by category to round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -5,71 +5,72 @@ class Round
               :number_correct,
               :guesses,
               :new_guess,
-              :turns
-
-
+              :turns,
+              :total_cards,
+              :total_vocabulary,
+              :correct_vocabulary,
+              :total_trivia,
+              :correct_trivia
   def initialize(deck)
     @deck = deck
     @current_card = 0
     @total_cards = 4
+    @correct_vocabulary = []
+    @total_vocabulary = 3
+    @correct_trivia = []
+    @total_trivia = 1
     @guesses = []
     @number_correct = 0
     @turns = []
     @new_guess = new_guess
     @category_correct = []
   end
-
   def total_cards
     @total_cards
   end
-
   def count
     deck.cards.count
   end
-
   def current_card
     deck.cards[@current_card]
   end
-
   def percent_correct
     (@number_correct.to_f / @total_cards.to_f) * 100
   end
-
-  def card_by_category(category_name)
-     deck.cards.select do |card|
-      card.category == category_name
+  def number_correct_by_category(category)
+    if category == :Vocabulary
+      @correct_vocabulary.count
+    elsif category == :Trivia
+      @correct_vocabulary.count
     end
   end
-
-  def number_correct_by_category(category_name)
-      deck.collect do |card|
-      card.category == category_name
+  def percent_correct_by_category(category)
+    if category == :Vocabulary
+      (@correct_vocabulary.count.to_f / @total_vocabulary.to_f) * 100
+    elsif category == :Trivia
+      (@correct_vocabulary.count.t_f / @total_vocabulary.to_f) * 100
     end
   end
-
-  def category_correct
-    @category_correct.count
-  end
-
   def take_turn(guess)
     @new_guess = Turn.new(guess, current_card)
     @guesses << @new_guess
     @new_guess
   end
-
   def next_card
     if @new_guess.correct?
       @number_correct += 1
+      if new_guess.card.category == :Vocabulary
+        @correct_vocabulary << @new_guess
+      elsif new_guess.card.category == :Trivia
+        @correct_trivia << @new_guess
+      end
       @turns << @new_guess
     end
     @current_card += 1
   end
-
   def start
-
     p "Welcome! You're playing with 4 cards"
     p "---------------------------------------------"
-
     deck.cards.each do |card|
     p "This is card number #{@current_card + 1} out of 4."
     p card.question
@@ -77,12 +78,13 @@ class Round
     take_turn(answer)
     next_card
     sleep(1)
-    p "Storing Answer"
+    p "Checking Answer"
     sleep(1)
+    p @guesses.last.feedback
     end
     p "************ GAME OVER ************"
     p "You had #{@number_correct} correct guesses out of 4 for a total score of #{percent_correct}%"
-
+    p "In Vocabulary, you had #{number_correct_by_category(:Vocabulary)} out of 3, for a score of #{percent_correct_by_category(:Vocabulary)}"
+    p "In Trivia, you had #{number_correct_by_category(:Trivia)} out of 1, for a score of #{percent_correct_by_category(:Trivia)}"
   end
-
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -4,21 +4,7 @@ require './lib/turn'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
-
-
 class RoundTest < MiniTest::Test
-
-# def setup
-#
-#   card_1 = Card.new("What is a name that associates with a particular object?", "Variable", :Vocabulary)
-#   card_2 = Card.new("Behavior an object can do is a?", "Method", :Vocabulary)
-#   card_3 = Card.new("What is an ordered, integer-indexed collection of any object?", "Array", :Vocabulary)
-#   card_4 = Card.new("What is my sons Favorite Movie?", "Iron Giant", :Trivia)
-#
-#   deck = Deck.new([card_1, card_2, card_3, card_4])
-#   round = Round.new(deck)
-# end
-
 def test_it_exists
     card_1 = Card.new("What is a name that associates with a particular object?", "Variable", :Vocabulary)
     card_2 = Card.new("Behavior an object can do is a?", "Method", :Vocabulary)
@@ -32,18 +18,16 @@ def test_it_exists
     round.next_card
     new_turn = round.take_turn("WrongMethod")
     round.next_card
+
+
     new_turn = round.take_turn("ARRAY")
     round.next_card
-    new_turn = round.take_turn("IRON GIANT")
+    new_turn = round.take_turn("IRON")
     round.next_card
 
 
     require "pry"; binding.pry
 
-
-  assert_instance_of Round, round
-
-  new_turn = round.take_turn("Juneau")
 
 end
 
