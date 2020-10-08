4af27ecc0e51cb65872b2207a70ad14a9cdb00f6
Ely Hess <ely.hess@me.com>
2020-10-07 18:29:33 -0600

    Add calulcation methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,9 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :correct
 
   def initialize(deck)
     @turns = []
+    @correct = 0
     @deck = deck
   end
 
@@ -17,5 +18,25 @@ class Round
       @correct += 1
     end
     return new_turn
+  end
+
+  def number_correct
+    @correct
+  end
+
+  def number_correct_by_category(category)
+    @turns.count do |turn|
+      if turn.correct? && turn.card.category == category
+        turn.card.category
+      end
+    end
+  end
+
+  def percent_correct
+     number_correct.to_f / @turns.count * 100
+  end
+
+  def percent_correct_by_category(category)
+    number_correct_by_category(category).to_f / number_correct * 100
   end
 end
