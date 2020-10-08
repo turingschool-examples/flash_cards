a0f5048596c922ff8d7f5191b5997968eb029d31
Catherine Dean <catherinemdean15@gmail.com>
2020-10-08 12:54:45 -0600

    add test_it_has_cards method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,12 +1,19 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
+require './lib/deck'
 
 class DeckTest < Minitest::Test
 
 
   def test_it_has_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card 1, card 2, card 3]
     deck = Deck.new(cards)
-    
-    assert_equal "card", turn.card
+
+    assert_equal "cards", Deck.cards
   end
+
+end
