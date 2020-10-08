6f7e3cb2a11c57631ce8d8d843b464650110cdf3
Max Ribbans <ribbans.max@gmail.com>
2020-10-07 20:01:15 -0400

    Add count and cards_in_category methods for deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,22 @@ class Deck
   def initialize(cards = [])
     @cards = cards
   end
+
+  def count
+    ticker = 0
+    @cards.each do |card|
+      ticker += 1
+    end
+    ticker
+  end
+
+  def cards_in_category(category)
+    cards_in_category = []
+    @cards.each do |card|
+      if card.category == category
+        cards_in_category << card
+      end
+    cards_in_category
+    end
+  end
 end
