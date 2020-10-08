22d0da2fd9ef1c96044897205a1184f8124e4608
ninesky00 <jiangzho@gmail.com>
2020-10-08 17:48:30 -0500

    refactor cards_generator and cards.txt for string inputs

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -12,7 +12,7 @@ class CardGenerator
         File.open("./lib/#@filename").map do |line|
             line = line.chomp
             card_parts = line.split","
-            Card.new(card_parts[0], card_parts[1], card_parts[2].to_sym)
+            Card.new(card_parts[0].to_s, card_parts[1].to_s, card_parts[2].to_sym)
         end
     end
 end
