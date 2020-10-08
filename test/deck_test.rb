e7bba5ef3e65b4595ac2cad6e91ec7e034eeaa0b
awosborn <alanturing@example.coclear>
2020-10-08 14:59:59 -0600

    Add round.rb and round_test.rb

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,3 +6,37 @@ require './lib/deck'
 
 
 class TurnTest < Minitest::Test
+
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+
+  end
+
+  def test_cards_in_deck
+    # skip
+
+    assert_equal @cards, @deck.cards
+  end
+
+  def test_card_count
+    # skip
+
+    assert_equal 3, @deck.count
+
+  end
+
+  def test_cards_in_category
+    # skip
+
+
+    assert_equal [@card_2,@card_3], @deck.cards_in_category(:STEM)
+    assert_equal [@card_1], @deck.cards_in_category(:Geography)
+    assert_equal [], @deck.cards_in_category("Pop Culture")
+  end
+
+end
