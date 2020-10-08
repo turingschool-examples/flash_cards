a13d2b5d0300382041a349df95e69382f5fb9fb1
Adam Etzion <adametzion90@gmail.com>
2020-10-08 15:19:39 -0600

    Run test_deck_has_correc_count successfully

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -42,7 +42,7 @@ class DeckTest < Minitest::Test
   end
 
   def test_deck_has_correct_count
-    skip
+    # skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
