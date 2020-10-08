391d3b51e809bc8a32a359b6a0b2e30a99cb7525
Ely Hess <ely.hess@me.com>
2020-10-08 15:09:36 -0600

    Rolocate runner to start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,38 +3,11 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-card_1 = Card.new("What are acceptable reasons for discharge or transfer?", "Medical reasons", :CLIENT)
-card_2 = Card.new("Patients have the right to be free from restraints unless what is obtained?", "Authorized order", :CLIENT)
-card_3 = Card.new("the ability to understand the feelings of another without pitying them", "Empathy", :COMMUNICATION)
-card_4 = Card.new("The “Resident’s Bill of Rights” was put out by which group?", "AHA", "Ethical Behavior")
+card_1 = Card.new("What are acceptable reasons for discharge or transfer?", "1", :CLIENT)
+card_2 = Card.new("Patients have the right to be free from restraints unless what is obtained?", "2", :CLIENT)
+card_3 = Card.new("the ability to understand the feelings of another without pitying them", "3", :COMMUNICATION)
+card_4 = Card.new("The “Resident’s Bill of Rights” was put out by which group?", "4", "Ethical Behavior")
 cards = [card_1, card_2, card_3, card_4]
 deck = Deck.new(cards)
 round = Round.new(deck)
-
-
-total_cards = round.turns.count + cards.count
-total_rounds = round.deck.cards + round.turns
-
-cats = round.deck.cards.map do |card|
-  card.category
-end.uniq
-
-
-
-puts "Welcome! You're playing with #{total_cards} cards."
-puts '-' * 51
-
-num = 0
-total_rounds.each do |card|
-  puts "This card is number #{num += 1} out of #{total_cards}"
-  puts "Question: #{card.question}"
-  user_answer = gets.chomp
-  guess = round.take_turn(user_answer)
-  puts guess.feedback
-end
-
-puts "****** Game Over! ******"
-puts "You had #{round.correct} correct guesses out of #{total_cards} for a total score of #{(round.percent_correct).to_i}%"
-cats.each do |cat|
-  puts "#{cat} - #{(round.percent_correct_by_category(cat)).to_i}%"
-end
+round.start
