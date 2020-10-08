41123793daeaab066051fea692d4be803eda59d1
Catherine Dean <catherinemdean15@gmail.com>
2020-10-08 17:20:03 -0600

    add cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,4 +11,11 @@ class Deck
     @cards.count
   end
 
+  def cards_in_category(input_category)
+    @cards.find_all do |card|
+    card.category == input_category
+    end
+    # require "pry"; binding.pry
+  end
+
 end
