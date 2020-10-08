7a8127bc5a8f84388193e65bb5bfbe1b137f12c3
Liam Cusack <LCusack8108@gmail.com>
2020-10-08 15:54:33 -0600

    All tests pass, Iteration 2 deck.rb finished

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -46,5 +46,7 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
 
     assert_equal [card_2, card_3], deck.cards_in_category(:STEM)
+    assert_equal [card_1], deck.cards_in_category(:Geography)
+    assert_equal [], deck.cards_in_category("Pop Culture")
   end
 end
