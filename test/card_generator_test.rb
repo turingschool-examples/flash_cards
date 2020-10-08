4dc0bc714ef0c480432632e8b583f9fe1b6d5b8c
Aiden Mendez <aidenmendez@gmail.com>
2020-10-08 14:15:34 -0600

    Set up test class and create a text doc for cards

---
+++ test/card_generator_test.rb
@@ -0,0 +1,10 @@
+require "Minitest/autorun"
+require "Minitest/pride"
+require "./lib/card_generator"
+
+filename = "cards.txt"
+
+class CardGeneratorTest < Minitest::Test
+  # def test_it_exists
+
+end
