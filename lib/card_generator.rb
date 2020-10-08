ca65dfb59aad7ec70570a72e664f48f6a798e90b
ninesky00 <jiangzho@gmail.com>
2020-10-08 15:42:38 -0500

    create CardGenerator class to open cards.txt

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,6 +1,15 @@
+require './lib/card'
+
 class CardGenerator
     attr_reader :filename
+
     def initialize(filename)
     @filename = filename    
+    end
+
+    def cards
+        File.open("./lib/#@filename").each do |line|
+            card_1 = Card.new(line)
+        end
     end
 end
