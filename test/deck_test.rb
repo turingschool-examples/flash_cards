af6219731142a05f07fdf3e51a1de691bb7c0263
Liam Cusack <LCusack8108@gmail.com>
2020-10-08 14:50:23 -0600

    Wrote deck_test.rb test 2 Code success test 2

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -9,17 +9,22 @@ class DeckTest < Minitest::Test
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
     assert_instance_of Card, card_1
     assert_instance_of Card, card_2
     assert_instance_of Card, card_3
+    assert_instance_of Deck, deck
   end
 
   def test_there_are_cards
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
-    assert_equal [cards_1, card_2, card_3], cards
+    assert_equal deck.cards, cards
   end
 end
