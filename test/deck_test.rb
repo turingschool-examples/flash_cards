44243ab7981bbfc7ac890db72d07ffec2071a78b
Liam Cusack <LCusack8108@gmail.com>
2020-10-07 18:26:42 -0600

    Wrote deck_test.rb test 2

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,4 +14,12 @@ class DeckTest < Minitest::Test
     assert_instance_of Card, card_2
     assert_instance_of Card, card_3
   end
+
+  def test_there_are_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    assert_equal [cards_1, card_2, card_3], cards
+  end
 end
