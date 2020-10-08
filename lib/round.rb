7664bcbb4ed12fc8d1a6aab32174e798870e20e5
Phil McCarthy <philmcc2631@gmail.com>
2020-10-07 18:16:20 -0600

    Add progress on iteration 3

--- lib/round.rb
+++ lib/round.rb
@@ -1,12 +1,14 @@
 class Round
   attr_reader :deck,
               :turns,
+              :turn,
               :number_correct,
               :number_incorrect
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @turn = nil
     @number_correct = 0
     @number_incorrect = 0
   end
@@ -16,14 +18,19 @@ class Round
   end
 
   def take_turn(guess)
-    turn = Turn.new(guess, current_card)
+    @turn = Turn.new(guess, current_card)
     @turns << turn
-    if turn.correct?
-      add_number_correct
-    else add_number_incorrect
-    end
+    process_guess(turn)
     @deck.cards.rotate!
     return turn
+  end
+
+  def process_guess(turn)
+    if turn.correct?
+      add_number_correct
+    else
+      add_number_incorrect
+    end
   end
 
   def add_number_correct
@@ -54,4 +61,24 @@ class Round
     (number_correct_by_category(category).to_f / turns_by_category(category).length.to_f) * 100
   end
 
+  def start
+    puts "Welcome! You're playing with #{@deck.cards.length} cards."
+    puts "-------------------------------------------------"
+    puts "This is card number #{@turns.length + 1} out of #{@deck.cards.length}."
+    puts "Question: #{current_card.question}"
+    print "> "
+    guess = gets.chomp
+    take_turn(guess)
+    until @turns.length == @deck.cards.length
+      puts "This is card number #{@turns.length + 1} out of #{@deck.cards.length}."
+      puts "Question: #{current_card.question}"
+      print "> "
+      guess = gets.chomp
+      take_turn(guess)
+    end
+    puts "****** Game over! ******"
+    puts "You had #{@number_correct} correct guesses out of #{@deck.cards.length} for a total score of #{percent_correct}%"
+    puts "STEM - #{percent_correct_by_category(:STEM)}% correct"
+    puts "Geography - #{percent_correct_by_category(:Geography)}% correct"
+  end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -71,7 +71,7 @@ class RoundTest < Minitest::Test
     assert_equal 1, round.number_correct
   end
 
-  def test_second_guess_added_to_turns_array
+  def test_second_guess_adds_to_turns_array
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
