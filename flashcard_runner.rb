41bfef9cb79737d4618a977e9e80eaa1bcab39c1
ninesky00 <jiangzho@gmail.com>
2020-10-08 17:43:36 -0500

    refactor start method iterations

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -12,29 +12,36 @@ require './lib/card_generator'
 
         puts "Welcome! You're playing with #{cards.count} cards."
         puts "--------------------------------------------------"
+        #implement deck.count.times do following
+        deck.count.times do 
         puts "This is card number #{round.turns.count + 1} out of #{cards.count}."
         puts round.current_card.question
         guess = gets.chomp.to_s
         new_turn = round.take_turn(guess)
         puts new_turn.feedback
+        end
 
-        puts "This is card number #{round.turns.count + 1} out of #{cards.count}."
-        puts round.current_card.question
-        guess2 = gets.chomp.to_s
-        new_turn = round.take_turn(guess2)
-        puts new_turn.feedback
+        # puts "This is card number #{round.turns.count + 1} out of #{cards.count}."
+        # puts round.current_card.question
+        # guess2 = gets.chomp.to_s
+        # new_turn = round.take_turn(guess2)
+        # puts new_turn.feedback
 
-        puts "This is card number #{round.turns.count + 1} out of #{cards.count}."
-        puts round.current_card.question
-        guess3 = gets.chomp.to_s
-        new_turn = round.take_turn(guess3)
-        puts new_turn.feedback
+        # puts "This is card number #{round.turns.count + 1} out of #{cards.count}."
+        # puts round.current_card.question
+        # guess3 = gets.chomp.to_s
+        # new_turn = round.take_turn(guess3)
+        # puts new_turn.feedback
 
         puts "----------------- Game Over! ------------------"
         puts "You had #{round.number_correct} out of #{cards.count} for a total of #{round.percent_correct}%."
-        puts "#{cards[0].category} - #{round.percent_correct_by_category(cards[0].category)} % correct"
-        puts "#{cards[1].category} - #{round.percent_correct_by_category(cards[1].category)} % correct"
-        puts "#{cards[2].category} - #{round.percent_correct_by_category(cards[2].category)} % correct"
+        #iterate below
+        cards.each do |card|
+            puts "#{card.category} - #{round.percent_correct_by_category(card.category)} % correct"
+        end
+        # puts "#{cards[0].category} - #{round.percent_correct_by_category(cards[0].category)} % correct"
+        # puts "#{cards[1].category} - #{round.percent_correct_by_category(cards[1].category)} % correct"
+        # puts "#{cards[2].category} - #{round.percent_correct_by_category(cards[2].category)} % correct"
     end
 
     start
