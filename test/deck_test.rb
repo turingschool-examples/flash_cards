c8b14fd3fbeeeaccba3111d471da88fcf01165be
Adam Etzion <adametzion90@gmail.com>
2020-10-08 15:20:28 -0600

    Run test_deck_has_correct_cars_in_[catgeory] successfully 3 separate tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -53,7 +53,7 @@ class DeckTest < Minitest::Test
   end
 
   def test_deck_has_correct_cards_in_STEM
-    skip
+    # skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -64,7 +64,7 @@ class DeckTest < Minitest::Test
   end
 
   def test_deck_has_correct_cards_in_Geography
-    skip
+    # skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -75,7 +75,7 @@ class DeckTest < Minitest::Test
   end
 
   def test_deck_has_correct_cards_in_PopCulture
-    skip
+    # skip
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
