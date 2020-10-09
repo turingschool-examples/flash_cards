cf26937a6e47fc07385fe8b0769a5cf4e6056ffa
ninesky00 <jiangzho@gmail.com>
2020-10-08 22:06:31 -0500

    add aesthetics to center text

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -10,29 +10,28 @@ require './lib/card_generator'
         deck = Deck.new(cards)
         round = Round.new(deck)
 
-        puts "Welcome! You're playing with #{cards.count} cards."
-        puts "You can add or remove cards through cards.txt file."
-        puts "--------------------------------------------------"
-        puts "          Press enter to begin"
+        puts "Welcome! You're playing with #{cards.count} cards.".center(60)
+        puts "You can add or remove cards through cards.txt file.".center(60)
+        puts "-".center(60, "-")
+        puts "Press enter to begin".center(60)
         gets.chomp
 
         deck.count.times do 
-        puts "This is card number #{round.turns.count + 1} out of #{cards.count}."
-        puts round.current_card.question
+        puts "This is card number #{round.turns.count + 1} out of #{cards.count}.".center(60)
+        puts round.current_card.question.center(60)
         guess = gets.chomp.to_s
         new_turn = round.take_turn(guess)
         puts
-        puts new_turn.feedback
-        puts "Press enter to continue"
+        puts new_turn.feedback.center(60)
+        puts "Press enter to continue".center(60)
         gets.chomp
         end
 
-        puts "----------------- Game Over! ------------------"
-        puts "            Thank you for playing"
-        puts "You had #{round.number_correct} out of #{cards.count} for a total of #{round.percent_correct}%."
-        #iterate below
+        puts "Game Over!".center(60, "-")
+        puts "Thank you for playing".center(60)
+        puts "You had #{round.number_correct} out of #{cards.count} for a total of #{round.percent_correct}%.".center(60)
         cards.each do |card|
-            puts "#{card.category} - #{round.percent_correct_by_category(card.category)} % correct"
+            puts "#{card.category} - #{round.percent_correct_by_category(card.category)} % correct".center(60)
         end
     end
 
