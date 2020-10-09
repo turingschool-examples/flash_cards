7addf74eb0565a24a753f5d5882d0ca8ad013943
Catherine Dean <catherinemdean15@gmail.com>
2020-10-09 14:40:37 -0600

    dry up code in turn class

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,6 +8,8 @@ class TurnTest < Minitest::Test
   def setup
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @turn = Turn.new("Juneau", @card)
+    @card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    @turn2 = Turn.new("Saturn", @card2)
   end
 
   def test_it_exists
@@ -16,19 +18,23 @@ class TurnTest < Minitest::Test
 
   def test_it_has_a_guess
     assert_equal "Juneau", @turn.guess
+    assert_equal "Saturn", @turn2.guess
   end
 
   def test_it_has_a_card
     assert_equal @card, @turn.card
+    assert_equal @card2, @turn2.card
   end
 
 
-  def test_it_has_checks_correct?
+  def test_it_checks_correct?
     assert_equal true, @turn.correct?
+    assert_equal false, @turn2.correct?
   end
 
   def test_it_gives_feedback
     assert_equal "Correct!", @turn.feedback
+    assert_equal "Incorrect!", @turn2.feedback
   end
 
 end
