a680322486701b2e10d6f80e11b728c9e6dbceac
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-08 21:13:50 -0500

    Add tests and methods for Turn Class.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,20 @@
 class Turn
-  def initialize
-
+  attr_reader :guess, :card
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
   end
+
+def correct?
+  card.answer == guess
+end
+
+def feedback
+  if correct? == true
+    "Correct!"
+  else
+    "Incorrect!"
+  end
+end
+
 end
