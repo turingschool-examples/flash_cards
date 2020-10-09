bb663a045fd7d201805f6b3e8aebb592af3db7a0
Jordan Beck <jordanfbeck@gmail.com>
2020-10-08 22:12:47 -0600

    Add storing cards in a deck work

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,17 @@
 class Deck
+  attr_reader :cards
 
-def initialize(cards)
-@cards = []
+  def initialize(cards)
+    @cards = cards
+  end
 
-end
+  def count
+    cards.count
+  end
 
+  def cards_in_category(category)
+    cards.select do |card|
+      card.category == category
+    end
+  end
 end
