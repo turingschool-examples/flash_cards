ab56b98ff5f19aef7b700ee9ce26cdb9dce06615
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-09 09:01:31 -0500

    Clean and refractor code for Card class and test.

--- test/card_test.rb
+++ test/card_test.rb
@@ -5,29 +5,22 @@ require './lib/card'
 class CardTest < Minitest::Test
 
   def test_it_exists
-
-    require 'pry'; binding.pry
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     assert_instance_of Card, card
   end
 
   def test_it_has_a_question
-    skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     assert_equal "What is the capital of Alaska?", card.question
   end
 
   def test_it_has_an_answer
-    skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     assert_equal "Juneau", card.answer
   end
 
   def test_it_has_a_category
-    skip
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     assert_equal :Geography, card.category
   end
 end
