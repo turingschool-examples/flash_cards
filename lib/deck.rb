973920ceb5dde7053c25af3a19354f8cd6b887d5
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-09 18:20:28 -0500

    Add test_it_can_hold_cards and test_it_can_count_cards methods.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,14 @@
 class Deck
-attr_reader :card
+attr_reader :card, :cards, :deck
 
-  def initialize(card)
+  def initialize(cards)
     @card = card
+    @cards = cards
+    @deck = deck
   end
+
+  def count
+    cards.length
+  end
+
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,11 +2,49 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
+require 'pry'
 
 class DeckTest < Minitest::Test
   def test_it_exists
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_instance_of Deck, deck
   end
+
+  def test_it_can_hold_cards
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal cards, deck.cards
+  end
+
+  def test_it_can_count_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal 3, deck.count
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
