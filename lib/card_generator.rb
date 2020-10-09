de2345daa108b68423dbb2f4bdfd0c36e7a689e3
Phil McCarthy <philmcc2631@gmail.com>
2020-10-09 12:43:46 -0600

    Add card_generator methods to process cards.txt and create a deck of cards

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,11 +1,23 @@
 class CardGenerator
+  attr_reader :deck
+
   def initialize(filename)
     @filename = filename
-    @cards = []
+    @card_data = []
+    @deck = Deck.new([])
   end
 
   def process_cards_file
-    cards_data = File.open(@filename)
-    @cards = cards_data.readlines.map(&:chomp)
+    card_data = File.open(@filename)
+    @card_data = card_data.readlines.map(&:chomp)
+  end
+
+  def cards
+    process_cards_file
+    @card_data.each do |card|
+      card = card.split(',')
+      @deck.cards << Card.new(card[0], card[1], card[2])
+    end
+    @deck
   end
 end
