9a8faa255a72fb97d322e38a568139d0de3de572
Grayson Cunningham <gecun289@gmail.com>
2020-10-09 22:02:24 -0600

    revise flashcards runner to include card generator

--- flashcards_runner.rb
+++ flashcards_runner.rb
@@ -2,19 +2,23 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 
-@card_1 = Card.new("Which NFL position has, on average, the shortest career?", "running back", :Sports)
-@card_2 = Card.new("Where were the Utah Jazz originally located?", "new orleans", :Sports)
-@card_3 = Card.new("What year was the World Wide Web launched in?", "1991", :STEM)
-@card_4 = Card.new("What animal has the highest blood pressure?", "giraffe", :STEM)
-@card_5 = Card.new("Which Queen of England and wife of Henry VIII had six fingers on one of her hands?", "anne boleyn", :History)
-@card_6 = Card.new("What was the former name of Times Square before it was renamed in 1904?", "longacre square", :History)
-@deck = Deck.new([@card_1, @card_2, @card_3, @card_4, @card_5, @card_6])
+# @card_1 = Card.new("Which NFL position has, on average, the shortest career?", "running back", :Sports)
+# @card_2 = Card.new("Where were the Utah Jazz originally located?", "new orleans", :Sports)
+# @card_3 = Card.new("What year was the World Wide Web launched in?", "1991", :STEM)
+# @card_4 = Card.new("What animal has the highest blood pressure?", "giraffe", :STEM)
+# @card_5 = Card.new("Which Queen of England and wife of Henry VIII had six fingers on one of her hands?", "anne boleyn", :History)
+# @card_6 = Card.new("What was the former name of Times Square before it was renamed in 1904?", "longacre square", :History)
+# @deck = Deck.new([@card_1, @card_2, @card_3, @card_4, @card_5, @card_6])
+@cards = CardGenerator.new("cards.txt").cards
+@deck = Deck.new(@cards)
 @round = Round.new(@deck)
 
 def start
-  puts "Welcome! You're playing with 6 cards."
-  puts (40.times {print "-"}).to_s + "\n"
+  puts "Welcome! You're playing with #{@cards.count} cards."
+  40.times {print "-"}
+  puts "\n"
   play
 end
 
