a866f7e7207dd8b01d8f8fd3919715e80becb1ca
Jordan Beck <jordanfbeck@gmail.com>
2020-10-10 17:42:58 -0600

    Add test it took in turns

--- test/round_test.rb
+++ test/round_test.rb
@@ -67,4 +67,30 @@ class Test < Minitest::Test
 
     assert_equal Turn, new_turn.class
   end
+
+  def test_is_it_correct
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+      and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+      clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+     assert_equal true, new_turn.correct?
+  end
+
+  def test_it_took_in_turns
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+      and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+      clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    assert_equal [new_turn], round.turns
+  end
 end
