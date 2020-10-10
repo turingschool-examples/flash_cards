3dcc5c398e3c084c935266ac6860f9eef74256d6
Grayson Cunningham <gecun289@gmail.com>
2020-10-10 10:56:30 -0600

    add setup test to turn_test.rb

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,52 +4,35 @@ require './lib/card'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
 
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of Turn, turn
+    assert_instance_of Turn, @turn
   end
 
   def test_it_has_a_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal "Juneau", turn.guess
+    assert_equal "Juneau", @turn.guess
   end
 
   def test_it_has_a_card_method
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card1)
-
-    assert_equal card1, turn.card
+    assert_equal @card, @turn.card
   end
 
-  def test_it_returns_true_for_correct_guess
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card1)
+  def test_correct?
+    assert_equal true, @turn.correct?
 
-    assert_equal true, turn.correct?
-  end
-
-  def test_it_returns_false_for_an_incorrect_guess
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Anchorage", card1)
+    turn = Turn.new("Anchorage", @card)
 
     assert_equal false, turn.correct?
   end
 
-  def test_it_returns_correct_for_a_correct_guess
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card1)
+  def test_feedback
+    assert_equal "Correct!", @turn.feedback
 
-    assert_equal "Correct!", turn.feedback
-  end
-
-  def test_it_returns_correct_for_an_incorrect_guess
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Fairbanks", card1)
+    turn = Turn.new("Fairbanks", @card)
 
     assert_equal "Incorrect.", turn.feedback
   end
