aa22a0a93c8ac177d462b05865d51d4eaafca6de
Cydnee Owens <cks410@yahoo.com>
2020-10-10 11:13:04 -0600

    Refactor each to inject

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -8,11 +8,9 @@ class CardGenerator
 
   def cards
     card_text = File.readlines(@filename)
-    lines_array = []
-    card_text.each do |line|
+    card_text.inject([]) do |lines_array, line|
       each_line = line.chomp.split(",")
       lines_array << Card.new(each_line[0].strip, each_line[1].strip, each_line[2].strip.to_sym)
     end
-    lines_array
   end
 end
