e4f0b0af879ef20d70847ddd2bcbdbd8a8b81c35
Grayson Cunningham <gecun289@gmail.com>
2020-10-10 12:03:18 -0600

    revise card generator instance test

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -8,7 +8,7 @@ class CardGeneratorTest < Minitest::Test
   def test_generate_cards
     cards = CardGenerator.new("cards.txt").cards
 
-    assert_instance_of Card, cards.first
+    assert cards.all? {|card| card.class == Card}
     assert_equal "camel", cards.first.answer
     assert_equal "STEM", cards.last.category
   end
