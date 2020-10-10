5dde908386ae80b9f0ea5650ec948e5fd4170ad5
Sam Yeo <yeosamuel95@gmail.com>
2020-10-09 22:52:32 -0600

    Start method complete, added Game Over into functionality

--- test/flashcard_runner.rb
+++ test/flashcard_runner.rb
@@ -3,9 +3,8 @@ require './lib/deck'
 require './lib/turn'
 require './lib/round'
 
-def start
+# Create game resources
 
-end
 card = Card.new("What is 5 + 5?", "10", :Math)
 card2 = Card.new("When did World War II officially end?", "1945", :History)
 card3 = Card.new("What is the most popular framework for Ruby to quickly create a backend system for a webapp?", "Ruby on Rails", :Ruby)
@@ -13,25 +12,29 @@ card4 = Card.new("In majority of programming languages, they're called Functions
 cards = [card, card2, card3, card4]
 deck = Deck.new(cards)
 round = Round.new(deck)
-arr_of_cats = round.deck.cards.collect do |card|
-  card.category
-end
-arr_of_cats = arr_of_cats.uniq
 
 
-puts "Welcome! You're playing with #{deck.count} cards."
-puts '-------------------------------------------------'
-total_cards = deck.count
-round.deck.count.times do |i|
-  puts "This is card number #{i+1} out of #{total_cards}"
-  puts "Question: #{round.current_card.question}"
-  input = gets.chomp
-  round.take_turn(input)
-  puts round.turns[i].feedback
+def start(round, deck)
+  arr_of_cats = round.deck.cards.collect do |card|
+    card.category
+  end
+  arr_of_cats = arr_of_cats.uniq
+  puts "Welcome! You're playing with #{deck.count} cards."
+  puts '-------------------------------------------------'
+  total_cards = deck.count
+  round.deck.count.times do |i|
+    puts "This is card number #{i+1} out of #{total_cards}"
+    puts "Question: #{round.current_card.question}"
+    input = gets.chomp
+    round.take_turn(input)
+    puts round.turns[i].feedback
+  end
+
+  puts "****** Game over! ******"
+  puts "You had #{round.number_correct} correct guesses out of #{total_cards} for a total score of #{round.percent_correct}%."
+  arr_of_cats.each do |cat|
+    puts "#{cat.to_s} - #{round.percent_correct_by_category(cat)}% correct"
+  end
 end
 
-puts "****** Game over! ******"
-puts "You had #{round.number_correct} correct guesses out of #{total_cards} for a total score of #{round.percent_correct}%."
-arr_of_cats.each do |cat|
-  puts "#{cat.to_s} - #{round.percent_correct_by_category(cat)}% correct"
-end
+start(round, deck)
