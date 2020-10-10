1a830b615cccfc9ecc312a1d2342c698e3229573
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-10 15:32:01 -0600

    Finish card generator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -17,13 +17,14 @@ class CardGenerator
     hold_2.pop
 
     hold_3 = []
-    
+
     hold_2.each do |card|
       question = card[0]
       answer = card[1]
-      category = card[2].chomp.to_sym
+      category = card[2].chomp.gsub(" ", "_").to_sym
       hold_3 << Card.new(question, answer, category)
     end
+    hold_3
     # require 'pry'; binding.pry
   end
 
