bfb7e1c8715d65408f93860f0a5d47800f31403a
Yesi Meza <yesi.meza10@gmail.com>
2020-10-10 22:17:39 -0600

    Passes it still knows current card test

--- test/round_test.rb
+++ test/round_test.rb
@@ -196,7 +196,22 @@ class RoundTest < Minitest::Test
     round.take_turn("Venus")
 
     assert_equal 100.0, round.percent_correct_by_category(:Geography)
-
   end
 
+  def test_it_still_knows_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+      and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+      clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    assert_equal card_3, round.current_card
+  end
 end
