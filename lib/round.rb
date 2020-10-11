73845717cae981427f3fb8bbffbe90f3b9508aeb
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-11 14:26:39 -0500

    Adds number correct by turn and category.

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,5 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :answer
   def initialize(deck)
     @deck = deck
     @turns = []
@@ -8,6 +8,34 @@ class Round
   def current_card
     deck.cards.shift
   end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    turns.push(new_turn)
+    new_turn
+  end
+
+  def number_correct
+    correct_guess = 0
+    turns.each do |turn|
+       if turn.feedback == "Correct!"
+        correct_guess += 1
+       end
+    end
+    correct_guess
+  end
+
+  def number_correct_by_category(category)
+    correct_guess_by_category = 0
+    turns.each do |turn|
+      if turn.feedback == "Correct!" && turn.card.category == category
+        correct_guess_by_category += 1
+      end
+    end
+    correct_guess_by_category
+  end
+
+
 
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -46,5 +46,73 @@ class RoundTest < Minitest::Test
   end
 
 
+  def test_it_has_a_new_trun
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    assert_instance_of Turn, new_turn
+    assert_equal Turn, new_turn.class
+    assert_equal true, new_turn.correct?
+    assert_equal [new_turn], round.turns
+    assert_equal card_2, round.current_card
+  end
+
+
+  def test_it_can_take_another_turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    expected = Turn.new("Venus", card_2)
+    new_turn = round.take_turn("Juneau")
+    refute expected == round.take_turn("Venus")
+    assert_equal 2, round.turns.count
+  end
+
+
+
+  def test_it_can_check_second_turn_guess
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    second_turn = round.take_turn("Venus")
+    assert_equal "Incorrect!", round.turns.last.feedback
+  end
+
+  def test_it_can_tell_how_many_correct_turns
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    second_turn = round.take_turn("Venus")
+    assert_equal 1, round.number_correct
+  end
+
+
+  def test_it_can_tell_how_many_correct_guesses_by_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    second_turn = round.take_turn("Venus")
+    assert_equal 1, round.number_correct_by_category(:Geography)
+    assert_equal 0, round.number_correct_by_category(:STEM)
+  end
+
+
+
+
+
 
 end
