f3062141191db93d2b46e9ccc5f0cd3797e868d8
Yesi Meza <yesi.meza10@gmail.com>
2020-10-11 14:24:29 -0600

    Add to runner file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,11 +1,23 @@
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-  and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is
-  697.5° clockwise from due north?", "North north west", :STEM)
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
 
-cards = [card_1, card_2, card_3]
 
-deck = Deck.new(cards)
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+    and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is
+    697.5° clockwise from due north?", "North north west", :STEM)
 
-round = Round.new(deck)
+  cards = [card_1, card_2, card_3]
+
+  deck = Deck.new(cards)
+
+  round = Round.new(deck)
+
+
+
+  puts "Welcome! You're playing with #{cards.length} cards."
+  puts "-------------------------------------------------"
+  puts "This is card number ____  out of #{cards.length}."
