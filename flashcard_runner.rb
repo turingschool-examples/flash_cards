2a6a28967dd2f51b0e76f0fc851a1a23b6ef4145
Jordan Beck <jordanfbeck@gmail.com>
2020-10-11 15:50:07 -0600

    Add .get notes for flashcard_runner

---
+++ flashcard_runner.rb
@@ -0,0 +1,9 @@
+# puts "Welcome! You're playing with #{4} cards."
+# 4 = gets
+# puts "This is card number #{1} out of #{4}"
+# puts "Question: What is #{5} + #{5}?"
+#
+
+puts "What is your name?"
+name = gets.chomp
+puts "Hello, #{name}"
