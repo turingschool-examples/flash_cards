9916caa77d3b7099e2f31916c80674373df9286a
Adam Etzion <adametzion90@gmail.com>
2020-10-10 20:07:00 -0600

    Create flashcard_runner.rb file

--- lib/runner.rb
+++
@@ -1,9 +0,0 @@
-require './lib/card'
-require './lib/turn'
-require './lib/deck'
-
-card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-turn = Turn.new("Juneau", card)
-
-require "pry"
-binding.pry
