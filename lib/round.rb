333b73cd4bbfb924c10cb5da21b6b0f840e2dc6f
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-10 21:07:04 -0500

    Add methods, tests/passing for turns and first deck card.

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,13 @@
 class Round
+  attr_reader :deck, :turns
   def initialize(deck)
-    @deck = deck 
+    @deck = deck
+    @turns = []
   end
+
+  def current_card
+    deck.cards.shift
+  end
+
+
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -13,8 +13,38 @@ class RoundTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
-    binding.pry
     assert_instance_of Round, round
   end
+
+  def test_it_has_a_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    assert_equal deck, round.deck
+  end
+
+  def test_it_can_take_turns
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    assert_equal [], round.turns
+  end
+
+  def test_current_card_is_first_card_in_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    expected = card_1
+    actual = round.current_card
+    assert_equal expected, actual
+  end
+
+
 
 end
