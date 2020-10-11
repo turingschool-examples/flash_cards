eabb032fa71591e67ad152f965a9333dff68328b
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-10 19:13:41 -0600

    Refactor all files

--- test/round_test.rb
+++ test/round_test.rb
@@ -75,7 +75,7 @@ class RoundTest < Minitest::Test
     assert_equal 2, round.turns.count
   end
 
-  def test_the_other_test_did_not_break
+  def test_the_other_tests_did_not_break
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
