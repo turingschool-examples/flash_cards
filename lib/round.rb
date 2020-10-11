220efbb73a93fa3390ea1045f08e8f54b2061494
Isabelle Villasenor <villasenori@outlook.com>
2020-10-11 17:23:40 -0600

    Add round and round_test

---
+++ lib/round.rb
@@ -0,0 +1,41 @@
+class Round
+  attr_reader :deck,
+              :turns
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, @deck.cards.shift)
+    @turns << new_turn
+    new_turn
+  end
+
+  def advance_card
+    deck.cards.rotate!(1)
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+
+  def number_correct
+    @turns.count { |turn| turn.correct? }
+  end
+
+  def number_correct_by_category(category)
+    @turns.count { |turn| turn.correct? && turn.card.category == category}
+  end
+
+  def percent_correct
+    100.0 * (number_correct / @turns.count)
+  end
+
+  def percent_correct_by_category(category)
+    100.0 * (number_correct_by_category(category).to_f /
+    @turns.map.count { |turn|
+      turn.card.category == category})
+  end
+end
---
+++ test/round_test.rb
@@ -0,0 +1,69 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+class RoundTest < Minitest::Test
+
+  def round_setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @deck = Deck.new([card_1, card_2, card_3])
+    @round = Round.new(deck)
+  end
+
+  def test_it_exists
+    assert_equal Round, @round
+  end
+
+  def test_it_has_attributes
+    assert_equal @deck, @round.deck
+    assert_equal [], @round.turns
+  end
+
+  def test_it_takes_turns
+    new_turn = @round.take_turn("Juneau")
+
+    assert_equal Turn, new_turn
+    assert new_turn.correct?
+    assert [new_turn], round.turns
+  end
+
+  def test_value_of_current_card
+    assert_equal card_1, round.current_card
+  end
+
+  def test_number_of_correct_guesses
+    new_turn = @round.take_turn("Juneau")
+
+    assert_equal 1, round.number_correct
+  end
+
+  def test_number_correct_by_category
+    new_turn = @round.take_turn("Juneau")
+
+    @round.take_turn("Venus")
+
+    assert_equal 1, @round.number_correct_by_category(:Geography)
+    assert_equal 0, @round.number_correct_by_category(:STEM)
+  end
+
+  def test_percent_correct
+    new_turn = @round.take_turn("Juneau")
+
+    @round.take_turn("Venus")
+
+    assert_equal 50.0, @round.percent_correct
+  end
+
+  def test_percent_correct_by_category #
+    new_turn = @round.take_turn("Juneau")
+
+    @round.take_turn("Venus")
+
+    assert_equal 100.0, @round.percent_correct_by_category(:Geography)
+  end
+end
