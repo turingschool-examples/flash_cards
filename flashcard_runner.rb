ecd55b7a143d327a887983f2526c5bc3ea631c52
Jordan Beck <jordanfbeck@gmail.com>
2020-10-12 10:16:13 -0600

    clean up notes in flashcar_runner.rb

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -12,41 +12,26 @@ def start
   card_4 = Card.new("Question: What cardboard cutout lives at Turing?", "Justin Bieber", :cutouts)
   deck = Deck.new([card_1, card_2, card_3, card_4])
   round = Round.new(deck)
-  # new_turn = round.take_turn("10")
-  # puts "What is your name?"
-  # name = gets.chomp
-  # puts "#{name}, how many cards would you like to play with?"
-  # cards = gets.chomp
-  # puts "Welcome! You're playing with #{cards} cards."
-  # require "pry"; binding.pry
-  # def turn_number
-  #   turn_number_count = 0
-  #   # round.take_turn == turn_number_count
-  #   deck.each do |turn_num|
-  #     turn_num.count
-  #   end
-  # end
-
-# require "pry"; binding.pry
+  
   puts "Welcome! You're playing with #{round.deck.count} cards."
   puts "-------------------------------------------------"
 
-  puts "This is card number #{round.turn_number} out of #{deck.cards.count}."
+  puts "This is card number #{round.turn_number} out of #{deck.count}."
   puts "Question: What is 5 + 5?"
   guess = gets.chomp
   puts round.take_turn(guess).feedback
 
-  puts "This is card number #{round.turn_number} out of #{deck.cards.count}."
+  puts "This is card number #{round.turn_number} out of #{deck.count}."
   puts "Question: What is Rachel's favorite animal?"
   guess = gets.chomp
   puts round.take_turn(guess).feedback
 
-  puts "This is card number #{round.turn_number} out of #{deck.cards.count}."
+  puts "This is card number #{round.turn_number} out of #{deck.count}."
   puts "Question: What is Mike's middle name?"
   guess = gets.chomp
   puts round.take_turn(guess).feedback
 
-  puts "This is card number #{round.turn_number} out of #{deck.cards.count}."
+  puts "This is card number #{round.turn_number} out of #{deck.count}."
   puts "Question: What cardboard cutout lives at Turing?"
   guess = gets.chomp
   puts round.take_turn(guess).feedback
