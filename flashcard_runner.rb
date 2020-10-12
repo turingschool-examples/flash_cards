2db54a017a4d02eac9024670eeca4ed12f3f7cc5
Jordan Beck <jordanfbeck@gmail.com>
2020-10-12 09:17:17 -0600

    add more logic to flashcard_runner. Need to resolve conflict with round.rb and total amount of cards.

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,7 +7,7 @@ require './lib/round'
 def start
 
   card_1 = Card.new("Question: What is 5 + 5", "10", :maths)
-  card_2 = Card.new("What is Rachel's favorite animal?", "panda", :animals)
+  card_2 = Card.new("Question: What is Rachel's favorite animal?", "dolphin", :animals)
   card_3 = Card.new("Question: What is Mike's middle name?", "nobody knows", :middle_names)
   card_4 = Card.new("Question: What cardboard cutout lives at Turing?", "Justin Bieber", :cutouts)
   deck = Deck.new([card_1, card_2, card_3, card_4])
@@ -27,13 +27,29 @@ def start
   #   end
   # end
 
-require "pry"; binding.pry
+# require "pry"; binding.pry
   puts "Welcome! You're playing with #{round.deck.count} cards."
   puts "-------------------------------------------------"
 
-  puts "This is card number #{round.turn_number} out of #{deck.new.count}."
+  puts "This is card number #{round.turn_number} out of #{deck.cards.count}."
   puts "Question: What is 5 + 5?"
-  gets.chomp
+  guess = gets.chomp
+  puts round.take_turn(guess).feedback
+
+  puts "This is card number #{round.turn_number} out of #{deck.cards.count}."
+  puts "Question: What is Rachel's favorite animal?"
+  guess = gets.chomp
+  puts round.take_turn(guess).feedback
+
+  puts "This is card number #{round.turn_number} out of #{deck.cards.count}."
+  puts "Question: What is Mike's middle name?"
+  guess = gets.chomp
+  puts round.take_turn(guess).feedback
+
+  puts "This is card number #{round.turn_number} out of #{deck.cards.count}."
+  puts "Question: What cardboard cutout lives at Turing?"
+  guess = gets.chomp
+  puts round.take_turn(guess).feedback
 
 end
 
