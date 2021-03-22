9f30d285b9449a744619edbc6d477df94d75b4a8
Richard DeSilvey <rdesilvey@gmail.com>
2021-03-22 16:19:45 -0600

    Write tests for Deck

---
+++ spec/deck_spec.rb
@@ -0,0 +1,35 @@
+require './lib/deck'
+
+RSpec.describe Deck do
+
+  test_cards = [
+    Card.new("What is the capital of Alaska?", "Juneau", :Geography),
+    Card.new("When did world war 2 end?", "Sep 2, 1945", :History),
+    Card.new("What is the square root of 25?", "5", :Math),
+    Card.new("What is the square root of 144?", "12", :Math
+  ]
+
+  it 'exists' do
+    test_deck = Deck.new(test_cards)
+    expect(test_deck).to be_instance_of(Deck)
+  end
+
+  it 'returns the number of cards via count' do
+    test_deck = Deck.new(test_cards)
+    expect(test_deck.count).to eq(3)
+  end
+
+  it 'returns the cards for a given category' do
+    test_deck = Deck.new(test_cards)
+
+    cards = test_deck.cards_in_category(:Math)
+
+    expect(cards.length).to eq(2)
+
+    cards.each {|card|
+      expect(card.category).to eq(:Math)
+    }
+  end
+
+
+end
