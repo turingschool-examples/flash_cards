901adf2642bd032c8853c495e19d0e0a4ae95041
Richard <rdesilvey@gmail.com>
2021-03-22 16:34:03 -0600

    Fix tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,26 +1,29 @@
 require './lib/deck'
+require './lib/card'
 
 RSpec.describe Deck do
 
-  test_cards = [
-    Card.new("What is the capital of Alaska?", "Juneau", :Geography),
-    Card.new("When did world war 2 end?", "Sep 2, 1945", :History),
-    Card.new("What is the square root of 25?", "5", :Math),
-    Card.new("What is the square root of 144?", "12", :Math
-  ]
+  before(:all) do
+    @test_cards = [
+      Card.new("What is the capital of Alaska?", "Juneau", :Geography),
+      Card.new("When did world war 2 end?", "Sep 2, 1945", :History),
+      Card.new("What is the square root of 25?", "5", :Math),
+      Card.new("What is the square root of 144?", "12", :Math)
+    ]
+  end
 
   it 'exists' do
-    test_deck = Deck.new(test_cards)
+    test_deck = Deck.new(@test_cards)
     expect(test_deck).to be_instance_of(Deck)
   end
 
   it 'returns the number of cards via count' do
-    test_deck = Deck.new(test_cards)
-    expect(test_deck.count).to eq(3)
+    test_deck = Deck.new(@test_cards)
+    expect(test_deck.count).to eq(4)
   end
 
   it 'returns the cards for a given category' do
-    test_deck = Deck.new(test_cards)
+    test_deck = Deck.new(@test_cards)
 
     cards = test_deck.cards_in_category(:Math)
 
