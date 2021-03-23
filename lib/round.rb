deb1b09ed1089171d8a176576ee5769bc4ec56f5
Richard <rdesilvey@gmail.com>
2021-03-22 20:58:37 -0600

    Fill out Round
    No implementation

---
+++ lib/round.rb
@@ -0,0 +1,19 @@
+class Round
+  attr_reader :deck, :current_card, :number_correct, :turns
+
+  def initialize(deck)
+  end
+
+  def take_turn(guess)
+  end
+
+  def number_correct_by_category(category)
+  end
+
+  def percent_correct
+  end
+
+  def percent_correct_by_category(category)
+  end
+
+end
