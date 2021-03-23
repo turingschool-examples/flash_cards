8576675899544ad545299a4595787f110e20d60e
Jamison Griffith <jamison.griffith@gmail.com>
2021-03-23 12:03:21 -0600

    Add deck class with associated tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,5 @@
+require_relative 'card'
+
 class Deck
   attr_reader :cards
 
@@ -5,4 +7,29 @@ class Deck
       @cards = card_array
   end
 
+  # This method counts the number of cards in a deck
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(card_category)
+    array = []
+    @cards.each do |card|
+      if card.category == card_category
+        array << card
+      end
+    end
+    return array
+  end
 end
+
+# This chunk of code below was used for testing purposes.
+# Not necessary to keep.
+
+# card1 = Card.new("1?", "Yes", :History)
+# card2 = Card.new("2?", "No", :Math)
+# card3 = Card.new("3?", "Maybe", :STEM)
+# cards = [card1, card2, card3]
+# deck = Deck.new(cards)
+#
+# # require "pry"; binding.pry
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,14 +2,31 @@ require_relative '../lib/card.rb'
 require_relative '../lib/deck.rb'
 
 RSpec.describe Deck do
-  it 'exists' do
-    card1 = Card.new("1?", "Yes", :STEM)
-    card2 = Card.new("2?", "No", :STEM)
+
+  before :each do
+    card1 = Card.new("1?", "Yes", :Math)
+    card2 = Card.new("2?", "No", :History)
     card3 = Card.new("3?", "Maybe", :STEM)
     cards = [card1, card2, card3]
 
-    deck = Deck.new(cards)
-
-    expect(deck).to be_instance_of(Deck)
+    @deck = Deck.new(cards)
   end
+
+  it 'exists' do
+    expect(@deck).to be_instance_of(Deck)
+  end
+
+  it 'can count cards' do
+    expect(@deck.count).to eq(3)
+  end
+
+  it 'can select card categories' do
+    expect(@deck.cards_in_category(:History)).to be_instance_of(Array)
+  end
+
+  it 'can count categorized cards' do
+    math_cards = @deck.cards_in_category(:Math)
+    expect(math_cards.count).to eq(1)
+  end
+
 end
