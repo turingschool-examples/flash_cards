c0aa01907708c0dc76d5e196eb9b6bc572404710
NoahZinter <noah.zinter@gmail.com>
2021-03-22 19:28:45 -0600

    Create tests for .take_turn method, get them to pass

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -51,6 +51,58 @@ RSpec.describe Round do
     expect(round.current_card).to eq(card_2)
   end
 
+  it 'can take a turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    round.take_turn("Fairbanks")
+# require "pry"; binding.pry
+    expect(round.turns[0]).to be_instance_of(Turn)
+  end
+
+  it 'changes current card when taking turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Fairbanks")
+
+    expect(round.current_card).to eq(card_2)
+  end
+
+  it 'when taking turn can answer to .correct?' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      round.take_turn("Fairbanks")
+
+# require "pry"; binding.pry
+      expect(round.turns[0].correct?).to eq(false)
+    end
+
+    it 'can return a positive answer to .correct?' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      round.take_turn("Juneau")
+
+# require "pry"; binding.pry
+      expect(round.turns[0].correct?).to eq(true)
+    end
 
 
 
