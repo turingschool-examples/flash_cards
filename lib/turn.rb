a33fc19b66f1a74ebe82817403f386a747e8f2e7
Caleb Wittman <calebjwittman1991@gmail.com>
2021-03-23 16:47:41 -0600

    Removed guess and card method, redundant due to attr_reader

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,28 +1,20 @@
 class Turn
-  attr_accessor :guess, :card
+  attr_reader :guess, :card
 
     def initialize(guess, card)
       @guess = guess
       @card = card
     end
 
-    def guess
-      return guess
-    end
-
-    def card
-      return card
-    end
-
     def correct?
-      if guess == "Juneau"
+      if @guess == "Juneau"
         true
       else false
       end
     end
 
     def feedback
-      if guess == "Juneau"
+      if @guess == "Juneau"
         return "Correct!"
       else
         return "Incorrect."
