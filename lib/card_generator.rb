9aebb69bede27793b8cd80d24ea83c6c8568b491
Richard <rdesilvey@gmail.com>
2021-03-23 11:58:57 -0600

    Implement CardGenerator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,12 +1,27 @@
 require "./lib/card"
 
 class CardGenerator
-  attr_reader :cards
 
   def initialize(filename)
+    @cards = []
+    @filename = filename
   end
 
-  def generate_cards(lines)
+  def cards
+    self.generate_cards_from cards_file(@filename)
+  end
+
+  def generate_cards_from(lines)
+    new_cards = []
+    lines.each {|line|
+      sections = line.split(',')
+      new_cards << Card.new(sections[0], sections[1], sections[2])
+    }
+    return new_cards
+  end
+
+  def cards_file(filename)
+    IO.readlines(filename).map {|line| line.chomp }
   end
 
 end
