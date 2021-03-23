dd9a407f8dd4c0dcb0176bc079c9931d4e482041
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-03-23 19:33:41 -0400

    Create turn class file

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+
+
+class Turn
+    attr_reader :guess,
+                :card
+    def initialize(string, card)
+      @guess = string
+      @card = card
+    end
+
+    def correct?
+
+    end
+
+  def feedback
+    
+  end
+end
