94932a07a2a8e72369b2ab24e1283dc107a89994
JoannaCoPo <jococopo@gmail.com>
2021-03-23 08:13:18 -0600

    Edits

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,18 @@
 class Deck
 
+  attr_reader :cards
+
   def initialize(cards)
+    @cards = cards
   end
+
+  def count
+    self.cards.count
+    #is this really what they want? Re interaction pattern
+  end
+
+  def cards_in_category(category)
+
+  end
+
 end
