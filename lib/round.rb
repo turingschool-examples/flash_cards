647e63e6b84f70e212c5d85d9443d05f412c22d9
NoahZinter <noah.zinter@gmail.com>
2021-03-22 18:45:19 -0600

    Create .current_card, .next_card, and .take_turn methods for round.rb, create tests for .current_card and .next_card

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,24 @@
 class Round
 
-  attr_reader :deck
+  attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    self.deck.cards.first
+  end
+
+  def next_card
+    self.deck.cards.shift
+  end
+
+  def take_turn(guess)
+    card = self.current_card
+    turn = Turn.new(guess, card)
+    @turns << turn
+    self.next_card
   end
 
 end
