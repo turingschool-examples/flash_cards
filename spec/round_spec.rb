cd4c49e4767cfef9b13b95daa2acdca28e7c78f1
NoahZinter <noah.zinter@gmail.com>
2021-03-23 16:26:58 -0600

    Create more tests in round_spec.rb to ensure .take_turn works properly

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -87,7 +87,7 @@ RSpec.describe Round do
       round.take_turn("Fairbanks")
 
 # require "pry"; binding.pry
-      expect(round.turns[0].correct?).to eq(false)
+      expect(round.turns.first.correct?).to eq(false)
     end
 
     it 'can return a positive answer to .correct?' do
@@ -100,9 +100,41 @@ RSpec.describe Round do
 
       round.take_turn("Juneau")
 
-# require "pry"; binding.pry
-      expect(round.turns[0].correct?).to eq(true)
+      expect(round.turns.first.correct?).to eq(true)
     end
+
+    it 'can give feedback on guesses' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      round.take_turn("Juneau")
+
+      expect(round.turns.first.feedback).to eq("Correct!")
+    end
+
+    it 'can handle multiple turns' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      round.take_turn("Juneau")
+      round.take_turn("Mars")
+
+      expect(round.turns.first.feedback).to eq("Correct!")
+      expect(round.turns[1].correct?).to eq(true)
+    end 
+
+
+
+
+
 
 
 
