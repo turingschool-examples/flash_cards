271d557be268742384f6875e64e640daaad0bf3f
Aliya Merali <merali4@gmail.com>
2021-03-22 18:55:14 -0600

    Add round file take_turn progress

--- lib/round.rb
+++ lib/round.rb
@@ -1,20 +1,44 @@
+# ## ISSUES TO RESOLVE:
+# Not iterating through card deck - stuck on first card
+# Need to ID how to define number correct as separate method (started, not finished)
+# Need to build all tests beyond initialization
+
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :current_card, :number_correct
 
   def initialize(deck)
     @deck = deck
     @turns = []
-    # @current_card = @deck.cards[0]
+    @current_card = @deck.cards[0]
+    @number_correct = 0
   end
 
-  # TODO:
-  # Current card as first in deck
-  # Make a guess: guess is recorded and the next card becomes current card
 
-  def take_turn
-    #takes a string representing the guess, creates a new Turn object with this
-    #guess and card. Store this turn and return from the method.
-    #should also trigger a move to the next card in the deck.
+  def take_turn(guess)
+    # create a new turn object based on the guess, return it
+    @turn = Turn.new(guess, current_card)
+    # store this turn
+    @turns << [@turn]
+    # move on to the next card - NOT WORKING
+    new_index = @turns.length
+    puts "New index is #{new_index}"
+    current_card = @deck.cards[new_index]
+    # return the turn
+    return @turn
+  end
+
+  def number_correct # NOT WORKING
+    @turns.count do |turn| #Error here that .correct not recognized as method
+      turn.correct? # ERROR: NoMethodError: undefined method `correct?' for #<Array:0x0000000157255208>
+    end
+  end
+
+  def percent_correct
+    (@number_correct.to_f / @turns.length.to_f) * 100
+  end
+
+  def number_correct_by_category(category)
+
   end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -6,7 +6,7 @@ require './lib/round'
 RSpec.describe Round do
 
   it 'exists' do
-    cards = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    cards = [Card.new("What is the capital of Alaska?", "Juneau", :Geography)]
     deck = Deck.new(cards)
     round = Round.new(deck)
 
@@ -14,11 +14,22 @@ RSpec.describe Round do
   end
 
   it 'starts with an empty turns array' do
-    cards = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    cards = [Card.new("What is the capital of Alaska?", "Juneau", :Geography)]
     deck = Deck.new(cards)
     round = Round.new(deck)
 
     expect(round.turns).to eq([])
+  end
+
+  it 'starts with the first card in the deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("What is the capital of Colombia?", "Bogota", :Geography)
+    card_3 = Card.new("What planet is closest to the sun?", "Mercury", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.current_card).to eq(card_1)
   end
 
 
