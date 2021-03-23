6e49cb4deb4a75d54cb45f18ed3d0eee69bf061a
Mitch Standley <mitch_standley@yahoo.com>
2021-03-23 16:09:58 -0700

    Add questions to test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -7,7 +7,22 @@ Rspec.describe Turn do
     turn = Turn.new("Juneau", card)
     expect(turn).to be_instance_of(Turn)
     expect(card).to be_instance_of(Card)
+
   end
+
+  it 'guesses' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card.guess).to eq("Juneau")
+  end
+
+  it 'has an answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card.answer).to eq("Juneau")
+  end
+
+  it ''
 
 
 
@@ -16,5 +31,3 @@ Rspec.describe Turn do
   #   expect(@turn_correct.feedback).to eq("Correct!")
   #   expect(@turn_incorrect.feedback).to eq("Incorrect.")
   # end
-
-end
