3553a1898c12489aecd810a0e9319b0bdf7ca079
JoannaCoPo <jococopo@gmail.com>
2021-03-22 20:39:58 -0600

    Initiate Turn class and test

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+class Turn
+
+  attr_reader :guess
+
+  def initialize(guess, card)
+    @guess = guess
+    binding.pry
+
+  end
+
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,27 @@
+require './lib/card'
+require './lib/turn'
+require 'pry'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a quess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'has a quess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+end
