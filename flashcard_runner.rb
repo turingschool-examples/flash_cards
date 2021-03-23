f910c9de3701ad55bf4e5add8473aee88210e533
Richard <rdesilvey@gmail.com>
2021-03-23 10:34:09 -0600

    Import for runner to use generator

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,6 +2,7 @@ require './lib/round'
 require './lib/deck'
 require './lib/card'
 require './lib/turn'
+require './lib/card_generator'
 
 puts "Welcome! You're playing with 4 cards."
 puts "-------------------------------------------------"
