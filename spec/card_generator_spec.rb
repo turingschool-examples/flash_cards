c1ec7d4ec4a9fb5b234cabb9cefbe60ac58b41eb
Richard <rdesilvey@gmail.com>
2021-03-23 10:33:47 -0600

    Skeleton for test file

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,21 @@
+require './lib/card_generator'
+
+RSpec.describe CardGenerator do
+
+  before(:each) do
+    @card_generator = CardGenerator.new('./spec/test_cards.txt')
+  end
+
+  it 'exists' do
+  end
+
+  it 'loads a file' do
+  end
+
+  it 'has cards' do
+  end
+
+  it 'generates cards from given lines' do
+  end
+
+end
