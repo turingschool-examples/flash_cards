2dd23debc82e650e5c5e1d213d30d25b12ad6a4d
Caleb Wittman <calebjwittman1991@gmail.com>
2021-03-23 07:02:19 -0600

    Created turn.rb and turn_spec.rb, as well as corrected misspelling of 'initialize'

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,10 @@
 class Card
   attr_accessor :question, :answer, :catergory
 
-  def intialize(question, answer, catergory)
+  def initialize(question, answer, catergory)
     @question = question
     @answer = answer
     @catergory = catergory
   end
-
+  
 end
