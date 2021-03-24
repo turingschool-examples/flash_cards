ec44c9448df6ce98e126ad4947c98b4f78573a6e
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-23 20:59:44 -0700

    Create attr_reader

--- lib/card.rb
+++ lib/card.rb
@@ -1,20 +1,11 @@
 class Card
-
+  attr_reader :question,
+                   :answer,
+                   :category
+                   
   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
-  end
-
-  def question
-    "What is the capital of Alaska?"
-  end
-
-  def answer
-    "Juneau"
-  end
-
-  def category
-    :Geography
   end
 end
