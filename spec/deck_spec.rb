46dec2dd029b241c7a27f11fa5c12bea207b6225
Molly Krumholz <mkkrumholz@gmail.com>
2021-03-23 18:20:56 -0600

    Update formatting of Deck and Turn test files

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,25 +3,35 @@ require './lib/card'
 
 RSpec.describe Deck do
 
-  before do
-    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @cards = [@card_1, @card_2, @card_3]
-    @deck = Deck.new(@cards)
-  end
-
   it "has cards" do
-    expect(@deck.cards).to eq [@card_1, @card_2, @card_3]
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq [card_1, card_2, card_3]
   end
 
   it "has the right number of cards" do
-    expect(@deck.count).to eq 3
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.count).to eq 3
   end
 
   it "can count cards in a given category" do
-    expect(@deck.cards_in_category(:STEM)).to eq 2
-    expect(@deck.cards_in_category(:Geography)).to eq 1
-    expect(@deck.cards_in_category("Pop Culture")).to eq 0
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq 2
+    expect(deck.cards_in_category(:Geography)).to eq 1
+    expect(deck.cards_in_category("Pop Culture")).to eq 0
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,44 +4,44 @@ require './lib/card'
 RSpec.describe Turn do
 
   it "has a guess" do
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @turn = Turn.new("Juneau", @card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    expect(@turn.guess).to eq "Juneau"
+    expect(turn.guess).to eq "Juneau"
   end
 
   it "has a card" do
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @turn = Turn.new("Juneau", @card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    expect(@turn.card).to eq @card
+    expect(turn.card).to eq card
   end
 
   it "recognizes correct guesses" do
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @turn = Turn.new("Juneau", @card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    expect(@turn.correct?).to eq true
+    expect(turn.correct?).to eq true
   end
 
   it "recognizes incorrect guesses" do
-    @bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    @bad_turn = Turn.new("Saturn", @bad_card)
+    bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    bad_turn = Turn.new("Saturn", bad_card)
 
-    expect(@bad_turn.correct?).to eq false
+    expect(bad_turn.correct?).to eq false
   end
 
   it "gives feedback if correct" do
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @turn = Turn.new("Juneau", @card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    expect(@turn.feedback).to eq "Correct!"
+    expect(turn.feedback).to eq "Correct!"
   end
 
   it "gives feedback if incorrect" do
-    @bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    @bad_turn = Turn.new("Saturn", @bad_card)
-    
-    expect(@bad_turn.feedback).to eq "Incorrect."
+    bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    bad_turn = Turn.new("Saturn", bad_card)
+
+    expect(bad_turn.feedback).to eq "Incorrect."
   end
 end
