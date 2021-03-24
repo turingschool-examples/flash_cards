863e59f6dd033942c46b7d82483af448827db0ec
Aliya Merali <merali4@gmail.com>
2021-03-24 17:38:34 -0600

    Refactor test formatting'

---
+++ flashcard_runner.rb
@@ -0,0 +1,6 @@
+# Inside of this file, write the code to do the following:
+#
+# Create some Cards
+# Put those card into a Deck
+# Create a new Round using the Deck you created
+# Start the round using a new method called start
