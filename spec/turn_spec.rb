2a577c305a12828dac2106319385c49f305fca4f
Molly Krumholz <mkkrumholz@gmail.com>
2021-03-23 18:04:24 -0600

    Updated turn_spec.rb to remove before hook

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,34 +3,45 @@ require './lib/card'
 
 RSpec.describe Turn do
 
-  before do
+  it "has a guess" do
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @turn = Turn.new("Juneau", @card)
-    @bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    @bad_turn = Turn.new("Saturn", @bad_card)
-  end
 
-  it "has a guess" do
     expect(@turn.guess).to eq "Juneau"
   end
 
   it "has a card" do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+
     expect(@turn.card).to eq @card
   end
 
   it "recognizes correct guesses" do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+
     expect(@turn.correct?).to eq true
   end
 
   it "recognizes incorrect guesses" do
+    @bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    @bad_turn = Turn.new("Saturn", @bad_card)
+
     expect(@bad_turn.correct?).to eq false
   end
 
   it "gives feedback if correct" do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+
     expect(@turn.feedback).to eq "Correct!"
   end
 
   it "gives feedback if incorrect" do
+    @bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    @bad_turn = Turn.new("Saturn", @bad_card)
+    
     expect(@bad_turn.feedback).to eq "Incorrect."
   end
 end
