9928563bc4b64f0d66e138927a46a1d42490189f
Zach.Trokey <ztrokey@gmail.com>
2021-03-24 07:52:43 -0600

    Add tests to turn_spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -16,10 +16,25 @@ RSpec.describe Turn do
     expect(turn.card).to eq(card)
   end
 
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess). to eq("Juneau")
+  end
+
+
   it 'is correct' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
+    expect(turn.guess). to eq(card.answer)
+  end
+
+  it 'is incorrect' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Satrun", card)
+    require 'pry'; binding.pry 
     expect(turn.guess). to eq(card.answer)
   end
 end
