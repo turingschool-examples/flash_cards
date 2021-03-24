18b6c41ccdf51b3ab0dc74b0056ea23cc76acd41
Zach.Trokey <ztrokey@gmail.com>
2021-03-23 21:08:51 -0600

    Add testing and method to deck and deck_spec

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,20 @@
 class Deck
   attr_accessor :cards
-  
+  attr_reader :card
+
   def initialize(cards)
-  @cards = []
+  @cards = cards
 
   end
+
+  def count
+   cards.length
+  end
+  # This doesn't work
+  # def cards_in_category(category)
+  #   @cards.select { |card| card.category }
+  # end
+
 
 end
 
