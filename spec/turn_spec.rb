f74b0eebb891b4f0400b2a230ae9416a68660fac
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-03-23 20:26:35 -0400

    Fix local var issue in turn_spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,41 +1,47 @@
 require './lib/turn'
+require './lib/card'
 
 RSpec.describe Turn do
   it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.guess).to eq("Juneau")
   end
 
   it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.card).to eq(card)
   end
 
   it 'checks for accuracy' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.correct?).to eq(true)
 
     turn2 = Turn.new("Not Juneau", card)
 
-    expect(turn.correct?).to eq(false)
+    expect(turn2.correct?).to eq(false)
   end
 
   it 'provides feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.feedback).to eq("Correct!")
 
     turn2 = Turn.new("Not Juneau", card)
 
-    expect(turn.feedback).to eq("Incorrect.")
-  end 
+    expect(turn2.feedback).to eq("Incorrect.")
+  end
 end
