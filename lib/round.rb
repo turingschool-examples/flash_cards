00e02367e31fadbcb1d6527aaa189da72356026f
Molly Krumholz <mkkrumholz@gmail.com>
2021-03-23 22:09:29 -0600

    Complete functionality for Round class and tests

--- lib/round.rb
+++ lib/round.rb
@@ -1,26 +1,59 @@
 class Round
 
-  attr_reader :deck, :turns, :current_card
+  attr_reader :deck,
+              :turns,
+              :number_correct
   def initialize(deck)
     @deck = deck
     @turns = []
-      # require "pry"; binding.pry
-    @current_turn = 0
-    @current_card = @deck.cards[@current_turn]
     @number_correct = 0
   end
 
+  def current_card
+    turns_taken = self.turns.length
+    @deck.cards[turns_taken]
+  end
+
   def take_turn(guess)
-    new_turn = Turn.new(guess, @current_card)
+    new_turn = Turn.new(guess, self.current_card)
     turns << new_turn
-    @current_turn += 1
     if new_turn.correct? == true
       @number_correct += 1
-      new_turn
+    end
+    return new_turn
+  end
+
+  def number_correct_by_category(category)
+    total_for_category = 0
+    @turns.each do |turn|
+      if turn.correct? && turn.card.category == category
+        total_for_category += 1
+      end
+    end
+    return total_for_category
+  end
+
+  def percent_correct
+    if @turns.length > 0
+      ( @number_correct / @turns.length.to_f ) * 100.0
     else
-      new_turn
+      "No turns have yet been taken."
     end
   end
 
-
+  def percent_correct_by_category(category)
+    # num correct for category divided by num turns in category
+    turns_in_category = 0
+    @turns.each do |turn|
+      if turn.card.category == category
+        turns_in_category += 1
+      end
+    end
+    correct_in_category = self.number_correct_by_category(category)
+    if turns_in_category > 0.0
+      (correct_in_category / turns_in_category.to_f ) * 100.0
+    else
+      "No turns in this category have yet been taken."
+    end
+  end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -5,32 +5,114 @@ require './lib/turn'
 
 RSpec.describe Round do
 
-  before do
-    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    @deck = Deck.new([@card_1, @card_2, @card_3])
-    @round = Round.new(@deck)
-  end
-
   it "has a deck" do
-    expect(@round.deck).to eq @deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.deck).to eq deck
   end
 
   it "has a turns array" do
-    expect(@round.turns).to eq []
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.turns).to eq []
   end
 
   it "knows what the current card is before starting" do
-    expect(@round.current_card).to eq @card_1
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.current_card).to eq card_1
   end
 
   it "creates a new Turn object" do
-    expect(@round.take_turn("Juneau")).to be_instance_of Turn
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.take_turn("Juneau")).to be_instance_of Turn
   end
 
-  it "increments the current card up" do
-    @round.take_turn("Juneau")
-    
+  it "increments the current card up after a turn" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    expect(round.current_card).to eq card_2
+  end
+
+  it "counts the number of correct answers given" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    expect(round.number_correct).to eq 1
+  end
+
+  it "counts the number correct by category" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    expect(round.number_correct_by_category(:Geography)).to eq 1
+    expect(round.number_correct_by_category(:STEM)).to eq 0
+
+    round.take_turn("Mars")
+    expect(round.number_correct_by_category(:Geography)).to eq 1
+    expect(round.number_correct_by_category(:STEM)).to eq 1
+  end
+
+  it "calculates the percent correct overall" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.percent_correct).to eq "No turns have yet been taken."
+
+    round.take_turn("Juneau")
+    expect(round.percent_correct).to eq 100.0
+
+    round.take_turn("Pluto")
+    expect(round.percent_correct).to eq 50.0
+  end
+
+  it "calculates the percent correct by category" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    expect(round.percent_correct_by_category(:Geography)).to eq 100.0
+    expect(round.percent_correct_by_category(:STEM)).to eq "No turns in this category have yet been taken."
+
+    round.take_turn("Pluto")
+    round.take_turn("North north west")
+    expect(round.percent_correct_by_category(:Geography)).to eq 100.0
+    expect(round.percent_correct_by_category(:STEM)).to eq 50.0
   end
 end
