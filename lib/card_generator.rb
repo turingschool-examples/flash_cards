3fb0e45744391ed26c6394ee3dc8d1a03dbf78d8
Aliya Merali <merali4@gmail.com>
2021-03-24 19:33:13 -0600

    Add working card_generator class and spec

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,5 +1,21 @@
 class CardGenerator
+  attr_reader :file
+
+  # require 'pry'; binding.pry
+
   def initialize(filename)
-    @input_file = filename
+    @file = File.open(filename)
   end
+
+  def cards
+    card_set = []
+    cards_as_array = @file.read.split("\n")
+    cards_as_array.each do |card_in_array|
+      card_info = card_in_array.split(",")
+      card = Card.new(card_info[0], card_info[1], card_info[2])
+      card_set << card
+    end
+    return card_set
+  end
+
 end
