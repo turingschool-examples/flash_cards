ab88603554d532f2088cac30fec4199be840b996
matthewjholmes <>
2021-03-25 13:22:11 -0600

    Initial commit

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(string, card)
+    @guess = string
+    @card = card
+  end
+
+  def correct?
+    @guess == @card.answer
+  end
+
+  def feedback
+    correct? ? "Correct!" : "Incorrect!"
+  end
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,44 @@
+require 'rspec'
+require './lib/turn'
+require './lib/card'
+require 'pry'
+
+describe Turn do
+    it 'exists' do
+      carabiner = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("iPhone", carabiner)
+
+      expect(turn).to be_instance_of(Turn)
+    end
+
+    it 'read the guess' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau",card)
+      expect(turn.guess).to eq("Juneau")
+    end
+
+    it 'reads the card instance' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau",card)
+      expect(turn.card).to eq(card)
+    end
+
+    it 'checks the guess' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau",card)
+      expect(turn.correct?).to be true
+    end
+
+    it 'gives feedback if correct' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau",card)
+      expect(turn.feedback).to eq("Correct!")
+    end
+
+    it 'gives feedback if incorrect' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Wrong answer",card)
+      binding.pry
+      expect(turn.feedback).to eq("Incorrect!")
+    end
+  end
