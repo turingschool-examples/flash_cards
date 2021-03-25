29215145b8e7f8f84daea64ced2b6d3b65ec0e83
Molly Krumholz <mkkrumholz@gmail.com>
2021-03-24 20:35:34 -0600

    Add describe blocks for different methods in test files

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,45 +3,51 @@ require './lib/card'
 
 RSpec.describe Turn do
 
-  it "has a guess" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+  describe '#initialize' do
+    it "has a guess" do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
 
-    expect(turn.guess).to eq "Juneau"
+      expect(turn.guess).to eq "Juneau"
+    end
+
+    it "has a card" do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
+
+      expect(turn.card).to eq card
+    end
   end
 
-  it "has a card" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+  describe '#correct?' do
+    it "recognizes correct guesses" do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
 
-    expect(turn.card).to eq card
+      expect(turn.correct?).to eq true
+    end
+
+    it "recognizes incorrect guesses" do
+      bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      bad_turn = Turn.new("Saturn", bad_card)
+
+      expect(bad_turn.correct?).to eq false
+    end
   end
 
-  it "recognizes correct guesses" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+  describe '#feedback' do
+    it "gives feedback if correct" do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
 
-    expect(turn.correct?).to eq true
-  end
+      expect(turn.feedback).to eq "Correct!"
+    end
 
-  it "recognizes incorrect guesses" do
-    bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    bad_turn = Turn.new("Saturn", bad_card)
+    it "gives feedback if incorrect" do
+      bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      bad_turn = Turn.new("Saturn", bad_card)
 
-    expect(bad_turn.correct?).to eq false
-  end
-
-  it "gives feedback if correct" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.feedback).to eq "Correct!"
-  end
-
-  it "gives feedback if incorrect" do
-    bad_card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    bad_turn = Turn.new("Saturn", bad_card)
-
-    expect(bad_turn.feedback).to eq "Incorrect."
+      expect(bad_turn.feedback).to eq "Incorrect."
+    end
   end
 end
