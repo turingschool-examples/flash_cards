33cf3363f21e6d2f85e495ca7730bf5219d067cc
Brant Fuller <brant.fuller@gmail.com>
2021-03-25 15:26:44 -0500

    Refactor correct? method

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,16 +6,8 @@ class Turn
   end
 
   def correct?
-    if @guess == @card.answer
-      return true
-    else
-      return false
-    end
+    @guess == @card.answer
   end
-
-  # def correct?
-  #   @guess == @card.answer
-  # end
 
   def feedback
     if correct?
