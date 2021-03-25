9ebcfb8c711621fb5d5e1a0fe428c75f15b5ae1b
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-03-25 17:00:00 -0600

    add further tests to turn_spec and create turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,10 @@
 class Turn
+  attr_reader :guess,
+              :card,
+              :category
   def initialize(guess, card)
+    @guess = guess
+    @card = card
+    @category = category
   end
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,26 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new('Juneau', card)
+    expect(turn).to be_instance_of(Turn)
+  end
+end
+
+RSpec.describe Turn do
+  it 'it has guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new('Juneau', card)
+    expect(turn.guess).to eq("Juneau")
+  end
+end
+
+RSpec.describe Turn do
+  it 'it has card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new('Juneau', card)
+    expect(turn.card).to eq(Card)
+  end
+end
