e0fefa4a7d1bab5f1232c27a0891121b242e842f
Leah Riffell <leah.riffell@gmail.com>
2021-03-25 18:28:27 -0400

    Add turn class and intial tests

---
+++ spec/turn_spec.rb
@@ -0,0 +1,21 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+  before :each do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
+  it 'exists' do
+    expect(@turn).to be_instance_of(Turn)
+  end
+
+  it 'has a card' do
+    expect(@turn.card).to eq(@card)
+  end
+
+  it 'has a guess' do
+    expect(@turn.guess).to eq('Juneau')
+  end
+end
