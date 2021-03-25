f4b66a298fa1ac5c00e520edfacbdb0f3173f892
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-25 15:18:29 -0700

    Separate specs

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -3,8 +3,8 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-describe do
-  context '#initialize' do
+describe Round do
+  context 'initialize' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -20,7 +20,7 @@ describe do
     end
   end
 
-  context '#start of round' do
+  context 'start of round' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -31,12 +31,12 @@ describe do
       expect(round.turns).to eq([])
     end
 
-    it 'can show the current card' do
+    xit 'can show the current card' do
       expect(round.current_card).to eq(card_1)
     end
   end
 
-  context '#new turn' do
+  context 'new turn with correct guess' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -45,23 +45,32 @@ describe do
 
     new_turn = round.take_turn("Juneau")
 
-    it 'can take a turn where the answer is correct' do
+    it 'creates a new turn' do
       expect(new_turn.class).to eq(Turn)
-      expect(new_turn.card).to eq(card_1)
+    end
+
+    it 'can match the guess to the answer' do
       expect(new_turn.guess).to eq(card_1.answer)
+    end
+
+    it 'can tell if the guess is correct' do
       expect(new_turn.correct?).to eq(true)
     end
 
-    it 'has a turn object in the array' do
+    it 'can put the new turn in the turns array' do
       expect(round.turns).to eq([new_turn])
-      expect(round.deck).to eq(deck)
+    end
+
+    it 'has a new current card' do
+      expect(round.current_card).to eq(card_2)
     end
 
     it 'can give the number of correct answers' do
       expect(round.number_correct).to eq(1)
     end
+  end
 
-    it 'can take a turn where the answer is false' do
+  context 'new turn with an Incorrect guess' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -69,19 +78,22 @@ describe do
     round = Round.new(deck)
 
     round.take_turn("Juneau")
-
-    expect(round.current_card).to eq(card_2)
-    expect(round.number_correct).to eq(1)
-
     round.take_turn("Venus")
 
-    expect(round.turns.count).to eq(2)
-    expect(round.turns.last.feedback).to eq("Incorrect.")
-    expect(round.number_correct).to eq(1)
+    it 'can count amount of turns taken' do
+      expect(round.turns.count).to eq(2)
+    end
+
+    it 'can give accurate feedback' do
+      expect(round.turns.last.feedback).to eq("Incorrect.")
+    end
+
+    it 'can give amount correct after taking two turns' do
+      expect(round.number_correct).to eq(1)
     end
   end
 
-  context '#correct by category' do
+  context 'correct by category' do
     xit 'number correct in Geography' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
