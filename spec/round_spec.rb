85e0d6ae48d3caabdd7133f860b637aac5834ccf
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-25 18:32:09 -0700

    Create correct by category specs

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -31,7 +31,7 @@ describe Round do
       expect(round.turns).to eq([])
     end
 
-    xit 'can show the current card' do
+    it 'can show the current card' do
       expect(round.current_card).to eq(card_1)
     end
   end
@@ -94,30 +94,25 @@ describe Round do
   end
 
   context 'correct by category' do
-    xit 'number correct in Geography' do
-      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-      deck = Deck.new([card_1, card_2, card_3])
-      round = Round.new(deck)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
 
-      round.take_turn("Juneau")
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
 
-      expect(round.current_card).to eq(card_2)
-      expect(round.number_correct).to eq(1)
+      it 'can total correct answers in Geography category' do
+        expect(round.number_correct_by_category(:Geography)).to eq(1)
+      end
 
-      round.take_turn("Venus")
-
-      expect(round.turns.count).to eq(2)
-      expect(round.number_correct).to eq(1)
-
-      expect(round.number_correct_by_category(:Geography)).to eq(1)
-    end
+      it 'can total correct answers in :STEM category' do
+        expect(round.number_correct_by_category(:STEM)).to eq(0)
+      end
   end
 end
 
-# round.number_correct_by_category(:STEM)
-# #=> 0
 # round.percent_correct
 # #=> 50.0
 # round.percent_correct_by_category(:Geography)
