3eb7e5808c30d2f54dbd1301e9819f61d1abe2ac
NoahZinter <noah.zinter@gmail.com>
2021-03-26 16:12:04 -0600

    Test .correct? on .take_turn method in .round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -71,9 +71,44 @@ describe Round do
       cards = [card_1, card_2, card_3]
       deck = Deck.new(cards)
       round = Round.new(deck)
+      new_turn = round.take_turn("Juneau")
 
-      expect(round.take_turn("Juneau")).to be_a(Turn)
+      expect(new_turn.class).to eq(Turn)
+
     end
+
+    it 'reads correct guesses' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+        and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5°
+         clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      new_turn = round.take_turn("Juneau")
+
+      expect(new_turn.correct?).to eq(true)
+    end
+
+
+
+
+    # it 'adds the turn to @turns array' do
+    #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+    #     and reports about the surface of which planet?", "Mars", :STEM)
+    #   card_3 = Card.new("Describe in words the exact direction that is 697.5°
+    #      clockwise from due north?", "North north west", :STEM)
+    #   cards = [card_1, card_2, card_3]
+    #   deck = Deck.new(cards)
+    #   round = Round.new(deck)
+    #   round.take_turn("Juneau")
+    #
+    #   expect(round.turns[0]).to be_a(Turn)
+    # end
+
+
   end
 
 
