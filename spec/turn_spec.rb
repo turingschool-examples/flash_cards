890db068b3ddca71e38d37ce584e3b67a7e0b684
NoahZinter <>
2021-03-26 11:45:23 -0600

    Create .card test, pass it

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -19,6 +19,14 @@ describe Turn do
 
       expect(turn.guess).to eq("Juneau")
     end
-  end 
+
+    it 'has a card' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
+
+      expect(turn.card).to eq(card)
+    end 
+
+  end
 
 end
