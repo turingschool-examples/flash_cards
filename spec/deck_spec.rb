72cb529dc8cf9d28bcd38d88544930ddb985f964
NoahZinter <noah.zinter@gmail.com>
2021-03-26 12:28:13 -0600

    Create .categorize and .cards_in_category tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -45,6 +45,36 @@ describe '#count' do
   end
 end
 
+describe '#categorize' do
+  xit 'puts cards in categories' do 
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+      and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+       clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.categorize(:STEM)).to eq([card_2, card_3])
+  end
+end
+
+
+
+describe '#cards_in_category' do
+  xit 'counts cards by category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+      and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+       clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq(2)
+  end
+end
+
 
 
 
