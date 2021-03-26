5be8995bd248718333383570f54443a343603725
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-25 20:21:52 -0700

    Create precent_correct_by_category specs

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -11,13 +11,13 @@ describe Round do
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
 
-    it 'exists' do
-      expect(round).to be_instance_of(Round)
-    end
+      it 'exists' do
+        expect(round).to be_instance_of(Round)
+      end
 
-    it 'has a deck' do
-      expect(round.deck).to eq(deck)
-    end
+      it 'has a deck' do
+        expect(round.deck).to eq(deck)
+      end
   end
 
   context 'start of round' do
@@ -45,29 +45,29 @@ describe Round do
 
     new_turn = round.take_turn("Juneau")
 
-    it 'creates a new turn' do
-      expect(new_turn.class).to eq(Turn)
-    end
+      it 'creates a new turn' do
+        expect(new_turn.class).to eq(Turn)
+      end
 
-    it 'can match the guess to the answer' do
-      expect(new_turn.guess).to eq(card_1.answer)
-    end
+      it 'can match the guess to the answer' do
+        expect(new_turn.guess).to eq(card_1.answer)
+      end
 
-    it 'can tell if the guess is correct' do
-      expect(new_turn.correct?).to eq(true)
-    end
+      it 'can tell if the guess is correct' do
+        expect(new_turn.correct?).to eq(true)
+      end
 
-    it 'can put the new turn in the turns array' do
-      expect(round.turns).to eq([new_turn])
-    end
+      it 'can put the new turn in the turns array' do
+        expect(round.turns).to eq([new_turn])
+      end
 
-    it 'has a new current card' do
-      expect(round.current_card).to eq(card_2)
-    end
+      it 'has a new current card' do
+        expect(round.current_card).to eq(card_2)
+      end
 
-    it 'can give the number of correct answers' do
-      expect(round.number_correct).to eq(1)
-    end
+      it 'can give the number of correct answers' do
+        expect(round.number_correct).to eq(1)
+      end
   end
 
   context 'new turn with an Incorrect guess' do
@@ -80,17 +80,17 @@ describe Round do
     round.take_turn("Juneau")
     round.take_turn("Venus")
 
-    it 'can count amount of turns taken' do
-      expect(round.turns.count).to eq(2)
-    end
+      it 'can count amount of turns taken' do
+        expect(round.turns.count).to eq(2)
+      end
 
-    it 'can give accurate feedback' do
-      expect(round.turns.last.feedback).to eq("Incorrect.")
-    end
+      it 'can give accurate feedback' do
+        expect(round.turns.last.feedback).to eq("Incorrect.")
+      end
 
-    it 'can give amount correct after taking two turns' do
-      expect(round.number_correct).to eq(1)
-    end
+      it 'can give amount correct after taking two turns' do
+        expect(round.number_correct).to eq(1)
+      end
   end
 
   context 'correct by category' do
@@ -111,11 +111,29 @@ describe Round do
         expect(round.number_correct_by_category(:STEM)).to eq(0)
       end
   end
-end
 
-# round.percent_correct
-# #=> 50.0
-# round.percent_correct_by_category(:Geography)
-# #=> 100.0
+  context 'correct percentages' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+      it 'can find percent correct overall' do
+        expect(round.percent_correct).to eq(50.0)
+      end
+
+      it 'can find percent correct by category' do
+        expect(round.percent_correct_by_category(:Geography)).to eq 100.0
+      end
+
+      it 'only has one card left' do
+        expect(round.current_card).to eq(card_3)
+      end
+  end
+end
 # round.current_card
 # #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
