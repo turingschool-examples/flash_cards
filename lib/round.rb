c1ec972d6e28740cd66901d9e9d939fec6f0a693
NoahZinter <noah.zinter@gmail.com>
2021-03-26 17:17:28 -0600

    Create flashcard_runner, set up Start method

--- lib/round.rb
+++ lib/round.rb
@@ -48,13 +48,21 @@ class Round
 
   def percent_correct_by_category(subject)
     correct = number_correct_by_category(subject)
-    total = @turns.select do |turn| 
+    total = @turns.select do |turn|
       turn.card.category == subject
     end
     total = total.length
     percent = (correct.to_f / total.to_f) * 100
     percent.truncate(2)
   end
+
+  def start
+
+  end
+
+  def get_guess
+
+  end 
 
 
 
