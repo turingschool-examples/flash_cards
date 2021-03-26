e44fff2b50e436e3e612df6138b66f0a58e24ed1
Gunnar <dunkcrunk.com>
2021-03-26 09:10:02 -0600

    Finish correct percentage by category

--- lib/round.rb
+++ lib/round.rb
@@ -30,4 +30,14 @@ class Round
             turn.correct? && turn.card.category == category
         end.length
     end
+
+    def percent_correct
+        (number_correct.to_f / @turns.length.to_f).round(4) * 100
+    end
+
+    def percent_correct_by_category(category)
+        number_correct = number_correct_by_category(category).to_f
+        total = (@deck.cards_in_category(category).length).to_f
+        (number_correct / total) * 100
+    end
 end
