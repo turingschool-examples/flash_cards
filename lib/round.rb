6ba3a1a47ac11eb86c94ccfcbff0e78004db6da7
NoahZinter <noah.zinter@gmail.com>
2021-03-26 15:54:11 -0600

    Create #current_card test, pass it

--- lib/round.rb
+++ lib/round.rb
@@ -1,10 +1,14 @@
 class Round
 
   attr_reader :deck,
-              :turns 
+              :turns
   def initialize(deck)
     @deck = deck
     @turns = []
   end
+
+  def current_card
+    deck.cards.first
+  end 
 
 end
