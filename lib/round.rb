eb30476019d52ea55fcb4d41fe212d6636b40619
Suzie K <suziekim.dev@gmail.com>
2021-03-26 11:02:29 -0400

    Add pseudo code for number_correct_by_category method

--- lib/round.rb
+++ lib/round.rb
@@ -29,5 +29,19 @@ class Round
     end
     correct_turns.length 
   end 
+
+  def number_correct_by_category(specific_category) 
+    #check if guess is correct according to category 
+    #round >> deck >> cards_in_category
+    #round >> turns >> card >> correct? 
+    #return the number of in correct by category array
+    correct_by_category = deck.find_all do |turn|
+      deck.card.category.correct? 
+    end
+    correct_by_category.length
+
+    #collect array of correct cards first
+    #then sort by the category
+  end
 end
  
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -61,11 +61,11 @@ describe Round do
     end
 
     it 'takes new turn and rotates current card' do
-      card_1   = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      card_2   = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-      card_3   = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-      deck     = Deck.new([card_1, card_2, card_3])
-      round    = Round.new(deck)
+      card_1     = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2     = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3     = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck       = Deck.new([card_1, card_2, card_3])
+      round      = Round.new(deck)
       new_turn_1 = round.take_turn("Juneau")
       new_turn_2 = round.take_turn("Venus")
 
@@ -81,7 +81,7 @@ describe Round do
       round    = Round.new(deck)
       new_turn = round.take_turn("Juneau")
       new_turn = round.take_turn("Venus")
-      
+     
       expect(round.turns.count).to eq(2)
     end
   end
@@ -108,8 +108,21 @@ describe Round do
       round    = Round.new(deck)
       new_turn = round.take_turn("Denver")
 
-      expect(new_turn.correct?).to eq true
-      expect(new_turn.feedback).to eq 'Correct.'
+      expect(new_turn.correct?).to eq false
+      expect(new_turn.feedback).to eq 'Incorrect.'
     end
+  end
+
+  describe '#correct_by_category' do
+    it 'returns number correct by category'
+    card_1     = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2     = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3     = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck       = Deck.new([card_1, card_2, card_3])
+    round      = Round.new(deck)
+    new_turn   = round.take_turn("Juneau")
+    new_turn   = round.take_turn("Venus")
+    
+    expect(round.number_correct_by_category(:Geography)).to eq 1
   end
 end
