93dbc672827fc8eb976ec830a4a00b7c068bc0b8
Sarah Rudy <sarahrudy@gmail.com>
2021-03-26 12:17:59 -0600

    Add 'cards_in_category' method

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,7 +5,15 @@ class Deck
     @cards = cards
   end
 
-  def count
-    @cards.length
+    def count
+      @cards.length
+    end
+
+  def cards_in_category(category)
+    @cards.find_all do |card|
+      card.category == category
+# because we want the card.category to match the category we've chosen
+    end
   end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -24,7 +24,7 @@ describe Deck do
     expect(deck.cards).to eq(cards)
   end
 
-  it 'counts cards in deck' do
+  it 'count cards in deck' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -33,8 +33,16 @@ describe Deck do
 
     expect(deck.count).to eq(3)
   end
-end
 
-# deck.cards_in_category(:STEM)
-# deck.cards_in_category(:Geography)
-# deck.cards_in_category("Pop Culture")
+  it 'count cards in deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+    expect(deck.cards_in_category(:Geography)).to eq([card_1])
+    expect(deck.cards_in_category("Pop Culture")).to eq([])
+  end
+end
