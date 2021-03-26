fef8e5bfd02d9567704ed7260e7c7864f90334cd
Jamison Griffith <jamison.griffith@gmail.com>
2021-03-26 10:20:05 -0600

    Refactor card test, format card class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,24 +1,24 @@
 require_relative '../lib/card.rb' # changed to require_relative
 
 RSpec.describe Card do
+  describe "#initialize" do
+    card = Card.new("What is the capital of Alaska?",
+          "Juneau", :Geography)
 
-  before :each do
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  end
+    it 'exists' do
+      expect(card).to be_instance_of(Card)
+    end
 
-  it 'exists' do
-    expect(@card).to be_instance_of(Card)
-  end
+    it 'has a question' do
+      expect(card.question).to eq("What is the capital of Alaska?")
+    end
 
-  it 'has a question' do
-    expect(@card.question).to eq("What is the capital of Alaska?")
-  end
+    it 'has an answer' do
+      expect(card.answer).to eq("Juneau")
+    end
 
-  it 'has an answer' do
-    expect(@card.answer).to eq("Juneau")
-  end
-
-  it 'has a category' do
-    expect(@card.category).to eq(:Geography)
+    it 'has a category' do
+      expect(card.category).to eq(:Geography)
+    end
   end
 end
