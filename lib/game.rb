5629a17f7d55aaf39c43bb517f9ceaa409a262be
Netia Ingram <netia1128@hotmail.com>
2021-03-26 17:46:44 -0600

    Refactor game class

--- lib/game.rb
+++ lib/game.rb
@@ -4,7 +4,7 @@ include ActionView::Helpers::NumberHelper
 class Game
   attr_reader :categories
 
-  def initialize(is_test)
+  def initialize
     @card_1 = Card.new("What is 5 + 5?", "10", "STEM")
     @card_2 = Card.new("What is Rachels favorite animal?", "toad", "Turing Staff")
     @card_3 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff")
@@ -16,7 +16,6 @@ class Game
     @game = self
     @guess = ""
     @categories = []
-    @is_test = is_test
   end
 
   def start
@@ -24,34 +23,34 @@ class Game
   end
 
   def message
-    until @turn == @cards.count
+    (@cards.count + 1).times do
       if @turn == 1
         puts "Welcome! You're playing with #{@deck.cards.count} cards."
         puts "-------------------------------------------------"
-        puts "This is card number #{@turn} out of #{@cards.count}."
-        puts "Question: #{@cards[@turn -1 ].question}"
-        @turn += 1
-        @guess = gets.chomp
-        @round.take_turn(@guess)
+        turn_steps
       elsif @turn <= @cards.count
-        puts "#{@round.turns.last.feedback}"
-        @round.take_turn(@guess)
+        turn_steps
       else
         puts "#{@round.turns.last.feedback}"
         puts "****** Game over! ******"
         puts "You had #{@round.number_correct} out of #{@cards.count} for a total score of #{number_to_percentage(@round.percent_correct, :precision => 0)}."
-        #determines which categories exist in deck
         @round.turns.each do |turn|
-          if @categories.include? turn.card.category
-          else
+          if @categories.exclude? turn.card.category
             @categories << turn.card.category
           end
         end
-        #reports on score for each category
       @categories.each do |category|
           puts "#{category} - #{number_to_percentage(@round.percent_correct_by_category(category), :precision => 0)} correct"
         end
       end
     end
+  end
+
+  def turn_steps
+    puts "This is card number #{@turn} out of #{@cards.count}."
+    puts "Question: #{@cards[@turn -1 ].question}"
+    @turn += 1
+    @guess = gets.chomp
+    @round.take_turn(@guess)
   end
 end
