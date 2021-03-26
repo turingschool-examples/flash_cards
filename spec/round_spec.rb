87cc18e0ad8834cd44d11ec0f5bf7baaa7597899
NoahZinter <noah.zinter@gmail.com>
2021-03-26 16:34:24 -0600

    Test that @turns takes appropriate number of turn elements

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -122,6 +122,24 @@ describe Round do
 
       expect(round.turns[0]).to be_a(Turn)
     end
+
+    it 'add multiple cards to @turns array ' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+        and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5°
+         clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      new_turn = round.take_turn("Juneau")
+      round.next_turn(new_turn)
+      second_turn = round.take_turn("Pluto")
+      round.next_turn(second_turn)
+
+      expect(round.turns.count).to eq(2)
+    end 
+
   end
 
   describe '#number_correct' do
