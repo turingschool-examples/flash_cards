fda3c98d278eab139a78c6916d1e0cbaa34a6421
Jermaine Braumuller <braumuller02@gmail.com>
2021-03-26 15:44:56 +0100

    Added rspec test for deck class

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,10 +1,12 @@
 require './lib/card'
 
 
-RSpec.describe Turn do
+RSpec.describe Deck do
   it 'exists' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    expect(deck).to be_instance_of(Deck)
   end
 end
