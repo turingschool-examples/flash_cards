bee9d0043a7a42cb4df3cc423d7896b715e848bb
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-26 17:01:34 -0700

    Refactor methods

--- lib/round.rb
+++ lib/round.rb
@@ -16,29 +16,19 @@ class Round
   def take_turn(guess)
     new_turn = Turn.new(guess, current_card)
     @turns << new_turn
-    @deck.cards.shift #may need to change this back to rotate!
+    @deck.cards.shift 
     new_turn
   end
 
-  def number_correct #want to refactor this
-    amount_correct = []
-    @turns.each do |turn|
-      if turn.guess == turn.card.answer
-        amount_correct << turn.card
-      end
+    @turns.count do |turn|
+      turn.card.answer == turn.guess
     end
-    amount_correct.length
   end
-    # @turns.find_all do |turn|
-    #   if turn.card.answer == turn.guess
-    #     @amount_correct += 1
-    #   end
-    # end
 
   def number_correct_by_category(category)
-    @turns.find_all do |turn|
-      turn.card.category == category && turn.card.answer == turn.guess #want to refactor this
-    end.length
+    @turns.count do |turn|
+      turn.card.category == category && turn.card.answer == turn.guess
+    end
   end
 
   def percent_correct
@@ -49,13 +39,9 @@ class Round
     (number_correct_by_category(category).to_f / correct_category(category)) * 100
   end
 
-  def correct_category(category) #refactor
-    cat = []
-    @turns.each do |turn|
-      if turn.card.category == category
-        cat << turn.card
-      end
+  def correct_category(category)
+  @turns.count do |turn|
+    turn.card.category == category
     end
-    cat.length
   end
 end
