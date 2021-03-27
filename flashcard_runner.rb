915608f8ffd449b9f21caaf0bbf728ded34bbbc4
Molly Krumholz <mkkrumholz@gmail.com>
2021-03-27 17:54:09 -0600

    Update runner for CardGenerator rather than hard-coded cards

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,15 +1,14 @@
-# source https://www.parents.com/fun/activities/indoor/trivia-questions-for-kids/
-require './lib/game'
-require './lib/round'
-require './lib/card'
-require './lib/deck'
-require './lib/turn'
+# extra trivia source: https://www.parents.com/fun/activities/indoor/trivia-questions-for-kids/
 
-card_1 = Card.new("How many teeth does an adult human have?", "32", :Science)
-card_2 = Card.new("How many noses does a slug have?", "4", :Science)
-card_3 = Card.new("Where was Martin Luther King, Jr. born?", "Atlanta", :History)
-card_4 = Card.new("Which was the first country to use paper money?", "China", :History)
-deck = Deck.new([card_1, card_2, card_3, card_4])
+require './lib/card_generator'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+require './lib/game'
+
+card_generator = CardGenerator.new("cards.txt")
+deck = Deck.new(card_generator.cards)
 round = Round.new(deck)
 game = Game.new(round)
 
