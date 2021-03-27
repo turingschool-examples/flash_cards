9a7ac60c4208fe33a3b5b6dfd73f17de8c2cb26c
NoahZinter <noah.zinter@gmail.com>
2021-03-27 11:56:15 -0600

    Create card_generator_spec.rb

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,14 @@
+require './lib/card.rb'
+require './lib/turn.rb'
+require './lib/deck.rb'
+require './lib/round.rb'
+require './files/cards.txt'
+require './lib/flashcard_runner'
+require 'pry'
+
+describe CardGenerator do
+  it 'exists' do
+
+  end 
+
+end
