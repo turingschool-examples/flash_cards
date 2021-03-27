d689def3569426bc20782b004583474e69f22028
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-03-27 17:24:34 -0600

    add count and reads methods to deck_spec

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,26 @@
 class Deck
   attr_reader :cards,
+              :count,
               :cards_in_category
 
   def initialize(count, cards_in_category)
     @cards = []
     @count = count
     @cards_in_category = cards_in_category
+  end
+
+  def add_cards(card)
+    @cards << card
+  end
+
+  def count_cards
+    @count = cards.count #or .length?
+  end
+
+  def cards_in_category
+    result = []
+    cards.each do |card|
+      return << card if card.last?
+    end
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -10,11 +10,54 @@ describe Deck do
     end
 
     it 'has cards' do
+      expect(deck.cards).to eq []
+    end
+
+    it 'can count cards' do
+     expect(deck.count).to eq 0
+    end
+
+    it 'reads the cards' do
+     expect(deck.cards_in_category).to eq :STEM
+    end
+  end
+
+  describe '#add_cards' do
+    it 'can add cards' do
+      deck = Deck.new(0, :STEM)
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-      expect(deck.cards).to eq []
+      deck.add_cards(card_1)
+      deck.add_cards(card_2)
+      deck.add_cards(card_3)
+      expect(deck.cards).to eq [card_1, card_2, card_3]
+    end
+  end
+
+  describe '#count_cards' do
+    it 'can count cards' do
+     deck = Deck.new(0, :STEM)
+     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+     deck.add_cards(card_1)
+     deck.add_cards(card_2)
+     deck.add_cards(card_3)
+     expect(deck.count_cards).to eq 3
+    end
+  end
+
+  describe '#cards_in_category' do
+    it 'reads the cards' do
+      deck = Deck.new(0, :STEM)
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck.add_cards(card_1)
+      deck.add_cards(card_2)
+      deck.add_cards(card_3)
+     expect(deck.cards_in_category).to eq :STEM
     end
   end
 end
-#
