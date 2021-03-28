7ff08b290a8ff9ad4e53e1e749f00dbf3fa5f5bc
JoePeterson51 <>
2021-03-28 17:24:49 -0600

    Final refactor

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -1,3 +1,3 @@
 require './lib/game'
-game = Game.new
+game = Game.new 
 game.start 
--- lib/game.rb
+++ lib/game.rb
@@ -15,7 +15,7 @@ class Game
   def load_cards
     card_data = CSV.open("cards.txt")
     card_data.map do |row|
-      row = Card.new(row[0], row[1], row[2].gsub(" ", "_").to_sym)
+      row = Card.new(row[0], row[1].downcase, row[2])
     end 
   end 
 
@@ -24,28 +24,37 @@ class Game
     round = Round.new(deck)
     total_deck_num = deck.count 
     turns_array_counter = 0 
-
-    puts "Welcome! You're playing with 4 cards."
-    puts "                  _                  "
-    puts "              .__(.)<  (MEOW)        "
-    puts "               \\___)                "
-    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    
+    puts "Welcome! You're playing with #{total_deck_num} cards."
+    puts "           _                  "
+    puts "       .__(.)< (Lets test your brain!)"
+    puts "        \\___)                "
+    puts "~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~"
 
     until deck.cards.empty? == true 
-        puts "This is card number #{card_num} out of #{total_deck_num}."
-        puts "Question: #{round.current_card.question}"
-        guess = gets.chomp
-        round.take_turn(guess)
-        puts round.turns[turns_array_counter].feedback
-        puts "-------------------------------------------------"
-        @card_num += 1
-        turns_array_counter += 1
+      puts "This is card number #{card_num} out of #{total_deck_num}."
+      puts "Question: #{round.current_card.question}"
+      guess = gets.chomp.downcase
+      round.take_turn(guess)
+      puts round.turns[turns_array_counter].feedback
+      puts "-------------------------------------------------"
+      @card_num += 1
+      turns_array_counter += 1
     end 
 
     puts "****** Game over! ******"
+    puts "           _                  "
+    puts "       .__(.)< (Here's your score!)"
+    puts "        \\___)                "
+    puts "~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~"
     puts "You had #{round.number_correct} out of #{total_deck_num} for a total score of #{round.percent_correct}."
-    puts "STEM - #{round.percent_correct_by_category(:STEM)} correct"
-    puts "Turing Staff - #{round.percent_correct_by_category(:Turing_Staff)} correct"
-    puts "Pop Culture - #{round.percent_correct_by_category(:PopCulture)} correct"
+    
+    categorys = round.turns.map do |turn|
+      turn.card.category
+    end.uniq
+
+    categorys.each do |category|
+      puts "#{category} - #{round.percent_correct_by_category(category)} correct"
+    end
   end 
 end 
