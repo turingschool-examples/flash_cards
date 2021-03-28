e4ee6dfd607e599eb36886d30c836b6bc4025806
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-27 20:19:28 -0700

    Refactor methods, create game_play method

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,6 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
-require './lib/messages'
 
 class Game
   attr_reader  :card_1,
@@ -10,9 +9,7 @@ class Game
                     :card_3,
                     :card_4,
                     :deck,
-                    :turns,
                     :round,
-                    :messages,
                     :begin,
                     :user_input
 
@@ -22,9 +19,7 @@ class Game
     @card_3 = Card.new("Is an avocado a fruit or a vegetable?", "fruit", :Science)
     @card_4 = Card.new("What is the return value of .each?", "the original array", :Wisdom)
     @deck = Deck.new([card_1, card_2, card_3, card_4])
-    @turns = []
     @round = Round.new(deck)
-    # @messages = Messages.new(card_1, card_2, card_3, card_4)
     @begin = 0
     @user_input = user_input
   end
@@ -32,41 +27,21 @@ class Game
   def start_game
     @begin = deck.count
     beginning
-      until round.deck.cards.empty? == true
-        puts card_out_of
-        puts questions
-
-        print "> "
-        @user_input = $stdin.gets.chomp.downcase
-        round.take_turn(@user_input)
-        puts "Gosh, great job! You got that right!" # FIX you have this hard coded
-        puts separator
-      end
+    game_play
     ending_message
   end
 
-  def card_out_of
-    "This is card number #{round.card_num} out of #{@begin}."
-  end
-
-  def questions
-    "Question: #{round.current_card.question}"
-  end
-
-  def separator
-    "- " * 50
-  end
-
-  def welcome
-    "Welcome to flashcards!"
-  end
-
-  def amount_of_cards
-    "You're playing with #{deck.count} cards."
-  end
-
-  def quit_info
-    "You can press 'q' to quit at any time....but it would be really fun if you play!"
+  def game_play
+    while round.deck.cards.empty? != true
+      puts card_out_of
+      puts questions
+      print "> "
+      @user_input = $stdin.gets.chomp.downcase
+      puts feedback
+      round.take_turn(@user_input)
+      puts separator
+      break if @user_input == "q"
+    end
   end
 
   def beginning
@@ -77,91 +52,48 @@ class Game
     puts separator
   end
 
-  def incorrect
-    "Sorry, that's incorrect. Please try again."
+  def separator
+    "- " * 50
   end
 
-  def quit
-    "Okay, guess you're done now, bummer."
+  def welcome
+    "✨ " * 5 + "Welcome to flashcards!" + "✨ " * 5
+  end
+
+  def amount_of_cards
+    "You're playing with #{deck.count} cards."
+  end
+
+  def feedback
+    if round.current_card.answer == @user_input
+      "Gosh, great job! You got that right!"
+    else
+      "Sorry, that's not quite right."
+    end
+  end
+
+  def quit_info
+    "You can press 'q' to quit at any time....but it would be really fun if you play!"
+  end
+
+  def card_out_of
+    "This is card number #{round.card_num} out of #{@begin}."
+  end
+
+  def questions
+    "Question: #{round.current_card.question}"
+  end
+
+  def incorrect
+    "Sorry, that's incorrect."
   end
 
   def ending_message
-    puts "~ ~ ~ ~ ~ ~ ~ The game is finished, you did it! ~ ~ ~ ~ ~ ~ ~"
-    puts "You had #{round.number_correct} correct guesses out of #{@begin} for a total schore of #{round.percent_correct}%."
+    puts "✨ " * 5 + "The game is finished, YOU DID IT!" + "✨ " * 5
+    puts "You had #{round.number_correct} correct guesses out of #{@begin} for a total score of #{round.percent_correct.to_i}%."
     puts "Education - #{round.percent_correct_by_category(:Education)}"
     puts "Wisdom - #{round.percent_correct_by_category(:Wisdom)}"
     puts "Science - #{round.percent_correct_by_category(:Science)}"
-    puts "~ " * 50
+    puts "- " * 50
   end
-
-
-
-
-
-
-  # def first_question #WORKS
-  #   puts messages.card_out_of(card_1)
-  #   puts messages.questions
-  #
-  #   print "> "
-  #   @user_input = $stdin.gets.chomp
-  #   second_question
-  #   round.take_turn("turing")
-  # end
-  #
-  # def second_question
-  #   if @user_input == round.current_card.answer
-  #     puts messages.separator
-  #     puts messages.card_out_of(card_2)
-  #     puts messages.questions
-  #
-  #     print "> "
-  #     @user_input = $stdin.gets.chomp
-  #     third_question
-  #   elsif @user_input == "q"
-  #     puts messages.quit
-  #   end
-  # end
-  #
-  # def third_question
-  #   if @user_input == round.current_card.answer
-  #     puts "Yes! Yay! Glad you're old enough to know that one!"
-  #     puts messages.separator
-  #     puts messages.card_out_of(card_3)
-  #     puts messages.questions
-  #
-  #     print "> "
-  #     @user_input = $stdin.gets.chomp
-  #     fourth_question
-  #   elsif @user_input == "q"
-  #     puts messages.quit
-  #   end
-  # end
-  #
-  # def fourth_question
-  #   if @user_input == round.current_card.answer
-  #     puts "Correct! That was a tricky one."
-  #     puts messages.separator
-  #     puts messages.card_out_of(card_4)
-  #     puts messages.questions
-  #
-  #     print "> "
-  #     @user_input = $stdin.gets.chomp
-  #     last_answer
-  #   elsif @user_input == "q"
-  #     puts messages.quit
-  #   end
-  # end
-  #
-  # def last_answer
-  #   if @user_input == round.current_card.answer
-  #     puts "Yes! Someone sure was listening in class!"
-  #     puts messages.separator
-  #     puts "Game over, you did it!"
-  #     # puts "You had #{round.number_correct} correct guesses out of #{deck.count} for a total score of #{round.percent_correct}."
-  #     # puts "#{card.category} - #{round.percent_correct_by_category(category)}% correct"
-  #   elsif @user_input == "q"
-  #     puts messages.quit
-  #   end
-  # end
 end
