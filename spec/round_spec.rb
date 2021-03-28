66836f4c24dc8d058f888cc64dae707063017e81
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-03-28 17:39:26 -0600

    Create #reads_deck method with argument

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -12,11 +12,20 @@ describe Round do
      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
      cards =[card_1, card_2, card_3]
      deck = Deck.new(cards)
-     # deck.add_cards(card_1)
-     # deck.add_cards(card_2)
-     # deck.add_cards(card_3)
      round = Round.new(deck)
      expect(round).to be_instance_of(Round)
+    end
+  end
+
+  describe '#reads_deck' do
+    it 'can read a deck'
+     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+     cards =[card_1, card_2, card_3]
+     deck = Deck.new(cards)
+     round = Round.new(deck)
+     expect(round.deck).to eq(deck)
     end
   end
 end
