f98e6df9c72606b2607e6e6757d64fb570f9023b
Sarah Rudy <sarahrudy@gmail.com>
2021-03-28 16:46:03 -0600

    Add changes to round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -84,65 +84,27 @@ end
     end
 
 
+    # it 'goes to next card' do
+    #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    #   deck = Deck.new([card_1, card_2, card_3])
+    #   round = Round.new(deck)
+    #   new_turn = round.take_turn("Juneau")
+    #   turn_2 = round.take_turn("Venus")
+    #   turn_3 = round.take_turn("North north west")
+    #
+    #     expect(turn_3.guess).to eq("North north west")
+    #     expect(turn_3.card).to eq(card_2)
+    #     expect(round.turns.count).to eq(2)
+    #     expect(round.turns.last.feedback).to eq("Incorrect.")
+    #     expect(round.number_correct).to eq(1)
+    #     expect(round.number_correct_by_category(:Geography)).to eq(1)
+    #     expect(round.number_correct_by_category(:STEM)).to eq(0)
+    #     expect(round.percent_correct).to eq(50.0)
+    #     expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
+    #   end
+
+
 
   end
-# the point the spec file is to test whether or not the methods are working, not to play the game
-
-  # it 'turns to next card' do
-  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  #   deck = Deck.new([card_1, card_2, card_3])
-  #   round = Round.new(deck)
-  #   new_turn = round.take_turn("Juneau")
-  #
-  #   expect(new_turn.correct?).to eq(Turn)
-  #
-  # end
-
-
-
-
-
-
-# new_turn.class  #.class method that exists in Ruby
-# #=> Turn
-#
-# new_turn.correct?
-# #=> true
-#
-# round.turns
-# #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
-#
-# round.number_correct
-# #=> 1
-#
-# round.current_card
-# #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
-#
-# round.take_turn("Venus")
-# #=> #<Turn:0x00007f972a215b38...>
-#
-# round.turns.count
-# #=> 2
-#
-# round.turns.last.feedback
-# #=> "Incorrect."
-#
-# round.number_correct
-# #=> 1
-#
-# round.number_correct_by_category(:Geography)
-# #=> 1
-#
-# round.number_correct_by_category(:STEM)
-# #=> 0
-#
-# round.percent_correct
-# #=> 50.0
-#
-# round.percent_correct_by_category(:Geography)
-# #=> 100.0
-#
-# round.current_card
-# #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
