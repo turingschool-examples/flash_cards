dbd38ed0b2d073d7e0613a723a1dfa3cdd7efcef
JoannaCoPo <jococopo@gmail.com>
2021-03-28 14:38:38 -0600

    Accomodate guess edge case via .downcase

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,56 +6,20 @@ require './lib/printer'
 require 'pry'
 
 card_1 = Card.new("What is 5 + 5?", "10", :Arithmetic)
-card_2 = Card.new("What is Joanna's favorite animal?", "cheetah" || "Cheetah", :Animal_Stuff)
-card_3 = Card.new("What is the name of Megan's doggo?", "Nile" || "nile", :Animal_Stuff)
-card_4 = Card.new("Which important person in history was recently unveiled as the new face of the 50 pound note in the UK?", "Alan Turing" || "alan turing", :History)
+card_2 = Card.new("What is Joanna's favorite animal?", ("cheetah"), :Animal_Stuff)
+card_3 = Card.new("What is the name of Megan's doggo?", "nile", :Animal_Stuff)
+card_4 = Card.new("Which important person in history was recently unveiled as the new face of the 50 pound note in the UK?", "alan turing", :History)
 @deck = Deck.new([card_1, card_2, card_3, card_4])
 @round = Round.new(@deck)
 @printer = Printer.new
 
-#REFACTOR - move def of start method to aeparate class
-
-# def start #REFACTOR TO LOOP IF TIME
-#   @printer.welcome_message
-#   #card_1
-#   @printer.card_message(@round.current_card, (@round.turns.count + 1))
-#   initiate_game_flow
-#   @printer.feedback_message(@round.turns[0].guess, @round.turns[0].card.answer)
-#   #card_2
-#   @printer.card_message(@round.current_card, (@round.turns.count + 1))
-#   initiate_game_flow
-#   @printer.feedback_message(@round.turns[1].guess, @round.turns[1].card.answer)
-#   #card_3
-#   @printer.card_message(@round.current_card, (@round.turns.count + 1))
-#   initiate_game_flow
-#   @printer.feedback_message(@round.turns[2].guess, @round.turns[2].card.answer)
-#   #card_4
-#   @printer.card_message(@round.current_card, (@round.turns.count + 1))
-#   initiate_game_flow
-#   @printer.feedback_message(@round.turns[3].guess, @round.turns[3].card.answer)
-#
-#   @printer.final_score_message(@round.number_correct, @round.percent_correct)
-#
-#
-#   @deck.cards.each do |card|
-#     percents = @round.percent_correct_by_category(card.category)
-#     @printer.final_score_by_category_message(card.category, percents)
-#   end
-#
-# end
-
-#loop through deck, each_with_index |card, index|
-# @printer.card_message(@round.current_card, (@round.turns.count + 1))
-# initiate_game_flow
-#  @printer.feedback_message(@round.turns[index].guess, @round.turns[index].card.answer)
-
-def start #REFACTOR TO LOOP IF TIME
+def start
   @printer.welcome_message
 
   @deck.cards.each_with_index do |card, index|
     @printer.card_message(@round.current_card, (@round.turns.count + 1))
     initiate_game_flow
-    @printer.feedback_message(@round.turns[index].guess, @round.turns[index].card.answer)
+    @printer.feedback_message(@round.turns[index].guess.downcase, @round.turns[index].card.answer)
   end
 
   @printer.final_score_message(@round.number_correct, @round.percent_correct)
