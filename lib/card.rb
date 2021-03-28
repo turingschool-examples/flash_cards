43bf4c190969a646e8525b82ee4399406bdac407
Mitch Standley <mitch_standley@yahoo.com>
2021-03-28 11:44:40 -0500

    Finalize card

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,32 @@
 class Card
-  attr_accessor :question, :answer, :category
+  attr_accessor :question,
+                :answer,
+                :category
+
   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
   end
 end
+
+
+# def number_correct_by_category(category)
+#   @turns.count do |turn|
+#     turn.card.category == category && turn.correct?
+#   end
+# end
+#
+# def percent_correct
+#   number_correct.to_f / @turns.count.to_f * 100
+# end
+#
+# def total_cards_in_category(category)
+#   @deck.cards.count do |card|
+#     card.category == category
+#   end
+# end
+#
+# def percent_correct_by_category(category)
+#   number_correct_by_category(category).to_f / total_cards_in_category(category).to_f * 100
+# end
