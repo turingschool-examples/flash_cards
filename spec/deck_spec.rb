5f73b0c3609252dd7c8cad59421326f2cc46f697
Caleb Wittman <calebjwittman1991@gmail.com>
2021-03-28 07:50:16 -0600

    Updated tests by removing describe above most, added tests to behaviors that were not being tested

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,31 +1,38 @@
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
-require 'pry'
 
-describe Deck do
+RSpec.describe Deck do
   it 'tests for an instance' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-  end
-    expect(deck).to be_an_instance_of(Deck)
-end
 
-describe 'count' do
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it 'accepts an array' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq(cards)
+  end
+
   it 'counts number of cards in deck' do
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
   cards = [card_1, card_2, card_3]
   deck = Deck.new(cards)
-  end
-  expect(deck.count).to eq(cards.length)
-end
 
-describe 'cards_in_category' do
+  expect(deck.count).to eq(3)
+  end
+
   it 'returns cards in a given category' do
 
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -33,6 +40,7 @@ describe 'cards_in_category' do
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-  end
+
     expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+  end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,25 +3,37 @@ require './lib/card'
 
 RSpec.describe Turn do
   it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
-    expect(turn.guess).to eq(@guess)
+    expect(turn.guess).to eq("Juneau")
   end
 
   it 'accepts a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
-    expect(turn.card).to eq(@card)
+    expect(turn.card).to eq(card)
   end
 
   it 'checks correct answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.correct?).to eq(true)
   end
+
+  it ' gives feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+end
