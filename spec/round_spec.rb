b17abe7726929090fd0924bffdf654fa56f44171
Dane Brophy <heartbeat@Danes-MacBook-Pro.local>
2021-03-28 17:15:16 -0600

    Reorder methods to correct runner file error

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -6,16 +6,16 @@ require './lib/round'
 
 describe Round do
   describe '#initialize' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    deck.add_cards(card_1)
-    deck.add_cards(card_2)
-    deck.add_cards(card_3)
-    cards =[card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    round = Round.new(deck)
     it 'is a round' do
+     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+     cards =[card_1, card_2, card_3]
+     deck = Deck.new(cards)
+     # deck.add_cards(card_1)
+     # deck.add_cards(card_2)
+     # deck.add_cards(card_3)
+     round = Round.new(deck)
      expect(round).to be_instance_of(Round)
     end
   end
