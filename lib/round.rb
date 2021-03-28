8afda9e8c3cd4fe7c16448409fb820df8ecb4b6c
sami-p <sami.peterson14@gmail.com>
2021-03-28 18:23:00 -0500

    Submitted final round files

--- lib/round.rb
+++ lib/round.rb
@@ -1,16 +1,33 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck,
+              :turns,
+              :current_turn
 
   def initialize(deck)
     @deck = deck
-  end
-
-  def turns
-    []
+    @turns = []
+    @current_turn = 0
   end
 
   def current_card
-    @deck.cards[0]
+    card = @deck.cards[@current_turn]
+    @current_turn += 1
+    card
   end
 
-end
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    @turns << turn
+    turn
+  end
+
+  def number_correct
+    number_right = 0
+    @turns.each do |turn|
+      if turn.correct?
+        number_right += 1
+      end
+    end
+    number_right
+  end
+end 
