ae5c199053770ee066a5991ef02e50782f4742c5
Caleb Wittman <calebjwittman1991@gmail.com>
2021-03-28 13:25:56 -0600

    Reduced number of categories called upon in results output, is dependent upon variables

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -3,10 +3,10 @@ require './deck'
 require './turn'
 require './round'
 
-card_1 = Card.new("What is Aang's name in the Fire Nation?", "Kuzon", :popculture)
+card_1 = Card.new("What is Aang's name in the Fire Nation?", "Kuzon", :Pop_Culture)
 card_2 = Card.new("Izuku Midoriya's hero name is?", "Deku", :popculture)
-card_3 = Card.new("bb", "dd", :L)
-card_4 = Card.new("cc", "dd", :L)
+card_3 = Card.new("The Colorado Avalanche have won how many Stanley Cups(requires number spelled out)?", "Two", :Sports)
+card_4 = Card.new("In cooking; a julienne is a type of what?", "Cut", :Cooking)
 cards = [card_1, card_2, card_3, card_4]
 deck = Deck.new(cards)
 round = Round.new(deck)
@@ -16,7 +16,7 @@ puts "Welcome! You're playing with #{deck.cards.count} cards
 
 while round.cards_remaining?
 puts "This is card number #{round.turns.count + 1} out of #{round.deck.cards.count}
-Question: #{deck.cards[0].question}\n"
+Question: #{cards[0].question}\n"
 round.take_turn(gets.capitalize.chomp)
 puts "#{round.turns[-1].feedback}\n"
 end
@@ -24,8 +24,5 @@ end
 puts "******** Game over! *******\n
 You had #{round.number_correct} out of #{deck.cards.count} for a total score of #{round.percent_correct}\n
 #{deck.cards[0].category} - #{round.percent_correct_by_category(deck.cards[0].category)}\n
-#{deck.cards[1].category} - #{round.percent_correct_by_category(deck.cards[1].category)}\n
 #{deck.cards[2].category} - #{round.percent_correct_by_category(deck.cards[2].category)}\n
 #{deck.cards[3].category} - #{round.percent_correct_by_category(deck.cards[3].category)}"
-#perhaps use an if so that iit remains user friendly
-# so if deck.cards[1].category != deck.cards[0].category puts deck.cards[1].category
