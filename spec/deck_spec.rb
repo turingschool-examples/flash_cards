8575e8caadcb408b629935930debfd20ddb888f6
sami-p <sami.peterson14@gmail.com>
2021-03-28 16:26:33 -0500

    Added deck files

---
+++ spec/deck_spec.rb
@@ -0,0 +1,51 @@
+require './lib/card'
+require './lib/deck'
+require 'pry'
+
+RSpec.describe Deck do
+  it 'exists' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it 'counts' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.count).to eq(3)
+
+  end
+
+  it 'finds cards in a category' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+
+    geography_card = deck.cards_in_category(:Geography)
+    stem_cards = deck.cards_in_category(:STEM)
+    pop_culture_cards = deck.cards_in_category("Pop Culture")
+
+    expect(stem_cards.count).to eq(2)
+    expect(geography_card.count).to eq(1)
+    expect(pop_culture_cards).to eq([])
+    # expect(geography_cards).to incl(card_1)
+  end
+
+end
