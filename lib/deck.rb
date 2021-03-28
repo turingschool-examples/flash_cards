a2eed9d10d4d73e158c0e602279ecd5df99c175c
Caleb Wittman <calebjwittman1991@gmail.com>
2021-03-28 17:28:04 -0600

    Refactored cards_in_category

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,10 +10,8 @@ class Deck
   end
 
 
-def cards_in_category(category)
-  @cards.find_all do |card|
-    card.category == category
-  end
-end
 
+ def cards_in_category(category)
+   @cards.find_all {|card| card.category == category}
+ end
 end
