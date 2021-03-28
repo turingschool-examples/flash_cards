530cdaa2b1b59bd4002a33264b0dcb6fb65cae94
Suzie K <suziekim.dev@gmail.com>
2021-03-27 22:26:36 -0400

    Complete Iteration 3

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,7 +3,6 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-def start
   card_1 = Card.new("What is the capital of Spain?", "Madrid", :Geography)
   card_2 = Card.new("What actress has won the most Oscars?", "Katharine Hepburn", :Cinema)
   card_3 = Card.new("What is the colour of a Welsh poppy?", "Yellow", :Gardening)
@@ -13,6 +12,8 @@ def start
   cards  = [card_1, card_2, card_3, card_4, card_5, card_6]
   deck   = Deck.new(cards)
   round  = Round.new(deck)
+
+def start(cards, deck, round)
 
   greeting    = "Welcome! You're playing with #{deck.count} cards."
   line_break = "-------------------------------------------------"
@@ -32,60 +33,15 @@ def start
     new_turn = round.take_turn(user_input)
     puts new_turn.feedback
   end
+
+  end_message = "****** Game over! ******"
+  number_correct_message = "You had #{round.number_correct} guesses out of #{deck.count} for a total score of #{round.percent_correct.to_i}%."
+
+  puts end_message
+  puts number_correct_message 
+  deck.category_scores(round)
 end
 
-start
+start(cards,deck,round)
 
 
-# def start
-#   # starts Round
-
-#   #Cards - Deck - Round
-#   card_1 = Card.new("What is the capital of Spain?", "Madrid", :Geography)
-#   card_2 = Card.new("What actress has won the most Oscards?", "Katharine Hepburn", :Cinema)
-#   card_3 = Card.new("What is the colour of a Welsh poppy?", "Yellow", :Gardening)
-#   card_4 = Card.new("What is another name for a mountain ash tree?", "Rowan", :Gardening)
-#   card_5 = Card.new("What country is Prague in?", "Czech Republic", :Geography)
-#   card_6 = Card.new("Who directed the Lord of the Rings trilogy?", "Peter Jackson", :Cinema)
-#   cards  = [card_1, card_2, card_3, card_4, card_5, card_6]
-#   deck   = Deck.new(cards)
-#   round  = Round.new(deck)
-#   turn_number = round.turns.length + 1
-
-#   #CLI output 
-#   greeting    = "Welcome! You're playing with #{deck.count} cards."
-#   card_number = "This is card number #{turn_number} out of #{deck.count} cards."
-#   #should include the number of the card - add counter on card?
-#   #should include the number of cards in the deck array 
-
-#   line_break = "-------------------------------------------------"
-
-#   #turn >> card >> question
-#   #deck >> cards >> card >> question
-#   #round >> current_card >> question
-#   #check out the methods >> current_card.question
-#   question = "Question: #{round.current_card.question}"
-  
-#   #should return the question on the current card. 
-#   #new method in the round
-#   #takes answer 
-#   puts greeting
-#   puts line_break
-#   puts card_number
-#   puts question
-#   puts round.current_card
-#   print '> '
-#   user_input = gets.chomp
-
-#   new_turn = round.take_turn(user_input)
-#   #round >> current_card >> correct?
-#   puts new_turn.feedback
-
-#   #round 2
-#   turn_number = round.turns.length + 1
-#   puts round.current_card
-#   puts card_number
-#   puts question
-# end
-
-# start
