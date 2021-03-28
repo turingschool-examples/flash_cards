23c290b0254afa5e25958c1e25cdc292a1e12008
Gunnar <dunkcrunk.com>
2021-03-28 17:16:46 -0600

    Psuedo coded how to read abd write the txt file.

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,24 @@
-open = File.open(ARGV[0],  "r")
+# open = File.open(ARGV[0],  "r")
+# cards_txt = open.read
+# open.close
 
-cards_txt = open.read
 
-open.close
+class CardGenerator
+    def initialize(file)
+        # open like we did above then
+        # want to break each line and iterate over each line. 
+        # make an instance of each line passed into each line as a card.
+    end
+end
 
-puts cards_txt
+# def break_by_line
+#     line_num=0
+#     open = File.open(ARGV[0],  "r")
+#     cards_txt = open.read
+#     cards_txt.gsub!(/\r\n?/, "\n")
+#     open.each do |line|
+#     puts "#{line_num += 1} #{line}"
+#     end
+# end
+# break_by_line
+
