0e60c7ad34e30433b0270b2a738e7f6a94ffb4ff
Brant Fuller <brant.fuller@gmail.com>
2021-03-28 13:22:00 -0500

    Refactor runner file for iteration 4

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,12 +3,15 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 require './lib/game'
+require './lib/card_generator'
 
 card_1 = Card.new("What is 5 + 5?", "10", "STEM")
 card_2 = Card.new("What is Rachel's favorite animal?", "red panda", "Turing Staff")
 card_3 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff")
 card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
 deck = Deck.new([card_1, card_2, card_3, card_4])
+# cards = CardGenerator.new("cards.txt").cards
+# deck = Deck.new(cards)
 round = Round.new(deck)
 game = Game.new(round)
 
