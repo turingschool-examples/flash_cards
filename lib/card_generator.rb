d8362d459a99d46f37f7ba3421afe4fd41735017
Suzie K <suziekim.dev@gmail.com>
2021-04-01 09:00:11 -0400

    Update CardGenerator class

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,5 +1,7 @@
 require 'rspec'
 
+
+
 class CardGenerator
 
   def initialize(filename)
@@ -7,20 +9,8 @@ class CardGenerator
     @cards = []
   end
 
-  def convert_text_file
-    # this method should open the text file 
-    card_file = File.open(ARGV[0], 'r')
-    #uploads the card.txt files 
-    cards_txt = #should split the text by the commas
-    #and runs 
-    open.close
-    puts cards
-  end
+  #open cards.txt file and split each line into multiple lines. 
+  
 
-  # def 
-  #   #replaces cards = [card_1, card_2, card_3]
-  #   # map each line to a variable >> array of cards
-  #   # return array of cards 
-  # end
 end
 
