278731d9eb345350b4de1473e057387f2130e790
Suzie K <suziekim.dev@gmail.com>
2021-04-03 12:08:11 -0400

    Update read_file method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,15 +1,21 @@
-require 'rspec'
-
-
+require './lib/card'
 
 class CardGenerator
+  attr_reader :filename
 
   def initialize(filename)
     @filename = filename
     @cards = []
   end
 
-  #open cards.txt file and split each line into multiple lines. 
+  #because we are initializing a class with the filename, we won't need to use ARGV 
 
-  end
-
+  def read_file
+    file = File.open(@filename, 'r')
+    file_data = file.read
+    # iterate through file_data 
+    #try to play with what gets churned out inside file_data
+    file.close
+    return file_data
+  end 
+end
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -6,8 +6,8 @@ describe CardGenerator do
   describe 'initialize' do
     it 'exists' do
     filename = 'cards.txt'
-    cards = CardGenerator.new(cards.txt)
-
+    cards = CardGenerator.new('cards.txt')
+     
     expect(cards).is_a? CardGenerator
     end
   end
@@ -15,12 +15,13 @@ describe CardGenerator do
   describe '#read_file' do
     
     it 'reads file' do
-    cards = CardGenerator.new(cards.txt)
+    cards = CardGenerator.new('cards.txt')
+
     
-    expect(cardgenerator.read_file).to eq ("What is 5 + 5?, 10, STEM\n
-                                            What is Rachel's favorite animal?, red panda, Turing Staff\n
-                                            What is Mike's middle name?, nobody knows, Turing Staff\n
-                                            What cardboard cutout lives at Turing, Justin bieber, PopCulture")
+    expect(cards.read_file).to eq ("What is 5 + 5?, 10, STEM\n
+                                  What is Rachel's favorite animal?, red panda, Turing Staff\n
+                                  What is Mike's middle name?, nobody knows, Turing Staff\n
+                                  What cardboard cutout lives at Turing, Justin bieber, PopCulture")
     end
   end
   
