e7e492d8be15223473bf361d51d515eb06d43f69
todd-estes <elestes@gmail.com>
2021-04-21 11:27:53 -0600

    Add methods to turn class and change attr string

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,26 @@
 class Turn  
-  attr_reader :guess, :card
+  attr_reader :string, :card
   def initialize(string, card)
-    @guess = string
+    @string = string
     @card = card 
+  end 
+
+  def guess 
+    @string
+  end
+
+  def card 
+    @card 
+  end
+
+  def correct? 
+    @guess == @card.answer 
+  end
+
+  def feedback 
+    if correct?
+      "Correct!"
+    else return "Incorrect." 
+    end 
   end 
 end
