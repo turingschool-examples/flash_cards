97c5621bea17db75992c61e35f490ce6f3cc1511
Ozzie <mikeosmonson@gmail.com>
2021-06-25 11:44:21 -0600

    Complete iteration 2

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+class Turn
+  attr_reader :guess,
+              :card
+
+
+  def initialize(guess, card)
+    @card = card
+    @guess = guess
+  end
+
+  def correct?
+    @guess == @card.answer
+  end
+
+  def feedback
+    if correct? == true
+      return 'Correct!'
+    else
+      return "Incorrect."
+    end
+  end
+end
