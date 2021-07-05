2acbe1906e02f4f9eafc172f98588ec0e3d9147a
Ozzie <mikeosmonson@gmail.com>
2021-07-05 16:48:28 -0600

    Add Turn class w/attributes #correct? & #feedback

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+class Turn
+  attr_reader :guess,
+              :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    @card.answer == @guess
+  end
+
+  def feedback
+    correct? ? 'Correct!' : 'Incorrect'
+  end
+end
