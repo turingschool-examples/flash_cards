2f56dc4297c3c5035f94c0c397a926f95dd9695e
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-07-05 17:58:10 -0600

    create deck class

---
+++ lib/deck.rb
@@ -0,0 +1,19 @@
+
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    cards.count
+  end
+
+  def cards_in_category(subject)
+    cards.find_all do |card|
+      subject == card.category
+    end
+  end
+
+end
