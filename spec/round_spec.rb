1838751e88642801a37249a779806451d0c9c73c
Tanner Dale <tanner@tannerdale.com>
2021-07-05 17:45:40 -0500

    Add turn taking, correct counting tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -25,5 +25,32 @@ RSpec.describe Round do
     expect(@round.current_card).to eq(@card_1)
   end
 
+  describe 'taking turns' do
+    before(:each) do
+      @new_turn = @round.take_turn("Juneau")
+    end
 
+    it 'has turns as Turn objects' do
+      expect(@new_turn).to be_a(Turn)
+    end
+
+    it 'keeps track of turns' do
+      expect(@round.turns).to eq([@new_turn])
+    end
+
+    it 'can add another turn' do
+      @round.take_turn("Venus")
+
+      expect(@round.turns.count).to eq(2)
+    end
+
+    it 'knows total number correct' do
+      expect(@turn.number_correct).to eq(1)
+    end
+
+    it 'knows number correct by category' do
+      expect(@round.number_correct_by_category(:Geography)).to eq(1)
+      expect(@round.number_correct_by_category(:STEM)).to eq(0)
+    end
+  end
 end
