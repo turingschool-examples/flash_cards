cf3fc466e279181652b6fcab3df9cba2ea0388a7
Tanner Dale <tanner@tannerdale.com>
2021-07-05 18:04:01 -0500

    Add initialize, current_card, take_turn methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,1 +1,20 @@
 require './lib/deck'
+
+class Round
+  attr_reader :deck, :turns
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    @deck.cards[@turns.size]
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    new_turn
+  end
+end
