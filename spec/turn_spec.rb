6496d8c364bd0930cf594e68782ed9e554ecd8f9
Tanner Dale <tanner@tannerdale.com>
2021-07-05 16:58:22 -0500

    Add exist, card, guess tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Turn do
     @turn1 = Turn.new("Juneau", @card1)
     @turn2 = Turn.new("Saturn", @card2)
   end
+
+  it 'exists' do
+    expect(@turn1).to be_a(Turn)
+  end
+
+  it 'has a card as a card object' do
+    expect(@turn1.card).to eq(@card1)
+  end
+
+  it 'has a guess' do
+    expect(@turn1.guess).to eq("Juneau")
+  end
 end
