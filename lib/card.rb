1679af28c376dcd69f62c2e775db1dec479c92e9
Miriam Aceves <miriammerari02@gmail.com>
2021-07-05 18:36:02 -0400

    Create and initialize class

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,9 @@
 class Card
+  attr_reader :question, :answer, :catagory
   
+  def initialize(question, answer, catagory)
+    @question = question
+    @answer = answer
+    @catagory = catagory
+  end
 end
