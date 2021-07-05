c9d2622e3e4fffad9543a4c4a7eb40083a8644e2
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-07-05 17:59:09 -0600

    create round tests, in progress

---
+++ spec/round_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+RSpec.describe Round do
+  context 'initialize' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    it 'exists' do
+      expect(round).to be_a(Round)
+    end
+
+    it 'has a deck' do
+      expect(round.deck).to eq()
+    end
+  end
+end
