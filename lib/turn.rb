b3b4d15bb7e6f71aa594a0b9cfe2eb55700b513d
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-07-05 17:58:37 -0600

    create turn class

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    if card.answer == @guess
+      true
+    end
+  end
+
+  def feedback
+    if correct? == true
+      return "Correct!"
+    else
+      return "Incorrect."
+    end
+  end
+
+end
