6a443edc1e23909d6603e6d391b9e5fc62642784
Ozzie <mikeosmonson@gmail.com>
2021-07-05 16:53:32 -0600

    Add #card & #guess tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,7 +3,9 @@ require './lib/card'
 RSpec.describe Turn do
   before(:each) do
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card1 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     @turn = Turn.new("Juneau", @card)
+    @turn1 = Turn.new("Saturn", @card1)
   end
 
   it 'exits & has attriubtes' do
@@ -13,4 +15,11 @@ RSpec.describe Turn do
     expect(@turn.correct?).to eq(true)
     expect(@turn.feedback).to eq("Correct!")
   end
-end 
+
+  it 'returns card and feedback' do
+    expect(turn.card).to eq(@card1)
+    expect(turn.guess).to eq("Saturn")
+    expect(turn.correct?).to eq(false)
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+end
