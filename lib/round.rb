d7f3f2035b519611133966d3fe52fed68707b366
Tanner Dale <tanner@tannerdale.com>
2021-07-05 18:14:01 -0500

    Add correct calculation methods

--- lib/round.rb
+++ lib/round.rb
@@ -17,4 +17,27 @@ class Round
     @turns << new_turn
     new_turn
   end
+
+  def number_correct
+    @turns.count(&:correct?)
+  end
+
+  def number_correct_by_category(category)
+    @turns.count do |turn|
+      turn.correct? && turn.card.category == category
+    end
+  end
+
+  def percent_correct
+    (number_correct.fdiv(@turns.size) * 100).round
+  end
+
+  def percent_correct_by_category(category)
+    category_correct = number_correct_by_category(category)
+    (category_correct.fdiv(category_size(category)) * 100).round
+  end
+
+  def category_size(category)
+    @turns.count { |turn| turn.card.category == category }
+  end
 end
