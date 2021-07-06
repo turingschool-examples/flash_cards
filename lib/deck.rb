9e9a8e5f1637aab7b910c688163a16eae0dbcc53
Rowan DeLong <rowanwinzer@gmail.com>
2021-07-05 19:26:51 -0600

    Add methods to Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,22 @@
 class Deck
 
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
 
+  def count
+    @cards.size
+  end
 
+  def cards_in_category(category)
+    sorted = []
+    @cards.each do |card|
+      if card.category == category
+        sorted << card
+      end
+    end
+    return sorted
+  end
+  
 end
