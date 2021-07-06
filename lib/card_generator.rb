86af1a760ce9e6038d3b54e760b6b632d9dfb06b
Tanner Dale <tanner@tannerdale.com>
2021-07-05 21:26:41 -0500

    Add card generator class

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,2 +1,45 @@
 require './lib/card'
-require './resources/cards.txt'
+
+class CardGenerator
+  def file_reader
+    file = File.open("./resources/cards.txt")
+    cards = make_cards(file)
+    file.close
+    cards
+  end
+
+  def make_cards(file)
+    raw_cards = file.read.split("\n")
+    raw_cards.map do |card|
+      process_card(card)
+    end
+  end
+
+  def process_card(card)
+    question, answer, category = card.split(",")
+    category = format_category(category)
+
+    create_card(question, answer, category)
+  end
+
+  def format_category(category)
+    if category.count(" ") >= 1
+      category = category.split
+      formatted = category.join("_")
+    else
+      formatted = parse_category(category)
+    end
+    formatted.to_sym
+  end
+
+  def parse_category(category)
+    words = category.scan(/[A-Z][a-z]+/)
+    words.size >= 1 ? words.join("_") : category
+  end
+
+  def create_card(question, answer, category)
+    Card.new(question, answer, category)
+  end
+end
+
+CardGenerator.new.file_reader
