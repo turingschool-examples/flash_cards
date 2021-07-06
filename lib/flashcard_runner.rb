15f8f5ad6afd7fe785b2897d8b31fa789f3a1c68
Tanner Dale <tanner@tannerdale.com>
2021-07-06 12:22:53 -0500

    Add card generator module include, refactor make_round

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,6 +2,8 @@ require './lib/round'
 require './lib/card_generator'
 
 class FlashcardRunner
+  include CardGenerator
+
   def initialize
     @round = make_round
     @deck = @round.deck
@@ -84,7 +86,7 @@ class FlashcardRunner
     # card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     # card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     # deck = Deck.new([card_1, card_2, card_3, card_4])
-    deck = Deck.new(CardGenerator.new.file_reader)
+    deck = Deck.new(CardGenerator.make_cards_from_file)
     Round.new(deck)
   end
 end
