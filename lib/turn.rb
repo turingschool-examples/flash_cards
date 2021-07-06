1a205d2630c41ca8cbdf0cac0050bc9946b5027c
Henry Schmid <henry.schmid1@gmail.com>
2021-07-06 16:54:17 -0600

    Create correct? and feedback methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,17 @@
 class Turn
 
-  attr_accessor :string, :card
-  def initialize(string, card)
-    @string = string
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
     @card = card
+  end
+
+  def correct?
+    guess == @card.answer
+  end
+
+  def feedback
+    return "Correct!" if correct? == true
   end
 end
