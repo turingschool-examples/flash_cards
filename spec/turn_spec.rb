bce549bf94ff348613272a4123af61e0951068b1
Henry Schmid <henry.schmid1@gmail.com>
2021-07-05 19:19:09 -0600

    Create tests for Turn initialization in turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,4 +1,25 @@
 require './lib/turn'
+require './lib/card'
 
 RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.string).to eq("Juneau")
+  end
+
+  it "has a Card object" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
 end
