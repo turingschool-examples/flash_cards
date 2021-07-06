77a80f2ba710f57bb4e4af27ae901f1d00cb9cc9
Rowan DeLong <rowanwinzer@gmail.com>
2021-07-05 21:37:41 -0600

    Add percent_correct and percent_correct_by_category methods.

--- lib/round.rb
+++ lib/round.rb
@@ -18,7 +18,7 @@ class Round
   end
 
   def number_correct
-    total_correct = 0
+    total_correct = 0.0
     @turns.each do |turn|
       if turn.correct?
         total_correct += 1
@@ -28,13 +28,27 @@ class Round
   end
 
   def number_correct_by_category(category)
-    total_correct = 0
+    total_correct = 0.0
     @turns.each do |turn|
       if turn.correct? && turn.card.category == category
         total_correct += 1
       end
     end
     return total_correct
+  end
+
+  def percent_correct
+    (number_correct / @turns.size) * 100
+  end
+
+  def percent_correct_by_category(category)
+    category_total = 0
+    @turns.each do |turn|
+      if turn.card.category == category
+        category_total += 1
+      end
+    end
+    return (number_correct_by_category(category) / category_total) * 100
   end
 
 end
