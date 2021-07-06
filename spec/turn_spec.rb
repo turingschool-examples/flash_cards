c979b654251296d641850ad03ccd4c77932f2b99
Henry Schmid <henry.schmid1@gmail.com>
2021-07-06 16:53:25 -0600

    Refactor turn_spec.rb and add tests for correct? and feedback

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,24 +2,28 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
+  before :each do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn).to be_instance_of(Turn)
+    expect(@turn).to be_instance_of(Turn)
   end
 
-  it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.string).to eq("Juneau")
+  it 'returns the guess with the guess method' do
+    expect(@turn.guess).to eq("Juneau")
   end
 
-  it "has a Card object" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+  it 'returns the Card with the card method' do
+    expect(@turn.card).to eq(@card)
+  end
 
-    expect(turn.card).to eq(card)
+  it 'confirms if the guess is Correct' do
+    expect(@turn.correct?).to be true
+  end
+
+  it 'provides feedback' do
+    expect(@turn.feedback).to eq('Correct!')
   end
 end
