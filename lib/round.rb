40a0f8e8c1a02a14ebfc19212021ea2a227fa3dd
George Soderholm <georgesoderholm@gmail.com>
2021-07-06 15:26:53 -0600

    Add ability to find percentages of correct by all and by category

--- lib/round.rb
+++ lib/round.rb
@@ -21,8 +21,32 @@ class Round
   end
 
   def number_correct
-    @turns.count do |turn|
+    correct_turns.count
+  end
+
+  def number_correct_by_category(category)
+    correct_turns.count do |turn|
+      turn.card.category == category
+    end
+  end
+
+  def correct_turns
+    @turns.find_all do |turn|
       turn.card.answer == turn.guess
+    end
+  end
+
+  def percent_correct
+    ((number_correct).to_f / (@turns.count).to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    ((number_correct_by_category(category)).to_f / (total_number_by_category(category)).to_f) * 100
+  end
+
+  def total_number_by_category(category)
+    @turns.count do |turn|
+      turn.card.category == category
     end
   end
 end
