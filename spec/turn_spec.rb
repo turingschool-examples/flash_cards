f69e7f0273fa651888442f74cdba83974f292304
Tanner Dale <tanner@tannerdale.com>
2021-07-06 13:52:14 -0500

    Change before each to context initialize

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,32 +2,32 @@ require './lib/card'
 require './lib/turn'
 
 RSpec.describe Turn do
-  before(:each) do
-    @card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    @turn1 = Turn.new("Juneau", @card1)
-    @turn2 = Turn.new("Saturn", @card2)
-  end
+  context 'initialize' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn1 = Turn.new("Juneau", card1)
+    turn2 = Turn.new("Saturn", card2)
 
-  it 'exists' do
-    expect(@turn1).to be_a(Turn)
-  end
+    it 'exists' do
+      expect(turn1).to be_a(Turn)
+    end
 
-  it 'has a card as a card object' do
-    expect(@turn1.card).to eq(@card1)
-  end
+    it 'has a card as a card object' do
+      expect(turn1.card).to eq(card1)
+    end
 
-  it 'has a guess' do
-    expect(@turn1.guess).to eq("Juneau")
-  end
+    it 'has a guess' do
+      expect(turn1.guess).to eq("Juneau")
+    end
 
-  it 'can correctly check a guess' do
-    expect(@turn1.correct?).to be(true)
-    expect(@turn2.correct?).to be(false)
-  end
+    it 'can correctly check a guess' do
+      expect(turn1.correct?).to be(true)
+      expect(turn2.correct?).to be(false)
+    end
 
-  it 'can provide correct feedback' do
-    expect(@turn1.feedback).to eq("Correct!")
-    expect(@turn2.feedback).to eq("Incorrect.")
+    it 'can provide correct feedback' do
+      expect(turn1.feedback).to eq("Correct!")
+      expect(turn2.feedback).to eq("Incorrect.")
+    end
   end
 end
