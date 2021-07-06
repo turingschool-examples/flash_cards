042703716c47d0aa470dcfe9681ddca7d8e5052a
Weston Ellis <ellisweston112@gmail.com>
2021-07-05 18:23:45 -0600

    Create turn

--- lib/card.rb
+++ lib/card.rb
@@ -8,3 +8,29 @@ class Card
   end
 
 end
+
+class Turn
+  attr_reader :guess, :card
+
+  def initialize (guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    @guess == @answer
+  end
+
+  def feedback
+
+    if @guess == @answer
+      return "Correct!"
+    else
+      return "incorrect."
+    end
+  end
+
+
+  end
+
+end
