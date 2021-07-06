6ea2f101e409a28789eb6a69df7b994929c3d5c7
Rowan DeLong <rowanwinzer@gmail.com>
2021-07-05 21:29:14 -0600

    Update take_turn, add number_correct and number_correct_by_category.

--- lib/round.rb
+++ lib/round.rb
@@ -13,8 +13,28 @@ class Round
   def take_turn(guess)
     turn = Turn.new(guess, current_card)
     @turns << turn
+    @deck.cards.shift
     return turn
   end
 
+  def number_correct
+    total_correct = 0
+    @turns.each do |turn|
+      if turn.correct?
+        total_correct += 1
+      end
+    end
+    return total_correct
+  end
+
+  def number_correct_by_category(category)
+    total_correct = 0
+    @turns.each do |turn|
+      if turn.correct? && turn.card.category == category
+        total_correct += 1
+      end
+    end
+    return total_correct
+  end
 
 end
