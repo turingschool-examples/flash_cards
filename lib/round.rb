c91914dc09b63504bca3c93a49b9f834150dd838
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-07-05 20:22:29 -0600

    create methods, for turns

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,27 @@
 
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
-  end 
+    @turns = []
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+
+  def take_turn(guess)
+    next_turn = Turn.new(guess, current_card)
+    @turns << next_turn
+    @deck.cards.shift
+    next_turn
+  end
+
+  def number_correct
+    @turns.count do |turn|
+      turn.correct?
+    end
+  end
 
 end
