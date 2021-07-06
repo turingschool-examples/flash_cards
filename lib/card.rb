14180ca4b1f14acc37e6d07143db92b9acd08baf
Jessica Jones <daddehjones@gmail.com>
2021-07-05 20:23:11 -0400

    Initialize Card class. Add attr_reader.

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,12 @@
 class Card
-  
+
+  attr_reader :question,
+              :answer,
+              :category
+              
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
 end
