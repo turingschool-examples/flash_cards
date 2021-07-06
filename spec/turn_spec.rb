29a0c8a4797f50e97792c0e9d91b596978ccc4a6
Jessica Jones <daddehjones@gmail.com>
2021-07-05 20:48:06 -0400

    Create Turn object tests.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,9 +3,9 @@ require './lib/turn'
 
 RSpec.describe Turn do
   it 'exists' do
-    turn = Turn.new("Juneau", card)
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-    expect(turn).to be_instance_of(Turn)
-    expect(card).to be_instance_of(Card)
+    expect(turn).to be_an_instance_of(Turn)
   end
+end
