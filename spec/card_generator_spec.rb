0d6a7c9768e4ee5489d432178bb9aa1439722b37
Tanner Dale <tanner@tannerdale.com>
2021-07-06 17:53:34 -0500

    Add correctly parses cards test

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe CardGenerator do
     answer_2 = "red panda"
     category_2 = :Turing_Staff
 
-    question_3 = "What is Rachel's favorite animal?"
-    answer_3 = "red panda"
+    question_3 = "What is Mike's middle name?"
+    answer_3 = "nobody knows"
     category_3 = :Turing_Staff
 
     question_4 = "What cardboard cutout lives at Turing?"
@@ -21,7 +21,33 @@ RSpec.describe CardGenerator do
     category_4 = :Pop_Culture
 
     it 'can seperate cards' do
-      expect(cards.length).to eq(6)
+      expect(cards).not_to be_empty
+    end
+
+    it 'has cards as Card objects' do
+      result = cards.all? do |card|
+        card.is_a?(Card)
+      end
+
+      expect(result).to be(true)
+    end
+
+    it 'parses cards correctly' do
+      card_1 = cards.first
+
+      expect(card_1.question).to eq(question_1)
+      expect(card_1.answer).to eq(answer_1)
+      expect(card_1.category).to eq(category_1)
+
+      card_2 = cards[1]
+      expect(card_2.question).to eq(question_2)
+      expect(card_2.answer).to eq(answer_2)
+      expect(card_2.category).to eq(category_2)
+
+      card_3 = cards[2]
+      expect(card_3.question).to eq(question_3)
+      expect(card_3.answer).to eq(answer_3)
+      expect(card_3.category).to eq(category_3)
     end
   end
 end
