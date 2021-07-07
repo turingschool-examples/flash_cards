e8ecbeb63bb2b48d7d298596897424964d6eae32
Weston Ellis <ellisweston112@gmail.com>
2021-07-07 14:17:25 -0600

    Add truth checker

---
+++ spec/turn_spec.rb
@@ -0,0 +1,43 @@
+require './lib/turn'
+require './lib/card'
+
+
+RSpec.describe Turn do
+
+
+  it 'create turn' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_an_instance_of(Turn)
+  end
+
+  it 'call card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+  it 'check guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'check correct' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'return feddback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+end
