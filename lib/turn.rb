f6b7b0c6179eea67b7375808154724596b9b474b
ChloeLL <chloell@protonmail.ch>
2021-07-06 18:54:04 -0700

    Initial Commit

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+class Turn
+
+  def initialize(string, card)
+    @string = string
+    @card = card
+  end
+
+  def guess
+  end
+
+  def card
+  end
+
+  def correct?
+  end
+
+  def feedback
+  end
+
+end
