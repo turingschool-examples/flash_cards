52fbbf1b41762a65f4645ad79552a8e6569c510d
Laura Court <laura.mcourt@gmail.com>
2021-07-06 22:09:06 -0400

    Write class methods

---
+++ lib/turn.rb
@@ -0,0 +1,21 @@
+class Turn
+
+  attr_reader :guess, :card, :feedback
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+    @feedback = feedback
+  end
+
+  def correct?
+     @guess == card.answer
+  end
+
+  def feedback
+    if correct? == true
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
+end
