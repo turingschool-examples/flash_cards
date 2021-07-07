73ad40408339b145b9e79a39167164c42948ebf2
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-06 20:51:49 -0600

    Add class to card.rb and test card_spec.rb

---
+++ lib/card.rb
@@ -0,0 +1,12 @@
+class Card
+  attr_reader :question, :answer, :category
+
+  def initialize(question_param, answer_param, category_param)
+    @question = question_param
+    @answer = answer_param
+    @category = category_param
+    # keeping pry within the initialize method will allow
+    # you to access the data within the method 
+    require 'pry'; binding.pry
+  end
+end
