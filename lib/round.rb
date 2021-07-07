a12213a481106e6d293ad90b56447f51a9f97228
Jessica Jones <daddehjones@gmail.com>
2021-07-06 21:05:35 -0400

    Create take_turn method.

--- lib/round.rb
+++ lib/round.rb
@@ -13,10 +13,15 @@ class Round
     @deck.cards[0]
   end
 
-  def take_turn
+  def take_turn(guess)
     #takes a string representing the guess
     #creates new turn object with guess and card
     #stores new turn and returns it from take_turn
     #when called the round moves to the next card in the deck
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    @deck.cards.shift
+    new_turn
   end
+  
 end
