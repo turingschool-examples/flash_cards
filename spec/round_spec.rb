2c82553cba35728edd9c1ba81424cd99a3a7afd8
Jessica Jones <daddehjones@gmail.com>
2021-07-06 20:49:05 -0400

    Create new turn test.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Round do
     expect(round.deck).to eq(deck)
   end
 
-  it "takes turns" do
+  it "begins a fresh round" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -35,4 +35,14 @@ RSpec.describe Round do
     expect(round.current_card).to eq(card_1)
   end
 
+  it "starts a new turn" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    expect(new_turn.class).to eq(Turn)
+  end
 end
