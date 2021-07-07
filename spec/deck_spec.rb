55e8575282b1cde1fdbd84bcc920340810daac74
Isika Powers <isika.powers@gmail.com>
2021-07-07 15:43:13 -0600

    Add deck_spec file

---
+++ spec/deck_spec.rb
@@ -0,0 +1,46 @@
+require "./lib/card"
+require "./lib/turn"
+require "./lib/deck"
+
+RSpec.describe Deck do
+  it "exists" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([])
+
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it "has all the cards" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([])
+
+    expect(deck.cards).to eq([card_1, card_2, card_3])
+  end
+
+  it 'has a count' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([])
+
+    expect(deck.count).to eq(3)
+  end
+
+  it 'has cards in category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([])
+    deck.cards_in_category(:Geography)
+
+    expect(deck.cards_in_category).to eq(card_1)
+  end
+end
