ad65cb3ea705286c5dc0601737acb95bd6487195
Laura Court <laura.mcourt@gmail.com>
2021-07-06 21:59:51 -0400

    Add gives feedback test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -31,4 +31,10 @@ RSpec.describe Turn do
     expect(turn.correct?).to be(true)
   end
 
+  it 'gives feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
 end
