7381eae8f42e9e461dc96d7152ec57405ac4a10a
Tanner Dale <tanner@tannerdale.com>
2021-07-07 12:22:18 -0500

    Wrap related tests into context blocks

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -4,6 +4,22 @@ RSpec.describe CardGenerator do
   context 'initialize' do
     cards = CardGenerator.make_cards_from_file
 
+    it 'can seperate cards' do
+      expect(cards).not_to be_empty
+    end
+
+    it 'has cards as Card objects' do
+      result = cards.all? do |card|
+        card.is_a?(Card)
+      end
+
+      expect(result).to be(true)
+    end
+  end
+
+  context 'parsing' do
+    cards = CardGenerator.make_cards_from_file
+
     question_1 = "What is 5 + 5?"
     answer_1 = "10"
     category_1 = :STEM
@@ -20,30 +36,22 @@ RSpec.describe CardGenerator do
     answer_4 = "Justin bieber"
     category_4 = :Pop_Culture
 
-    it 'can seperate cards' do
-      expect(cards).not_to be_empty
-    end
-
-    it 'has cards as Card objects' do
-      result = cards.all? do |card|
-        card.is_a?(Card)
-      end
-
-      expect(result).to be(true)
-    end
-
-    it 'parses cards correctly' do
+    it 'card 1' do
       card_1 = cards.first
 
       expect(card_1.question).to eq(question_1)
       expect(card_1.answer).to eq(answer_1)
       expect(card_1.category).to eq(category_1)
+    end
 
+    it 'card 2' do
       card_2 = cards[1]
       expect(card_2.question).to eq(question_2)
       expect(card_2.answer).to eq(answer_2)
       expect(card_2.category).to eq(category_2)
+    end
 
+    it 'card 3' do
       card_3 = cards[2]
       expect(card_3.question).to eq(question_3)
       expect(card_3.answer).to eq(answer_3)
