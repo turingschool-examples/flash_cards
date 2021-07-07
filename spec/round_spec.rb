9930d1a11f17e122ab0ee9f5c24a8c4541891f89
Jessica Jones <daddehjones@gmail.com>
2021-07-07 18:07:59 -0400

    Create test for next card. Create feedback test.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe Round do
     expect(new_turn.class).to eq(Turn)
   end
 
-  it "can guess the correct answer" do
+  it "can check to see if the answer is correct" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -79,4 +79,29 @@ RSpec.describe Round do
     expect(round.number_correct).to eq(1)
   end
 
+  it "shows the next card" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Venus")
+
+    expect(round.current_card).to eq(card_2)
+  end
+
+  it "takes a turn and provides feedback" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    expect(round.turns.count).to eq(2)
+    expect(round.turns.last.feedback).to eq("Incorrect.")
+
+  end
 end
