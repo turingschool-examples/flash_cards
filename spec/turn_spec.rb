4d5cf9d973ffd12f2658e79571c4bb816fd9f93a
Tanner Dale <tanner@tannerdale.com>
2021-07-07 12:22:00 -0500

    Seperate related tests in context blocks

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,18 +3,23 @@ require './lib/turn'
 
 RSpec.describe Turn do
   context 'initialize' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    it 'exists' do
+      expect(turn).to be_a(Turn)
+    end
+
+    it 'has a card as a card object' do
+      expect(turn.card).to eq(card)
+    end
+  end
+
+  context 'guess checking' do
     card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn1 = Turn.new("Juneau", card1)
     turn2 = Turn.new("Saturn", card2)
-
-    it 'exists' do
-      expect(turn1).to be_a(Turn)
-    end
-
-    it 'has a card as a card object' do
-      expect(turn1.card).to eq(card1)
-    end
 
     it 'has a guess' do
       expect(turn1.guess).to eq("Juneau")
