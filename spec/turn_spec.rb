23aca3eaa8c4d86a421e8cd9d679d4dff9abf370
ChloeLL <chloell@protonmail.ch>
2021-07-07 16:52:16 -0700

    Refactor Code

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,45 +2,36 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
-  it 'initializes a card' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  turn = Turn.new("Juneau", card)
 
+  it 'initializes a card' do
     expect(card).to be_instance_of(Card)
   end
 
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
     turn.guess
 
     expect(turn.guess).to eq("Juneau")
   end
 
   it 'has a card' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
     turn.card
 
+    expect(turn).to be_instance_of(Turn)
   end
 
   it 'is correct' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
     turn.correct?
 
     expect(turn.correct?).to be true
   end
 
   it 'has feedback' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
     turn.correct?
     turn.feedback
 
