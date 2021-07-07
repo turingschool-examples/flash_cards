12de32becc93288ede1c20e5eb80b7ba7257c6cd
Blake Dempsey <bdempsey864@gmail.com>
2021-07-06 20:48:44 -0600

    Create check if correct method

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,9 @@
 class Round 
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
   end
 
   def current_card
@@ -12,7 +13,14 @@ class Round
   def take_turn(guess)
     new_turn = Turn.new(guess, current_card)
     @turns << new_turn
+    check_if_correct(new_turn)
     @deck.cards.shift
     new_turn
+  end
+
+  def check_if_correct(new_turn)
+    if new_turn.correct?
+      @number_correct += 1
+    end
   end
 end
