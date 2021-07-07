b86811db5d4835132a0e240cf88975f173b7a95f
Jessica Jones <daddehjones@gmail.com>
2021-07-06 20:55:13 -0400

    Attempt pseudocoding to process this method.

--- lib/round.rb
+++ lib/round.rb
@@ -12,4 +12,11 @@ class Round
     #need to figure out how to show the card
     @deck.cards[0]
   end
+
+  def take_turn
+    #takes a string representing the guess
+    #creates new turn object with guess and card
+    #stores new turn and returns it from take_turn
+    #when called the round moves to the next card in the deck
+  end
 end
