be4cac8b284a6e846aff40d52e52e72cefdbe252
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-07 20:27:45 -0400

    Add turn.rb and turn_spec.rb files

---
+++ spec/turn_spec.rb
@@ -0,0 +1,50 @@
+require './lib/card'
+require './lib/turn'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", Card)
+
+    expect(turn).to be_a(Turn)
+  end
+
+  it 'has a card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+
+    expect(turn.card).to eq(card_1)
+  end
+
+  it 'has a guess' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", Card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'has a correct? guess' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", Card)
+
+    expect(turn.guess).to eq(card_1.answer)
+  end
+
+  it 'has feedback' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+    turn.correct?
+
+    expect(turn.feedback).to eq("Correct")
+  end
+
+  it 'has incorrect feedback' do
+    card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card_2)
+
+    expect(turn.card).to eq(card_2)
+    expect(turn.guess).to eq("Saturn")
+    expect(turn.correct?).to eq(false)
+    expect(turn.feedback).to eq("Incorrect")
+  end
+end
