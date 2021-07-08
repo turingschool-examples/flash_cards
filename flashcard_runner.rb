8c7035ba4b41c1686ce755983a3bd050523b62a2
Weston Ellis <ellisweston112@gmail.com>
2021-07-08 16:47:28 -0600

    Add card

---
+++ flashcard_runner.rb
@@ -0,0 +1,4 @@
+card_1 = Card.new("What is 1+1?", "2", :Math)
+card_2 = Card.new("When is it ok to divide by 0?", "Never.", :Math)
+card_3 = Card.new("Did the last living T'Rex live closer to the moonlanding or the last living Stegasaurus?", "The moonlanding", :Dinosaurs)
+card_4 = Card.new("What is the spikey bit on the end of Stegasaurus' tail called?", "Thagomizer", :Dinosaurs)
