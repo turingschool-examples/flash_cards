071e18459dc6f1ae397af45c4417874aeaa1b4f8
Sawaez Amin <sawaezamin@gmail.com>
2021-07-07 21:20:24 -0600

    finish the round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,22 @@
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
+
+
 class Round
   attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
     @turns = []
+  end
+
+  def current_card
+    @deck.cards.shift
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    new_turn
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -5,7 +5,7 @@ require './lib/round'
 require 'rspec'
 
 RSpec.describe Round do
-  xit 'exists' do
+  it 'exists' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -16,7 +16,7 @@ RSpec.describe Round do
     expect(round.deck).to eq(deck)
   end
 
-  xit 'has no turns by default' do
+  it 'has no turns by default' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -26,7 +26,7 @@ RSpec.describe Round do
     expect(round.turns).to eq([])
   end
 
-  xit 'shows the current card' do
+  it 'shows the current card' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -36,7 +36,7 @@ RSpec.describe Round do
     expect(round.current_card).to eq(card_1)
   end
 
-  xit 'makes a new turn' do
+  it 'makes a new turn' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -44,7 +44,7 @@ RSpec.describe Round do
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
 
-    expect(new_turn.class).to be_a(Turn)
+    expect(new_turn).to be_a(Turn)
     expect(round.turns).to eq([new_turn])
   end
 
@@ -56,7 +56,7 @@ RSpec.describe Round do
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
 
-    expect(round.correct?).to eq(true)
+    expect(new_turn.correct?).to eq(true)
     expect(round.number_correct).to eq(1)
   end
 
@@ -99,7 +99,7 @@ RSpec.describe Round do
     expect(round.number_correct_by_category(:STEM)).to eq(0)
 
     expect(round.percent_correct).to eq(50.0)
-    expect(round.percent_correct_by_category).to eq(100.0)
+    expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
     expect(round.current_card).to eq(card_3)
   end
 end
