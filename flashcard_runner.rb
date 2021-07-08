c924674deac99764afdbe2c78efc6b2369223615
Ted S <tstaros23@gmail.com>
2021-07-07 19:31:10 -0600

    Start runner file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,2 +1,18 @@
-class FlashcardRunner
-  def initialize()
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+require './lib/turn'
+
+card_1 = Card.new("Question: What is 5 + 5?", 10, :STEM)
+
+card_2 = Card.new("Question: What is Rachel's favorite animal?", "panda", :TuringStaff)
+
+card_3 = Card.new("Question: What is Mike's middle name?","nobody knows", :TuringStaff)
+
+card_4 = Card.new("What cardboard cutout lives at Turing?","Justin Bieber", :PopCulture)
+
+cards = [card_1,card_2,card_3,card_4]
+
+deck = Deck.new(cards)
+require "pry"; binding.pry
