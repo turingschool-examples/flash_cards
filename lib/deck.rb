6406876640fba7d50c19f1c968a1be457b8f722c
ChloeLL <chloell@protonmail.ch>
2021-07-08 14:17:04 -0700

    Complete cards_in_category Method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,17 +1,17 @@
 class Deck
+  attr_reader :cards
 
   def initialize(cards)
     @cards = cards
-  end
-
-  def cards
-    @cards
   end
 
   def count
     @cards.length
   end
 
-  def cards_in_category()
+  def cards_in_category(category)
+    @cards.find_all do |card|
+      card.category == category
+    end
   end
 end
