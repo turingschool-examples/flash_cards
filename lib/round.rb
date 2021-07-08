05ef767105a347627f6d2ec92edde3da8a5aeaae
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-07 22:16:49 -0600

    Make Round tests pass

---
+++ lib/round.rb
@@ -0,0 +1,45 @@
+class Round
+  attr_reader :deck, :turns, :current_card
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+    @current_card = deck.cards.first
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, @current_card)
+    @turns << turn
+    @current_card = next_card
+    turn
+  end
+
+  def current_index
+    @deck.cards.index(@current_card)
+  end
+
+  def next_card
+    next_index = current_index + 1
+    @deck.cards[next_index]
+  end
+
+  def number_correct
+    @turns.count { |turn| turn.correct? }
+  end
+
+  def number_correct_by_category(category)
+    @turns.count { |turn| turn.correct? if turn.card.category == category }
+  end
+
+  def percent_correct
+    (number_correct.to_f / @turns.count.to_f) * 100.0
+  end
+
+  def percent_correct_by_category(category)
+    (number_correct_by_category(category).to_f / turns_by_category(category).to_f) * 100.0
+  end
+
+  def turns_by_category(category)
+    @turns.count { |turn| turn.card.category == category }
+  end
+end
