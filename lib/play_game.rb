882b11a229dffdea5240ee5717c160216c571e0d
Christina Delpone <cdelpone@gmail.com>
2021-07-08 15:34:54 -0600

    Refactor playgame methods

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -1,27 +1,42 @@
 class PlayGame
 
   def start_game
+    make_cards
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+    begin_game
+    game_flow
+    end_game
+  end
+
+  def make_cards
     @card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
     @card_2 = Card.new('How long is the backend program at Turing?', '7 months', :Turing)
     @card_3 = Card.new('What does FSD stand for? Hint: Pesto Paws!', 'Freedom Service Dogs', :Pesto)
     @card_4 = Card.new('How long are the modules or innings at Turing?', '6 weeks', :Turing)
     @card_5 = Card.new("Is it appropriate to greet or 'say hello' to Pesto when you meet him?", 'No', :Pesto)
     @cards = [@card_1, @card_2, @card_3, @card_4, @card_5]
+  end
 
-    @deck = Deck.new(@cards)
-    @round = Round.new(@deck)
-
+  def begin_game
     puts "Welcome! You're playing with #{@deck.count} cards."
+  end
 
+  def game_flow
     until @round.turn_count == (@deck.cards.count + 1)
       puts "This is card number #{@round.turn_count} out of #{@deck.cards.count}."
 
       puts "Question: #{@round.current_card.question}"
-      user_guess = gets.chomp
       @round.take_turn(user_guess)
       puts "#{@round.turns.last.feedback}"
     end
+  end
 
+  def user_guess
+    gets.chomp
+  end
+
+  def end_game
     puts "****** Game over! ******"
     puts "You had #{@round.number_correct} correct guesses out of #{@deck.count} for a total score of #{@round.percent_correct}%.
     Geography - #{@round.percent_correct_by_category(:Geography).round(0)}% correct
--- spec/play_game_spec.rb
+++ spec/play_game_spec.rb
@@ -7,7 +7,14 @@ RSpec.describe PlayGame do
     expect(play).to be_a PlayGame
   end
 
-  it 'starts the game' do
-
-  end
+  # it 'starts the game' do
+  #   play = PlayGame.new
+  #   expect(play.begin_game).to eq("Welcome! You're playing with #{@deck.count} cards.")
+  # end
 end
+
+# to - do:
+  # 3 - 7 methods - testing depends on if it takes user input or just returns a string
+  # don't need to test puts
+  # cards method - tests instantiates and returns an array of cards
+  # user input gets.chomp as it's own method so I can later stub
