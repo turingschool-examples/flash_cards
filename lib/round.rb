608beb1717803be2a8462f97bdf5e4546df702a9
Jamie Pace <jamiejpace@gmail.com>
2021-07-08 16:29:19 -0600

    Refactor round class and add test

--- lib/round.rb
+++ lib/round.rb
@@ -12,11 +12,6 @@ class Round
 
   def take_turn(guess)
     turns << Turn.new(guess, current_card)
-    # category_total_count[current_card.category] += 1
-    # if turns.last.correct?
-    #   @number_correct += 1
-    #   category_count_correct[current_card.category] += 1
-    # end
     deck.cards.shift
     turns.last
   end
@@ -25,17 +20,27 @@ class Round
     turns.count do |turn|
       turn.correct?
     end
-  end 
+  end
 
-  # def number_correct_by_category(category)
-  #   category_count_correct[category]
-  # end
-  #
-  # def percent_correct
-  #   (number_correct.to_f / turns.length.to_f) * 100
-  # end
-  #
-  # def percent_correct_by_category(category)
-  #   (category_count_correct[category].to_f / category_total_count[category].to_f) * 100
-  # end
+  def number_correct_by_category(category)
+    turns.count do |turn|
+      turn.card.category == category && turn.correct?
+    end
+  end
+
+  def percent_correct
+    (number_correct.to_f / turns.length.to_f) * 100
+  end
+
+  def turns_per_category(category)
+    turns.count do |turn|
+      turn.card.category == category
+    end
+  end
+
+  def percent_correct_by_category(category)
+    (number_correct_by_category(category).to_f / turns_per_category(category).to_f) * 100
+  end
+
 end
+ 
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -70,7 +70,7 @@ RSpec.describe Round do
     expect(round.turns.last.feedback).to eq("Correct!")
   end
 
-  xit 'counts correct by category' do
+  it 'counts correct by category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -84,7 +84,7 @@ RSpec.describe Round do
     expect(round.number_correct_by_category(:STEM)).to eq(0)
   end
 
-  xit 'calculates percent correct' do
+  it 'calculates percent correct' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -96,7 +96,19 @@ RSpec.describe Round do
     expect(round.percent_correct).to eq(50.0)
   end
 
-  xit "calculates percent correct by category" do
+  it 'counts the number of turns by category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    expect(round.turns_per_category(:Geography)).to eq(1)
+  end
+
+  it 'calculates percent correct by category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
