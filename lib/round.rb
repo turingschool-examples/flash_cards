3e138f5852d1457c5b9f71ed47e3946ce15d669b
ChloeLL <chloell@protonmail.ch>
2021-07-08 16:45:30 -0700

    Add Methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,27 @@
 class Round
+  attr_reader :deck, :turns
 
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+
+  def take_turn(guess)
+  end
+
+  def number_correct
+  end
+
+  def number_correct_by_category(category)
+  end
+
+  def percent_correct
+  end
+
+  def percent_correct_by_category(category)
+  end
 end
