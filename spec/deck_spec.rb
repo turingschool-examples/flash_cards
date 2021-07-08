7a14adecd2f1738fe78cb88dbd5650ab07fc41a4
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-08 13:19:07 -0600

    Add deck exists test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,15 @@
-'./lib/card'
-'./lib/deck'
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+  describe "deck" do
+    it 'exists' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new(card_1, card_2, card_3)
+
+      expect(deck).to be_instance_of(Deck)
+    end
+  end
+end
