b39805452182cdc46c8ec48228445cdd21d3afff
Jamie Pace <jamiejpace@gmail.com>
2021-07-08 14:52:17 -0600

    Add to take turn test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -54,6 +54,7 @@ RSpec.describe Round do
 
     expect(new_turn).to be_a(Turn)
     expect(new_turn.correct?).to be true
+    expect(round.current_card).to eq(card_2)
     expect(round.turns).to eq([new_turn])
   end
 
