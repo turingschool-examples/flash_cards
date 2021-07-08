2bd3459d45e9943d888be43444145d5d296e545c
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-08 00:24:37 -0700

    Update setup in runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,19 +1,33 @@
+require './lib/card_generator'
 require './lib/card'
 require './lib/deck'
 require './lib/turn'
 require './lib/round'
 
 def start
-  card_1 = Card.new("Who is Khoi's favorite superhero?", "Batman", "Khoi")
-  card_2 = Card.new("What is Kathy's pet bunny's name?", "Toki", "Kathy")
-  card_3 = Card.new("Does Khoi wear glasses?", "Yes", "Khoi")
-  card_4 = Card.new("Is Duy the eldest or youngest sibling?", "Youngest", "Duy")
-  cards = [card_1, card_2, card_3, card_4]
+  filename = './lib/cards.txt'
+  cards = CardGenerator.new(filename).cards
   deck = Deck.new(cards)
   round = Round.new(deck)
+  categories_all = cards.map { |card| card.category }
+  categories_unique = categories_all.uniq
 
   puts "Welcome! You're playing with #{deck.count} cards."
   puts "-------------------------------------------------"
+
+  while round.turns.size < deck.count
+    puts "This card is #{round.turns.size + 1} out of #{deck.count}"
+    puts "Question: #{round.current_card.question}"
+    guess = gets.capitalize.chomp
+
+    round.take_turn(guess)
+    puts round.turns.last.feedback
+  end
+
+  puts "****** Game over! ******"
+  puts "You had #{round.number_correct} correct guesses out of #{deck.count} for a total score of #{round.percent_correct}%."
+  
+  categories_unique.each { |category| puts "#{category} - #{round.percent_correct_by_category("#{category}")}% correct" }
 end
 
 start
