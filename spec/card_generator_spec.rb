78f2ea3257b4f4005e76447725ad8b09ea58e3d3
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-08 00:21:55 -0700

    Update card generator spec

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,8 +1,19 @@
 require './lib/card_generator'
+require './lib/card'
 
 RSpec.describe CardGenerator do
   it 'exists' do
     filename = './lib/cards.txt'
+    generated = CardGenerator.new(filename)
+
+    expect(generated).to be_a(CardGenerator)
+  end
+
+  it 'can create cards' do
+    filename = './lib/cards.txt'
+    cards = CardGenerator.new(filename).cards
     
+    expect(cards[0]).to be_a(Card)
+    expect(cards.all? { |card| card.class == Card }).to eq(true)
   end
 end
