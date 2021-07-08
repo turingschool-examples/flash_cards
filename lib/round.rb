76c23e37b3e3044fb8e5f8e0141d8b143b69a9e5
Jamie Pace <jamiejpace@gmail.com>
2021-07-08 08:34:48 -0600

    Add number correct by category class and test

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,11 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct, :category_count
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
+    @category_count = {Geography: 0, STEM: 0, "Pop Culture" => 0}
   end
 
   def current_card
@@ -12,7 +14,16 @@ class Round
 
   def take_turn(guess)
     turns << Turn.new(guess, current_card)
+    if turns.last.correct?
+      @number_correct += 1
+      @category_count[current_card.category] += 1
+    end
     deck.cards.shift
     turns.last
   end
+
+  def number_correct_by_category(category)
+    @category_count[category]
+  end
+
 end
