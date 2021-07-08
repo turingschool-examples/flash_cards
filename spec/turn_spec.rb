523b4dc6f7d9f33179e0fecafd70791a81aeb61b
Miriam Aceves <miriammerari02@gmail.com>
2021-07-08 11:05:03 -0400

    Write tests for all methods in Turn class

---
+++ spec/turn_spec.rb
@@ -0,0 +1,50 @@
+require './lib/card'
+require './lib/turn'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_a(Turn)
+  end
+
+  it 'has attributes' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.string).to eq("Juneau")
+    expect(turn.card.question).to eq("What is the capital of Alaska?")
+  end
+
+  it 'can guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'returns card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to be_a(Card)
+    expect(turn.card.question).to eq("What is the capital of Alaska?")
+
+  end
+
+  it 'correct?' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    turn.feedback
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+end
