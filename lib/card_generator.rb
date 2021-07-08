dc211fc50089065bf984c31a638dfaa88a38fab4
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-08 00:23:15 -0700

    Update cards method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,18 @@
 class CardGenerator
   attr_reader :filename
-  
+
   def initialize(filename)
     @filename = filename
+  end
+
+  def cards
+    cards_array = []
+    read_file = File.read(@filename).split("\n")
+    
+    read_file.each do |line|
+      card = line.split(",")
+      cards_array << Card.new(card[0], card[1], card[2])
+    end
+    cards_array
   end
 end
