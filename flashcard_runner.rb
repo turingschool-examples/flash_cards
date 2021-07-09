6ea4341af412600424fee550c3f776a51ec00e45
Sawaez Amin <sawaezamin@gmail.com>
2021-07-08 21:08:26 -0600

    Make a start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,14 +1,54 @@
 require './lib/card'
 require './lib/turn'
-require '.lib/deck'
-require '.lib/round'
+require './lib/deck'
+require './lib/round'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-deck = Deck.new([card_1, card_2, card_3])
+card_1 = Card.new("What is 5 + 5?", "10", :STEM)
+card_2 = Card.new("What is Rachel's favorite animal?", "koala", :turing_staff)
+card_3 = Card.new("What is Mike's middle name?", "nobody knows", :turing_staff)
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :pop_culture)
+cards = [card_1, card_2, card_3, card_4]
+deck = Deck.new(cards)
 round = Round.new(deck)
 
-class RunnerFile
+def start(round)
+  deck_count = round.deck.count
+
+  puts "Welcome! You're playing with #{deck_count} cards."
+  puts "-------------------------------------------------"
+
 
 end
+
+start(round)
+# puts "Welcome! You're playing with #{@deck.count} cards."
+# puts "-------------------------------------------------"
+# puts "This is card number 1 out of #{@deck.count}."
+# Question: What is 5 + 5?
+#
+#
+# 10
+# Correct!
+# This is card number 2 out of 4.
+# Question: What is Rachel's favorite animal?
+#
+#
+#
+# panda
+# Incorrect.
+# This is card number 3 out of 4.
+# Question: What is Mike's middle name?
+# nobody knows
+# Correct!
+# This is card number 4 out of 4.
+# Question: What cardboard cutout lives at Turing?
+# Justin Bieber
+# Correct!
+#
+#
+#
+# ****** Game over! ******
+# You had 3 correct guesses out of 4 for a total score of 75%.
+# STEM - 100% correct
+# Turing Staff - 50% correct
+# Pop Culture - 100% correct
