d3ec3d54a07f9580b18731cb65b0f2bfa840931c
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-07-09 09:14:15 -0600

    create play class that runner file will use to start game

---
+++ play.rb
@@ -0,0 +1,45 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+class Play
+  attr_reader :card_1, :card_2, :card_3, :card_4, :deck, :round
+
+  def initialize
+    @card_1 = Card.new("What is the largest state in the US by landmass?", "alaska", :Geography)
+    @card_2 = Card.new("What planet is second closest to the sun?", "venus", :Astronomy)
+    @card_3 = Card.new("What band did George Harrison play for?", "the beatles", :Music)
+    @card_4 = Card.new("What band wrote the Dark Side Of The Moon album?", "pink floyd", :Music)
+    @deck = Deck.new([card_1, card_2, card_3, card_4])
+    @round = Round.new(deck)
+  end
+
+  def start
+    puts welcome_message
+    puts dashes
+    puts card_number
+    puts question
+
+  end
+
+  def welcome_message
+    "Welcome to Flashcards! You're playing with #{deck.count} cards."
+  end
+
+  def dashes
+    "-" * 60
+  end
+
+  def card_number
+    "This is card number #{round.number_card} out of #{deck.count}"
+  end
+
+  def question
+    "#{round.current_card.question}"
+  end
+
+end
+
+play = Play.new
+play.start
