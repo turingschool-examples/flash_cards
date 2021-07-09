6b7c7e9e66f5493c6882f1504989a3586d007a7e
Laura Court <laura.mcourt@gmail.com>
2021-07-09 13:25:24 -0400

    Add number and peercent correct methods

--- lib/round.rb
+++ lib/round.rb
@@ -11,5 +11,35 @@ class Round
 
   def take_turn(guess)
     new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    @deck.cards.shift
+    @turns.last
   end
+
+  def number_correct
+    @turns.count do |turn|
+      turn.correct?
+    end
+  end
+
+# use group by
+  def number_correct_by_category(category)
+    @turns.count do |turn|
+      if turn.card.category == category
+      turn.correct?
+      end
+    end
+  end
+
+  def percent_correct
+    (number_correct.to_f / turns.count.to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    num_in_cat = @turns.find_all do |turn|
+      turn.card.category == category
+    end
+    (number_correct_by_category(category).to_f / num_in_cat.length) * 100
+  end
+
 end
