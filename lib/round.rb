5ac1cb6abf2c43e40f612ec519daef196608b3c7
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-09 09:26:34 -0400

    Add tests and code for percent_correct and percent_correct_by_category

--- lib/round.rb
+++ lib/round.rb
@@ -14,7 +14,7 @@ class Round
     current_turn = Turn.new(current_guess, current_card)
     @turns << current_turn
     add_correct_turns(current_turn)
-    shift_deck
+    @current_card = next_card
     return current_turn
   end
 
@@ -24,7 +24,26 @@ class Round
     end
   end
 
-  def shift_deck
-    deck.shift
+  def current_card_slot
+    @deck.cards.index
+  end
+
+  def next_card
+    next_card_slot = current_card_slot + 1
+    @deck.cards
+  end
+
+  def number_correct_by_category(category)
+    @turns.count do |turn|
+      turn.correct? if turn.card.category == category
+    end
+  end
+
+  def percent_correct
+    (@number_correct / @turns.count) * 100
+  end
+
+  def percent_correct_by_category(category)
+    (number_correct_by_category(category) / @turns.count) * 100
   end
 end
