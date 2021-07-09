7eb357b731b5f20e6523a65667891deb66227408
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-09 00:44:01 -0400

    Complete test for round class for iteration2

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -44,5 +44,98 @@ RSpec.describe Round do
     new_turn = round.take_turn("Juneau")
 
     expect(new_turn.class).to eq(Turn)
+    expect(round.turns.class).to eq(Array) #proves that round.turns is an array upon which array methods such as '.last' can be called
+  end
+
+  it 'deterimines if new turn was correct' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    expect(new_turn.correct?).to eq(true)
+  end
+
+  it 'can change to next card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    expect(new_turn.correct?).to eq(true)
+  end
+
+  it 'can determine number of correct guesses in round' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.number_correct).to eq(0)
+    round.take_turn("Juneau")
+    expect(round.number_correct).to eq(1)
+    round.take_turn("Mars")
+    expect(round.number_correct).to eq(2)
+    round.take_turn("North west") #wrong answer for guess
+    expect(round.number_correct).to eq(2)
+  end
+
+  it 'can determine number of correct guesses within a category in a round' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.number_correct_by_category(:Geography)).to eq(0)
+    round.take_turn("Juneau")
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+    expect(round.number_correct_by_category(:STEM)).to eq(0)
+    round.take_turn("Mars")
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+    expect(round.number_correct_by_category(:STEM)).to eq(1)
+    round.take_turn("North north west")
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+    expect(round.number_correct_by_category(:STEM)).to eq(2)
+  end
+
+  it 'can determine percent correct of all turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.percent_correct).to eq(0)
+    round.take_turn("Juneau")
+    expect(round.percent_correct).to eq(100)
+    round.take_turn("Mars")
+    expect(round.percent_correct).to eq(100)
+    round.take_turn("North west") #wrong answer for guess
+    expect(round.percent_correct).to eq(66.7)
+  end
+
+  it 'can determine percent correct of turns in category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.percent_correct_by_category(:Geography)).to eq(0)
+    round.take_turn("Juneau")
+    expect(round.percent_correct_by_category(:Geography)).to eq(100)
+    expect(round.percent_correct_by_category(:STEM)).to eq(0)
+    round.take_turn("Mars")
+    expect(round.percent_correct_by_category(:Geography)).to eq(100)
+    expect(round.percent_correct_by_category(:STEM)).to eq(100)
+    round.take_turn("North west")
+    expect(round.percent_correct_by_category(:Geography)).to eq(100)
+    expect(round.percent_correct_by_category(:STEM)).to eq(50)
   end
 end
