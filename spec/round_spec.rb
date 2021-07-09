3c3857337b82f361da88126a01c460a2209fb29e
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-09 14:08:13 -0400

    Add test for number of turns

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -71,10 +71,21 @@ RSpec.describe Round do
     new_turn = round.take_turn("Juneau")
     expect(round.current_card).to eq(cards[1])
   end
-  it 'determines turns in a round' do
 
+  it 'determines turns in a round' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.take_turn("Mars")
+    round.take_turn("North west") #wrong answer for guess
+
+    expect(round.turns.count).to eq(3)
   end
-  
+
   it 'can determine number of correct guesses in round' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
