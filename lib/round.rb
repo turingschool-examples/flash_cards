ef8d675471f1f87f1acf9b5e121be81bb5ad580d
Laura Court <laura.mcourt@gmail.com>
2021-07-08 21:34:25 -0400

    Add takes turn, is correct, and holds turns tests

---
+++ lib/round.rb
@@ -0,0 +1,15 @@
+class Round
+  attr_reader :deck, :turns
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def current_card
+      @deck.cards.first
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+  end
+end
