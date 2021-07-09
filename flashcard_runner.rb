d89220f04deecb3e92ece8845523516940c67e15
Weston Ellis <ellisweston112@gmail.com>
2021-07-09 17:03:42 -0600

    Refactor and made not case sensitive

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -14,37 +14,25 @@ def start
   round_1 = Round.new(deck_1)
 
 
-  puts "Welcome! You're playing with 4 cards."
-  puts "-------------------------------------------------"
-  puts "This is card number #{round_1.used_cards.length + 1} out of 4" #how can I make this dynamic?
-  puts "#{round_1.current_card.question}" #how can I automate this?
+  (deck_1.count).times {
+    puts "Welcome! You're playing with 4 cards."
+    puts "-------------------------------------------------"
+    puts "This is card number #{round_1.used_cards.length + 1} out of 4" #how can I make this dynamic?
+    puts "#{round_1.current_card.question}" #how can I automate this?
 
-  guess = gets.chomp
-  puts round_1.take_turn(guess).feedback
+    guess = gets.chomp.upcase
 
-  puts "This is card number #{round_1.used_cards.length + 1} out of 4 cards"
-  puts "#{round_1.current_card.question}"
 
-  guess = gets.chomp
-  puts round_1.take_turn(guess).feedback
 
-  puts "This is card number #{round_1.used_cards.length + 1} out of 4 cards"
-  puts "#{round_1.current_card.question}"
+    puts round_1.take_turn(guess).feedback
 
-  guess = gets.chomp
-  puts round_1.take_turn(guess).feedback
+  }
 
-  puts "This is card number #{round_1.used_cards.length + 1} out of 4 cards"
-  puts "#{round_1.current_card.question}"
-
-  guess = gets.chomp
-  puts round_1.take_turn(guess).feedback
-  #end of questions, next is conclusion
 
   puts "****** Game Over! ******"
-      # num correct guess, out of total guess, total percent
+
   puts "You had #{round_1.number_correct} correct guesses out of 4, for a total score of #{round_1.percent_correct}%"
-      #percent correct by category
+
   puts "Math - #{round_1.percent_correct_by_category(:Math)}% correct"  #make dynamic?
   puts "Dinosaurs - #{round_1.percent_correct_by_category(:Dinosaurs)}% correct"
 
