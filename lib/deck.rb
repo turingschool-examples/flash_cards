19d5c4cfa9ac4c69a8d5dce19ecabd4dc96ae30e
Matthew Kimball <mekimba6@gmail.com>
2021-07-09 16:51:35 -0600

    adds count test

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,11 @@ attr_reader :cards
   def initialize(cards)
   @cards = cards
   end
+#
+  def count
+    count = cards.count
+  end
+
+  def cards_in_category
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -24,4 +24,26 @@ RSpec.describe Deck do
     deck = Deck.new(cards)
     expect(deck.cards).to eq([card_1, card_2, card_3])
   end
+  it "can be counted" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    expect(card_1).to be_instance_of(Card)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    expect(card_2).to be_instance_of(Card)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    expect(card_3).to be_instance_of(Card)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck.count).to eq(3)
+  end
+  it 'has a category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    expect(card_1).to be_instance_of(Card)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    expect(card_2).to be_instance_of(Card)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    expect(card_3).to be_instance_of(Card)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck.cards_in_category).to eq(:STEM)
+  end
 end
