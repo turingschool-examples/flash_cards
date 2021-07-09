9567307ff58d427555639628f439d101cbb1ffac
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-08 21:20:09 -0600

    Add cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,4 +5,18 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(category)
+    include_category = []
+    cards.each do |card|
+      if card.category == category
+        include_category << card
+      end
+    end
+    include_category.count
+  end
 end
