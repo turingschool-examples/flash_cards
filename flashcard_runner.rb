32bbe71e894509a98b9e5e96c77610678f93d0da
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-10 17:49:22 -0400

    Add game over method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,4 +1,4 @@
-# require "pry";binding pry
+
 
 require './lib/card'
 require './lib/turn'
@@ -9,34 +9,37 @@ require './lib/round'
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-deck = Deck.new([card_1, card_2, card_3])
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop culture")
+deck = Deck.new([card_1, card_2, card_3, card_4])
 round = Round.new(deck)
-round_2 = Round.new(deck)
 
 def start(round)
-  puts welcome_message(round.deck.count)
+  welcome_message(round.deck.count)
 
   round.deck.cards.each do |card|
-    puts turn_message(round)
+    turn_message(round)
     user_guess(round)
     puts round.turns.last.feedback
   end
+  game_over(round)
 end
 
 def welcome_message(count)
-  "Welcome! You're playing with #{count} cards.
+  puts "Welcome! You're playing with #{count} cards.
   -------------------------------------------------"
 end
 
 def user_guess(round)
   guess = gets.chomp
   round.take_turn(guess)
-
 end
 
 def turn_message(round)
-  "This is card number #{round.turns.length + 1} out of #{round.deck.count}.
+  puts "This is card number #{round.turns.length + 1} out of #{round.deck.count}.
   Question: #{round.current_card.question}"
 end
 
+def game_over(round)
+  puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total of #{round.percent_correct}%."
+end
 start(round)
