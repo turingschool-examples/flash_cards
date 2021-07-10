042701deecb3b1203fc7aded0d0076f2aae25777
Ida Olson <idaolson@gmail.com>
2021-07-10 16:38:21 -0600

    Update runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -11,6 +11,27 @@ round = Round.new(deck)
 
 def start(round)
 
+  puts "Welcome! You're playing with #{round.deck.count} cards"
+  puts "-------------------------------------------------"
+
+  while round.turns.length >= 0
+    puts "This is card number #{round.turns.length + 1} out of #{round.deck.count}."
+    puts "Question: #{round.current_card.question}"
+    round.take_turn(gets.chomp)
+    puts round.turns.last.feedback
+  end
+
+
+  # # round.take_turn(guess)
+  # # #
+  # # # puts {}
+  # # #
+  # puts "****** Game over! ******"
+  # puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct}."
+  # #
+  # # for each |category|
+  # puts "#{turn.card.category} - #{round.percent_correct_by_category(category)}% correct"
+
 end
 
 start(round)
