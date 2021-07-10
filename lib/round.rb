9d941df3c7a7e22ad8a901a78990fc9921af4fb4
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-10 14:51:01 -0600

    Add take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -4,9 +4,20 @@ require './lib/deck'
 
 class Round
   attr_reader :deck, :turns
-  
+
   def initialize(deck)
     @deck = deck
     @turns = []
+  end
+
+  def current_card
+    @deck.cards[@turns.count]
+  end
+
+  def take_turn(guess)
+    next_turn = Turn.new(guess, current_card)
+
+    @turns << next_turn
+    next_turn
   end
 end
