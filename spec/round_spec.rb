b549966486cec3f17bfca6e25ce7b6de7ba96836
matthewjholmes <ilookup@gmail.com>
2021-07-10 15:27:57 -0600

    Finish writing tests for Round class; reach 100% test coverage.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -45,6 +45,16 @@ RSpec.describe(Round) do
     expect(round.current_card).to eq(card_1)
   end
 
+  it 'has a current_card_index function' do
+    card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    card_2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+    card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.current_card_index).to eq(1)
+  end
+
   it 'can take a turn' do
     card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
     card_2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
@@ -79,6 +89,19 @@ RSpec.describe(Round) do
 
     expect(round.number_correct_by_category(:STEM)).to eq(0)
     expect(round.number_correct_by_category(:Geography)).to eq(1)
+  end
+
+  it 'can calculate percent correct' do
+    card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    card_2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+    card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    turn_1 = round.take_turn('Juneau')
+    turn_2 = round.take_turn('Venus')
+
+    expect(round.percent_correct).to eq(33)
   end
 
   it 'can calculate percent correct by category' do
