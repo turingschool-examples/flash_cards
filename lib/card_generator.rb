9fa475c3d8d5ae34f4f212f84d6331c03a88a3f7
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-10 12:35:02 -0600

    Refactor all methods

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -4,24 +4,40 @@ class CardGenerator
   attr_reader :filename
 
   def initialize(filename)
-    @filename = filename
-    @raw_cards = File.open(filename, 'r')
+    @filename  = filename
   end
 
-  def cards_by_line
-    @raw_cards.readlines
+  # test this
+  def open_cards
+    File.open(@filename, 'r')
   end
 
-  def clean_cards_by_line
-    cards_by_line.map { |line| line.chop }
+  # test this
+  def raw_cards_by_line(raw_cards)
+    raw_cards.readlines
   end
 
-  def lines_without_commas
-    clean_cards_by_line.map { |line| line.split(',') }
+  # Removes '\n' from an array of strings.
+  def remove_newlines(array)
+    array.map { |string| string.chop }
+  end
+
+  # Splits string elements into array of strings, within an array.
+  def split_on_commas(array)
+    array.map { |string| string.split(',') }
+  end
+
+  # Requires array of arrays. Inner arrays must contain 3 string elements.
+  def make_cards_from_array(array)
+    array.map { |line| Card.new(line[0], line[1], line[2]) }
   end
 
   def cards
-    lines_without_commas.map { |line| Card.new(line[0], line[1], line[2]) }
+    raw_cards = self.open_cards
+    raw_cards_array = raw_cards_by_line(raw_cards)
+    array_without_newlines = remove_newlines(raw_cards_array)
+    array_without_commas = split_on_commas(array_without_newlines)
+    make_cards_from_array(array_without_commas)
   end
 end
 
