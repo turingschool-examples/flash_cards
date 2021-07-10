3015766e161cb7a6a0819ede99b9d4e69eb32a6b
Ida Olson <idaolson@gmail.com>
2021-07-10 02:36:54 -0600

    Changes to round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,20 @@
+require './lib/turn'
+
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
   def initialize (deck)
     @deck = deck
-    # @turns = turns
+    @turns = []
   end
+
+  def current_card
+    @deck.cards[0 + @turns.count]
+  end
+
+
+
+  # def take_turn(guess)
+  # @turns << Turn.new(guess, current_card)
+  # @turns.last
+  # end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -28,5 +28,41 @@ RSpec.describe Round do
 
       expect(round.deck).to eq(deck)
     end
+
+    it 'has turns' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      expect(round.turns).to eq([])
+    end
+
+    it 'can return the current card' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      expect(round.current_card).to eq(card_1)
+    end
+
+    it 'can take a turn' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+
+      expect(new_turn).to eq(turns[0])
+
+      # finish this one 
+
+    end
+
   end
 end
