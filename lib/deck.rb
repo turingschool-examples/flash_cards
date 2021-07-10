25e29545d84437b88706d4ca254c565677a3217c
Isika Powers <isika.powers@gmail.com>
2021-07-09 20:15:18 -0600

    Refactor Deck class and Flashcard_runners

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,28 +1,22 @@
 class Deck
-  attr_reader :card, :cards
+  attr_reader :cards
+
   def initialize(cards)
     @cards = cards
-    @cards << @card
-  end
-
-  # def card_category
-  #   @card.category
-  # end
-  def cards
-    @cards = []
   end
 
   def count
-    @cards.count
+    @cards.length
   end
 
   def cards_in_category(category)
     list_of_cards = []
     @cards.each do |card|
       if card.category == category
-        list_of_cards << card
+      list_of_cards << card
       end
     end
-    list_of_cards
+  list_of_cards
   end
+
 end
