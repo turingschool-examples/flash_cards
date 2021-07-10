6f3ebe88a9034b88d028c8daf617cf6c422b4c7d
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-10 13:00:33 -0400

    Revise code to make it work

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -11,31 +11,32 @@ card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and repo
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 deck = Deck.new([card_1, card_2, card_3])
 round = Round.new(deck)
+round_2 = Round.new(deck)
 
-def start
-  puts welcome_message
-  until @deck.count == 0
-    start.turn_message
-    user_guess
-    @card.turn.guess.feedback
+def start(round)
+  puts welcome_message(round.deck.count)
+
+  round.deck.cards.each do |card|
+    puts turn_message(round)
+    user_guess(round)
+    puts round.turns.last.feedback
   end
 end
 
-def welcome_message
-  message = "Welcome! You're playing with #{@deck.count} cards.
+def welcome_message(count)
+  "Welcome! You're playing with #{count} cards.
   -------------------------------------------------"
-  puts message
 end
 
-def user_guess
-  @round.take_turn.new_turn = gets.chomp
-  take_turn
-  put @round.new_turn
+def user_guess(round)
+  guess = gets.chomp
+  round.take_turn(guess)
+
 end
 
-def turn_message
-  message = "This is card number #{round.current_card.index_number} out of #{deck.count}.
+def turn_message(round)
+  "This is card number #{round.turns.length + 1} out of #{round.deck.count}.
   Question: #{round.current_card.question}"
-  puts message
-  gets.chomp
 end
+
+start(round)
