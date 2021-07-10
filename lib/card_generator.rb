78653f145d7933a7b5244e2c3962ae0cd06c201a
Rteske <77249542+Rteske@users.noreply.github.com>
2021-07-10 12:48:51 -0700

    Add resources and refactor

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,6 @@
 require './lib/card'
 
 
-
 class CardGenerator
   attr_reader :filename, :cards
 
@@ -10,7 +9,29 @@ class CardGenerator
     @cards = []
   end
 
+  def read_file
+    file = File.open(@filename)
+    file_data = file.readlines.map{|line| line.chomp}
+    file.close
+    return file_data
+  end
 
+  def input_formatter
+    read_file.map {|line| line.split(",")}
+  end
+
+  def make_cards
+    x = 0
+    while x != input_formatter.length
+      category = input_formatter[x][2]
+      answer = input_formatter[x][1]
+      question = input_formatter[x][0]
+
+      @cards << card = Card.new(question, answer, category)
+      x += 1
+    end
+    @cards
+  end
 
 
 
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -6,27 +6,61 @@ RSpec.describe CardGenerator do
   context 'initialize' do
     it 'exists' do
       filename = "cards.txt"
-      cards = CardGenerator.new(filename)
+      generated_cards = CardGenerator.new(filename)
 
 
-      expect(cards).to be_instance_of(CardGenerator)
+      expect(generated_cards).to be_instance_of(CardGenerator)
     end
 
     it 'has a way to store the filename' do
-      filename = "cards.txt"
-      cards = CardGenerator.new(filename)
+      filename = "lib/cards.txt"
+      generated_cards = CardGenerator.new(filename)
 
-      expect(cards.filename).to eq("cards.txt")
+      expect(generated_cards.filename).to eq("lib/cards.txt")
     end
 
     it 'has an empty array to store cards' do
-    filename = "cards.txt"
-    cards = CardGenerator.new(filename)
+    filename = "./resources/cards.txt"
+    generated_cards = CardGenerator.new(filename)
 
-    expect(cards.cards).to eq([])
+    expect(generated_cards.cards).to eq([])
     end
-  context 'methods'
-    it 'can read other files by using the filename'
-      
+  end
+
+  context 'methods' do
+
+    context 'read_file' do
+      it 'can read other files by using the filename' do
+        filename = "./resources/cards.txt"
+        generated_cards = CardGenerator.new(filename)
+
+        expect(generated_cards.read_file).to eq(["What is 5 + 5?,10,STEM",
+          "What is Rachel's favorite animal?,red panda,Turing Staff",
+          "What is Mike's middle name?,nobody knows,Turing Staff",
+          "What cardboard cutout lives at Turing?,Justin bieber,PopCulture"])
+      end
+    end
+
+    context 'input_formatter' do
+      it 'can take file data and convert it into an array of each string/element' do
+        filename = "./resources/cards.txt"
+        generated_cards = CardGenerator.new(filename)
+
+        expect(generated_cards.input_formatter[0]).to eq(["What is 5 + 5?", "10", "STEM"])
+        expect(generated_cards.input_formatter[1]).to eq(["What is Rachel's favorite animal?", "red panda", "Turing Staff"])
+        expect(generated_cards.input_formatter[2]).to eq(["What is Mike's middle name?", "nobody knows", "Turing Staff"])
+        expect(generated_cards.input_formatter[3]).to eq(["What cardboard cutout lives at Turing?", "Justin bieber", "PopCulture"])
+      end
+    end
+    context 'make_cards' do
+      it 'uses input_formatter to convert each array into a card object' do
+        filename = "./resources/cards.txt"
+        generated_cards = CardGenerator.new(filename)
+
+        expect(generated_cards.make_cards[0]).to be_a(Card)
+        expect(generated_cards.make_cards[1]).to be_a(Card)
+        expect(generated_cards.make_cards[2]).to be_a(Card)
+      end
+    end
   end
 end
