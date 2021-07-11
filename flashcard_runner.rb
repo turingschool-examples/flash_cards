2c87be97f77d4fd3ded0ee3ffe1cfec60e0522fa
Henry Schmid <henry.schmid1@gmail.com>
2021-07-11 16:24:24 -0600

    Add option to choose number of cards

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,11 +3,6 @@ require './lib/deck'
 require './lib/round'
 require './lib/card_generator'
 
-filename = 'lib/cards.txt'
-@cards = CardGenerator.new(filename)
-@deck = Deck.new(@cards.cards.shuffle)
-@round = Round.new(@deck)
-
 def print_slow(text)
   text.split(//).each do |char|
     sleep 0.05
@@ -18,13 +13,22 @@ end
 
 print_slow('Welcome to Flash Cards!')
 sleep 1
-print_slow "\nYou're playing with #{@cards.cards.length} cards.\nYou must score 80% correct or higher to win.\n"
+puts 'How many cards would you like to play with?'
+
+@number_of_cards = gets.chomp.to_i
+
+print_slow "\nYou're playing with #{@number_of_cards} cards.\nYou must score 80% correct or higher to win.\n"
 50.times { print '-' }
 sleep 1
 
+filename = 'cards.txt'
+@cards = CardGenerator.new(filename).cards.sample(@number_of_cards)
+@deck = Deck.new(@cards)
+@round = Round.new(@deck)
+
 def play_flashcards
-  while @round.turns.length < @cards.cards.length
-    puts "\nThis is card number #{@round.turns.length + 1} out of #{@cards.cards.length}."
+  while @round.turns.length < @number_of_cards
+    puts "\nThis is card number #{@round.turns.length + 1} out of #{@number_of_cards}."
     puts "Question: #{@round.current_card.question}"
     print 'Answer: '
 
@@ -42,7 +46,7 @@ def play_flashcards
     50.times { print '-' }
   end
 
-  if @round.turns.length == @cards.cards.length
+  if @round.turns.length == @number_of_cards
     puts "\n\n****** Game over! ******\n\n"
 
     guess = if @round.number_correct == 1
@@ -51,18 +55,18 @@ def play_flashcards
               'guesses'
             end
 
-    if @round.percent_correct > 80.0
+    if @round.percent_correct >= 80
       print_slow "Congrats! You win!\n\n"
     else
       print_slow "As Willy Wonka would say...YOU LOSE!\n"
     end
 
     sleep 1
-    puts "You had #{@round.number_correct} correct #{guess} out of #{@cards.cards.length} for a total score of #{@round.percent_correct.to_i}% correct."
+    puts "You had #{@round.number_correct} correct #{guess} out of #{@cards.length} for a total score of #{@round.percent_correct.to_i}% correct."
     sleep 3
     puts "\nCategories:"
 
-    categories = @cards.cards.map do |card|
+    categories = @cards.map do |card|
       card.category
     end
 
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -2,7 +2,7 @@ require './lib/card_generator'
 
 RSpec.describe CardGenerator do
   before :each do
-    filename = './lib/cards.txt'
+    filename = 'cards.txt'
     @cards = CardGenerator.new(filename)
   end
 
@@ -11,15 +11,15 @@ RSpec.describe CardGenerator do
   end
 
   it 'returns the cards filename' do
-    expect(@cards.filename).to eq('./lib/cards.txt')
+    expect(@cards.filename).to eq('cards.txt')
   end
 
   it 'reads the cards file and creates Card instances for each' do
     expect(@cards.cards[0].question).to eq("Who is the greatest quarterback of all time?")
     expect(@cards.cards[0].answer).to eq("Tom Brady")
     expect(@cards.cards[0].category).to eq(:Sports)
-    expect(@cards.cards[10].question).to eq("What mountain's peak is furthest from the center of the Earth?")
-    expect(@cards.cards[10].answer).to eq("Chimborazo")
-    expect(@cards.cards[10].category).to eq(:Geography)
+    expect(@cards.cards[11].question).to eq("What mountain's peak is furthest from the center of the Earth?")
+    expect(@cards.cards[11].answer).to eq("Chimborazo")
+    expect(@cards.cards[11].category).to eq(:Geography)
   end
 end
