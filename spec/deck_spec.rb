86d9c03dc21b84f40c66d987cd8b67a93d8f9e80
Andrew Patterson <andrew.patterson747@gmail.com>
2021-07-11 14:47:05 -0500

    deck_spec.rb up-and-running fully

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,4 @@
+require 'rspec'
 require './lib/card'
 require './lib/deck'
 
@@ -12,7 +13,7 @@ RSpec.describe Deck do
     expect(deck).to be_instance_of(Deck)
   end
 
-  xit'exists' do
+  it 'fans the cards' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -22,7 +23,7 @@ RSpec.describe Deck do
     expect(deck.cards).to eq(cards)
   end
 
-  xit'exists' do
+  it 'counts the cards' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -32,32 +33,28 @@ RSpec.describe Deck do
     expect(deck.count).to eq(3)
   end
 
-  xit'exists' do
+  it 'shows the cards in a category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
-    expect(deck.cards_in_category(:STEM)).to eq(card.category)
-    # if this ^ doesnt work. run below
-    # expect(deck.cards_in_category(:STEM)).to eq(:STEM)
-    # with below
-    # expect(deck.cards_in_category(:STEM)).to eq(:Geography)
-    # and remove the following `it` statement
+    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+
   end
 
-  xit'exists' do
+  it 'shows the cards in another category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
-    expect(deck.cards_in_category(:STEM)).to eq(:Geography)
+    expect(deck.cards_in_category(:Geography)).to eq([card_1])
   end
 
-  xit'exists' do
+  it 'looks for a category that is empty' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
