063338a1541fa07207f18ddf613ae597c11ea417
Ted S <tstaros23@gmail.com>
2021-07-11 13:20:00 -0600

    Fix main

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,21 +5,21 @@ require './lib/round'
 require './lib/turn'
 require './lib/game'
 
-RSpec.describe FlashcardRunner do
+RSpec.describe Game do
   it "exists" do
     cards = []
     deck = Deck.new(cards)
     round = Round.new(deck)
-    flashcard_runner = FlashcardRunner.new(round)
+    flashcard_runner = Game.new(round)
     flashcard_runner.game_over_categories
-    expect(flashcard_runner).to be_a(FlashcardRunner)
+    expect(flashcard_runner).to be_a(Game)
   end
 
   it "has attributes" do
     cards = []
     deck = Deck.new(cards)
     round = Round.new(deck)
-    flashcard_runner = FlashcardRunner.new(round)
+    flashcard_runner = Game.new(round)
 
     expect(flashcard_runner.round).to eq(round)
   end
@@ -28,9 +28,9 @@ RSpec.describe FlashcardRunner do
     cards = []
     deck = Deck.new(cards)
     round = Round.new(deck)
-    flashcard_runner = FlashcardRunner.new(round)
+    flashcard_runner = Game.new(round)
 
-    allow_any_instance_of(FlashcardRunner).to receive(:get_user_input).and_return("input")
+    allow_any_instance_of(Game).to receive(:get_user_input).and_return("input")
     expect(flashcard_runner.get_user_input).to eq("input")
   end
 
