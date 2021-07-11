c24c6a4193e259628443b116be7f333136961312
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-10 22:19:40 -0400

    Attempt to clean up code and make iterations work properly. Goodnight

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,11 +4,10 @@ require './lib/turn'
 require './lib/round'
 
 class FlashCardRun
-
+#
   def initialize(round)
     @round = round
     @deck = round.deck
-    @current_card = round.current_card
     @current_question = round.current_card.question
   end
 
@@ -33,9 +32,10 @@ class FlashCardRun
 
 #turn message - card x of 4, question
   def turn_message(round)
-    puts "This is card number #{round.turns.length + 1} of #{@deck.count + round.turns.length}." #NEED TO FIX DECK count
+    puts "This is card number #{round.turns.length + 1} of #{@deck.count + round.turns.length}."
     puts "Question: #{round.current_card.question}?"
   end
+
 
 #where i get user input for each guess
   def get_guess(round)
@@ -46,7 +46,7 @@ class FlashCardRun
 #final message when all cards are used
   def game_over(round)
     puts "****** Game over! ******"
-    puts "You had #{round.number_correct} correct guesses out of #{@deck.count} for a total score of #{round.percent_correct} !"
+    puts "You had #{round.number_correct} correct guesses out of #{@deck.count} for a total score of #{round.percent_correct}%!"
   end
 end
 
@@ -57,6 +57,8 @@ card_1 = Card.new("Who has the most career points in the NHL", "Wayne Gretzky", 
 card_2 = Card.new("Who won the UEFA Champions League in 2021", "Chelsea", :Sports)
 card_3 = Card.new("How many movies are there in the Harry Potter series?", "8", :Entertainment)
 card_4 = Card.new("What year did The Lord of the Rings: The Fellowship of the Ring release?", "2001", :Entertainment)
-deck = Deck.new([card_1, card_2, card_3, card_4])
+cards = [card_1, card_2, card_3, card_4]
+deck = Deck.new(cards)
 first_round = Round.new(deck)
-new_game = FlashCardRun.new(first_round).start(first_round)
+new_game = FlashCardRun.new(first_round)
+new_game.start(first_round)
