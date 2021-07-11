f76be1856aeac772950648485c018713493483b9
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-11 17:45:43 -0600

    Add comments to turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,11 +4,17 @@ require './lib/card'
 class Turn
   attr_reader :guess, :card
 
+  # initialize a turn with a guess and a Card object
   def initialize(guess, card)
     @guess = guess
     @card = card
   end
 
+  # determine if the user's guess matches the expected guess
+  # compare the user's guess to the card's answer - downcase both to account
+  # for any capitalization at the beginning
+  # return true if guess is correct
+  # return false if guess is incorrect
   def correct?
     if @guess.downcase == @card.answer.downcase
       true
@@ -17,6 +23,9 @@ class Turn
     end
   end
 
+  # provide feedback to the user if they have guessed correctly/incorrectly
+  # call the 'correct?' - if the user's guess was true, return 'Correct!' feedback
+  # if user's guess was false, return "Incorrect" feedback
   def feedback
     if correct? == true
       "Correct!"
@@ -24,5 +33,4 @@ class Turn
       "Incorrect"
     end
   end
-
  end
