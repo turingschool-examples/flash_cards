d7a15335ac431a0e5633a6992f94ec8f139bdb29
Andrew Massey <acmassey@ncsu.edu>
2021-07-11 14:13:56 -0400

    Complete methods and testing for iteration 2

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,14 @@
+require './lib/deck'
+
 class Round
-  attr_reader :deck, :turns, :correct_counter, :current_card
+  attr_reader :deck, :turns, :correct_counter, :current_card, :num_turns
 
   def initialize (deck)
     @deck = deck
     @turns = []
     @correct_counter = 0
     @current_card = 0
+    @correct_for_category = 0
   end
 
   def current_card
@@ -24,8 +27,29 @@ class Round
   end
 
   def number_correct
-    @turns.find_all do |turn|
-      return @correct_counter + 1 if turn.correct?
+    @turns.each do |turn|
+      @correct_counter += 1 if turn.correct?
     end
+    @correct_counter
+  end
+
+  def number_correct_by_category(category)
+    @turns.count do |turn|
+     turn.correct? if turn.card.category == category
+    end
+  end
+
+  def percent_correct
+    (number_correct / @turns.count.to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    num_in_category = 0
+
+      @turns.each do |turn|
+        num_in_category += 1 if turn.card.category == category
+      end
+      
+    (number_correct_by_category(category) / num_in_category.to_f) * 100
   end
 end
