8e58a1fe9314a5f3581aa373473503322f00e8ec
Matthew Kimball <mekimba6@gmail.com>
2021-07-11 09:58:50 -0600

    updates test comments

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -92,19 +92,33 @@ end
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
     expect(new_turn).to be_a(Turn)
+    # expects the turn to be true
     expect(new_turn.correct?).to eq(true)
+    # expects round.turn to move to a new turn
     expect(round.turns).to eq([new_turn])
+    # expects the number correct to be 1
     expect(round.number_correct).to eq(1)
+    # expects it to go to the next card
     expect(round.current_card).to eq(card_2)
+    #adds a new turn
     new_turn = round.take_turn("Venus")
+    #new turn is a turn
     expect(new_turn).to be_a(Turn)
+    # expects turn count to be 2
     expect(round.turns.count).to eq(2)
+    # checks that the answer is wrong
     expect(round.turns.last.feedback).to eq("Incorrect!")
+    # expects the correct answers to be 1
     expect(round.number_correct).to eq(1)
+    # expects number of geography questions correct to be 1
     expect(round.number_correct_by_category(:Geography)).to eq(1)
+    # expects STEM correct answers to be 0
     expect(round.number_correct_by_category(:STEM)).to eq(0)
+    # checks percentage correct
     expect(round.percent_correct).to eq(50.0)
+    # checks percentage correct by category
     expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
+    # expects to be on card 3
     expect(round.current_card).to eq(card_3)
   end
 
