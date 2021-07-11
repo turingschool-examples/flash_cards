0e02fe0362dfc92e86afd52a36b2ec6987109bd2
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-11 17:38:52 -0600

    Add comments in round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -12,10 +12,15 @@ class Round
       @count = 0
     end
 
+    # return the number of cards in a deck
     def current_card
       @deck.cards[count]
     end
 
+    # creates a new turn (has a guess, has the current card)
+    # stores the turn into the @turns array
+    # updates @count - the current number of turns
+    # returns a Turn object
     def take_turn(guess)
       turn = Turn.new(guess, current_card)
       @turns << turn
@@ -23,6 +28,10 @@ class Round
       turn
     end
 
+    # create a counter for the number of correct turns
+    # run through the @turns array and determine if each turn is correct
+    # update the counter for the number of correct turns for each turn correct
+    # return the integer value for the number of correct turns
     def number_correct
       count_of_correct_turns = 0
 
@@ -34,6 +43,10 @@ class Round
         count_of_correct_turns
     end
 
+    # create a counter for the number of correct turns by each category
+    # if a turn is correct and that turns category matches the category passed in
+    # update the counter
+    # return the integer value for the number correct by category
     def number_correct_by_category(category)
       correct_count = 0
       @turns.each do |turn|
@@ -44,17 +57,24 @@ class Round
       correct_count
     end
 
+    # determine the total percentage of the round
+    # Divide the number of correct turns by the total number of cards in the deck
+    # turn the integers into floats then multiply by 100 to get the total percent
+    # return the float value for the percentage
     def percent_correct
       division_to_integer  = (number_correct.to_f / @count)
       ((number_correct.to_f / @count) * 100).to_i
     end
 
+    # determine the percent correct by the category passed in
+    # if there are no cards of that category in that deck, return a float of 0.0
+    # divide the number correct in that category by the total cards in that category
+    # multiply by 100 to get a percentage, then change integer to a float
+    # return a float value for the percent correct in that category
     def percent_correct_by_category(category)
-      # if there are no cards of that category in that deck
-      # return 0.0
       if deck.cards_in_category(category).length == 0
         return 0.0
-      end 
+      end
       ((number_correct_by_category(category) / deck.cards_in_category(category).length) * 100).to_f
     end
 end
