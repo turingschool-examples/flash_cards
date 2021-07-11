7d6fe3eb03f33614236277bc14186b9e151ddf8c
ChloeLL <chloell@protonmail.ch>
2021-07-11 11:22:01 -0700

    Initial Commit

---
+++ flashcard_runner.rb
@@ -0,0 +1,7 @@
+card_1 = Card.new()
+card_2 = Card.new()
+card_3 = Card.new()
+card_4 = Card.new()
+cards = [card_1, card_2, card_3, card_4]
+deck = Deck.new([card_1, card_2, card_3, card_4])
+round = Round.new(deck)
