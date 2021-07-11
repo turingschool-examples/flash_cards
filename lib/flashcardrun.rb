f85648b9d96c07bd953d7d6944c5411e6aeef5e4
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-11 18:11:30 -0400

    Remove unneccessary code comments

--- lib/flashcardrun.rb
+++ lib/flashcardrun.rb
@@ -6,7 +6,6 @@ class FlashCardRun
     @current_question = round.current_card.question
   end
 
-#running the game
   def start(round)
     puts "Welcome! You're playing with #{@deck.count} cards."
     puts "-------------------------------------------------"
@@ -20,13 +19,11 @@ class FlashCardRun
     game_over(round)
   end
 
-#turn message - card x of 4, question
   def turn_message(round)
     puts "This is card number #{round.turns.length + 1} of #{@deck.count + round.turns.length}."
     puts "Question: #{round.current_card.question}?"
   end
 
-#where i get user input for each guess
   def get_guess(round)
     new_guess = gets.chomp
     round.take_turn(new_guess)
@@ -38,7 +35,6 @@ class FlashCardRun
     end
   end
 
-#final message when all cards are used
   def game_over(round)
     puts "****** Game over! ******"
     puts "You had #{round.number_correct} correct guesses out of #{round.turns.length} for a total score of #{round.percent_correct.to_i}%."
