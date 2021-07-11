397677582f779a4ac3fcb183647059d72a5c2fec
Sawaez Amin <sawaezamin@gmail.com>
2021-07-11 16:30:47 -0600

    make changes to deck lib file

--- lib/deck.rb
+++ lib/deck.rb
@@ -17,4 +17,10 @@ class Deck
     end
     categorized_cards
   end
+
+  # def cards_in_category(category)
+  # can use the enumerable find_all
+  # @cards.find_all do |card|
+  #  card.category == category
+  # end
 end
