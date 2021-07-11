7e67db8e170b0eb135a00e4368ce981ce8d52054
Laura Court <laura.mcourt@gmail.com>
2021-07-11 04:47:14 -0400

    Refactor cards_in_category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,6 @@
 class Deck
-
   attr_reader :cards
+
   def initialize(cards)
     @cards = cards
   end
@@ -12,9 +12,7 @@ class Deck
   def cards_in_category(category)
     card_category = []
     @cards.each do |card|
-      if card.category == category
-      card_category << card
-      end
+      card_category << card if card.category == category
     end
     card_category
   end
