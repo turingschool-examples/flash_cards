abc672e7e92577153eaec08fafad705493eb2b56
Kelsey Thompson <kelsthompson2@gmail.com>
2021-07-11 13:15:21 -0400

    Write code that works

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,4 +1,31 @@
 
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
 
-class CardGenerator(filename)
-  
+class CardGenerator
+  attr_reader :deck
+  def initialize(filename)
+    @filename = filename
+    @deck = []
+  end
+
+  def create_new_deck
+    File.readlines(@filename).each do |line|
+      @deck << card_from_line(line)
+    end
+    @deck
+  end
+
+  def card_from_line(line)
+    card_pieces = line.split(',').map do |piece|
+      if piece.include?("\n")
+        piece.delete("\n")
+      else
+        piece
+      end
+    end
+    Card.new(card_pieces[0], card_pieces[1], card_pieces[2])
+  end
+end
