8ab9d98cd6fd287ebf3ae0b1331b7d5d1c53a79c
Andrew Massey <acmassey@ncsu.edu>
2021-07-11 18:13:01 -0400

    Refactor flashcards_runner

--- lib/flashcards_runner.rb
+++ lib/flashcards_runner.rb
@@ -4,13 +4,9 @@ card_1 = Card.new("What is 5 + 5?", "10", :Math)
 card_2 = Card.new("What is Rachel's favorite animal?", "Dog", :Turing_staff)
 card_3 = Card.new("What is Mike's middle name?", "Nobody knows", :Turing_staff)
 card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_culture)
-
-
 cards = [card_1, card_2, card_3, card_4]
-
 deck = Deck.new(cards)
 round = Round.new(deck)
-
 
 puts "Welcome! You're playing with #{cards.count} cards.
 -------------------------------------------------"
@@ -27,5 +23,4 @@ puts "****** Game over! ******
 You had #{round.number_correct} correct guesses out of #{round.turns.count} for a total score of #{round.percent_correct}.
 Math - #{round.percent_correct_by_category(:Math)}
 Turing Staff - #{round.percent_correct_by_category(:Turing_staff)}
-Pop Culture - #{round.percent_correct_by_category(:Pop_culture)}
-"
+Pop Culture - #{round.percent_correct_by_category(:Pop_culture)}"
