10b3acc863be5699b77812f5d874efdd7fab34e9
Andrew Massey <acmassey@ncsu.edu>
2021-07-11 18:43:19 -0400

    Set up basic structure for card generator class

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,5 +1,12 @@
-
-
-
-
 require './lib/card_generator'
+
+
+RSpec.describe Card_generator do
+
+  xit 'Exists' do
+    filename = 'cards.txt'
+    cards = Card_generator.new(filename).cards
+
+    expect(cards).to be_a(Card_generator)
+  end
+end
