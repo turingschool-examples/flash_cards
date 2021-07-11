cbb36a3857b5d8c035c6fb16e86991395d448d56
Matthew Kimball <mekimba6@gmail.com>
2021-07-11 11:10:40 -0600

    rewrites and adds questions and answers

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,13 +3,17 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-cards = [card_1, card_2, card_3]
+card_1 = Card.new("What is the name of my cat?", "Mabel", :Pets)
+card_2 = Card.new("What type of beer is sour and made with coriander and salt?", "Gose", :Beer)
+card_3 = Card.new("What is the name of a type of climbing hold that is big and easy to grip?", "Jug", :Climbing)
+card_4 = Card.new("What was the name of my childhood puppy?", "Cera", :Pets)
+card_5 = Card.new("What is the name of a type of climbing hold that is tiny and sharp?", "Crimp", :Climbing)
+card_6 = Card.new("What type of beer comes from the Cologne region of Germany?", "Kolsch", :Beer)
+
+
+cards = [card_1, card_2, card_3, card_4, card_5, card_6]
 deck = Deck.new(cards)
 round = Round.new(deck)
-# new_turn = round.take_turn()
 
 puts "Welcome! You're playing with #{cards.count} cards."
 40.times {print "-"}
@@ -20,7 +24,7 @@ card_answer = gets.chomp
 if card_answer.capitalize == card_1.answer
   p "Correct!"
 else
-  p "Incorrect!"
+  p "Incorrect! The correct answer is #{card_1.answer}"
 end
 
 new_turn = round.take_turn("Juneau")
@@ -30,7 +34,7 @@ card_answer = gets.chomp
 if card_answer.capitalize == card_2.answer
   p "Correct!"
 else
-  p "Incorrect!"
+  p "Incorrect! The correct answer is #{card_2.answer}"
 end
 
 new_turn = round.take_turn("Juneau")
@@ -40,5 +44,5 @@ card_answer = gets.chomp
 if card_answer.capitalize == card_3.answer
   p "Correct!"
 else
-  p "Incorrect!"
+  p "Incorrect! The correct answer is #{card_3.answer}"
 end
