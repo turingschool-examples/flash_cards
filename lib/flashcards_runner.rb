e30b9e09fd047496f7e6b8b73594932a0d7b7c1b
Andrew Massey <acmassey@ncsu.edu>
2021-07-11 15:37:42 -0400

    Create working method for question 1

--- lib/flashcards_runner.rb
+++ lib/flashcards_runner.rb
@@ -8,3 +8,23 @@ card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Po
 cards = [card_1, card_2, card_3, card_4]
 
 deck = Deck.new(cards)
+round = Round.new(deck)
+
+
+
+puts "Welcome! You're playing with 4 cards.
+-------------------------------------------------
+This is card number 1 out of 4.
+Question: What is 5 + 5?"
+
+new_turn = round.take_turn(gets.chomp)
+
+if new_turn.correct?
+  puts "Correct!
+This is card number 2 out of 4.
+Question: What is Rachel's favorite animal?"
+else
+  puts "Incorrect.
+  This is card number 2 out of 4.
+  Question: What is Rachel's favorite animal?"
+end
