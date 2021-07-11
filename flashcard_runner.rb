e85cbd7ff65fab7bab27d4e90f1061a04a96403d
Ida Olson <idaolson@gmail.com>
2021-07-10 19:52:35 -0600

    Add array of categories

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -15,22 +15,40 @@ def start(round)
   puts "-------------------------------------------------"
 
   card_count = round.deck.count
+  list_of_categories = []
 
   while round.deck.count > 0
      puts "This is card number #{round.turns.length + 1} out of #{card_count}."
      puts "Question: #{round.current_card.question}"
+
+     round.deck.cards.map {|card| card.category }.uniq.each do |category|
+       list_of_categories.push(category) unless list_of_categories.include?(category)
+     end
+
      round.take_turn(gets.chomp)
      puts round.turns.last.feedback
    end
 
-
-
   puts "****** Game over! ******"
   puts "You had #{round.number_correct.round} correct guesses out of #{card_count} for a total score of #{round.percent_correct.round}%."
 
+
+
   # round.turns.each do |category|
-  #   puts "#{turn.card.category} - #{round.percent_correct_by_category(category)}% correct"
+  #   puts "#{round.card.category} - #{round.percent_correct_by_category(category)}% correct"
   # end
+
+  # round.turns.map do |category|
+  #   puts "#{category} - #{round.percent_correct_by_category(category)}% correct"
+  # end
+  puts round.deck.count
+  puts list_of_categories
+
+  # round.deck.cards.map { |category|  }
+  #
+  #   puts "#{category} - #{round.percent_correct_by_category(category).round}%"
+  # end
+
 
 end
 
