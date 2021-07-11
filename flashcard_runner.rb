b35b083779c81011881a1915853f50164abdd682
Erin Quinn <equinn125@gmail.com>
2021-07-11 16:04:11 -0600

    Get game to loop

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,11 +7,24 @@ require './lib/game'
 card_1 = Card.new("What is 5 + 5", "10", :STEM)
 card_2 = Card.new("What is Rachel's favorite animal?", "cat", :Turing_Staff)
 card_3 = Card.new("What is Mikes's middle name?", "nobody knows", :Turing_Staff)
-card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin_Bieber", :Pop_Culture)
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
 deck = Deck.new([card_1, card_2, card_3, card_4])
 round = Round.new(deck)
 game = Game.new(round)
 game.start
+# game.draw_card
+
+# puts "Welcome! you're playing with #{round.deck.count} cards
+#  ----------------------------------------------------
+#  This is card #{round.turns.count+1} out of #{round.deck.count}
+#  Question: #{round.current_card.question}"
+#
+# new_turn=round.take_turn(gets.chomp)
+#
+# if new_turn == turns.correct?
+#   puts "new_turn
+#   Correct!"
+# end
 # def start
 #   puts "Welcome! you're playing with #{current_card} cards"
 # end
--- lib/game.rb
+++ lib/game.rb
@@ -9,9 +9,32 @@ class Game
     @round = round
   end
   def start
+
     puts "Welcome! you're playing with #{round.deck.count} cards"
     puts '-'*45
-    puts "This is card #{round.turns.count+1} out of #{round.deck.count} "
+    until round.deck.count == 0
+    puts "This is card #{round.turns.count+1} out of 4 "
     puts "Question: #{round.current_card.question}"
+     # new_guess= round.take_turn(gets.chomp)
+    # if new_guess == round.current_card.answer.correct?
+    if gets.chomp == round.current_card.answer
+      round.take_turn(gets.chomp)
+      puts "Correct!"
+    else
+      round.take_turn(gets.chomp)
+    puts "Incorrect!"
   end
+end
+puts "****** Game over! ******
+You had correct guesses out of #{round.deck.count+4}"
+end
+# end
+  # def draw_card
+  #   new_guess= gets.chomp
+  #   if new_guess == round.current_card.answer
+  #     puts "Correct!"
+  #   else
+  #   puts "Incorrect!"
+  #   end
+  # end
 end
