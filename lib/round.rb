1dea0419c25be874e2cf783d7457cc1cb214a478
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-11 15:42:30 -0400

    Clean up files, remove uncessary comments

--- lib/round.rb
+++ lib/round.rb
@@ -6,26 +6,17 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
-    # @number_correct = 0
   end
 
   def take_turn(current_guess)
     current_turn = Turn.new(current_guess, current_card)
     @turns << current_turn
-    # add_correct_turns(current_turn)
     @deck.cards.shift
-    # return current_turn #maybe @turns.last
   end
 
   def current_card
     @deck.cards[0]
   end
-
-  # def add_correct_turns(turn)
-  #   if turn.correct?
-  #     @number_correct += 1
-  #   end
-  # end
 
   def number_correct
     correct_turns = @turns.find_all do |turn|
@@ -33,10 +24,6 @@ class Round
     end
     correct_turns.length
   end
-
-  #   @turns.each do |turn|
-  #
-  # end
 
   def number_correct_by_category(category)
     @turns.count do |turn|
