17e78f885d6aa8dd09767f264389d992eff41651
Henry Schmid <henry.schmid1@gmail.com>
2021-07-11 12:56:48 -0600

    Create cards method for CardGenerator class

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -7,6 +7,19 @@ RSpec.describe CardGenerator do
   end
 
   it 'exists' do
-    expect(@cards).to be_instance_of(CardGenerator)
+    expect(@cards).to be_a(CardGenerator)
+  end
+
+  it 'returns the cards filename' do
+    expect(@cards.filename).to eq('./lib/cards.txt')
+  end
+
+  it 'reads the cards file and creates Card instances for each' do
+    expect(@cards.cards[0].question).to eq("Who is the greatest quarterback of all time?")
+    expect(@cards.cards[0].answer).to eq("Tom Brady")
+    expect(@cards.cards[0].category).to eq(:Sports)
+    expect(@cards.cards[10].question).to eq("What mountain's peak is furthest from the center of the Earth?")
+    expect(@cards.cards[10].answer).to eq("Chimborazo")
+    expect(@cards.cards[10].category).to eq(:Geography)
   end
 end
