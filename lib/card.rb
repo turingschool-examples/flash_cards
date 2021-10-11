ad615f2be6b37cdd9a2680c10ecb1f73ba9d46cf
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-11 17:26:17 -0600

    Add comments

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,10 @@
 class Card
   attr_accessor :question, :answer, :category
 
-  def initialize(q = nil, a = nil, c = nil)
+  def initialize(q = nil, a = nil, c = nil) # Default values for parameters are null
     @question = q
     @answer = a
-    @category = c.to_sym
+    @category = c.to_sym # Make sure our category variable is a symbol
   end
 
 end
