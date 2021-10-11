a2035a0dc89014a2a0c50ebf01d38a41cdd099a3
John Napier <jcee.napier@gmail.com>
2021-10-11 17:40:01 -0600

    Feat: add turn class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,25 +1,25 @@
 require './lib/card'
 
 RSpec.describe Card do
-  it 'exists' do
+  xit 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card).to be_instance_of(Card)
   end
 
-  it 'has a question' do
+  xit 'has a question' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.question).to eq("What is the capital of Alaska?")
   end
 
-  it 'has an answer' do
+  xit 'has an answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.answer).to eq("Juneau")
   end
 
-  it 'has a category' do
+  xit 'has a category' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.category).to eq(:Geography)
