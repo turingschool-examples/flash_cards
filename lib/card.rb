a8780394f2989f8ff987f60c4f5121e5077dacf8
Eric Chon <echon92@gmail.com>
2021-10-11 16:36:51 -0600

    Completed and set up Card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,12 +1,9 @@
 class Card
   attr_accessor :question, :answer, :category
-
+  
   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
   end
 end
-
-card_1 = Card.new("What is the capital of Alaska?", "Jueau", "Geography")
-puts card_1
