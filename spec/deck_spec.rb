a0956f4f6b05edff21576233cde9b5c74a20e286
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-11 18:24:10 -0500

    Test: add test to check for 3 cards in deck when initialized

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe do
     expect(deck).to be_instance_of(Deck)
   end
 
-  xit "has starts with a deck of 3 cards" do
+  it "has starts with a deck of 3 cards" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
