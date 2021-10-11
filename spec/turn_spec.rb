ca28c8796ccc65cc314546f7c7c49c3bf990fad1
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-11 17:41:56 -0500

    Test: add test for feedback method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe Turn do
     expect(turn.correct?).to eq(true)
   end
 
-  xit "can give feedback" do
+  it "can give feedback" do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
