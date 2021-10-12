fce02ecfc1c6b49d144e09c3565402949c44d917
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-11 19:48:36 -0600

    Create class skeleton

---
+++ lib/round.rb
@@ -0,0 +1,39 @@
+class Round
+  attr_accessor :deck
+
+  def initialize(d)
+    @obj_deck = d
+  end
+
+  def deck
+    self.obj_deck
+  end
+
+  def turns
+
+  end
+
+  def curent_card
+
+  end
+
+
+  def take_turn(i)
+
+  end
+
+  def number_correct
+
+  end
+
+  def number_correct_by_category
+
+  end
+
+  def percent_correct
+
+  end
+
+
+
+end
