69f3811615fade277fce1ca5267f3f67c9de0f36
Tim Roeder <timothyjroeder@gmail.com>
2021-10-11 21:51:29 -0600

    Add Turn class guess test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Turn do
     # Test whether the Turn class exists.
     expect(turn).to be_instance_of(Turn)
   end
+
+  it 'has a guess' do
+    card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    turn = Turn.new('Juneau', card)
+
+    # Test the Turn class guess.
+    expect(turn.guess).to eq('Juneau')
+  end
 end
