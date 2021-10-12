7404932c3b43411bd2c6219beda5263e47b24a60
Kevin Gloss <kgloss89@gmail.com>
2021-10-12 08:48:56 -0600

    Create turn class

---
+++ lib/turn.rb
@@ -0,0 +1,26 @@
+class Turn
+  attr_accessor :guess, :card
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    # require 'pry'; binding.pry
+    if @guess == @card.answer
+      @guess = true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if @guess == @card.answer
+      'Correct!'
+    else
+      'Incorrect.'
+    end
+  end
+
+
+end
