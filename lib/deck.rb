9d64f4de79fe624cf03713aa5297ef4e5b9694fb
Eldridge Turambi <eturambi95@gmail.com>
2021-10-12 09:03:33 -0600

    Add count and cards_in_category methods

---
+++ lib/deck.rb
@@ -0,0 +1,22 @@
+class Deck
+  attr_accessor :cards
+
+  def initialize(cards)
+    @cards = []
+  end
+
+  def add_card(card_added)
+    @cards << card_added
+  end
+
+  def count
+    @cards.length
+  end
+
+  def cards_in_category(card_category)
+    cards_in_category_array = @cards.find_all do |card|
+      card.category == card_category
+    end
+    cards_in_category_array
+  end
+end
