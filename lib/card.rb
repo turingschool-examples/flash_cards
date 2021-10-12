279907217b4d7563d04ebb62d313ed6f0efc5be7
Kevin Gloss <kgloss89@gmail.com>
2021-10-11 20:28:56 -0600

    Refactor card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,20 +1,20 @@
 class Card
-
+  attr_reader :question, :answer, :category
   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
   end
 
-  def question
-    @question
-  end
-
-  def answer
-    @answer
-  end
-
-  def category
-    @category
-  end
+  # def question
+  #   @question
+  # end
+  #
+  # def answer
+  #   @answer
+  # end
+  #
+  # def category
+  #   @category
+  # end
 end
