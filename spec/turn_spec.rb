64de76069f2c53ba5e81cce5d5165b0c8ddb2dd4
Darren Kulback <dkulback@gmail.com>
2021-10-11 23:08:14 -0600

    Add tests to spec file

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,12 +1,28 @@
 require 'rspec'
 require './lib/turn'
 
-Rspec.describe Turn do
+RSpec.describe Turn do
   it 'exists' do
     turn = Turn.new("Juneau", card)
-    expect(turn).to be_instance_of(Turn)
-end
 
+    expect(turn).to be_instance_of(Turn)
   end
 
+it 'has a guess' do
+  turn = Turn.new("Juneau", card)
+
+  expect(turn.guess).to eq("Juneau")
+  end
+
+it 'checks if correct' do
+  turn = Turn.new("Juneau", card)
+
+  expect(turn.correct?).to eq(true)
+  end
+
+it 'gives feedback' do
+  turn = Turn.new("Juneau", card)
+
+  expect(turn.feedback).to eq(false)
+  end
 end
