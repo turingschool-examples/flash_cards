724f1e6081bda0e4a61f254eda9baf8e71a9ec21
John Napier <jcee.napier@gmail.com>
2021-10-11 18:49:03 -0600

    Feat: add deck class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,25 +1,25 @@
 require './lib/card'
 
 RSpec.describe Card do
-  xit 'exists' do
+  it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card).to be_instance_of(Card)
   end
 
-  xit 'has a question' do
+  it 'has a question' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.question).to eq("What is the capital of Alaska?")
   end
 
-  xit 'has an answer' do
+  it 'has an answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.answer).to eq("Juneau")
   end
 
-  xit 'has a category' do
+  it 'has a category' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.category).to eq(:Geography)
