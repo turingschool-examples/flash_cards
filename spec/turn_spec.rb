3cb8f6b986a15f14d33ef128a7190a564a8ee620
Kevin Gloss <kgloss89@gmail.com>
2021-10-12 08:49:37 -0600

    Test guess method

---
+++ spec/turn_spec.rb
@@ -0,0 +1,28 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'take a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to be true
+  end
+
+  # it 'give feedback' do
+  #   turn = Turn.new("Juneau", card)
+  #   expect(turn.feedback).to eq('Correct!')
+  # end
+  #
+  # it 'wrong answer feedback' do
+  #   turn = Turn.new("Anchorage", card)
+  #   expect(turn.feedack).to eq('Incorrect')
+  # end
+end
