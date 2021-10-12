d1f952f3ce6317262ab467c84cf29de572f8904a
craig cardwell <wizeduvcc@gmail.com>
2021-10-11 20:16:37 -0600

    feat: add method for correct? and feedback along with their tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,20 @@
 class Turn
+  attr_reader :guess,
+              :card
   def initialize(guess, card)
+    @guess = guess
+    @card  = card
+  end
+
+  def correct?
+    @guess == @card.answer
+  end
+
+  def feedback
+    if correct? == true
+      p "Correct"
+    else
+      p "Incorrect."
+    end
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,5 +6,26 @@ RSpec.describe Turn do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     expect(turn).to be_an_instance_of(Turn)
+    expect(turn.card).to be_instance_of(Card)
   end
+
+  it 'attributes' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.guess).to eq("Saturn")
+  end
+
+  it 'sets correct to false by default' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.correct?).to eq false
+  end
+
+  it 'sets correct to false by default' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+
+
 end
