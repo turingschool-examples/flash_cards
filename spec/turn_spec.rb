59d189f33d99541e6d01f9ddcbd0f3155bec9ae5
Tim Roeder <timothyjroeder@gmail.com>
2021-10-11 21:46:28 -0600

    Corrected require rspec and RSpec test group call

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,10 +1,10 @@
 # Requirements for testing the Turn class.
-require 'pry'
+require 'rspec'
 require './lib/turn'
 require './lib/card'
 
 # Rspec tests for Turn class.
-Rspec.describe Turn do
+RSpec.describe Turn do
   it 'exists' do
     card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
     turn = Turn.new('Juneau', card)
