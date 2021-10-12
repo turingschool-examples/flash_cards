06ea5ee0aa352cd34e9c4b1464fff6f206dbbf36
David Kassin <david.j.kassin@gmail.com>
2021-10-12 17:22:43 -0600

    Add pry and remove prints

--- lib/card.rb
+++ lib/card.rb
@@ -1,50 +1,24 @@
 
+
+
 class Card
   def initialize(question_param, answer_param, category_param)
     @question = question_param
     @answer = answer_param
-    @category = category_param 
+    @category = category_param
   end
 
   def question
-    p @question
+    @question
   end
 
   def answer
-    p @answer
+    @answer
   end
 
   def category
-    p @category
+    @category
   end
 end
 
-card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card.question
-card.answer
-card.category
-
-class Turn
-  def initialize(guess_param, card)
-    @guess = guess_param
-  end
-
-  def guess
-    p @guess
-  end
-
-  def correct?
-    guess_lower = @guess.lowercase
-    result = answer.include?(card.answer)
-    p result
-  end
-
-  def feedback
-    if result == true
-      p "Correct!"
-    else
-      p "Incorrect"
-  end
-end
-
-turn = Turn.new("Juneau")
+require 'pry'; binding.pry
