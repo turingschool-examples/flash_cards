7f7eede3a12a24cd05fef79a9230da8d4dfb26ca
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-11 19:41:34 -0600

    Update object variable names for consistency

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,22 +1,22 @@
 class Deck
-  attr_accessor :card_array
+  attr_accessor :obj_array
 
   def initialize(input) # Declare internal variable
-    @card_array = input
+    @obj_array = input
   end
 
 
   def cards # Return  internal array
-    self.card_array
+    self.obj_array
   end
 
   def count # Return array size
-    self.card_array.length()
+    self.obj_array.length()
   end
 
   def cards_in_category(x)
 
-    self.card_array.select do |i| # Iterate through our internal array - only select and return objects who's category == user key input
+    self.obj_array.select do |i| # Iterate through our internal array - only select and return objects who's category == user key input
 
       i.category == x
 
