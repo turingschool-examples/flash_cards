1cb878a29016e26add62cefcce25287c9d5cdf49
David Kassin <david.j.kassin@gmail.com>
2021-10-11 20:28:59 -0600

    Add turn class

--- lib/card.rb
+++ lib/card.rb
@@ -3,7 +3,7 @@ class Card
   def initialize(question_param, answer_param, category_param)
     @question = question_param
     @answer = answer_param
-    @category = category_param
+    @category = category_param 
   end
 
   def question
@@ -23,3 +23,28 @@ card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card.question
 card.answer
 card.category
+
+class Turn
+  def initialize(guess_param, card)
+    @guess = guess_param
+  end
+
+  def guess
+    p @guess
+  end
+
+  def correct?
+    guess_lower = @guess.lowercase
+    result = answer.include?(card.answer)
+    p result
+  end
+
+  def feedback
+    if result == true
+      p "Correct!"
+    else
+      p "Incorrect"
+  end
+end
+
+turn = Turn.new("Juneau")
