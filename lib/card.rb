cbbfc5560866ee058e0082bf6fe6be0c2fd39355
Seth Perna <gseth26@gmail.com>
2021-10-11 20:11:11 -0400

    initial commit

---
+++ lib/card.rb
@@ -0,0 +1,8 @@
+#class Card
+  #attr_reader :question, :answer, :category
+  #def initalize(question, answer, category)
+    #@question = question
+    #@answer = answer
+    #@category = category
+  #end
+#
