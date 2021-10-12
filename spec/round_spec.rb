d3481de2f8aeb7e752de48a84a1b3e0bb348a014
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-11 21:33:54 -0500

    Test: add 'turns' array to initialize

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe Round do
     expect(round.deck).to eq(deck)
   end
 
-  xit "starts with an empty list of turns" do
+  it "starts with an empty list of turns" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)    #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)    #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)    #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
