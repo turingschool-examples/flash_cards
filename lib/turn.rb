e9396d869e2ed2f60ba16c9a3c95c785869875b2
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-12 07:46:15 -0400

    Create class with methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,36 @@
+
 class Turn
+  attr_accessor :string, :card, :correct
+
+def initialize(guess, card)
+  @guess = guess
+  @card = card
+  correct = correct
+
+end
+
+def guess
+  @guess
+end
+
+def card
+  @card
+end
+
+def correct?
+  if @guess == card.answer
+    @correct = true
+  else
+    @correct = false
+end
+end
+
+def feedback
+  if correct == true
+    puts "Correct!"
+  else
+    puts "Incorrect!"
+end
+end
 
 end
