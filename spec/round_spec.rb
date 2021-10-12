ae0b40cc9f3dc3b919fdd6d5916ff6a39cf558ee
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-11 21:59:37 -0500

    Test: add test to 'take another turn'

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -50,14 +50,13 @@ RSpec.describe Round do
     expect(round.current_card).to eq(card_1)
   end
 
-  xit "can take another turn" do
+  it "can take another turn" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)    #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)    #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)    #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
-    #=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
 
     expect(new_turn.class).to eq(Turn)
     expect(new_turn.correct?).to eq(true)
