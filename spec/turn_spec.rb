220cc7d2207ca766a4066927e80dbc574b820fad
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-13 08:46:05 -0600

    create spec files for turn and deck classes. start turn_spec

---
+++ spec/turn_spec.rb
@@ -0,0 +1,27 @@
+# gather required files
+require './lib/turn'
+require './lib/card'
+
+# test the Turn class
+describe Turn do
+  # Check initialize class
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  # check get guess methods
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  # check get card method
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.card).to eq(card)
+  end
+end
