9b3c7c116f9b0c2ccd8f39d4b06e49579212c6bc
Tim Roeder <timothyjroeder@gmail.com>
2021-10-12 22:58:57 -0600

    Renamed class setup variables to match naming of Deck class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -7,20 +7,20 @@ require './lib/card'
 RSpec.describe Turn do
   # Setup two card and turn objects to test different objects for all tests.
   before(:each) do
-    @card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    @turn1 = Turn.new('Juneau', @card1)
-    @card2 = Card.new('What is the largest city in Alaska?', 'Anchorage', :Geography)
-    @turn2 = Turn.new('Anchorage', @card2)
-    @wrong_turn1 = Turn.new('Anchorage', @card1)
-    @wrong_turn2 = Turn.new('Juneau', @card2)
+    @card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    @turn_1 = Turn.new('Juneau', @card_1)
+    @card_2 = Card.new('What is the largest city in Alaska?', 'Anchorage', :Geography)
+    @turn_2 = Turn.new('Anchorage', @card_2)
+    @wrong_turn_1 = Turn.new('Anchorage', @card_1)
+    @wrong_turn_2 = Turn.new('Juneau', @card_2)
   end
 
   # Turn class test group.
   describe 'class' do
     it 'exists' do      
       # Test whether the Turn class exists.
-      expect(@turn1).to be_instance_of(Turn)
-      expect(@turn2).to be_instance_of(Turn)
+      expect(@turn_1).to be_instance_of(Turn)
+      expect(@turn_2).to be_instance_of(Turn)
     end
   end
 
@@ -28,14 +28,14 @@ RSpec.describe Turn do
   describe '#initialize' do
     it 'has a guess' do
       # Test the Turn class guess.
-      expect(@turn1.guess).to eq('Juneau')
-      expect(@turn2.guess).to eq('Anchorage')
+      expect(@turn_1.guess).to eq('Juneau')
+      expect(@turn_2.guess).to eq('Anchorage')
     end
     
     it 'has a card object' do
       # Test the Turn class attribute card object.
-      expect(@turn1.card).to be(@card1)
-      expect(@turn2.card).to be(@card2)
+      expect(@turn_1.card).to be(@card_1)
+      expect(@turn_2.card).to be(@card_2)
     end
   end
   
@@ -43,14 +43,14 @@ RSpec.describe Turn do
   describe '#correct?' do  
     it 'can check if guess is correct' do
       # Test correct? method for a correct guess.
-      expect(@turn1.correct?).to eq(true)
-      expect(@turn2.correct?).to eq(true)
+      expect(@turn_1.correct?).to eq(true)
+      expect(@turn_2.correct?).to eq(true)
     end
     
     it 'can check if guess is incorrect' do
       # Test correct? method for a correct guess.
-      expect(@wrong_turn1.correct?).to eq(false)
-      expect(@wrong_turn2.correct?).to eq(false)
+      expect(@wrong_turn_1.correct?).to eq(false)
+      expect(@wrong_turn_2.correct?).to eq(false)
     end
   end 
   
@@ -58,14 +58,14 @@ RSpec.describe Turn do
   describe '#feedback' do
     it 'can return correct feedback to the user' do
       # Test feedback method for a right guess.
-      expect(@turn1.feedback).to eq('Correct!')
-      expect(@turn2.feedback).to eq('Correct!')
+      expect(@turn_1.feedback).to eq('Correct!')
+      expect(@turn_2.feedback).to eq('Correct!')
     end
     
     it 'can return incorrect feedback to the user' do
       # Test feedback method for a wrong guess.
-      expect(@wrong_turn1.feedback).to eq('Incorrect.')
-      expect(@wrong_turn2.feedback).to eq('Incorrect.')
+      expect(@wrong_turn_1.feedback).to eq('Incorrect.')
+      expect(@wrong_turn_2.feedback).to eq('Incorrect.')
     end
   end
 end
