f6eb8cd7267e680494abb3831e962aa81c61e281
Hannah Warren <hannahkwarren@gmail.com>
2021-10-12 21:19:02 -0400

    Add Iteration 1 card and turn classes with specs

---
+++ spec/turn_spec.rb
@@ -0,0 +1,40 @@
+require './lib/turn'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
+    turn = Turn.new("Sacramento", card)
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'guess uses Turn string' do
+    card = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+    turn = Turn.new("Denver", card)
+    expect(turn.guess).to eq("Denver")
+  end
+
+  it 'correct?' do
+    card = Card.new("What is the capital of Connecticut?", "Stamford", :Geography)
+    turn = Turn.new("Stamford", card)
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'feedback when guess is correct' do
+    card = Card.new("What is the capital of Delaware?", "Dover", :Geography)
+    turn = Turn.new("Dover", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+  it 'feedback when guess is incorrect' do
+    card = Card.new("What is the capital of Florida?", "Tallahassee", :Geography)
+    turn = Turn.new("Fort Lauderdale", card)
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+  # it 'has a question' do
+  #   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #
+  #   expect(card.question).to eq("What is the capital of Alaska?")
+  # end
+
+
+end
