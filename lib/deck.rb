d993d41b463929be904f9501db85b41cc5fa3a84
Tim Roeder <timothyjroeder@gmail.com>
2021-10-13 00:13:21 -0600

    Created #cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,16 +2,21 @@
 
 class Deck
   attr_accessor :cards
-
+  
+  # Initialize Deck class attributes.
   def initialize(cards)
     @cards = cards
   end
 
+  # count returns the size of the cards array parameter.
   def count
     @cards.size
   end
 
-  # def cards_in_category(sel_category)
-
-  # end
+  # cards_in_category returns an array of cards from selected category.
+  def cards_in_category(sel_category)
+    @cards.select do |card|
+      card.category == sel_category
+     end
+  end
 end
