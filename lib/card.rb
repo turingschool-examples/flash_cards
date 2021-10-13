ecf3f2a1cf0f75d532d71c52d1db1a0a9f7dee70
Dylan Harper <dylanharper4@icloud.com>
2021-10-12 21:59:47 -0700

    Add category method and fix ask_input method

--- lib/card.rb
+++ lib/card.rb
@@ -1,30 +1,34 @@
 class Card
 
-  attr_reader :Geography
+  attr_reader :question, :answer, :category
 
-  def initialize(question, answer)
+  def initialize(question, answer, category)
 
     @question = question
     @answer = answer
+    @category  = category
 
+  end
 
+  def category
+    @category
   end
 
   def show_question
     puts @question
   end
 
-  def ask_for_answer(answer)
-    gets
-      if user_input == @answer || user_input == answer.downcase
-        puts "Correct. The answer is Juneau."
+  def ask_input(answer)
+    answer = gets.chomp
+      if @answer == answer or @answer.downcase == answer
+        puts "Correct. The answer is #{@answer}."
       else
-        puts "Incorrect. The answer is Juneau."
+        puts "Incorrect. The answer is #{@answer}."
       end
   end
 
 end
 
-card = Card.new("What is the capital of Alaska?", "Juneau")
+card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card.show_question
-card.ask_for_answer
+card.ask_input("")
