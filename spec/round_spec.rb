de4a7e60db8c5078754f7293411ff2cde6bf0a1b
Tim Roeder <timothyjroeder@gmail.com>
2021-10-13 00:44:51 -0600

    Created #initialize test groups

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -15,4 +15,25 @@ RSpec.describe Round do
     @deck = Deck.new(@cards)
     @round = Round.new(@deck)
   end
+
+  # Class instance test group.
+  describe 'class' do
+    # Test existence of a Round class instance.
+    it 'exists' do
+      expect(@round).to be_instance_of(Round)
+    end
+  end
+
+  # Round#initialize test group.
+  describe '#initialize' do
+    # Test for a Deck class object parameter.
+    it 'has a deck object' do
+      expect(@round.deck).to be_instance_of(Deck)
+    end
+  
+    # Test for an initially empty turns array.
+    it 'has a empty turns array' do
+      expect(@round.turns).to eq([])
+    end
+  end
 end
