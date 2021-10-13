6ad4c66ec010e384c1964de11f0a4c916d4c406e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-12 19:38:06 -0500

    Test: add test for number correct by category

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -91,19 +91,19 @@ RSpec.describe Round do
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
     new_turn = round.take_turn("Venus")
-      #=> #<Turn:0x00007f972a215b38...>
     expect(round.turns.count).to eq(2)
     expect(round.turns.last.feedback).to eq("Incorrect.")
     expect(round.number_correct).to eq(1)
   end
 
-  xit "can give number correct by category" do
+  it "can give number correct by category" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)    #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)    #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)    #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
+    new_turn = round.take_turn("Venus")
 
     expect(round.number_correct_by_category(:Geography)).to eq(1)
     expect(round.number_correct_by_category(:STEM)).to eq(0)
