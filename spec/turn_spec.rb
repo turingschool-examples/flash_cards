9d84ee40c54e931f948d8e36c438cb540127303b
Mallory Vining <mallorygallery12@gmail.com>
2021-10-12 19:47:05 -0600

    Add attributes

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -9,7 +9,13 @@ RSpec.describe Turn do
     expect(turn).to be_an_instance_of(Turn)
   end
 
-  xit 'attributes' do
-    expect(turn.card).to eq()
+  it 'has attributes' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
+    expect(turn.card).to eq(card)
+  end
+
+  xit 'guess' do
   end
 end
