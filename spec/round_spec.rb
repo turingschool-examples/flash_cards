be63088ac0b28c1a9323b1ee649ea36f42bfad22
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-12 19:43:34 -0500

    Test: add test for finding percent turns correct

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -109,13 +109,14 @@ RSpec.describe Round do
     expect(round.number_correct_by_category(:STEM)).to eq(0)
   end
 
-  xit "can give percent correct" do
+  it "can give percent correct" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)    #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)    #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)    #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
+    new_turn = round.take_turn("Venus")
 
     expect(round.percent_correct).to eq(50.0)
   end
@@ -127,6 +128,7 @@ RSpec.describe Round do
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
+    new_turn = round.take_turn("Venus")
 
     expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
     expect(round.current_card).to eq(card_3)
