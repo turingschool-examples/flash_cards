12aa5568d3a903c5d54766c6d23133d9462c8db2
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-13 09:59:23 -0600

    Create cards_in_class method

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,9 +4,29 @@ class Deck
   attr_reader :cards
 
   # initialize class
+  def initialize(cards)
+    @cards = cards
+  end
 
   # create count method
+  def count
+    self.cards.count
+  end
 
   # create cards_in_category method
+  def cards_in_category(category)
+    # create aggregator array
+    cards_in_category = []
+    # loop though all cards and check their category.
+    self.cards.each do |card|
+      # if correct category, add to temp cards_in_category array
+      if card.category == category
+        cards_in_category << card
+      end
+    # return array that only has those cards that meet the category requirement
+    cards_in_category
+    end
+
+  end
 
 end
