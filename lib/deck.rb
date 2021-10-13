81e98405f572bdd1cb732450f316b81324adbe85
Seth Perna <gseth26@gmail.com>
2021-10-13 08:15:30 -0400

    deck class and test

---
+++ lib/deck.rb
@@ -0,0 +1,21 @@
+class Deck
+  attr_accessor :cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    cards.length
+  end
+
+
+  def cards_in_category(category)
+    card_categories = []
+    cards.each do |card|
+      if card.category == category
+        card_categories << card
+      end
+    end
+    return card_categories
+  end
+end
