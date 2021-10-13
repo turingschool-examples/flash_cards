e653cb40d2b753275fa74fa5f256a2be4403c3f9
Mallory Vining <mallorygallery12@gmail.com>
2021-10-12 20:26:27 -0600

    Add correct? feedback boolean

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,4 +5,16 @@ class Turn
     @guess = guess
     @card = card
   end
+
+  def correct?
+    card.answer == guess
+  end
+
+  def feedback
+    if correct?
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -16,6 +16,27 @@ RSpec.describe Turn do
     expect(turn.card).to eq(card)
   end
 
-  xit 'guess' do
+  it 'correct?' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+  it 'handles incorrect feedback' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.card).to eq(card)
+    expect(turn.guess).to eq('Saturn')
+    expect(turn.correct?).to eq(false)
+    expect(turn.feedback).to eq("Incorrect.")
   end
 end
