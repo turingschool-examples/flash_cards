33e5a88361ac24c94aa447023cce607c3b2553f0
Mallory Vining <mallorygallery12@gmail.com>
2021-10-12 19:32:38 -0600

    Add category attribute

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,10 @@
 class Card
   attr_reader :question,
-              :answer
+              :answer,
+              :category
   def initialize(question, answer, category)
     @question = question
     @answer = answer
+    @category = category
   end
 end
