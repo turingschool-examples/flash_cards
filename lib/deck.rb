6b2f6250587a4cf935d4f2670e19fd98e8659a4b
Dani Coleman <danicoleman00@gmail.com>
2021-10-12 20:01:32 -0600

    feat: create deck class with attributes and methods

---
+++ lib/deck.rb
@@ -0,0 +1,17 @@
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @cards.size
+  end
+
+  def cards_in_category(category)
+    cards.find_all do |card|
+      category == card.category 
+    end
+  end
+end
