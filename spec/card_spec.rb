04e9152cf1574cb23e6c3d2d8194b6685b335beb
Dione <dionew1@gmail.com>
2021-10-13 12:35:34 -0600

    Revert "3 iteration"

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,23 +1,27 @@
 require './lib/card'
 
 RSpec.describe Card do
-  before(:each) do
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  end
-
   it 'exists' do
-    expect(@card).to be_instance_of(Card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card).to be_instance_of(Card)
   end
 
   it 'has a question' do
-    expect(@card.question).to eq("What is the capital of Alaska?")
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card.question).to eq("What is the capital of Alaska?")
   end
 
   it 'has an answer' do
-    expect(@card.answer).to eq("Juneau")
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card.answer).to eq("Juneau")
   end
 
   it 'has a category' do
-    expect(@card.category).to eq(:Geography)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card.category).to eq(:Geography)
   end
 end
