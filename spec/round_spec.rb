29f97f3202a6fe04ed84d88e546e48df5a2692cb
Kevin Gloss <kgloss89@gmail.com>
2021-10-13 22:02:09 -0600

    Add testing block outlines

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -5,25 +5,75 @@ require './lib/round'
 require './lib/deck'
 
 RSpec.describe Round do
+
+  before(:each) do
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+  end
+
   describe 'class checks' do
     it 'is an instance of round' do
-      deck = []
-      round = Round.new(deck)
-      expect(round).to be_a(Round)
+      expect(@round).to be_a(Round)
+    end
+
+    it 'is an instance of deck' do
+      expect(@round.deck).to be_a(Deck)
     end
 
     it 'is an instance of turn' do
-      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-      cards = [card_1, card_2, card_3]
-      deck = Deck.new(cards)
-      round = Round.new(deck)
-      new_turn = round.take_turn("Juneau")
-      # require 'pry'; binding.pry
-      expect(new_turn.class).to eq(Turn)
+      new_turn = @round.take_turn("Juneau")
+
+      expect(new_turn).to be_a(Turn)
     end
   end
 
-  # describe ''
+  describe 'default values' do
+    it 'turn is an array by default' do
+      expect(@round.turns).to eq([])
+    end
+  end
+
+  describe 'current card' do
+    it 'test current card is correct' do
+      # require 'pry'; binding.pry
+      expect(@round.current_card).to eq(@card_1)
+
+      @round.take_turn("Juneau")
+
+      expect(@round.current_card).to eq(@card_2)
+
+      @round.take_turn("Mars")
+
+    end
+  end
+  describe 'turn results' do
+    it 'test guess is correct/incorrect' do
+      new_turn = @round.take_turn("Juneau")
+
+      expect(new_turn.correct?).to be true
+      new_turn_2 = @round.take_turn("Venus")
+
+      expect(new_turn_2.correct?).to be false
+      # require 'pry'; binding.pry
+    end
+
+    it 'test number of correct guesses' do
+      @round.take_turn("Juneau")
+      # require 'pry'; binding.pry
+      @round.take_turn("Venus")
+      expect(@round.number_correct).to eq(1)
+    end
+
+    it 'test number correct by category' do
+      #add content
+    end
+
+    it ' ' do
+      #add content
+    end
+  end
 end
