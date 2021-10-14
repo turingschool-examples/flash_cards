9f55c0432a6fb8bbc1eb587c06f50aa5cad734e3
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-10-13 19:01:16 -0600

    Functionality for Number and %Correct methods, Complete the Round Library

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,49 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck,
+              :turns,
+              :number_correct
 
-  def initilize(deck)
-    @deck = deck
-    @turns = []
+  def initialize(deck)
+    @deck           = deck
+    @turns          = []
+    @correct_turns  = []
   end
 
   def current_card
     @deck.cards.first
+  end
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+
+    @turns << new_turn
+
+    if guess == current_card.answer
+      @correct_turns << new_turn
+    end
+
+    @deck.cards.rotate!
+
+    return new_turn
+  end
+
+  def number_correct
+    @correct_turns.length
+  end
+
+  def number_correct_by_category(category)
+    num = @correct_turns.select do |turn|
+              category == turn.card.category
+          end
+    num.length
+  end
+
+  def percent_correct
+    (@correct_turns.length.to_f / @turns.length.to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    ((number_correct_by_category(category)).to_f / @correct_turns.length.to_f) * 100
+  end
+
 end
