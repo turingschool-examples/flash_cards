ebfd2110bc1aa296e3d337a408743750ab912d07
Tim Roeder <timothyjroeder@gmail.com>
2021-10-13 20:10:40 -0600

    Created methods for take_turn and number_correct

--- lib/round.rb
+++ lib/round.rb
@@ -11,4 +11,16 @@ attr_accessor :deck, :turns
   def current_card
     @deck.cards[@turns.count]
   end
+
+  def take_turn(guess)
+    return_turn = Turn.new(guess, current_card)
+    @turns << return_turn
+    return_turn
+  end
+
+  def number_correct
+    @turns.count do |turn|
+      turn.correct?
+    end
+  end
 end
