be10b345f305b16f37fc794b932d4a3f13ddbd47
Kevin Gloss <kgloss89@gmail.com>
2021-10-13 22:01:39 -0600

    Add method outlines

--- lib/round.rb
+++ lib/round.rb
@@ -1,26 +1,47 @@
 require '/turn'
 
 class Round
-  attr_reader :deck, :turns, :round
+  attr_reader :deck, :turns#, :round
 
   #need attributes for: deck, turns, number correct,
   def initialize(deck)
     @deck = deck
     @number_correct = 0
     @turns = []
-    @round = 0
+    # @round = 0
   end
 
   #need methods for: take_turn, current_card, correct?, number_correct
   def current_card
-    @deck.cards[@round - 1]
+    if @turns.length == 0
+      @deck.cards[0]
+    else
+      @deck.cards[@turns.length]
+    end
   end
 
   def take_turn(response)
     #need to iterate through the cards/get the card to match current card
-    turn = Turn.new(response, @deck.cards[@round])
-    @round += 1
-    @turns << turn
-    turn
+    @turn = Turn.new(response, @deck.cards[@turns.length])
+    # @round += 1
+    @turns << @turn
+    @turn
+  end
+#need to add a loop to check for each turn being true ******
+  def number_correct
+    @turns.each do |trivia|
+      if trivia.correct?  == true
+        # require 'pry'; binding.pry
+        @number_correct += 1 #this counter is increasing each time number_correct is run
+      end
+    end
+    @number_correct
+  end
+
+  def number_correct_by_category(category)
+
+  end
+  def percent_correct
+    @number_correct/@turns.length
   end
 end
