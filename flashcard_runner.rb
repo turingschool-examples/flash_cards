d91922b13d818d42d075eb62046625c4cdb4cc1f
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-14 14:51:43 -0600

    Change how take_turn method advances cards. Do not put current_card at back of deck

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,9 +3,8 @@ card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and repo
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 card_4 = Card.new("What is the tallet mountain in the world?", "Mt. Everest", :Geography)
 deck = Deck.new([card_1, card_2, card_3, card_4])
-round = Round.new(deck)
 
-def start(deck)
-    puts "Welcome! You're playing with #{deck.cards.length} cards"
-    puts "-------------------------------------------------------"
-end
+game = Game.new(deck)
+game.start
+answer = gets.chomp
+game.check_answer
--- lib/round.rb
+++ lib/round.rb
@@ -19,10 +19,10 @@ class Round
     turn = Turn.new(guess, self.current_card)
     # store turn / guess
     self.turns << turn
+
     # move deck to next card
-    # make current card the next card in deck and put the previous current card to end of deck
-    # get cards array from current deck and add current card to end
-    cards = deck.cards.concat([self.current_card])
+    # get cards array from current deck
+    cards = deck.cards
     # remove first card
     cards.shift
     # set current card to the first card and return
