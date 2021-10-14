a2d8e34d3e0d75e79444425124c0d292b3a0dc4d
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-10-14 16:18:03 -0600

    Add Card class and it's rspec file

---
+++ lib/deck.rb
@@ -0,0 +1,22 @@
+require './lib/card'
+
+class Deck
+  attr_reader :cards, :count
+
+  def initialize(cards_array)
+    @cards = cards_array
+    @count = cards_array.count
+  end
+
+  def cards_in_category(category_input) # Returns the number of cards that use the category from the argument provided
+    category_specific_cards = []
+    self.cards.find_all do |card|
+      if card.category == category_input
+          category_specific_cards.push(card)
+      end
+    end
+    return category_specific_cards
+  end
+
+
+end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,38 @@
+require './lib/turn'
+require './lib/card'
+require './lib/deck'
+
+def create_test_deck
+  card_1 = Card.new("What is 1 + 1?", "2", :Math)
+  card_2 = Card.new("What is 50 + 20?", "70", :Math)
+  card_3 = Card.new("What is 9 - 9?", "0", :Math)
+  card_4 = Card.new("What is earth's gravity?", "9.8", :Science)
+  card_5 = Card.new("Who came up with the Theory of Special Relativity?", "Albert Einstein", :Science)
+  card_6 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  cards = [card_1, card_2, card_3, card_4, card_5, card_6]
+  return Deck.new(cards)
+end
+
+
+RSpec.describe Deck do
+
+  it 'exists' do
+    deck = create_test_deck
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  describe '#cards_in_category' do
+    it 'returns an array' do
+      deck = create_test_deck
+      expect(deck.cards_in_category(:Math)).to be_an_instance_of(Array)
+    end
+    it 'returns an array that contaions cards of the same category' do
+      deck = create_test_deck
+      expect(deck.cards_in_category(:Math).count).to eq(3)
+    end
+  end
+
+
+
+
+end
