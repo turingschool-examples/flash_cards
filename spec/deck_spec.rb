886741548d0aa0bb3da7bd88b6723a49dd2da492
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-14 16:09:19 -0400

    Add tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,31 @@
 require './lib/card'
 require './lib/deck'
+require './lib/turn'
+
+RSpec.describe Deck do
+  it 'has cards in the deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it 'counts cards in the deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    cards = [card_1, card_2]
+    deck = Deck.new(cards)
+    expect(deck.count).to eq(2)
+  end
+
+  it 'has cards in category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+  end
+end
