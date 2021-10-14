d71b24c65a3539917a109bde684fde58fade6c46
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-10-14 14:37:23 -0600

    Add tests to turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -11,20 +11,26 @@ RSpec.describe Turn do
   end
 
   describe '#correct?' do
-    it 'returns correct turn evaluation as a boolean' do
+    it 'can return true if answer is true' do
       card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       true_turn = Turn.new('Juneau', card)
       expect(true_turn.correct?).to eq(true)
+    end
+    it 'can return false if answer is false' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       false_turn = Turn.new('bla bla answer', card)
       expect(false_turn.correct?).to eq(false)
     end
   end
 
   describe '#feedback' do
-    it 'returns a correct feedback string' do
+    it 'can return true feedback string if answer is true' do
       card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       true_turn = Turn.new('Juneau', card)
       expect(true_turn.feedback).to eq('Correct!')
+    end
+    it 'can return false feedback string if answer is false' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       false_turn = Turn.new('bla bla answer', card)
       expect(false_turn.feedback).to eq('Incorrect.')
     end
