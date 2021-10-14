39365cb67be55f1ef290013103147fbb32d8d672
Eldridge Turambi <eturambi95@gmail.com>
2021-10-14 12:03:58 -0600

    Add runner file

---
+++ flashcard_runner.rb
@@ -0,0 +1,12 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+@card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+@card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+@card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+@card_4 = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+@card_5 = Card.new("What is the capital of Japan?", "Tokyo", :Geography)
+@card_6 = Card.new("What is the capital of Indonesia?", "Jakarta", :Geography)
+@card_7 = Card.new("What programming language do we learn in Mod 1 at Turing?", "Ruby", :STEM)
