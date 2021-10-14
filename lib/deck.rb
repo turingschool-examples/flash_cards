43451fd42b5fa9286da92d8f6aed6406412cc1c0
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-10-14 16:08:32 -0400

    Add variables and methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,18 @@
+require './lib/card'
+require './lib/turn'
+
+
 class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @cards.count
+  end
   
+  def cards_in_category(cat)
+    @cards.select { |card| card.category == cat }
+  end
 end
