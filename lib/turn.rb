dbfd6f30d4de89fe63d7571401564c6707567d05
Dylan Harper <dylanharper4@icloud.com>
2021-10-13 21:04:14 -0700

    Add edits to turn class

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+class Turn
+
+  def initialize(string, card)
+    @string = string
+    @card = card
+    @guess = guess
+  end
+
+  def guess(guess)
+    
+  #   guess = gets.chomp
+  #     if @guess == answer or @guess.downcase == answer
+  #       puts "Correct. The answer is #{@guess}."
+  #     else
+  #       puts "Incorrect. The answer is #{@guess}."
+  #     end
+  end
