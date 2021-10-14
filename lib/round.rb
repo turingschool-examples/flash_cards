db1f57e5c1887ae2ecde75ba9061b7d9f78d5b5f
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-14 13:36:59 -0600

    Update num_correct_by_category method

--- lib/round.rb
+++ lib/round.rb
@@ -1,16 +1,18 @@
 class Round
-  attr_reader :obj_deck, :obj_turns
+
+  attr_reader :obj_deck, :obj_turns, :total_correct
 
   def initialize(input_deck)
     @obj_deck = input_deck
     @obj_turns = []
+    @total_correct = []
   end
 
-  def deck
+  def deck # Return internal deck
     self.obj_deck
   end
 
-  def turns
+  def turns # Return internal turns array
     self.obj_turns
   end
 
@@ -21,19 +23,41 @@ class Round
 
   def take_turn(user_guess)
 
+    temp = Turn.new(user_guess, current_card) # Create a temporary object holder with our new 'turn' instance containing the current card and user guess
+
+
+    if temp.card.answer == user_guess # Compare our current turn's card's answer to our user guess
+      self.total_correct << self.obj_deck.cards[0] # Push copy of card if answer is correct (utilized for number by category and percent correct later)
+    end
+
+    self.obj_turns << self.obj_deck.cards.shift # Remove the current card from our 'deck' object's internal array, push it to our current 'round' object's turn array
+    return temp # Return our new 'turn' instance
+
   end
 
-  def number_correct
+  def number_correct_by_category(user_category)
 
+    temp = [] # Temporary array for counting total correct in a given category
+
+    temp << self.total_correct.select do |i| # Select all elements in total_correct[] who's category matches our user input
+
+      i.category == user_category
+
+    end
+
+    return temp.size # Return total size of our temp[] array for total number correct by respective category
   end
 
-  def number_correct_by_category
-
+  def number_correct # Return total number of questions that are correct
+    self.total_correct.size
   end
 
   def percent_correct
 
+    
+
   end
+
 
 
 
