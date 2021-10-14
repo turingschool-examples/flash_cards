a7ea430ec16e8b6fc7e25a855de752e808f55a53
Ruben Pasillas <hobbbes@protonmail.com>
2021-10-13 18:37:20 -0600

    Add deck class test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -27,4 +27,18 @@ RSpec.describe Card do
 
     expect(card.category).to eq(:Geography)
   end
+
+  describe Deck do
+    it 'exists' do
+      card = Card.new("Q1", "A1", :C1)
+      card_two = Card.new("Q2", "A2", :C2)
+      card_three = Card.new("Q3", "A3", :C3)
+
+      card_array = [card, card_two, card_three]
+
+      deck = Deck.new(card_array)
+
+      expect(deck).to be_instance_of(Deck)
+    end
+  end
 end
