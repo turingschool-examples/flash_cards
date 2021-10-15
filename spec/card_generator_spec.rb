8a3a21dce2c8d9c091b78c79c97d67665a49dd34
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-15 14:52:09 -0600

    Update card_generator test

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -10,4 +10,10 @@ describe CardGenerator do
   it 'returns cards' do
     expect(@cards[0]).to be_instance_of(Card)
   end
+
+  it 'returns the correct card' do
+    expect(@cards[0].question).to eq('What is 5 + 5?')
+    expect(@cards[0].answer).to eq('10')
+    expect(@cards[0].category).to eq('STEM')
+  end
 end
