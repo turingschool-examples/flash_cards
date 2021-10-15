2f2b28ad2da0d41705c8f0b580c673612a31227a
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-10-15 09:29:50 -0600

    simplify/shorten unnessary lines of code

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,8 +8,8 @@ class Deck
     @count = cards_array.count
   end
 
-  def cards_in_category(category_input) # Returns the number of cards that use the category from the argument provided
-    category_specific_cards = self.cards.find_all do |card|
+  def cards_in_category(category_input) # Returns an array of cards that use the category from the argument provided
+    self.cards.find_all do |card|
       if card.category == category_input
           card
       end
--- lib/turn.rb
+++ lib/turn.rb
@@ -9,11 +9,7 @@ class Turn
   end
 
   def correct?
-    if @guess == @card.answer
-      return true
-    else
-      return false
-    end
+    @guess == @card.answer
   end
 
   def feedback
