39afffa0fb14e9bdf6bd0e0164883bdc5583f902
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-14 18:40:20 -0600

    Refactor turn_spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,48 +4,46 @@ require './lib/card'
 
 # test the Turn class
 describe Turn do
+  # prep all tests
+  before(:each) do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
   # Check initialize class
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn).to be_instance_of(Turn)
+    expect(@turn).to be_instance_of(Turn)
   end
 
   # check get guess methods
   it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.guess).to eq("Juneau")
+    expect(@turn.guess).to eq("Juneau")
   end
 
   # check get card method
   it 'has a card' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.card).to eq(card)
+    expect(@turn.card).to eq(@card)
   end
 
   # test correct? method
-  it 'correct? returns true' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.correct?).to eq(true)
-  end
-  it 'correct? returns false' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Harrisburgh", card)
-    expect(turn.correct?).to eq(false)
+  describe ' #correct?' do
+    it 'correct? returns true' do
+      expect(@turn.correct?).to eq(true)
+    end
+    it 'correct? returns false' do
+      turn = Turn.new("Harrisburgh", @card)
+      expect(turn.correct?).to eq(false)
+    end
   end
 
   # test feedback method
-  it 'feedback returns "Correct!"' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.feedback).to eq("Correct!")
-  end
-  it 'feedback returns "Incorrect."' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Harrisburgh", card)
-    expect(turn.feedback).to eq("Incorrect.")
+  describe ' #feedback' do
+    it 'feedback returns "Correct!"' do
+      expect(@turn.feedback).to eq("Correct!")
+    end
+    it 'feedback returns "Incorrect."' do
+      turn = Turn.new("Harrisburgh", @card)
+      expect(turn.feedback).to eq("Incorrect.")
+    end
   end
 end
