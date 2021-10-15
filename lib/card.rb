b059e382f27591d13a264949087a20d6a11afea3
Dylan Harper <dylanharper4@icloud.com>
2021-10-14 17:02:22 -0700

    Remove unnecessary notes

--- lib/card.rb
+++ lib/card.rb
@@ -10,20 +10,4 @@ class Card
 
   end
 
-
-
-  # def ask_input(answer) # do I need an argument for this method?
-  #   answer = gets.chomp # should I use the instance variable when calling answer?
-  #     if @answer == answer or @answer.downcase == answer
-  #       puts "Correct. The answer is #{@answer}."
-  #     else
-  #       puts "Incorrect. The answer is #{@answer}."
-  #     end
-  # end
-
 end
-
-
-#card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#card.show_question
-#card.ask_input("")
