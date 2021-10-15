b741eea2d7aa73fecff034bad4d92355cf6632ed
Sierra Tucker <sierra.tucker121@gmail.com>
2021-10-14 19:38:25 -0600

    Initialize Round

--- lib/round.rb
+++ lib/round.rb
@@ -1,18 +1,6 @@
 class Round
-  attr_accessor :deck, :turns, :cards, :guess, :annswer
+  attr_accessor :deck
   def initialize(deck)
     @deck = deck
-    @turns = []
   end
-
-def current_card
-  deck.cards[0]
-end
-
-def take_turn(guess)
-  this_turn = Turn.new(guess, current_card)
-  @turns << this_turn
-end
-
-def number_correct
-  
+end 
