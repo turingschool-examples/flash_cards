0964e3728f41074b246f2a8483141acaa7164c9e
Sierra Tucker <sierra.tucker121@gmail.com>
2021-10-14 19:09:16 -0600

    Update turn files

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,20 @@
 class Turn
-  def initialize(string, card)
-    @guess = string
+  attr_accessor :guess, :card
+  def initialize(guess, card)
+    @guess = guess
     @card = card
   end
+
+  def correct?
+    @guess == card.answer
+  end
+
+  def feedback
+    if correct? #== true
+      "Correct!"
+    else
+      "Incorrect!"
+    end
+  end
 end
+# require 'pry'; binding.pry;
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,17 +3,41 @@ require './lib/card'
 
 RSpec.describe Turn do
   it 'exists' do
-    turn = Turn.new("Juneau", Card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
-    turn = Turn.new("Juneau", Card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
     expect(turn.guess).to eq("Juneau")
   end
 
   it 'has a flashcard' do
-    turn = Turn.new("Juneau", Card)
-    expect(turn.card).to eq(Card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.card).to eq(card)
+  end
+
+  it 'is correct if guess matches answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to be true
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'is either correct or incorrect' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+  it 'can take other guesses' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.guess).to eq("Saturn")
+    expect(turn.correct?).to eq(false)
+    expect(turn.feedback).to eq("Incorrect!")
   end
 end
