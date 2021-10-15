5c14729469efe0a6ade195e60028061196fef2fd
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-15 14:55:21 -0600

    Refactor card_generator and card_generator_spec

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -4,11 +4,14 @@ class CardGenerator
   attr_reader :filename
   attr_accessor :cards
 
+  # initialize card generator with basic variables
   def initialize(filename)
     @filename = filename
     @cards = []
   end
 
+  # open the file and generate cards from the text values
+  # return the final array of cards
   def cards
     File.open(@filename).each do |line|
       question = line.split(',')[0]
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -2,15 +2,23 @@ require './lib/card'
 require './lib/card_generator.rb'
 
 describe CardGenerator do
+  # setup all tests
   before(:each) do
     filename = 'cards.txt'
     @cards = CardGenerator.new(filename).cards
   end
 
+  # check that a proper card object is generated
   it 'returns cards' do
     expect(@cards[0]).to be_instance_of(Card)
   end
 
+  # check that all cards are gathered from the file
+  it 'returns the correct number of cards' do
+    expect(@cards.length).to eq(4)
+  end
+
+  # check that the cards have the exact expected values
   it 'returns the correct card' do
     expect(@cards[0].question).to eq('What is 5 + 5?')
     expect(@cards[0].answer).to eq('10')
