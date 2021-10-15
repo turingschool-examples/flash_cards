849e313f3c8bcd9bd89cfee5607ac20ec6cf7e5e
Jacob Jenner <jennerjacob@gmail.com>
2021-10-15 10:50:20 -0600

    Update deck.rb and deck_spec.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,21 @@
 class Deck
-  attr_reader :card
+  attr_reader :cards
   def initialize(cards)
-  require "pry"; binding.pry
+    @cards = cards
+    # require "pry"; binding.pry
   end
+
+  def count
+    @cards.length
+  end
+
+  def cards_in_category(category)
+    number_of_cards = []
+    @cards.select do |card|
+      if card.category == category
+        number_of_cards << card
+      end
+    end
+  end
+
 end
