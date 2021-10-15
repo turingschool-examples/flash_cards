a806a7edf7cd7b21679098f9e42397ad1266a306
Kevin Gloss <kgloss89@gmail.com>
2021-10-15 12:21:36 -0600

    Remove comments

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,5 +1,3 @@
-require './lib/card'
-
 class CardGenerator
   attr_reader :file_name, :new_cards
 
@@ -12,14 +10,8 @@ class CardGenerator
     File.readlines(@file_name, chomp: true).each do |line|
       card = line.split(', ')
       card = Card.new(card[0], card[1], card[2])
-      @new_cards << card #change to new_card
+      @new_cards << card
     end
     @new_cards
   end
 end
-
-file_name = "cards.txt"
-
-cards = CardGenerator.new(file_name).cards
-
-require 'pry'; binding.pry
