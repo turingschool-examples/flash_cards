0b16518dc722bd115d2b15994d99813d5dc600e7
Brad Breiten <jonathan@breiten.us>
2021-10-15 11:07:01 -0600

    Comeplete deck testing

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,39 +2,81 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
-  it 'exists' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  describe '#initialize' do
+    it 'exists' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
+      cards = [card_1, card_2, card_3]
 
-    deck = Deck.new(cards)
+      deck = Deck.new(cards)
 
-    expect(deck).to be_instance_of(Deck)
+      expect(deck).to be_instance_of(Deck)
+    end
+
+    describe '#cards' do
+      it 'creates array of cards' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.cards).to eq(cards)
+      end
+    end
+
+    describe '#count' do
+      it 'counts length of cards array' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.count).to eq(cards.length)
+      end
+    end
+
+    describe '#cards_in_category' do
+      it 'returns cards with given category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+      end
+
+      it 'returns nothing if search criteria matches nothing' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.cards_in_category('Pop Culture')).to eq []
+      end
+    end
   end
-
 end
-
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-cards = [card_1, card_2, card_3]
-
-deck = Deck.new(cards)
-
-require "pry"; binding.pry
-
-
-
-
-# RSpec.describe Deck do
-#
-#
-#
-# end
