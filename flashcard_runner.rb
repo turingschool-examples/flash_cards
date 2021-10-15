ccc1775be0cdc4b5f6d9c0ec4188503318827ac7
Kevin Gloss <kgloss89@gmail.com>
2021-10-15 12:22:50 -0600

    Create flashcard runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,20 +2,25 @@ require './lib/card'
 require './lib/turn'
 require './lib/round'
 require './lib/deck'
+require './lib/card_generator'
 
-card_1 = Card.new("What is 100 - 47?", "53", :Math)
-card_2 = Card.new("How do you say hello in Spanish?", "Hola", :Spanish)
-card_3 = Card.new("Who is the leader of the Greek Pantheon?", "Zeus", "Mythology")
-card_4 = Card.new("Who is the hero who fought the Medusa?", "Perseus", "Mythology")
-card_5 = Card.new("What is the remainder of 100 divided by 3?", "1", :Math)
+# card_1 = Card.new("What is 100 - 47?", "53", :Math)
+# card_2 = Card.new("How do you say hello in Spanish?", "Hola", :Spanish)
+# card_3 = Card.new("Who is the leader of the Greek Pantheon?", "Zeus", "Mythology")
+# card_4 = Card.new("Who is the hero who fought the Medusa?", "Perseus", "Mythology")
+# card_5 = Card.new("What is the remainder of 100 divided by 3?", "1", :Math)
+#
+# cards = [card_1, card_2, card_3, card_4, card_5]
+# require 'pry'; binding.pry
+file_name = "cards.txt"
+# cards = CardGenerator.new("cards.txt").cards
+cards = CardGenerator.new(file_name).cards
 
-cards = [card_1, card_2, card_3, card_4, card_5]
 deck = Deck.new(cards)
 round = Round.new(deck)
 
 puts "Welcome! You're playing with #{deck.count} cards."
-37.times{print "-"}
-print "\n"
+puts "-" * 37
 
 def start(round)
     round.deck.cards.each do |i|
