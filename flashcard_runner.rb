66929374cff882f6ee6ec76dd0a73630a053817a
Kevin Gloss <kgloss89@gmail.com>
2021-10-15 14:46:06 -0600

    Update to read test file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,16 +4,7 @@ require './lib/round'
 require './lib/deck'
 require './lib/card_generator'
 
-# card_1 = Card.new("What is 100 - 47?", "53", :Math)
-# card_2 = Card.new("How do you say hello in Spanish?", "Hola", :Spanish)
-# card_3 = Card.new("Who is the leader of the Greek Pantheon?", "Zeus", "Mythology")
-# card_4 = Card.new("Who is the hero who fought the Medusa?", "Perseus", "Mythology")
-# card_5 = Card.new("What is the remainder of 100 divided by 3?", "1", :Math)
-#
-# cards = [card_1, card_2, card_3, card_4, card_5]
-# require 'pry'; binding.pry
 file_name = "cards.txt"
-# cards = CardGenerator.new("cards.txt").cards
 cards = CardGenerator.new(file_name).cards
 
 deck = Deck.new(cards)
@@ -23,24 +14,24 @@ puts "Welcome! You're playing with #{deck.count} cards."
 puts "-" * 37
 
 def start(round)
-    round.deck.cards.each do |i|
-      puts "This is card number #{round.turn_num} of #{round.deck.count}"
-      puts "Question: #{i.question}"
-      answer = gets.chomp
-      my_turn = round.take_turn(answer)
-      puts my_turn.feedback
-    end
+  round.deck.cards.each do |i|
+    puts "This is card number #{round.turn_num} of #{round.deck.count}"
+    puts "Question: #{i.question}"
+    answer = gets.chomp
+    my_turn = round.take_turn(answer)
+    puts my_turn.feedback
+  end
 
-    puts "****** Game Over! ******"
-    puts "You had #{round.number_correct} out of #{round.deck.count} for a total score of #{round.percent_correct.to_i}%"
+  puts "****** Game Over! ******"
+  puts "You had #{round.number_correct} out of #{round.deck.count} for a total score of #{round.percent_correct.to_i}%"
 
-    included_categories = []
-    round.deck.cards.each do |i|
-      if included_categories.include?(i.category) == false
-        puts "#{i.category} - #{round.percent_correct_by_category(i.category).to_i}% correct"
-        included_categories << i.category
-      end
+  included_categories = []
+  round.deck.cards.each do |i|
+    if included_categories.include?(i.category) == false
+      puts "#{i.category} - #{round.percent_correct_by_category(i.category).to_i}% correct"
+      included_categories << i.category
     end
+  end
 end
 
 start(round)
