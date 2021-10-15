4fec80c2d4eba348910e6b437bf26f5dddf75fe0
Leland Curtis <lelandcurtis88@gmail.com>
2021-10-14 18:46:49 -0600

    Refactor card_spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,27 +1,28 @@
 require './lib/card'
 
 RSpec.describe Card do
+  # setup all tests
+  before(:each) do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  end
+
+  #test initialization
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card).to be_instance_of(Card)
+    expect(@card).to be_instance_of(Card)
   end
 
+  # check question
   it 'has a question' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.question).to eq("What is the capital of Alaska?")
+    expect(@card.question).to eq("What is the capital of Alaska?")
   end
 
+  # check for answer
   it 'has an answer' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.answer).to eq("Juneau")
+    expect(@card.answer).to eq("Juneau")
   end
 
+  # check for category
   it 'has a category' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.category).to eq(:Geography)
+    expect(@card.category).to eq(:Geography)
   end
 end
