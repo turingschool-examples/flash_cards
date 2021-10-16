981e54fff68cec9c742922759c3a4a63ed11c316
Croix Kimmel <croixkimmel@gmail.com>
2021-10-16 15:17:43 -0600

    Refactor deck_spec.rb with before(:each)

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,39 +3,38 @@ require './lib/deck'
 
 #what else do I need to test?
 RSpec.describe Deck do
-  it 'exists' do
 
+  before(:each) do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
        reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5°
       clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
+    @deck = Deck.new(cards)
+  end
 
-    expect(deck). to be_instance_of(Deck)
+  it 'exists' do
+
+    expect(@deck). to be_instance_of(Deck)
   end
 
 #come back to this
   it '#count' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
-      reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5°
-      clockwise from due north?", "North north west", :STEM)
-    deck = Deck.new(cards)
-    expect(deck.count). to eq(3)
+
+    expect(@deck.count). to eq(3)
   end
 
-  # it '#cards_in_category' do
-  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
-  #      reports about the surface of which planet?", "Mars", :STEM)
-  #   card_3 = Card.new("Describe in words the exact direction that is 697.5°
-  #     clockwise from due north?", "North north west", :STEM)
-  #   deck = Deck.new(cards)
-  #   expect deck.cards_in_category(:Geography). to eq[#<Card:0x00007fa16104e160...>]
-  # end
+  #come back to this
+    it 'cards_in_category - Geography' do
 
+      expect(@deck.cards_in_category(:Geography).length). to eq(1)
+    end
+
+    #come back to this
+      it 'cards_in_category - STEM' do
+
+        expect(@deck.cards_in_category(:STEM).length). to eq(2)
+      end
 
 end
