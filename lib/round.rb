c159e6e92046726fb78318501dd163d21c60e074
Dylan Harper <dylanharper4@icloud.com>
2021-10-16 12:19:55 -0700

    Make further progress on Iteration 2

--- lib/round.rb
+++ lib/round.rb
@@ -2,9 +2,47 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 
+
+
 class Round
+
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
+    @turns = []
   end
+
+  def current_card
+    if @turns == []
+      @turns << deck.cards[0]
+      return deck.cards[0]
+    elsif @turns == [deck.cards[0]]
+      @turns << deck.cards[1]
+      return deck.cards[1]
+    elsif @turns == [deck.cards[0], deck.cards[1]]
+      @turns << deck.cards[2]
+      return deck.cards[2]
+    else
+    end
+  end
+
+
+  def take_turn(guess)
+    if @turns == []
+      turn = Turn.new(guess, deck.cards[0])
+    elsif @turns == [0]
+      turn = Turn.new(guess, deck.cards[1])
+    elsif @turns == [0, 1]
+      turn = Turn.new(guess, deck.cards[2])
+    elsif @turns == [0, 1, 2]
+    end
+  end
+  #   #create new turn object
+  #   #store turn object
+  #   #add to @turns counter
+  #   #return turn object
+  #   turn.correct?
+  #   @turns << turn
+  #   #return turn?
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -29,8 +29,10 @@ RSpec.describe Round do
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+    round = Round.new(deck)
 
     expect(deck).to be_instance_of(Deck)
+    expect(round.deck).to eq(deck)
   end
 
   it "is a round" do
@@ -42,5 +44,40 @@ RSpec.describe Round do
     round = Round.new(deck)
 
     expect(round).to be_instance_of(Round)
+  end
+
+  it "@turns is an array" do #change wording if need be
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.turns).to eq([])
+  end
+
+  it "#current_card method returns card instance" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.current_card).to eq(card_1)
+  end
+
+  it "#take_turn method; 'Juneau'" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    guess = "Juneau"
+    turn = Turn.new(guess, card_1)
+    round = Round.new(deck)
+
+    expect(round.take_turn(guess)).to eq(turn)
   end
 end
