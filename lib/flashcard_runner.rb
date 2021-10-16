43b9f06d0a023c7afcefeada0301d97f1282c2e8
John Napier <81737385+JCNapier@users.noreply.github.com>
2021-10-15 18:11:02 -0600

    Update flashcard_runner.rb

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,11 +2,11 @@ require "./lib/card"
 require "./lib/turn"
 require "./lib/deck"
 require "./lib/round"
+require "./lib/card_generator"
 
-@card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-@card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-@card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-@deck   = Deck.new([@card_1, @card_2, @card_3])
+filename = "cards.txt"
+cards = CardGenerator.new(filename).cards
+@deck   = Deck.new(cards)
 @round  = Round.new(@deck)
 
 def start
@@ -22,9 +22,18 @@ while @round.turns.length < @deck.cards.length do
 end
 
   puts "****** Game over! ******"
-  puts "You had #{@round.number_correct} correct guesses out of #{@round.deck.cards.length} for a total score of #{@round.percent_correct}%."
-  puts "Geography - #{@round.percent_correct_by_category(:Geography)}% correct"
-  puts "STEM - #{@round.percent_correct_by_category(:STEM)}% correct"
+  puts "You had #{@round.number_correct} correct guesses out of #{@round.deck.cards.length} for a total score of #{@round.percent_correct.to_i}%."
+  category
+end
+
+def category
+  categories = []
+  @deck.cards.each do |card|
+    categories << card.category
+  end
+    categories.uniq.each do |category|
+    puts "#{category} - #{@round.percent_correct_by_category(category)}% correct"
+  end
 end
 
 start
