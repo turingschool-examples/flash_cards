0adbc0b87e55d36e1491f09e73d7cef34df8d0da
Sierra Tucker <sierra.tucker121@gmail.com>
2021-10-16 15:20:38 -0600

    Finish iteration 2

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -150,7 +150,15 @@ RSpec.describe Round do
     expect(round.percent_correct).to eq(50.0)
   end
 
-  xit 'returns percent correct by category' do
+  it 'returns percent correct by category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
 
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    next_turn = round.take_turn("Venus")
+    expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
   end
 end
