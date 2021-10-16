7ca4cc1813edd9c5e37e658fb954b2fb76a51250
Parker Lockhart <parkerlockhart42@gmail.com>
2021-10-16 13:57:29 -0500

    Add methods

--- lib/round.rb
+++ lib/round.rb
@@ -27,22 +27,16 @@ class Round
   end
 
   def number_correct_by_category(category)
-    @turns.select do |turn|           #look at the array of turns and select these
+    @turns.select do |turn|                #look at array of turn objects and select these
       (turn.guess == turn.card.answer) && (turn.card.category == category)
-    end.size                               #output array of turns that are correct
-
-    # @deck.cards_in_category(category).each do #look at each card in the array
-    #   @turns.something do |turn|              #
-    #     turn.correct? == true
-    # @turns.select do |turn|
-    #   turn.correct?
-    # end
-    #returns an array of cards in given category
+    end.size                               #output array of turns that are correct AND category matches arg, return size
   end
 
-  # def percent_correct
-  # end
-  #
-  # def percent_correct_by_category(@category)
-  # end
+  def percent_correct
+    (number_correct.to_f / count.to_f) * 100
+  end
+
+  def percent_correct_by_category(category)
+    (number_correct_by_category(category).to_f / number_correct.to_f) * 100
+  end
 end
