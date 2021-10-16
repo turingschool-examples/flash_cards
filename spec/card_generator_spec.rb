78a8baced535b4da058f4064798ff9bd9ab04443
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-16 13:20:40 -0500

    Test: add tests for card generator

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,48 @@
+require './lib/card_generator'
+require './lib/card'
+require 'pry'
+
+RSpec.describe do CardGenerator
+  it "exists" do
+    card_generator = CardGenerator.new('cards.txt')
+
+    expect(card_generator).to be_an_instance_of(CardGenerator)
+  end
+
+  it "starts with no cards" do
+    card_generator = CardGenerator.new('cards.txt')
+
+    expect(card_generator.cards).to eq([])
+  end
+
+  it "can read a file" do
+    card_generator = CardGenerator.new('cards.txt')
+
+    expect(card_generator.read_file).to be_a(String)
+  end
+
+  it "separate file data into separate card info" do
+    card_generator = CardGenerator.new('cards.txt')
+
+    expect(card_generator.separate_card_data).to be_an(Array)
+  end
+
+  it "can make an array of 3 strings" do
+    card_generator = CardGenerator.new('cards.txt')
+
+    expect(card_generator.make_strings).to be_an(Array)
+  end
+
+  it "can make a new card" do
+    card_generator = CardGenerator.new('cards.txt')
+
+    expect(card_generator.make_cards).to be_an(Array)
+    expect(card_generator.cards).to be_an(Array)
+  end
+
+  it "can close the file" do
+    card_generator = CardGenerator.new(File.open('cards.txt' ,'r'))
+
+    expect(card_generator.close_file).to eq(nil)
+  end
+end
