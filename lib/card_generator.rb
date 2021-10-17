9b4d5e8c002d1242b0c266ad9c88ef5aea380cae
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-10-17 17:01:47 -0500

    Feat: change make_card method to cards and use .each instead of .map

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,18 +1,12 @@
-# card_file = File.open('cards.txt', "r")
-# cards_array = card_file.readlines
-# cards_array_without_newline = cards_array.map {|card| card.chomp }
-# ===> cards = cards_without_newline.map {|card| card = Card.new([0])}
-# card_file.close
-
 class CardGenerator
-  attr_reader :cards
-  def initialize(text_file)
-    @filename = File.open(text_file) #,'r'))
-    @cards = []
+  attr_reader :filename
+
+  def initialize(filename)
+    @filename = filename
   end
 
   def read_file
-    @filename.read
+    File.open(@filename).read
   end
 
   def separate_card_data
@@ -25,10 +19,12 @@ class CardGenerator
     end
   end
 
-  def make_cards
-    make_strings.map do |string|
-      @cards << Card.new(string[0], string[1], string[2].to_sym)
+  def cards
+    cards = []
+    make_strings.each do |string|
+      cards << Card.new(string[0], string[1], string[2].to_sym)
     end
+    cards
   end
 
   def close_file
