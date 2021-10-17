fb5dee72121a901a3dcdf9ef29326e247a83dd5a
Sierra Tucker <sierra.tucker121@gmail.com>
2021-10-17 08:10:10 -0600

    Refactor and edit code

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -12,7 +12,7 @@ card_2 = Card.new("What is Sierra's favorite color?",
 card_3 = Card.new("Which state is known for potatoes, trout, and precious stones?",
   "Idaho",
   :Geography)
-card_4 = Card.new("What is the sume of 55 and 45?",
+card_4 = Card.new("What is the sum of 55 and 45?",
   "100",
   :Math)
 cards = [card_1, card_2, card_3, card_4]
@@ -25,6 +25,7 @@ def start(round)
 
   puts "Welcome! You're playing with #{card_total} cards."
   puts "-"*50
+
   while(counter < card_total)
     counter += 1
     puts "This is card number #{counter} out of #{card_total}."
@@ -34,6 +35,7 @@ def start(round)
     round.take_turn(user_guess.capitalize)
     puts round.turns.last.feedback
   end
+  
 puts "***** Game over! *****"
 puts "You had #{round.number_correct} guesses out of #{card_total} for a total score of #{round.percent_correct}%."
 puts "Geography - #{round.percent_correct_by_category(:Geography)}% correct"
