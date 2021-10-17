e8d02d76bb4964ad8f072459306458b4bdaa2b79
Sierra Tucker <sierra.tucker121@gmail.com>
2021-10-17 08:08:34 -0600

    Add new card to game

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,17 +2,20 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
-# class Runner
+
 card_1 = Card.new("What is the capital of Colorado?",
   "Denver",
   :Geography)
 card_2 = Card.new("What is Sierra's favorite color?",
   "Orange",
   :Random)
-card_3 = Card.new("Which is the highest Colorado 14er",
-  "Mt. Elbert",
+card_3 = Card.new("Which state is known for potatoes, trout, and precious stones?",
+  "Idaho",
   :Geography)
-cards = [card_1, card_2, card_3]
+card_4 = Card.new("What is the sume of 55 and 45?",
+  "100",
+  :Math)
+cards = [card_1, card_2, card_3, card_4]
 deck = Deck.new(cards)
 round = Round.new(deck)
 
@@ -24,8 +27,9 @@ def start(round)
   puts "-"*50
   while(counter < card_total)
     counter += 1
-    puts "This is card number #{counter} out of 3."
+    puts "This is card number #{counter} out of #{card_total}."
     puts "Question: #{round.current_card.question}"
+    print ">"
     user_guess = gets.chomp
     round.take_turn(user_guess.capitalize)
     puts round.turns.last.feedback
@@ -34,9 +38,7 @@ puts "***** Game over! *****"
 puts "You had #{round.number_correct} guesses out of #{card_total} for a total score of #{round.percent_correct}%."
 puts "Geography - #{round.percent_correct_by_category(:Geography)}% correct"
 puts "Random - #{round.percent_correct_by_category(:Random)}% correct"
+puts "Math - #{round.percent_correct_by_category(:Math)}% correct"
 end
 
-
-
 start(round)
-#require 'pry'; binding.pry
