c03b7348aa77290ec403ff0624d5552272f604f5
Robin Dittrich <robinodittrich@gmail.com>
2021-10-17 13:58:18 -0700

    Complete deck_spec test file.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -22,4 +22,21 @@ RSpec.describe Deck do
     expect(deck.count).to eq(3)
   end
 
+  it 'has the category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(card_3.category).to eq(:STEM)
+  end
+
+  it 'also has the category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(card_1.category).to eq(:Geography)
+  end
 end
