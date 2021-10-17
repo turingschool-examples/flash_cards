81e982e72836a6d7c6d86add91053a0133f2c7c0
Robin Dittrich <robinodittrich@gmail.com>
2021-10-17 14:05:54 -0700

    Add number_correct_by_category, percent_correct, percent_correct_by_category methods to round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -19,4 +19,33 @@ class Round
     @deck.cards.rotate!(1)
     turn
   end
+
+  def number_correct
+    @correct_categories.length
+  end
+
+  def number_correct_by_category(category)
+
+    category_count = 0
+
+    @correct_categories.each do |cat|
+      if cat == category
+        category_count += 1
+      end
+    end
+
+    category_count
+  end
+
+  def percent_correct
+    percent_correct = 0
+    percent_correct = number_correct.to_f / @turns.length.to_f
+    percent_correct * 100
+  end
+
+  def percent_correct_by_category(category)
+    percent_correct = 0
+    percent_correct = number_correct_by_category(category).to_f / deck.cards_in_category(category).count.to_f
+    percent_correct * 100
+  end
 end
