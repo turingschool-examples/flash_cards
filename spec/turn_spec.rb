0c244eb22bea8f5d84e0449288c16573ce2bb5b3
Robin Dittrich <robinodittrich@gmail.com>
2021-10-17 13:40:58 -0700

    Add turn test to turn_spec test file.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,3 +8,11 @@ RSpec.describe Turn do
 
     expect(turn.guess).to eq("Juneau")
   end
+
+  it 'provides a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+end
