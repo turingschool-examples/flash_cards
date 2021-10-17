b15098f72e121c029ad448ab3c966ea1368c2554
Robin Dittrich <robinodittrich@gmail.com>
2021-10-17 14:02:52 -0700

    Add current_card and take_turn methods to round.rb.

--- lib/round.rb
+++ lib/round.rb
@@ -5,3 +5,18 @@ class Round
     @deck = deck
     @correct_categories = []
     end
+
+  def current_card
+    @deck.cards[0]
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    @turns << turn
+    if turn.correct?
+      @correct_categories << turn.card.category
+    end
+    @deck.cards.rotate!(1)
+    turn
+  end
+end
