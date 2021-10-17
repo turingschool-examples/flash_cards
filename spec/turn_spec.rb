7474b2c8efc7bf4a7f2988727069624356166459
Robin Dittrich <robinodittrich@gmail.com>
2021-10-17 13:44:00 -0700

    Review and edit turn_spec test file.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -17,30 +17,30 @@ RSpec.describe Turn do
   end
 
   it 'has cards' do
-   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-   turn = Turn.new("Juneau", card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-   expect(turn.card).to eq(card)
- end
+    expect(turn.card).to eq(card)
+  end
 
- it 'is given a guess' do
-   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-   turn = Turn.new("Juneau", card)
+  it 'is given a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-   expect(turn.guess).to eq("Juneau")
- end
+    expect(turn.guess).to eq("Juneau")
+  end
 
- it 'tells you if the guess is correct' do
-   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-   turn = Turn.new("Juneau", card)
+  it 'tells you if the guess is correct' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-   expect(turn.correct?).to eq(true)
- end
+    expect(turn.correct?).to eq(true)
+  end
 
- it 'gives you feedback' do
-   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-   turn = Turn.new("Juneau", card)
+  it 'gives you feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-   expect(turn.feedback).to eq("Correct!")
- end
+    expect(turn.feedback).to eq("Correct!")
+  end
 end
