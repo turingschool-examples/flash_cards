ebdf247f0850f39512c4bf99d249e8d3007f9088
Brad Breiten <jonathan@breiten.us>
2021-10-17 11:20:09 -0600

    Add iteration 4 features

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,22 +2,32 @@ require './lib/card'
 require './lib/deck'
 require './lib/turn'
 require './lib/round'
+require './lib/card_generator'
 
 def start
 
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+### Hard Coded Flash Cards ###
+  # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #
+  # card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  #
+  # card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  #
+  # card_4 = Card.new("What is the capital of New York state?", "Albany", :Geography)
+  #
+  # card_5 = Card.new("Who is the wizarding protagonist in the 7 part series written by J.K. Rowling?", "Harry Potter", :Pop_Culture)
+  #
+  # card_6 = Card.new("Name the fruit based company whose operating system is preferred by Turing.", "Apple", :Pop_Culture)
 
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+### Iteration 4 Attempt ###
 
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  filename = "cards.txt"
 
-  card_4 = Card.new("What is the capital of New York state?", "Albany", :Geography)
+  cards = CardGenerator.new(filename).cards
 
-  card_5 = Card.new("Who is the wizarding protagonist in the 7 part series written by J.K. Rowling?", "Harry Potter", :Pop_Culture)
+  # deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
 
-  card_6 = Card.new("Name the fruit based company whose operating system is preferred by Turing.", "Apple", :Pop_Culture)
-
-  deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
+  deck = Deck.new(cards)
 
   round = Round.new(deck)
 
