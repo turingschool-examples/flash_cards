f1fc026e8e3661343b616ff8bf6ebd1bef1f3e6e
Mallory Vining <mallorygallery12@gmail.com>
2021-10-16 18:03:52 -0600

    Add runner file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,40 +4,37 @@ require './lib/deck'
 require './lib/round'
 
 class FlashCards
+  attr_reader :card_1,
+              :card_2,
+              :card_3,
+              :card_4,
+              :cards,
+              :deck,
+              :round
   def initialize
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new(deck)
+    @card_1 = Card.new("Question: What is 5 + 5?", "10", :STEM)
+    @card_2 = Card.new("What is Rachel's favorite animal?", "panda", :Turing)
+    @card_3 = Card.new("Question: What is Mike's middle name?", "Lancelot", :Turing)
+    @card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Turing)
+    @deck = Deck.new([card_1, card_2, card_3, card_4])
+    @round = Round.new(@deck)
+    @cards_count = deck.cards.count
   end
-
   def start
-    puts "Welcome! You're playing with 4 cards."
+    puts "Welcome! You're playing with #{@cards_count} cards."
     puts "-------------------------------------------------"
-    puts "This is card number 1 out of 4"
-    puts "Question: What is 5 + 5?"
-    puts "10"
-    puts "Correct!"
-    puts "This is card number 2 out of 4."
-    puts "Question: What is Rachel's favorite animal?"
-    puts "panda"
-    puts "Incorrect."
-    puts "This is card number 3 out of 4."
-    puts "Question: What is Mike's middle name?"
-    puts "Lancelot"
-    puts "Correct!"
-    puts "This is card number 4 out of 4."
-    puts "Question: What cardboard cutout lives at Turing?"
-    puts "Justin Bieber"
-    puts "Correct!"
-    puts "****** Game over! ******"
-    puts "You had 3 correct guesses out of 4 for a total score of 75%."
-    puts "STEM - 100% correct"
-    puts "Turing Staff - 50% correct"
-    puts "Pop Culture - 100% correct"
-  end
+      until @round.turns.count && @deck.cards.count == 0
+        puts "This is card number #{@round.turns.count} out of #{@round.deck.cards.length}"
+        puts "Question: #{@round.current_card.question}"
+        guess = gets.chomp
+        @round.take_turn(guess)
+        puts @round.turns.last.feedback
+      end
+        puts "****** Game over! ******"
+        puts "You had #{@round.number_correct} correct guesses out of #{@round.deck.cards.length} for a total score of #{round.percent_correct}."
+        puts "STEM - #{@round.percent_correct_by_category(:STEM )} correct"
+        puts "Turing Staff - #{@round.percent_correct_by_category(:Turing )} correct"
+    end
 end
-
 flashcards = FlashCards.new
 flashcards.start
