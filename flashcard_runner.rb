cfaffe50394be4632e56bfdf8d370e424d041ae9
Brad Breiten <jonathan@breiten.us>
2021-10-17 12:17:32 -0600

    Update softcoded runner and txt file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,26 +6,9 @@ require './lib/card_generator'
 
 def start
 
-### Hard Coded Flash Cards ###
-  # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #
-  # card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  #
-  # card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  #
-  # card_4 = Card.new("What is the capital of New York state?", "Albany", :Geography)
-  #
-  # card_5 = Card.new("Who is the wizarding protagonist in the 7 part series written by J.K. Rowling?", "Harry Potter", :Pop_Culture)
-  #
-  # card_6 = Card.new("Name the fruit based company whose operating system is preferred by Turing.", "Apple", :Pop_Culture)
-
-### Iteration 4 Attempt ###
-
   filename = "cards.txt"
 
   cards = CardGenerator.new(filename).cards
-
-  # deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
 
   deck = Deck.new(cards)
 
