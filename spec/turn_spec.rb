7fdfb7369339d7c80aef521d073cfd3bbf149819
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-10-17 11:54:34 -0600

    Refactor/clarify tests for #correct? and #feedback

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,24 +3,27 @@ require './lib/card'
 
 RSpec.describe Turn do
   context 'Iteration I' do
-    it 'exists' do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Juneau", card)
+    before :each do
+      @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      @turn = Turn.new("Juneau", @card_1)
+    end
 
-      expect(turn.card).to be_a(Card)
-      expect(turn.guess).to eq("Juneau")
-      expect(turn.correct?).to eq(true)
-      expect(turn.feedback).to eq("Correct!")
+    it 'exists' do
+
+      expect(@turn.card).to be_a(Card)
     end
 
     it 'can give correct/incorrect guess responses' do
-      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-      turn = Turn.new("Saturn", card)
+      expect(@turn.guess).to eq("Juneau")
+      expect(@turn.correct?).to eq(true)
+      expect(@turn.feedback).to eq("Correct!")
 
-      expect(turn.card).to be_a(Card)
-      expect(turn.guess).to eq("Saturn")
-      expect(turn.correct?).to eq(false)
-      expect(turn.feedback).to eq("Incorrect.")
+      card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      turn_2 = Turn.new("Saturn", card_2)
+      expect(turn_2.card).to be_a(Card)
+      expect(turn_2.guess).to eq("Saturn")
+      expect(turn_2.correct?).to eq(false)
+      expect(turn_2.feedback).to eq("Incorrect.")
     end
   end
 end
