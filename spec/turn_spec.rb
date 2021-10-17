e179b6d7fff5bc4c9e77f5286ad3a04a4f04d1cd
Brad Breiten <jonathan@breiten.us>
2021-10-16 22:51:39 -0600

    Commit mistakes on purpose

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Turn do
    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
    turn = Turn.new("Juneau", card)
 
-   expect(turn).to be_instance_of(Turn)
+   expect(turn).to be_instance_of(Hearts)
  end
 
  it 'makes card' do
