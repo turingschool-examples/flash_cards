0eab4f221ae0f10b32f0baadd28b68d40624db81
Brad Breiten <jonathan@breiten.us>
2021-10-16 22:57:25 -0600

    Revert "Commit mistakes on purpose"
    This reverts commit e179b6d7fff5bc4c9e77f5286ad3a04a4f04d1cd.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Turn do
    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
    turn = Turn.new("Juneau", card)
 
-   expect(turn).to be_instance_of(Hearts)
+   expect(turn).to be_instance_of(Turn)
  end
 
  it 'makes card' do
