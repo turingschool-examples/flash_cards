f739edfcc97b8c9332abe0c2b33523903bcc115f
Russell Rockwood <russell.rockwood@gmail.com>
2021-10-16 22:06:35 -0500

    Add round.start method, flashcard_runner

--- lib/round.rb
+++ lib/round.rb
@@ -4,23 +4,23 @@ require './lib/deck'
 
 class Round
   attr_accessor :deck
-  attr_reader :turns
+  attr_reader :turns, :index
 
   def initialize(input_deck)
     @deck = input_deck
     @turns = []
+    @index = 0
   end
 
   def current_card
-    @deck.cards.first
+    @deck.cards[@index]
   end
 
   def take_turn(guess)
     @turns << Turn.new(guess, current_card)
-    @deck.cards.shift
+    @index += 1
     @turns.last
   end
-
 
   def number_correct
     counter = 0
@@ -43,7 +43,7 @@ class Round
   end
 
   def percent_correct
-    (Float(self.number_correct) / @turns.count).round(2) * 100
+    ((Float(self.number_correct) / @turns.count).round(2) * 100).to_i
   end
 
   def percent_correct_by_category(category)
@@ -54,7 +54,38 @@ class Round
         count += 1
       end
     end
-    (Float(correct) / count).round(2) * 100
+    ((Float(correct) / count).round(2) * 100).to_i
+  end
+
+  def start
+    card_count = self.deck.cards.count
+
+    puts "Welcome! You're playing with #{card_count} cards."
+    puts " * * * * * * * * * * * * * * * *"
+
+    while self.index + 1 <= card_count
+
+      puts "This is card #{self.index + 1} out of #{card_count}"
+      puts "Question: #{self.current_card.question}"
+      response = gets.chomp
+
+      self.take_turn(response)
+      puts @turns[@index - 1].feedback
+    end
+
+    puts "* * * * Game Over! * * * *"
+    puts "You had #{number_correct} correct guesses out of #{card_count} for a total score of #{percent_correct}%"
+
+    category_array = []
+    turns.each do |turn|
+      category_array << turn.card.category
+    end
+    category_array = category_array.uniq
+
+    category_array.each do |category|
+      puts "#{category} - #{percent_correct_by_category(category)}% correct"
+    end
+    
   end
 
 end
