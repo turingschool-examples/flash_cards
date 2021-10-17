114e9b3063e9ac14fb60d4705bfb581f565e6f1f
Robin Dittrich <robinodittrich@gmail.com>
2021-10-17 13:54:16 -0700

    Complete deck.rb file

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,3 +3,18 @@ class Deck
   def initialize (cards)
     @cards = cards
   end
+
+  def count
+    cards.count
+  end
+
+  def cards_in_category(category)
+    current_category = []
+    @cards.each do|card|
+        if card.category == category
+        current_category << card.category
+      end
+    end
+    current_category
+  end
+end
