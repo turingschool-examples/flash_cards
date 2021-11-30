d2111ff2e055b77e7f1020f2dae3e4a09673e7cf
Conor Barthel <conor.barthel@gmail.com>
2021-11-30 16:48:31 -0700

    Fix errors in card instance test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,10 +1,10 @@
 require './lib/turn'
 require './lib/card'
 
-Rspec.describe Card do
+RSpec.describe Card do
   it "is an instance of Card" do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    expect(card).to be_instance_of(card)
+    expect(card).to be_instance_of(Card)
   end
 end
