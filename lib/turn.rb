f456d412f57e7fdb9eaa639f51fbd80da3ca2d16
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-11-30 17:05:35 -0500

    Add Turn instance test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,7 @@
 #require './lib/card'
 
 class Turn
+  attr_reader :string, :card 
   def initialize(string,card)
     @string = string
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
