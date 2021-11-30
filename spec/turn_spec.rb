3765e3e9449f7bc626b7731c50e5fbd26b947e2e
Conor Barthel <conor.barthel@gmail.com>
2021-11-30 16:43:42 -0700

    Add test for 'Card' instance

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,1 +1,10 @@
+require './lib/turn'
+require './lib/card'
 
+Rspec.describe Card do
+  it "is an instance of Card" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card).to be_instance_of(card)
+  end
+end
