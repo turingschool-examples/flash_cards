7c466aa28a93aa70bc58495fc18fee7d34669daa
Cole Bradley <cole.bradley303@gmail.com>
2021-11-30 21:09:40 -0700

    Create .correct? method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,15 @@
 # require 'pry'
 
 class Turn
-  attr_reader :card
+  attr_reader :guess, :card
 
 def initialize(guess, card)
+  @guess = guess
   @card = card
   end
+
+def correct?
+  card.answer == guess
+  end
 end
-
-
 # binding.pry
