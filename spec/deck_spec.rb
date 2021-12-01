a4f847a71dc35d7de601f3ac667e384109c29ebf
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-11-30 23:08:48 -0500

    Add display cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -30,11 +30,23 @@ RSpec.describe Deck do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
     cards = []
     cards << card_1 << card_2 << card_3
 
     deck = Deck.new(cards)
-    
     expect(deck.count).to eq(3)
+  end
+
+  it "can show cards in deck" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = []
+    cards << card_1 << card_2 << card_3
+
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq(cards)
   end
 end
