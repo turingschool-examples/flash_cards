432661104d0d9d1df91e65dff411e0d06164e8b0
Cole Bradley <cole.bradley303@gmail.com>
2021-11-30 20:30:43 -0700

    test for presence of card instance

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,7 +1,6 @@
 require './lib/turn'
 require './lib/card'
 
-
 RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -9,4 +8,14 @@ RSpec.describe Turn do
 
     expect(turn).to be_instance_of(Turn)
   end
+
+it 'Contains card instance' do
+  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  turn = Turn.new("Juneau", card)
+
+  expect(turn.card).to eq(card)
+end
+
+
+
 end
