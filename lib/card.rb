e5c3d82f3d8d83025bfb3d570cc77e5fe4ed0882
Joseph Galvin <jwgalvin@gmail.com>
2021-11-30 17:42:16 -0700

    fix card test issue

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,4 @@
+
 class Card
   attr_reader :question, :answer, :category
   def initialize(question, answer, category)
@@ -5,4 +6,15 @@ class Card
     @answer   = answer
     @category = category
   end
+
 end
+
+#class Turn
+#  def initialize(string, card)
+#    @string = string
+#    @card   = card
+#  end
+
+#  def guess()
+#    guess =
+# end
