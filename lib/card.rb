fbd00b448eaa87f28be0fdb649ab077e3e710cd0
Cole Bradley <cole.bradley303@gmail.com>
2021-11-30 17:14:03 -0700

    Creat question method

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,12 @@
+# require 'pry'
+
 class Card
+  attr_reader :question #:answer, #:catagory
 
 def initialize(question, answer, catagory)
+  @question = question
+  # @answer = answer
+  # @catagory = catagory
   end
 end
 
@@ -8,5 +14,5 @@ end
 
 
 
-
 card = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+# binding.pry
