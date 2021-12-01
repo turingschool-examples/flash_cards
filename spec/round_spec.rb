a2dbcc82781825223a13278c78cc7a42e2e1a4e7
Michael Brandt <msbrandt00@gmail.com>
2021-12-01 12:45:10 -0700

    Add tests on adding Turn classes to #turns array

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Round do
     expect(round).to be_instance_of(Round)
   end
   describe 'take_turn' do
-    it 'records all cards in @turns array' do
+    it 'adds Turn class information to turns array' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
         and reports about the surface of which planet?", "Mars", :STEM)
@@ -30,9 +30,23 @@ RSpec.describe Round do
       cards= [card_1, card_2, card_3]
       deck = Deck.new(cards)
       round = Round.new(deck)
-      round.take_turn('Juneau')
-      expect(round.turns).to eq(round.deck.cards[0..round.card_counter])
+      round.take_turn('hi')
+      expect(round.turns).not_to be_empty
+      expect(round.turns.last).to be_instance_of(Turn)
+    end
+    it 'can add more than one Turn to array' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+        and reports about the surface of which planet?", "Mars", :STEM)
+
+      card_3 = Card.new("Describe in words the exact direction
+         that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards= [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      round.take_turn('hi')
+      round.take_turn('France')
+      expect(round.turns.length).to eq(2)
     end
   end
-
-end
+end 
