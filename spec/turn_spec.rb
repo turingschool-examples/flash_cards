5858f0647dffb72c473da50d43908971d19bd8e2
Jesse Ochs <jessescottochs@gmail.com>
2021-11-30 20:08:45 -0700

    Add exists and has guess test

---
+++ spec/turn_spec.rb
@@ -0,0 +1,19 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("string", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("string", card)
+    expect(turn.guess).to eq('string')
+  end
+
+
+end
