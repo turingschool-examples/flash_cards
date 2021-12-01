0ebf901372f6965f3170a13b352c52934ae2c93b
Conor Barthel <conor.barthel@gmail.com>
2021-11-30 21:37:35 -0700

    Add 'correct?' test and method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,17 @@
 #~/turn.rb
 require './lib/card'
 
+
 class Turn
   attr_reader :guess, :card
+
   def initialize(guess, card)
     @guess = guess
     @card = card
+
+  end
+
+  def correct?
+    guess == card.answer
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -11,15 +11,32 @@ end
 
 RSpec.describe Turn do
   it "is an instance of Turn" do
-    turn = Turn.new("Juneau", Card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
     expect(turn).to be_instance_of(Turn)
   end
 
-  it "returns guess" do
-    turn = Turn.new("Juneau", Card)
+  it "returns info in Card" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+  it "returns guess argument" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
     expect(turn.guess).to eq("Juneau")
+  end
+
+  it "returns true" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+
+    expect(turn.correct?).to be true
   end
 
 
