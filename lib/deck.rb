0f8515f45e98998e7642c604e7c20720249e6435
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-11-30 23:05:36 -0500

    Add test to show number of cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,6 +2,10 @@ class Deck
   attr_reader :cards
 
   def initialize(cards)
-    @cards = cards 
+    @cards = cards
+  end
+
+  def count
+    @cards.count
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,6 +1,7 @@
 require '../lib/card'
 require '../lib/turn'
 require '../lib/deck'
+require 'pry'
 
 RSpec.describe Deck do
   it "can hold cards" do
@@ -25,5 +26,15 @@ RSpec.describe Deck do
     expect(deck).to be_instance_of Deck
   end
 
-  
+  it "can show number of cards in deck" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = []
+    cards << card_1 << card_2 << card_3
+
+    deck = Deck.new(cards)
+    
+    expect(deck.count).to eq(3)
+  end
 end
