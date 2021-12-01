8b0459c544870865e7e2b0e96897291ab48a1918
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-12-01 09:40:22 -0500

    Add test to show current card

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe Round do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
     cards = []
     cards << card_1 << card_2 << card_3
 
@@ -36,6 +37,7 @@ RSpec.describe Round do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
     cards = []
     cards << card_1 << card_2 << card_3
 
@@ -44,5 +46,18 @@ RSpec.describe Round do
 
     expect(round.turns).to eq([])
   end
+
+  it "shows current card" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = []
+    cards << card_1 << card_2 << card_3
+
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.current_card).to eq(cards[0])
+  end
+
 
 end
