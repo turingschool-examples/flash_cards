a2ca376947d48972f94e4d0d16357ee58b8aae21
Jesse Ochs <jessescottochs@gmail.com>
2021-11-30 20:07:13 -0700

    Add guess method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,15 @@
+require './lib/card'
+
+
 class Turn
 
-  def initialize(string, card)
-    @string = string
+  def initialize(guess, card)
+    @guess = guess
     @card = card
+  end
+
+  def guess
+    return @guess
   end
 
 end
