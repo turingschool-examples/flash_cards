f181a8de02375bf48d9336f5c1ded7e221f97458
Michael Brandt <msbrandt00@gmail.com>
2021-11-30 22:59:51 -0700

    Add first test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,31 +3,33 @@ require './lib/card'
 require 'rspec'
 require 'pry'
 
-RSpec.describe Turn do
+
+RSpec.describe Card do
   it 'exists' do
-    my_card = Card.new("What is the capital of Alaska", "Juneau", :Geography)
-    my_turn = Turn.new("What is the capital of Alaska?", my_card)
-    expect(my_turn).to be_instance_of(Turn)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(card).to be_instance_of(Card)
+    expect(turn).to be_instance_of(Turn)
   end
 
-  it 'has a question' do
+  xit 'has a question' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.question).to eq("What is the capital of Alaska?")
   end
 
-  it 'has an answer' do
+  xit 'has an answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.answer).to eq("Juneau")
   end
 
-  it 'has a category' do
+  xit 'has a category' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.category).to eq(:Geography)
   end
-  it 'can turn a card' do
+  xit 'can turn a card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     expect(card.category).to eq(:Geography)
   end
