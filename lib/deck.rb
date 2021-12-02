a74cea9cd1e55cc35cfbb780b2ae0e0ea7a32cb7
Conor Barthel <conor.barthel@gmail.com>
2021-12-02 07:59:20 -0700

    Fix errors in test file, create rough draft for #cards_in_catagory method

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,15 @@ class Deck
   def initialize(flash_cards)
     @flash_cards = flash_cards
   end
+
+  def cards_in_category(requested_category)
+    catagorized_cards = []
+    @flash_cards.each do |card|
+      if requested_category == card.category
+        catagorized_cards << card
+      end
+    end
+  end
+
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -14,10 +14,12 @@ RSpec.describe Deck do
 
   describe '#cards_in_category' do
     it "return cards 2 and 3"
-    ard_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+    deck.cards_in_category(:STEM)
     expect(deck.cards_in_category).to eq(card_1, card_2)
+  end
 end
