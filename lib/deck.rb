d522f6f5f8841fa45384cf86aa5be11aa7447948
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-02 17:14:45 -0500

    pass card count test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,7 @@
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :count
   def initialize(cards)
     @cards = cards.push
+    @count = @cards.length
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,4 @@
+#require 'pry' binding.pry
 require './lib/card'
 require './lib/deck'
 
@@ -5,9 +6,11 @@ RSpec.describe Deck do
   it 'exists' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+
     expect(deck).to be_instance_of (Deck)
   end
 
@@ -18,5 +21,14 @@ RSpec.describe Deck do
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     expect(deck.cards).to eq([card_1, card_2, card_3])
+  end
+
+  it 'can count cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck.count).to eq(cards.length)
   end
 end
