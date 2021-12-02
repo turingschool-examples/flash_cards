141c8eb072885ef8a38d61067db7e2283f268fa2
Mark Ertmer <mark.ertmer@gmail.com>
2021-12-02 14:17:07 -0700

    Add test to check that round.current_card advances

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -68,5 +68,19 @@ RSpec.describe Deck do
     expect(round.number_correct).to eq(2)
   end
 
+it 'advances to the next card after each turn' do
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
+  deck = Deck.new(cards)
+  round = Round.new(deck)
+  expect(round.current_card).to eq(card_1)
+  new_turn = round.take_turn("Juneau")
+  expect(round.current_card).to eq(card_2)
+  next_turn = round.take_turn("Mars")
+  expect(round.current_card).to eq(card_3)
+end
+
 
 end
