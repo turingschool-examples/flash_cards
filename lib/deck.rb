2c1fa5cc77db776e87e5cdb7280da29ddb5257cb
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-02 18:01:21 -0500

    pass see a category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,19 @@
+require './lib/card'
 class Deck
   attr_reader :cards, :count
   def initialize(cards)
     @cards = cards.push
     @count = @cards.length
+
   end
+  def cards_in_category(category)
+    cards_in = []
+    @cards.each do|card|
+      if card.category == category
+        cards_in << card
+      end
+    end
+    return cards_in
+  end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Deck do
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    expect(deck.cards).to eq([card_1, card_2, card_3])
+    expect(deck.cards).to eq(cards)
   end
 
   it 'can count cards' do
@@ -30,5 +30,14 @@ RSpec.describe Deck do
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     expect(deck.count).to eq(cards.length)
+  end
+
+  it 'see a category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
   end
 end
