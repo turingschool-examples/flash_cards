62f24ea2366e7ae6668f739f2660d58ba9551f5e
Cole Bradley <cole.bradley303@gmail.com>
2021-12-02 14:44:42 -0700

    Fix indentation

--- lib/card.rb
+++ lib/card.rb
@@ -1,15 +1,10 @@
+
 class Card
   attr_reader :question, :answer, :category
 
-def initialize(question, answer, category)
-  @question = question
-  @answer = answer
-  @category = category
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
   end
 end
-
-
-
-
-
-card = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
