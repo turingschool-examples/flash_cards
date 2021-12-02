84c401d427195e3d49b68ff0e61a3d574aed72bb
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-12-02 17:56:56 -0500

    Add 'create a new turn' test

--- lib/round.rb
+++ lib/round.rb
@@ -1,12 +1,21 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
   end
 
   def current_card
     @deck.cards[0]
+  end
+
+  def take_turn(guess)
+      new_turn = Turn.new(guess,current_card)
+      @turns << new_turn
+      @number_correct +=1 if new_turn.correct?
+      @deck.cards.shift
+      new_turn
   end
 end
