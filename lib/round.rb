1bc6a71b2beea1551d0076eaf82bd5274a4bfea2
Michael Brandt <msbrandt00@gmail.com>
2021-12-01 22:08:11 -0700

    Refactor #correct? method

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,10 @@
 class Round
-  attr_reader :turns, :deck, :cards, :card_counter
+  attr_reader :turns, :deck, :cards, :card_counter, :number_correct, :answer
   def initialize(deck)
     @deck = deck
     @turns = []
     @card_counter = -1
+    @number_correct = 0
   end
 
 
@@ -16,6 +17,15 @@ class Round
     @card_counter += 1
     @turns.push Turn.new(guess, current_card)
     return @turns.last
+  end
+
+  def correct?
+    if self.turns.last.guess == self.turns.last.answer
+      true
+      @number_correct += 1
+    else
+      false
+    end
   end
 
 
