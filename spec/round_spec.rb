dc53c396ad9827f992c0212ff09534272c9814f6
Michael Brandt <msbrandt00@gmail.com>
2021-12-01 22:07:43 -0700

    Add #correct? method tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -49,4 +49,32 @@ RSpec.describe Round do
       expect(round.turns.length).to eq(2)
     end
   end
-end 
+  describe 'correct?' do
+    it 'can return true when guess is correct' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+        and reports about the surface of which planet?", "Mars", :STEM)
+
+      card_3 = Card.new("Describe in words the exact direction
+         that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards= [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      new_turn = round.take_turn('Juneau')
+      expect(new_turn.correct?).to be(true)
+    end
+    it 'can return false when guess is incorrect' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+        and reports about the surface of which planet?", "Mars", :STEM)
+
+      card_3 = Card.new("Describe in words the exact direction
+         that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards= [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      new_turn = round.take_turn('Sacramento')
+      expect(new_turn.correct?).to be(false)
+    end
+  end
+end
