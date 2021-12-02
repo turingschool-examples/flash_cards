9773490eae8835595932dc571f556e32b152ff4c
Cole Bradley <cole.bradley303@gmail.com>
2021-12-02 16:09:46 -0700

    Test that round.current_card returns first card in deck array

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Card do
     expect(round).to be_instance_of(Round)
   end
 
-  it "returns empty array" do
+  it 'returns empty array' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -22,6 +22,16 @@ RSpec.describe Card do
     round = Round.new(deck)
 
     expect(round.turns).to eq([ ])
+  end
+
+  it 'returns first card in deck array' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.current_card).to eq(card_1)
   end
 
 
