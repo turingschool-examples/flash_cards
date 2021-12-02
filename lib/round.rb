a4f0457681644180fac347988bd6abf08235f896
Mark Ertmer <mark.ertmer@gmail.com>
2021-12-01 19:19:42 -0700

    Add curr_card to Round.initialize and attr_reader

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,11 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :curr_card
   def initialize(deck)
     @deck = deck
+    @curr_card = 0
+  end
+
+  def current_card
+    @deck.cards[@curr_card]
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -19,4 +19,30 @@ RSpec.describe Deck do
     expect(round.deck).to eq(deck)
   end
 
+  it 'has a current_card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.current_card).to eq(card_1)
+  end
+
+  it 'takes a turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    expect(new_turn.class).to eq(Turn)
+    expect(new_turn.correct?).to eq(true)
+  end
+
+
+
+
+
 end
