e029200d380f6dca897beda89a2db0353b4baa0d
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-02 18:33:43 -0500

    Annotate for complex portions

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -6,11 +6,11 @@ class CardGenerator
   def initialize(filename)
     @filename = filename
     @cards = []
-    @file = File.open("lib/#{filename}", "r")
-    @file.map do |line|
-      line.chomp!
-      line = line.split(',')
-      @cards << Card.new(line[0], line[1], line[2])
+    @file = File.open("lib/#{filename}", "r") #opens file
+    @file.map do |line| #iterates through each line
+      line.chomp! # removes \n which were spaces in .txt file
+      line = line.split(',') #reverts to array by element
+      @cards << Card.new(line[0], line[1], line[2]) #assign elements to card attr
     end
   end
 end
--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -10,28 +10,28 @@ deck = Deck.new(cards)
 round = Round.new(deck)
 
 def start(round)
-  puts "Welcome! You're playing with #{round.deck.cards.length} cards."
-  puts "-" * 50
-  while round.turns.length < round.deck.cards.length
-    puts "This is card number #{round.turns.length + 1} out of #{round.deck.cards.length}."
-    puts "Question: #{round.current_card.question}"
-    guess = gets.chomp
-    round.take_turn(guess)
-    p round.turns.last.feedback
+  puts "Welcome! You're playing with #{round.deck.cards.length} cards." #inserts how many cards are in
+  puts "-" * 50                                                         # the deck
+  while round.turns.length < round.deck.cards.length #tracks when all cards have been attempted
+    puts "This is card number #{round.turns.length + 1} out of #{round.deck.cards.length}." #turns starts with 0 turns so '+ 1 ' is added
+    puts "Question: #{round.current_card.question}" #gets question of current_card
+    guess = gets.chomp #gets users guess
+    round.take_turn(guess) #provides user guess as argument
+    p round.turns.last.feedback #returns feedback of most recent turn
   end
-  gameover(round)
+  gameover(round) #once all cards have been attempted - run gameover method
 end
 
 def gameover(round)
   puts "****** Game over! ******"
   puts "You had #{round.number_correct} correct guesses out of #{round.deck.cards.length} for a total score of #{round.percent_correct.to_i}%"
-  categories = []
+  categories = [] #empty container for upcoming iteration
   round.turns.each do |turn|
-    categories << turn.card.category unless categories.include?(turn.card.category)
+    categories << turn.card.category unless categories.include?(turn.card.category) #creates an array of categories preventing duplicates
   end
-  categories.each do |category|
+  categories.each do |category| #iterate through each category and provide % correct
     puts "#{category} - #{round.percent_correct_by_category(category).to_i}% correct"
   end
 end
 
-start(round)
+start(round) #start flashcard game 
