c55d09b556e3e5018a62aeb5c07fc44b19d8e5aa
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-01 22:52:28 -0500

    Create gives bad news and counts higher than 1 test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -91,5 +91,18 @@ RSpec.describe Round do
 
     new_turn = round.take_turn("Juneau")
     expect(round.current_card).to be(card_2)
-  end 
+  end
+
+  it 'gives bad news and counts higher than 1' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    new_turn_2 = round.take_turn("Venus")
+
+    expect(round.turns.count).to be(2)
+    expect(round.turns.last.feedback).to eq('Incorrect.')
+  end
 end
