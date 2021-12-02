1210d76d2391fc897120c73625610e70851e1a75
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-01 22:12:37 -0500

    Create it creates a new turn test - require turn file

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,6 +1,7 @@
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/turn'
 
 RSpec.describe Round do
   it 'exists' do
@@ -41,5 +42,16 @@ RSpec.describe Round do
     round = Round.new(deck)
 
     expect(round.current_card).to be(card_1)
+  end
+
+  it 'creates a new turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    expect(new_turn).to be_an_instance_of(Turn)
   end
 end
