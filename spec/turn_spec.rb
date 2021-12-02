355dbbde0c762789705ad2571fec36eb8999ff9e
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-01 20:56:59 -0500

    pass holds a guess test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,13 +8,16 @@ RSpec.describe Turn do
     turn = Turn.new("Juneau", card)
     expect(turn).to be_instance_of (Turn)
   end
+
   it 'holds a card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.card).to be_instance_of (Card)
   end
+
+  it 'holds a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
+  end
 end
-  #xit 'holds a card' do
-  #  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#  turn = Turn.new("Juneau", card)
-#end
