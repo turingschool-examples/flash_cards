03d4a6f15de99068e6841a7730de415aa904ba07
Joseph Galvin <jwgalvin@gmail.com>
2021-12-02 14:00:48 -0700

    test geography class and pass

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -61,8 +61,8 @@ RSpec.describe Deck do
     end
   end
 
-  describe "count the category" do
-    it "counts categories" do
+  describe "check categories" do
+    it "Stem check" do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -71,5 +71,18 @@ RSpec.describe Deck do
       expect(deck.cards_in_category(:STEM)).to include(card_2, card_3)
     end
   end
+
+  describe "check categories" do
+    it "Geography check" do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      expect(deck.cards_in_category(:Geography)).to include(card_1)
+    end
+  end
+
+  
 
 end
