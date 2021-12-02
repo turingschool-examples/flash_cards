9cc50ca27cb79f2ad137efdd9eae155a41c54b4a
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-01 21:30:05 -0500

    Create cards_in_category method - all tests pass

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,13 @@
 class Deck
   attr_reader :cards
-  
+
   def initialize(cards)
     @cards = cards
+  end
+
+  def cards_in_category(category)
+    @sorted_cards = @cards.select do |card|
+      card.category == category
+    end
   end
 end
