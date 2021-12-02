5caec22fc4e6529b48cd2605940f5e91b1766443
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-01 22:00:53 -0500

    Create provides first card as current test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Round do
     round = Round.new(deck)
 
     expect(round.turns).to eq([])
-  end 
+  end
 
   it 'has a deck' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -31,5 +31,15 @@ RSpec.describe Round do
     round = Round.new(deck)
 
     expect(round.deck).to eq(deck)
+  end
+
+  it 'provides first card as current' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.current_card).to be(card_1)
   end
 end
