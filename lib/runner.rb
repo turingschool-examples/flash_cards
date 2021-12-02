1fd0a71c3c16e3d238e569fc41ebf1b7782b1ec2
Michael Brandt <msbrandt00@gmail.com>
2021-12-02 11:09:40 -0700

    Add cards and initialize Game class

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,1 +1,21 @@
 #Iteration 3
+require_relative './card'
+
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+  and reports about the surface of which planet?", "Mars", :STEM)
+
+card_3 = Card.new("Describe in words the exact direction
+   that is 697.5° clockwise from due north?", "North north west", :STEM)
+$cards= [card_1, card_2, card_3]
+
+class Game
+  def welcome
+    puts "Welcome! You're playing a game with #{$cards.length} cards \n
+    ------------------------------------------- \nThis is card number 1 out of #{$cards.length}"
+  end
+
+end
+
+game = Game.new
+game.welcome
