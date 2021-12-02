dfe3483612f80f144e0d137fab7a86ea991dc6be
Cole Bradley <cole.bradley303@gmail.com>
2021-12-02 15:52:25 -0700

    Test for round.turns to return []

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -14,6 +14,17 @@ RSpec.describe Card do
     expect(round).to be_instance_of(Round)
   end
 
+  it "returns empty array" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.turns).to eq([ ])
+  end
+
+
 
 
 
