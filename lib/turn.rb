abe4301c49ee07877ede1bcdc72ac213ce165a23
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-01 20:22:14 -0500

    pass it holds a card test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,7 @@
 require './lib/card'
 require 'pry'
 class Turn
+  attr_reader :guess, :card
   def initialize(guess, card)
     @guess = guess
     @card = card
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,6 +6,15 @@ RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-
+    expect(turn).to be_instance_of (Turn)
+  end
+  it 'holds a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.card).to be_instance_of (Card)
   end
 end
+  #xit 'holds a card' do
+  #  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+#  turn = Turn.new("Juneau", card)
+#end
