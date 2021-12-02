eec359a68e5d15529c831603b746b900bfd2cf3f
Michael Brandt <msbrandt00@gmail.com>
2021-12-02 09:29:43 -0700

    Add #percent_correct method testing

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -128,9 +128,29 @@ RSpec.describe Round do
       round.take_turn('Venus') #incorrect
       round.correct?
       round.take_turn('North north west') #correct
-      round.correct? 
+      round.correct?
       expect(round.number_correct_by_category(:STEM)).to equal(1)
       expect(round.number_correct_by_category(:Geography)).to equal(0)
+    end
+  end
+  describe '#percent_correct' do
+    it 'will return the percent_correct' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+        and reports about the surface of which planet?", "Mars", :STEM)
+
+      card_3 = Card.new("Describe in words the exact direction
+         that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards= [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      round.take_turn('Sacramento') #incorrect
+      round.correct?
+      round.take_turn('Venus') #incorrect
+      round.correct?
+      round.take_turn('North north west') #correct
+      round.correct?
+      expect(round.percent_correct).to eq(0.33)
     end
   end
 end
