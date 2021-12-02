334db3d622947a91424fbdc3e3dd087043083a0d
Michael Brandt <msbrandt00@gmail.com>
2021-12-02 15:15:19 -0700

    Finish iteration 3

--- lib/runner.rb
+++ lib/runner.rb
@@ -25,22 +25,30 @@ class Game
   def welcome
     puts "Welcome! You're playing a game with #{@round.deck.cards.length} cards."
     puts "-------------------------------------------"
-    puts "This is card number 1 out of #{@round.deck.cards.length}"
+    puts "This is card number 1 out of #{@round.deck.cards.length}."
   end
 
   def start
+    puts welcome
+    @round.deck.cards.length.times {
+
     puts "Question: #{@round.deck.cards[@round.card_counter].question}"
     user_answer = gets.to_s.chomp
-    #this needs to go into round object turns.
-    @round.turns << Turn.new(user_answer, @round.deck.cards[@round.card_counter]) #turn or round object?
-    #do i need to put in a stats_hash array? Is there where I put attr_writer?
-    @round.turns.last.feedback
-    @round.turns.last.correct?
-    binding.pry
+    @round.turns << Turn.new(user_answer, @round.deck.cards[@round.card_counter])
+    puts @round.feedback
+    }
+    puts '***** Game over! *****'
+    puts "You had #{@round.number_correct} guesses out of #{@round.deck.cards.length} for a total score of #{@round.percent_correct}%."
+
+    uniq_categories = @round.deck.cards.map {|t| t.category}.uniq
+    uniq_categories.each do |category|
+      puts "#{category.to_s}: #{@round.percent_correct_by_category(category)} correct"
+    end 
+
+
   end
 
 end
 
 game = Game.new(round)
-game.welcome
 game.start
