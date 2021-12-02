2a487504d258eb63cc31ebcd4b7ea3d68da6a57b
Nick Cooley <cooley8706@gmail.com>
2021-12-02 16:28:45 -0700

    Create turn spec 2 tests passed

---
+++ spec/turn_spec.rb
@@ -0,0 +1,39 @@
+require 'pry'
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    #binding.pry
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.card).to eq(card)
+  end
+
+  xit 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  xit 'is correct?' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to eq(true)
+  end
+
+  xit 'can give feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+#   xit ''
+#   card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+end
