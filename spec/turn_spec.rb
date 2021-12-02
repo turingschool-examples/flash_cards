9e64df0598001c53fa4eb86e3de757f6b2081593
Jacob Jenner <jennerjacob@gmail.com>
2021-12-01 17:16:08 -0700

    Pass exists test in turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,6 +5,11 @@ require './lib/turn'
 RSpec.describe Turn do
   before(:each) do
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
   end
-  binding.pry
+
+  it 'exists' do
+    expect(@turn).to be_a(Turn)
+  end
+  # binding.pry
 end
