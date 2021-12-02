af17b319e341a47d11222aa1b15a9312fbf78cf4
Jesse Ochs <jessescottochs@gmail.com>
2021-12-02 14:02:30 -0700

    Pass test 2, deck can hold cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,10 @@
 
 class Deck
 
-  attr_reader :card
-  def initialize(card)
-    @card = []
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
   end
-
-  def cards(new_card)
-    @card << new_card
-  end
-
 
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,10 +3,20 @@ require './lib/deck'
 
 RSpec.describe Deck do
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)  
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     deck = Deck.new(card)
 
     expect(deck).to be_instance_of(Deck)
+  end
+
+  it 'can hold cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq(cards)
   end
 
 end
