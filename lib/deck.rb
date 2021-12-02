091d95561a776f3ce18179b3fd5f27ec6cdc5237
Mark Ertmer <mark.ertmer@gmail.com>
2021-12-01 18:26:29 -0700

    Add & test Deck.cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,12 @@ class Deck
   def count
     @cards.count
   end
+
+  def cards_in_category(cat)
+    count = 0
+    @cards.each do |card|
+      count += 1 if card.category == cat
+    end
+    return count
+  end
 end
