b857f6aaa3edf38ea34b2fef560f80998497d2fc
Michael Brandt <msbrandt00@gmail.com>
2021-12-02 14:10:27 -0700

    Add test for #percent_correct

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -87,11 +87,11 @@ RSpec.describe Round do
       deck = Deck.new(cards)
       round = Round.new(deck)
       round.take_turn('Juneau')
-      round.correct?
+      round.feedback
       round.take_turn('Venus')
-      round.correct?
+      round.feedback
       round.take_turn('North north west')
-      round.correct?
+      round.feedback
 
       expect(round.stats_hash).to include(Geography: [true], STEM: [false, true])
     end
@@ -108,7 +108,7 @@ RSpec.describe Round do
       deck = Deck.new(cards)
       round = Round.new(deck)
       round.take_turn('Juneau')
-      round.correct?
+      round.feedback
       expect(round.feedback).to eq('Correct, the answer is Juneau')
     end
   end
@@ -124,11 +124,11 @@ RSpec.describe Round do
       deck = Deck.new(cards)
       round = Round.new(deck)
       round.take_turn('Sacramento') #incorrect
-      round.correct?
+      round.feedback
       round.take_turn('Venus') #incorrect
-      round.correct?
+      round.feedback
       round.take_turn('North north west') #correct
-      round.correct?
+      round.feedback
       expect(round.number_correct_by_category(:STEM)).to equal(1)
       expect(round.number_correct_by_category(:Geography)).to equal(0)
     end
@@ -145,12 +145,26 @@ RSpec.describe Round do
       deck = Deck.new(cards)
       round = Round.new(deck)
       round.take_turn('Sacramento') #incorrect
-      round.correct?
+      round.feedback
       round.take_turn('Venus') #incorrect
-      round.correct?
+      round.feedback
       round.take_turn('North north west') #correct
-      round.correct?
+      round.feedback
       expect(round.percent_correct).to eq(33.33)
+    end
+    it 'will work for just one value' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
+        and reports about the surface of which planet?", "Mars", :STEM)
+
+      card_3 = Card.new("Describe in words the exact direction
+         that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards= [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      round.take_turn('Juneau') #incorrect
+      round.feedback
+      expect(round.percent_correct).to eq(100.0)
     end
   end
   describe '#percent_correct_by_category' do
@@ -164,13 +178,12 @@ RSpec.describe Round do
       cards= [card_1, card_2, card_3]
       deck = Deck.new(cards)
       round = Round.new(deck)
-      binding.pry
       round.take_turn('Sacramento') #incorrect
-      round.correct?
+      round.feedback
       round.take_turn('Venus') #incorrect
-      round.correct?
+      round.feedback
       round.take_turn('North north west') #correct
-      round.correct?
+      round.feedback
       expect(round.percent_correct_by_category(:STEM)).to eq(50.00)
       expect(round.percent_correct_by_category(:Geography)).to eq(0)
     end
