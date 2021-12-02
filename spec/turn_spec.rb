a65018d14f715a3106791f393e8e7d87ed1bedbd
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-01 21:26:23 -0500

    pass incorrect answer test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -27,9 +27,16 @@ RSpec.describe Turn do
     turn = Turn.new("Juneau", card)
     expect(turn.correct?).to eq(true)
   end
+
   it 'gives feedback when correct' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.feedback).to eq("Correct!")
+  end
+
+  it 'gives feedback when incorrect' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.feedback).to eq("Incorrect.")
   end
 end
