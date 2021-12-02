ddc4e7c95dc66baa5e0c1cde9262292df2f22f9f
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-01 20:48:42 -0500

    Create it has cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Deck do
 
     expect(deck).to be_an_instance_of(Deck)
   end
+
+  it 'has cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards.size).to be(3)
+  end
 end
