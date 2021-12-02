309b1c044d0fd50b92a83ecbf8128359e5c6ba92
Mark Ertmer <mark.ertmer@gmail.com>
2021-12-02 14:33:06 -0700

    Add & test Round.number_correct

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -82,7 +82,7 @@ it 'advances to the next card after each turn' do
   expect(round.current_card).to eq(card_3)
 end
 
-xit 'advances to the next card after each turn' do
+it 'counts the number of correct answers' do
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
