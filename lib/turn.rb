6ad69a11428d501dc4554652de1abbbad80979e8
Mark Ertmer <mark.ertmer@gmail.com>
2021-12-01 17:47:30 -0700

    Add & testTurn.feedback method

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,4 +9,12 @@ class Turn
   def correct?
     @card.answer == @guess
   end
+
+  def feedback
+    if @card.answer == @guess
+      return "Correct!"
+    else
+      return "Incorrect."
+    end
+  end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -52,6 +52,20 @@ RSpec.describe Turn do
     expect(turn.correct?).to be(true)
   end
 
+  it 'gives feedback for correct answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+  it 'gives feedback for incorrect answer' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.correct?).to be (false)
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+
+
 
 
 
