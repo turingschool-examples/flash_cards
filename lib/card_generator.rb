63370923014b3aff0060f0bf23ceba6242b8cf79
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-02 17:25:21 -0500

    Create block that opens file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,8 +1,17 @@
+
+
 class CardGenerator
   attr_accessor :filename, :cards
 
   def initialize(filename)
     @filename = filename
+    File.open("lib/#{filename}", "r") do |file|
+      puts file.readline()
+    end
     @cards = []
   end
 end
+
+
+#filename = "cards.txt"
+#generator = CardGenerator.new(filename)
