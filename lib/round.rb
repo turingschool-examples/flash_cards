ac10806156a2fc061aad0e6a045ed0c41ece1701
Mark Ertmer <mark.ertmer@gmail.com>
2021-12-02 14:13:15 -0700

    Add & test Round.number_correct

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,10 @@
 class Round
-  attr_reader :deck, :curr_card, :turns
+  attr_reader :deck, :curr_card, :turns, :number_correct
   def initialize(deck)
     @deck = deck
     @curr_card = 0
     @turns = []
+    @number_correct = 0
   end
 
   def current_card
@@ -12,7 +13,14 @@ class Round
 
   def take_turn(guess)
     turn = Turn.new(guess, self.current_card)
+    if guess = @deck.cards[@curr_card].answer
+      @number_correct += 1
+    else
+    end
     @turns << turn
+    @curr_card += 1
     return turn
   end
+
+
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -55,10 +55,18 @@ RSpec.describe Deck do
     expect(round.turns).to eq([new_turn, next_turn])
   end
 
-
-
-
-
+  it 'gives the number of correct answers' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    expect(round.number_correct).to eq(1)
+    next_turn = round.take_turn("Mars")
+    expect(round.number_correct).to eq(2)
+  end
 
 
 end
