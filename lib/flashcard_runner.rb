83f7a4c1b5245db3f0ab1abba137c89d1bec4b4e
Greg Flaherty <gregoryjflaherty@gmaul.com>
2021-12-02 15:34:14 -0500

    Create and instance for deck and round

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -7,3 +7,17 @@ card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 cards = [card_1, card_2, card_3]
+deck = Deck.new(cards)
+round = Round.new(deck)
+
+def start(round)
+  puts "Welcome! You're playing with #{round.deck.cards.length} cards."
+  puts "-" * 50
+  while round.turns.length < round.deck.cards.length
+    puts "This is card number #{round.turns.length + 1} out of #{round.deck.cards.length}."
+    puts "Question: #{round.current_card.question}"
+    guess = gets.chomp
+    round.take_turn(guess)
+    p round.turns.last.feedback
+  end
+end
