047066d5beeceea943250133e416eb1e7271dc72
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-01 19:56:49 -0700

    define cards_in_category to pass :STEM test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,5 @@
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :card_1, :card_2, :card_3
 
   def initialize(cards)
     @cards = cards
@@ -7,5 +7,10 @@ class Deck
 
   def count
     @cards.count
+  end
+
+  def cards_in_category(category)
+    cards.include? :STEM
+    return cards[1,2]
   end
 end
