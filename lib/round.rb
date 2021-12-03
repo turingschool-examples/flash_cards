8c9037a1d85a0ccc4b731bc7189e58200bcfbdf6
Jacob Jenner <jennerjacob@gmail.com>
2021-12-02 21:43:08 -0700

    Write beast of a turn test and wrote take_turn and number_correct method

--- lib/round.rb
+++ lib/round.rb
@@ -2,12 +2,26 @@ class Round
   attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
-    @turns = Array.new
+    @turns = []
+    @correct_turns = []
   end
 
   def current_card
     @deck.cards.first
   end
 
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    if guess == current_card.answer
+      @correct_turns << new_turn
+    end
+    # binding.pry
+    @deck.cards.rotate!
+    return new_turn
+  end
 
+  def number_correct
+    @correct_turns.length
+  end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -13,10 +13,7 @@ RSpec.describe Round do
     @cards = [@card_1, @card_2, @card_3]
     @deck = Deck.new(@cards)
     @round = Round.new(@deck)
-  end
-
-  it "exists" do
-    expect(@round).to be_instance_of(Round)
+    @new_turn = Turn.new("Juneau", @card_1)
   end
 
   it "has a deck" do
@@ -24,13 +21,25 @@ RSpec.describe Round do
   end
 
   it "starts with no turns" do
-    expect(@round.turns).to be_instance_of(Array)
+    expect(@round.turns).to eq([])
   end
 
   it "gets first card in deck" do
-    expect(@round.current_card).to eq(@deck.cards[0])
+    expect(@round.current_card).to eq(@card_1)
+  end
+
+  it "takes a turn, and prepares a new turn" do
+    expect(@new_turn).to be_a(Turn)
+    new_turn = @round.take_turn("Juneau")
+    expect(@new_turn.correct?).to be(true)
+    expect(@round.turns).to eq([new_turn])
+    expect(@round.number_correct).to eq(1)
+    expect(@round.current_card).to eq(@card_2)
+    @round.take_turn("Venus")
+    expect(@round.turns.count).to eq(2)
+    expect(@round.turns.last.feedback).to eq('Incorrect.')
+    expect(@round.number_correct).to eq(1)
   end
 
   
-
 end
