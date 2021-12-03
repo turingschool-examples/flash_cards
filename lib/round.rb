7f29c0c6d45e1f614f66ec5d2f30a00aadfdadb6
Joseph Galvin <jwgalvin@gmail.com>
2021-12-03 12:25:14 -0700

    fix test round.number_correct and pass

--- lib/round.rb
+++ lib/round.rb
@@ -6,30 +6,42 @@ class Round
     @turns = []
     @number_correct = 0
     @number_wrong = 0
-
   end
 
   def current_card
     @current_card = deck.cards[0]
-    # @deck.cards[0]
   end
 
   def take_turn(guess)
+
     @turns = Turn.new(guess, current_card)
-
-    # if @current_card.answer == guess
-    #   @number_correct += 1
-    # else
-    #   @number_wrong += 1
-    # end
-
+    if turns.correct?
+      @number_correct += 1
+    else
+      @number_wrong += 1
+    end
     #@deck.insert(2, current_card.delete_at(0))
+    @turns
   end
 
   def turns
     @turns
   end
+
   #binding.pry
 
+  def number_correct
+    # if turns.correct?
+    #   @number_correct += 1
+    # else
+    #   @number_wrong += 1
+    # end
+    @number_correct
+
+  end
+
+  def number_wrong
+    @number_wrong
+  end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe Deck do
 
   describe "Test Round stuff_2" do
     it "current_card" do
+
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -28,8 +29,11 @@ RSpec.describe Deck do
       round = Round.new(deck)
       new_turn = round.take_turn("Juneau")
       expect(round.turns).to eq(new_turn)
+      #new_turn = round.take_turn("Juneau")
+      #binding.pry
+
+      expect(round.number_correct).to be(1)
       # expect(new_turn.current_card.correct?).to be true
-      #expect(new_turn.number_correct).to eq(1)
       #expect(round.current_card).to be(card_1)
       #expect(new_turn).to be_instance_of(Turn)
     end
