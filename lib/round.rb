2560b4c909bb62942d44ff53651255c42afd422c
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-02 21:15:14 -0700

    iteration 2 - pass round turns creates empty array

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,12 @@
 class Round
+  attr_reader :deck
+
   def initialize(round_deck)
     @deck = round_deck
   end
+
+  def turns
+    turns_array = []
+  end
+
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -22,4 +22,22 @@ RSpec.describe Deck do
       expect(round).to be_a Round
   end
 
+  it 'deck method yeilds Deck object' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+      expect(round.deck).to be_a Deck
+  end
+
+  it 'deck method yeilds Deck object' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+      expect(round.turns).to eq([])
+  end
+
 end
