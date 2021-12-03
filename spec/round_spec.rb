9311f03fee635615330243a96e2d30946274e52f
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-02 17:24:04 -0700

    create has turns test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
@@ -13,4 +14,24 @@ RSpec.describe Round do
     round = Round.new(deck)
     expect(round).to be_instance_of(Round)
   end
-end
+
+  it 'Has a deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = cards
+    round = Round.new(deck)
+    expect(round.deck).to eq(deck)
+  end
+
+  it 'Has turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = cards
+    round = Round.new(deck)
+    expect(round.turns).to eq []
+  end
+end 
