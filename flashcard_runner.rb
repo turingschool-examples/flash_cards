853cea071c67eece36f9d6dce917aee69d40108a
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-12-02 21:27:55 -0500

    Add deck and round objects

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -9,3 +9,6 @@ card_2 = Card.new("Question: 1 + 2 = ?", 3, :Math)
 card_3 = Card.new("Question: What food do I like to eat now?", "Humus", :Food)
 
 cards = [card_1, card_2, card_3]
+
+deck = Deck.new(cards)
+round = Round.new(deck)
