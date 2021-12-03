933112e0f866b8a5d92ea03cc21f60234af0d98b
Chris_K <ckjolhede@gmail.com>
2021-12-02 20:27:04 -0700

    All Deck tests working

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -52,8 +52,28 @@ require 'pry'
       card_array = [card_1, card_2, card_3]
       deck = Deck.new(card_array)
 
-      # binding.pry
       expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
     end
+
+    it 'idenitfy cards with categories' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      card_array = [card_1, card_2, card_3]
+      deck = Deck.new(card_array)
+
+      expect(deck.cards_in_category(:Geography)).to eq([card_1])
+    end
+
+
+        it 'idenitfy cards with categories' do
+          card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+          card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+          card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+          card_array = [card_1, card_2, card_3]
+          deck = Deck.new(card_array)
+
+          expect(deck.cards_in_category("Pop Culture")).to eq([])
+        end
 
   end
