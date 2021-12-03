59917d8ce3a5bd5f11a59d1dfb4d3eb4de94606f
Ian Ross <ianrossdrums@gmail.com>
2021-12-03 09:59:49 -0700

    Add Turn Class

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+  end
+
+#   def correct?(guess, card)
+#     if guess == card
+#       return true
+#     else
+#       return false
+#   end
+#
+#   def feedback(guess, card)
+#     if guess == true
+#       return "Correct!"
+#     else
+#       return "Incorrect."
+#   end
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,11 @@
+require './lib/turn'
+require './lib/card'
+
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("How many ferrets does Ian have?", "3", :Myself)
+    turn = Turn.new("3", card)
+    expect(turn).to be_instance_of(Turn)
+  end
+end
