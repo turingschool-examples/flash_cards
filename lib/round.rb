fb475d7a05e628b909638426b74bb1718fec5de3
Cole Bradley <cole.bradley303@gmail.com>
2021-12-03 12:57:25 -0700

    Add #number_correct method

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,6 @@
+
 class Round
-attr_reader :deck, :turns
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
@@ -8,5 +9,17 @@ attr_reader :deck, :turns
 
   def current_card
     deck.cards.first
+  end
+
+  def take_turn(guess)
+    x = Turn.new(guess, deck.cards.shift)
+    @turns << x
+    turns.last
+  end
+
+  def number_correct
+    turns.count do |turn|
+      turn.correct?
+    end
   end
 end
