74185041109b1bbe56a506bfa7a1701853892fa1
Richard LaBrecque <RichardlaBreque@Gmail.com>
2021-12-03 12:54:39 -0500

    round stores new turn

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
@@ -22,5 +23,48 @@ RSpec.describe Round do
     deck = Deck.new(cards)
     round = Round.new(deck)
     expect(round.deck).to eq(deck)
+  end
+
+  it 'has empty array for turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.turns).to eq([])
+  end
+
+  it 'tracks starting card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.current_card).to eq(card_1)
+  end
+
+  it 'takes a turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(new_turn = round.take_turn("Juneau")).to be_instance_of (Turn)
+    #binding.pry
+
+  end
+  xit 'tracks card and answer' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    binding.pry
+    expect(round.turns).to eq(new_turn)
   end
 end
