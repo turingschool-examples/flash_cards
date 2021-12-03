1acf89dd182f514aabeca53a6c6f02838aa03a86
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-02 21:11:58 -0700

    iteration 2 - pass round exists

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -5,11 +5,21 @@ require './lib/round'
 require 'pry'
 
 RSpec.describe Deck do
-  it 'deck variable exists with 3 cards' do
+  it 'deck variable exists' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
       deck = Deck.new([card_1, card_2, card_3])
       expect(deck).to be_a Deck
   end
+
+  it 'round variable exists' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+      expect(round).to be_a Round
+  end
+
 end
