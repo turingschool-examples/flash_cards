5aec1727bfee706500ff15a1d591273aea6f6c62
Sulaiman Birashk <sully.birashk@gmail.com>
2021-12-02 20:19:28 -0700

    Finish Cards

--- lib/card.rb
+++ lib/card.rb
@@ -1,15 +1,21 @@
+# require 'pry'
+
 class Card
+   attr_reader :fc_question
+                :fc_answer
+                :fc_category
 
- def initialize(question, answer, category)
-  @question = question
-  @answer = answer
-  @category = category
+ def initialize(fc_question, fc_answer, fc_category)
+  @question = fc_question
+  @answer = fc_answer
+  @category = fc_category
  end
-
+#binding.pry
  def question
+   #binding.pry
     return @question
  end
-
+# binding.pry
  def answer
   return @answer
 end
@@ -17,7 +23,5 @@ end
 def category
   return @category
 end
-
-
 
 end
