3a681b7661ee8064c829d375782f38f05215e15e
Steven James <StevenJamesSTL@gmail.com>
2021-12-03 11:20:45 -0600

    Add test for round#number_correct_by_category

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -98,14 +98,18 @@ RSpec.describe Round do
     expect(round.turns.last.feedback).to eq("Incorrect.")
   end
 
+  it 'evaluates accuracy per category' do
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  deck = Deck.new([card_1, card_2, card_3])
+  round = Round.new(deck)
+  new_turn = round.take_turn("Juneau")
 
-
-
-
-
-
-
-
+  expect(round.number_correct).to eq(1)
+  expect(round.number_correct_by_category(:Geography)).to eq(1)
+  expect(round.number_correct_by_category(:STEM)).to eq(0)
+end
 
 
 end
