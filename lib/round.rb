3f90cf80bac855f6993804cf3e78a6c59615b2a3
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-02 17:59:57 -0700

    define current_card class

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,5 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :cards
 
   def initialize(deck)
     @deck = deck
@@ -11,5 +11,11 @@ class Round
 
   def turns
     return []
+  end
+
+  def current_card
+    @deck.select do |card|
+      return [card] 
+    end
   end
 end
