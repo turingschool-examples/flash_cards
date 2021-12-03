5868f5edb79499ef35fd37a21b5597992f2afc9c
Tyler Ross <tross020890@gmail.com>
2021-12-03 00:33:20 -0600

    Iteration 1 complete

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,4 +5,16 @@ class Turn
     @card = card
   end
 
+  def correct?
+    guess==card.answer
+  end
+
+  def feedback
+    if guess==card.answer
+      return "Correct!"
+    else
+      return "Incorrect"  
+    end
+  end
+
 end
