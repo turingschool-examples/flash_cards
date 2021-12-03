4e42db7fd5a032b1d1d18160a2db60cc8fc6770d
Steven James <StevenJamesSTL@gmail.com>
2021-12-02 18:26:40 -0600

    Write #take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -1,15 +1,28 @@
+require './lib/turn'
+require './lib/card'
+require './lib/deck'
+
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :correct_guesses
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @correct_guesses = []
   end
 
   def current_card
     @deck.cards.first
-
   end
+
+  def take_turn(guess)
+    @turns << current_card
+    Turn.new(guess, current_card)
+  end
+
+  # def number_correct
+  #   @correct_guesses.size
+  # end
 
 
 end
