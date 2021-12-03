a9cc8b343683d46ffd34edcdb5175a8a8b86a857
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-02 17:00:11 -0700

    Round contains empty turns array

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -28,6 +28,18 @@ RSpec.describe Round do
     expect(round.deck).to be deck
     expect(round.deck.cards.first).to be_instance_of Card
     expect(round.deck.cards.last).to be_instance_of Card
+  end
 
+  it 'keeps track of previous turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.turns).to be_instance_of Array
+    expect(round.turns).to be_empty
   end
 end
