64568e63110e49e162fee6f2edba27a5281241f2
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-12-02 21:26:50 -0500

    Add cards to an array

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,6 +1,11 @@
 require '../lib/card'
-
+require '../lib/turn'
+require '../lib/deck'
+require '../lib/round'
+require 'pry'
 
 card_1 = Card.new("Question: 1 + 1 = ?", 2, :Math)
 card_2 = Card.new("Question: 1 + 2 = ?", 3, :Math)
 card_3 = Card.new("Question: What food do I like to eat now?", "Humus", :Food)
+
+cards = [card_1, card_2, card_3]
