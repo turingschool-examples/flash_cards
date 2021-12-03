42711bd4c06ffa41855dd8788c3c27ab8c53672b
Cole Bradley <cole.bradley303@gmail.com>
2021-12-03 09:33:59 -0700

    Test for return of a turn object

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -34,13 +34,27 @@ RSpec.describe Card do
     expect(round.current_card).to eq(card_1)
   end
 
+  it 'returns a turn object' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
 
+    expect(new_turn).to be_instance_of(Turn)
+  end
 
-
-
-
-
-
+  it 'returns an array of turn objects' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+  
+    expect(round.turns).to eq([new_turn])
+  end
 
 
 
