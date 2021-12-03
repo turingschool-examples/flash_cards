ce0c58a29c251b315e9753cd05cc53c2aa451f46
oscar santos <oscar303data@gmail.com>
2021-12-03 07:44:29 -0700

    Assertion of it has a feedback block is created

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -20,4 +20,10 @@ RSpec.describe Turn do
     turn = Turn.new("Juneau", card)
     expect(turn.correct?).to eq(true)
   end
+
+  it 'has a feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
 end
