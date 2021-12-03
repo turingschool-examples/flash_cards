2522057449bb88b8707c548a19382fa8a9131253
Ian Ross <ianrossdrums@gmail.com>
2021-12-03 10:27:50 -0700

    Add the correct? method

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,17 +4,18 @@ class Turn
   def initialize(guess, card)
   end
 
-#   def correct?(guess, card)
-#     if guess == card
-#       return true
-#     else
-#       return false
-#   end
-#
-#   def feedback(guess, card)
-#     if guess == true
-#       return "Correct!"
-#     else
-#       return "Incorrect."
-#   end
+  def correct?
+    if guess == card
+      return true
+    else
+      return false
+    end
+  end
+
+  # def feedback
+  #   if guess == true
+  #     return "Correct!"
+  #   else
+  #     return "Incorrect."
+  # end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Turn do
     turn = Turn.new("3", card)
     expect(turn).to be_instance_of(Turn)
   end
+
+  it "is correct?" do
+    card = Card.new("How many ferrets does Ian have?", "3", :Myself)
+    turn = Turn.new("3", card)
+    expect(turn.correct?)
+  end
 end
