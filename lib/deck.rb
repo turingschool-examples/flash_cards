ef709f161ccdc6dc496da4511fee612be8457d18
Chris_K <ckjolhede@gmail.com>
2021-12-02 20:23:04 -0700

    parse cards by category working

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,7 @@
 require './lib/card'
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :cat_card_array
 
   def initialize(cards)
     @cards = cards
@@ -11,6 +11,15 @@ class Deck
     cards.count
   end
 
-  # def cards_in_category(category_name)
-  # end
+  def cards_in_category(cat)
+    cat_card_array = []
+    cards.each do |card_number|
+      if card_number.category == cat
+        cat_card_array << card_number
+      end
+
+    end
+    cat_card_array
+
+  end
 end
