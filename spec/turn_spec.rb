ef5db0c81da84a9c442914abdee21ac61488da67
Tyler Ross <tross020890@gmail.com>
2021-12-03 00:22:58 -0600

    Pass is correct, has feedback

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -24,6 +24,19 @@ RSpec.describe Turn do
     expect(turn.card).to eq(card)
   end
 
+  it 'is correct' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'has feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
 
 
 
