6b62cfb4c49c4ebe5c4d539252f481764a61f291
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-03 08:59:24 -0700

    Turn returns false if answer is incorrect

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -81,6 +81,20 @@ RSpec.describe Round do
     expect(new_turn.correct?).to be true
   end
 
+  it 'returns false if answer is incorrect' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Rome")
+
+    expect(new_turn.correct?).to be false
+  end
+
   it 'keeps track of previous turns' do
   end
 
