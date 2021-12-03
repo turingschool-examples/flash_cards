e8bbebef7b1dfb72bcc83057d5dc4b2c73a3b3e3
Tyler Ross <tross020890@gmail.com>
2021-12-03 00:19:41 -0600

    Pass it exists, has a guess, has card

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,18 +1,30 @@
 require './lib/card'
 require './lib/turn'
 
-RSpec.describe Card do
+RSpec.describe Turn do
+
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card).to be_instance_of(Card)
-  end
-end
-
-RSpec.describe Turn do
-  it 'exists' do
     turn = Turn.new("Juneau", card)
 
     expect(turn).to be_instance_of(Turn)
   end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'has card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+
+
+
 end
