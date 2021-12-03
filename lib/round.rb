fe9ae7e0dc3e7476acaf387223a80b9a0a1743ba
Conor Barthel <conor.barthel@gmail.com>
2021-12-03 09:42:19 -0700

    Add #number correct method and test

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,12 @@
 #~/round.rb
 class Round
-  attr_reader :deck, :turns, :count
+  attr_reader :deck, :turns, :count, :correct_tally
 
   def initialize(deck)
     @deck = deck
     @turns = []
     @count = 1
+    @correct_tally = 0
   end
 
   def current_card
@@ -14,8 +15,16 @@ class Round
 
   def take_turn(guess)
     @turns << Turn.new(guess, current_card)
+    number_correct
     @count += 1
     @turns.last
   end
 
+  def number_correct
+    if @turns.last.correct? == true
+      @correct_tally += 1
+    end
+  end
+
+  
 end
