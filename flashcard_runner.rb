3ad5423dea22c18c6e7ced6e50ec89517e9257ef
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-12-03 12:28:10 -0500

    Add Play_Game Class

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,24 +1,29 @@
-require './lib/card'
-require './lib/turn'
-require './lib/deck'
-require './lib/round'
-require 'pry'
+  require './lib/card'
+  require './lib/turn'
+  require './lib/deck'
+  require './lib/round'
+  require 'pry'
+
+class Play_Game
+
+    def initialize
+      @card_1 = Card.new("Question: 1 + 1 = ?", "2", :Math)
+      @card_2 = Card.new("Question: 1 + 2 = ?", "3", :Math)
+      @card_3 = Card.new("Question: What food do I like to eat now?", "Humus", :Food)
+      @card_4 = Card.new("Question: What other food do I like to eat now?", "Grapes", :Food)
+      @cards = [@card_1, @card_2, @card_3, @card_4]
+      @deck = Deck.new(@cards)
+      @round = Round.new(@deck)
+      @round_number = 0
+    end
+
+  def start
+    puts "Welcome! You're playing with #{@deck.count} cards."
+    50.times {print "-"}
+  end
 
 
-
-def start
-  card_1 = Card.new("Question: 1 + 1 = ?", 2, :Math)
-  card_2 = Card.new("Question: 1 + 2 = ?", 3, :Math)
-  card_3 = Card.new("Question: What food do I like to eat now?", "Humus", :Food)
-  card_4 = Card.new("Question: What other food do I like to eat now?", "Grapes", :Food)
-  cards = [card_1, card_2, card_3, card_4]
-
-  deck = Deck.new(cards)
-  round = Round.new(deck)
-  puts "Welcome! You're playing with 4 cards."
-  50.times {print "-"}
-  puts "\nThis is card number #{cards.index(card_1) + 1} out of #{cards.size} "
-  puts card_1.question
 end
 
-start
+  game = Play_Game.new
+  game.start
