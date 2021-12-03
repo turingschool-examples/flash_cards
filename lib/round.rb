b718a32eeec2645a52b3cce381cdf1ed572b36ac
Jakob Edelstein <jakobedelstein@gmail.com>
2021-12-02 17:03:36 -0700

    Round returns first card in deck.cards array

--- lib/round.rb
+++ lib/round.rb
@@ -5,4 +5,8 @@ class Round
     @deck = deck
     @turns = []
   end
+
+  def current_card
+    deck.cards.first
+  end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -42,4 +42,16 @@ RSpec.describe Round do
     expect(round.turns).to be_instance_of Array
     expect(round.turns).to be_empty
   end
+
+  it 'can return the current card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.current_card).to be card_1
+  end
 end
