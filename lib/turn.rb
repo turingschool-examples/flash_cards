c2aed9510ab1a2ee2275f9ed2ff1b457afba3223
Ian Ross <ianrossdrums@gmail.com>
2021-12-03 10:58:58 -0700

    Fix initialize method

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,8 @@ class Turn
   attr_reader :guess, :card
 
   def initialize(guess, card)
+    @guess = guess
+    @card = card
   end
 
   def correct?
@@ -12,10 +14,13 @@ class Turn
     end
   end
 
-  # def feedback
-  #   if guess == true
-  #     return "Correct!"
-  #   else
-  #     return "Incorrect."
-  # end
+  def feedback
+    if guess == true
+      return "Correct!"
+    elsif guess == false
+      return "Incorrect."
+    else
+      return "Invalid Input"
+    end
+  end
 end
