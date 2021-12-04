3582d450c9704bca2ea1a73116da2005060be5b5
Tamara Dowis <tamara.dowis@gmail.com>
2021-12-04 11:00:38 -0600

    Round will calculate the percent correct by category

--- lib/round.rb
+++ lib/round.rb
@@ -18,12 +18,6 @@ class Round
   def take_turn(guess)
     x = Turn.new(guess, current_card)
     x.correct? ? @number_correct += 1 : @number_wrong += 1
-    # puts x.card.category
-    # number_turns_in_catagory = []
-    # turns.each do |turn|
-    #   number_turns_in_catagory << turn if turn.category == x.card.category
-    # end
-    # if x.correct?
     @turns << x
     @current_card = deck.cards[+1]
     @turns.last
@@ -38,13 +32,14 @@ class Round
       category == turn.card.category && turn.guess == turn.card.answer
     end
   end
-end
 
-# def take_turn(guess)
-#   @turns << Turn.new(guess, current_card)
-#   if turns.correct?
-#     @number_correct += 1
-#   elsif @number_wrong += 1
-#   end
-#   @turns.last
-# end
+  def percent_correct_by_category(category)
+    turns_correct_by_category = turns.count do |turn|
+      category == turn.card.category && turn.guess == turn.card.answer
+    end
+    total_turns_by_category = turns.count do |turn|
+      category == turn.card.category
+    end
+    (turns_correct_by_category / total_turns_by_category.to_f) * 100
+  end
+end
