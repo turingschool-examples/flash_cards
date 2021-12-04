5f46284972992614f524510139f30114dc74e402
Conor Barthel <conor.barthel@gmail.com>
2021-12-04 12:28:14 -0700

    Game welcome and round run

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -5,7 +5,7 @@ require './lib/deck'
 require './lib/round'
 def start
   card_1 = Card.new("What is 5 + 5?", "10", :STEM)
-  card_2 = Card.new("What is Rachel's favorite animal?", "panda", "Turing Staff")
+  card_2 = Card.new("What is Rachel's favorite animal?", "Bonobo", "Turing Staff")
   card_3 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff")
   card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
   cards = [card_1, card_2, card_3, card_4]
@@ -14,7 +14,14 @@ def start
 
   puts "Welcome! You're playing with #{deck.count} cards."
   puts "-------------------------------------------------"
-  puts "This is card number #{round.count} of #{deck.count}"
-  puts "#{deck.cards[0].question}"
+  while round.count <= deck.count do
+    puts "This is card number #{round.count} of #{deck.count}"
+    puts "#{round.current_card.question}"
+    guess = gets.chomp
+    round.take_turn(guess)
+    puts "#{round.turns.last.card.answer}"
+    round.turns.last.feedback
+    
+  end
 end
 start()
