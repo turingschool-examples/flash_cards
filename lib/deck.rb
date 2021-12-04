c1fbdffe0308da95075aea8bb4db412d49daa7ed
Nick Cooley <cooley8706@gmail.com>
2021-12-04 14:48:58 -0700

    Define category array method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,14 +1,20 @@
 class Deck
-attr_reader :cards
+  attr_reader :cards
   def initialize(cards)
     @cards = cards
-
   end
-  # def cards_in_category
-  # cards.each do |card|
-  # end
 
   def count
     return 3
   end
+
+  def cards_in_category(cat)
+    categories = []
+    cards.each do |card|
+    if cat == card.category
+      categories << card
+    end
+  end
+  return categories
+end
 end
