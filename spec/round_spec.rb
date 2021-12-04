75e2c5109b92f0fd95194a5d7422676caaa4ce30
Joseph Galvin <jwgalvin@gmail.com>
2021-12-04 12:59:36 -0700

    turn 2 to count pass

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -113,17 +113,17 @@ RSpec.describe Deck do
       expect(round.take_turn("Venus")).to be_instance_of(Turn)
     end
 
-    # it "card_1 test_2 turn_class" do
-    #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    #   cards = [card_1, card_2, card_3]
-    #   deck = Deck.new(cards)
-    #   round = Round.new(deck)
-    #   new_turn = round.take_turn("Juneau")
-    #   new_turn = round.take_turn("Venus")
-    #   expect(round.turns.count).to eq(2)
-    # end
+    it "card_1 test_2 turn_class" do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      new_turn = round.take_turn("Juneau")
+      new_turn = round.take_turn("Venus")
+      expect(round.turns.count).to eq(2)
+    end
 
     # it "card_1 test_2 turn_class" do
     #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
