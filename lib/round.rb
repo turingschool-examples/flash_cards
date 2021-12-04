b05b8494b297dd9ac7ace845e33400f6dee22037
Jesse Ochs <jessescottochs@gmail.com>
2021-12-03 19:42:18 -0700

    Move to next card after each guess

--- lib/round.rb
+++ lib/round.rb
@@ -1,23 +1,22 @@
 
 class Round
 
-  attr_reader :deck, :turns, :number_correct, :number_wrong
+  attr_reader :deck, :turns, :current_card, :number_correct, :number_wrong
   def initialize(deck)
     @deck = deck
     @turns = []
+    @current_card = deck.cards[0]
     @number_correct = 0
     @number_wrong = 0
   end
 
-  def current_card
-    return deck.cards[0]
-  end
 
   def take_turn(guess)
       turn = Turn.new(guess, current_card)
       @turns << turn
       turn.correct? ? @number_correct += 1 : @number_wrong += 1
-    
+      @current_card = deck.cards[+1]
+
     return turn
   end
 
