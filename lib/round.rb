93a66f1f045a9592e40addb789d3d534a6622a42
Jesse Ochs <jessescottochs@gmail.com>
2021-12-03 19:32:16 -0700

    Add correct and wrong counter to round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,10 +1,12 @@
 
 class Round
 
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct, :number_wrong
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
+    @number_wrong = 0
   end
 
   def current_card
@@ -12,7 +14,11 @@ class Round
   end
 
   def take_turn(guess)
-    return turn = Turn.new(guess, current_card)
+      turn = Turn.new(guess, current_card)
+      @turns << turn
+      turn.correct? ? @number_correct += 1 : @number_wrong += 1
+    
+    return turn
   end
 
 
