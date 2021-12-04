032b22343cc20dc07d98699261844a4bd64d4784
Jesse Ochs <jessescottochs@gmail.com>
2021-12-03 20:01:20 -0700

    Pass test 9, move onto next card and check guess

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -81,5 +81,16 @@ RSpec.describe Round do
     expect(round.current_card).to be(card_2)
   end
 
+  it 'can move to the next card and check guess' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    new_turn = round.take_turn("Venus")
+    expect(new_turn.feedback).to eq('Incorrect.')
+  end
+
 
 end
