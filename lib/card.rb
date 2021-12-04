fa6ba01a66631fccd2f475d1e811ef786a82e555
Luke Pascale <lpluke5@gmail.com>
2021-12-04 15:34:18 -0700

    Initial commit. Add Card class to card.rb file.

---
+++ lib/card.rb
@@ -0,0 +1,19 @@
+class Card
+  def initialize(trivia, answer, category)
+    @trivia = trivia
+    @answer = answer
+    @category = category
+  end
+
+  def trivia
+    @trivia
+  end
+
+  def answer
+    @answer
+  end
+
+  def category
+    @category
+  end
+end
