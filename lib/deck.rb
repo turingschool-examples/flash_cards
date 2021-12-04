2764850082109a804bdb78b1137a1eb1f8d130f3
Austin Moore <austinchristianmoore@gmail.com>
2021-12-03 21:56:57 -0500

    Pass Deck categories tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,14 @@ attr_reader :cards
     cards.count
   end
 
+  def cards_in_category(category)
+    collector = []
+
+    @cards.each do |card|
+      if card.category == category
+        collector << card
+      end
+    end
+    return collector
+  end
 end
