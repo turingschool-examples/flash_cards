eeab89a90c8895721cd5a2c0096672dffb813176
Ian Ross <ianrossdrums@gmail.com>
2021-12-04 14:02:32 -0700

    Add Deck class

---
+++ spec/deck_spec.rb
@@ -0,0 +1,13 @@
+require './lib/card'
+require './lib/deck'
+
+describe Deck do
+  it 'exists' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck).to be_instance_of(Deck)
+  end
+end
