23be2cad32c148c1ecde605f6feb8832291eced2
glsorensen <gunnar.l.sorensen@gmail.com>
2021-12-04 09:19:10 -0700

    Completed Round interaction pattern through round.turns

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -9,13 +9,22 @@ RSpec.describe Round do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
+    deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     expect(card_1).to be_instance_of(Card)
     expect(card_2).to be_instance_of(Card)
     expect(card_3).to be_instance_of(Card)
   end
+
+  it 'has rounds' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    expect(round).to be_instance_of(Round)
+  end
+# binding.pry
 
   it 'has turns' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -26,14 +35,5 @@ RSpec.describe Round do
     round = Round.new(deck)
     expect(round.turns).to eq([])
   end
-  
-  it 'has turns' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    round = Round.new(deck)
-    expect(round.turns).to eq([])
-  end
 end
+# require 'pry'; binding.pry
