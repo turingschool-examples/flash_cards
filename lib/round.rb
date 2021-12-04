4345cb652d697abd970e3a0a4ba59d18c2cfaa63
glsorensen <gunnar.l.sorensen@gmail.com>
2021-12-04 15:46:59 -0700

    Completed Round interaction pattern through" round.number_correct to eq 1"

--- lib/round.rb
+++ lib/round.rb
@@ -3,10 +3,11 @@ class Round
 
   def initialize(deck)
     @round = round
-    @deck = []
+    @deck = deck
     @turns = []
     @current_card = deck.cards[0]
     # @deck = deck
+    #require 'pry'; binding.pry
   end
 
 
@@ -15,7 +16,21 @@ class Round
   # end
 
   def take_turn(guess)
-   @turns << Turn.new(guess, current_card)
+    x = Turn.new(guess, current_card)
+   @turns << x
    @turns.last
+  end
+  # def turns
+  #
+  # end
+#can be done in take turn class
+  def number_correct
+    correct_guesses = 0
+    @turns.each do |turn|
+      if turn.correct?
+        correct_guesses += 1
+      end
+    end
+    return correct_guesses
   end
 end
