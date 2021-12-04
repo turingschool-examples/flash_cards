02bbc93ad6d9ee1b684bbec15f0dc6679b8884e6
Nick Cooley <cooley8706@gmail.com>
2021-12-04 14:49:24 -0700

    Pass categories array test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,7 +3,7 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
-  it 'exists' do
+  xit 'exists' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -28,12 +28,14 @@ RSpec.describe Deck do
     expect(deck.count).to eq(3)
   end
 
-  xit 'has cards in these categories' do
+  it 'has cards in these categories' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new ([card_1, card_2, card_3])
-    expect(deck.cards_in_category). to eq(":STEM, :Geography, 'Pop Culture'")
+    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+    expect(deck.cards_in_category(:Geography)).to eq([card_1])
+    expect(deck.cards_in_category(:PopCulture)).to eq([])
   end
 
 
