8880f9a42776a302a75924f94fa465220057ebfa
Luke Pascale <lpluke5@gmail.com>
2021-12-05 16:17:39 -0700

    Add rspec testing for count and feedback.

--- lib/round.rb
+++ lib/round.rb
@@ -1,24 +1,33 @@
 class Round
 attr_reader :deck,
-            :turns
+            :turns,
+            :number_correct,
+            :current_card,
+            :count
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
+    @current_card = deck.cards[0]
+    @count = 0
   end
 
   def current_card
-  #  @current_card = current_card
-    deck.cards.first
+    @current_card = deck.cards[@count]
   end
 
 #  def take_turn(guess)
 #    @take_turn(guess)
 #  end
   def take_turn(guess)
-    turn = Turn.new(guess, deck.cards.shift)
+    turn = Turn.new(guess, current_card)
     @turns << turn
-    return turn 
+    if turn.correct?
+      @number_correct += 1
+    end
+    @count += 1
+    @turns.last
   #  made_guesses = []
   #  @guess = guess
   #  @round.each do |round|
@@ -26,5 +35,17 @@ attr_reader :deck,
   #    end
   #  end
   #  return made_guesses
+  end
+
+  #def number_correct
+
+#    correct_guess = []
+#    if take_turn(guess) == card.answer
+#      correct_guess + 1
+#    end
+#    return correct_guess
+  #end
+  def percent_correct
+    ((number_correct * 100) / turns.length).to_f
   end
 end
