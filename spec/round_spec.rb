39fba19dc1bbbee3698df277fb9485dccdde82fa
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-04 19:01:15 -0700

    pass can return the percent of correct turns from given category

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -122,6 +122,7 @@ RSpec.describe Round do
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+    # new_turn = round.take_turn("Juneau")
     round.take_turn("Juneau")
     expect(round.current_card).to eq(card_2)
   end
@@ -133,20 +134,21 @@ RSpec.describe Round do
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
+    # new_turn = round.take_turn("Juneau")
     round.take_turn("Juneau")
     round.take_turn("Venus")
     expect(round.percent_correct).to eq(50.0)
   end
 
-  xit 'Can return the percent of correct turns from a given category' do
+  it 'Can return the percent of correct turns from a given category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
-    round.take_turn("Juneau")
+    new_turn = round.take_turn("Juneau")
+    round.number_correct_by_category(:Geography)
     expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
   end
-
 end
