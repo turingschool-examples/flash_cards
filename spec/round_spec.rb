300e9461309e7d606715607148f4315841975960
Jimmy Page <jpage@knights.ucf.edu>
2021-12-05 14:39:39 -0700

    created tests for round class

---
+++ spec/round_spec.rb
@@ -0,0 +1,61 @@
+require 'rspec'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+RSpec.describe Round do
+  before :each do
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @round = Round.new(@deck)
+  end
+
+  it 'exists' do
+    expect(@round).to be_instance_of(Round)
+  end
+
+  it 'shows the current card' do
+    expect(@round.current_card). to eq(@card_1)
+  end
+
+  it 'has a deck' do
+    expect(@round.deck).to be_instance_of(Deck)
+  end
+
+  it 'has turns' do
+    expect(@round.turns).to eq([])
+  end
+
+  it 'shows the number of correct answers' do
+    @new_turn = @round.take_turn("Juneau")
+
+    expect(@round.number_correct). to eq(1)
+    expect(@new_turn.class).to eq(Turn)
+    expect(@new_turn.correct?).to eq(true)
+  end
+
+  xit 'shows the number of correct answers by category' do
+    @round.take_turn("Venus")
+    @new_turn = @round.take_turn("Juneau")
+
+    expect(@round.number_correct_by_category(:STEM)).to eq(0)
+    expect(@round.number_correct_by_category(:Geography)).to eq(1)
+  end
+
+  xit 'shows the percent correct' do
+    @round.take_turn("Venus")
+    @new_turn = @round.take_turn("Juneau")
+
+    expect(@round.percent_correct).to eq(50.0)
+  end
+
+  xit 'shows the percent correct by category' do
+    @round.take_turn("Venus")
+    @new_turn = @round.take_turn("Juneau")
+
+    expect(@round.percent_correct_by_category(:Geography)).to eq(100.0)
+  end
+end
