9784fff75942b4dd47a7f1936012023c68b37e2a
Jesse Ochs <jessescottochs@gmail.com>
2021-12-05 11:52:51 -0700

    Return percent correct, category percent correct

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -29,4 +29,6 @@ round.take_turn(gets.chomp)
 p round.turns[+1].feedback
 
 p "****** Game over! ******"
-puts "You had #{round.number_correct} correct guesses out of #{round.turns.count} for a total score of #{round.percent_correct}%"
+p "You had #{round.number_correct} correct guesses out of #{round.turns.count} for a total score of #{round.percent_correct}%"
+puts "STEM - #{round.percent_correct_by_category(:STEM)}% correct"
+puts "Geography - #{round.percent_correct_by_category(:Geography)}% correct"
