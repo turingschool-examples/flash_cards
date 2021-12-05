0b51aa0d5e022aea173b86d15014df28b630c30f
Ian Ross <ianrossdrums@gmail.com>
2021-12-05 15:40:41 -0700

    Add turn method in Round class

--- lib/round.rb
+++ lib/round.rb
@@ -3,4 +3,17 @@ class Round
   def initialize(deck)
     @deck = deck
   end
+
+  def turns
+    return []
+  end
+
+  def current_card
+  end
+
+  # def take_turn(guess)
+  #   turn = Turn.new(guess, card)
+  #
+  #   return turn
+  # end
 end
