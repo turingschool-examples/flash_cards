9699e1e93bdb9dee7216b5ec852dd43402cc3e0b
Luke Pascale <lpluke5@gmail.com>
2021-12-05 16:34:55 -0700

    Add rspec test for percentage correct.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -143,4 +143,28 @@ describe Round do
     expect(round.number_correct_by_category(:Geography)).to eq(1)
   end
 
+  it '#.round.number_correct_by_category(:STEM)' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    new_turn = round.take_turn("Venus")
+#binding.pry
+    expect(round.number_correct_by_category(:STEM)).to eq(0)
+  end
+
+  it '#.round.percent_correct' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    new_turn = round.take_turn("Venus")
+#binding.pry
+    expect(round.percent_correct).to eq(50.0)
+  end
+
 end
