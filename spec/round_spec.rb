a4a6a2cfeea808a9678da19f02cfd0db7413c25f
Ian Ross <ianrossdrums@gmail.com>
2021-12-05 15:41:18 -0700

    Add turn method test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,8 +1,10 @@
 require './lib/card'
 require './lib/deck'
+require './lib/turn'
 require './lib/round'
 
 describe Round do
+
   it 'exists' do
     card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -11,5 +13,15 @@ describe Round do
     deck = Deck.new(cards)
     round = Round.new(deck)
     expect(round).to be_instance_of(Round)
-  end]
+  end
+
+  it 'start turn' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.turns).to eq([])
+  end
 end
