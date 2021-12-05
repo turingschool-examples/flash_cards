6eb313b8f8178248b730f8fddeb8730b6cae38bd
Ian Ross <ianrossdrums@gmail.com>
2021-12-05 16:13:52 -0700

    Add count method

--- lib/round.rb
+++ lib/round.rb
@@ -1,19 +1,37 @@
 class Round
- attr_reader :deck
+ attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
+    @turns = []
   end
 
-  def turns
-    return []
+  def count
+    return @cards.length
   end
 
-  def current_card
-  end
+  # def current_card
+  #   return
+  # end
 
   # def take_turn(guess)
   #   turn = Turn.new(guess, card)
-  #
   #   return turn
+  # end
+
+  # def number_correct(card.guess)
+  #   x = 0
+  #   if card.guess == card.answer
+  #     x = x + 1
+  #   end
+  #   return x
+  # end
+
+  # def class
+  # end
+
+  # def number_correct_by_category
+  # end
+
+  # def percent_correct
   # end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -24,4 +24,35 @@ describe Round do
     round = Round.new(deck)
     expect(round.turns).to eq([])
   end
+
+  it 'counts number of cards' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.count).to eq(3)
+  end
+
+  # it 'show currect card' do
+  #   card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #   card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  #   card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  #   cards = [card1, card2, card3]
+  #   deck = Deck.new(cards)
+  #   round = Round.new(deck)
+  #   expect(round).to eq([])
+  # end
+
+  xit 'counts number of correct' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.number_correct).to eq(1)
+  end
+
 end
