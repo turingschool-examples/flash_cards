2342f7764e0cb1f97288c00ff44594d59fd6ba14
Luke Pascale <lpluke5@gmail.com>
2021-12-05 13:48:16 -0700

    Update take_turn method within round.rb.

--- lib/round.rb
+++ lib/round.rb
@@ -1,16 +1,30 @@
 class Round
-attr_reader :deck
+attr_reader :deck,
+            :turns
 
   def initialize(deck)
     @deck = deck
-  end
-
-  def turns
     @turns = []
   end
 
   def current_card
   #  @current_card = current_card
     deck.cards.first
+  end
+
+#  def take_turn(guess)
+#    @take_turn(guess)
+#  end
+  def take_turn(guess)
+    turn = Turn.new(guess, deck.cards.shift)
+    @turns << turn
+    return turn 
+  #  made_guesses = []
+  #  @guess = guess
+  #  @round.each do |round|
+  #    if guess == round.guess
+  #    end
+  #  end
+  #  return made_guesses
   end
 end
