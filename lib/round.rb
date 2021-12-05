d6c5877c2eb24d33df46e6d6ed460f637cfe5773
Sam Smith <samlsmith424@gmail.com>
2021-12-04 17:09:35 -0800

    Add start method

--- lib/round.rb
+++ lib/round.rb
@@ -43,4 +43,16 @@ class Round
   def percent_correct_by_category(category)
     (number_correct_by_category(category).to_f / turns_per_cat(category).to_f) * 100
   end
-end
+
+#   def start
+#     require "pry"; binding.pry
+#     puts "Welcome! You're playing with #{deck.count} cards."
+#     puts "-------------------------------------------------"
+#
+#     until deck.cards.empty? do
+#       puts "This is card number #{turns.length} out of #{deck.cards.count}."
+#       puts "#{current_card.question}"
+#     end
+#
+#   end
+# end
