4bc81a8f842574535f731d3c56de6097aebe70d9
Luke Pascale <lpluke5@gmail.com>
2021-12-05 13:48:45 -0700

    Add rspec test for take_turn and correct.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -38,4 +38,26 @@ describe Round do
 #binding.pry
     expect(round.current_card).to eq(card_1)
   end
+
+  it '#.new_turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+#binding.pry
+    expect(new_turn.correct?).to be true
+  end
+
+  it '#.take_turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+#binding.pry
+    expect(new_turn).to eq(round.turns.first)
+  end
 end
