83c98de7043b014b66a5ec592e0a65ba5b4e4f0b
Adrian Campos <adriancampos2992@yahoo.com>
2021-12-04 18:35:52 -0800

    pass has cards, count cards, cards in stem category tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,17 @@
 class Deck
+  attr_reader :cards
   def initialize(cards)
     @cards = cards
   end
 
+  def count
+    cards.count
+  end
+
+  def cards_in_category(category)
+    cards.find_all do |card|
+      card.category == category
+      #require 'pry'; binding.pry
+    end
+  end
 end
