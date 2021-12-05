9790ecaee4e5240043910f9f145f963fb948c097
Lisa Taylor <ltaylor.lisa@gmail.com>
2021-12-05 13:39:35 -0700

    iteration 4 - card generator appears to be working

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,26 +1,37 @@
 
 
 class CardGenerator
-  attr_reader :cards
+  attr_reader :cards, :info, :deck
 
   def initialize(cards)
+    @info = []
+    @deck = []
     @cards = File.readlines(cards).map(&:chomp)
-    deck = []
-    # @cards.each do |question, answer, category|
-    #   deck << Card.new(question, answer, category)
-    # end
-    # return deck
-    # card_1 = Card.new(@cards[0])
+    @cards.each do |string|
+      @info << string.split(",")
+    end
+    @info.each do |card_info|
+      question = card_info[0]
+      answer = card_info[1]
+      category = card_info[2]
+      @deck << Card.new(question, answer, category)
+    end
+    return @deck
+  end
+end
+  # def sort
+  #   @cards.each do |question, answer, category|
+  #     @deck << Card.new(question, answer, category)
+  #   end
+  #   return deck
+  #   # card_1 = Card.new(@cards[0])
     # card_2 = Card.new(@cards[1])
     # card_3 = Card.new(@cards[2])
     # card_4 = Card.new(@cards[3])
     # deck = [card_1, card_2, card_3, card_4]
-
-  end
 
   # def card_info
   #   @cards.each do |card_info|
   #     card_info.chunk(",")
   #   end
   #   return @deck
-end
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe CardGenerator do
     # binding.pry
     expect(cards).to be_a(CardGenerator)
   end
+
+  it 'is a class' do
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename)
+    binding.pry
+    expect(cards).to eq(cards)
+  end
+
+
+
 end
