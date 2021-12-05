01240489a58e1796593382ae20912df8e6138285
Ian Ross <ianrossdrums@gmail.com>
2021-12-05 13:34:31 -0700

    Add cards in category spec" q

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -19,4 +19,14 @@ describe Deck do
     deck = Deck.new(cards)
     expect(deck.count).to eq(3)
   end
+
+  it 'finds category' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.cards_in_category(:STEM)).to eq([card2, card3])
+
+  end
 end
