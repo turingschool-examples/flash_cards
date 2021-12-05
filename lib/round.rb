a3ed44b7c20052c8f9f846938e5487d5bf7b2fff
glsorensen <gunnar.l.sorensen@gmail.com>
2021-12-05 11:41:32 -0700

    Completed Round interaction pattern through "round.percent_correct"

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,4 @@
+# require 'pry';
 class Round
   attr_reader :round, :turns, :current_card, :deck, :take_turn
 
@@ -11,13 +12,6 @@ class Round
     def current_card
       @deck.cards[0]
     end
-    # @deck = deck
-
-
-
-  # def current_card
-  #   @deck.cards.first
-  # end
 
   def take_turn(guess)
     new_turn = Turn.new(guess, current_card)
@@ -26,10 +20,7 @@ class Round
    @deck.cards.rotate!
    return new_turn
   end
-  # def turns
-  #
-  # end
-#can be done in take turn class
+
   def number_correct
     correct_guesses = 0
     @turns.each do |turn|
@@ -41,25 +32,47 @@ class Round
   end
 
   def number_correct_by_category(category)
-    # cat_geography = 0
-    # cat_stem = 0
-    # p @turns.count
     @turns.count do |turn|
       turn.card.category == category && turn.correct?
-      # if turn.card.category == :Geography
-      #   cat_geography += 1
-      # elsif turn.card.category = :STEM
-      #   cat_stem +=1
-      # else
-      #   puts "There is no category"
-      # end
     end
-
-    require 'pry'; binding.pry
-  def percent_correct
-      round.number_correct.to_f
-      @turns/
-
   end
+
+  def percent_correct
+    #@turns.count.to_f
+    #number_correct.to_f
+    (number_correct.to_f / @turns.count.to_f) * 100.0
+    #require 'pry'; binding.pry
+      # @turns
+  end
+
+  def percent_correct_by_category(category)
+    @turns.count.to_f
+    # percent_correct_by_category(category).to_f
+    # (percent_correct_by_category(category) / turns.count) * 100.0
   end
 end
+# def turns
+#
+# end
+#can be done in take turn class
+
+
+# @deck = deck
+
+
+
+# def current_card
+#   @deck.cards.first
+# end
+# cat_geography = 0
+# cat_stem = 0
+# p @turns.count
+
+
+# if turn.card.category == :Geography
+#   cat_geography += 1
+# elsif turn.card.category = :STEM
+#   cat_stem +=1
+# else
+#   puts "There is no category"
+# end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -96,7 +96,7 @@ RSpec.describe Round do
     expect(round.number_correct_by_category(:STEM)).to eq(0)
   end
 
-  xit 'correct by category' do
+  it 'correct by percent' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -107,7 +107,7 @@ RSpec.describe Round do
     new_turn_2 = round.take_turn("Venus")
 
     expect(round.percent_correct).to eq(50.0)
-    expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
+    #expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
 
   end
 end
