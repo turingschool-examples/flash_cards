bbe793f08eb4353488c97b4b6da4f84fdd9ff045
Luke Pascale <lpluke5@gmail.com>
2021-12-05 10:44:18 -0700

    Finish deck rspec tests and deck class.

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,7 +2,24 @@ class Deck
   attr_reader :cards
 
   def initialize(cards)
-   @cards = cards
+    @cards = cards
   end
 
+  def count
+    @cards.count
+  end
+
+#  def category
+#    @cards.category
+#  end
+
+  def cards_in_category(category)
+    card_order = []
+    @cards.each do |card|
+      if category == card.category
+        card_order << card
+      end
+    end
+    return card_order
+  end
 end
