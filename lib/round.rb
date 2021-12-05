209aeacb72e35054ea803831916b8a16d84ad178
Jesse Ochs <jessescottochs@gmail.com>
2021-12-04 19:56:01 -0700

    return percent correct by category

--- lib/round.rb
+++ lib/round.rb
@@ -5,18 +5,20 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
-    @current_card = deck.cards[0]
     @number_correct = 0
     @number_wrong = 0
   end
 
 
   def take_turn(guess)
-      turn = Turn.new(guess, current_card)
+      turn = Turn.new(guess, deck.cards.shift)
       @turns << turn
       turn.correct? ? @number_correct += 1 : @number_wrong += 1
-      @current_card = deck.cards[+1]
     return turn
+  end
+
+  def current_card
+    deck.cards.first
   end
 
   def number_correct_by_category(category)
@@ -30,7 +32,20 @@ class Round
   end
 
   def percent_correct
-    100 / turns.count / number_correct 
+    turns.count == 0 ? 0 :  100 / (turns.count / number_correct).to_f
   end
 
+  def percent_correct_by_category(cat)
+
+    if number_correct_by_category(cat) == 0
+      0
+    else
+      (number_correct_by_category(cat).to_f / (turns.count{|turn| turn.card.category == cat})) * 100
+    end
+
+  end
 end
+
+
+  # number_correct_by_category(cat) == 0 ? 0 : 100 % (deck.cards_in_category(cat).count / number_correct_by_category(cat)).to_f
+  # (number_correct_by_category(cat).to_f/@turns.count{|turn|turn.card.category == cat})*100.to_i
