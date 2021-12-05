92a9e10de75ce965e7a506cdcc1227b93b29de23
Jimmy Page <jpage@knights.ucf.edu>
2021-12-05 14:37:59 -0700

    created methods for deck class

---
+++ lib/deck.rb
@@ -0,0 +1,21 @@
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    cards.length
+  end
+
+  def cards_in_category(category)
+    category_matches_card = []
+
+    cards.each do |card|
+      if card.category == category
+        category_matches_card << card
+      end
+    end
+    category_matches_card
+ end
+end
