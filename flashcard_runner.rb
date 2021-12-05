ac018fff24f29bedc45b4d5b296afd0d3b527387
Luke Pascale <lpluke5@gmail.com>
2021-12-05 16:55:01 -0700

    Add addition input for flashcard_runner.rb.

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -8,3 +8,15 @@ if gets = 10
 else
   puts "Incorrect."
 end
+puts "This is card number 2 out of 4."
+puts "Question: What is Rachel's favorite animal?"
+gets
+puts "Incorrect."
+puts "This is card number 3 out of 4."
+puts "Question: What is Mike's middle name?"
+gets
+puts "Correct!"
+puts "This is card number 4 out of 4."
+puts "Question: What cardboard cutout lives at Turing?"
+gets
+puts "Correct!"
