e0c3175689d06078aa1abc8e9dcc18f397dc2d08
Luke Pascale <lpluke5@gmail.com>
2021-12-05 16:51:38 -0700

    Begin iteration 3. Add flashcard_runner.rb file.

---
+++ flashcard_runner.rb
@@ -0,0 +1,10 @@
+puts "Welcome! You're playing with 4 cards."
+puts "-------------------------------------------------"
+puts "This is card number 1 out of 4."
+puts "Question: What is 5 + 5?"
+gets
+if gets = 10
+  puts "Correct!"
+else
+  puts "Incorrect."
+end
