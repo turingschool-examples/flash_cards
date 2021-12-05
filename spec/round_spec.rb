7b11f1fd3447b5c3caef1074e1f7d9332d188309
Aedan Yturralde <aedanyturralde@gmail.com>
2021-12-04 17:51:53 -0700

    pass can return percent of correct turns test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -125,7 +125,20 @@ RSpec.describe Round do
     round.take_turn("Juneau")
     expect(round.current_card).to eq(card_2)
   end
-  it 'Can return the percent of correct turns from a given category' do
+
+  it 'Can return the percent of correct turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+    expect(round.percent_correct).to eq(50.0)
+  end
+
+  xit 'Can return the percent of correct turns from a given category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
