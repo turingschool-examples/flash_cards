4f3e667b4c48415a46823b7f3c4283fa4b03e4f0
Tyler Ross <tross020890@gmail.com>
2021-12-05 15:04:16 -0600

    Pass new turn

--- lib/round.rb
+++ lib/round.rb
@@ -3,10 +3,22 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
+
+  end
+
+
+
+  def current_card
+    deck.current_card(turns.length)
   end
 
   def take_turn(guess)
-    turn = Turn.new(deck, guess)
+    turn = Turn.new(guess, deck.current_card(turns.length))
+    turns << deck.current_card(turns.length)
+    #if turn.correct? == true
+    #  number_correct += 1
+    #end
   end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -48,4 +48,37 @@ RSpec.describe Deck do
 
     expect(round.turns).to eq([])
   end
+
+  it 'has current card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    round = Round.new(deck)
+
+    round.deck
+
+    expect(round.current_card).to eq(card_1) #returning false, but are the same output in pry?
+  end
+
+  it 'has new turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    round = Round.new(deck)
+
+    round.deck
+
+    new_turn = round.take_turn("Juneau")
+    expect(new_turn).to eq(round.take_turn("Juneau")) 
+  end
 end
