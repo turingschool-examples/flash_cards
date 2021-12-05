6abdec482db227d78f1fb295b3325b8e7bbc1c4b
Nick Cooley <cooley8706@gmail.com>
2021-12-04 18:07:00 -0700

    Define current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,9 @@
 class Round
 
-attr_reader :deck, :turns
+attr_reader :deck, :turns, :current_card
   def initialize(deck)
     @deck = deck
     @turns = []
+    @current_card = deck.cards[0]
   end
 end
