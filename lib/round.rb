40919953270735b20f350307028b47d8640a2d21
Tyler Ross <tross020890@gmail.com>
2021-12-05 13:06:28 -0600

    Pass has turns

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,12 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
+    @turns = []
   end
+
+  def take_turn(guess)
+    turn = Turn.new(deck, guess)
+  end
+
 end
