866b26adf979b95ba82fb26ccf5e490117414781
Nick Cooley <cooley8706@gmail.com>
2021-12-05 13:03:31 -0700

    Pass feedback test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -56,19 +56,20 @@ RSpec.describe Round do
     expect(round.number_correct).to eq(1)
   end
 
-  it 'guesses Venus' do
+  it 'is a new turn class' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
     round = Round.new(deck)
-    new_turn = round.take_turn("Venus")
-    # expect(round.current_card).to eq(card_1)
-    # round.take_turn("Juneau")
+    new_turn = round.take_turn("Juneau")
+    new_turn2 = round.take_turn("Venus")
     expect(round.current_card).to eq(card_2)
-    # expect(round.take_turn).to eq("Venus")
-    # expect(round.turns.count).to eq(2)
+    expect(new_turn2).to be_instance_of(Turn)
+    expect(round.turns.count).to eq(2)
+    expect(round.turns.last.feedback2).to eq('Incorrect')
+    expect(round.number_correct).to eq(1)
   end
 
 end
