78e729126d64d3fc531bb989128b95ddc0f09ec2
Nick Cooley <cooley8706@gmail.com>
2021-12-05 11:28:17 -0700

    Pass number correct test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -40,9 +40,10 @@ RSpec.describe Round do
     cards = [card_1, card_2, card_3]
     deck = Deck.new (cards)
     round = Round.new(deck)
+    expect(round.turns).to eq([])
     expect(round.current_card).to eq(card_1)
   end
-  it 'takes a turn' do
+  it 'guesses Juneau' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -52,9 +53,20 @@ RSpec.describe Round do
     new_turn = round.take_turn("Juneau")
     expect(new_turn.class).to eq(Turn)
     expect(new_turn.correct?).to eq(true)
-    expect(round.turns).to eq([])
     expect(round.number_correct).to eq(1)
-    # expect(round.current_card). to eq(card_2)
+  end
+
+  xit 'guesses Venus' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.current_card).to eq(card_1)
+    round.take_turn("Juneau")
+    expect(round.current_card).to eq(card_2)
+    # expect(round.take_turn).to eq("Venus")
   end
 
 end
