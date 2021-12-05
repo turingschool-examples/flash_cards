84928df7189243fc93ac037bbea0cb7a603bc5da
Jesse Ochs <jessescottochs@gmail.com>
2021-12-05 11:51:37 -0700

    Fix percent_correct to return correct response

--- lib/round.rb
+++ lib/round.rb
@@ -32,7 +32,7 @@ class Round
   end
 
   def percent_correct
-    turns.count == 0 ? 0 :  100 / (turns.count / number_correct).to_f
+    number_correct == 0 ? 0 : (number_correct.to_f / turns.count) * 100
   end
 
   def percent_correct_by_category(cat)
