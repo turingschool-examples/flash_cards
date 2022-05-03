d94169d120d84719596de1c8305ee710fe43066b
John Santosuosso <john.santosuosso1@gmail.com>
2022-05-03 17:27:16 -0400

    update deck cards in category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,21 @@
 # deck.rb
-require_relative 'turn'
-class Deck(cards)
+require_relative 'card'
+
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
   @cards = cards
+  end
+
+  def count
+    @cards.length
+  end
+
+  def cards_in_category(category)
+    @cards.select { |card| card.category == category }
+  end
+
 end
+
+require 'pry'; binding.pry
