90804a171e2838342f13c451479e376e857cf69b
Candace Eckels <candace.eckels@gmail.com>
2022-05-04 15:46:25 -0600

    Created tests to match the interaction pattern

---
+++ spec/deck_spec.rb
@@ -0,0 +1,32 @@
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+    before :each do
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        @deck = Deck.new(cards)
+    end
+
+    it 'exists' do
+        expect(@deck).to be_a(Deck)
+    end
+
+    it 'can count the cards' do
+        expect(@deck.count).to eq(3)
+    end
+
+    it 'can sort cards by category' do
+        expect(@deck.cards_in_category(:STEM)).to eq ([card_2,card_3])
+    end
+
+    it 'can sort by any category' do
+        expect(@deck.cards_in_category(:Geography)).to eq([card_1])
+    end
+
+    it 'can create new categories to sort by' do
+        expect(@deck.cards_in_category('Pop Culture')).to eq([])
+    end
+
+end
