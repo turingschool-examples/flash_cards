f64ef3121630af46b94a3929dab4974b5d73d514
Candace Eckels <candace.eckels@gmail.com>
2022-05-04 12:57:43 -0600

    Created attributes, edited test to read them correctly

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,11 +3,16 @@ require './lib/card'
 
 RSpec.describe Turn do
     before :each do
-        @turn = Turn.new("Juneau", @card)
         @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @turn = Turn.new("Juneau", @card)
     end
 
     it 'exists' do
         expect(@turn).to be_a(Turn)
+    end
+
+    it 'has attributes' do
+        expect(@turn.guess).to eq('Juneau')
+        expect(@turn.card).to eq(@card)
     end
 end
