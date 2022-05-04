610ea0cd1a782f5074549a7f7d50c58dfb0f72b8
Candace Eckels <candace.eckels@gmail.com>
2022-05-04 12:53:17 -0600

    Created an instance of turn

---
+++ lib/turn.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+class Turn
+    attr_reader :guess, :card
+    def initialize(guess,card)
+        @guess = guess
+        @card = card
+    end
+
+end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,8 +3,11 @@ require './lib/card'
 
 RSpec.describe Turn do
     before :each do
-        turn = Turn.new("Juneau", card)
-        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @turn = Turn.new("Juneau", @card)
+        @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     end
 
+    it 'exists' do
+        expect(@turn).to be_a(Turn)
+    end
 end
