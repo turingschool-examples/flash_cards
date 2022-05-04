b1fb3fa0444c081140b48acaabf75680a24725b5
Candace Eckels <candace.eckels@gmail.com>
2022-05-04 17:09:19 -0600

    Condensed some of the code

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,13 +2,9 @@ require 'pry'
 
 class Deck 
     attr_reader :cards, :count
-    def initialize(cards)
-        @cards = cards
-    end
+    def initialize(cards); @cards = cards end
 
-    def count
-        @cards.length
-    end
+    def count; @cards.length end
 
     def cards_in_category(category)
         card_same_category = []
