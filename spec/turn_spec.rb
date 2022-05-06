7a6647c6b5763e62def69280e263139a8f9ef6be
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-06 14:17:14 -0700

    Add tests for incorrect response

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -18,4 +18,14 @@ RSpec.describe Turn do
     expect(turn.correct?).to eq true
     expect(turn.feedback).to eq "Correct!"
   end
+
+  it "can give incorrect response" do
+    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn2 = Turn.new("Saturn", card2)
+
+    expect(turn2.guess).to eq "Saturn"
+    expect(turn2.correct?).to eq false
+    expect(turn2.feedback).to eq "Incorrect."
+
+  end
 end
