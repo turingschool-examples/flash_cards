c7952196d409ba9fb700238ff9d69f9c60f44f15
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-06 14:19:22 -0700

    Refactor turn spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,20 +3,18 @@ require './lib/turn'
 require 'pry'
 
 RSpec.describe Turn do
+  before :each do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
   it "exists" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn).to be_a Turn
+    expect(@turn).to be_a Turn
   end
 
   it "can give correct response" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.guess).to eq "Juneau"
-    expect(turn.correct?).to eq true
-    expect(turn.feedback).to eq "Correct!"
+    expect(@turn.guess).to eq "Juneau"
+    expect(@turn.correct?).to eq true
+    expect(@turn.feedback).to eq "Correct!"
   end
 
   it "can give incorrect response" do
@@ -26,6 +24,5 @@ RSpec.describe Turn do
     expect(turn2.guess).to eq "Saturn"
     expect(turn2.correct?).to eq false
     expect(turn2.feedback).to eq "Incorrect."
-
   end
 end
