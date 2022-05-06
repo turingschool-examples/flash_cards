58a2b8d31877c8a90880aa73b819d528cbb3630e
philmarcu <philmark18@gmail.com>
2022-05-06 09:27:10 -0600

    Finish number correct method & test

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,26 @@
 class Round
 
-  attr_reader :deck, :turns #:correct
+  attr_reader :deck, :turns, :correct
   def initialize(deck)
     @deck = deck
     @turns = []
-    # @correct = 0
+    @correct = 0
+  end
+
+  def current_card
+    @deck.cards.first
+  end
+
+  def take_turn(guess)
+    if guess == current_card.answer
+      @correct += 1
+    end
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
+    deck.cards.delete_at(0)
+  end
+
+  def number_correct
+    @correct
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Round do
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     @deck = Deck.new([@card_1, @card_2, @card_3])
-    # @new_turn = Turn.new("Juneau", @card_1)
     @round = Round.new(@deck)
   end
 
@@ -17,6 +16,24 @@ RSpec.describe Round do
     expect(@round).to be_a(Round)
     expect(@round.deck).to eq(@deck)
     expect(@round.turns).to eq([])
+  end
+
+  it 'can tell us the current card' do
+    expect(@round.current_card).to eq(@card_1)
+  end
+
+  it 'can take a new turn' do
+    @new_turn = Turn.new("Juneau", @card_1)
+
+    expect(@new_turn).to be_a(Turn)
+    expect(@new_turn.correct?).to be(true)
+  end
+
+  it 'can tally correct answers' do
+    @new_turn = Turn.new("Juneau", @card_1)
+    @round.take_turn("Juneau")
+
+    expect(@round.number_correct).to eq(1)
   end
 
 end
