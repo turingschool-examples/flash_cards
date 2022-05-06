27f68a03b50a7428a40598b39289d8b34d5b737a
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-06 13:42:59 -0700

    Add test for turn class

---
+++ spec/turn_spec.rb
@@ -0,0 +1,12 @@
+require './lib/card'
+require './lib/turn'
+require 'pry'
+
+RSpec.describe Turn do
+  it "exists" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_a Turn 
+  end
+end
