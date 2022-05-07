9512dae51bed2580e61252c860e27aeab50f0289
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-06 20:11:35 -0700

    Add initialize round test

---
+++ spec/round_spec.rb
@@ -0,0 +1,18 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+require 'pry'
+
+RSpec.describe Round do
+  it "exists" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round).to be_a Round
+    expect(round.deck).to be_a Deck
+  end
+end
