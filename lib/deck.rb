206d3b623f57de56914298cb16084188b343697c
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-06 19:45:03 -0700

    Add method for cards in a category

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,4 +7,14 @@ class Deck
   def count
     @cards.length
   end
+
+  def cards_in_a_category(category)
+    card_categories = []
+    @cards.each do |card|
+      if card.category == category
+        card_categories << card
+      end
+    end
+    card_categories
+  end
 end
