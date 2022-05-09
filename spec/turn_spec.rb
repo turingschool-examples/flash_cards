3f4e43d7d41b9194f5a728ba9f1f8b59538e0b31
Eli Sachs <easachs13@gmail.com>
2022-05-09 17:18:05 -0600

    Create Turn spec (iteration 1)

---
+++ spec/turn_spec.rb
@@ -0,0 +1,46 @@
+require './lib/card'
+require './lib/turn'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has an associated card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.card).to eq(card)
+  end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'has correct? method (true)' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to be(true)
+  end
+
+  it 'has feedback method (true)' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+  it 'has correct? method (false)' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.correct?).to be(false)
+  end
+
+  it 'has feedback method (false)' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+end
