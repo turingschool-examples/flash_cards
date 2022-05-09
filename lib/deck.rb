ede5cd13523d273a8373d79206530d2be8487e18
Eli Sachs <easachs13@gmail.com>
2022-05-09 17:37:21 -0600

    Finish Deck class (iteration 2)

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,19 @@
 class Deck
 
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(cat)
+    @cards.find_all do |card|
+      card.category == cat
+    end
+  end
+
 end
