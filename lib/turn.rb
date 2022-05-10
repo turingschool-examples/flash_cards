d302f6f27e89f943a7a8d2137d32f974e8dbd01f
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-10 13:16:02 -0500

    Add Turn class

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+class Turn
+  attr_accessor :card
+
+  def initialize(string, card)
+    @string = string
+    @card = card
+  end
+
+  def guess
+    @string
+  end
+
+  def correct?
+    if @string == @card.answer
+      return true
+    else
+      return false
+    end
+  end
+
+  def feedback
+    if @string == @card.answer
+      return "Correct!"
+    else
+      return "Incorrect."
+    end
+  end
+
+end
