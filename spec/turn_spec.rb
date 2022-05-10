f494a83738f9537422b641285a03c0dcda92dce6
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-10 14:03:52 -0500

    Add test for wrong answer

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -42,4 +42,49 @@ RSpec.describe Turn do
 
     expect(turn.feedback).to eq("Correct!")
   end
+
+end
+
+RSpec.describe Turn do
+  it 'can access Card' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+
+    expect(card).to be_instance_of(Card)
+  end
+
+  it 'exists' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'stores a card' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.card).to be_instance_of(Card)
+  end
+
+  it 'stores a guess' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.guess).to eq("Saturn")
+  end
+
+  it 'tells you if you were correct' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.correct?).to eq(false)
+  end
+
+  it 'gives you feedback' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+
 end
