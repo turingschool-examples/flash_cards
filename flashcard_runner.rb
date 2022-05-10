87b96345b195eadaed459beb878c4143b16bec95
Eli Sachs <easachs13@gmail.com>
2022-05-10 17:49:55 -0600

    Refactor Round class and Flashcard runner (iteration 3)

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,9 +4,9 @@ require './lib/deck'
 require './lib/round'
 
 card_1 = Card.new("What is the capital of Togo?", "lome", :Geography)
-card_2 = Card.new("Saipan and Guam are located near which underseas topographical feature in the Pacific Ocean?", "mariana trench", :Geography)
+card_2 = Card.new("The deepest point in the ocean is called the _______ Trench.", "mariana", :Geography)
 card_3 = Card.new("What year did the Titanic sink?", "1912", :History)
-card_4 = Card.new("What style of art is Salvador Dali known for?", "surrealism", :Art)
+card_4 = Card.new("Salvador _______ is a surrealist painter.", "dali", :Art)
 deck_1 = Deck.new([card_1, card_2, card_3, card_4])
 round = Round.new(deck_1)
 round.start
--- lib/round.rb
+++ lib/round.rb
@@ -25,13 +25,9 @@ class Round
   end
 
   def number_correct_by_category(cat)
-    number_correct_by_category = 0
-    @turns.each do |t|
-      if t.card.category == cat && t.correct?
-        number_correct_by_category += 1
-      end
+    number_correct_by_category = @turns.count do |turn|
+      turn.card.category == cat && turn.correct?
     end
-    return number_correct_by_category
   end
 
   def percent_correct
@@ -39,8 +35,8 @@ class Round
   end
 
   def percent_correct_by_category(cat)
-    self.number_correct_by_category(cat) * 100 / @turns.count do |t|
-      t.card.category == cat
+    number_correct_by_category(cat) * 100 / @turns.count do |turn|
+      turn.card.category == cat
     end
   end
 
@@ -48,48 +44,48 @@ class Round
     puts "Welcome! You're playing with #{deck.cards.length} cards."
     puts "-" * 30
 
-    total_cards = deck.cards.length + self.used_cards.length
+    total_cards = deck.cards.length + used_cards.length
 
     # when card_number <= 4 /or/ deck.cards.length > 0
 
     card_number = 1
-    # self.deck.cards.each do |c|
+    # deck.cards.each do |card|
     puts "This is card number #{card_number} out of #{total_cards}."
-    puts "Question: #{@deck.cards.first.question}"
+    puts "Question: #{deck.cards.first.question}"
     answer_1 = gets.chomp.to_s.downcase
-    first_turn = self.take_turn(answer_1)
+    first_turn = take_turn(answer_1)
     puts first_turn.feedback
 
     card_number += 1
     puts "This is card number #{card_number} out of #{total_cards}."
-    puts "Question: #{@deck.cards.first.question}"
+    puts "Question: #{deck.cards.first.question}"
     answer_2 = gets.chomp.to_s.downcase
-    second_turn = self.take_turn(answer_2)
+    second_turn = take_turn(answer_2)
     puts second_turn.feedback
 
     card_number += 1
     puts "This is card number #{card_number} out of #{total_cards}."
-    puts "Question: #{@deck.cards.first.question}"
+    puts "Question: #{deck.cards.first.question}"
     answer_3 = gets.chomp.to_s.downcase
-    third_turn = self.take_turn(answer_3)
+    third_turn = take_turn(answer_3)
     puts third_turn.feedback
 
     card_number += 1
     puts "This is card number #{card_number} out of #{total_cards}."
-    puts "Question: #{@deck.cards.first.question}"
+    puts "Question: #{deck.cards.first.question}"
     answer_4 = gets.chomp.to_s.downcase
-    fourth_turn = self.take_turn(answer_4)
+    fourth_turn = take_turn(answer_4)
     puts fourth_turn.feedback
 
     # when deck.cards == []
 
     puts "***** Game over! *****"
-    puts "You had #{self.number_correct} out of #{total_cards} for a total score of #{self.percent_correct.to_i}%."
-    used_cards_by_cat = self.used_cards.group_by do |c|
-      c.category
+    puts "You had #{number_correct} correct guesses out of #{total_cards} for a total score of #{percent_correct.to_i}%."
+    used_cards_by_category = used_cards.group_by do |card|
+      card.category
     end
-    stats = used_cards_by_cat.each do |k,v|
-      puts "#{k} - #{self.percent_correct_by_category(k)}% correct"
+    stats = used_cards_by_category.each do |k,v|
+      puts "#{k} - #{percent_correct_by_category(k)}% correct"
     end
   end
 
