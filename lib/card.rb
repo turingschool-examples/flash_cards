a543d67d5089c8ced94f871eb287430897b6392f
Blake Saylor <blakesaylor@gmail.com>
2022-05-10 12:42:58 -0600

    Add attributes for Card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,13 +1,21 @@
+# Creates an instance of Card with a question, answer, and a category
 class Card
-  def question
+  # Initialize Card
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
 
+  def question
+    @question
   end
 
   def answer
-
+    @answer
   end
 
   def category
-
+    @category
   end
 end
