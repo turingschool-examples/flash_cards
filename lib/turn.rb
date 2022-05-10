f37f86e2971e7576adf5329e404d23d2249f4bc5
Jonathan Pope <jonathanmpope@gmail.com>
2022-05-10 17:41:22 -0600

    Add passed tests 1-3 for turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,5 @@
 class Turn
+    attr_reader :guess, :card
 
 def initialize(guess, card)
   @guess = guess
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,9 +3,21 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
-  it 'exists' do
+  it 'card exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    expect(card).to be_instance_of(Card)
+  end
+
+  it 'turn exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'card can be called' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    turn.card
+    expect(turn.card).to eq(card)
   end
 end
