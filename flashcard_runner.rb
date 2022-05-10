462ea4209c6ff3804d0e825fb3b442c447305c84
Eli Sachs <easachs13@gmail.com>
2022-05-10 12:40:11 -0600

    Create start method, move start code to Round class

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -8,51 +8,5 @@ card_2 = Card.new("Saipan and Guam are located near which underseas topographica
 card_3 = Card.new("What year did the Titanic sink?", "1912", :History)
 card_4 = Card.new("What style of art is Salvador Dali known for?", "surrealism", :Art)
 deck_1 = Deck.new([card_1, card_2, card_3, card_4])
-first_round = Round.new(deck_1)
-
-puts "Welcome! You're playing with #{deck_1.cards.length} cards."
-puts "-" * 30
-
-total_cards = deck_1.cards.length + first_round.used_cards.length
-
-# when card_number <= 4 /or/ deck.cards.length > 0
-
-card_number = 1
-# first_round.deck.cards.each do |c|
-puts "This is card number #{card_number} out of #{total_cards}."
-puts "Question: #{card_1.question}"
-answer_1 = gets.chomp.to_s.downcase
-first_turn = first_round.take_turn(answer_1)
-puts first_turn.feedback
-
-card_number += 1
-puts "This is card number #{card_number} out of #{total_cards}."
-puts "Question: #{card_2.question}"
-answer_2 = gets.chomp.to_s.downcase
-second_turn = first_round.take_turn(answer_2)
-puts second_turn.feedback
-
-card_number += 1
-puts "This is card number #{card_number} out of #{total_cards}."
-puts "Question: #{card_3.question}"
-answer_3 = gets.chomp.to_s.downcase
-third_turn = first_round.take_turn(answer_3)
-puts third_turn.feedback
-
-card_number += 1
-puts "This is card number #{card_number} out of #{total_cards}."
-puts "Question: #{card_4.question}"
-answer_4 = gets.chomp.to_s.downcase
-fourth_turn = first_round.take_turn(answer_4)
-puts fourth_turn.feedback
-
-# when deck.cards == []
-
-puts "***** Game over! *****"
-puts "You had #{first_round.number_correct} out of #{total_cards} for a total score of #{first_round.percent_correct.to_i}%."
-used_cards_by_cat = first_round.used_cards.group_by do |c|
-  c.category
-end
-stats = used_cards_by_cat.each do |k,v|
-  puts "#{k} - #{first_round.percent_correct_by_category(k)}% correct"
-end
+round = Round.new(deck_1)
+round.start
--- lib/round.rb
+++ lib/round.rb
@@ -44,4 +44,53 @@ class Round
     end
   end
 
+  def start
+    puts "Welcome! You're playing with #{deck.cards.length} cards."
+    puts "-" * 30
+
+    total_cards = deck.cards.length + self.used_cards.length
+
+    # when card_number <= 4 /or/ deck.cards.length > 0
+
+    card_number = 1
+    # self.deck.cards.each do |c|
+    puts "This is card number #{card_number} out of #{total_cards}."
+    puts "Question: #{@deck.cards.first.question}"
+    answer_1 = gets.chomp.to_s.downcase
+    first_turn = self.take_turn(answer_1)
+    puts first_turn.feedback
+
+    card_number += 1
+    puts "This is card number #{card_number} out of #{total_cards}."
+    puts "Question: #{@deck.cards.first.question}"
+    answer_2 = gets.chomp.to_s.downcase
+    second_turn = self.take_turn(answer_2)
+    puts second_turn.feedback
+
+    card_number += 1
+    puts "This is card number #{card_number} out of #{total_cards}."
+    puts "Question: #{@deck.cards.first.question}"
+    answer_3 = gets.chomp.to_s.downcase
+    third_turn = self.take_turn(answer_3)
+    puts third_turn.feedback
+
+    card_number += 1
+    puts "This is card number #{card_number} out of #{total_cards}."
+    puts "Question: #{@deck.cards.first.question}"
+    answer_4 = gets.chomp.to_s.downcase
+    fourth_turn = self.take_turn(answer_4)
+    puts fourth_turn.feedback
+
+    # when deck.cards == []
+
+    puts "***** Game over! *****"
+    puts "You had #{self.number_correct} out of #{total_cards} for a total score of #{self.percent_correct.to_i}%."
+    used_cards_by_cat = self.used_cards.group_by do |c|
+      c.category
+    end
+    stats = used_cards_by_cat.each do |k,v|
+      puts "#{k} - #{self.percent_correct_by_category(k)}% correct"
+    end
+  end
+
 end
