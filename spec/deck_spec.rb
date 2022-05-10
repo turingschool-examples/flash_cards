36e218ff144f9b71d50479d261a6636f83e1d9e9
Eli Sachs <easachs13@gmail.com>
2022-05-10 09:12:42 -0600

    Fix Deck spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -37,7 +37,6 @@ RSpec.describe Deck do
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    require "pry"; binding.pry
     expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
     expect(deck.cards_in_category(:Geography)).to eq([card_1])
     expect(deck.cards_in_category("Pop Culture")).to eq([])
