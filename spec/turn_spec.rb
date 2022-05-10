3f1cdb99f63cffe2b38f510efdeb149446ceb956
mkbonini <mkbonini@gmail.com>
2022-05-10 14:20:00 -0600

    Add test to turn spec for correct? method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe Turn do
 
     expect(turn.card).to eq(card)
   end
-end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new('Juneau',card)
+
+    expect(turn.guess).to eq('Juneau')
+  end
+
+  it 'checks if the guess is correct'
+  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  turn = Turn.new('Juneau',card)
+
+  expect(turn.correct?).to be true
+  end
