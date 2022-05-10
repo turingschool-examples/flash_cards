3b6860b214eb372a534e06e7f93d6c212cef3e2d
Blake Saylor <blakesaylor@gmail.com>
2022-05-10 17:59:19 -0600

    Add test for if Turn exists

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,10 +2,18 @@ require 'rspec'
 require './lib/turn'
 require './lib/card'
 
-RSpec.describe Turn do
+describe Turn do
   it 'Card exists' do
     card = Card.new("What is the capital of Oklahoma?", "Oklahoma City",
       :Geography)
     expect(card).to be_a Card
   end
+
+  it 'Turn exists' do
+    card = Card.new("What is the capital of Oklahoma?", "Oklahoma City",
+      :Geography)
+    turn = Turn.new("Oklahoma City", card)
+    expect(turn).to be_a Turn
+  end
+
 end
