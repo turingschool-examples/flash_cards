3b115216491fe0327225a82604e726b6efae4c0e
Bryce Simonds <bsimonds14@gmail.com>
2022-05-10 20:19:33 -0600

    pass tests 3 and 4

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -23,16 +23,18 @@ describe Turn do
   end
 end
 
-# describe 'guess' do
-#   it 'returns the guess' do
-#   turn = Turn.new("Junea", "Placeholder for current card")
-#   expect(turn.guess). to eq("Junea")
-#   end
-# end
-#
-# describe 'correct?' do
-#   it 'checks if the guess matches the answer' do
-#     turn = Turn.new("Junea", "Placeholder")
-#     expect(turn.correct?).to eq(true)
-#   end
-# end
+describe 'guess' do
+  it 'returns the guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  turn = Turn.new("Junea", card)
+  expect(turn.guess). to eq("Junea")
+  end
+end
+
+describe 'correct?' do
+  it 'checks if the guess matches the answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Junea", card)
+    expect(turn.correct?).to eq(true)
+  end
+end
