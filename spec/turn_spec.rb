4a6d8b8a84e332943d336bd3211b65a2e360d72f
Bryce Simonds <bsimonds14@gmail.com>
2022-05-10 20:15:09 -0600

    fix placeholder and pass the first 2 tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,27 +1,38 @@
 require 'rspec'
 require './lib/turn.rb'
+require './lib/card'
 
 
 describe Turn do
   describe 'argument' do
-    it 'is initialized with two arguments' do
-      turn = Turn.new("Junea", "Placeholder for current card")
+    it 'accepts the first argument' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Junea", card)
       expect(turn.guess).to eq("Junea")
-      expect(turn.card).to eq("Placeholder for current card")
+    end
+
+  describe 'second argument' do
+    it 'accepts the second argument' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Junea", card)
+      expect(turn.card).to eq(card)
     end
   end
-end
 
-describe 'guess' do
-  it 'returns the guess' do
-  turn = Turn.new("Junea", "Placeholder for current card")
-  expect(turn.guess). to eq("Junea")
+
   end
 end
 
-describe 'correct?' do
-  it 'checks if the guess matches the answer' do
-    turn = Turn.new("Junea", "Placeholder")
-    expect(turn.correct?).to eq(true)
-  end
-end
+# describe 'guess' do
+#   it 'returns the guess' do
+#   turn = Turn.new("Junea", "Placeholder for current card")
+#   expect(turn.guess). to eq("Junea")
+#   end
+# end
+#
+# describe 'correct?' do
+#   it 'checks if the guess matches the answer' do
+#     turn = Turn.new("Junea", "Placeholder")
+#     expect(turn.correct?).to eq(true)
+#   end
+# end
