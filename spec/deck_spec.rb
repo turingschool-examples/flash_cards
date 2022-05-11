4bfa3eeb392adf99713d63788d612278a3bbeebe
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-10 22:51:40 -0400

    Add test for returning count of cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -18,9 +18,39 @@ RSpec.describe Deck do
 
     deck = Deck.new(cards)
 
-    turn = Turn.new("My guess",card)
-
     expect(deck).to be_instance_of(Deck)
+
+  end
+
+  it 'can return collection of cards' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1,card_2,card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq([card_1,card_2,card_3])
+
+  end
+
+  it 'can return count of cards in deck' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1,card_2,card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.count).to eq(3)
 
   end
 
