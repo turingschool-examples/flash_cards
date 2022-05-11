5c57142ff36ae2d9ae3c98afa5cf4c78a2e54803
Jonathan Pope <jonathanmpope@gmail.com>
2022-05-11 16:16:32 -0600

    Add test 3 and passing code

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -22,5 +22,16 @@ describe 'Round' do
     round = Round.new(deck)
     expect(round.deck).to eq deck
   end
-  
+
+  it 'will be able to take turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.turns
+    expect(round.turns).to eq []
+  end
+
+
 end
