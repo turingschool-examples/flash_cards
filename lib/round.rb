7bac932c2a27f6b1d8077fd698149a004ed3dcbf
Jonathan Pope <jonathanmpope@gmail.com>
2022-05-11 16:15:09 -0600

    Add round test 2 and passing code

--- lib/round.rb
+++ lib/round.rb
@@ -1,4 +1,5 @@
 class Round
+  attr_reader :deck
 
 def initialize(deck)
   @deck = deck
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -14,5 +14,13 @@ describe 'Round' do
     expect(round).to be_instance_of Round
   end
 
-
+  it 'will be able to read the deck variable' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    expect(round.deck).to eq deck
+  end
+  
 end
