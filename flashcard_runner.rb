6816dab0fe68e0d0f68917b85e06ad1336fbf136
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-10 19:06:28 -0700

    Add cards to runner file

---
+++ flashcard_runner.rb
@@ -0,0 +1,9 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+card_1 = Card.new("Question: What is 5 + 5?", "10", :STEM)
+card_2 = Card.new("Question: What is Rachel's favorite animal?", "panda", :Turing_Staff)
+card_3 = Card.new("Question: What is Mike's middle name?", "nobody knows", :Turing_Staff)
+card_4 = Card.new("Question: What carbiard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
