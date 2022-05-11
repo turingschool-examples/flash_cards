75eac896f66640aea032842baf43e8d85bab08f0
Bryce Simonds <bsimonds14@gmail.com>
2022-05-10 20:14:39 -0600

    fix placeholder and create card variable

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,19 @@
 class Turn
   attr_reader :guess, :card
+
   def initialize(guess, card)
     @guess = guess
     @card = card
   end
+
+# def correct?
+#   if @guess = card.answer
+#     true
+#   elsif @guess != card.answer
+#     false
+#   else
+#     p "error!"
+#   end
+# end
 
 end
