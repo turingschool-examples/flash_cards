9b820473ef512c9c1497b19f8606ee72f50ab037
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-10 19:11:50 -0700

    Add new instances of round and deck to runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,3 +7,6 @@ card_1 = Card.new("Question: What is 5 + 5?", "10", :STEM)
 card_2 = Card.new("Question: What is Rachel's favorite animal?", "panda", :Turing_Staff)
 card_3 = Card.new("Question: What is Mike's middle name?", "nobody knows", :Turing_Staff)
 card_4 = Card.new("Question: What carbiard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+cards = [card_1, card_2, card_3, card_4]
+deck = Deck.new(cards)
+round = Round.new(deck)
