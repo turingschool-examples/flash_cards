faeb67664fb8a55b29c4658e772c6a529994d938
Ethan Nguyen <etnguyen16@gmail.com>
2022-05-10 22:38:00 -0600

    fix to consistant quotes instead of switching

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,21 +5,21 @@ require './card'
 RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new('Juneau',card)
+    turn = Turn.new("Juneau",card)
 
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new('Juneau',card)
+    turn = Turn.new("Juneau",card)
 
     expect(turn.card).to eq(card)
   end
 
   it 'has a guess' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new('Juneau',card)
+    turn = Turn.new("Juneau",card)
 
     expect(turn.guess).to eq('Juneau')
   end
@@ -37,8 +37,8 @@ RSpec.describe Turn do
     correct = Turn.new("Juneau",card)
     incorrect = Turn.new([],card)
 
-    expect(correct.feedback).to eq('Correct!')
-    expect(incorrect.feedback).to eq('Incorrect.')
+    expect(correct.feedback).to eq("Correct!")
+    expect(incorrect.feedback).to eq("Incorrect.")
   end
-  
+
 end
