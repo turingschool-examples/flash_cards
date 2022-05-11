f738355cdb12f7d707889d67a6c013d77bbe33cd
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-10 20:10:01 -0700

    Feat:code for cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,14 @@ class Deck
     @cards.count
   end
 
+  def cards_in_category(category)
+    cards_by_category = []
+    @cards.each do |card|
+      if card.category == category
+        cards_by_category << card
+      end
+    end
+    cards_by_category
+  end
+
 end
