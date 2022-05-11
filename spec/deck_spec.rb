5c927f904f27d387a16b71b4aea304647cd47737
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-10 19:49:09 -0500

    Test Card functionality

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,21 +2,29 @@ require './lib/card'
 require './lib/deck'
 require 'pry'
 
-card_1 = Card.new(
-  "What is the capital of Alaska?",
-  "Juneau",
-  :Geography
-)
+RSpec.describe Card do
+  it 'can create instances of Cards' do
+    card_1 = Card.new(
+      "What is the capital of Alaska?",
+      "Juneau",
+      :Geography
+    )
 
-card_2 = Card.new(
-  "The Viking spacecraft sent back to Earth photographs and
-  reports about the surface of which planet?",
-  "Mars",
-  :STEM
-)
+    expect(card_1).to be_instance_of(Card)
+  end 
 
-card_3 = Card.new("Describe in words the exact direction that
-  is 697.5° clockwise from due north?",
-  "North north west",
-  :STEM
-)
+  # it 'can create three '
+  #
+  #   card_2 = Card.new(
+  #     "The Viking spacecraft sent back to Earth photographs and
+  #     reports about the surface of which planet?",
+  #     "Mars",
+  #     :STEM
+  #   )
+  #
+  #   card_3 = Card.new("Describe in words the exact direction that
+  #     is 697.5° clockwise from due north?",
+  #     "North north west",
+  #     :STEM
+  #   )
+  end
