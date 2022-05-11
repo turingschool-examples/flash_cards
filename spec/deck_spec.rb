849e212ef1441410103ef717e61ecce4b92c4d98
Jonathan Pope <jonathanmpope@gmail.com>
2022-05-11 12:36:03 -0600

    Add finished deck test 2 and passing code

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,7 +3,7 @@ require './lib/card'
 require './lib/deck'
 
 describe 'Deck' do
-  it 'has a deck of cards' do
+  it 'is a deck' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -12,5 +12,13 @@ describe 'Deck' do
     expect(deck).to be_instance_of Deck
   end
 
+  it 'has a deck of cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq [card_1, card_2, card_3]
+  end 
 
 end
