39588e1cc47f3005030f37a78a167175a30f77ad
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-11 10:20:54 -0400

    Add take_turn test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -59,7 +59,25 @@ RSpec.describe Deck do
 
     round = Round.new(deck)
 
-    expect(round.current_card).to eq(deck[0])
+    expect(round.current_card).to eq(card_1)
+
+  end
+
+  it 'take_turn takes a guess as an argument' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+
+    expect(new_turn).to eq(card_1 + "Juneau")
 
   end
 
