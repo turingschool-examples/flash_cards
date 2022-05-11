2ab60c32bda830b5fa11eab7dcaa3c981735d63d
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-10 23:18:33 -0400

    Add test to check turns method

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -7,7 +7,7 @@ require './lib/round'
 
 RSpec.describe Deck do
 
-  it 'exists' do
+  xit 'exists' do
 
     round = Round.new()
 
@@ -28,6 +28,22 @@ RSpec.describe Deck do
     round = Round.new(deck)
 
     expect(round.deck).to eq(deck)
+
+  end
+
+  it 'returns empty array on .turns before turns called' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    expect(round.turns).to eq([])
 
   end
 
