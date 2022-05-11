bf3e8f5d0411fb60d440d15f8f99732b4020884f
Bryce Simonds <bsimonds14@gmail.com>
2022-05-10 18:18:43 -0600

    add a second test and pass

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,9 +5,16 @@ require './lib/turn.rb'
 describe Turn do
   describe 'argument' do
     it 'is initialized with two arguments' do
-      turn1 = Turn.new("Junea", "Placeholder for current card")
-      expect(turn1.guess).to eq("Junea")
-      expect(turn1.card).to eq("Placeholder for current card")
+      turn = Turn.new("Junea", "Placeholder for current card")
+      expect(turn.guess).to eq("Junea")
+      expect(turn.card).to eq("Placeholder for current card")
     end
   end
 end
+
+describe 'guess' do
+  it 'returns the guess' do
+  turn = Turn.new("Junea", "Placeholder")
+  expect(turn.guess). to eq("Junea")
+  end
+end 
