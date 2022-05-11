59bdd18d02f69883ef317ee401121886fb1cfc5f
Anna Marie Sterling <sterling.am@gmail.com>
2022-05-10 18:15:35 -0600

    Add ReactiveNonmetals card 2

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -21,6 +21,18 @@ RSpec.describe Card do
     expect(card.answer).to eq("Hydrogen")
   end
 
+  it 'has 2 out of 11 questions for this category' do
+    card = Card.new("Which element has the symbol 'C'?", "Carbon", :ReactiveNonmetals)
+
+    expect(card.question).to eq("Which element has the symbol 'C'?")
+  end
+
+  it 'has 2 out of 11 answers for this category' do
+    card = Card.new("Which element has the symbol 'C'?", "Carbon", :ReactiveNonmetals)
+
+    expect(card.answer).to eq("Carbon")
+  end
+
   it 'has a category' do
     card = Card.new("Which element has the symbol 'H'?", "Hydrogen", :ReactiveNonmetals)
 
