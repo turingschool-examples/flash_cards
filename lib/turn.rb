23460e975f67da020093113c7e30ebb0dc71d1b6
Ethan Nguyen <etnguyen16@gmail.com>
2022-05-10 19:18:53 -0600

    add feedback

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    return guess == card.answer
+  end
+
+  def feedback
+    if correct?
+      return "Correct!"
+
+    else
+      return "Incorrect."
+    end
+  end
+
+
+
+end
