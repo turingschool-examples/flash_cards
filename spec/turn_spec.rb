0729ccc98f0569bc041c400aa367c89ada6f4a99
Jonathan Pope <jonathanmpope@gmail.com>
2022-05-10 18:01:03 -0600

    Add better descriptions to rspec file

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,39 +3,39 @@ require './lib/turn'
 require './lib/card'
 
 describe Turn do
-  it 'card exists' do
+  it 'card is an instance of Card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     expect(card).to be_instance_of(Card)
   end
 
-  it 'turn exists' do
+  it 'turn is an instance of Turn and accepts card as an argument' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn).to be_instance_of(Turn)
   end
 
-  it 'card can be called' do
+  it 'you can access card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     turn.card
     expect(turn.card).to eq(card)
   end
 
-  it 'guess can be called' do
+  it 'you can access guess' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     turn.guess
     expect(turn.guess).to eq('Juneau')
   end
 
-  it 'can determine if the guess is correct' do
+  it 'determine if the guess is correct' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     turn.correct?
     expect(turn.correct?).to eq true
   end
 
-  it 'can respond depending on the answer' do
+  it 'respond appropriately to a correct answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     turn.feedback
@@ -49,7 +49,7 @@ describe Turn do
     expect(turn.correct?).to eq false
   end
 
-  it 'can handle incorrect guesses and return the right message' do
+  it 'can handle incorrect guesses and return the correct message' do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     turn.feedback
