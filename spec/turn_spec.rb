00dcdb1423094d186e51e33d4e006249d118040d
John Santosuosso <john.santosuosso1@gmail.com>
2022-05-10 22:42:41 -0400

    add TDD to turn spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,46 +1,59 @@
+require 'rspec'
 require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
-  it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn).to be_instance_of(Turn)
+    before do
+      @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      @turn = Turn.new("Juneau", @card)
+    end
+
+  describe '#initialize' do
+    it 'is an instance of turn' do
+      expect(@turn).to be_instance_of(Turn)
+    end
   end
 
-  it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.guess).to eq("Juneau")
+  it 'has a guess by default' do
+    expect(@turn.guess).to eq("Juneau")
   end
 
-  it 'has an card' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.card).to eq(card)
+
+  describe '#correct?' do
+    it 'identifies a correct guess' do
+      expect(@turn.correct?).to eq true
+    end
+
+    it 'identifies a case insensitive correct guess' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("juneau", card)
+      expect(turn.correct?).to eq true
+    end
+
+    it 'identifies an incorrect guess' do
+      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      turn = Turn.new("saturn", card)
+      expect(turn.correct?).to eq false
+    end
   end
 
-  it 'identifies a correct guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.correct?).to eq true
-  end
+  describe '#correct?' do
+    it 'gives correct feedback on a correct answer' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
+      expect(turn.feedback?).to eq('Correct')
+    end
 
-  it 'identifies an incorrect guess' do
-    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    turn = Turn.new("Saturn", card)
-    expect(turn.correct?).to eq false
-  end
+    it 'gives correct feedback on a case insensitive correct answer' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("juneau", card)
+      expect(turn.feedback?).to eq('Correct')
+    end
 
-  it 'gives feedback on a correct answer' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.feedback?).to eq('Correct')
-  end
-
-  it 'gives feedback on an incorrect answer' do
-    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    turn = Turn.new("Saturn", card)
-    expect(turn.feedback?).to eq('Incorrect')
+    it 'gives correct feedback on an incorrect answer' do
+      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      turn = Turn.new("Saturn", card)
+      expect(turn.feedback?).to eq('Incorrect')
+    end
   end
 end
