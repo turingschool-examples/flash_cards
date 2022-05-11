4d035e20ec241d7ca65321ef38fd43aeab474962
John Santosuosso <john.santosuosso1@gmail.com>
2022-05-10 22:24:13 -0400

    refactor take turn method

--- lib/round.rb
+++ lib/round.rb
@@ -16,19 +16,33 @@ class Round
     @deck.cards.first
   end
 
-  def take_turn(guess)
+  def attempts_correct_answer(guess)
     @attempt = Turn.new(guess, deck.cards.first)
+  end
+
+  def store_guess_as_turn
     @turns << @attempt
-    @attempt.guess == @attempt.card.answer ? @number_correct += 1 : @number_correct
+  end
+
+  def new_card_added_to_top_of_deck
     @deck.cards.rotate!(1)
+  end
+
+  def take_turn(guess)
+    guess = guess.downcase.capitalize
+    attempts_correct_answer(guess)
+    @attempt.guess == @attempt.card.answer.downcase.capitalize ? @number_correct += 1 : @number_correct
+    store_guess_as_turn
+    new_card_added_to_top_of_deck
     @attempt
   end
+
 
   def number_correct_by_category(category)
     total_questions = []
     total_correct = []
     total_questions << @turns.each { |turn| turn.card.category == category ? total_questions << turn : turn }
-    total_correct << @turns.each { |turn| turn.card.category == category && turn.guess == turn.card.answer ? total_correct << turn : turn }
+    total_correct << @turns.each { |turn| turn.card.category == category && turn.guess == turn.card.answer.downcase.capitalize ? total_correct << turn : turn }
     (total_correct.length / total_questions.length.to_f).round(2) * 100
   end
 
