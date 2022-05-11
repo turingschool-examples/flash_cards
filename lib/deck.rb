01091163733b4fc569b34dbd7d3f0ad4014fec80
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-10 23:05:28 -0400

    Add cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,4 +7,17 @@ class Deck
     @count = cards.length
   end
 
+  def cards_in_category(category)
+    
+    return_array = []
+
+    cards.each do |card|
+      if card.category == category
+        return_array << card
+      end
+    end
+
+    return return_array
+  end
+
 end
