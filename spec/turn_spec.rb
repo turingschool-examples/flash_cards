405a841590632347aeda9d38d0f99153e83cac3f
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-10 18:09:52 -0700

    Test for initialize and code for initialize method for turn class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,1 +1,18 @@
+require './lib/turn'
+require './lib/card'
 
+RSpec.describe Turn do
+  before :each do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
+  it "exists" do
+    expect(@turn).to be_a(Turn)
+  end
+
+  it "has attributes" do
+    expect(@turn.card).to eq(@card)
+  end
+
+end
