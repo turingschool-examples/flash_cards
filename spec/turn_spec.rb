2fbe68eb149d3de5b534b0398efdb8bb1a8090b3
Ethan Nguyen <etnguyen16@gmail.com>
2022-05-10 21:47:12 -0600

    add turn_spec.rb

---
+++ spec/turn_spec.rb
@@ -0,0 +1,36 @@
+require 'rspec'
+require './turn'
+require './card'
+
+RSpec.describe Turn do
+  card = Card.new("What is the capital of Alaska" "Juneau", :Geography)
+  turn = Turn.new("Juneau", card)
+
+  it "exists" do
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it "has a card" do
+    card = Card.new("What is the capital of Alaska" "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+  it "has a guess" do
+    card = Card.new("What is the capital of Alaska" "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it "checks if the guess is correct" do
+    expect(turn.correct?).to eq(true)
+  end
+
+  it "can give the user feedback" do
+   expect(turn.feedback).to eq("Correct!")
+ end
+
+
+end
