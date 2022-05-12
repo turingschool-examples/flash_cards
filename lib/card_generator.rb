6888913bcb607831bbbea3db2ca9d54db836aff8
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-12 17:29:03 -0500

    Test: Passed first test to store a text file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -2,24 +2,24 @@ require './lib/card'
 
 class CardGenerator
 
-  attr_accessor :card_deck
+  attr_accessor :filename, :card_deck
 
   def initialize(filename)
     @filename = filename
     @card_deck = []
   end
 
-  def cards
-    # @filename
-    
-    contents = File.read(@filename)
-    p contents
-    array = contents.split("\n")
-    # turn each content into a string
-
-    # do this 4 times
-    @card_deck << Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    # Deck.new(card_deck)
-  end
+  # def cards
+  #   # @filename
+  #   file = File.open(@filename, "r")
+  #   contents = File.read(@filename)
+  #   p contents
+  #   array = contents.split("\n")
+  #   # turn each content into a string
+  #
+  #   # do this 4 times
+  #   @card_deck << Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #
+  #   # Deck.new(card_deck)
+  # end
 end
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -6,10 +6,14 @@ RSpec.describe CardGenerator do
 
     filename = "cards.txt"
     generator = CardGenerator.new(filename)
-    text_to_cards = generator.cards
-    binding.pry
-    expect(generator).to be_instance_of(CardGenerator)
+
+    expect(generator.filename).to eq("cards.txt")
   end
+
+    # text_to_cards = generator.cards
+    # binding.pry
+    # expect(generator).to be_instance_of(CardGenerator)
+  # end
 
 
 end
