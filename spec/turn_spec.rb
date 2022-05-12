db58a0b7e93c3634627df9635b728b17cdcbd9a9
Sunny Moore <moore.sunny@gmail.com>
2022-05-12 14:33:10 -0700

    Added test turn having a card

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,21 +6,32 @@ require './lib/card'
 describe 'Iteration 1' do
   describe 'Turn' do
     describe '#initialize' do
+
       it "it exists" do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         turn = Turn.new("Juneau", card)
 
         expect(turn).to be_a(Turn)
       end
+
       it "has a guess" do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         turn = Turn.new("Juneau", card)
 
         expect(turn.guess).to eq("Juneau")
       end
+      it "has a card" do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
 
+        expect(turn.card).to be_a(Card)
+      end
     end
 
+    describe '#correct?' do
+      it 'is true or false'
+
+    end
 
   end
 end
