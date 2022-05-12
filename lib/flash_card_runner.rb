f996698e4cee5d39e4bbbf7366bd37e24ab51e96
philmarcu <philmark18@gmail.com>
2022-05-12 09:21:04 -0600

    Finish Iteration 3

--- lib/flash_card_runner.rb
+++ lib/flash_card_runner.rb
@@ -6,11 +6,12 @@ require './lib/round'
 @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-@card_4 = Card.new("How many licks does it take to get to the center of a tootsy pop?", "1337", "Pop Culture")
+@card_4 = Card.new("What's the best place in the world?", "Home", :Misc)
 @cards = [@card_1, @card_2, @card_3, @card_4]
 @deck = Deck.new(@cards)
 @round = Round.new(@deck)
 
 @round.start
+@round.display_results
 
 require "pry"; binding.pry
