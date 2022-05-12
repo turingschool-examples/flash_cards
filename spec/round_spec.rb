05cddc2d6e46b0e01405f65b1040d4dafed9c73b
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 21:56:38 -0600

    Create test for if the round has a deck

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -19,4 +19,19 @@ describe Round do
 
     expect(round).to be_a Round
   end
+
+  it "Has a deck" do
+    card1 = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
+    card2 = Card.new("What do the letters CPU stand for in computing?", "Central Processing Unit", :Technology)
+    card3 = Card.new("What do the letters GPU stand for in computing?", "Graphics Processing Unit", :Technology)
+    card4 = Card.new("What does NBA stand for in US Sports?", "National Basketball Association", :Sports)
+
+    cards = [card1, card2, card3, card4]
+
+    deck = Deck.new(cards)
+
+    round = Round.new(deck)
+
+    expect(round.deck).to eq deck
+  end
 end
