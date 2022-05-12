79d1db30f87f08bc9d5a45e99a7d55e522bafc45
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-05-11 23:56:02 -0600

    create turn test file, worked on method feedback

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,26 @@
 class Turn
 
-  attr_reader :string, :card
+  attr_reader :string, :card, :guess, :correct, :feedback
 
   def initialize(string, card)
     @string = string
     @card = card
   end
 
+  def guess
+    #this method returns the guess
+  end
+
+  def correct?
+    
+  end
+
+  def feedback
+    if @correct == true
+      puts "Correct"
+    else
+      puts "Incorrect."
+    end
+  end
 
 end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,4 @@
+require './lib/card'
+require './lib/turn'
+
+RSpec.describe Turn do
