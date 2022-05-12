a3872ab321391251053908724c86793eef59702f
mkbonini <mkbonini@gmail.com>
2022-05-11 20:23:19 -0600

    add test for percent_correct

--- lib/round.rb
+++ lib/round.rb
@@ -1,37 +1,37 @@
 class Round
-require 'turn'
-attr_reader :deck, :turns
-def initialize(deck)
-  @deck = deck
-  @turns = []
-  @guess = 0
-end
-def current_card
-  return deck.cards[@guess]
-end
+  # require 'turn'
+  attr_reader :deck, :turns
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+    @guess = 0
+  end
+  def current_card
+    return deck.cards[@guess]
+  end
 
-def take_turn(guess)
-    turn = Turn.new(guess, deck.cards[@guess])
-    @turns << turn
-    @guess += 1
-    return turn
-end
-def number_correct
-  correct = 0
-  @turns.each do |turn|
-    if turn.correct?
-      correct += 1
-    end
+  def take_turn(guess)
+      turn = Turn.new(guess, deck.cards[@guess])
+      @turns << turn
+      @guess += 1
+      return turn
   end
-  return correct
-end
-def number_correct_by_category(category)
-  correct = 0
-  @turns.each do |turn|
-    if turn.correct? && turn.card.category == category
-      correct += 1
+  def number_correct
+    correct = 0
+    @turns.each do |turn|
+      if turn.correct?
+        correct += 1
+      end
     end
+    return correct
   end
-  return correct
-end
+  def number_correct_by_category(category)
+    correct = 0
+    @turns.each do |turn|
+      if turn.correct? && turn.card.category == category
+        correct += 1
+      end
+    end
+    return correct
+  end
 end
