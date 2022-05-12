24425d19c41edef3a13264958bd7b91214113c23
Paker Thomson <pdthomson1239@gmail.com>
2022-05-11 23:07:55 -0400

    erase and refactor test spec and card methods to get rid of add cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,16 +6,12 @@ class Deck
     @cards = cards
   end
 
-  # def add_cards(card)
-  #   @cards << card
-  # end
-
   def count
     @cards.length
   end
 
   def cards_in_category(category)
-    @cards.select do |card| 
+    @cards.select do |card|
       category == card.category
     end
   end
