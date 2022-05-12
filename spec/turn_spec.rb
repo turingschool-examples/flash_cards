fb866a0061212db413ce63901d13f44e829500b1
Thiago Silveira <thiago1992@yahoo.com>
2022-05-11 23:50:33 -0600

    Test: Add turn can check if guess is correct

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -9,17 +9,24 @@ RSpec.describe Turn do
     expect(turn).to be_a(Turn)
   end
 
-  it 'returns a guess' do
+  it 'can return a guess' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.guess).to eq("Juneau")
   end
 
-  it 'returns a card' do
+  it 'can return a card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.card).to eq(card)
+  end
+
+  it 'can check if the guess matched the answer on the card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to eq(true)
   end
 end
