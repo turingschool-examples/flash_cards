76651cb39b466d190575d1f8f908d3c39dd2583f
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-12 14:02:13 -0400

    Add number_correct test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -151,9 +151,25 @@ RSpec.describe Deck do
 
     new_turn_1 = round.take_turn("Juneau")
 
-
-
     expect(round.turns).to eq([new_turn_1])
+
+  end
+
+  it 'keeps track of number of correct guesses' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    new_turn_1 = round.take_turn("Juneau")
+
+    expect(round.number_correct).to eq(1)
 
   end
 
