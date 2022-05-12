ad3f5c6ca6c399f05b58534527bb4a67655718db
mkbonini <mkbonini@gmail.com>
2022-05-11 19:31:46 -0600

    Add turns method test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -24,6 +24,18 @@ RSpec.describe Round do
     deck = Deck.new(cards)
     round = Round.new(deck)
 
-    expect(round.deck).to eq(deck) 
+    expect(round.deck).to eq(deck)
+  end
+
+  it 'holds turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.turns).to eq([]) 
   end
 end
