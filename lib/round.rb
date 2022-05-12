0cd498b238146dae19b6a541657417ee4858c9ff
Michael Koulouvaris <mikekoul@gmail.com>
2022-05-12 09:06:49 -0600

    Add current_card test and method

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,12 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :current_card
   def initialize(deck)
     @deck = deck
     @turns = []
   end
+
+  def current_card
+    deck.cards[0]
+  end
+
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -53,5 +53,13 @@ describe Round do
     round = Round.new(deck)
     expect(round.turns).to eq([])
   end
+  it "return the current card" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new(card_1, card_2, card_3)
+    round = Round.new(deck)
+    expect(round.current_card).to eq(deck.cards[0])
+  end
 
 end
