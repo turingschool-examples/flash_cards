e59c461c8c63e040a50b62f938845880d782d787
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-11 20:15:23 -0600

    Iteration 1 - add turn test

---
+++ spec/turn_spec.rb
@@ -0,0 +1,66 @@
+require './lib/card'
+require './lib/turn'
+
+
+RSpec.describe Turn do
+    it 'exists' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
+
+      expect(turn).to be_instance_of(Turn)
+    end
+    it 'has a guess' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau" , card)
+
+      expect(turn.guess).to eq("Juneau")
+    end
+    it 'has a card' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
+
+      expect(turn.card).to eq(card)
+    end
+
+    it 'is correct?' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
+
+      expect(turn.correct?).to eq(true)
+    end
+
+    it 'it has feedback?' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
+
+      expect(turn.feedback).to eq("Correct!")
+    end
+
+    it 'has a guess' do
+      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      turn = Turn.new("Saturn" , card)
+
+      expect(turn.guess).to eq("Saturn")
+    end
+    it 'has a card' do
+      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      turn = Turn.new("Saturn" , card)
+
+      expect(turn.card).to eq(card)
+    end
+
+    it 'is correct?' do
+      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      turn = Turn.new("Saturn" , card)
+
+      expect(turn.correct?).to eq(false)
+    end
+
+    it 'it has feedback?' do
+      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      turn = Turn.new("Saturn" , card)
+
+      expect(turn.feedback).to eq("Incorrect.")
+    end
+
+  end
