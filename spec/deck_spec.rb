00812bfdaefb5a4d51358a633b5b68de78bcd170
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 20:35:13 -0600

    Add initial test for if Deck exists

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,17 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
+
+describe Deck do
+  it 'Deck exists' do
+    card1 = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
+    card2 = Card.new("What do the letters CPU stand for in computing?", "Central Processing Unit", :Technology)
+
+    cards = [card1, card2]
+
+    deck = Deck.new(cards)
+
+    expect(deck).to be_a Deck
+  end
+
+end
