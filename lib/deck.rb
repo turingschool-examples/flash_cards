fbda22b7705faf455fc50ee639932b8080f02610
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 21:24:40 -0600

    Add logic for cards_in_category to pass tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,7 +11,15 @@ class Deck
   end
 
   def cards_in_category(category)
-
+    category_cards = []
+    # Iterate through all of the cards in the deck
+    # If the category of the given card == category parameter, shovel into category_cards
+    @cards.each do |card|
+      if card.category == category
+        category_cards << card
+      end
+    end
+    return category_cards
   end
 
 end
