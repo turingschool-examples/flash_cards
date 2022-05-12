d77a4e0724d96b2a81726cc381e34afa76a57534
Nick Jones <nickjones9605@gmail.com>
2022-05-11 20:00:50 -0600

    Add turn and turn test

---
+++ spec/turn_spec.rb
@@ -0,0 +1,21 @@
+require './lib/turn'
+require './lib/card'
+require 'rspec'
+RSpec.describe Turn do
+
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it "has a guess" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau",card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+
+end
