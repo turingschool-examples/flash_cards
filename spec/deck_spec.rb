99b100e6cd2441cbd2497091504ba5289ea4b369
Michael Koulouvaris <mikekoul@gmail.com>
2022-05-11 20:57:10 -0600

    Update Geography test and Pop Culture test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -52,4 +52,22 @@ describe Deck do
       expect(deck.found_cards).to eq([card_2, card_3])
     end
 
+    it "cards in Geography category" do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new(card_1, card_2, card_3)
+      deck.cards_in_category(:Geography)
+      expect(deck.found_cards).to eq([card_1])
+    end
+
+    it "cards in Pop Culture category" do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new(card_1, card_2, card_3)
+      deck.cards_in_category("Pop Culture")
+      expect(deck.found_cards).to eq([])
+    end
+
 end
