757ad457165d1f1eb6a330ed406e0946587afe71
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-12 10:46:30 -0400

    Update methods for correct guesses

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,25 @@
 class Turn
-
-  def initialize(string, card)
+  attr_reader :guess, :card
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
   end
 
-  
+  def guess
+    @guess
+
+  end
+
+  def correct?
+    @guess == @card.answer
+  end
+
+  def feedback
+    if @guess == @card.answer
+      "Correct!"
+    else
+      "Incorrect."
+    end
+
+  end
+end
