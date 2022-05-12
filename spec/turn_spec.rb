0f30201d556fbf8f14161fd8ac0ab716d0352405
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-12 12:48:51 -0700

    Add test to turn spec to check for card

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Turn do
   end
 
   it "can give correct response" do
+    expect(@turn.card).to eq @card
     expect(@turn.guess).to eq "Juneau"
     expect(@turn.correct?).to eq true
     expect(@turn.feedback).to eq "Correct!"
@@ -21,6 +22,7 @@ RSpec.describe Turn do
     card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn2 = Turn.new("Saturn", card2)
 
+    expect(turn2.card).to eq card2
     expect(turn2.guess).to eq "Saturn"
     expect(turn2.correct?).to eq false
     expect(turn2.feedback).to eq "Incorrect."
