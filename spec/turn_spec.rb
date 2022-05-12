bb77b68b7a514390a828768d238cebd53ca2f110
Justin Ramirez <jusrez528@gmail.com>
2022-05-12 18:21:58 -0400

    Create test for card method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Turn do
 
     expect(turn).to be_instance_of(Turn)
   end
+
+	it 'returns a card' do
+		card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+		turn = Turn.new("Juneau", card)
+
+		expect(turn.card).to eq(card)
+	end
+	
 end
