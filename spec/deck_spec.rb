2c3eaed4fae4b7f5ca04cd4a7f1b1af77129d2dc
Anna Marie Sterling <sterling.am@gmail.com>
2022-05-12 17:38:39 -0600

    Passed Deck file tests I2

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,16 +5,30 @@ require 'pry'
 
 RSpec.describe Deck do
 
-  it 'stores cards' do
-    card_1 = Card.new("Which element has the symbol 'H'?", "Hydrogen", :ReactiveNonmetals)
-
-    card_2 = Card.new("Which element has the symbol 'Ne'?", "Neon", :NobleGases)
-
-    card_3 = Card.new("Which element has the symbol 'B'?", "Boron", :Metalloids)
-
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-
+  before :each do
+    @card_1 = Card.new("Which element has the symbol 'H'?", "Hydrogen", :ReactiveNonmetals)
+    @card_2 = Card.new("Which element has the symbol 'Ne'?", "Neon", :NobleGases)
+    @card_3 = Card.new("Which element has the symbol 'B'?", "Boron", :Metalloids)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
   end
 
+  it 'is a collection of flash cards' do
+
+    expect(@deck).to be_instance_of Deck
+    expect(@deck.cards).to eq(@cards)
   end
+
+  it 'is a count of cards in the deck' do
+
+    expect(@deck.count).to eq(3)
+  end
+
+  it 'has categories' do
+
+    expect(@deck.cards_in_category(:ReactiveNonmetals)).to eq([@card_1])
+    expect(@deck.cards_in_category(:NobleGases)).to eq([@card_2])
+    expect(@deck.cards_in_category(:Metalloids)).to eq([@card_3])
+  end
+
+end
