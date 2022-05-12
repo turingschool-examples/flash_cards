d17002944a7236f04d81cd880f90ed02b80127f3
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-12 14:53:09 -0400

    Add 'incorrect guesses' tests and refactor

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,30 +2,51 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
-  it "exists" do
+  describe "Correct guesses:" do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-    expect(turn).to be_an_instance_of Turn
+
+    it "exists" do
+      expect(turn).to be_an_instance_of Turn
+
+    end
+
+    it "has guess" do
+      expect(turn.guess).is_a? String
+
+    end
+
+    it "is correct" do
+      expect(turn.correct?).to be(true)
+    end
+
+    it "returns feedback" do
+      expect(turn.feedback).to eq("Correct!")
+    end
 
   end
 
-  it "has guess" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.guess).to eq("Juneau")
+  describe "Incorrect guesses:" do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
 
-  end
+    it "exists" do
+      expect(turn).to be_an_instance_of Turn
 
-  it "is correct" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.correct?).to be(true)
-  end
+    end
 
-  it "returns feedback" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.feedback).to eq("Correct!")
+    it "has guess" do
+      expect(turn.guess).to eq("Saturn")
+
+    end
+
+    it "is correct" do
+      expect(turn.correct?).to be(false)
+    end
+
+    it "returns feedback" do
+      expect(turn.feedback).to eq("Incorrect.")
+    end
   end
 
 end
