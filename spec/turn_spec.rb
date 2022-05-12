6bab3a70b91fc39745b95d37e5a6a665156f0156
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-12 10:47:13 -0400

    Update tests for correct guesses

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,8 +1,31 @@
 require './lib/turn'
+require './lib/card'
 
 RSpec.describe Turn do
+  it "exists" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn).to be_an_instance_of Turn
 
+  end
 
+  it "has guess" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
 
+  end
+
+  it "is correct" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to be(true)
+  end
+
+  it "returns feedback" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
 
 end
