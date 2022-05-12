ca621ac2c5ea70a82536a18fb2075a29bfe410d7
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 20:50:00 -0600

    Add test for cards_in_category method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -34,6 +34,20 @@ describe Deck do
     deck = Deck.new(cards)
 
     expect(deck.count).to eq 2
+  end
 
+  it "Returns how many cards of a given category are in the deck" do
+    card1 = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
+    card2 = Card.new("What do the letters CPU stand for in computing?", "Central Processing Unit", :Technology)
+    card3 = Card.new("What do the letters GPU stand for in computing?", "Graphics Processing Unit", :Technology)
+    card4 = Card.new("What does NBA stand for in US Sports?", "National Basketball Association", :Sports)
+
+    cards = [card1, card2, card3, card4]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:Technology)).to eq 2
+    expect(deck.cards_in_category(:Sports)).to eq 1
+    expect(deck.cards_in_category(:Video_Games)).to eq 0
   end
 end
