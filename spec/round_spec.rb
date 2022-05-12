f5d3766ed4abb8c670bb13d0a679db24f2e7ce17
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 21:54:45 -0600

    Create test for if Round exists

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -3,3 +3,20 @@ require './lib/card'
 require './lib/deck'
 require './lib/turn'
 require './lib/round'
+
+describe Round do
+  it "Round exists" do
+    card1 = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
+    card2 = Card.new("What do the letters CPU stand for in computing?", "Central Processing Unit", :Technology)
+    card3 = Card.new("What do the letters GPU stand for in computing?", "Graphics Processing Unit", :Technology)
+    card4 = Card.new("What does NBA stand for in US Sports?", "National Basketball Association", :Sports)
+
+    cards = [card1, card2, card3, card4]
+
+    deck = Deck.new(cards)
+
+    round = Round.new(deck)
+
+    expect(round).to be_a Round
+  end
+end
