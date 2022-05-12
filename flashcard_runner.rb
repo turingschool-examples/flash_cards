68f8c67a33980433460d0ce84cb5334e65b93e09
Eli Sachs <easachs13@gmail.com>
2022-05-11 18:10:15 -0600

    Update Flashcard runner, add cards (iteration 3)

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,12 +3,14 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-card_1 = Card.new("What is the capital of Togo?", "lome", :Geography)
-card_2 = Card.new("The _______ Trench is the deepest point on Earth.", "mariana", :Geography)
+card_1 = Card.new("What is the capital of Togo?", "Lome", :Geography)
+card_2 = Card.new("The _______ Trench is the deepest point on Earth.", "Mariana", :Geography)
 card_3 = Card.new("What year did the Titanic sink?", "1912", :History)
-card_4 = Card.new("Salvador _______ is a Spanish surrealist painter.", "dali", :Art)
-# card_5 = Card.new("Who painted 'La Guernica'?", "picasso", :Art)
-# card_6 = Card.new("The Count of Monte Cristo is set in which country?", "france", :Literature)
-deck_1 = Deck.new([card_1, card_2, card_3, card_4])
-round = Round.new(deck_1)
+card_4 = Card.new("Salvador _______ is a Spanish surrealist painter.", "Dali", :Art)
+card_5 = Card.new("Who painted 'La Guernica'?", "Picasso", :Art)
+card_6 = Card.new("The Count of Monte Cristo is set in which country?", "France", :Literature)
+card_7 = Card.new("What does the Spanish word 'embarazada' mean?", "Pregnant", :Languages)
+card_8 = Card.new("What county is Centennial, CO in?", "Arapahoe", :Geography)
+deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8])
+round = Round.new(deck)
 round.start
