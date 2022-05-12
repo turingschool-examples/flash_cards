9f2b7be7ed6f4c040896814c448d2d860b86dcf5
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-12 16:51:00 -0500

    Test: Add card_generator_spec.rb

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,13 @@
+require './lib/card_generator'
+require 'pry'
+
+RSpec.describe CardGenerator do
+  it 'can store a text file' do
+
+    filename = "cards.txt"
+    generator = CardGenerator.new(filename)
+    text_to_cards = generator.cards
+    binding.pry 
+    expect(generator).to be_instance_of(CardGenerator)
+  end
+end
