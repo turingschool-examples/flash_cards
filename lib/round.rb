ce098d096a35a988566a7695c5747c2eb5aa1749
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 21:49:35 -0600

    Add outline of methods based on interaction pattern

--- lib/round.rb
+++ lib/round.rb
@@ -4,4 +4,27 @@ class Round
     @deck = deck
   end
 
+  def turns
+
+  end
+
+  def take_turn(guess)
+
+  end
+
+  def number_correct
+
+  end
+
+  def current_card
+
+  end
+
+  def number_correct_by_category(category)
+
+  end
+
+  def percent_correct_by_category(category)
+
+  end
 end
