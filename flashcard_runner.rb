69d82a7477cabb222abea4706577aec97162aaad
mkbonini <mkbonini@gmail.com>
2022-05-12 15:36:58 -0600

    Implement card generator in runner file.

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,11 +2,13 @@ require './lib/card'
 require './lib/deck'
 require './lib/turn'
 require './lib/round'
+require './lib/card_generator'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-cards = [card_1, card_2, card_3]
+# card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+# card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+# cards = [card_1, card_2, card_3]
+cards = CardGenerator.new("cards.txt").cards
 deck = Deck.new(cards)
 round = Round.new(deck)
 categories = []
@@ -17,7 +19,7 @@ deck.count.times do
   puts "This is card #{round.turns.length + 1} out of #{deck.count}."
   puts "Question: #{round.current_card.question}"
   guess = gets.chomp
-  puts round.take_turn(guess.capitalize).feedback
+  puts round.take_turn(guess).feedback
 end
 puts "****** Game over! ******"
 puts "You had #{round.number_correct} correct guesses out of #{deck.count} for a total score of #{round.percent_correct.to_i}%."
--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,13 +1,13 @@
 class CardGenerator
-  #require './cards.txt'
   attr_reader :cards
   def initialize(filename)
     @filename = filename
     @cards = []
-    @card_data = File.readlines(File.join(__dir__, filename))
-    @card_data.each do |line|
-       @card_input = line.split(",")
-       @cards << Card.new(card_input[0].chomp,card_input[1].chomp,card_input[2].chomp)
+    card_input = []
+    card_data = File.readlines(File.join(__dir__, filename))
+    card_data.each do |line|
+       card_input = line.split(",")
+       @cards << Card.new(card_input[0],card_input[1],card_input[2].chomp)
      end
   end
 
