c1887055dacb686b952ac35a9b7fc1e21a2a583f
Nicole Esquer <nicole.esquer5@gmail.com>
2022-05-12 13:52:43 -0700

    Passed has a card and has a guess test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,8 +1,30 @@
 require './lib/turn'
-require './lib/turn'
+require './lib/card'
 
 describe Turn do
+  it "has a card" do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
 
+  expect(@turn.card).to eq(@card)
+end
+
+it "has a guess" do
   @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  @turn = Turn.new("Juneau", card)
+  @turn = Turn.new("Juneau", @card)
+
+  expect(@turn.guess).to eq("Juneau")
+end
+
+describe "correct?" do
+  @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  @turn = Turn.new("Juneau", @card)
+
+if
+  expect(@turn.correct?).to eq(true)
+  end
+
+
+  end
+
 end
