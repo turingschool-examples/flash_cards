d41a7a2444a62151410a4b53365a2ff152ec9de5
Blake Saylor <blakesaylor@gmail.com>
2022-05-12 13:59:57 -0600

    Update to pass current card test and include comments

--- lib/round.rb
+++ lib/round.rb
@@ -10,18 +10,30 @@ class Round
   end
 
   def number_correct
+    # Needs to iterate through turns
+    #
+    # number_correct = 0
+    # round.turns.each do |turn|
+    #   If guess == answer
+    #     number_correct += 1
+    #   end
+    # end
+    #
+    # return number_correct
 
   end
 
   def current_card
-
+    return deck.cards[turns.count]
   end
 
   def number_correct_by_category(category)
+    # Needs to compare number_correct to count of turns
 
   end
 
   def percent_correct_by_category(category)
+    # Needs to compare number_correct to count of turns then return as a percentage
 
   end
 end
