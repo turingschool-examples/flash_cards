fa2fd9a079e3d4013f1990cafb79a873a6bb0096
Nick Jones <nickjones9605@gmail.com>
2022-05-12 14:55:08 -0600

    Add to Deck class methods; count, cards in category. Add deck tests exists, returns count of cards.

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,4 +3,17 @@ class Deck
   attr_reader :cards
 
   def initialize(cards)
+    @cards = cards
   end
+
+  def count
+    cards.size
+  end
+
+  def cards_in_category(category)
+    cards.select do |card|
+      card.category == category
+    end
+
+end
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,27 @@
 require './lib/card'
-require 'rspec'
 require './lib/deck'
+require './lib/turn'
+require 'rspec'
+
+RSpec.describe Deck do
+
+  it 'exists' do
+
+    card1 = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capitol of Texas?", "Austin", :Geography)
+    card3 = Card.new("Who is the first officer of the Starship Enterprise D?", "Riker", :Star_Trek_Trivia)
+    card4 = Card.new("Who is the chief engineer of the Starship Enterprise D?", "La Forge", :Star_Trek_Trivia)
+    deck = Deck.new([card1, card2, card3,card4])
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it "returns a count of cards" do
+  card1 = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+  card2 = Card.new("What is the capitol of Texas?", "Austin", :Geography)
+  card3 = Card.new("Who is the first officer of the Starship Enterprise D?", "Riker", :Star_Trek_Trivia)
+  card4 = Card.new("Who is the chief engineer of the Starship Enterprise D?", "La Forge", :Star_Trek_Trivia)
+  deck = Deck.new([card1, card2, card3, card4])
+  expect(deck.count).to eq 4
+  end
+
+end
