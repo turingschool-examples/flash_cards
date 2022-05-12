85493cc5a338d3ca2a8da1dcb8b81cb4b8a33b54
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 20:44:43 -0600

    Create test for count

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -24,4 +24,16 @@ describe Deck do
 
     expect(deck.cards).to eq [card1, card2]
   end
+
+  it "Returns how many cards are in the deck" do
+    card1 = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
+    card2 = Card.new("What do the letters CPU stand for in computing?", "Central Processing Unit", :Technology)
+
+    cards = [card1, card2]
+
+    deck = Deck.new(cards)
+
+    expect(deck.count).to eq 2
+
+  end
 end
