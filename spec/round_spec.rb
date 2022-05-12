90d671a05715cfea9a603bb14673a7b86ed9bce9
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-12 17:12:30 -0600

    Iteration 2 - add tests: initialize, current_card, take_turn

---
+++ spec/round_spec.rb
@@ -0,0 +1,50 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+RSpec.describe Round do
+  it 'exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+  end
+
+  it 'gives current_card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.current_card).to eq(card_1)
+  end
+
+  it 'take_turn gives instance of turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    expect(round.take_turn("Juneau")).to be_instance_of(Turn)
+    expect(new_turn.class).to eq(Turn)
+  end
+
+
+end
+
+
+
+# new_turn = round.take_turn("Juneau")
+#
+# expect(new_turn).to be_instance_of(Turn)
+# expect(new_turn.correct?).to eq(true)
+# expect(round.turns).to eq([new_turn])
+# expect(round.current_card).to eq(card_2)
