2c6f5dadf7761e4dff6112d530b33b5afa497c7a
Blake Saylor <blakesaylor@gmail.com>
2022-05-11 18:53:16 -0600

    Add test for method 'correct?'

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,22 +4,29 @@ require './lib/card'
 
 describe Turn do
   it 'Card exists' do
-    card = Card.new("What is the capital of Oklahoma?", "Oklahoma City",
-      :Geography)
+    card = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
+
     expect(card).to be_a Card
   end
 
   it 'Turn exists' do
-    card = Card.new("What is the capital of Oklahoma?", "Oklahoma City",
-      :Geography)
+    card = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
     turn = Turn.new("Oklahoma City", card)
+
     expect(turn).to be_a Turn
   end
 
   it 'Has a guess' do
-    card = Card.new("What is the capital of Oklahoma?", "Oklahoma City",
-      :Geography)
+    card = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
     turn = Turn.new("Oklahoma City", card)
+
     expect(turn.guess).to eq "Oklahoma City"
+  end
+
+  it 'Tells you if you are correct' do
+    card = Card.new("What is the capital of Oklahoma?", "Oklahoma City", :Geography)
+    turn = Turn.new("Oklahoma City", card)
+
+    expect(turn.correct?).to eq true
   end
 end
