37b4e3ebe62a9c5ad366937d878ec19121621ab7
Thiago Silveira <thiago1992@yahoo.com>
2022-05-11 23:37:19 -0600

    Test: Add turn returns a card

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -9,10 +9,17 @@ RSpec.describe Turn do
     expect(turn).to be_a(Turn)
   end
 
-  it 'can guess' do
+  it 'returns a guess' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'returns a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
   end
 end
