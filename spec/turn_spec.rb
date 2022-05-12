78a82e1ee95b63a2712bab61fb598df71358d794
Nikky Rojas <rojas7313@gmail.com>
2022-05-12 14:15:06 -0600

    modify turn test code

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,31 +2,40 @@ require './lib/turn'
 require 'rspec'
 require 'pry'
 
-RSpec.describe Turn do
+RSpec.describe Card do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card).to be_instance_of(Card)
+  end
+
   it 'turn is present' do
-    turn = Turn.new ("Juneau", card,)
+    turn = Turn.new ("Juneau", card)
 
-    expect(turn).to_be_an_instance_of(Turn)
+    expect(turn).to_be_instance_of(Turn)
   end
 
-  it "returns the card" do
-      turn = Turn.new ("Juneau", card,)
-
-      expect(turn.card).to eq (Card)
-  end
-  it "guess" do
-    turn = Turn.new ("Juneau", card,)
-
-    expect(turn.guess). to eq("Juneau")
-  end
-  it "is guess correct" do
-    turn = Turn.new ("Juneau", card,)
-
-    expect(turn.correct) to eq()
-  end
-  it "user feedback correct or incorrect" do
-    turn = Turn.new ("Juneau", card,)
-
-    expect(turn.feedback) to eq()
-  end
-end
+#   xit "returns the card" do
+#       turn = Turn.new ("Juneau", card)
+#
+#       expect(turn.card).to eq (Card)
+#   end
+#
+#   xit "the guess" do
+#     turn = Turn.new ("Juneau", card)
+#
+#     expect(turn.guess).to eq(turn.users_guess)
+#   end
+#
+#   xit "is guess correct" do
+#     turn = Turn.new ("Juneau", card)
+#
+#     expect(turn.correct?).to eq(Card.answer)
+#   end
+#
+#   xit "user feedback correct or incorrect" do
+#     turn = Turn.new ("Juneau", card)
+#
+#     expect(turn.feedback).to eq(turn.guess_result)
+#   end
+# end
