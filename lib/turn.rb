167883940a5212ef7df0bb19d28060383f20a648
Sunny Moore <moore.sunny@gmail.com>
2022-05-12 16:42:02 -0700

    Write feedback method testing correct

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,8 +5,19 @@ class Turn ()
     @guess = guess
     @card = card
   end
+
   def correct?(guess)
     @guess == card.answer
+
+  end
+  def feedback(guess)
+    if correct?(guess)
+      return "Correct!"
+    elsif !correct?(guess)
+      return "Incorrect."
+    else
+      puts "Error"
+    end
 
   end
 end
