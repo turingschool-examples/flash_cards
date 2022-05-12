16a154d7ede539ac406ace204bdee6c6f4a6f801
Blake Saylor <blakesaylor@gmail.com>
2022-05-12 17:40:12 -0600

    Create runner file, four cards, and a deck

---
+++ flashcard_runner.rb
@@ -0,0 +1,13 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+card1 = Card.new("What Olympic sport has competitors throw a sphere as far as possible?", "Shotput", :Sports)
+card2 = Card.new("How many animated movies have been nomiated for the Academy Award for Best Picture?", "3", :Movies)
+card3 = Card.new("Which Disney movie was the first animated film to be nominated for the Academy Award for Best Picture?", "Beauty and the Beast", :Movies)
+card4 = Card.new("What city is the capital of Germany?", "Berlin", :Geography)
+
+cards = [card1, card2, card3, card4]
+
+deck = Deck.new(cards)
