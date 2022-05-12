dcaaa2718429784d1b50c47526e75b2b9b33a194
mkbonini <mkbonini@gmail.com>
2022-05-12 13:58:28 -0600

    Finish Iteration 3

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,9 +7,9 @@ card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 cards = [card_1, card_2, card_3]
-
 deck = Deck.new(cards)
 round = Round.new(deck)
+categories = []
 
 puts "Welcome! you're playing with #{deck.count} cards."
 puts "-"*38
@@ -21,3 +21,11 @@ deck.count.times do
 end
 puts "****** Game over! ******"
 puts "You had #{round.number_correct} correct guesses out of #{deck.count} for a total score of #{round.percent_correct.to_i}%."
+
+deck.cards.each do |card|
+  categories << card.category
+end
+categories = categories.uniq
+categories.each do |category|
+  puts "#{category} - #{round.percent_correct_by_category(category).to_i}% correct"
+end
