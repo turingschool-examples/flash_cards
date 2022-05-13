cb038ede404025be3c74221379edf6a0d83dd65c
Blake Saylor <blakesaylor@gmail.com>
2022-05-13 15:21:51 -0600

    Remove unnecessary comments

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,8 +12,6 @@ class Deck
 
   def cards_in_category(category)
     category_cards = []
-    # Iterate through all of the cards in the deck
-    # If the category of the given card == category parameter, shovel into category_cards
     @cards.each do |card|
       if card.category == category
         category_cards << card
