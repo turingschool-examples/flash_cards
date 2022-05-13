51c89ebb68f15267d9c60cff590050c03d342c88
Blake Saylor <blakesaylor@gmail.com>
2022-05-13 15:21:08 -0600

    Remove unneccessary comments

--- lib/card.rb
+++ lib/card.rb
@@ -1,22 +1,11 @@
 # Creates an instance of Card with a question, answer, and a category
 class Card
   attr_reader :question, :answer, :category
-  # Initialize Card
+
   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
   end
 
-  # def question
-  #   @question
-  # end
-  #
-  # def answer
-  #   @answer
-  # end
-  #
-  # def category
-  #   @category
-  # end
 end
