25c7691182d5fb05c821b9926628daa4cfaae048
Nick Jones <nickjones9605@gmail.com>
2022-05-13 16:12:19 -0600

    Add number correct, number correct by category, percent correct, percent correct by category, add tests

--- lib/round.rb
+++ lib/round.rb
@@ -1,41 +1,64 @@
 class Round
 
-  attr_reader :deck, :card, :turn, :turns, :turn_index
-=begin Initalizes a round object with a argument deck. This has a empty turns
-array,and a int turns_index.=end
+  attr_reader :deck, :card, :turn, :turns, :turn_index, :guesses_correct
+#Initalizes a round object with a argument deck. This has a empty turns
+#array,and a int turns_index.
   def initialize(deck)
     @deck = deck
     @turns = []
     @turn_index = 0
+    @guesses_correct = 0
+
   end
 
-=begin This function takes a argument guess, and sets it to a turn object.
-adds that turn object to our turn array and incrementally increases our turn.
-index by 1 =end
+# This function takes a argument guess, and sets it to a turn object.
+#adds that turn object to our turn array and incrementally increases our turn.
+#index by 1. This block also checks if the turn was correct, and adds 1 to the guesses_correct.
   def take_turn(guess)
-    @turn = Turn.new(guess, card)
+
+    @turn = Turn.new(guess, current_card)
     @turns << @turn
     @turn_index += 1
-    return @turn
+
+    if @turn.correct? == true
+      @guesses_correct += 1
+    end
+     return @turn
   end
-=beginThis function returns the element of our cards array in our deck object at the
-position indicated by turn_index=end
+#This function returns the element of our cards array in our deck object at the
+#position indicated by turn_index
   def current_card
 
     @deck.cards[@turn_index]
 
   end
-
+#This returns the number of correct guesses based on what's in our array of
+#given guesses
   def number_correct
+    @guesses_correct
   end
-
-  def number_correct_by_category(category)
-  end
-
+#This returns a percentage correct by taking the number of correct answers and
+#dividing it by the turn index. integer math ruby rounds to zero, so had to cast
+#to floats.
   def percent_correct
+    (@guesses_correct.to_f/ @turns.count.to_f) * 100
   end
-
-  def percent_correct_by_category
+#This creates a local correct_by_category variable then runs an each loop for
+#every turn in my turns array. If the turn has a correct guess AND it's a part
+#of that category, it'll add it to the local variable correct_by_category.
+  def number_correct_by_category(category)
+    correct_by_category = 0
+    turns.each do |turn|
+    if turn.correct? && turn.card.category == category
+      correct_by_category += 1
+    end
   end
+    return correct_by_category
+  end
+  #
 
+  #
+  def percent_correct_by_category(category)
+    (number_correct_by_category(category).to_f/ deck.cards_in_category(category).length.to_f) * 100
+  end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -60,4 +60,56 @@ RSpec.describe Round do
     expect(round.current_card).to eq card2
   end
 
+  it "gives number of correct guesses" do
+    card1 = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capitol of Texas?", "Austin", :Geography)
+    card3 = Card.new("Who is the first officer of the Starship Enterprise D?", "Riker", :Star_Trek_Trivia)
+    card4 = Card.new("Who is the chief engineer of the Starship Enterprise D?", "La Forge", :Star_Trek_Trivia)
+    deck = Deck.new([card1, card2, card3, card4])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    turn2 = round.take_turn("Austin")
+    expect(round.number_correct).to eq 2
+  end
+
+  it "gives percent correct" do
+    card1 = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capitol of Texas?", "Austin", :Geography)
+    card3 = Card.new("Who is the first officer of the Starship Enterprise D?", "Riker", :Star_Trek_Trivia)
+    card4 = Card.new("Who is the chief engineer of the Starship Enterprise D?", "La Forge", :Star_Trek_Trivia)
+    deck = Deck.new([card1, card2, card3, card4])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    turn2 = round.take_turn("Austin")
+    turn3 = round.take_turn("Kirk")
+    turn4 = round.take_turn("La Forge")
+
+    expect(round.percent_correct).to eq 75
+  end
+
+  it "gives number of correct guesses by category" do
+    card1 = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capitol of Texas?", "Austin", :Geography)
+    card3 = Card.new("Who is the first officer of the Starship Enterprise D?", "Riker", :Star_Trek_Trivia)
+    card4 = Card.new("Who is the chief engineer of the Starship Enterprise D?", "La Forge", :Star_Trek_Trivia)
+    deck = Deck.new([card1, card2, card3, card4])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    turn2 = round.take_turn("Austin")
+    turn3 = round.take_turn("Kirk")
+    expect(round.number_correct_by_category(:Geography)).to eq 2
+    expect(round.number_correct_by_category(:Star_Trek_Trivia)).to eq 0
+  end
+
+  it "gives percent of guesses correct by category" do
+    card1 = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capitol of Texas?", "Austin", :Geography)
+    card3 = Card.new("Who is the first officer of the Starship Enterprise D?", "Riker", :Star_Trek_Trivia)
+    card4 = Card.new("Who is the chief engineer of the Starship Enterprise D?", "La Forge", :Star_Trek_Trivia)
+    deck = Deck.new([card1, card2, card3, card4])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    turn2 = round.take_turn("Austin")
+    expect(round.percent_correct_by_category(:Geography)).to eq 100
+  end
 end
