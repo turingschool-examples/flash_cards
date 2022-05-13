c1eed8d3d080a4f4c9e7b2c44528dae5c931f14b
Anna Marie Sterling <sterling.am@gmail.com>
2022-05-13 14:28:41 -0600

    4th Round test passed

--- lib/round.rb
+++ lib/round.rb
@@ -1,13 +1,22 @@
 require 'pry'
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :class
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @class = Turn
   end
 
   def current_card
     @deck.cards[0]
+  end
+
+  def take_turn(guess)
+    @turn_1
+  end
+
+  def correct?
+    
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe Round do
     @card_1 = Card.new("Which element has the symbol 'H'?", "Hydrogen", :ReactiveNonmetals)
     @card_2 = Card.new("Which element has the symbol 'Ne'?", "Neon", :NobleGases)
     @card_3 = Card.new("Which element has the symbol 'B'?", "Boron", :Metalloids)
+
+
     @deck = Deck.new([@card_1, @card_2, @card_3])
     @round = Round.new(@deck)
   end
@@ -30,5 +32,9 @@ RSpec.describe Round do
    expect(@round.current_card).to eq(@card_1)
   end
 
-
+  it 'takes its turn' do
+    new_turn = @round.take_turn("Hydrogen")
+    new_turn.class
+    expect(new_turn.correct?).to be true
+  end
 end
