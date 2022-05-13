268715a69f09bea6659a3ca34d9d38226b84d09f
Tyler Caudill <teecaud@hotmail.com>
2022-05-13 13:51:37 -0500

    added chunk of code to round

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,37 @@
 class Round
-  attr_reader :decks
-  def initialize(decks)
+  attr_reader :deck, :turns
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+
 
   end
+
+  def current_card
+    @deck.cards[@turns.count]
+
+  end
+
+  def take_turn(guess)
+  turn = Turn.new(guess, current_card)
+  @turns << turn
+  turn
+
+  end
+
+  def number_correct
+    correct = 0
+    @turns.each do |turn|
+      if turn.correct?
+        correct += 1
+      end
+    end
+    correct
+  end
+
+
+
+
+
+
 end
