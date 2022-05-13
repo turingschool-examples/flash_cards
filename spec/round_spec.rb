e7cb66de15e8a9bf62a484e82cf9dfc606cc2b7e
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-13 13:07:36 -0400

    Add percent_correct_by_category test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -262,7 +262,7 @@ RSpec.describe Round do
 
   end
 
-  it 'returns number correct by category' do
+  it 'returns percent_correct of total turns' do
 
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
@@ -279,6 +279,26 @@ RSpec.describe Round do
     new_turn_2 = round.take_turn("Venus")
 
     expect(round.percent_correct).to eq(50.0)
+
+  end
+
+  it 'returns percent_correct of specific category' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    new_turn_1 = round.take_turn("Juneau")
+
+    new_turn_2 = round.take_turn("Venus")
+
+    expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
 
   end
 
