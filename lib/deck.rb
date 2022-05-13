6b24b03eae7ff8534bee3379c1ab065e902d29b7
Nick Jones <nickjones9605@gmail.com>
2022-05-13 17:41:45 -0600

    remove card generator as couldn't get working right atm

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,4 @@
+require './lib/card'
 class Deck
 
   attr_reader :cards
--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,7 +2,35 @@ require './lib/card'
 require './lib/deck'
 require './lib/turn'
 require './lib/round'
+require './lib/card_generator'
 
 class Flashcard_Runner
 
-  
+  attr_reader :round
+
+   def initialize
+     card_generator = CardGenerator.new('./card.txt')
+     deck = Deck.new(card_generator.cards)
+     @round = Round.new(deck)
+   end
+
+  def start
+
+    puts "Welcome to the card game! You're playing with #{round.deck.count} cards!"
+    puts "-------------------------------------------------------"
+
+    while (round.turns.size + 1) <= round.deck.count
+      puts "This is card number #{round.turns.size + 1} out of #{round.deck.count}."
+      puts "Question: #{round.current_card.question}"
+      round.take_turn(gets.chomp)
+      puts round.turns.last.feedback
+    end
+
+   puts '****** Game over! ******'
+   puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct.round}."
+   # Loop to print percent_correct_by_category for each category present in deck
+   round.deck.cards.map {|card| card.category }.uniq.each do |category|
+     puts "#{category} - #{round.percent_correct_by_category(category).round}%"
+   end
+ end
+end
