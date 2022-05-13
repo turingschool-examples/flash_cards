0bf7e28d54681d1d802e78fd947a4858a0548647
Nick Jones <nickjones9605@gmail.com>
2022-05-12 19:14:01 -0600

    Add turn_index variable to attr_reader, and take turn function. Add test for changes card after turn taken.

--- lib/round.rb
+++ lib/round.rb
@@ -1,20 +1,25 @@
 class Round
 
-  attr_reader :deck, :card, :turn, :turns
-
+  attr_reader :deck, :card, :turn, :turns, :turn_index
+# Initalizes a round object with a argument deck. This has a empty turns array, and a int turns_index.
   def initialize(deck)
     @deck = deck
     @turns = []
+    @turn_index = 0
   end
-
+#This function takes a argument guess, and sets it to a turn object.
+#adds that turn object to our turn array and incrementally increases our turn index by 1
   def take_turn(guess)
     @turn = Turn.new(guess, card)
     @turns << @turn
+    @turn_index += 1
     return @turn
   end
-
+#This function returns the element of our cards array in our deck object at the position indicated by turn_index
   def current_card
-    @deck.cards.first
+
+    @deck.cards[@turn_index]
+
   end
 
   def number_correct
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -43,11 +43,21 @@ RSpec.describe Round do
     card2 = Card.new("What is the capitol of Texas?", "Austin", :Geography)
     card3 = Card.new("Who is the first officer of the Starship Enterprise D?", "Riker", :Star_Trek_Trivia)
     card4 = Card.new("Who is the chief engineer of the Starship Enterprise D?", "La Forge", :Star_Trek_Trivia)
-    deck = Deck.new([card1, card2, card3,card4])
+    deck = Deck.new([card1, card2, card3, card4])
     round = Round.new(deck)
     turn = round.take_turn("Juneau")
     expect(round.turns[0]).to eq turn
+  end
 
+  it "changes current card after turn is taken" do
+    card1 = Card.new("What is the capitol of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capitol of Texas?", "Austin", :Geography)
+    card3 = Card.new("Who is the first officer of the Starship Enterprise D?", "Riker", :Star_Trek_Trivia)
+    card4 = Card.new("Who is the chief engineer of the Starship Enterprise D?", "La Forge", :Star_Trek_Trivia)
+    deck = Deck.new([card1, card2, card3, card4])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    expect(round.current_card).to eq card2
   end
 
 end
