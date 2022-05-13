7f1a0134cf595a70dd126e782361e1fb77d1913b
Thiago Silveira <thiago1992@yahoo.com>
2022-05-13 12:35:12 -0600

    Test: Add round can create and return new turn

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -44,6 +44,16 @@ RSpec.describe Round do
     expect(round.current_card).to eq(card_1)
   end
 
+  it 'can create and return a new turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    expect(new_turn.class).to eq(Turn)
+  end
 
 
 end
