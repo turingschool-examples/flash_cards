3bc641c5f0e1ba7ff0ee65c1c0656ea29789a61d
Gauri Joshi <gauri728@gmail.com>
2022-05-13 10:08:22 -0700

    Define current card method and pass test

--- lib/round.rb
+++ lib/round.rb
@@ -1,10 +1,14 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns, :turn_counter, :correct
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @turn_counter = 0
+    @correct = 0
   end
 
-  def turns
-    return []
+
+  def current_card #method current_card
+    @deck.cards[@turn_counter]
   end
 end
