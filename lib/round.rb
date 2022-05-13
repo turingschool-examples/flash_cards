79c316dba1b6ed358801a02d65f240b31bd8ba5a
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-13 12:27:27 -0400

    Adjust take_turn to pass new card test

--- lib/round.rb
+++ lib/round.rb
@@ -7,39 +7,54 @@ require 'pry'
 
 class Round
 
-  attr_reader :deck, :turns, :turn_count,:number_correct
+  attr_reader :deck, :turns, :turn_count,:used_card_array,:number_correct
 
   def initialize(deck)
     @deck = deck
     @turns = []
     @turn_count = 0
     @number_correct = 0
+    @used_card_array = []
   end
 
   def current_card
-    deck.cards.shift
+    if @used_card_array == []
+      @deck.cards.first
+    else
+      @used_card_array.last
+    end
   end
 
   def take_turn(guess)
-    @turn_count += 1
-    new_turn = Turn.new(guess,current_card)
+
+    new_turn = Turn.new(guess,@deck.cards.first)
+
+    @used_card_array << @deck.cards.first
+
+    @deck.cards.rotate!(1)
 
     if new_turn.guess == new_turn.card.answer
       increment_number_correct
     end
 
     @turns << new_turn
+
+    increment_turn_count
+
     new_turn
+
   end
 
   def correct?
-
     take_turn.guess == take_turn.answer
-
   end
 
   def increment_number_correct
     @number_correct += 1
+  end
+
+  def increment_turn_count
+    @turn_count += 1
   end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,11 +1,12 @@
 require 'rspec'
+require 'pry'
 require './lib/turn'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
 
 
-RSpec.describe Deck do
+RSpec.describe Round do
 
   xit 'exists' do
 
@@ -174,6 +175,28 @@ RSpec.describe Deck do
     new_turn_2 = round.take_turn("Mars")
 
     expect(round.number_correct).to eq(2)
+
+  end
+
+  it 'produces a new card each turn' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    new_turn_1 = round.take_turn("Juneau")
+
+    expect(round.current_card).to eq(card_1)
+
+    new_turn_2 = round.take_turn("Mars")
+
+    expect(round.current_card).to eq(card_2)
 
   end
 
