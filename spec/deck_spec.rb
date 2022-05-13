5fc11a3645801d5309815fa68405cf79733f6741
Sunny Moore <moore.sunny@gmail.com>
2022-05-12 19:53:26 -0700

    Creat deck.count test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -12,4 +12,13 @@ describe 'Deck' do
 
       expect(deck.cards).to eq(cards)
     end
+    it "can count cards in a deck" do
+      card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards =[card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(deck.count).to eq(3)
+    end
 end
