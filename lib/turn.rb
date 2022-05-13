994db564d6fcdb2b41f3b9ce385b3d465a844fdb
Bryan Shears <brshears@gmail.com>
2022-05-12 20:04:12 -0600

    Add correct? and feedback methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,22 @@ class Turn
   def initialize(guess, card)
     @guess = guess
     @card = card
+  end
 
+  def correct?
+    if card.answer = @guess
+      true
+    else
+      false
+    end
+  end
+
+
+  def feedback
+    if correct? == true
+       "Correct!"
+     elsif
+       "Incorrect."
+    end
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,36 +4,31 @@ require './lib/card'
 RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card).to be_instance_of(Card)
-  end
-
-
-
-  it 'has a turn' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-    expect(turn.card).to eq(card)
+    expect(turn).to be_instance_of(Turn)
   end
-#require "pry"; binding.pry
 
-  xit "has a card" do
+
+  it "has a card" do
    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
    turn = Turn.new("Juneau", card)
-   expect(turn.card).to eq("What is the capital of Alaska?", "Juneau", :Geography)
+   expect(turn.card).to eq(card)
   end
 
-  xit "has a guess" do
+  it "has a guess" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
      expect(turn.guess).to eq("Juneau")
   end
 
-  xit "has a correct answer" do
+  it "has a correct answer" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.correct?).to be true
   end
 
-  xit "has feedback" do
+  it "has feedback" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.feedback).to eq("Correct!")
   end
