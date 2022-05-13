d1fbb2d5acb68b133f3609974411ec10a92cfdcb
Drew MacNicholas <dmacnicholas@gmail.com>
2022-05-13 12:55:51 -0500

    Add Deck Iteration 2

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,19 @@
+require 'pry'
 class Deck
-  attr_reader :cards, :count, :card_category
+  attr_reader :cards, :count, :cards_in_category
 
   def initialize(cards)
     @cards =cards
     @count = cards.length
   end
 
-  def card_category
+  def cards_in_category(category)
+    card_category = []
+      cards.each do |card|
+        if category == card.category
+        card_category << card
+      end
+    end
+    card_category
   end
-    
 end
