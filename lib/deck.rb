c9de422f948a9a1708eaf1194b639427ab62723f
Justin Ramirez <jusrez528@gmail.com>
2022-05-13 18:18:10 -0400

    Add deck methods and tests

---
+++ lib/deck.rb
@@ -0,0 +1,21 @@
+class Deck
+	attr_reader :cards
+
+	def initialize(cards)
+		@cards = cards
+	end
+
+	def count
+		@cards.count
+	end
+
+	def cards_in_category(category)
+		cards_in_category = []
+		@cards.each do |card|
+			if card.category == category
+				cards_in_category << card
+			end
+		end
+		cards_in_category
+	end
+end
