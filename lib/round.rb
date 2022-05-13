c83de5632d11b10cdccc299c5594d965fd716179
Tyler Caudill <teecaud@hotmail.com>
2022-05-13 17:48:07 -0500

    Complete round file

--- lib/round.rb
+++ lib/round.rb
@@ -29,6 +29,38 @@ class Round
     correct
   end
 
+  def number_correct_by_category(category)
+    correct = 0
+    @turns.each do |turn|
+      if turn.correct? && turn.card.category == category
+        correct += 1
+      end
+    end
+    correct
+  end
+
+  def cards_by_category(category)
+    correct = 0
+    @turns.each do |turn|
+      if  turn.card.category == category
+        correct += 1
+      end
+    end
+    correct
+  end
+
+  def percent_correct
+    (number_correct / @turns.count.to_f) * 100
+
+  end
+
+    def percent_correct_by_category(category)
+      (number_correct_by_category(category) / cards_by_category(category).to_f) * 100
+
+
+  end
+
+
 
 
 
