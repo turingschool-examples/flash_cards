4e2824df4a34503156f83a0dd96979aa6d1b5947
Thiago Silveira <thiago1992@yahoo.com>
2022-05-13 10:32:42 -0600

    Test: Add edge case for deck returns card by string category

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -42,4 +42,16 @@ RSpec.describe Deck do
     expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
     expect(deck.cards_in_category(:Geography)).to eq([card_1])
   end
+
+  it 'edge case can return cards by their respective category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    card_4 = Card.new("What company has the largest production of rubber tires?", "Legos", :Trivia)
+    cards = [card_1, card_2, card_3, card_4]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:Trivia)).to eq([card_4])
+    expect(deck.cards_in_category("Pop Culture")).to eq([])
+  end
 end
