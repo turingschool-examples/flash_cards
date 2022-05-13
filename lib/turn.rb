b3fd777dfe70c506630bcea6403a17474781df99
Andre Pedro <andrerpedro170@gmail.com>
2022-05-13 07:09:22 -0600

    complete 1st itineary

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,36 @@
 class Turn
-  attr_reader :guess, :card
+
+  attr_reader :guess, :card, :turn
 
   def initialize(guess, card)
-    @guess << guess
+    #@turn = []
     @card = card
+    #@turn_counter = []
+    @guess = guess
   end
+
+  def correct?
+     @guess == card.answer
+  end
+
+  def feedback
+    if correct?
+      return "Correct!"
+    else
+      return "Incorrect!"
+    end
+  end
+
+
+  # def incorrect?
+  #   @guess != card.answer
+  #   puts "Incorrect!"
+  #
+  # end
+
+
+
+
+
+
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,7 +5,28 @@ require 'rspec'
 RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.guess).to eq(@answer)
+
+    expect(card).to be_instance_of(Card)
   end
+
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to eq(true)
+  end
+
+  # it 'exists' do
+  #   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #   turn = Turn.new("Juneau", card)
+  #   expect(turn.incorrect?). to eq(false)
+  #
+  # end
+
+
 end
