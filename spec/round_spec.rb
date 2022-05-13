e6d916f3580cd446b26d1da0ae4c85092fd99b71
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-13 12:29:33 -0400

    Add test for returning count of turns

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -200,4 +200,24 @@ RSpec.describe Round do
 
   end
 
+  it 'returns number of turns taken' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round = Round.new(deck)
+
+    new_turn_1 = round.take_turn("Juneau")
+
+    new_turn_2 = round.take_turn("Mars")
+
+    expect(round.turns.count).to eq(2)
+
+  end
+
 end
