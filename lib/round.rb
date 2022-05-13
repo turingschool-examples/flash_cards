d48235bac4eb4f5cbb55bec556b10230ca7dc850
Nick Jones <nickjones9605@gmail.com>
2022-05-12 19:19:36 -0600

    Fix comments on Round

--- lib/round.rb
+++ lib/round.rb
@@ -1,21 +1,25 @@
 class Round
 
   attr_reader :deck, :card, :turn, :turns, :turn_index
-# Initalizes a round object with a argument deck. This has a empty turns array, and a int turns_index.
+=begin Initalizes a round object with a argument deck. This has a empty turns
+array,and a int turns_index.=end
   def initialize(deck)
     @deck = deck
     @turns = []
     @turn_index = 0
   end
-#This function takes a argument guess, and sets it to a turn object.
-#adds that turn object to our turn array and incrementally increases our turn index by 1
+
+=begin This function takes a argument guess, and sets it to a turn object.
+adds that turn object to our turn array and incrementally increases our turn.
+index by 1 =end
   def take_turn(guess)
     @turn = Turn.new(guess, card)
     @turns << @turn
     @turn_index += 1
     return @turn
   end
-#This function returns the element of our cards array in our deck object at the position indicated by turn_index
+=beginThis function returns the element of our cards array in our deck object at the
+position indicated by turn_index=end
   def current_card
 
     @deck.cards[@turn_index]
