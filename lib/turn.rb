71dc2d0fc067e854e4ac762f0b32460381a62e38
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-13 11:01:52 -0400

    Refactor

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,17 +5,12 @@ class Turn
     @card = card
   end
 
-  def guess
-    @guess
-
-  end
-
   def correct?
     @guess == @card.answer
   end
 
   def feedback
-    if @guess == @card.answer
+    if correct?
       "Correct!"
     else
       "Incorrect."
