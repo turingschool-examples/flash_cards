ea752a8b78e54e359c01d8adb093aac701d37fa6
Sunny Moore <moore.sunny@gmail.com>
2022-05-13 11:43:43 -0700

    Add cards_with_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,33 @@
+require './lib/card'
 class Deck
-  attr_reader :cards
+  attr_accessor :cards, :cards_in_category
+
   def initialize(cards)
-
     @cards = cards
-    p cards
-
+    @cards_in_category = []
   end
 
   def count
     return cards.length
+  end
+
+  def cards_with_category(category)
+    i = 0
+    while i < cards.count
+      if @cards[i].category == category
+           @cards_in_category << @cards[i]
+      end
+      i += 1
+    end
+    return @cards_in_category
+    #unsuccessful! Ask why 
+    # require 'pry'; binding.pry
+    # @cards.each_with_index do |card, index|
+    #   if @cards[index].category == @category
+    #     @cards_in_category << @card[index]
+    #   end
+    # end
+    # return @cards_in_category
+
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -25,9 +25,9 @@ describe 'Deck' do
       card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-      cards =[card1, card2, card3]
+      cards = [card1, card2, card3]
       deck = Deck.new(cards)
-
-      expect(cards_in_category(:STEM)).to eq(card2, card3)
+      
+      expect(deck.cards_with_category(:STEM)).to eq(cards[1..2])
     end
 end
