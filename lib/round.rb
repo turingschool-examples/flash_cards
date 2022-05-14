add2b82f614b611cf1f23dcfea93c3f8a3c63921
Sunny Moore <moore.sunny@gmail.com>
2022-05-14 12:16:14 -0700

    Make take_turn method return a Turn

--- lib/round.rb
+++ lib/round.rb
@@ -1,4 +1,4 @@
-
+# require './lib/turn'
 
 class Round
   attr_reader :deck, :turns
@@ -9,5 +9,13 @@ class Round
 
   def current_card()
     return deck.cards[0]
+  end
+
+  def take_turn(guess)
+    this_turn = Turn.new(guess, current_card())
+    @turns << this_turn
+    # @deck.rotate(1)
+    return this_turn
+    #require 'pry'; binding.pry
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,6 +1,7 @@
 require 'pry'
 # require './lib/card'
 require './lib/deck'
+require './lib/turn'
 require './lib/round'
 
 describe Round do
@@ -36,7 +37,7 @@ describe Round do
 
     expect(round.current_card).to eq(card_1)
   end
-  it "can take_turn" do
+  it "take_turn stores a new turn" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -44,6 +45,8 @@ describe Round do
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
-    expect(round.turns).to eq(new_turn)
+    expect(round.turns[0]).to eq(new_turn)
+    expect(new_turn.class).to eq(Turn)
   end
+
 end
