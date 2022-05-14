71e3713ed06d3adf8bcb87740747a9b44ea2f858
Wesley Miranda <wesleywes42@gmail.com>
2022-05-14 01:59:51 -0600

    Current card

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,12 @@
 class Round
-attr_reader :deck 
+attr_reader :turns, :deck
 
   def initialize(deck)
     @deck = deck
+    @turns = []
   end
 
-  def turns
-    truns = []
+  def current_card
+    deck.cards[0]
   end
 end
