3fd1662f5b4aa269d5555841615a0a2802401368
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-14 16:11:49 -0600

    Iteration 1 - Modify deck test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Deck do
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+
+    expect(deck).to be_instance_of(Deck)
   end
 
   it 'counts cards' do
