9f2cbd37c978e4e4ee4d39e179743d4b6add0516
Sunny Moore <moore.sunny@gmail.com>
2022-05-14 12:57:57 -0700

    Add test that take_turn moves to next card

--- lib/round.rb
+++ lib/round.rb
@@ -1,10 +1,11 @@
 # require './lib/turn'
 
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
   end
 
   def current_card()
@@ -14,6 +15,9 @@ class Round
   def take_turn(guess)
     this_turn = Turn.new(guess, current_card())
     @turns << this_turn
+    if this_turn.guess == current_card().answer
+      @number_correct += 1
+    end
     # @deck.rotate(1)
     return this_turn
     #require 'pry'; binding.pry
