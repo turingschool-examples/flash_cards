94f2d2aac75e1137dbf5e87a77ddbc07ea6fceaf
Brenna Stuart <brennacodes@gmail.com>
2022-05-14 11:03:16 -0600

    Add cards in category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,5 +4,10 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
-  
+
+  def cards_in_category(category)
+    cards.select do |card|
+      card.category == category
+    end
+  end
 end
