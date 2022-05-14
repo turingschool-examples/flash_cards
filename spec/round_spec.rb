5285cfbbed5c6596593e31ad3ff4b60bc577bc2e
Lucas Colwell <colwellc37@gmail.com>
2022-05-14 16:03:28 -0600

    Write basic tests for round

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,19 +1,26 @@
 require 'rspec'
-require './lib/class'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
 
-RSpec.describe Class do
-  describe 'Iteration 1' do
+RSpec.describe Round do
+  describe 'Iteration 2' do
     before :each do
-      @
-      @
+      @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      @deck = Deck.new([@card_1, @card_2, @card_3])
+      @round = Round.new(@deck)
     end
 
     it 'exists' do
-      expect(@class).to be_a(Class)
+      expect(@round).to be_a(Round)
     end
 
     it 'has attributes' do
-      expect(@class.attribute).to eq()
+      expect(@round.deck).to eq(@deck)
+      expect(@round.turns).to eq([])
     end
 
   end
