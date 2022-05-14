9f7904d344bbc750370dc9bdca7fe3fc9cb1f554
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-13 21:31:13 -0400

    Add method to sort by category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,19 @@
 class Deck
+  attr_reader :cards, :count
+  def initialize (cards)
+    @cards = cards
+    @count = cards.count
+  end
 
+  def cards_in_category(category)
+    same_cat = []
+    @cards.each do |card|
+      if card.category == category
+        same_cat << card
+      end
+    end
+    same_cat
 
-
+  end
 
 end
