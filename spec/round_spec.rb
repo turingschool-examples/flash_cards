6855cb744fc638afa89e01cadf1f79d376548a16
Sunny Moore <moore.sunny@gmail.com>
2022-05-14 09:55:46 -0700

    Add test for turn variable

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,17 +4,8 @@ require './lib/deck'
 require './lib/round'
 
 describe Round do
-  it "is a round" do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    deck = Deck.new([card_1, card_2, card_3])
-    round = Round.new()
-
-    expect(round).to be_an_instance_of(Round)
-  end
-  it "#initializes" do
+  it "#initializes and is a Round" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -22,6 +13,17 @@ describe Round do
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
 
-    expect(round.deck).to eq([card_1, card_2, card_3])
+    expect(round).to be_an_instance_of(Round)
+    expect(round.deck).to eq(deck)
+  end
+  it "has a turn at initialize" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.turn).to eq([])
   end
 end
