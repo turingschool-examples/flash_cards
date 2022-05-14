5b92d7b6759400580cac68e08c925a80c7a76321
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-14 19:39:14 -0400

    Complete tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -3,6 +3,7 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
+
 RSpec.describe Round do
   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -35,74 +36,68 @@ RSpec.describe Round do
     expect(new_turn.correct?).to eq(true)
     #=> true
 
-    expect(round.turns).to eq(new_turn)
+    expect(round.turns[0]).to eq(new_turn)
     #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
   end
 
-  xit '' do
+  it '' do
 
-    expect(round.number_correct)
+    expect(round.number_correct).to eq (1)
     #=> 1
   end
 
-  xit '' do
+  it '' do
 
-    expect(round.current_card)
+    expect(round.current_card).to eq(card_2)
     #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
   end
 
-  xit '' do
+  it '' do
+    new_turn = round.take_turn("Venus")
 
-    expect(round.take_turn("Venus"))
-    #=> #<Turn:0x00007f972a215b38...>
-  end
+    expect(new_turn.correct?).to eq(false)
 
-  xit '' do
-
-    expect(round.turns.count)
+    expect(round.turns.count).to eq(2)
     #=> 2
-  end
 
-  xit '' do
-
-    expect(round.turns.last.feedback)
+    expect(round.turns.last.feedback).to eq("Incorrect.")
     #=> "Incorrect."
+
+    expect(round.number_correct).to eq(1)
+    #=> 1
+
   end
 
-  xit '' do
+  it '' do
 
-    expect(round.number_correct)
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
     #=> 1
   end
 
-  xit '' do
+  it '' do
 
-    expect(round.number_correct_by_category(:Geography))
-    #=> 1
-  end
-
-  xit '' do
-
-    expect(round.number_correct_by_category(:STEM))
+    expect(round.number_correct_by_category(:STEM)).to eq(0)
     #=> 0
   end
 
-  xit '' do
+  it '' do
 
-    expect(round.percent_correct)
+    expect(round.percent_correct).to eq(50.0)
     #=> 50.0
   end
 
-  xit '' do
+  it '' do
 
-    expect(round.percent_correct_by_category(:Geography))
+    expect(round.percent_correct_by_category(:Geography)).to eq (100.0)
     #=> 100.0
   end
 
 
-  xit '' do
+  it '' do
 
-    expect(round.current_card)
+    expect(round.current_card).to eq(card_3)
     #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
+
+    expect(round.deck_index).to eq(2)
   end
 end
