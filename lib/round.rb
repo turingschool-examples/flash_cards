aa96eb9c2f92697df84e02ff47716f738abef0d8
Bryce Simonds <bsimonds14@gmail.com>
2022-05-14 11:36:06 -0600

    Deleted turn test, changed test method to an attribute

--- lib/round.rb
+++ lib/round.rb
@@ -2,19 +2,20 @@ class Round
   attr_reader :deck
   def initialize(deck)
     @deck = deck
-  end
-
-  def turns
-    []
+    @turn_counter = 0
+    @turns = []
   end
 
   def current_card
-    deck.cards[0]
+    deck.cards[@turn_counter]
   end
+
+
 
   def take_turn(guess)
     turn = Turn.new(guess, current_card)
-    turns << turn
+    @turns << turn
+    @turn_counter += 1
     return turn
   end
 
@@ -26,3 +27,15 @@ class Round
   end
 
 end
+
+
+#FIX round.turns so that the collection
+# of array works. right now still
+# printing [] after it should
+# have a turn stored in it
+
+#fixing that^ will fix the
+#round.turns.count
+
+#is feedback not a method yet?
+#
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -20,20 +20,6 @@ describe Round do
     end
   end
 
-  describe 'turns method' do
-    it 'returns the current card with question, answer, category and guess' do
-      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs
-        and reports about the surface of which planet?", "Mars", :STEM)
-      card_3 = Card.new("Describe in words the exact direction that is 697.5°
-        clockwise from due north?", "North north west", :STEM)
-      cards = [card_1, card_2, card_3]
-      deck = Deck.new(cards)
-      round = Round.new(deck)
-      expect(round.turns).to eq([])
-    end
-  end
-
   describe 'current_card method' do
     it 'returns the current card with question, answer, category and guess and moves onto next card in the round' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -45,8 +31,10 @@ describe Round do
       deck = Deck.new(cards)
       round = Round.new(deck)
       expect(round.current_card).to eq(deck.cards[0])
+      new_turn = round.take_turn("Juneau")
       expect(round.current_card).to eq(deck.cards[1])
-      expect(round.current_card).to eq(deck.cards[3])
+      new_turn2 = round.take_turn("Venus")
+      expect(round.current_card).to eq(deck.cards[2])
     end
   end
 
