70f0363b938303e0608a686995c111ef193426d1
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-14 11:11:32 -0400

    Add 2 methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,25 @@
 class Round
-  attr_reader :round
-  def initialize(round)
-    @round = round
+  attr_reader :deck, :turns, :new_turn
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+
 
   end
+
+  def current_card
+    deck.cards.first
+  end
+
+  def take_turn(guess)
+    @turns = Turn.new(guess, current_card)
+  end
+
+  
+
+
+
+
+
+
+end
