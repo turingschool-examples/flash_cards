a542994e617fa099f20c49ce5d1494a9b545ad24
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-13 18:04:43 -0700

    Add new card to runner to check new start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -8,7 +8,8 @@ card_2 = Card.new("Question: What is the tallest mountain in the world?", "Mount
 card_3 = Card.new("Question: Which organ do insects not have?", "Lungs", :STEM)
 card_4 = Card.new("Question: What African country serves as the setting for Tatooine in Star Wars?", "Tunisia", :Geography)
 card_5 = Card.new("Question: What is the smallest country?", "Vatican City", :Geography)
-cards = [card_1, card_2, card_3, card_4, card_5]
+card_6 = Card.new("Question: Who has won more tenis grand slam titles, Venus Williams or Serena Williams?", "Serena Williams", :Sports)
+cards = [card_1, card_2, card_3, card_4, card_5, card_6]
 deck = Deck.new(cards)
 round = Round.new(deck)
 
