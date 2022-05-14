51e229d9e8a4594e218b49f3cab9c5ab57ee328e
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-05-14 14:44:15 -0600

    create round.rb and round_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -24,8 +24,10 @@ RSpec.describe Deck do
 
   it 'catergory equals :STEM and :Geography' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and
+      reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5°
+      clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
