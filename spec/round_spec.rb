6622f90ede391651d7b69c1f2f92eef625c4ca30
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-05-14 16:06:18 -0600

    round initialized and current card, tests passing

---
+++ spec/round_spec.rb
@@ -0,0 +1,28 @@
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
+require './lib/round'
+
+RSpec.describe Round do
+  it 'deck and turns exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.deck).to eq(deck)
+    expect(round.turns).to eq([])
+  end
+  it 'current_card equals card_1' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.current_card).to eq(card_1)
+  end
+end
