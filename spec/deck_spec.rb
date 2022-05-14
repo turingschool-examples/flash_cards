4727ad62293a10e8afef1022e3564ce723718e98
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-13 21:51:28 -0400

    remove unneccessary line

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Deck do
   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
   cards = [card_1, card_2, card_3]
-  category = cards
   deck = Deck.new(cards)
 
   it 'exists' do
