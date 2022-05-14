acc65b6cbd45033ff4f27ec12a79df3383409ce4
Lucas Colwell <colwellc37@gmail.com>
2022-05-14 15:30:27 -0600

    Initial commit + structure

---
+++ spec/deck_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+
+RSpec.describe Deck do
+  describe 'Iteration 1' do
+    before :each do
+      @
+      @
+    end
+
+    it 'exists' do
+
+    end
+
+    it 'has attributes' do
+
+    end
+
+  end
+
+end
