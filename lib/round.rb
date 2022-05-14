2b5206a718ff765a3ab9113fc867b89baa0afc09
Wesley Miranda <wesleywes42@gmail.com>
2022-05-14 14:36:16 -0600

    Trun class

--- lib/round.rb
+++ lib/round.rb
@@ -9,4 +9,10 @@ attr_reader :turns, :deck
   def current_card
     deck.cards[0]
   end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    turns << turn
+    return turn
+  end
 end
