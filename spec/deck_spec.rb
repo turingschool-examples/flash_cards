f589cac2a7f0543b5a991b9dea636e3ac19cce4c
Lucas Colwell <colwellc37@gmail.com>
2022-05-14 15:38:29 -0600

    Write it2 tests for deck class

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,18 +4,31 @@ require './lib/turn'
 require './lib/deck'
 
 RSpec.describe Deck do
-  describe 'Iteration 1' do
+  describe 'Iteration 2' do
     before :each do
-      @
-      @
+      @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      @cards = [@card_1, @card_2, @card_3]
+      @deck = Deck.new(@cards)
     end
 
     it 'exists' do
-
+      expect(@deck).to be_a(Deck)
     end
 
     it 'has attributes' do
+      expect(@deck.cards).to eq([@card_1, @card_2, @card_3])
+    end
 
+    it 'can count cards in a deck' do
+      expect(@deck.count).to eq(3)
+    end
+
+    it 'can tell cards in a category' do
+      expect(@deck.cards_in_category(:Stem)).to eq([@card_2, @card_3])
+      expect(@deck.cards_in_category(:Geography)).to eq([@card_1])
+      expect(@deck.cards_in_category("Pop Culture")).to eq([])
     end
 
   end
