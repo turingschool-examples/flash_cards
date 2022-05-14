9dbf039b2ce2bc89e010ef57630ad72e42f67c65
Sunny Moore <moore.sunny@gmail.com>
2022-05-14 12:28:08 -0700

    Add tests for returning result, storing a turn

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -48,5 +48,24 @@ describe Round do
     expect(round.turns[0]).to eq(new_turn)
     expect(new_turn.class).to eq(Turn)
   end
+  it "turn returns result of guess" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    expect(new_turn.correct?(new_turn.guess)).to eq(true)
+  end
+  it "stores the turn" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    expect(round.turns).to eq([new_turn])
+  end
 end
