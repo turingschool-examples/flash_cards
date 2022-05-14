3f28a87b639cf8ce49200446d57258614efba830
Blake Saylor <blakesaylor@gmail.com>
2022-05-14 13:26:49 -0600

    Update to strip newline characters from add_cards

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -11,10 +11,10 @@ class CardGenerator
 
   def add_cards
     File.readlines(@filename).each do |line|
-      split_line = line.split(",")
+      split_line = line.rstrip.split(",")
       question = split_line[0]
       answer = split_line[1]
-      category = (":" + split_line[2]).to_sym
+      category = split_line[2].to_sym
       @cards << Card.new(question, answer, category)
     end
   end
