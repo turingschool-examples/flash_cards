92f5a9703ee31a8edefbd31f5006e9ced85bb761
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-14 19:47:29 -0400

    Add test titles and remove comments

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -15,88 +15,74 @@ RSpec.describe Round do
     expect(round).to be_instance_of Round
   end
 
-  it '' do
+  it 'has deck' do
     expect(round.deck).to eq(deck)
   end
 
-  it '' do
+  it 'initializes an empty array' do
     expect(round.turns).to eq([])
   end
 
-  it '' do
+  it 'has correct current card' do
     expect(round.current_card).to eq(card_1)
   end
 
-  it '' do
+  it 'takes turn' do
     new_turn = round.take_turn("Juneau")
 
     expect(new_turn.class).to eq(Turn)
-    #=> Turn
 
     expect(new_turn.correct?).to eq(true)
-    #=> true
 
     expect(round.turns[0]).to eq(new_turn)
-    #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
   end
 
-  it '' do
+  it 'has number of correct answers' do
 
     expect(round.number_correct).to eq (1)
-    #=> 1
   end
 
-  it '' do
+  it 'has correct current card' do
 
     expect(round.current_card).to eq(card_2)
-    #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
   end
 
-  it '' do
+  it 'takes turn' do
     new_turn = round.take_turn("Venus")
 
     expect(new_turn.correct?).to eq(false)
 
     expect(round.turns.count).to eq(2)
-    #=> 2
 
     expect(round.turns.last.feedback).to eq("Incorrect.")
-    #=> "Incorrect."
 
     expect(round.number_correct).to eq(1)
-    #=> 1
-
   end
 
-  it '' do
+  it 'has number correct by category' do
 
     expect(round.number_correct_by_category(:Geography)).to eq(1)
-    #=> 1
   end
 
-  it '' do
+  it 'has number correct by category' do
 
     expect(round.number_correct_by_category(:STEM)).to eq(0)
-    #=> 0
   end
 
-  it '' do
+  it 'has percent correct' do
 
     expect(round.percent_correct).to eq(50.0)
-    #=> 50.0
   end
 
-  it '' do
+  it 'has percent correct by category' do
 
     expect(round.percent_correct_by_category(:Geography)).to eq (100.0)
-    #=> 100.0
   end
 
 
-  it '' do
+  it 'has correct current card' do
 
     expect(round.current_card).to eq(card_3)
-    #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
 
     expect(round.deck_index).to eq(2)
   end
