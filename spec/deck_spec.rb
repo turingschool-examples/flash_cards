682fd8c3affd9360a83760cd6886f14a1e2764f1
Wesley Miranda <wesleywes42@gmail.com>
2022-05-14 00:49:00 -0600

    Pop Culture

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -59,5 +59,14 @@ RSpec.describe Deck do
 
   end
 
-  
+  it 'has an answer' do
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category("Pop Culture")).to eq([])
+  end
 end
