71599017ecce92a63bbee89502274a25184ddfa7
Bryan Shears <brshears@gmail.com>
2022-05-14 13:41:32 -0600

    Create current_card method 7 take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -1,17 +1,20 @@
 class Round
   attr_reader :deck, :turns
 #Initializes the round object and passes an arguement deck.
-  def initialize(deck, turns)
+  def initialize(deck)
     @deck = deck
     @turns = []
 
 
   end
 
-#   def current_card
-#
-#     deck.cards[0]
-#
-#   end
-#
+  def current_card
+
+   deck.cards.first
+
+  end
+
+  def take_turn
+
+  end
 end
