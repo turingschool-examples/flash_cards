52802938e8a9146c15a4309fb3a9a2cd62b6d0dd
Sunny Moore <moore.sunny@gmail.com>
2022-05-14 15:19:50 -0700

    take_turn adds new turn to rounds.turn

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -88,7 +88,7 @@ describe Round do
     new_turn = round.take_turn("Juneau")
     expect(round.current_card).to eq(card_2)
   end
-  xit "take_turn adds new turn to rounds.turn" do
+  it "take_turn adds new turn to rounds.turn" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
