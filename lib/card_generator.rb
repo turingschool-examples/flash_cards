28fcf33db3f8e6b23991657b3b1e7329d10199a1
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-05-14 17:57:49 -0500

    Made final edits before final commit and pull request.

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -8,11 +8,8 @@ class CardGenerator
   def initialize(filename)
     @filename = filename
     @cards = []
-  # end
-  #
-  # def compiler
+
     File.open(@filename, "r").each do |line|
-    # line = IO.readlines(@filename)[0].strip.split(",")
       new_card_content = line.strip.split(",")
       new_card = Card.new(new_card_content[0], new_card_content[1], new_card_content[2])
       @cards << new_card
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -12,28 +12,28 @@ RSpec.describe Card do
     @deck = Deck.new(@cards)
   end
 
-    it 'can create three instances of Cards' do
-      expect(@card_1).to be_instance_of(Card)
-      expect(@card_1.answer).to eq("Juneau")
-      expect(@card_2.question).to eq("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?")
-      expect(@card_3.category).to eq(:STEM)
-    end
+  it 'can create three instances of Cards' do
+    expect(@card_1).to be_instance_of(Card)
+    expect(@card_1.answer).to eq("Juneau")
+    expect(@card_2.question).to eq("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?")
+    expect(@card_3.category).to eq(:STEM)
+  end
 
-    it 'can store Cards in a Deck' do
-      expect(@deck).to be_instance_of(Deck)
-      expect(@deck.cards).to eq(@cards)
-      expect(@deck.cards[0].answer).to eq("Juneau")
-      expect(@deck.cards[1].question).to eq("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?")
-      expect(@deck.cards[2].category).to eq(:STEM)
-    end
+  it 'can store Cards in a Deck' do
+    expect(@deck).to be_instance_of(Deck)
+    expect(@deck.cards).to eq(@cards)
+    expect(@deck.cards[0].answer).to eq("Juneau")
+    expect(@deck.cards[1].question).to eq("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?")
+    expect(@deck.cards[2].category).to eq(:STEM)
+  end
 
-    it 'can count the number of cards in a deck' do
-      expect(@deck.count).to equal(3)
-    end
+  it 'can count the number of cards in a deck' do
+    expect(@deck.count).to equal(3)
+  end
 
-    it 'can call cards by category' do
-      expect(@deck.cards_in_category(:STEM)).to eq(@cards[1,2])
-      expect(@deck.cards_in_category(:Geography)).to eq(@cards.first(1))
-      expect(@deck.cards_in_category("Pop Culture")).to eq([])
-    end
+  it 'can call cards by category' do
+    expect(@deck.cards_in_category(:STEM)).to eq(@cards[1,2])
+    expect(@deck.cards_in_category(:Geography)).to eq(@cards.first(1))
+    expect(@deck.cards_in_category("Pop Culture")).to eq([])
+  end
 end
