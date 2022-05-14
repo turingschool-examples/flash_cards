04c7a3cc7be839d6d718e9e0a6fa09816cf38ca3
Blake Saylor <blakesaylor@gmail.com>
2022-05-14 12:33:48 -0600

    Update add cards method to pass tests

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,9 +1,21 @@
+require './lib/card'
+require './lib/deck'
+
 class CardGenerator
-  attr_reader :filename
+  attr_reader :filename, :cards
 
   def initialize(filename)
     @filename = filename
+    @cards = []
   end
 
-
+  def add_cards
+    File.readlines(@filename).each do |line|
+      split_line = line.split(",")
+      question = split_line[0]
+      answer = split_line[1]
+      category = (":" + split_line[2]).to_sym
+      @cards << Card.new(question, answer, category)
+    end
+  end
 end
