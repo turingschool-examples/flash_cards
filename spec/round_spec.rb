b6dc269b9caeefa4d35c4aaa9b955e446c3422a8
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-14 10:34:12 -0700

    Refactored tests to have less lines of code

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -40,31 +40,32 @@ RSpec.describe Round do
 
   it "can return the number of correct guesses" do
     new_turn = @round.take_turn("Juneau")
-    expect(new_turn.class).to eq (Turn)
-    expect(new_turn.correct?).to eq true
-    expect(@round.turns).to eq ([new_turn])
     expect(@round.number_correct).to eq(1)
 
     expect(@round.current_card).to eq (@card_2)
-    expect(@round.take_turn("Venus")).to be_a(Turn)
-    expect(@round.turns.count).to eq (2)
+    new_turn = @round.take_turn("Venus")
     expect(@round.turns.last.feedback).to eq ("Incorrect.")
     expect(@round.number_correct).to eq(1)
   end
 
   it "can return the number of guesses correct by category" do
     new_turn = @round.take_turn("Juneau")
-    expect(new_turn.class).to eq (Turn)
-    expect(new_turn.correct?).to eq true
-    expect(@round.turns).to eq ([new_turn])
-    expect(@round.number_correct).to eq(1)
     expect(@round.number_correct_by_category(:Geography)).to eq(1)
 
     expect(@round.current_card).to eq (@card_2)
-    expect(@round.take_turn("Venus")).to be_a(Turn)
-    expect(@round.turns.count).to eq (2)
+    new_turn = @round.take_turn("Venus")
+    expect(@round.turns.last.feedback).to eq ("Incorrect.")
+    expect(@round.number_correct_by_category(:STEM)).to eq(0)
+  end
+
+  it "can return percent correct" do
+    new_turn = @round.take_turn("Juneau")
+    expect(@round.number_correct).to eq(1)
+
+    expect(@round.current_card).to eq (@card_2)
+    new_turn = @round.take_turn("Venus")
     expect(@round.turns.last.feedback).to eq ("Incorrect.")
     expect(@round.number_correct).to eq(1)
-    expect(@round.number_correct_by_category(:STEM)).to eq(0)
+    expect(@round.percent_correct).to eq(50.0)
   end
 end
