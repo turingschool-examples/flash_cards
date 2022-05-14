86a17ab51f1a4888f756f244ddb5ca7af97091ad
Lucas Colwell <colwellc37@gmail.com>
2022-05-14 16:00:21 -0600

    Initial commit + structure

---
+++ spec/round_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/class'
+
+RSpec.describe Class do
+  describe 'Iteration 1' do
+    before :each do
+      @
+      @
+    end
+
+    it 'exists' do
+      expect(@class).to be_a(Class)
+    end
+
+    it 'has attributes' do
+      expect(@class.attribute).to eq()
+    end
+
+  end
+
+end
