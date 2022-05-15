154930bfecea6fc417ec1b52ebdac4e27820c2bb
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-05-14 21:17:33 -0600

    Added tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -53,4 +53,20 @@ RSpec.describe Round do
     expect(round.number_correct).to eq(1)
     expect(round.current_card).to eq(card_2)
   end
+
+  it '' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    turns = [new_turn]
+
+    expect(round.take_turn("Venus")).to be_instance_of(Turn)
+    expect(round.turns.count).to eq(2)
+    expect(round.turns.last.feedback).to eq("Incorrect.")
+    expect(round.number_correct).to eq(1)
+  end
 end
