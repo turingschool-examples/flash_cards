76f2e7880be6ab78baf3c60f1a5208db0d61347d
Andre Pedro <andrerpedro170@gmail.com>
2022-05-15 09:28:01 -0600

    create cards

---
+++ lib/flashcard_runner.rb
@@ -0,0 +1,8 @@
+
+
+p "Welcome! You're playing with 4 cards."
+
+card_1 = "What is the magic ingredient to the krabby patty?", "it's a secret", :Bikini_bottom
+card_2 = "What color are the buses in Bikini Bottom?", "orange", :Bikini_bottom
+card_3 = "How many flags are on the moon currently?" "1", :Astronomy
+card_4 = "Should you eat food found on the ground on the street?", "No", :Street_smarts
