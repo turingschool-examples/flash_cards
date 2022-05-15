f62cc640a1045e97527aa832d8aafa7ce22f5558
Andre Pedro <andrerpedro170@gmail.com>
2022-05-15 13:18:07 -0600

    replace card info, works now

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,10 +4,10 @@
   require './lib/deck'
   require './lib/round'
 
-card_1 = Card.new("What is the magic ingredient to the krabby patty?", "it's a secret", :Bikini_bottom)
-card_2 = Card.new("What color are the buses in Bikini Bottom?", "orange", :Bikini_bottom)
-card_3 = Card.new("How many flags are on the moon currently?" "1", :Astronomy)
-card_4 = Card.new("Should you eat food found on the ground on the street?", "No", :Street_smarts)
+card_1 = Card.new("What is 5 + 5?", "10", :STEM)
+card_2 = Card.new("What is Rachel's favorite animal?","red panda",:"Turing Staff")
+card_3 = Card.new("What is Mike's middle name?", "nobody knows", :"Turing Staff")
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin bieber" , "PopCulture")
 cards = [card_1, card_2, card_3, card_4]
 deck = Deck.new(cards)
 round = Round.new(deck)
