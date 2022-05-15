88acdcdc8a9852e8c22d504e08c8d7d6ebe4e218
Thiago Silveira <thiago1992@yahoo.com>
2022-05-15 16:23:27 -0600

    Add: Flashcard runner file + IT3 readme file

---
+++ flashcard_runner.rb
@@ -0,0 +1,7 @@
+# Create some Cards
+# Put those card into a Deck
+# Create a new Round using the Deck you created
+# Start the round using a new method called start
+
+# puts "Welcome! You're playing with #{cards.size} cards."
+# puts "--------------------------------------------------"
