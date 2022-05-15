d3cae5198608d5b8bc00bfe007fa205e89691af1
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-14 18:07:52 -0600

    Iteration 2 - Add tests: Count turns Give feedback

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -49,7 +49,8 @@ RSpec.describe Round do
     expect(round.current_card).to eq(card_1)
   end
 
-  it 'it take_turn' do
+  describe 'take_turn' do
+  it 'it has correct class, returns correct, displays turns' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -59,23 +60,23 @@ RSpec.describe Round do
     new_turn = round.take_turn("Juneau")
     turns = [new_turn]
 
-
     expect(new_turn.class).to eq(Turn)
-    expect(round.turns).to eq(turns)
-
-  end
-
-  it 'is correct?' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    round = Round.new(deck)
-    new_turn = round.take_turn("Juneau")
-
     expect(new_turn.correct?).to eq(true)
+    expect(round.turns).to eq(turns)
   end
+  end
+
+  # it 'is correct?' do
+  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  #   cards = [card_1, card_2, card_3]
+  #   deck = Deck.new(cards)
+  #   round = Round.new(deck)
+  #   new_turn = round.take_turn("Juneau")
+  #
+  #   expect(new_turn.correct?).to eq(true)
+  # end
 
   it 'counts number_correct' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -87,6 +88,45 @@ RSpec.describe Round do
     new_turn = round.take_turn("Juneau")
 
     expect(round.number_correct).to eq(1)
+  end
+
+  it 'take_turn 2' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    expect(round.take_turn("Venus")).to be_instance_of(Turn)
+  end
+
+  it 'counts turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    expect(round.turns.count).to eq(2)
+  end
+
+  it 'gives feedback' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    expect(round.turns.last.feedback).to eq("Incorrect.")
   end
 
 end
