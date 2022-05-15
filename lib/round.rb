7e2245ad8f6cfdd7ecb4f5634516911d577e04b6
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-15 03:29:17 -0600

    add 2 methods

--- lib/round.rb
+++ lib/round.rb
@@ -5,21 +5,56 @@
 # The take_turn method is the crux of this problem. The take_turn method takes a string representing the guess. It should create a new Turn object with the appropriate guess and Card. It should store this new Turn, as well as return it from the take_turn method. Also, when the take_turn method is called, the Round should move on to the next card in the deck.
 
 class Round
-  attr_reader :deck, :turns, :current_card
+  attr_reader :deck, :turns, :current_card, :current_turn, :number_correct, :correct_qs
 
   def initialize(deck)
     @deck = deck #deck of cards for this round
     @turns = [] #initialized with empty array
-    @current_card = @deck.cards[0] #initializes current card with first card of deck
-
+    @current_turn = 0 #to iterate through deck depending on turn
+    @number_correct = 0 #inititalized with 0
+    @correct_qs = [] #placeholder array for correct answers
+    @current_card = @deck.cards[@current_turn]#initializes current card with first card of deck
   end
 
-  # def take_turn(guess_string)
-  #   #shovel guesses into turns array
-  #   #should create a new turn object with guess + card, and store in new turn
-  #
-  #   #return new Turn
-  #
-  # end
+ #
+ #  def current_card
+ #
+
+ #
+ #  end
+
+  def take_turn(guess_string)
+    if @current_turn > @deck.count #checks to make sure you stay within array bounds
+       return  "you have reached the limit of questions"
+    else
+      new_turn = Turn.new(guess_string, @current_card)
+      @turns << new_turn #shovel guesses into turns array
+      @current_turn += 1 #incrementing current # of turns
+
+    # if @current_card.category
+
+      if new_turn.correct?
+        @number_correct += 1 #increments correct if correct
+        @correct_qs << @current_card #will send in current card
+      end
+
+      return new_turn
+    end
+  end
+
+  def correct_by_category(category_choice) #will send in a searchable category
+    num_right = 0 #initialized with 0, will increment for correct # for given category
+    @number_correct.times do #however # correct times, iterate through correct_qs array and check category of choice
+        if @correct_qs[num_right].category == category_choice
+          num_right += 1
+        end
+    end
+
+    return num_right
+  end
+
+  def percent_correct
+    return @deck.count / @number_correct #divides total number of questions by number of correct
+  end
 
 end
