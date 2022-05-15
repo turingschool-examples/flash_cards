236d9998ed1a807d21266fb0c8958e84bfeff326
Bryan Shears <brshears@gmail.com>
2022-05-15 10:01:03 -0600

    Add percent_correct method

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,7 @@
 
 class Round
   attr_reader :deck, :turns, :number_correct
-#Initializes the round object and passes an arguement deck.
+  #Initializes the round object and passes an arguement deck.
   def initialize(deck)
     @deck = deck
     @turns = []
@@ -19,16 +19,18 @@ class Round
 
   def take_turn(guess)
 
-    turn = Turn.new(guess, current_card)
-      @turns << turn
-      if guess == current_card.answer
-        @number_correct += 1
-      end
-      deck.cards.rotate!
-      #may require tinkering in iteration_3
-      return turn
+      turn = Turn.new(guess, current_card)
+        @turns << turn
+        if guess == current_card.answer
+            @number_correct += 1
+        end
+        deck.cards.rotate!
+            #may require tinkering in iteration_3
+        return turn
 
   end
+
+
 
   def number_correct_by_category(category)
 
@@ -43,10 +45,21 @@ class Round
 
   end
 
-  def round.percent_correct
+  def percent_correct
+
+   (number_correct.to_f/(turns.count.to_f)) * 100
 
   end
 
-#select retuns an array by default [] when nothing is in the block
+
+
+  def percent_correct_by_category(category)
+
+    
+
+
+  end
+
 
 end
+    #select retuns an array by default [] when nothing is in the block
