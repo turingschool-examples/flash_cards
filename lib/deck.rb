c9ca6e8dc1f6a0045a82bac6dab40da85877a264
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-15 01:50:42 -0600

    complete all method requests

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,9 +5,28 @@ class Deck
   def initialize(cards)
     @cards = cards
     @count = @cards.count() #not sure if should make individual method - .count() counts the # of objects in array
-
   end
 
+  def cards_in_category(selected_category) #selected category = category you're searching extend for
+     #setting iterate# to current count will (if i == @count, break)
+
+    current_search = []
+    # couldn't figure out how to use  do
+    # @cards.each do |card|
+    #   if @cards.category == selected_category
+    #     current_search << card
+    #   end
+
+    i = 0
+    @count.times do #will iterate through each card category and add to current search to print
+      if @cards[i].category == selected_category
+        current_search << @cards[i]
+      i += 1
+    end
+
+      return current_search
+    end
 
 
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -37,12 +37,15 @@ RSpec.describe Deck do
 
     expect(deck.count).to eq(3)
   end
-  #
 
-  #
-  # it 'has a category' do #done
-  #   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #
-  #   expect(card.category).to eq(:Geography)
-  # end
+  it 'will return cards of given category' do #done
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq([])
+  end
 end
