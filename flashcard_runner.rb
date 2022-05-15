41aad5aa5f08d14874678a2391de40b35a36057f
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-15 15:14:17 -0400

    Add percent correct for all categories method call

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,10 +3,10 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-card_1 = Card.new("What is 5 + 5?", "10", :STEM)
-card_2 = Card.new("What is Rachel's favorite animal?", "Giraffe", :Turing_Staff)
-card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
-card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+card_1 = Card.new("What is 5 + 5?", "10", :"STEM")
+card_2 = Card.new("What is Rachel's favorite animal?", "red panda", :"Turing Staff")
+card_3 = Card.new("What is Mike's middle name?", "nobody knows", :"Turing Staff")
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :"Pop Culture")
 deck = Deck.new([card_1, card_2, card_3, card_4])
 round = Round.new(deck)
 
@@ -16,4 +16,6 @@ puts "-" * 50
 round.start
 
 puts "****** Game over! ******"
-puts "You had #{round.number_correct} out of #{deck.count} for a total score of #{round.percent_correct}%."
+puts "You had #{round.number_correct} correct guesses out of #{deck.count} for a total score of #{round.percent_correct.to_i}%."
+
+round.percent_correct_for_all_categories
