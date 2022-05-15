7509d31a7f9ef723fe6e7cf9c6305571459a5e5b
Bryan Shears <brshears@gmail.com>
2022-05-15 15:00:41 -0600

    Re-factor flashcard_runner.rb

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -3,17 +3,17 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-card_1 = Card.new("Question: What is 5 + 5?," "10", :STEM)
-card_2 = Card.new("What is Rachel's favorite animal?", "panda", :"Turing Staff")
-card_3 = Card.new("What is Mike's middle name?", "nobody knows", :"Turing Staff")
-card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
-deck = Deck.new([card_1, card_2, card_3, card_4])
+card_1 = Card.new("Question: What is 5 + 5?", "10", :STEM)
+card_2 = Card.new("Question: What is Rachel's favorite animal?", "panda", :"Turing Staff")
+card_3 = Card.new("Question: What is Mike's middle name?", "nobody knows", :"Turing Staff")
+card_4 = Card.new("Question: What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
+cards = ([card_1, card_2, card_3, card_4])
+
+deck = Deck.new(cards)
 round = Round.new(deck)
 
 
 
-puts "Welcome! You're playing with #{deck.count} cards."
-puts "-" * 42
 
 round.start
 
