401d6ab24cd466a673d8420113309c18547901c3
Anna Marie Sterling <sterling.am@gmail.com>
2022-05-15 16:17:55 -0600

    Flash card game runs

--- lib/round.rb
+++ lib/round.rb
@@ -44,17 +44,25 @@ class Round
   end
 
   def percent_correct_by_category(category)
-      @number_correct_by_category / @deck.cards_in_category(category).count.to_f * 100
+      (number_correct_by_category(category) / @deck.cards_in_category(category).count.to_f) * 100
+
   end
 
   def start
     puts "Welcome! You're playing with 4 cards."
-    puts "This is card number 1 out of 4."
-    current_card
-    puts (current_card.question)
-    guess = gets.capitalize
-    take_turn(guess)
-    binding.pry
-    puts (turns.feedback)
+    -------------------------------------------------
+    i= 0
+    until i == deck.count
+        i += 1
+    puts "This is card number #{i} out of #{deck.count}"
+    puts "Question: #{current_card.question}"
+    user_guess = gets.chomp.capitalize
+    puts take_turn(user_guess).feedback
+    end
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of 4 for a total score of #{percent_correct.to_i}%."
+    puts "Reactive Nonmetals - #{percent_correct_by_category(:ReactiveNonmetals).to_i}% correct"
+    puts "Noble Gases - #{percent_correct_by_category(:NobleGases).to_i}% correct"
+    puts "Metalloids - #{percent_correct_by_category(:Metalloids).to_i}% correct"
+    end
   end
-end
