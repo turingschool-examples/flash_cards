26128d867e938b6f0ec3b11fb31a94d044483619
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-05-15 11:32:46 -0600

    add cards, deck, and round to flashcard_runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,5 +7,7 @@ card_1 = Card.new("What is 5 + 5?", "10", :STEM)
 card_2 = Card.new("What is Rachel's favorite animal?", "cat", :Turing_Staff)
 card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
 card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Turing_Staff)
-
+cards = [card_1, card_2, card_3, card_4]
+deck = Deck.new(cards)
+round = Round.new(deck)
 #calling start in runner
