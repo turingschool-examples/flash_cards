1be8fd486b57086d6c965d48edb5c4bb52bec32e
Bryan Shears <brshears@gmail.com>
2022-05-15 12:14:02 -0600

    pass percent_correct_by_category test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -166,7 +166,7 @@ RSpec.describe Round do
     expect(round.percent_correct).to eq(50.0)
   end
 
-  it 'can determine the percent of correct guesses' do
+  it 'can determine the percent of correct guesses by category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
