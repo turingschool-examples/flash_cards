77c74b98d051e19c5f52002b20a39c369c5952b3
Andre Pedro <andrerpedro170@gmail.com>
2022-05-14 20:56:31 -0600

    complete round.take_turn

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -85,8 +85,19 @@ RSpec.describe Round do
     expect(round.current_card).to eq(card_2)
     expect(round.turns).to eq(turns)
     expect(round.number_correct).to eq(1)
+  end
 
+  it 'takes turn Venus' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    turns = [turn]
 
+    expect(round.take_turn("Venus")).to be_instance_of(Turn)
 
   end
 
