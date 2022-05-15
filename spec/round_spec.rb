15d795cea0ac0ebabc7a4c51da50a4b02663187e
Ben Silverstein <benjsilverstein@comcast.net>
2022-05-14 21:16:56 -0600

    Iteration 2 - Add tests: percen_correct percent_correct_by_category current_card

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -168,9 +168,45 @@ RSpec.describe Round do
     expect(round.number_correct_by_category(:STEM)).to eq(0)
   end
 
+  it 'counts percent_correct' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    expect(round.percent_correct).to eq(50.0)
+  end
+
+  it 'counts percent_correct_by_category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
+  end
+  it 'returns current_card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
+
+    expect(round.current_card).to eq(card_3)
+  end
+
 end
-
-
 
 
 
