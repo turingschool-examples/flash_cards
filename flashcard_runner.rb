b419d4c3e46c4a8c3060c7c452dd7afc3f888c86
Lucas Colwell <colwellc37@gmail.com>
2022-05-15 17:11:37 -0600

    Speedcode runner file

---
+++ flashcard_runner.rb
@@ -0,0 +1,41 @@
+# note to reader
+
+# This is still a draft and needs work, I wrote the whole runner file
+# without testing between steps while the process was fresh in my head
+
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+@card_1 = Card.new("What is 5 + 5?", 10, :STEM)
+@card_2 = Card.new("What is Rachel's favorite animal?", "Dog", :TuringStaff)
+@card_3 = Card.new("What is Mike's middle name?", "nobody knows", :TuringStaff)
+@card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :PopCulture)
+@deck = Deck.new([@card_1, @card_2, @card_3, @card_4])
+@round = Round.new(@deck)
+
+def start
+  @round.deck.count.times do
+    puts "Welcome! You're playing with #{@deck.count} cards."
+    puts "-------------------------------------------------"
+    puts "This is card number #{@round.current_card} out of #{@round.deck.count}."
+    puts "Question: #{@round.deck[0].question}"
+
+    answer = gets.chomp
+
+    if answer == @round.deck[0].answer
+      puts answer
+      puts "#{@round.turns.last.feedback}"
+    else
+      puts answer
+      puts "#{@round.turns.last.feedback}"
+    end
+
+    puts "****** Game over! ******"
+    puts "You had #{@round.number_correct} correct guesses out of #{@round.deck.count} for a total score of #{@round.percent_correct}."
+
+
+  end
+
+end
