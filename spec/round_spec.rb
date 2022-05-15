4a5280ef03176d2c2ee1ae663a3b12525f532c83
Alex M <Alexm717@hotmail.com>
2022-05-15 16:34:38 -0700

    rounds final

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,42 +1,25 @@
-require './lib/card'
-require './lib/turn'
-require './lib/deck'
-require './lib/round'
+require "./lib/card"
+require "./lib/turn"
+require "./lib/deck"
+require "./lib/round"
 
-RSpec.describe Round do
 
-  it 'exsits' do
+RSpec.describe(Round) do
+  it("exsits") do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = [card_1, card_2, card_3]
     round = Round.new(deck)
-    expect(round).to be_instance_of(Round)
-end
-end
+    expect(round).to((be_instance_of(Round)))
+  end
 
-# it 'has deck' do
-#   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#   deck = [card_1, card_2, card_3]
-#   round = Round.new(deck)
-#   expect(rounds.deck).to eq(deck)
-# end
-#
-# it 'has turns' do
-#   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#   deck = [card_1, card_2, card_3]
-#   round = Round.new(deck)
-#   expect(rounds.turns).to eq([])
-# end
-# it 'has current_card' do
-#   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#   deck = [card_1, card_2, card_3]
-#   round = Round.new(deck)
-#   expect(rounds.current_card).to eq(card_1)
-# end
+  it("has turns") do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = [card_1, card_2, card_3]
+    round = Round.new(deck)
+    expect(round.turns).to(eq(turns))
+  end
+end
