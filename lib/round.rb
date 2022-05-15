cc046e8aa378b2dccf528fe90ce11ea45d84adcb
Wyatt Kribs <kribswyatt@gmail.com>
2022-05-15 15:06:38 -0600

    adds initial tests for take turn method

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,6 @@
+require './lib/turn'
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :new_turn
   def initialize(deck)
     @deck = deck
     @turns = []
@@ -7,5 +8,14 @@ class Round
 
   def current_card
     deck.cards[0]
+  end
+
+
+  def take_turn(guess)
+    new_turn = Turn.new(guess, current_card)
+    if new_turn.guess == new_turn.card.answer
+    end
+    turns << new_turn
+    new_turn
   end
 end
