40130bc29f34284b718795932d1b695306e175e9
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-15 17:54:52 -0600

    final final commit

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -51,4 +51,36 @@ RSpec.describe Round do
     expect(round.percent_correct).to eq(50.00)
   end
 
+  # it 'number_correct_by_category returns 2' do
+  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  #   deck = Deck.new([card_1, card_2, card_3])
+  #   round = Round.new(deck)
+  #   round.deck
+  #   round.turns
+  #   round.current_card
+  #   new_turn = round.take_turn("Juneau")
+  #   round.take_turn("Mars")
+  #   round.take_turn("North north west")
+  #
+  #   expect(round.number_correct_by_category(:STEM)).to eq(2)
+  # end
+
+  # it 'percent_correct_by_category returns 100.00' do
+  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  #   deck = Deck.new([card_1, card_2, card_3])
+  #   round = Round.new(deck)
+  #   round.deck
+  #   round.turns
+  #   round.current_card
+  #   new_turn = round.take_turn("Juneau")
+  #   round.take_turn("Mars")
+  #   round.take_turn("North north west")
+  #
+  #   expect(round.percent_correct_by_category(:STEM)).to eq(100.00)
+  # end
+
 end
