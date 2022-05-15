56708835bd0d97e0b8ab96ff1cd19d005d1f5b7b
Bryan Shears <brshears@gmail.com>
2022-05-14 22:30:37 -0600

    Define number_correct_by_category

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -140,28 +140,27 @@ RSpec.describe Round do
     expect(round.number_correct). to eq(1)
   end
 
-
-
-
-  xit 'can determine the number correct by category' do
+  it 'can determine the number correct by category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
     expect(round.number_correct_by_category(:Geography)).to eq(1)
     expect(round.number_correct_by_category(:STEM)).to eq(0)
-    #is this the correct way to test an integer?
+
   end
 
-  xit 'can determine the percent of correct guesses' do
+  it 'can determine the percent of correct guesses' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
     expect(round.percent_correct).to eq(50.0)
   end
 
@@ -172,6 +171,7 @@ RSpec.describe Round do
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
     expect(round.percent_correct_by_category).to eq(100.0)
   end
 
@@ -182,6 +182,7 @@ RSpec.describe Round do
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
     new_turn = round.take_turn("Juneau")
+    round.take_turn("Venus")
     expect(round.current_card).to eq(card_3)
   end
   # before :each do
