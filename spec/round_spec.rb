115f79a029eff0bd9510a2eb437c8484d51ed8f0
Drew MacNicholas <dmacnicholas@gmail.com>
2022-05-14 23:55:57 -0500

    Add turns count test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -110,7 +110,7 @@ RSpec.describe Round do
     expect(round.current_card).to eq(card_2)
   end
 
-  it 'the current Turn object' do
+  it 'the current Turn object for Card 2' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -122,6 +122,19 @@ RSpec.describe Round do
 
     expect(turn_2).to be_instance_of(Turn)
     expect(turn_2.card).to eq(card_2)
+  end
+
+  it 'counts the turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    turn_2 = round.take_turn("Venus")
+
+    expect(round.turns.count).to eq(2)
   end
 
 
