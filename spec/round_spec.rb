75f785c0e536f0c75524a3a9eb753a2a1729c674
Lucas Colwell <colwellc37@gmail.com>
2022-05-15 15:58:10 -0600

    Refactor test wording

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -67,9 +67,6 @@ RSpec.describe Round do
         it 'can tell number correct by category' do
           expect(@round.number_correct_by_category(:Geography)).to eq(1)
           expect(@round.number_correct_by_category(:STEM)).to eq(0)
-        end
-
-        it 'can tell percent correct' do
           expect(@round.number_correct_by_category(:STEM)).to eq(50.0)
         end
 
