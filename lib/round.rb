fd01dcba8c25bb2ed38a8337e10008fc1654cfd0
Wyatt Kribs <kribswyatt@gmail.com>
2022-05-15 16:56:59 -0600

    adds functionality to take turn method so it can add up correct guesses !!!

--- lib/round.rb
+++ lib/round.rb
@@ -1,9 +1,10 @@
 require './lib/turn'
 class Round
-  attr_reader :deck, :turns, :new_turn
+  attr_reader :deck, :turns, :new_turn, :number_correct
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
   end
 
   def current_card
@@ -12,10 +13,17 @@ class Round
 
 
   def take_turn(guess)
-    new_turn = Turn.new(guess, current_card)
-    if new_turn.guess == new_turn.card.answer
+    @new_turn = Turn.new(guess, current_card)
+    turns << @new_turn
+    if current_card.answer == guess
+      add_correct
     end
-    turns << new_turn
+    # require 'pry' ; binding.pry
     new_turn
   end
+
+  def add_correct
+    @number_correct =+ 1
+  end
+
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -45,18 +45,14 @@ describe Round do
     round = Round.new(deck)
     round.take_turn("Juneau")
     expect(round.current_card).to eq(card_1)
-    # require 'pry' ; binding.pry
   end
 
-  # it "can tell you that turn has a card" do
-  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  #   deck = Deck.new([card_1, card_2, card_3])
-  #   round = Round.new(deck)
-  #   round.take_turn("Juneau")
-  #   expect().to eq(card_1)
-  # end
+  it "can count correct answers" do
+    @round.take_turn("Juneau")
+    # require 'pry' ; binding.prys
+    expect(@round.number_correct).to eq(1)
+  end
+
 
 
 end
