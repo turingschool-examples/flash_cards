c3a87fa316d759fc3aaf57741f03753462d28ab4
Gauri Joshi <gauri728@gmail.com>
2022-05-15 15:22:32 -0700

    Completes Iteration 3

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,3 +1,4 @@
+require './lib/round.rb'
 #create some Cards
 #put those card into a Deck
 #create a Round using the Deck you created
@@ -11,10 +12,10 @@ class Card
   end
 end
 
-card_1 = Card.new("What is 5 + 5?", "10", :STEM)
-card_2 = Card.new("What is Rachel's favorite animal?", "girrafe", :Animal)
-card_3 = Card.new("What is Mike's middle name?", "Sal", :Turing_staff)
-card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_culture)
+card_1 = Card.new("What is the fastest animal?", "Cheetah", :Animal)
+card_2 = Card.new("What is the color of the ocean?", "Blue", :Geography)
+card_3 = Card.new("What is the coolest city on earth?", "NYC", :Geography)
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop)
 
 cards = [card_1, card_2, card_3, card_4]
 
@@ -31,15 +32,26 @@ end
 deck = Deck.new(cards)
 # p deck
 
-class Round
-  attr_reader :deck, :turns, :turn_counter, :correct, :num, :guess
-  def initialize(deck)
-    @deck = deck
-    @turns = []
-    @turn_counter = 0
-    @correct = 0
-    @num = 0
+class Turns
+  attr_reader :guess, :card, :answer
+  def initialize(guess, card)
     @guess = guess
+    @card = card
+    @answer = answer
+  end
+
+  def correct?
+    if @guess == @answer
+      return true
+    end
+  end
+
+  def feedback
+    if @guess == @answer
+      return "Correct!"
+    else
+      return "Incorrect"
+    end
   end
 
   def start
@@ -48,211 +60,188 @@ class Round
     print "------------------------------------"
     puts
     puts "This is card 1 out of 4."
-    puts "Question: What is 5 + 5?"
+    puts "What is the fastest animal?"
 
-    guess = gets.chomp
-    # correct = 0
+    @guess = gets.chomp
 
-    if guess == "10"
-      puts "Correct!"
-      # return correct += 1
-      # print "So far you have #{correct} correct."
-    else
-      puts "Incorrect."
-      # return correct = 0
-    end
-  end
-
-  def next_card
-    puts "This is card 2 out of 4."
-    puts "Question: What is Rachel's favorite animal?"
-
-    animal = gets.chomp
-    # correct = 1
-
-    if animal == "giraffe" || animal == "Giraffe"
-      puts "Correct!"
-      # return correct += 1
-      # print "So far you have #{correct} correct."
-    else
-      puts "Incorrect."
-      # return correct = 0
-    end
-  end
-
-  def third_card
-    puts "This is card 3 out of 4."
-    puts "Question: What is Mike's middle name?"
-
-    mid_name = gets.chomp
-    # correct = 2
-    # mid_name.capitalize
-
-    if mid_name == "Sal" || mid_name == "sal"
-      puts "Correct!"
-      # return correct += 1
-      # print "So far you have #{correct} correct."
-    else
-      puts "Incorrect."
-      # return correct = 0
-    end
-  end
-
-  def last_card
-    puts "This is card 4 out of 4."
-    puts "Question: What cardboard cutout lives at Turing?"
-
-    cutout = gets.chomp
-    # correct = 3
-
-    if cutout == "Justin Bieber" || cutout == "justin bieber"
-      puts "Correct!"
-      # return correct += 1
-      # print "So far you have #{correct} correct."
-    else
-      puts "Incorrect."
-      # return correct
-    end
   end
 end
 
+
+turns = Turns.new("Cheetah", cards[0])
 round = Round.new(deck)
-p round.start
-p round.next_card
-p round.third_card
-p round.last_card
-print "******* GAME OVER **********"
+p turns.start
+# puts turns.correct?
+
+round.add_1_to_turn
+turns.correct?
+
+# round.take_turn("Cheetah")
+
+puts turns.feedback
+# round.add_1_to_correct
+
+puts "This is card 2 out of 4."
+puts cards[1].question
+
+@guess = gets.chomp
+
+turns_2 = Turns.new("Blue", cards[1])
+
+round.add_1_to_turn
+turns_2.correct?
+
+# round.take_turn("Blue")
+
+puts turns_2.feedback
+# round.add_1_to_correct
+
+puts "This is card 3 out of 4."
+puts cards[2].question
+
+@guess = gets.chomp
+
+turns_3 = Turns.new("NYC", cards[2])
+
+
+round.add_1_to_turn
+turns_3.correct?
+
+# round.take_turn("NYC")
+puts turns_3.feedback
+# round.add_1_to_correct
+
+puts "This is card 4 out of 4."
+puts cards[3].question
+
+@guess = gets.chomp
+
+turns_4 = Turns.new("Justin Bieber", cards[3])
+
+round.add_1_to_turn
+turns_4.correct?
+
+# round.take_turn("Justin Bieber")
+puts turns_4.feedback
+
+# round.add_1_to_correct
+
+
+print "***********GAME OVER***************"
+
 puts
 
 
 
-puts "You had (num) correct guesses out of () for a total score of (%)"
+puts "You had 4 correct guesses out of 4 for a score of  "
+p round.percent_correct(4,4)
 
-puts "STEM - "
-puts "Turing Staff - "
-puts "Pop Culture - "
+p "Animal - #{round.percent_correct_by_category(:Animal)} % correct"
 
+p "Geography - #{round.percent_correct_by_category(:Geography)} % correct"
 
-
-
-
-  # def feedback
-  #   if guess == cards[0].answer
-  #     p "Correct"
-  #   else
-  #     p "Incorrect"
-  #   end
-  # end
+p "Pop - #{round.percent_correct_by_category(:Pop)} % correct"
 
 
 
 
-#   def rotate_cards
-#     @deck.cards.rotate
+
+
+# class Round
+#   attr_reader :deck, :turns, :turn_counter, :correct, :num, :guess
+#   def initialize(deck)
+#     @deck = deck
+#     @turns = []
+#     @turn_counter = 0
+#     @correct = 0
+#     @num = 0
+#     @guess = guess
 #   end
-#
-#   def current_Card
-#     rotate_cards
-#     p rotate_cards[0]
-#   end
-#
-#   def add_1_to_turn
-#     @turn_counter += 1
-#   end
-# end
-    # print "You guessed #{guess}"
-# answer = int(input("\n Enter answer"))
-  # def correct?
+
+  # def start
+  #   puts "Welcome! You're playing with 4 cards."
+  #
+  #   print "------------------------------------"
+  #   puts
+  #   puts "This is card 1 out of 4."
+  #   puts "Question: What is 5 + 5?"
+  #
+  #   guess = gets.chomp
+  #   # correct = 0
+  #
   #   if guess == "10"
-  #   return true
+  #     puts "Correct!"
+  #     # return correct += 1
+      # print "So far you have #{correct} correct."
+  #   else
+  #     puts "Incorrect."
+  #     # return correct = 0
   #   end
   # end
-    # if gets == 10 ## I want to use my feedback method here##
-    #    return "Correct!"
-    # else
-    #    return "Incorrect"
-    #  end
+  #
+  # def next_card
+  #   puts "This is card 2 out of 4."
+  #   puts "Question: What is the fastest animal?"
+  #
+  #   animal = gets.chomp
+  #   # correct = 1
 
-  # def correct?
-  #   if @guess == @answer
-  #       return true
+  #   if animal == "cheetah" || animal == "Cheetah"
+  #     puts "Correct!"
+  #     # return correct += 1
+  #     # print "So far you have #{correct} correct."
+  #   else
+  #     puts "Incorrect."
+  #     # return correct = 0
+  #   end
+  # end
+  #
+  # def third_card
+  #   puts "This is card 3 out of 4."
+  #   puts "Question: What is 8 + 8?"
+  #
+  #   guess = gets.chomp
+  #   # correct = 2
+    # mid_name.capitalize
+
+    # if guess == "16"
+    #   puts "Correct!"
+      # return correct += 1
+      # print "So far you have #{correct} correct."
+  #   else
+  #     puts "Incorrect."
+  #     # return correct = 0
   #   end
   # end
 
-  ##feedback method in place of this block
+  # def last_card
+  #   puts "This is card 4 out of 4."
+  #   puts "Question: What cardboard cutout lives at Turing?"
+  #
+  #   cutout = gets.chomp
+    # correct = 3
 
-# round = Round.new(deck)
-# p round.start
-# p round.feedback
-# round.rotate_cards
-# round.current_card
-
-
-# puts "This is card 2 out of 4."
-# puts "Question: What is Beyonce's middle name?"
-#
-# guess = gets.chomp
-
-# p round.feedback
-#
-# puts "This is card 3 out of 4."
-# puts "Question: ?"
-#
-# guess = gets.chomp
-# p round.feedback
-#
-# puts "This is card 4 out of 4."
-# puts "Question: ?"
-#
-# guess = gets.chomp
-# p round.feedback
-
-# p "You had  correct guesses out of "
-# p "STEM - correct"
-# p "Geography - correct"
-# p "Pop Culture - correct"
-
-# p round.feedback
-## This will START the new round of the Game.
-
-# class Turns
-#   attr_reader :guess, :card, :answer
-#   def initialize(guess, card)
-#       @guess = guess
-#       @card = card
-#       @answer = answer
-#   end
-#
-#   def correct?
-#     if @guess == @answer
-#       return true
+#     if cutout == "Justin Bieber" || cutout == "justin bieber"
+#       puts "Correct!"
+#       # return correct += 1
+#       # print "So far you have #{correct} correct."
+#     else
+#       puts "Incorrect."
+#       # return correct
 #     end
 #   end
 # end
 
-  # def feedback
-    # if turns.correct? == true
-    #How to call sister method scope for turns.correct??
-    # if @guess == @answer ## If the correct method isn't working this works
-    #   return "Correct!"
-    # else
-    #   return "Incorrect!"
-    # end
-# p turns.feedback
+# p cards
+# round = Round.new(deck)
 # p round.start
-
-
-
-
-# turns = Turns.new("10", card_1) #This will create a new Turn.
-# return turns.correct?
-# p turns.feedback
-
-
-  # if answer == 10
-  #   turns.correct? == true
-  # # else
-  # #   turns.correct? == false
-  #
-  # turns.feedback
-  # p "This is card 2 out of 4."
+# p round.next_card
+# p round.third_card
+# p round.last_card
+# print "******* GAME OVER **********"
+# puts
+# puts "You had 4 correct guesses out of 4 for a total score of 100%"
+#
+# puts "STEM - 100% "
+# puts "Geography - 100% "
+# puts "Pop Culture - 100% "
