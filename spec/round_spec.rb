adc9b782f70c5dbcf318a99d8445b14d867701cc
Drew MacNicholas <dmacnicholas@gmail.com>
2022-05-15 00:00:57 -0500

    Add returns feedback test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -137,6 +137,19 @@ RSpec.describe Round do
     expect(round.turns.count).to eq(2)
   end
 
+  it 'returns the last feedback' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    turn_2 = round.take_turn("Venus")
+
+    expect(round.turns.last.feedback).to eq("Incorrect.")
+  end
+
 
 
 end
