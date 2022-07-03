1fd9c7c9537a130af94abadccf8bc3018c14890f
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-03 11:56:52 -0600

    Add the beginnings of a Round class & spec file. Initialize is working but the methods need work.

---
+++ lib/round.rb
@@ -0,0 +1,21 @@
+require 'pry'
+#the purpose of Round is to record guesses & process reponses
+class Round
+
+  def initialize(deck)
+    @deck = deck
+  end
+
+  def turns
+  end
+
+  def current_card
+  end
+
+#the following method is going to be called on a Turn object
+#& here it should reveal the status of take_turn
+  def take_turn
+  end
+
+
+end
