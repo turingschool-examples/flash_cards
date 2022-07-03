6ef8c9344ba13f025cf4562b2a4d29da29807631
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-03 10:57:55 -0600

    Add a working deck class that passes the checkpoint in Iteration 2 for Deck

---
+++ lib/deck.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Deck
+
+  def initialize(cards_as_array)
+    @cards_as_array = cards_as_array
+  end
+
+  def cards
+    @cards_as_array
+  end
+
+  def count
+    @cards_as_array.count
+  end
+
+  def cards_in_category(specified_category)
+    @cards_as_array.select {|card| specified_category == card.category}
+  end
+
+end
