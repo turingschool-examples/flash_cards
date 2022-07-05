8ca46bbcde7a1eded21c0f001908b56daa57b2f7
Astrid Hecht <silentaviary@gmail.com>
2022-07-05 17:22:21 -0600

    Write preliminary deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,5 @@
 require './lib/card.rb'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-cards = [card_1, card_2, card_3]
-
-
-
 class Deck
 
   def initialize(cards)
@@ -35,7 +25,3 @@ class Deck
     p tally
   end
 end
-
-deck = Deck.new(cards)
-deck.cards_in_category(:STEM)
-card_1.answer
