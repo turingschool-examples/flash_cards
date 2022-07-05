2301de4954e5e2e408c7312e3d676b7e1635aee5
Riley McCullough <rileybmccullough@gmail.com>
2022-07-05 15:40:25 -0600

    Test update: can evaluate guess and return boolean

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,23 +3,15 @@ require './lib/card'
 
 RSpec.describe Turn do
 
-  before all do
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @turn = Turn.new("Juneau", card)
+  it 'can return card, guess and evauate guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    turn.card
+
+    expect(turn.guess).to eq("Juneau")
+
+    expect(turn.correct?).to eq(true)
   end
 
-  it 'can return card' do
-    expect(@turn.card).to eq("What is the capital of Alaska?", "Juneau", :Geography)
-  end
-
-  xit 'can return guess' do
-    expect(@turn.guess).to eq("Juneau")
-  end
-
-  xit 'can evaluate guess and give feedback' do
-    expect(@turn.correct?).to eq(true)
-
-    expect(@turn.feedback).to eq("Correct!")
-  end
 
 end
