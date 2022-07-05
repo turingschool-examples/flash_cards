5217ab1b33fffa888e7ef868743200fb3332fa2e
Riley McCullough <rileybmccullough@gmail.com>
2022-07-05 15:45:59 -0600

    Test: can return feedback

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -13,5 +13,10 @@ RSpec.describe Turn do
     expect(turn.correct?).to eq(true)
   end
 
+  it 'can give feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
 
 end
