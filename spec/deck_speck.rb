9f1ecf0e4fcf4559c3d946e1b6d9469b7d19efd5
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-05 17:17:48 -0600

    Add a working deck spec file

---
+++ spec/deck_speck.rb
@@ -0,0 +1,53 @@
+require './lib/card'
+require './lib/deck'
+require 'rspec'
+
+RSpec.describe Deck do
+  it 'exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    cards = [card_1, card_2]
+    deck = Deck.new(cards)
+
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it 'has an array of cards in the deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    cards = [card_1, card_2]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_as_array).to eq([card_1, card_2])#is this test running as expected?
+  end
+
+  it 'can count the number of cards in the deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    cards = [card_1, card_2]
+    deck = Deck.new(cards)
+
+    expect(deck.count).to eq(2)
+  end
+
+  it 'can list the cards in a given category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+  end
+
+  it 'can return an empty array if the selected category has no cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category("Pop Culture")).to eq([])
+  end
+
+end
