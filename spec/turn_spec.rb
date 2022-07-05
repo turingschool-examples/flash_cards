03ec0b512742b95df79eae4f3d3d9ed0c07ea5ed
Gavin Carew <gjcarew@gmail.com>
2022-07-05 15:25:57 -0700

    Refactor spec files to eliminate repetition

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,31 +2,23 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
-   it 'exists' do
-     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     turn = Turn.new("Juneau", card)
 
+  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  turn = Turn.new("Juneau", card)
+
+   it 'exists' do
      expect(turn).to be_instance_of(Turn)
    end
 
    it 'has a guess' do
-     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     turn = Turn.new("Juneau", card)
-
      expect(turn.guess).to eq("Juneau")
    end
 
    it 'has a card' do
-     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     turn = Turn.new("Juneau", card)
-
      expect(turn.card).to be_instance_of(Card)
    end
 
    it 'evaluates the answer' do
-     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     turn = Turn.new("Juneau", card)
-
      expect(turn.correct?).to be(true)
 
      turn2 = Turn.new("Anchorage", card)
@@ -35,9 +27,6 @@ RSpec.describe Turn do
    end
 
    it 'provides feedback' do
-     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     turn = Turn.new("Juneau", card)
-
      expect(turn.feedback).to eq("Correct!")
 
      turn2 = Turn.new("Anchorage", card)
