b71fcca7ecf6736b0fc32fe48679f9ffb3b80234
Alex M <Alexm717@hotmail.com>
2022-07-05 17:02:48 -0700

    updated before each

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -5,21 +5,46 @@ require "./lib/round"
 
 
 RSpec.describe(Round) do
-  it("exsits") do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    deck = [card_1, card_2, card_3]
-    round = Round.new(deck)
-    expect(round).to((be_instance_of(Round)))
+  before do
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @deck = Deck.new(([@card_1, @card_2, @card_3]))
+    @round = Round.new(@deck)
+  end
+
+  it("exsists") do
+    expect(@round).to((be_instance_of(Round)))
+  end
+
+  it("has deck") do
+    expect(@round.deck).to(eq(@deck))
   end
 
   it("has turns") do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    deck = [card_1, card_2, card_3]
-    round = Round.new(deck)
-    expect(round.turns).to(eq(turns))
+    expect(@round.turns).to(eq([]))
+  end
+
+  it("current card") do
+    expect(@round.current_card).to(eq(@card_1))
+  end
+
+  it("new turn is a Guess") do
+    @new_turn = @round.take_turn("Juneau")
+    expect(@new_turn).to(be_a(Turn))
+  end
+
+  it("is correct?") do
+    @new_turn = @round.take_turn("Juneau")
+    expect(@new_turn.correct?).to(eq(true))
+  end
+
+  it("has turns") do
+    @new_turn = @round.take_turn("Juneau")
+    expect(@round.turns).to(eq(@new_turn))
+  end
+
+  xit("counts the turns") do
+    expect(@round.turns.count).to(eq(2))
   end
 end
