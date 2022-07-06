be4b1fab9ea33e247c236e4c13cb01ed5fa5f821
Jake Kim <imjakekim@gmail.com>
2022-07-05 21:51:47 -0700

    Finish v1 deck_spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,7 +3,35 @@ require './lib/deck'
 RSpec.describe Deck do
   it 'exists' do
     card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    deck = Deck.new
+    card2 = Card.new("1 + 1", "2", :Math)
+    card3 = Card.new("1 * 1", "1", :Math)
+    deck = Deck.new([card1,card2,card3])
+    expect(deck).to be_instance_of(Deck)
   end
+
+  it 'has cards' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("1 + 1", "2", :Math)
+    card3 = Card.new("1 * 1", "1", :Math)
+    deck = Deck.new([card1,card2,card3])
+    expect(deck.cards).to eq([card1,card2,card3])
+  end
+
+  it 'counts the cards' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("1 + 1", "2", :Math)
+    card3 = Card.new("1 * 1", "1", :Math)
+    deck = Deck.new([card1,card2,card3])
+    expect(deck.count).to eq(3)
+  end
+
+  it 'tracks categories' do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("1 + 1", "2", :Math)
+    card3 = Card.new("1 * 1", "1", :Math)
+    deck = Deck.new([card1,card2,card3])
+    expect(deck.cards_in_category("Math")).to eq([card2,card2])
+  end
+
+  
 end
