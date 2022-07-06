94c921da366b913e04b69bc84bd3117eea2863a4
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-05 20:50:01 -0400

    add correct? method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,1 +1,18 @@
-require ./lib/card
+require './lib/card'
+
+# card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+class Turn
+
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    self.guess == self.card.answer
+  end
+
+end
