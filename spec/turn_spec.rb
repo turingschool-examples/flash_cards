7b27cec24053cc04314ea1a4929a0206132bf4a1
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-05 21:06:18 -0400

    flag incorrect answers

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -32,11 +32,18 @@ RSpec.describe Turn do
     expect(turn.guess).to eq(card.answer)
   end
 
-  it 'provides feeback' do
+  it 'provides feedback' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.feedback).to eq("Correct!")
+  end
+
+  it 'handles wrong answers' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.feedback).to eq("Incorrect.")    
   end
 
 
