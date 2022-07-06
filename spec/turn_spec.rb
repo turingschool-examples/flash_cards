a8b178711053e20ced282b9330acbfdc6a3b51a8
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-06 14:40:51 -0600

    Create Turn class file and corresponding test file

---
+++ spec/turn_spec.rb
@@ -0,0 +1,18 @@
+require './lib/turn'
+require './lib/turn'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to be_instance_of(Card)
+  end
+end
