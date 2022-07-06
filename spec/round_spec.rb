716613cce8624208d6abbe8636b44ab1b2a3ab13
Thomas Musselman <tmusselman@gmail.com>
2022-07-06 17:13:25 -0600

    Implement before :each on round spec file

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,45 +4,30 @@ require './lib/round'
 
 RSpec.describe Round do
 
-    it 'exists' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new([card_1, card_2, card_3])
-        round = Round.new(deck)
+    before(:each) do
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        @deck = Deck.new([@card_1, @card_2, @card_3])
+        @round = Round.new(@deck)
+    end
 
-        expect(round).to be_instance_of(Round)
+    it 'exists' do
+        expect(@round).to be_instance_of(Round)
     end
 
     it 'contains a deck' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new([card_1, card_2, card_3])
-        round = Round.new(deck)
-
-        expect(round.deck).to be_instance_of(Deck)
+        expect(@round.deck).to be_instance_of(Deck)
     end
 
     it 'contains an array of turns that is empty by default' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new([card_1, card_2, card_3])
-        round = Round.new(deck)
-
-        expect(round.turns).to eq([])
+        expect(@round.turns).to eq([])
     end
 
     it 'contains a current card that defaults to the first in the deck' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new([card_1, card_2, card_3])
-        round = Round.new(deck)
-
-        expect(round.current_card).to eq(card_1)
+        expect(@round.current_card).to eq(@card_1)
     end
+
 
 end
 
