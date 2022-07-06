6aa927e097736c12c92e084ffafc032995c70b57
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-06 15:03:59 -0600

    Add more tests to turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,16 +3,38 @@ require './lib/turn'
 
 RSpec.describe Turn do
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
 
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a card' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
 
     expect(turn.card).to be_instance_of(Card)
   end
+
+  it 'has a guess' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'evaluates whether guess equals answer' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'prints feedback based on guess' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card_1)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+
 end
