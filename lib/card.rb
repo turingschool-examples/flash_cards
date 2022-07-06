0e64910bf00d08f5cdf067832d7aa07fea5daa6f
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-06 14:07:13 -0600

    Create Card class and add question, answer, and category attributes

---
+++ lib/card.rb
@@ -0,0 +1,8 @@
+class Card
+  attr_reader :question, :answer, :category
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+end
