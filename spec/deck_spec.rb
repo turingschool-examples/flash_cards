7db9a38966efbf788210df1c283cf103f4c5fb79
Thomas Musselman <tmusselman@gmail.com>
2022-07-06 08:47:31 -0600

    Fix deck class test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe Deck do
         cards = [card_1, card_2, card_3]
         deck = Deck.new(cards)
 
-        expect(deck.cards_in_cateogry(:STEM)).to eq [card_2, card_3]
+        expect(deck.cards_in_category(:STEM)).to eq [card_2, card_3]
         expect(deck.cards_in_category(:Geography)).to eq [card_1]
         expect(deck.cards_in_category("Pop Culture")).to eq []
     end
