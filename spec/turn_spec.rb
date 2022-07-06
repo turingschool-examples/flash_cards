2ff854fd87759bc063c4c956f1a6b20c7f85dcdf
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-05 20:50:48 -0400

    add test for existence and guess

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,14 +1,20 @@
 require './lib/card'
+require './lib/turn'
 
-class Turn :: Card
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
-  attr_reader :string, :card
-
-  def initialize(string, card)
-    @string = string
-    @card = card
+    expect(turn).to be_instance_of(Turn)
   end
 
-  
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
 
 end
