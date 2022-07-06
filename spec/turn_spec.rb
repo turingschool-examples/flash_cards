29f89ee303151b6fb96bf4bfacdb9941d3a81bd8
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-06 08:56:08 -0600

    add tests to Turn Class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,6 +2,8 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
+  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
   it 'exists' do
     turn = Turn.new("Juneau", card)
 
@@ -11,7 +13,13 @@ RSpec.describe Turn do
   it 'shows current card' do
     turn = Turn.new("Juneau", card)
 
-    expect(turn.card).to eq("What is the capital of Alaska?", "Juneau", :Geography)
+    expect(turn.card).to eq(card)
   end
 
+  it 'gives feedback' do
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq('Correct!')
   end
+
+end
