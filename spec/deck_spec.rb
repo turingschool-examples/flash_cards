dead1d90aa551f5810cd0f752e7f4c746788803c
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-05 21:46:01 -0400

    update category test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -38,5 +38,6 @@ RSpec.describe Deck do
 
     expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
     expect(deck.cards_in_category(:Geography)).to eq([card_1])
+    expect(deck.cards_in_category("Pop Culture")).to eq([])
   end
 end
