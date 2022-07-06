fcbdfe6725298a49f54413ea088084e3200c747e
Andrew Mullins <mullinsand15@gmail.com>
2022-07-05 20:53:06 -0600

    Add current_card and take_turn methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,20 @@
 class Round
-    attr_reader :deck
+    attr_reader :deck, :turns, :number_of_rounds, :number_correct
     def initialize(deck)
         @deck = deck
+        @turns = []
+        @number_of_rounds = 0
+        @number_correct = 0
     end
+
+    def current_card
+        deck.cards.at(number_of_rounds)
+    end
+
+    def take_turn(guess)
+        current_turn = Turn.new(guess, current_card)
+        turns << current_turn
+        
+    end
+
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -14,4 +14,26 @@ require './lib/round'
             expect(round).to be_instance_of(Round)
         end
 
+        it 'has an empty array calls turns by default' do
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+            round = Round.new(deck)
+            
+            expect(round.turns).to eq([])
+        end
+
+        it 'keeps track of current card' do
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+            round = Round.new(deck)
+            require 'pry'; binding.pry
+            round.take_turn("Juneau")
+
+        end
     end
