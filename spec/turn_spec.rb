5ab40dfe10b6da78a62e6317dc84aa1bf98837b8
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-05 20:52:58 -0400

    add test for card contents

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -16,5 +16,15 @@ RSpec.describe Turn do
     expect(turn.guess).to eq("Juneau")
   end
 
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card.question).to eq("What is the capital of Alaska?")
+    expect(turn.card.answer).to eq("Juneau")
+    expect(turn.card.category).to eq(:Geography)
+
+  end
+
 
 end
