e4a643c0b125c57ef6f1cabc7a3fff87e5d96522
Jake Kim <imjakekim@gmail.com>
2022-07-05 20:23:55 -0700

    Finish iteration 1

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+#require 'card'
+
+class Turn
+  attr_reader :guess, :card, :is_correct
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+    @is_correct = guess == @card.answer
+  end
+
+  def guess()
+    @guess
+  end
+
+  def card()
+    @card
+  end
+
+  def correct?()
+    @is_correct
+  end
+
+  def feedback()
+    if is_correct
+      return "Correct!"
+    else
+      return "Incorrect."
+    end
+  end
+
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,33 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+  it 'exists' do
+    turn = Turn.new("guess", Card.new("What is the capital of Alaska?", "Juneau", :Geography))
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a guess' do
+    turn = Turn.new("guess", Card.new("What is the capital of Alaska?", "Juneau", :Geography))
+    expect(turn.guess).to eq("guess")
+  end
+
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card)
+    expect(turn.card).to eq(card)
+  end
+
+  it 'checks if the guess is correct' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card)
+    expect(turn.correct?).to eq(false)
+  end
+
+  it 'returns proper feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card)
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+
+end
