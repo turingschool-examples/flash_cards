7416921ab39185a6303e142ada7d34d751ea1e61
Andrew Mullins <mullinsand15@gmail.com>
2022-07-05 19:45:15 -0600

    Add cards' category counting method to deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,4 +7,12 @@ class Deck
     def count
         cards.count
     end
+
+    def cards_in_category(category)
+        card_categories = []
+        cards.each do |card|
+            card_categories << card.category
+        end
+        card_categories.count(category)
+    end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -23,4 +23,15 @@ RSpec.describe Deck do
         expect(deck.count).to be 3
     end
     
+    it 'counts cards in each category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        expect(deck.cards_in_category(:STEM)).to be 2
+        expect(deck.cards_in_category(:Geography)).to be 1
+        expect(deck.cards_in_category("Pop Culture")).to be 0
+    end    
 end
