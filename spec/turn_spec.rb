cdd8910ca350df7a6fdf87c192a76be523e681df
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-05 20:54:57 -0400

    add test for guessing an answer

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -23,7 +23,13 @@ RSpec.describe Turn do
     expect(turn.card.question).to eq("What is the capital of Alaska?")
     expect(turn.card.answer).to eq("Juneau")
     expect(turn.card.category).to eq(:Geography)
+  end
 
+  it 'guesses an answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq(card.answer)
   end
 
 
