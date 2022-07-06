45f03df672c8b45e0b05f4b2533a9286b4dcc81d
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-06 17:24:38 -0600

    Add 'correct?' method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,17 @@
+require './lib/card'
+
 class Turn
   attr_reader :guess, :card
 
   def initialize(guess, card)
     @guess = guess
     @card = card
+
   end
 
-  
+# Checks whether the guess matches the answer
+  def correct?
+     @guess == @card.answer
+  end
 
 end
