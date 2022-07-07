eb2eeada38dc0e05f5415bdcc2dbf054872734fb
Thomas Musselman <tmusselman@gmail.com>
2022-07-07 10:07:50 -0600

    Remove commented old code

--- lib/runner.rb
+++ lib/runner.rb
@@ -9,28 +9,4 @@ round_1 = Round.new(deck_1)
 
 round_1.start
 
-# def start(round)
-
-#     puts "Welcome! You're playing with #{round.deck.count} cards."
-#     puts "-------------------------------------------------"
-
-#     while round.current_card_index < round.deck.count
-#         puts "This is card number #{round.current_card_index + 1} out of #{round.deck.count}"
-#         puts "Questions: #{round.current_card.question}"
-#         print "Answer: "
-#         guess = gets.chomp.downcase
-#         puts round.take_turn(guess).feedback
-#         puts ""
-#     end
-
-#     puts "****** Game over! ******"
-#     puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct}%."
-#     puts "Math - #{round.percent_correct_by_category("Math")}% correct."
-#     puts "Geography - #{round.percent_correct_by_category("Geography")}% correct."
-#     puts "STEM - #{round.percent_correct_by_category("STEM")}% correct."
-
-# end
-
-# start(round_1)
-
 
