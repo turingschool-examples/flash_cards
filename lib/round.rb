f06206db62b66e8b2d3be959bf255790e6240170
Dominic ODonnell <dominicodonnell99@gmail.com>
2022-07-07 11:02:24 -0500

    Introduce new logic to round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -1,14 +1,22 @@
 class Round
-    attr_accessor :deck, :turns, :turn, :current_card
-  
-    def initialize(deck)
-      @deck = deck
-      @turns = []
-      @current_card = @deck.cards[0]
-    end
-    def take_turn(guess)
-        @turn = Turn.new(guess, @current_card)
-    end
+  attr_accessor :deck, :turns, :turn, :current_card, :number_correct
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+    # returns the first element of the deck.cards array and removes it from the array
+    @current_card = @deck.cards.shift
+    @number_correct = 0
   end
-  
-  
+
+  def take_turn(guess)
+    @turn = Turn.new(guess, @current_card)
+    @turns << @turn
+
+    @number_correct += 1 if @turn.correct? == true
+
+    @current_card = @deck.cards.shift
+
+    @turn
+  end
+end
