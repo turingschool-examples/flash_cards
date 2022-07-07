30c8da6770e7b6f8d87a913fd96729c97d0bf4d0
Dominic ODonnell <dominicodonnell99@gmail.com>
2022-07-07 11:01:34 -0500

    Update previous mistakes in deck_spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe Deck do
   # Uses before method to repeat code below for each it method
   before(:each) do
     card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    card_2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-    card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?','North north west', :STEM)
+    card_2 = Card.new(
+      'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM
+    )
+    card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?',
+                      'North north west', :STEM)
     @cards = [card_1, card_2, card_3]
     @deck = Deck.new(@cards)
   end
@@ -17,7 +20,7 @@ RSpec.describe Deck do
     expect(@deck.cards).to eq(@cards)
   end
   it 'displays count of cards in deck' do
-    expect(@deck.count).to eq (3)
+    expect(@deck.count).to eq(3)
   end
   it 'shows cards in correct category' do
     @deck.cards_in_category(:STEM)
@@ -28,7 +31,7 @@ RSpec.describe Deck do
 
     expect(@deck.categorized_cards).to eq([@deck.cards[0]])
 
-    @deck.cards_in_category("Pop Culture")
+    @deck.cards_in_category('Pop Culture')
 
     expect(@deck.categorized_cards).to eq([])
   end
