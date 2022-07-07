9c6048666396a75fe94305c352ea86f7c8ad23a9
Kevin Ta <kvnta@yahoo.com>
2022-07-06 21:26:47 -0600

    Add cards to runner file

---
+++ lib/runner.rb
@@ -0,0 +1,9 @@
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
+require './lib/round'
+
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+card_4 = Card.new("Who lives under a pineapple under the sea?", "Spongebob", "Pop-Culture")
