f03f9073a93e96a6bab6185f54c98cea9fe37dbf
Stephen Fabian <fabianstephen@gmail.com>
2022-07-06 18:07:58 -0600

    Add Card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,29 +1,10 @@
 class Card
-    initialize(question, answer, category)
+    
+attr_reader :question, :answer, :category
+
+   def initialize(question, answer, category)
     @question = question
     @answer = answer
     @category = category
     end 
-    end
-    
-    class Turn
-    initialize(string, card)
-    
-    def guess
-        puts string
-    end
-    
-    def card
-        puts card
-    end
-    
-    def correct?
-        string == answer
-    end
-    
-    def feedback
-        if answer == true
-            puts "Correct!"
-        else 
-            puts "Incorrect."
-    puts 
+end 
