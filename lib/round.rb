01505db07557e739f99de45df69bc7bb740a8d32
Alex M <Alexm717@hotmail.com>
2022-07-06 17:14:36 -0700

    add working new_turn

--- lib/round.rb
+++ lib/round.rb
@@ -1,17 +1,28 @@
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :correct
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @right_answer = []
   end
 
   def current_card
-    deck.cards.first
+    @deck.cards[turns.count]
   end
 
   def take_turn(guess)
-    turn = Turn.new(guess, current_card)
-    @turns << turn
+    @new_turn = Turn.new(guess, current_card)
+    @turns << @new_turn
+
+    if @new_turn.correct?
+      @right_answer << current_card
+    end
+
+    @new_turn
+  end
+
+  def number_correct
+    @right_answer.count
   end
 end
