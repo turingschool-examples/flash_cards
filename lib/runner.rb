dba35a2186be3308bb6fbbbd4963ed8e66bc8ad7
Thomas Musselman <tmusselman@gmail.com>
2022-07-06 21:54:43 -0600

    Implemented the card generator into the runner file

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,16 +1,9 @@
 require './lib/round'
+require './lib/card_generator'
 
-card_1 = Card.new("What is 5 + 5?", "10", :Math)
-card_2 = Card.new("What is the capital of Sri Lanka", "Sri Jayawardenapura Kotte", :Geography)
-card_3 = Card.new("What mountain range contains Mt Everest?", "Himalayas", :Geography)
-card_4 = Card.new("What is 5!", "120", :Math)
-card_5 = Card.new("What is the largest Planet?", "Jupiter", :STEM)
-card_6 = Card.new("What is the chemical symbol for Potassium?", "K", :STEM)
-card_7 = Card.new("What is the largest Freshwater lake in the world?", "Lake Superior", :Geography)
-
-cards = [card_1, card_2, card_3, card_4, card_5, card_6, card_7]
-
-deck_1 = Deck.new(cards)
+file_path = './lib/cards.txt'
+card_generator = CardGenerator.new(file_path)
+deck_1 = Deck.new(card_generator.cards)
 
 round_1 = Round.new(deck_1)
 
@@ -30,9 +23,9 @@ def start(round)
 
     puts "****** Game over! ******"
     puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct}%."
-    puts "Math - #{round.percent_correct_by_category(:Math)}% correct."
-    puts "Geography - #{round.percent_correct_by_category(:Geography)}% correct."
-    puts "STEM - #{round.percent_correct_by_category(:STEM)}% correct."
+    puts "Math - #{round.percent_correct_by_category("Math")}% correct."
+    puts "Geography - #{round.percent_correct_by_category("Geography")}% correct."
+    puts "STEM - #{round.percent_correct_by_category("STEM")}% correct."
 
 end
 
