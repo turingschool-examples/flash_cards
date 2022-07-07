cfe8aa9a131cca08b11681a36d9fadc745f610ea
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-06 19:25:43 -0600

    Add methods correct? feedback

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,16 @@
 class Turn
-    attr_reader :card
+    attr_reader :card, :guess
 
     def initialize(guess, card)
         @card = card
+        @guess = guess
+    end
+
+    def correct?
+      return true if @guess = @card.answer
+    end
+
+    def feedback
+        return "Correct!" if correct? == true
     end
 end
--- spec/turns_spec.rb
+++ spec/turns_spec.rb
@@ -2,30 +2,35 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
+
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    
     it 'exists' do
-        turn = Turn.new("Juneau", Card)
+        turn = Turn.new("Juneau", card)
         expect(turn).to be_instance_of(Turn)
     end
 
     it 'returns a card' do
-        turn = Turn.new("Juneau", Card)
+        turn = Turn.new("Juneau", card)
 
-        expect(turn.card).to eq(Card)
+        expect(turn.card).to eq(card)
     end
 
-    xit 'returns a guess' do
-        turn = Turn.new(guess)
+    it 'returns a guess' do
+        turn = Turn.new("Juneau", card)
 
         expect(turn.guess).to eq("Juneau")
     end
 
-    xit 'is_guess_correct?' do
-        turn = Turn.new(correct?)
+    it 'is_guess_correct?' do
+        turn = Turn.new("Juneau", card)
+
         expect(turn.correct?).to eq(true)
     end
 
-    xit 'gives_feedback' do
-        turn = Turn.new(feedback)
+    it 'gives_feedback' do
+        turn = Turn.new("Juneau", card)
+        
         expect(turn.feedback).to eq("Correct!")
     end
 end
