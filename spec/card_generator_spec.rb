7f3413afc573edd93648fd8f5b295af5b8f8e8ff
Thomas Musselman <tmusselman@gmail.com>
2022-07-06 21:30:27 -0600

    Create tests for CardGenerator

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,21 @@
+require './lib/card_generator'
+
+RSpec.describe CardGenerator do
+
+    it 'exists' do
+        file_path = './lib/cards.txt'
+        card_generator = CardGenerator.new(file_path)
+
+        expect(card_generator).to be_instance_of(CardGenerator)
+    end
+
+    it 'generates cards from the text file' do
+        file_path = './lib/cards.txt'
+        card_generator = CardGenerator.new(file_path)
+
+        expect(card_generator.cards.first).to be_instance_of(Card)
+        expect(card_generator.cards.first.question).to eq("What is 5 + 5?")
+        expect(card_generator.cards.last.answer).to eq("Justin bieber")
+    end
+
+end
