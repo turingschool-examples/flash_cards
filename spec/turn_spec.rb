78d2f021daa3880c443fe0405152cf45d86b0eda
Ken Lenhart <omnipaid@gmail.com>
2022-07-07 15:42:03 -0700

    Update turn_spec.rb refactor test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,30 +1,26 @@
 require './lib/turn'
 require './lib/card'
 
-describe Turn do 
+RSpec.describe Turn do 
+  
+  before :each do 
+    @card = Card.new("What is the capital of California?", "Sacramento", :Geography)
+    @turn = Turn.new("Sacramento", @card)
+  end 
+
   it 'exists' do 
-    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    turn = Turn.new("Sacramento", card)
-    expect(turn).to be_instance_of(Turn)
+    expect(@turn).to be_instance_of(Turn)
   end 
   it 'returns a card' do
-    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    turn = Turn.new("Sacramento", card)
-    expect(turn.card).to eq(card)
+    expect(@turn.card).to eq(@card)
   end 
   it 'returns a guess' do 
-    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    turn = Turn.new("Sacramento", card)
-    expect(turn.guess).to eq("Sacramento")
+    expect(@turn.guess).to eq("Sacramento")
   end 
   it 'returns correct?' do
-    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    turn = Turn.new("Sacramento", card)
-    expect(turn.correct?).to eq(true)
+    expect(@turn.correct?).to eq(true)
   end 
   it 'returns feedback' do
-    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
-    turn = Turn.new("Sacramento", card)
-    expect(turn.feedback).to eq("Correct!")
+    expect(@turn.feedback).to eq("Correct!")
   end 
 end   
