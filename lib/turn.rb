c46d364de96358147df0bdb15d28ffcea519d5f4
Ken Lenhart <omnipaid@gmail.com>
2022-07-06 22:10:04 -0700

    Update turn.rb turn_spec.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,18 @@
 class Turn 
-  
+  attr_reader :string, :card
+  def initialize(string, card)
+    @string = string
+    @card = card
+  end 
+  def guess
+    @string 
+  end 
+  def correct?
+    return true if self.card.answer == self.guess
+    false
+  end 
+  def feedback 
+    return "Correct!" if self.correct? == true
+    "Incorrect!"
+  end 
 end 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,5 +1,30 @@
 require './lib/turn'
+require './lib/card'
 
 describe Turn do 
-
+  it 'exists' do 
+    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
+    turn = Turn.new("Sacramento", card)
+    expect(turn).to be_instance_of(Turn)
+  end 
+  it 'returns a card' do
+    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
+    turn = Turn.new("Sacramento", card)
+    expect(turn.card).to eq(card)
+  end 
+  it 'returns a guess' do 
+    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
+    turn = Turn.new("Sacramento", card)
+    expect(turn.guess).to eq("Sacramento")
+  end 
+  it 'returns correct?' do
+    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
+    turn = Turn.new("Sacramento", card)
+    expect(turn.correct?).to eq(true)
+  end 
+  it 'returns feedback' do
+    card = Card.new("What is the capital of California?", "Sacramento", :Geography)
+    turn = Turn.new("Sacramento", card)
+    expect(turn.feedback).to eq("Correct!")
+  end 
 end   
