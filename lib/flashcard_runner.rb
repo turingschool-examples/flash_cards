90ac89dd83766a438a8bd4dbcc86698048476dcb
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-06 22:20:07 -0400

    add original cards

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -1,49 +1,49 @@
-# require './lib/round'
+require './lib/round'
 
-# # make a number of cards for the game
-# card_1 = Card.new(
-# "Where is Gruner Veltliner most popularly cultivated?",
-# "Austria",
-# :Wine
-# )
+# make a number of cards for the game
+card_1 = Card.new(
+"Where is Gruner Veltliner most popularly cultivated?",
+"Austria",
+:Wine
+)
 
-# card_2 = Card.new(
-# "Would you generally expect a Kabinett Riesling to be Dry, Off-Dry, or Sweet?",
-# "Off-Dry",
-# :Wine
-# )
+card_2 = Card.new(
+"Would you generally expect a Kabinett Riesling to be Dry, Off-Dry, or Sweet?",
+"Off-Dry",
+:Wine
+)
 
-# card_3 = Card.new(
-# "What heavily oxidized wine is a specialty of the Jura?",
-# "Vin Jaune",
-# :Wine
-# )
+card_3 = Card.new(
+"What heavily oxidized wine is a specialty of the Jura?",
+"Vin Jaune",
+:Wine
+)
 
-# card_4 = Card.new(
-# "Which regional cuisine is credited with the invention of Salt Cod, or Bacalao?",
-# "Basque",
-# :Food
-# )
+card_4 = Card.new(
+"Which regional cuisine is credited with the invention of Salt Cod, or Bacalao?",
+"Basque",
+:Food
+)
 
-# card_5 = Card.new (
-# "What is the Latin name for the leafy, edible herb often foraged in California and also known as ‘sour grass?’",
-# "Oxalis",
-# :Food
-# )
-# card_6 = Card.new (
-# "Where are Malpeque Bay oysters harvested?",
-# "Prince Edward Island",
-# :Food
-# )
+card_5 = Card.new (
+"What is the Latin name for the leafy, edible herb often foraged in California and also known as ‘sour grass?’",
+"Oxalis",
+:Food
+)
+card_6 = Card.new (
+"Where are Malpeque Bay oysters harvested?",
+"Prince Edward Island",
+:Food
+)
 
-# card_7 = Card.new (
-# "What popular grape variety is known as Spatburgunder in Germany?",
-# "Pinot Noir",
-# :Wine
-# )
+card_7 = Card.new (
+"What popular grape variety is known as Spatburgunder in Germany?",
+"Pinot Noir",
+:Wine
+)
 
-# card_8 = Card.new (
-# "What region in New York is known for its high-quality Rieslings?",
-# "Finger Lakes",
-# :Wine
-# )
+card_8 = Card.new (
+"What region in New York is known for its high-quality Rieslings?",
+"Finger Lakes",
+:Wine
+)
