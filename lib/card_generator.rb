9ce6766818abe539503fc1c2a58a642d531880b4
Thomas Musselman <tmusselman@gmail.com>
2022-07-06 21:33:05 -0600

    Implement CardGenerator class and add txt file for questions

---
+++ lib/card_generator.rb
@@ -0,0 +1,20 @@
+require './lib/card'
+
+class CardGenerator
+
+    attr_reader :cards
+
+    def initialize(filename)
+        @file_lines = File.readlines(filename).map{ |line| line.chomp }
+        @cards = []
+        self.generate_cards(@file_lines)
+    end
+
+    def generate_cards(lines)
+        lines.each do |line|
+            segments = line.split(/,/)
+            @cards << Card.new(segments[0],segments[1],segments[2])
+        end
+    end
+
+end
