01aaa2cea6b775b7fa70a15ef53284a841b7317f
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-07 08:22:45 -0600

    add working spec for take_turn method in round class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,33 +4,57 @@ require './lib/deck'
 require './lib/round'
 
 RSpec.describe Round do
-    before :each do
+
+    it 'exists' do   
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         deck = Deck.new([card_1, card_2, card_3])
         round = Round.new(deck)
-    end 
-
-    it 'exists' do
-            
+        
         expect(round).to be_instance_of(Round)
     end 
 
     it 'displays cards in deck' do 
-                
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
         expect(round.deck).to eq(deck)
     end 
 
-    it 'has no turns at start' do
+    it 'has no turns at start' do 
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
         
         expect(round.turns).to eq([])
     end 
 
     it 'checks current card' do
-        
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
         expect(round.current_card).to eq(card_1)      
     end 
 
+    it 'takes turn' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        new_turn = Turn.new("Juneau", card_1)
+
+        expect(new_turn).to be_instance_of(Turn)
+        expect(new_turn.correct?).to be true 
+    end 
     
 end 
