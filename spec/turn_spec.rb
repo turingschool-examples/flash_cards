6c63aebd4322fd14afa721aedbe8c6d54d7f2eee
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-08 08:34:51 -0400

    clean up using before(:each)

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,50 +1,37 @@
-require './lib/card'
 require './lib/turn'
 
 RSpec.describe Turn do
-  it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
 
-    expect(turn).to be_instance_of(Turn)
+  before(:each) do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
+  it 'exists' do
+    expect(@turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.guess).to eq("Juneau")
+    expect(@turn.guess).to eq("Juneau")
   end
 
   it 'has a card' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.card.question).to eq("What is the capital of Alaska?")
-    expect(turn.card.answer).to eq("Juneau")
-    expect(turn.card.category).to eq(:Geography)
+    expect(@turn.card.question).to eq("What is the capital of Alaska?")
+    expect(@turn.card.answer).to eq("Juneau")
+    expect(@turn.card.category).to eq(:Geography)
   end
 
   it 'guesses an answer' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.guess).to eq(card.answer)
+    expect(@turn.guess).to eq(@card.answer)
   end
 
   it 'provides feedback' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.feedback).to eq("Correct!")
+    expect(@turn.feedback).to eq("Correct!")
   end
 
   it 'handles wrong answers' do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
-
     expect(turn.feedback).to eq("Incorrect.")    
   end
-
-
 end
