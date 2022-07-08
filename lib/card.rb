e879bc12e092e4774ef4e70b0bbdf00ecad50fc9
Noah van Ekdom <noahvanekdom@gmail.com>
2022-07-08 12:33:46 -0600

    Add card class with 3 attributes

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,10 @@
 # This is a file creating the hard class
 # ITERATION 1
 
+class Card
+    def initialize(question,answer,category)
+        @question = question
+        @answer = answer
+        @category = category
+    end
+end
