19feffe6f99442f76c33852eb2e7d5eeb5ffb777
Dominic ODonnell <dominicodonnell99@gmail.com>
2022-07-08 14:10:30 -0500

    Allow rubocop to make changes

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -14,15 +14,14 @@ class CardGenerator
     # This block of code is taking the file_data, which is an array of lines from cards.txt
     # which then further splits them into seperate arrays, allowing me to grab
     # at certain chunks of text, to hand them to the variables of the class card.
-    file_data.map { |line| result << line.split(",")}
+    file_data.map { |line| result << line.split(',') }
 
-    for num in 0..file_data.count - 1 do
-        question = result[num][0]
-        answer = result[num][1]
-        category = result[num][2]
-        cards << (Card.new(question, answer ,category))
+    (0..file_data.count - 1).each do |num|
+      question = result[num][0]
+      answer = result[num][1]
+      category = result[num][2]
+      cards << (Card.new(question, answer, category))
     end
-    deck = Deck.new(cards)
-    return deck
+    Deck.new(cards)
   end
 end
