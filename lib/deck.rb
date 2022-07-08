a6eb3f14717bf432c44d8b3988e88ebe7bcb7e34
Andrew Mullins <mullinsand15@gmail.com>
2022-07-08 07:59:09 -0600

    Remove old code from deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,7 +2,6 @@ class Deck
     attr_reader :cards, :card_categories
     def initialize(cards)
         @cards = cards
-        # @card_categories = []
     end
 
     def count
@@ -12,14 +11,5 @@ class Deck
     def cards_in_category(category)
         @cards.select { |card| card.category == (category) }
     end
-
-    # def listing_card_categories
-    #     cards_processed = 0
-    #     until cards_processed == count
-    #         @card_categories << cards[cards_processed].category
-    #         cards_processed += 1
-    #     end
-    #     @card_categories.uniq!
-    # end
 
 end
