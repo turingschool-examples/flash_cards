0ce2f91733b50991af8e008c764f16de96725173
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-08 07:58:54 -0600

    Add correct methods for percent correct by category & brush up on string formatting

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -7,9 +7,13 @@ require 'pry'
 
 
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-cards = [card_1, card_2, card_3]
+card_2 = Card.new("In what state is Turing located?", "Colorado", :Geography)
+card_3 = Card.new("The extra file we can make to help us test & run code is called the ________ file.", "runner", :CS)
+card_4 = Card.new("What is the capital of Louisiana?", "Baton Rouge", :Geography)
+card_5 = Card.new("The four types of loops are do while time and _____", "until", :CS)
+card_6 = Card.new("What you have and where you have access to it is called the ________", "scope", :CS)
+card_7 = Card.new("The two directories you need when creating classes are spec & _____", "lib", :CS)
+cards = [card_1, card_2, card_3, card_4,card_5, card_6, card_7]
 deck = Deck.new(cards)
 round = Round.new(deck)
 
@@ -20,3 +24,11 @@ guess2 = gets.chomp
 round.show_card(guess2)
 guess3 = gets.chomp
 round.show_card(guess3)
+guess4 = gets.chomp
+round.show_card(guess4)
+guess5 = gets.chomp
+round.show_card(guess5)
+guess6 = gets.chomp
+round.show_card(guess6)
+guess7 = gets.chomp
+round.show_card(guess7)
--- lib/round.rb
+++ lib/round.rb
@@ -33,16 +33,17 @@ class Round
     ((turns.select { |turn| turn.correct? }.size).to_f / (turns.size)*100).round(1)
   end
 
+
   def percent_correct_by_category(category)
-    ((turns.select { |turn| turn.correct? && turn.card.category == category}.size).to_f /
-    (correct_turns.select { |turn| turn.card.category == category}.size)*100).round(1)
+    ((correct_turns.select { |turn| turn.card.category == category}.size).to_f/
+    (((turns.select { |turn| turn.card.category == category}.size)))*100).round(1)
   end
 
   def start
     puts "Welcome! You're playing with #{deck.cards.size} cards.
--------------------------------------------------
-This is card number 1 out of #{deck.cards.size}.
-puts Question: #{current_card.question}"
+        -------------------------------------------------
+        This is card number 1 out of #{deck.cards.size}.
+        puts Question: #{current_card.question}"
   end
 
   def show_card(user_guess)
@@ -54,11 +55,10 @@ puts Question: #{current_card.question}"
     if turns.size == deck.cards.size
       puts "****** Game over! ******
         You had #{correct_turns.size} correct guesses out of #{deck.cards.size} for a total score of #{percent_correct}%.
-        STEM - #{percent_correct_by_category(:STEM)}% correct
-        Geography - #{percent_correct_by_category(:Geography)}% correct
-        Pop Culture - 0% correct"
+        Mod1 Review Questions - #{percent_correct_by_category(:CS)}% correct
+        Geography - #{percent_correct_by_category(:Geography)}% correct"
     else
-      puts "Question: #{current_card.question}"
+      puts "Question #{current_card.question}"
     end
   end
 
