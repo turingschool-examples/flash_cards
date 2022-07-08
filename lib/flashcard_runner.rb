76e6398edf3c403811a8ea3cb5c6438e275f7fec
Andrew Mullins <mullinsand15@gmail.com>
2022-07-08 12:36:38 -0600

    Integrate CardGenerator into flashcard runner and add more questions to cards file

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,15 +2,17 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 
 
 def start
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    card_4 = Card.new("What is the capital of Arizona?", "Phoenix", :Geography)
-    card_5 = Card.new("How much wood can a wood chuck chuck?", "Not much", :Random)
-    cards = [card_1, card_2, card_3, card_4, card_5]
+    # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    # card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    # card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    # card_4 = Card.new("What is the capital of Arizona?", "Phoenix", :Geography)
+    # card_5 = Card.new("How much wood can a wood chuck chuck?", "Not much", :Random)
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename).cards
     deck = Deck.new(cards)
     round = Round.new(deck)
 
