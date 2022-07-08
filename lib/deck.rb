188d14ebed6ce0b61eb3316670b53829487a3f7d
Erik Riggs <riggs.erik@gmail.com>
2022-07-07 20:42:14 -0600

    Define cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,15 @@
 class Deck
+  attr_reader :cards
+
   def initialize(cards)
+    @cards = cards
+  end
+
+
+
+  def cards_in_category(category)
+    @cards.find_all do |card|
+      card.category == category
+    end
   end
 end
