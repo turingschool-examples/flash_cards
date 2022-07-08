94a2b008ae4faf35499908d7cc3eac6665e7373f
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-08 10:19:39 -0400

    move runner to main folder, migrate game code to round.rb

---
+++ flashcard_runner.rb
@@ -0,0 +1,17 @@
+require './lib/round'
+
+cards = []
+
+File.open('lib/flashcards.txt', 'r') do |file|
+    for line in file.readlines()
+        import_card = line.split("/")
+        card = Card.new(import_card[0], import_card[1], import_card[2].strip.to_sym)
+        cards << card
+    end
+end
+
+cards = cards.sample(cards.count)
+deck = Deck.new(cards)
+round = Round.new(deck)
+
+round.start
