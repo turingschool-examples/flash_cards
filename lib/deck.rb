54e41b9c946febdb66599d2b2693d69f4bc01f0e
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-07 19:24:18 -0600

    Complete deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,17 @@
 class Deck 
     attr_reader :cards
+    
     def initialize(cards) 
         @cards = cards
-        
     end
 
+    def count
+        cards.count
+    end
 
-
-
-
+    def cards_in_category(category)
+        cards.select do |card|
+            card.category == category 
+        end
+    end
 end
