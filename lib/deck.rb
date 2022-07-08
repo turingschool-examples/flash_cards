3d7190d13706040dacb72e27fb5e624d2e592f33
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-07-07 20:43:07 -0600

    Create deck class and count method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,13 @@
 require './lib/card'
 
 class Deck
+  # attr_reader :cards,
 
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @cards.count
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,27 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
+
+describe Deck do
+  it 'is an instance of deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it 'can count cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.count).to eq(3)
+  end
+end
