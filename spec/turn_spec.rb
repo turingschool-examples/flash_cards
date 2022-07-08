8bab40be8ce52d198c50e355692ae1ffb77b2df8
Kyongnam Kang <kangkna@gmail.com>
2022-07-08 14:25:50 -0400

    Add test for card

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -16,16 +16,24 @@ RSpec.describe Turn do
     expect(turn.guess).to eq("Juneau")
   end
 
+  it 'has a card' do
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card.question).to eq("What is the capital of Alaska?")
+    expect(turn.card.answer).to eq("Juneau")
+    expect(turn.card.category).to eq(:Geography)
+  end
+
   it 'has correct?' do
     turn = Turn.new("Juneau", card)
 
-    expect(turn.correct?).to eq true
+    expect(turn.correct?).to be true
   end
 
   it 'has feedback' do
     turn = Turn.new("Juneau", card)
 
     expect(turn.feedback).to eq("Correct!")
-  end   
+  end
 
 end
