3cb6ac31ea4c1c387c996a003e07e8a7716bc0c2
Andrew Mullins <mullinsand15@gmail.com>
2022-07-08 06:24:09 -0600

    Add card categories attribute to deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,11 +6,20 @@ class Deck
     end
 
     def count
-        cards.count
+        @cards.count
     end
 
     def cards_in_category(category)
-        cards.select { |card| card.category == (category) }
+        @cards.select { |card| card.category == (category) }
+    end
+
+    def listing_card_categories
+        cards_processed = 0
+        until cards_processed == count
+            @card_categories << cards[cards_processed].category
+            cards_processed += 1
+        end
+        @card_categories.uniq!
     end
 
 end
