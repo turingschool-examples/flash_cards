f4f8db99a766299ff74b747a3f61e9f5e02f8463
Riley McCullough <rileybmccullough@gmail.com>
2022-07-07 21:58:16 -0600

    Feat: can take turn

--- lib/round.rb
+++ lib/round.rb
@@ -1,12 +1,34 @@
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
+require "pry"
 
 class Round
-  attr_reader
+  attr_reader :deck,
+              :turns,
+              :current_card
 
   def initialize(deck)
+    @deck = deck
+    @turns = []
+    # @turns = turns
+    @correct_turns = []
+  end
 
+  def current_card
+    @deck.cards[turns.count]
+  end
+
+  def take_turn(input)
+    guess = input
+    turn_wasd = Turn.new(guess, current_card)
+    @turns << turn_wasd
+
+    if turn_wasd.correct?
+      @correct_turns << turn_wasd
+    end
+
+    turn_wasd
   end
 
 end
