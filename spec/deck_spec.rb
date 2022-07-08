54ec3b9d5f45b85c110cd41cee90795717bdc770
Thomas Hayes <hayes.thomas610@gmail.com>
2022-07-07 19:55:07 -0600

    fix last two tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -38,14 +38,24 @@ RSpec.describe Deck do
     expect(deck_1.count).to eq 3
   end
   
-  it 'can count cards based on category' do
+  it 'can call cards based on category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
 
     deck_1 = Deck.new(cards)
-    expect(deck_1.cards_in_category(:Geography).count).to eq 1
+    expect(deck_1.cards_in_category(:STEM)).to include(card_2, card_3)
+  end
+
+  it 'can call cards based on category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+
+    deck_1 = Deck.new(cards)
+    expect(deck_1.cards_in_category(:Geography)).to include(card_1)
   end
 
 end 
