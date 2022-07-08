2d9dfb984100449d3abda8153ba3eaeecf28255f
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-08 09:47:59 -0400

    add #collect_cards to read from .txt file

--- lib/round.rb
+++ lib/round.rb
@@ -1,33 +1,33 @@
-require './lib/card'
-require './lib/turn'
 require './lib/deck'
 
 class Round
 
-  attr_reader :deck, :turns
-  attr_accessor :correct_answers, :turn_count, :total_cards, :categories
+  attr_reader :deck, 
+              :turns
+  attr_accessor :correct_answers, 
+                :turn_count, 
+                :total_cards, 
+                :categories
 
-  def initialize(deck, turns = [], correct_answers = 0, turn_count = 1, total_cards = 0, categories = [])
+  def initialize(deck)
     @deck = deck
-    @turns = turns
-    @correct_answers = correct_answers
-    @turn_count = turn_count
-    @total_cards = total_cards
-    @categories = categories
+    @turns = []
+    @correct_answers = 0
+    @turn_count = 1
+    @total_cards = 0
+    @categories = []
   end
 
   def current_card
     deck.cards.first
   end
 
-  def take_turn(answer, card = self.current_card)
+  def take_turn(answer, card = current_card)
     @deck.cards.shift
-    @turn_count += 1 # do I need this? (could just use turns.count where it's needed)
+    @turn_count += 1
     new_turn = Turn.new(answer, card)
     @turns << new_turn
-    if @turns.last.correct? == true
-      @correct_answers += 1
-    end
+    @correct_answers += 1 if @turns.last.correct?
     new_turn
   end
 
@@ -39,46 +39,63 @@ class Round
   def percent_correct
     (@correct_answers.to_f / turns.count) * 100
   end
-  
-  
+
+
   # the number correct by category method needs to take a category as an argument. it should make an array of cards that include that category. it should then test that array for correct answers (answer == guess) and return the total number of correct answers.
   def number_correct_by_category(cat)
     num_by_cat = 0
     turns.each do |turn|
-      if turn.correct? && turn.card.category == cat
-        num_by_cat =+ 1
-      end
+      num_by_cat =+ 1 if turn.correct? && turn.card.category == cat
     end
     num_by_cat
   end
-  
+
   # The percent correct by category method will be passed a card category as an argument. It will then divide the value of the number_correct_by_category method converted to float by the total occurences of that category in the deck
+
+  def percent_correct_by_category(cat)
+    total_by_cat = 0
+    @turns.each do |turn|
+      total_by_cat += 1 if turn.card.category == cat
+    end
+    (number_correct_by_category(cat).to_f / total_by_cat) * 100
+  end
+
+  # method to collect cards from text file
+  def collect_cards
+    @cards = []
+    File.open('flashcards.txt', 'r') do |file|
+    for line in file.readlines()
+        import_card = line.split("/")
+        card = Card.new(import_card[0], import_card[1], import_card[2].strip.to_sym)
+        @cards << card
+    end
+    @cards
+  end
+
+  # shuffles deck => every game is played in diff order
+  # def shuffle
+  #   @cards = @cards.sample(@cards.count)
+  # end
+
+
+  # Add a method to start the game
+  def start
+    collect_cards
+    shuffle
+    @deck = Deck.new(@cards)
+    @total_cards = deck.cards.count
+    collect_category
+    puts "\n\n"
+    puts "Welcome! You're playing with #{total_cards} cards."
+    puts "-"*30
+  end
+
   
-    def percent_correct_by_category(cat)
-      total_by_cat = 0
-      @turns.each do |turn|
-        if turn.card.category == cat
-          total_by_cat += 1
-        end
-      end
-      (number_correct_by_category(cat).to_f / total_by_cat) * 100
-    end
 
-    # Add a method to start the game
-    def start
-      @total_cards = deck.cards.count
-      collect_category
-      puts "\n\n"
-      puts "Welcome! You're playing with #{@total_cards} cards."
-      puts "-"*30
-    end
-
-    # Add a collect category method to handle end game feedback
-    def collect_category
-      deck.cards.each do |card|
-        if categories.include?(card.category) == false
-          @categories << card.category
-        end
-      end
+  # Add a collect category method to handle end game feedback
+  def collect_category
+    deck.cards.each do |card|
+      @categories << card.category if categories.include?(card.category) == false
     end
   end
+end
