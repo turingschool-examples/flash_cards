6867da121295149e6e7585e9d9c87ba5302d9cd3
Erik Riggs <riggs.erik@gmail.com>
2022-07-07 18:15:10 -0600

    Creating a Deck class

---
+++ lib/turn.rb
@@ -0,0 +1,26 @@
+require 'card'
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    if @guess == @card.answer
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if correct? == true
+      return "Correct!"
+    else
+      return "Incorrect."
+    end
+  end
+#
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,42 @@
+require './lib/turn'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+  expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'is correct' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    turn_2 = Turn.new("", card)
+  expect(turn.correct?).to eq(true)
+  expect(turn_2.correct?).to eq(false)
+  end
+
+  it 'has feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Juneau", card)
+    turn_2 = Turn.new("Saturn", card_2)
+  expect(turn.feedback).to eq("Correct!")
+  expect(turn_2.feedback).to eq("Incorrect.")
+  end
+end
