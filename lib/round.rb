a612b41062d9ed9e4ec621f344b6752c0f0c10c0
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-07-08 12:52:53 -0600

    Correct deck attribute

--- lib/round.rb
+++ lib/round.rb
@@ -3,7 +3,15 @@ require './lib/turn'
 require './lib/deck'
 
 class Round
+  attr_reader :deck
+
   def initialize(deck)
     @deck = deck
+    @turns = []
   end
+
+  def turns
+    @turns += 1
+  end
+
 end
