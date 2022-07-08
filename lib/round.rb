c402bc02b29aecb54eac2669604b421a3e68a9dd
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-08 08:52:00 -0600

    Add to round spec

---
+++ lib/round.rb
@@ -0,0 +1,17 @@
+class Round
+    attr_reader :deck, :turns
+
+    def initialize(deck)
+        @deck = deck
+        @turns = []
+    end
+
+    def current_card
+        deck.cards[0] 
+    end
+
+    def take_turn(guess)
+        Turn.new(guess, current_card)
+    end
+
+end
