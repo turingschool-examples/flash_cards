de6918fc20d63bb877a08643c8fe6d819ad37aa9
Kyongnam Kang <kangkna@gmail.com>
2022-07-08 14:58:31 -0400

    Add test for incorrect

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,13 +1,14 @@
-'./lib/turn'
-'./lib/card'
+require './lib/turn'
+require './lib/card'
 
 RSpec.describe Turn do
   it 'exists' do
-     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     turn = Turn.new("Juneau", card)
+    turn = Turn.new("Juneau", card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-     expect(card).to be_instance_of(Card)
-     expect(turn).to be_instance_of(Turn)
+
+    expect(turn).to be_instance_of(Turn)
+    expect(card).to be_instance_of(Card)
   end
 
   it 'has a guess' do
