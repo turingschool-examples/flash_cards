4e5bec813cf2a79f4e0f20d0631b072a75afd87a
Astrid Hecht <silentaviary@gmail.com>
2022-07-08 15:55:03 -0600

    Make card tests more comprehenisve and robust

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,27 +1,29 @@
 require './lib/card.rb'
 
 RSpec.describe Card do
-  it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+before :each do
+  @test_question = "What is the capital of Alaska?"
+  @test_ans = "Juneau"
+  @test_category = :Geography
+  @card = Card.new(@test_question, @test_ans, @test_category)
+end
 
-    expect(card).to be_instance_of(Card)
+  it 'exists' do
+    expect(@card).to be_instance_of(Card)
   end
 
   it 'has a question' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.question).to be_a(String)
+    expect(@card.question).to be_a(String)
+    expect(@card.question).to eq(@test_question)
   end
 
   it 'has an answer' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.answer).to be_a(String)
+    expect(@card.answer).to be_a(String)
+    expect(@card.answer).to eq(@test_ans)
   end
 
   it 'has a category' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.category).to be_a(Symbol)
+    expect(@card.category).to be_a(Symbol)
+    expect(@card.category).to eq(@test_category)
   end
 end
