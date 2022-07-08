7bc905047b7738f51ea6636e4c0cbf9cb47bce2e
Andrew Mullins <mullinsand15@gmail.com>
2022-07-08 08:33:59 -0600

    Add one more flashcard and complete iteration 3

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -9,7 +9,8 @@ def start
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     card_4 = Card.new("What is the capital of Arizona?", "Phoenix", :Geography)
-    cards = [card_1, card_2, card_3, card_4]
+    card_5 = Card.new("How much wood can a wood chuck chuck?", "Not much", :Random)
+    cards = [card_1, card_2, card_3, card_4, card_5]
     deck = Deck.new(cards)
     round = Round.new(deck)
 
