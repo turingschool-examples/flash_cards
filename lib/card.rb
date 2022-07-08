ebd9005b5c7525fdd9f78d7c661cbbaed1a6b6db
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-08 08:55:43 -0600

    Oops, card.rb might not have been committed

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,9 @@
 class Card
     attr_reader :question, :answer, :category
     def initialize(question, answer, category)
-        @question = "What is the capital of Alaska?"
-        @answer = "Juneau"
-        @category = :Geography 
+        @question = question
+        @answer = answer
+        @category = category
     end
 end
 
