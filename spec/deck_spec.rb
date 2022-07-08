ee0dd69784dca728ac98ed6641bc36a163e6118c
Carter Ball <carter.k.ball@gmail.com>
2022-07-08 00:37:10 -0400

    Create Deck class and test file, create initial test for Deck class to confirm it exists

---
+++ spec/deck_spec.rb
@@ -0,0 +1,16 @@
+require './card'
+require 'rspec'
+require './deck'
+
+RSpec.describe Deck do
+
+  it 'exists' do
+    deck = Deck.new
+
+    expect(deck).to be_instance_of(Deck)
+  end
+
+
+
+
+  end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,6 +3,7 @@ require './card'
 require 'rspec'
 
 RSpec.describe Turn do
+
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
