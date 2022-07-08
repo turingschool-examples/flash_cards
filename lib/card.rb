58c7e2491af2cd83a2197484d0fedb80ab7579e1
Noah van Ekdom <noahvanekdom@gmail.com>
2022-07-08 13:40:09 -0600

    Add first turn test

--- lib/card.rb
+++ lib/card.rb
@@ -2,6 +2,10 @@
 # ITERATION 1
 
 class Card
+    attr_reader :question
+    attr_reader :answer
+    attr_reader :category
+    
     def initialize(question,answer,category)
         @question = question
         @answer = answer
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -9,3 +9,10 @@ describe Turn do
         expect(turn).to be_instance_of(Turn)
       end
     end
+
+    it 'has a guess' do
+        card = Card.new("What is a Giant Panda's favorite food", "Bamboo", :Animals)
+        turn = Turn.new("Bamboo", card)
+    
+        expect(turn.guess).to eq !nil
+      end
