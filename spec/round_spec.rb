e1f8b6860a9b8acfde36c19dd157a10a90537dcc
Riley McCullough <rileybmccullough@gmail.com>
2022-07-08 13:22:52 -0600

    Test: can return number correct by category and percent correct

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -47,4 +47,28 @@ RSpec.describe Round do
     expect(@round.turns.last.feedback).to eq("Incorrect.")
   end
 
+  it 'can return number correct by category and percent correct' do
+   @round.deck
+   expect(@round.turns).to eq([])
+   @round.current_card
+   new_turn = @round.take_turn("Juneau")
+   expect(@round.number_correct).to eq(1)
+   @round.turns
+
+   @round.current_card
+
+   @round.take_turn("Venus")
+
+   expect(@round.turns.count).to eq(2)
+   expect(@round.turns.last.feedback).to eq("Incorrect.")
+   expect(@round.number_correct).to eq(1)
+   expect(@round.number_correct_by_category(:Geography)).to eq(1)
+   expect(@round.number_correct_by_category(:STEM)).to eq(0)
+   expect(@round.percent_correct).to eq(50.0)
+   expect(@round.percent_correct_by_category(:Geography)).to eq(100.0)
+
+   @round.current_card
+ end
+
+
 end
