8c5cec8365d859781fcce993fbb7b96a593a62b0
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-08 10:40:45 -0400

    refactor file.readlines with each instead of for

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,7 +3,7 @@ require './lib/round'
 cards = []
 
 File.open('lib/flashcards.txt', 'r') do |file|
-  for line in file.readlines
+  file.readlines.each do |line|
     import_card = line.split('/')
     card = Card.new(import_card[0], import_card[1], import_card[2].strip.to_sym)
     cards << card
