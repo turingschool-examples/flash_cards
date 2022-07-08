7fb0837405fd649559682866e70306cd692941c6
Riley McCullough <rileybmccullough@gmail.com>
2022-07-08 14:31:50 -0600

    Feat: can return number correct by category and percent correct

--- lib/round.rb
+++ lib/round.rb
@@ -31,4 +31,27 @@ class Round
     turn_wasd
   end
 
+  def number_correct
+    @correct_turns.count
+  end
+
+  def number_correct_by_category(input)
+    how_many = @correct_turns.select { |turn| turn.card.category ==  input }
+    how_many.count
+  end
+
+  def percent_correct
+
+    numerator_1 = @correct_turns.count.to_f
+    denominator_1 = @turns.count.to_f
+    (numerator_1/denominator_1) * 100
+  end
+
+  def percent_correct_by_category(info)
+    numerator_1 = @correct_turns.select { |turn| turn.card.category ==  info }.count.to_f
+    denominator_1 = @turns.select { |turn| turn.card.category ==  info }.count.to_f
+    (numerator_1/denominator_1) * 100
+
+  end
+
 end
