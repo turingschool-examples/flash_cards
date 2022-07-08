85790f52d513e0fac900dd0b57e9ad29923ea5e7
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-08 11:32:45 -0600

    Refactor my class to delete an uneccessary method & add more functionality to the test file to account for more situations

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,11 @@
 require 'pry' ; require './lib/card'
 
 class Deck
-  attr_accessor :cards_as_array
+  attr_accessor :cards_as_array, :count
 
   def initialize(cards_as_array)
     @cards_as_array = cards_as_array
-  end
-
-  def cards
-    @cards_as_array
-  end
-
-  def count
-    @cards_as_array.count
+    @count = cards_as_array.count
   end
 
   def cards_in_category(specified_category)
@@ -20,9 +13,3 @@ class Deck
   end
 
 end
-
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-cards = [card_1, card_2, card_3]
-deck = Deck.new(cards)
--- spec/deck_speck.rb
+++ spec/deck_speck.rb
@@ -7,18 +7,21 @@ RSpec.describe Deck do
   it 'exists' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    cards = [card_1, card_2]
+    card_3 = Card.new("What is the capital of Louisiana?", "Baton Rouge", :Geography)
+    cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
+
     expect(deck).to be_instance_of(Deck)
   end
 
   it 'has an array of cards in the deck' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_1 = Card.new("What is 5 + 5", 10, :Math)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    cards = [card_1, card_2]
+    card_3 = Card.new("What is the capital of Louisiana?", "Baton Rouge", :Geography)
+    cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
 
-    expect(deck.cards_as_array).to eq([card_1, card_2])#is this test running as expected?
+    expect(deck.cards_as_array).to eq([card_1, card_2, card_3])
   end
 
   it 'can count the number of cards in the deck' do
@@ -34,7 +37,8 @@ RSpec.describe Deck do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
+    card_4 = Card.new("What is 5 + 5", 10, :Math)
+    cards = [card_1, card_2, card_3, card_4]
     deck = Deck.new(cards)
 
     expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
