a481a1c19ea676b06edf287494d0313603b5fede
Thomas Hayes <hayes.thomas610@gmail.com>
2022-07-08 11:26:11 -0600

    add current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,13 @@
+require'pry'
+
 class Round
+  attr_reader :deck, :turns
   def initialize(deck)
      @deck = deck
+     @turns = []
+  end
+
+  def current_card 
+    deck.cards[0]
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -15,4 +15,24 @@ RSpec.describe Round do
     round_1 = Round.new(deck)
     expect(round_1).to be_instance_of(Round)
   end
+
+  it 'has turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round_1 = Round.new(deck)
+    expect(round_1.turns).to eq([])
+  end
+
+  it 'can call current card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    round_1 = Round.new(deck)
+    expect(round_1.current_card).to eq(card_1)
+  end
 end 
