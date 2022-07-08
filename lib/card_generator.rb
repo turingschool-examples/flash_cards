21a067f1eb7aeae51dbf9e14e50e15b77b8a2f18
Dominic ODonnell <dominicodonnell99@gmail.com>
2022-07-08 14:12:58 -0500

    Adds more comments to explain the code written

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -16,12 +16,16 @@ class CardGenerator
     # at certain chunks of text, to hand them to the variables of the class card.
     file_data.map { |line| result << line.split(',') }
 
+    # This code block is moving through each line in file_data for the amount of lines inside of
+    # file_data. Then it is indexing into the file, taking those values and putting them into
+    # my new variables
     (0..file_data.count - 1).each do |num|
       question = result[num][0]
       answer = result[num][1]
       category = result[num][2]
       cards << (Card.new(question, answer, category))
     end
+    # Automatically creates a new deck
     Deck.new(cards)
   end
 end
