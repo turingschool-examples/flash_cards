e8a4727d2e9882efc9a26cf2c12820f68fce16c6
Gavin Carew <gjcarew@gmail.com>
2022-07-09 13:23:50 -0700

    Modify flashcard runner to work from a text file

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -4,28 +4,9 @@ require './lib/turn'
 require './lib/round'
 require './lib/card_generator'
 
-card_1 = Card.new(
-  "What is the fastest living creature",
-  "Mantis shrimp",
-  :Zoology)
+cards = CardGenerator.new('./lib/cards.txt').cards
 
-card_2 = Card.new(
-  "What species is the oldest known tree",
-  "Bristlecone pine",
-  :Botany)
-
-card_3 = Card.new(
-  "What is an organism that consists of a symbiotic relationship between a fungus and alga",
-  "Lichen",
-  :Mycology)
-
-card_4 = Card.new(
-  "What is the largest living organism?",
-  "Aspen",
-  :Botany)
-
-
-deck = Deck.new([card_1, card_2, card_3, card_4])
+deck = Deck.new(cards)
 
 round = Round.new(deck)
 
