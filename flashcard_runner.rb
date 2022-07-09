4fce704fa15c2b2b2be9641a507aaa13a6e63b45
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-09 17:20:08 -0600

    Adds start method to CLI

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,4 +1,4 @@
-require '.lib/round'
+require './lib/round'
 
 card_1 = Card.new("What is the capital of Colorado?", "Denver", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -9,3 +9,14 @@ card_5 = Card.new("Describe in words the exact direction that is 697.5° clockwi
 deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
 
 round = Round.new(deck)
+
+puts "Enter 'Start' - To Begin Flashcards"
+puts "Enter 'End' - To End Flashcards\n\n"
+
+print "> "
+start = $stdin.gets.chomp
+
+if start.capitalize == "Start"
+  puts " "
+  puts "Welcome! You're playing with #{deck.count} cards."
+  puts "-" * 60
