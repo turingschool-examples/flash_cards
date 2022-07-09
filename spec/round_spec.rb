77f5effbfb03cf1a7167b4917dbba0e29dc57c06
Erik Riggs <riggs.erik@gmail.com>
2022-07-09 16:36:25 -0600

    Test current_card method

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -133,6 +133,22 @@ RSpec.describe Round do
     new_turn2 = round.take_turn("Denver")
 
     expect(round.percent_correct_by_category(:Geography)).to eq(100)
-    expect(round.percent_correct_by_category(:STEM)).to eq(50)  
+    expect(round.percent_correct_by_category(:STEM)).to eq(50)
+  end
+
+  it 'can change turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+    new_turn1 = round.take_turn("Mars")
+
+
+    expect(round.current_card).to eq(card_3)
   end
 end
