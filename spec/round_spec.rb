3c221777f1e60e798bafed5061925583cae6352a
Thomas Hayes <hayes.thomas610@gmail.com>
2022-07-09 10:57:25 -0600

    confirm take_turn method and count method

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -91,7 +91,21 @@ RSpec.describe Round do
     round = Round.new(deck)
     turn = round.take_turn("Juneau")
     round.turns
+    round.take_turn("Venus")
      
-    expect(round.current_card).to eq(card_2)
+    expect(round.take_turn("Venus")).to be_instance_of(Turn)
+  end 
 
+  it 'is counting rounds correctly' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+    round.turns
+    round.take_turn("Venus")
+     
+    expect(round.turns.count).to eq(2)
+  end 
 end 
