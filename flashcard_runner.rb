948fbbaf516015acf59f9398dbe9af832983637c
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-08 23:36:45 -0600

    Change question-answer on existing card

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,7 +7,7 @@ require './lib/round'
 @card_2 = Card.new("What is the common name of Canis lupus familiaris?", "Dog", :Biology)
 @card_3 = Card.new("What element has the atomic number of 1?", "Hydrogen", :Chemistry)
 @card_4 = Card.new("What is the only metal that is liquid at room temperature?", "Mercury", :Chemistry)
-@card_5 = Card.new("What star is closest to the Sun?", "Proxima Centauri", :Astronomy)
+@card_5 = Card.new("Which planet in our solar system has the most moons?", "Saturn", :Astronomy)
 @card_6 = Card.new('Which planet is generally known as the "Morning Star"?', "Venus", :Astronomy)
 @card_7 = Card.new("Neocaridina is the genus name of what kind of animal?", "Shrimp", :Biology)
 
