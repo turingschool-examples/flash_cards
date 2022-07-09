8b6d888dd091fb000b6f819ce3c05f0db86087fd
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-08 23:30:46 -0600

    Add another card to the deck

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -9,8 +9,10 @@ require './lib/round'
 @card_4 = Card.new("What is the only metal that is liquid at room temperature?", "Mercury", :Chemistry)
 @card_5 = Card.new("What star is closest to the Sun?", "Proxima Centauri", :Astronomy)
 @card_6 = Card.new('Which planet is generally known as the "Morning Star"?', "Venus", :Astronomy)
+@card_7 = Card.new("Neocaridina is the genus name of what kind of animal?", "Shrimp", :Biology)
 
-@deck_1 = Deck.new([@card_1, @card_2, @card_3, @card_4, @card_5, @card_6])
+@deck_1 = Deck.new([@card_1, @card_2, @card_3, @card_4, @card_5, @card_6, @card_7
+  ])
 
 @round = Round.new(@deck_1)
 
