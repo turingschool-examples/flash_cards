fa1352383590d615091280f14866ddbc8b614ab7
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-09 16:53:02 -0400

    add methods to turn class

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+require './lib/card'
+#require 'pry';
+
+class Turn
+
+    attr_reader :guess, :card
+
+    def initialize(guess, card)
+        #binding.pry
+        @guess = guess 
+        @card = card  
+    end
+    
+    def correct?
+        @guess == @card.answer
+    end
+
+    def feedback
+        if @guess == @card.answer
+         "Correct!"
+        else
+         "Incorrect!"
+        end
+    end
+end
