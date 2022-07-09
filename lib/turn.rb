28fc450d144763c5916ff64cf6313111135d89fc
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-07-09 14:17:47 -0700

    Update for attr_readers

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,16 +1,10 @@
 class Turn
+  attr_reader :guess,
+              :card
 
   def initialize(guess, card)
     @guess = guess
     @card = card
-  end
-
-  def card
-    @card
-  end
-
-  def guess
-    @guess
   end
 
   def correct?
