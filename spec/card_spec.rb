f5c57300138b9a8f902d30144313999e2a69267b
Ken Lenhart <omnipaid@gmail.com>
2022-07-09 01:12:48 -0700

    Update card_spec.rb clean up spaces

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,19 +5,15 @@ RSpec.describe Card do
   before :each do 
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   end 
-
   it 'exists' do
     expect(@card).to be_instance_of(Card)
   end
-
   it 'has a question' do
     expect(@card.question).to eq("What is the capital of Alaska?")
   end
-
   it 'has an answer' do
     expect(@card.answer).to eq("Juneau")
   end
-
   it 'has a category' do
     expect(@card.category).to eq(:Geography)
   end
