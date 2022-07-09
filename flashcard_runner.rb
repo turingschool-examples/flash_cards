884d7047adcc30d9808b09fec7ae9bc22176572b
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-09 17:13:27 -0600

    Set up flashcard variables

---
+++ flashcard_runner.rb
@@ -0,0 +1,11 @@
+require '.lib/round'
+
+card_1 = Card.new("What is the capital of Colorado?", "Denver", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("What is the capital of Kansas?", "Topeka", :Geography)
+card_4 = Card.new("What is 4 + 4?", 8, :Math)
+card_5 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+
+round = Round.new(deck)
