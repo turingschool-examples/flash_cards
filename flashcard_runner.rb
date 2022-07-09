c73af368ab90c1ddab4b9f0506c2f5806e1e6c5d
Ken Lenhart <omnipaid@gmail.com>
2022-07-09 10:48:07 -0700

    Update flashcard_runner.rb refactor card_categories

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,7 +7,7 @@ def start
   @card_1 = Card.new("Who played Mr. Spock on the 1960's television show Star Trek?", "Leonard Nimoy", :Televison)
   @card_2 = Card.new("Which James Bond film was released in 1962?", "Dr. No", :Film)
   @card_3 = Card.new("Which Romani guitarist is credited with creating the musical style known as Gypsy Jazz?", "Django Reinhardt", :Music)
-  @card_4 = Card.new("Which character did Gillian Anderson play in the 1990's televsion show The X-Files?", "Dana Scully", :Television)
+  @card_4 = Card.new("Which character did Gillian Anderson play on the 1990's televsion show The X-Files?", "Dana Scully", :Television)
   @deck = Deck.new([@card_1, @card_2, @card_3, @card_4])
   @round = Round.new(@deck)
 
@@ -17,11 +17,20 @@ def start
 
   card_categories = []
 
+  extra_categories = []
+
+  @deck.cards.each do |card|
+    unless card_categories.include?(card.category) 
+      card_categories << card.category
+    else 
+      extra_categories << card.category
+    end 
+  end 
+
   @deck.count.times do 
     puts "\n"
     puts "This is card number #{@round.turn_count + 1 } out of #{@deck.count}"
     puts "Question: #{@round.current_card.question}"
-    card_categories << @round.current_card.category
     puts "\n"
     user_guess = gets.chomp
     turn = @round.take_turn(user_guess)   
@@ -37,6 +46,7 @@ def start
   
   card_categories.each do |card|
     puts "#{card} - #{@round.percent_correct_by_category(card)}% correct"
+    puts "\n" 
   end  
 end 
 
