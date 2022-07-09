127160a77d5ac7c0ac58743ac7a41beb0401e1f1
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-09 07:05:49 -0600

    Take away some commented out code

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -8,16 +8,6 @@ require 'pry'
 
 cards = CardGenerator.new("cards.txt").cards
 
-
-
-# card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# card_2 = Card.new("In what state is Turing located?", "Colorado", :Geography)
-# card_3 = Card.new("The extra file we can make to help us test & run code is called the ________ file.", "runner", :CS)
-# card_4 = Card.new("What is the capital of Louisiana?", "Baton Rouge", :Geography)
-# card_5 = Card.new("The four types of loops are do while time and _____", "until", :CS)
-# card_6 = Card.new("What you have and where you have access to it is called the ________", "scope", :CS)
-# card_7 = Card.new("The two directories you need when creating classes are spec & _____", "lib", :CS)
-# cards = [card_1, card_2, card_3, card_4,card_5, card_6, card_7]
 deck = Deck.new(cards)
 round = Round.new(deck)
 
