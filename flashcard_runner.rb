b1dc00386669e55f6879eb008192014190007e44
Alex M <Alexm717@hotmail.com>
2022-07-09 10:21:58 -0700

    create runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,9 +3,63 @@ require "./lib/turn"
 require "./lib/deck"
 require "./lib/round"
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-cards = [card_1, card_2, card_3]
-deck = Deck.new(cards)
-round = Round.new(deck)
+
+def start
+  card_1 = Card.new("What is 5 + 5?", 10, :STEM)
+  card_2 = Card.new("What is Rachel's favorite animal?", "panda", :PopCulture)
+  card_3 = Card.new("What is Mike's middle name?", "nobody knows", :PopCulture)
+  card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :TuringStaff)
+  cards = [card_1, card_2, card_3, card_4]
+  deck = Deck.new(cards)
+  round = Round.new(deck)
+  puts("Welcome! You're playing with #{deck.cards.count} cards.")
+  puts("-" * 40)
+  puts("This is card number 1 out of 4.")
+  puts("Question: #{card_1.question}")
+  num_c = 0
+  answer = gets.chomp.to_i
+
+  if card_1.answer == answer
+    puts("Correct!")
+    num_c += 1
+  else
+    puts("no")
+  end
+
+  puts("Question: #{card_2.question}")
+  answer = gets.chomp.downcase
+  p(answer)
+
+  if card_2.answer == answer
+    puts("Correct!")
+    num_c += 1
+  else
+    puts("Incorrect.")
+  end
+
+  puts("Question: #{card_3.question}")
+  answer = gets.chomp.downcase
+
+  if card_3.answer == answer
+    puts("Correct!")
+    num_c += 1
+  else
+    puts("Incorrect.")
+  end
+
+  puts("Question: #{card_4.question}")
+  answer = gets.chomp.split(/ |\_/).map(&:capitalize).join(" ")
+  p(answer)
+
+  if card_4.answer == answer
+    puts("Correct!")
+    num_c += 1
+  else
+    puts("Incorrect.")
+  end
+
+  puts("Game over!".center(40, "*"))
+  puts("You had #{num_c} correct guesses out of #{deck.cards.count} for a total score of #{((num_c.to_f / deck.cards.count) * 100).truncate}%.")
+end
+
+start
