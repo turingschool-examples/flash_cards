18c77a96105ec6a0bf9b81ab4e51cdf34945bab5
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-08 20:15:22 -0600

    Add number_correct method

--- lib/round.rb
+++ lib/round.rb
@@ -3,13 +3,24 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
-    @current_card = @deck.cards[@turns.length]
+    @current_card = @deck.cards[0]
   end
 
   def take_turn(guess)
-    @turns << Turn.new(guess, @current_card)
+    turn_taken = Turn.new(guess, @current_card)
+    @turns << turn_taken
+    @current_card = @deck.cards[@turns.length]
+    return turn_taken
+  end
 
-
+  def number_correct
+    number_correct = 0
+    @turns.each do |turn|
+      if turn.correct? == true
+        number_correct += 1
+      end
+    end
+    return number_correct
   end
 
 end
