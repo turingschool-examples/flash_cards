202c4873c1d0663f7c10daa30afcea364d5581e2
Ken Lenhart <omnipaid@gmail.com>
2022-07-08 17:47:11 -0700

    Add flashcard_runner.rb

---
+++ flashcard_runner.rb
@@ -0,0 +1,7 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+@card_1 = Card.new("What is 5 +5?", 10, ":STEM")
+@card_2 = Card.new("What is Rachel's favorite animal?", "panda")
