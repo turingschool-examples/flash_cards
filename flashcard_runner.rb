518e74bed22276ea5f837acfe19b861ddfdd1059
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-07-09 16:12:05 -0600

    Create cards

---
+++ flashcard_runner.rb
@@ -0,0 +1,19 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+
+card_1 = Card.new("What house is Harry Potter sorted into?", "Gryffindor", :HarryPotter)
+card_2 = Card.new("What is Harry's position on the Quidditch team?", "Seeker", :HarryPotter)
+card_3 = Card.new("How many staircases does Hogwarts have?", 142, :HarryPotter)
+card_4 = Card.new("Who is the Half-Blood Prince?", "Snape", :HarryPotter)
+card_5 = Card.new("What is the term used for the perimeter of a circle?", "Circumference", :Math)
+card_6 = Card.new("What society invented the use of negative numbers?", "Chinese", :Math)
+card_7 = Card.new("What is the name of the number system that has a base of two?", "Binary", :Math)
+card_8 = Card.new("Which town was Leonardo da Vinci born in?", "Vinci", :Art)
+card_9 = Card.new("What nationality was Picasso?", "Spanish", :Art)
+card_10 = Card.new("Jerry Garcia is the lead singer for what group?", "Grateful Dead", :Music)
+card_11 = Card.new("What is stored in a camel's hump?", "Fat", :Animals)
+card_12 = Card.new("What color is a giraffe's tounge?", "Blue", :Animals)
+card_13 = Card.new("Which is the world's largest lizard?", "Komodo Dragon", :Animals)
+card_14 = Card.new("In which country is Timbuktu?", "Mali", :Geography)
+card_15 = Card.new("What is the capitol of the state of Mississippi?", "Jackson", :Geography)
