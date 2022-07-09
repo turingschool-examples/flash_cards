5a1a8719d82455488dd2dd2b540426dfc89a4e04
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-07-08 18:42:37 -0700

    Add iteration 2 work thru cards_in_categories

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,22 @@
 class Deck
-  attr_reader :cards, :count
-  def initialize(cards, count)
+  attr_reader :cards
+  def initialize(cards)
     @cards = cards
-    @count = count
+
+  end
+
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(category)
+    card_category = []
+    @cards.each do |card|
+      if card.category == category
+        card_category << card
+      end
+    end
+    card_category
   end
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,47 +1,39 @@
 require './lib/card'
 require './lib/deck'
 
-RSpec.describe 'Deck Spec Harness' do
+RSpec.describe Deck do
   before :each do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     @cards = [@card_1, @card_2, @card_3]
 
   end
 
   describe 'Iteration 2' do
-    it '1. Deck Instantiation' do
-      deck = Deck.new(@cards, 3)
+    it '1. Storing cards in a deck' do
+      deck = Deck.new(@cards)
       expect(deck).to be_an_instance_of(Deck)
     end
 
     it '2. deck_has_cards' do
-      deck = Deck.new(@cards, 3)
+      deck = Deck.new(@cards)
       expect(deck.cards).to eq(@cards)
     end
 
     it '3. deck_count' do
-      deck = Deck.new(@cards, 3)
+      deck = Deck.new(@cards)
 
+      deck.count
       expect(deck.count).to eq(3)
     end
 
+    it '4. cards_in_categories' do
+      deck = Deck.new(@cards)
 
-
-      # expect(@school1).to be_an_instance_of(School)
-      #
-      # expect(@school1).to respond_to(:start_time).with(0).argument
-      # expect(@school1.start_time).to eq('9:00')
-      # expect(@school2.start_time).to eq('10:00')
-      #
-      # expect(@school1).to respond_to(:hours_in_school_day).with(0).argument
-      # expect(@school1.hours_in_school_day).to eq(7)
-      # expect(@school2.hours_in_school_day).to eq(2)
-      #
-      # expect(@school1).to respond_to(:student_names).with(0).argument
-      # expect(@school1.student_names).to eq([])
-      # expect(@school2.student_names).to eq([])
-
+      expect(deck.cards_in_category(:STEM)).to eq([@card_2, @card_3])#defined in an array
+      expect(deck.cards_in_category(:Geography)).to eq([@card_1])
+      expect(deck.cards_in_category("Pop Culture")).to eq([])
+    end
   end
 end
