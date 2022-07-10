005dc4a3cb1035189ea2321c337642182f8be9b0
Sid Mann <sjmann2@outlook.com>
2022-07-10 13:44:20 -0500

    Update tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,6 +4,16 @@ require './lib/deck'
 require './lib/round'
 
 describe Round do 
+    it 'exists' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        
+        expect(round).to be_an_instance_of(Round)
+    end
+
     it 'holds a deck and stores turns' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -15,7 +25,7 @@ describe Round do
         expect(round.turns).to eq([]) 
     end
 
-    it 'keeps track of current card' do 
+    it 'tracks current card' do 
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -28,7 +38,7 @@ describe Round do
         expect(round.current_card).to eq(cards[1])
     end
 
-    it 'keeps track of turns' do
+    it 'tracks current turn' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -39,11 +49,10 @@ describe Round do
         new_turn = round.take_turn("Juneau")
 
         expect(new_turn.class).to be(Turn)
-        expect(new_turn.correct?).to be(true)
         expect(round.turns).to eq([new_turn])
     end
 
-    it 'keeps track of correct and incorrect responses' do
+    it 'tracks responses and moves through cards' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -53,14 +62,29 @@ describe Round do
         turn = Turn.new("Juneau", card_1)
 
         new_turn = round.take_turn("Juneau")
-
-        expect(round.number_correct).to eq(1)
+        
         expect(round.current_card).to eq(card_2)
+        expect(new_turn.correct?).to be(true)
+        expect(round.number_correct).to eq(1)
 
         new_turn = round.take_turn("Venus")
 
         expect(round.turns.count).to eq(2)
         expect(round.turns.last.feedback).to eq("Incorrect.")
+    end
+
+    it 'tracks answers by total correct and correct by category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        turn = Turn.new("Juneau", card_1)
+
+        new_turn = round.take_turn("Juneau")
+        new_turn = round.take_turn("Venus")
+
         expect(round.number_correct).to eq(1)
         expect(round.number_correct_by_category(:Geography)).to eq(1)
         expect(round.number_correct_by_category(:STEM)).to eq(0)
