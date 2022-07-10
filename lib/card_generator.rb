cc7901fc333f4bf13f5baa686bf61f64a4ac061b
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-10 11:27:41 -0600

    Card generator implementation

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -9,6 +9,13 @@ class CardGenerator
 
     def initialize(filename)
         @filename = filename
+        @cards = []
+        lines = File.read(filename).strip.lines
+        lines.each do |line|
+            question, answer, category = line.strip.split(",")
+            card = Card.new(question, answer, category)
+            @cards << card
+        end
     end
 
 end
