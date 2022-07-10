325f2d9f860d526e712c1d1fe672aad408d4cf7b
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-10 01:29:00 -0400

    reattmpt card cat lib spec

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,25 +1,48 @@
-#  require './lib/card'
-  require './lib/turn'
+#   require './lib/card'
+#   require './lib/turn'
+#require 'pry'; binding.pry
 
 class Deck
 
-    attr_reader  :cards
+    attr_reader  :cards, :count, :cards_in_category
 
     def initialize(cards)
        @cards = cards
+
     end
 
     def count
-        @cards.count
+        return @cards.count
     end
 
+# ################*************************************NEED HELP HERE
+# look for card category, if it matches, print that card.
     def cards_in_category (category)
-        card_id = []
+        card_cat =[]
         @cards.each do |card|
-            if card.category == category
-                card_id << card 
+            if @card.category == @category
+                card_cat << card
             end
         end
-        card_id
+        card_cat
     end
+
+
+    # def cards_in_category(category)
+    #     if cards.category == (":Geography")
+    #         return [@card_1]
+    #     elsif cards.category == (":STEM")
+    #         return [@card_2, @card_3]
+    #     else return [ ]
+    #     end
+    # end 
+
+
+
+    # #    returned_cards= ()
+    #     @cards.find_all do |card|
+    #         card.category == catagory
+    #         # returned_cards << card 
+    #     end
+        
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,53 +1,45 @@
 require './lib/deck'
-
+require './lib/turn'
+require './lib/card'
+#require 'pry'; binding.pry
 
 
 RSpec.describe Deck do
     
-    # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    # card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    # card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    # cards = [card_1], [card_2], [card_3]
+    before do 
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        @cards = [@card_1], [@card_2], [@card_3]
+    end
+
 
     it 'deck exists' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        cards = [card_1], [card_2], [card_3]
-        deck = Deck.new(cards)
+        deck = Deck.new(@cards)
         deck.cards
+        
         expect(deck).to be_instance_of(Deck)
     end
 
     it 'deck card array' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        cards = [card_1], [card_2], [card_3]
-        deck = Deck.new(cards)
+        deck = Deck.new(@cards)
 
-        expect(deck.cards).to eq[card_1], [card_2], [card_3]
+        expect(deck.cards).to eq(@cards)
+
     end
 
     it 'deck count' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        cards = [card_1], [card_2], [card_3]
         deck = Deck.new(@cards)
-        expect(deck.count).to eq 3
+
+        expect(deck.count).to eq(3)
     end
 
-    it 'deck cards category' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        cards = [card_1], [card_2], [card_3]
+    it 'return cards in same category' do
         deck = Deck.new(@cards)
         
-        expect(deck.cards_in_category(:STEM)).to eq[card_2, card_3]
-        expect(deck.cards_in_category(:Geography)).to eq[card_1]
-        expect(deck.cards_in_category(:"Pop Culture")).to eq[]
+        expect(deck.cards_in_category(&:STEM)).to match_array([@card_2, @card_3])
+        expect(deck.cards_in_category(&:Geography)).to eq([@card_1])
+        expect(deck.cards_in_category(&:"Pop Culture")).to eq[]
     end
 
 end
