302db3bf7c01fbc33fb1937fd12a18834da7684b
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-10 12:56:38 -0400

    update @cards to one array in Deck lib spec

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,42 +7,17 @@ class Deck
     attr_reader  :cards, :count, :cards_in_category
 
     def initialize(cards)
-       @cards = cards
-
+      @cards = cards
     end
 
     def count
-        return @cards.count
+      return @cards.count
     end
 
-# ################*************************************NEED HELP HERE
-# look for card category, if it matches, print that card.
-    def cards_in_category (category)
-        card_cat =[]
-        @cards.each do |card|
-            if @card.category == @category
-                card_cat << card
-            end
-        end
-        card_cat
-    end
-
-
-    # def cards_in_category(category)
-    #     if cards.category == (":Geography")
-    #         return [@card_1]
-    #     elsif cards.category == (":STEM")
-    #         return [@card_2, @card_3]
-    #     else return [ ]
-    #     end
-    # end 
-
-
-
-    # #    returned_cards= ()
-    #     @cards.find_all do |card|
-    #         card.category == catagory
-    #         # returned_cards << card 
-    #     end
+    def cards_in_category(category)
+      @cards.find_all do |card|
+         card.category == category
+         end
+    end 
         
 end
