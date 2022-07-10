840acb3b7e27852af83968633d12779f3c1c1ab3
Sid Mann <sjmann2@outlook.com>
2022-07-09 19:28:17 -0500

    Fix syntax

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,29 +4,29 @@ require './lib/deck'
 require 'Rspec'
 require 'pry'
 
-RSpec.describe Deck do
-it 'holds cards' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+describe Deck do
+    it 'holds cards' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
 
-    expect(deck.cards).to eq([card_1, card_2, card_3])
+        expect(deck.cards).to eq([card_1, card_2, card_3])
     end
-it 'counts cards by category' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
+    it 'counts cards by category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    expect(deck.count).to eq(3)
-    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
-    expect(deck.cards_in_category(:Geography)).to eq([card_1])
-    expect(deck.cards_in_category("Pop Culture")).to eq([])
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
 
+        expect(deck.count).to eq(3)
+        expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+        expect(deck.cards_in_category(:Geography)).to eq([card_1])
+        expect(deck.cards_in_category("Pop Culture")).to eq([])
     end
 end
