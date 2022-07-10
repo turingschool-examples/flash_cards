a720d7784057c7924040ddd1b0273c35b07899d4
Thomas Hayes <hayes.thomas610@gmail.com>
2022-07-10 11:21:30 -0600

    add .round to percentage(s) methods

--- lib/round.rb
+++ lib/round.rb
@@ -2,6 +2,7 @@ require'pry'
 
 class Round
   attr_reader :deck, :turns, :correct_answers
+
   def initialize(deck)
      @deck = deck
      @turns = []
@@ -14,7 +15,7 @@ class Round
 
   def take_turn(prediction)
     card = current_card
-    turn =  Turn.new(prediction, card)
+       turn =  Turn.new(prediction, card)
     if turn.correct? 
       @correct_answers += 1
     end
@@ -34,7 +35,8 @@ class Round
   end
 
   def percent_correct 
-    (number_correct / turns.count.to_f) * 100
+    pct_crt = (number_correct / turns.count.to_f) * 100
+    pct_crt.round(1) 
   end
 
   def percent_correct_by_category(category)
@@ -42,7 +44,20 @@ class Round
       turn.card.category == category 
     end
     cat_pct = (number_correct_by_category(category) / turns_by_category.count.to_f) * 100
-    cat_pct.round
+    cat_pct.round(1) 
   end
+
+  def start
+    "Welcome! You're playing with 3 cards.
+  ------------------------------------
+  This is card number 1 out of 3."
+  end
+
+  # def game_over
+  #   results = correct_answers
+  #   results_pct = percent_correct
+    
+  #   puts "you had #{results} out of 3 for a total score of #{results_pct}%." 
+  # end
 end
 
