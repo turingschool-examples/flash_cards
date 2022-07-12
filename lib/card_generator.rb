dba92318905e074ccb4ae97b5aa6de08195ed6e7
Ken Lenhart <omnipaid@gmail.com>
2022-07-12 14:49:10 -0700

    Update card_generator.rb test working

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -10,7 +10,7 @@ class CardGenerator
       file_content.map do |line|
         @cards = Card.new(line[0], line[1], line[2])   
       end 
-      @cards
+      @cards 
     end  
   end
 end 
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,24 +1,14 @@
 require './lib/card_generator'
 require './lib/card'
 
-RSpec.describe CardGenerator do 
-
-  before :each do 
-    @filename = File.open("./lib/cards.txt")
-    file_content = []
-    File.readlines(@filename).map do |line|
-      file_content << line.gsub("\n", '').split(',')
-      file_content.map do |line|
-        @cards = Card.new(line[0], line[1], line[2])
-      end 
-       @cards
-    end 
-    @card_generator = CardGenerator.new(@filename)
+RSpec.describe CardGenerator do
+  it 'exists' do
+    filename = ("cards.txt")
+    card_generator = CardGenerator.new(filename)
   end 
-  it "exists" do 
-    expect(@card_generator).to be_instance_of(CardGenerator)
-  end 
-  it 'cards' do 
-    expect(@card_generator.cards).to eq(@cards)
+  it 'reads files' do 
+    filename = ("cards.txt")
+    cards = CardGenerator.new(filename).cards
+    expect(CardGenerator.new(filename).cards).to all(be_a Card)
   end 
 end 
