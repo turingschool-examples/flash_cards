b6cf2605fe4795ba52a0269ba34ceb1c6e9e5e3d
J Seymour <JustJakeSeymour@gmail.com>
2022-10-11 10:02:58 -0600

    storyboard and add cards.txt

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,6 @@
 class Card
 
-  
+  #initialize with :question :answer and :category
 
 
 end
--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,9 @@
 class Turn
 
-  
+  #initialize with *string for @guess* and card (variable created in test)
+
+  #def correct?
+
+  #def feedback which responds to correct? boolean
 
 end
