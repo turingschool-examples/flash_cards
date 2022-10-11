80dbe60583a09f0a0c8fa47fd01517466c64cda7
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-11 16:35:19 -0600

    Initial commit

---
+++ lib/card.rb
@@ -0,0 +1,8 @@
+class Card
+
+    def initialize(question, answer, subject)
+        @question = question
+        @answer = answer
+        @subject = subject
+    end
+end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,3 +1,4 @@
+require 'rspec'
 require './lib/card'
 
 RSpec.describe Card do
@@ -7,19 +8,19 @@ RSpec.describe Card do
     expect(card).to be_instance_of(Card)
   end
 
-  it 'has a question' do
+  xit 'has a question' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.question).to eq("What is the capital of Alaska?")
   end
 
-  it 'has an answer' do
+  xit 'has an answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.answer).to eq("Juneau")
   end
 
-  it 'has a category' do
+  xit 'has a category' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.category).to eq(:Geography)
