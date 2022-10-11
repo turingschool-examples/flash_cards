3f21d26541fbd59c25db50fe49f62c09f3dfdc73
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-11 15:57:11 -0700

    Add Card class

---
+++ lib/card.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# represents a specific card in the deck
+class Card
+  attr_reader :question, :answer, :category
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+end
