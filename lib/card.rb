426c554763911788900ec2656ba2fdf5e27cfe40
Anhnhi Tran <anhtran811@gmail.com>
2022-10-11 17:18:20 -0600

    Refactor card code

--- lib/card.rb
+++ lib/card.rb
@@ -1,17 +1,21 @@
 class Card
+    attr_reader :question, :answer, :category
+    
     def initialize(question, answer, category)
-       
+       @question = question
+       @answer = answer
+       @category = category
     end
 
-    def question
-        "What is the capital of Alaska?"
-    end
+    # def question
+    #     "What is the capital of Alaska?"
+    # end
 
-    def answer
-        "Juneau"
-    end
+    # def answer
+    #     "Juneau"
+    # end
 
-    def category
-        :Geography
-    end
+    # def category
+    #     :Geography
+    # end
 end
