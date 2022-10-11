ac35295e5beeb998d5f88f9cbf8117aef4c316c2
Alex Pitzel <pitzelalex@gmail.com>
2022-10-11 19:53:47 -0400

    Add initialize test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,3 +1,13 @@
 require 'rspec'
 require './lib/turn'
 require './lib/card'
+
+describe turn do
+    describe '#initialize' do
+        it 'is an instance of turn' do
+            card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            turn = Turn.new("Juneau", card)
+            expect(turn).to be_a Turn
+        end
+    end
+end
