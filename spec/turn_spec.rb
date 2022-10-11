d1d66f708fd4e1043905a0b81e90547a4ce940cf
Ryan Canton <ryancanton999@gmail.com>
2022-10-11 14:32:52 -0600

    Add tests in spec file for turn class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,2 +1,34 @@
+require 'rspec'
 require './lib/card'
 require './lib/turn'
+
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+  it 'has correct feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+end
