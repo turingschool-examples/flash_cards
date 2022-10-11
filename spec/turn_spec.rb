b722f55a3c9c450abf28e2d421a26cf98d69fd88
Max MacGillivray <mary.macgillivray@gmail.com>
2022-10-11 08:37:25 -0700

    Create turn.rb and turn_spec.rb

---
+++ spec/turn_spec.rb
@@ -0,0 +1,11 @@
+require './lib/card'
+require './lib/turn'
+
+RSpec.describe Card do
+  let (:card1) {Card.new("What is the capital of Alaska?", "Juneau", :Geography)}
+  
+  it 'exists' do
+    
+  end
+  
+end
