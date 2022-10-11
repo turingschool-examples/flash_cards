97730bafbe1451b41ab86c92a22d08e1a76c9d20
Ryan Canton <ryancanton999@gmail.com>
2022-10-11 13:46:59 -0600

    Add completed version of turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,32 @@
+require './lib/card'
+
 class Turn
 
+  attr_reader :guess, :card
+
+
+  def initialize(string, card)
+    @guess = string
+    @card = card
+  end
+
+
+  def correct?
+    if (@guess == @card.answer)
+      return true
+    end
+
+    return false
+  end
+
+
+  def feedback
+    if self.correct?
+      return "Correct!"
+    end
+
+    return "Incorrect."
+  end
 
 
 end
