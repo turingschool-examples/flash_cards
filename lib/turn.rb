bd1215e8b657d32da0acfee2ca8348fd69315597
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2022-10-11 16:16:23 -0700

    Add correct? checker method

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# class representing one turn of trying a flash card
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    return true if guess == card.answer
+    false
+  end
+end
