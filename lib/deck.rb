f378953354aa4205b7251dc30874bb5facccdea5
Samuel Cox <samc1253@gmail.com>
2022-10-11 19:59:38 -0600

    Add count method to Deck class and update initialize to work correctly

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,10 @@
 class Deck
     attr_reader :cards
-    def initialize
-        @cards = []
+    def initialize(cards)
+        @cards = cards
     end
 
-    
+    def count
+        return cards.count
+    end
+end
--- lib/flash_cards_runner.rb
+++ lib/flash_cards_runner.rb
@@ -2,8 +2,18 @@ require './card'
 
 require './turn'
 
+require './deck'
+
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
 turn = Turn.new("Juneau", card_1)
+
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+cards = [card_1, card_2, card_3]
+
+turn.feedback
 
 require 'pry'; binding.pry
