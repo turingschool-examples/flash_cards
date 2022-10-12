c73af81f2b558afdb97ecf20839ef911dc7b3f54
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-12 17:38:26 -0500

    Add current card method test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -36,10 +36,16 @@ describe Round do
       expect(round.turns).to eq []
     end
 
-    it 'it starts the current card as the first in the deck' do
-
-    end
-
-
   end
+
+  describe '#current_card' do
+    it 'it starts the current card as the first in the deck' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+    end
+  end
+
 end
