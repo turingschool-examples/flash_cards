43e40523a78e40b4f082e9eb3f0e997f4aad2a63
Brady Rohrig <brady.rohrig@gmail.com>
2022-10-11 18:51:00 -0700

    Completed testing for iteration 1

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -17,5 +17,41 @@ RSpec.describe Turn do
 
         expect(turn.card).to be_a(Card)
     end
+
+    it 'has a guess' do 
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.guess).to eq("Juneau")
+    end
+
+    it 'evaluates answer as correct' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.correct?).to be true
+    end
+
+    it 'evaluates incorrect answer' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Seattle", card)
+
+        expect(turn.correct?).to be false
+    end
+
+    it 'gives feedback to correct answer' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.feedback).to eq("Correct!")
+    end
+
+    it 'gives feedback to incorrect answer' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Seattle", card)
+
+        expect(turn.feedback).to eq("Incorrect.")
+    end
+
 end
 
