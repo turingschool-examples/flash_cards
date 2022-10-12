bedeb439adaaaddce604e321192a27f3a48c86a5
Thomas Casady <tscasady@gmail.com>
2022-10-12 18:25:15 -0500

    Add tests for Round initialize method.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,1 +1,19 @@
 require './lib/round'
+
+Rspec.describe Round do
+    describe '#initialize' do
+        it 'exists' do 
+            round = Round.new(deck)
+
+            expect(round).to be_a Deck
+        end
+
+        it 'has a deck'
+            expect(Round.new(deck).deck).to eq deck
+        end
+
+        it 'has empty turns array by default'
+            expect(round.turns).to eq []
+        end
+    end
+end
