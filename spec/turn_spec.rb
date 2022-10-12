597cf41ee3207f1a64467205cdf586e3277700ba
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-12 09:05:42 -0500

    Add has card test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,6 +5,7 @@ require 'rspec'
 describe Turn do
   describe '#initialize' do
     it 'exists' do
+
       card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       turn = Turn.new("Juneau", card)
 
@@ -12,11 +13,23 @@ describe Turn do
     end
 
     it 'can have different guesses' do
+
       card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       turn_1 = Turn.new("Juneau", card)
       turn_2 = Turn.new("Austin", card)
+
       expect(turn_1.guess).to eq 'Juneau'
       expect(turn_2.guess).to eq 'Austin'
+    end
+
+    it 'has a card' do
+
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+      turn = Turn.new("Juneau", card)
+
+      expect(turn.card).to eq(card)
+
     end
 
 
