16e6bd6afb6895fb5ffa10d414944569f00005a6
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-11 21:32:13 -0700

    Add methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,21 @@
+require_relative 'card'
+
 class Turn
-    attr_reader :string
+    attr_reader :guess
     attr_reader :card
 
     def initialize(guess, card)
         @guess = guess
         @card = card
+    end
+
+    def guess
+        @guess 
+    end
+
+    def correct?
+        #returns boolean if guess matches answer on card
+        @guess == @answer 
     end
 end
 
