7b60db390969c9ad21b5a347cdf6014d89e82e23
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-12 16:57:49 -0500

    Pass cards in category method test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,23 @@
 require './lib/card'
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :category_groups
   def initialize(cards)
     @cards = cards
+    @category_groups = cards.group_by do |card|
+      card.category
+    end
   end
 
   def count
     cards.length
+  end
+
+  def cards_in_category(category)
+    if category_groups.include?(category)
+      category_groups[category]
+    else
+      []
+    end
   end
 end
