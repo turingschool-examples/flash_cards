69f29622935b543d6679a522d6672aa303c7c295
Alex Pitzel <pitzelalex@gmail.com>
2022-10-11 20:00:50 -0400

    Add fixed describe block

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,12 +2,12 @@ require 'rspec'
 require './lib/turn'
 require './lib/card'
 
-describe turn do
+describe Turn do
     describe '#initialize' do
         it 'is an instance of turn' do
             card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
             turn = Turn.new("Juneau", card)
-            expect(turn).to be_a Turn
+            expect(turn).to be_instance_of(Turn)
         end
     end
 end
