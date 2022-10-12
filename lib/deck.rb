b5a2eabe471176564a273f51196c26d1d77c1307
Joe King <j.king92@gmail.com>
2022-10-11 21:55:46 -0700

    Add the ability to check a deck for the cards in a given category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,22 @@
 class Deck
+
   def initialize(cards)
     @cards = cards
   end
+
   def count
     @cards.count
   end
+
+  def cards_in_category(category)
+    this_category = []
+    @cards.each do |card|
+      if card.category == category
+        this_category << card
+      end
+    end
+    return this_category
+  end
 end
+
+# require "pry"; binding.pry
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -26,4 +26,28 @@ describe Deck do
 
     expect(deck.count).to eq 3
   end
+
+  it 'can provide the cards in a category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq [card_2, card_3]
+  end
+
+  it 'can check if cards are in a category that does not have cards yet' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category("Pop culture")).to eq []
+  end
 end
