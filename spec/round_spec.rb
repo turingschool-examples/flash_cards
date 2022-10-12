cd9190b0f506e057b2e968cdeacc6ecc3a7c4d20
Thomas Casady <tscasady@gmail.com>
2022-10-12 18:50:00 -0500

    Create current_card method tests.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -27,4 +27,18 @@ RSpec.describe Round do
             expect(Round.new(deck).turns).to eq []
         end
     end
+
+    describe '#current_card' do
+        it 'can return the current card in the deck' do
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM) 
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+            round = Round.new(deck)
+
+            expect(round.current_card).to eq card_1
+        end
+    end
+
 end
