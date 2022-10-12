9c84121fd27d2fa1e79019ebcd6d56e095b0eb47
Kelsie Porter <krporter22@gmail.com>
2022-10-12 16:55:14 -0600

    WIP Create take_turn method in Round Class

--- lib/round.rb
+++ lib/round.rb
@@ -11,7 +11,13 @@ class Round
     @deck.cards[0]
   end
 
-  def take_turn
+  def take_turn(guess)
+    turn_2 = Turn.new(guess, card)
+    return turn_2
     # might need @deck.shift to remove the current card (first element) from the deck array
   end
+
+  # def class
+  #   @turns
+  # end
 end
