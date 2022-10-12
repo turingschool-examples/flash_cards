b0713dcbc08135907e8e3930ee0ae57ed3fe20b2
Samuel Cox <samc1253@gmail.com>
2022-10-12 17:45:14 -0600

    Add method for number_correct in round file and corresponding test

--- lib/round.rb
+++ lib/round.rb
@@ -8,14 +8,27 @@ class Round
     attr_reader :deck, :turns
     def initialize(deck)
         @deck = deck
+        @number_correct = 0
     end
 
     def current_card
-        return @deck.cards[0]
+        @deck.cards[0]
+
     end
 
     def take_turn(guess)
         @guess = guess
         @turns = Turn.new("#{guess}", self.current_card)
+        if
+            @turns.card.answer == @turns.guess
+            @number_correct += 1
+        end
+        @turns = Turn.new("#{guess}", self.current_card)
+
     end
+
+    def number_correct
+        @number_correct
+    end
+
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -52,7 +52,29 @@ RSpec.describe Round do
 
             new_turn = round.take_turn("Juneau")
 
-            expect(new_turn).to eq 
+            expect(round.turns).to eq(new_turn) 
         end
     end
+
+    describe '#number_correct' do
+        it 'returns the amount of questions answered correctly' do
+
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+            cards = [card_1, card_2, card_3]
+
+            deck = Deck.new(cards)
+
+            round = Round.new(deck)
+
+            new_turn = round.take_turn("Juneau")
+
+            expect(round.number_correct).to eq 1
+        end
+    end
+
 end
