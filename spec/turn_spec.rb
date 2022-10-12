793ccfa920f9cb23285ad52a2c8f3e88af5499c3
Kerynn Davis <kerynn.davis@gmail.com>
2022-10-11 20:07:44 -0700

    Created turn spec file

---
+++ spec/turn_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+    # test it exists
+  it 'exists' do
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_a(Turn)
+  end
+end
+
+  # test it has a card
+  # test it has a guess
+  # test correct?
+  # test return feedback
+
+
+
+
+
+
+
+
+
+# test
