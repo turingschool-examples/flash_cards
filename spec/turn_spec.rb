f0a721b1ad80d220533b9aee015a68b004d24a55
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-10-11 22:36:24 -0500

    Have all errors fixed in turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,20 +1,22 @@
 require './lib/turn'
+require './lib/card'
 
-RSpec.decribe Turn do
-  it 'exist' do
-    turn = Turn.new("Juneau", card)
+RSpec.describe Turn do
+  it 'exists' do
+    turn = Turn.new("Juneau", Card)
 
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
-    turn = Turn.new("Juneau",card)
+    turn = Turn.new("Juneau",Card)
 
     expect(turn.guess).to eq("Juneau")
   end
 
   it 'has a card' do
-    turn = Turn.new("Juneau",card)
+    turn = Turn.new("Juneau",Card)
 
-    expect(turn.card).to eq(card)
+    expect(turn.card).to eq(Card)
   end
+end
