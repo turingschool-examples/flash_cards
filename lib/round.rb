a59ef6bf9ff3e18715a418af98625312cb0947fa
Joe King <j.king92@gmail.com>
2022-10-12 15:52:13 -0700

    Add round files including current_card and take_turn methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,14 +1,22 @@
 class Round
   attr_reader :deck, :turns
+  # , :current_turn
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    # @current_turn = nil
   end
 
   def current_card
     @deck.cards[0]
   end
+
+  def take_turn(guess)
+    current_turn = Turn.new(guess, @deck.cards[0])
+    @deck.cards.shift
+    return current_turn
+  end
 end
 
-require "pry"; binding.pry
+# require "pry"; binding.pry
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,9 +2,11 @@ require 'rspec'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/turn'
+require 'pry'
 
 describe Round do
-  it 'exists and can take a deck of cards as a parameter' do
+  it 'exists and can deck of cards can be added to the round' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -15,18 +17,48 @@ describe Round do
 
     expect(round).to be_a Round
   end
+  describe '#current_card' do
+    it 'can provide the current card' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-  it 'can provide the current card' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
 
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    round = Round.new(deck)
-
-    expect(round.current_card).to eq card_1
-
+      expect(round.current_card).to eq card_1
+    end
   end
 
+  describe '#take_turn' do
+    it 'take a guess and store it with the card in a turn' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+
+      expect(new_turn).to be_a Turn
+    end
+
+    it 'can confirm if a question is correct' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+
+      expect(new_turn.correct?).to eq (true)
+
+    end
+  end
 end
