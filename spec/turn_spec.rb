5daf902f43b62622432f822f8ca2509973069567
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-10-11 18:37:36 -0700

    Add test for has a guess

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,13 +5,21 @@ RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-    
+
     expect(turn).to be_a(Turn)
   end
-it 'has a card' do
-  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  turn = Turn.new("Juneau", card)
 
-  expect(turn.card).to be_a(Card)
-end
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to be_a(Card)
+  end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")  
+  end
 end
