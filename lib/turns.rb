48c144717e1935d84c2398bd15c9f6bac19c85f1
Kelsie Porter <krporter22@gmail.com>
2022-10-11 20:16:46 -0600

    Create corrrect? and feedback methods and make associated tests pass

--- lib/turns.rb
+++ lib/turns.rb
@@ -6,4 +6,17 @@ class Turn
     @guess = guess
     @card = card
   end
+
+  def correct?
+    @guess == card.answer
+  end
+
+  def feedback
+    if correct?
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
+
 end
