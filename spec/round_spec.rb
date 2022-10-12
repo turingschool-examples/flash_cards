c93f3d737ca864e8c8717777cdf4020a266853e2
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-12 17:34:05 -0500

    Add empty turns arry test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -26,8 +26,14 @@ describe Round do
       expect(round.deck).to eq deck
     end
 
-    it 'it defaults turns as empty' do
+    it 'has an array for Turns that defaults empty' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
 
+      expect(round.turns).to eq []
     end
 
     it 'it starts the current card as the first in the deck' do
