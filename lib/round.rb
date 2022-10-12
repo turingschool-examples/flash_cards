9c2a845d7b1a8053f2a9bb721f0002227ab3d6ae
Kelsie Porter <krporter22@gmail.com>
2022-10-11 21:37:47 -0600

    WIP Create take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -3,4 +3,8 @@ class Round
   def initialize(deck)
     @deck = deck
   end
+
+  def take_turn
+    # might need @deck.shift to remove the current card (first element) from the deck array
+  end
 end
