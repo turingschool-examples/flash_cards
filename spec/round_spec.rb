63f83f935dd837302d953afa2cad48ef33f18962
Kelsie Porter <krporter22@gmail.com>
2022-10-11 21:45:27 -0600

    Create and pass round method specs

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -25,6 +25,33 @@ RSpec.describe Round do
     deck = Deck.new(cards)
 
     expect(deck.cards).to eq(cards)
+    expect(deck.cards[1]).to eq(card_2)
+  end
+
+  describe '#round' do
+    it 'student plays a round' do
+      card_1 = Card.new(
+        "What is the capital of Alaska?",
+        "Juneau",
+        :Geography
+      )
+      card_2 = Card.new(
+        "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?",
+        "Mars",
+        :STEM
+      )
+      card_3 = Card.new(
+        "Describe in words the exact direction that is 697.5° clockwise from due north?",
+        "North north west",
+        :STEM
+      )
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      expect(round.deck).to eq(deck)
+      expect(round.deck.cards[2].category).to eq(:STEM)     
+    end
   end
 
 
