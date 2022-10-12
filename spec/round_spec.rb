ece96d1f39a2ccd6bf287c3a2ac12f0e75cac356
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-12 18:04:06 -0500

    Add a take turn instance test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -39,7 +39,7 @@ describe Round do
   end
 
   describe '#current_card' do
-    it 'it starts the current card as the first in the deck' do
+    it 'starts the current card as the first in the deck' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -50,4 +50,18 @@ describe Round do
     end
   end
 
+  describe '#take_turn' do
+    it 'can make a new Turn instance' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+
+      expect(new_turn.class).to eq Turn
+
+    end
+  end
 end
