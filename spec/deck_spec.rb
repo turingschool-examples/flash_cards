5403a00f64cdda30f9637e2eb9987b4ae023d7e9
Kelsie Porter <krporter22@gmail.com>
2022-10-11 21:12:13 -0600

    Fix spacing and long lines of code

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,10 +6,21 @@ require 'pry'
 RSpec.describe Deck do
   describe '#cards' do
     it 'has a deck of cards' do
-      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-      # Is there a correct way to condence these long lines of arguments for each card instance? Would you do that?
+      card_1 = Card.new(
+        "What is the capital of Alaska?",
+        "Juneau",
+        :Geography
+      )
+      card_2 = Card.new(
+        "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?",
+        "Mars",
+        :STEM
+      )
+      card_3 = Card.new(
+        "Describe in words the exact direction that is 697.5° clockwise from due north?",
+        "North north west",
+        :STEM
+      )
       cards = [card_1, card_2, card_3]
       deck = Deck.new(cards)
 
@@ -20,9 +31,21 @@ RSpec.describe Deck do
 
   describe '#cards_in_category' do
     it 'returns the cards from the same category' do
-      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      card_1 = Card.new(
+        "What is the capital of Alaska?",
+        "Juneau",
+        :Geography
+      )
+      card_2 = Card.new(
+        "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?",
+        "Mars",
+        :STEM
+      )
+      card_3 = Card.new(
+        "Describe in words the exact direction that is 697.5° clockwise from due north?",
+        "North north west",
+        :STEM
+      )
       cards = [card_1, card_2, card_3]
       deck = Deck.new(cards)
 
