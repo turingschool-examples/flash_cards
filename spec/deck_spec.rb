178dfdcb1bb2c81a926844b27cc60b71f619ac25
KaraJH <karas.jones@yahoo.com>
2022-10-12 15:59:14 -0600

    Added first test

---
+++ spec/deck_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require './lib/turn'
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+  it 'exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    pry(main)> card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    cards = [card_1, card_2]
+    deck = Deck.new(cards)
+    expect(deck).to be_instance_of(Deck)
+  end
+end
