b1aa3aff21198b9b4da185052b45c020f5063200
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-10-12 09:35:59 -0500

    Add new method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,18 @@
 class Turn
-  attr_reader :string, :card, :guess
+  attr_reader :string, :card, :guess, :correct, :feedback, :anwser
   def initialize(string, card)
-    @string = string
     @card = card
-    @guess = "Juneau"
+    @guess = string
+    @correct = true
+    @feedback = "Correct!"
+  end
+
+
+  def correct?
+    if @correct == true
+      correct == true
+    else correct == false
+    end
   end
 
 end
