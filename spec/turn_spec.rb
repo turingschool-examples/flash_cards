758b92a86b7042c5d52047d4d554c7da69f0b60e
Alastair Lloyd <dex8056@aol.com>
2022-10-12 08:21:05 -0600

    Add turn spec describes

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,21 +2,45 @@ require 'rspec'
 require './lib/card'
 require './lib/turn'
 
+
 RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card).to be_instance_of(Card)
+
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a turn' do
-    turn = turn.new("Juneau", card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card).to be_instance_of(Card)
+
+    turn = Turn.new("Juneau", card)
 
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
-    turn = turn.new("Juneau", card)
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card).to be_instance_of(Card)
+
+    turn = Turn.new("Juneau", card)
 
     expect(card.answer).to eq("Juneau")
   end
+
+  it 'has the correct answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card).to be_instance_of(Card)
+
+    turn = Turn.new("Juneau", card)
+
+    expect(card.answer).to eq("Juneau")
+  end
+end
