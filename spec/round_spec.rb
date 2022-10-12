e53c5ff1192b6fd0c2bd7f821a6a46ba3489224c
Thomas Casady <tscasady@gmail.com>
2022-10-12 18:35:21 -0500

    Add missing files and associated objects to round_spec tests.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,18 +1,29 @@
 require './lib/round'
+require './lib/deck'
+require './lib/card'
+require './lib/turn'
+
 
 RSpec.describe Round do
     describe '#initialize' do
         it 'exists' do 
-            round = Round.new(deck)
+            card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            deck = Deck.new(card)
 
-            expect(round).to be_a Deck
+            expect(Round.new(deck)).to be_a Deck
         end
 
         it 'has a deck' do
+            card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            deck = Deck.new(card)
+
             expect(Round.new(deck).deck).to eq deck
         end
 
         it 'has empty turns array by default' do
+            card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            deck = Deck.new(card)
+            
             expect(round.turns).to eq []
         end
     end
