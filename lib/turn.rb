ed696cb48e878ee5163ece00a4cd17bbf9a8cbda
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-11 19:26:56 -0600

    Add correct? and feedback methods to turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,22 @@
 class Turn
-    attr_reader :card
-    
+    attr_reader :guess, 
+                :card
+
     def initialize(guess, card)
         @guess = guess
         @card = card
+    end
+
+    def correct?
+        guess == card.answer
+    end
+
+    def feedback
+        # require 'pry'; binding.pry
+       if correct? == true
+        return "Correct!"
+       else 
+        return "Incorrect!"
+       end
     end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -7,14 +7,35 @@ RSpec.describe Turn do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         turn = Turn.new("Juneau", card)
   
-        expect(turn).to be_instance_of(Turn)
+        expect(turn).to be_a (Turn)
     end
 
     it 'can have a card with attributes' do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         turn = Turn.new("Juneau", card)
 
-        expect(turn.card).to eq card
+        expect(turn.card).to eq (card)
     end
+
+    it 'can accept a guess' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.guess).to eq ("Juneau")
+    end
+
+    it 'can return true if a guess is correct' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.correct?).to eq (true)
+    end
+
+    it 'can return "Correct!" if answer is true' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.feedback).to eq ("Correct!")
+    end 
 end
   
