3c0c81f93572177387a462395a69628a2355978d
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-12 09:19:31 -0500

    Add true false test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -25,19 +25,23 @@ describe Turn do
     it 'has a card' do
 
       card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
       turn = Turn.new("Juneau", card)
 
       expect(turn.card).to eq(card)
 
     end
+  end
 
+  describe '#correct?' do
+    it 'shows true or false for guess' do
 
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn_1 = Turn.new("Juneau", card)
+      turn_2 = Turn.new("Austin", card)
 
+      expect(turn_1.correct?).to be true
+      expect(turn_2.correct?).to be false
 
-
-
-
-
+    end
   end
 end
