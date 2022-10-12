d8cd40947d5dc2fb074b1d62568a1479c51e9ff3
Max MacGillivray <mary.macgillivray@gmail.com>
2022-10-11 22:10:57 -0700

    Complete iteration 1, start iteration 2

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,10 +1,14 @@
+require 'pry'
 require './lib/card'
 require './lib/turn'
 
 RSpec.describe Turn do
   let (:card1) {Card.new("What is the capital of Alaska?", "Juneau", :Geography)}
+  let (:card2) {Card.new("Which planet is closest to the sun?", "Mercury", :STEM)}
 
   let (:turn1) {Turn.new("Juneau", card1)}
+  let (:turn2) {Turn.new("Saturn", card2)}
+  
 
   it 'exists' do
     expect(turn1).to be_a(Turn)
@@ -19,15 +23,11 @@ RSpec.describe Turn do
   end
 
   it 'can determine if an answer is correct' do
-    turn2 = Turn.new("Anchorage", card1)
-    
     expect(turn1.correct?).to eq(true)
     expect(turn2.correct?).to eq(false)
   end
 
   it 'returns feedback after an answer is recieved' do
-    turn2 = Turn.new("Anchorage", card1)
-    
     expect(turn1.feedback).to eq("Correct!")
     expect(turn2.feedback).to eq("Incorrect!")
   end
