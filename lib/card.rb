e32eaf2341fa4baaa6b0b200484caa9afa3a945d
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-11 19:21:31 -0700

    Make the first four tests pass

---
+++ lib/card.rb
@@ -0,0 +1,21 @@
+class Card
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+
+  def question
+    @question
+  end
+
+  def answer
+    @answer
+  end
+
+  def category
+    @category
+  end
+
+end
