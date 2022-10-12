ce014ffed6c952afe548d9896e3cace82fc8bd29
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-12 18:17:26 -0500

    Add stores turn test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -88,5 +88,17 @@ describe Round do
       expect(new_turn_1.correct?).to be true
       expect(new_turn_2.correct?).to be false
     end
+
+    it 'stores new turn' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+
+      expect(round.turns).to eq [new_turn]
+    end
   end
 end
