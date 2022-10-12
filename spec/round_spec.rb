40a32d551adcb2c96deaf4d402bf2b33a0ea4c15
Kelsie Porter <krporter22@gmail.com>
2022-10-12 16:53:40 -0600

    Create spec for take_turn method in Round Class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -80,5 +80,33 @@ RSpec.describe Round do
     end
   end
 
+  describe '#take_turn' do
+    it 'student takes a turn by guessing the question' do
+      it 'student takes a turn' do
+        card_1 = Card.new(
+          "What is the capital of Alaska?",
+          "Juneau",
+          :Geography
+        )
+        card_2 = Card.new(
+          "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?",
+          "Mars",
+          :STEM
+        )
+        card_3 = Card.new(
+          "Describe in words the exact direction that is 697.5° clockwise from due north?",
+          "North north west",
+          :STEM
+        )
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+
+        expect(new_turn.class).to eq(Turn)
+        expect(new_turn.correct?).to eq True
+    end
+  end
+
 
 end
