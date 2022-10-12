4d7e402f26b6e84e2217035af2fb526556379a37
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-11 20:45:53 -0700

    Add attr_readers

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,8 @@
 class Card
+    attr_reader :question
+    attr_reader :answer
+    attr_reader :category
+    
     def initialize(question, answer, category)
         @question = question
         @answer = answer
