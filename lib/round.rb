799b46ce1e5a5fc9c5b878aa377d8dea93fbb51f
Ryan Canton <ryancanton999@gmail.com>
2022-10-13 10:10:34 -0600

    Add and define take_turn and number_correct methods for class Round

--- lib/round.rb
+++ lib/round.rb
@@ -1,16 +1,30 @@
 
 class Round
 
+attr_reader :deck, :current_card, :turns
 
   def initialize(deck)
     @deck = deck
     @card_index = 0
     @turns = []
-    @current_card = @deck[@card_index]
+    @current_card = @deck.cards[@card_index]
   end
 
+  def take_turn(guess)
+    @turns.push(Turn.new(guess, @current_card))
+    @card_index++
+    return @turns[@card_index - 1]
+  end
 
+  def number_correct()
+    correct_count = 0
 
+    @turns.each do |turn|
+      if (@turns[turn].correct?)
+        correct_count++
+      end
+    end
 
-
+      return correct_count
+  end
 end
