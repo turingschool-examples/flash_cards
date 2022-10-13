c2d6fdce57cf4c8a4ff64785221ad3b60c5013a4
Alex Pitzel <pitzelalex@gmail.com>
2022-10-12 20:25:27 -0400

    Add has cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -10,6 +10,15 @@ describe Deck do
             deck = Deck.new
             expect(deck).to be_instance_of(Deck)
         end
-        it 'has cards'
+        it 'has cards' do
+        
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+
+            expect(deck.cards).to eq(cards)
+        end
     end
 end
