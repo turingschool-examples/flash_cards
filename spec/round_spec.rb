f13d88c7298f0bc8385bdb160e3874212be5517b
Thomas Casady <tscasady@gmail.com>
2022-10-13 16:31:24 -0500

    Add tests for number_coorect_by_category method for Round class.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -103,7 +103,7 @@ RSpec.describe Round do
             round = Round.new(deck)
             turn = round.take_turn('Juneau')
             turn2 = round.take_turn('Mars')
-            
+
             expect(round.number_correct).to eq 2
         end
 
@@ -118,6 +118,34 @@ RSpec.describe Round do
             turn = round.take_turn('Juneau')
 
             expect(round.current_card).to be card_2
+        end
+    end
+
+    describe '#number_correct_by_category' do
+        it 'can check if the answer is correct' do
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM) 
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+            round = Round.new(deck)
+            turn = round.take_turn('Juneau')
+            
+            expect(round.number_correct_by_category(:Geography)).to eq 1
+        end
+
+        it 'can record a correct answer in the appropriate category' do
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM) 
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+            round = Round.new(deck)
+            turn = round.take_turn('Juneau')
+            
+            expect(round.number_correct_by_category(:Geography)).to eq 1
+            expect(round.number_correct_by_category(:STEM)).to eq 0
+                        
         end
     end
 
