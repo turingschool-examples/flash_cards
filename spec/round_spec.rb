e0415b308542bcfafd019bde254e7578677aba16
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-13 14:52:37 -0500

    Add switch current card test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -125,7 +125,24 @@ describe Round do
 
       expect(round.correct).not_to include new_turn
       expect(round.incorrect).to include new_turn
+
+    end
+
+    it 'switches current card to the next card in the deck ' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn_1 = round.take_turn("Austin")
+
+      expect(round.current_card).to eq card_2
+
+      new_turn_2 = round.take_turn("Saturn")
       
+      expect(round.current_card).to eq card_3
+
     end
 
   end
@@ -146,7 +163,7 @@ describe Round do
 
   end
 
-  describe '#'
+
 
 
 
