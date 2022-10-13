b8857a572230b23dee91d1e5805675f82ee261c4
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-12 19:11:21 -0600

    Created test 2 in turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Turn do
 
     expect(turn).to be_instance_of(Turn)
     end
+
+    it 'turn has a card' do
+        turn.card
+
+    expect(turn.card).to eq(card)
+    end
+
 end
 
 binding.pry
