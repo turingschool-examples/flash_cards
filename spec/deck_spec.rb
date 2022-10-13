0ed6ca98d92f84a04c46355d63565a9880c45f7d
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-13 15:14:51 -0600

    Wrote test 3 for deck_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -33,12 +33,27 @@ RSpec.describe Deck do
     
     end
 
-
-
-
-
 #Test 3
 #Deck, cards exists
+    it "Deck exists" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        expect(deck).to be_instance_of(Deck)
+    end
+
+
+
+
+
 
 #Test 4
 #Can call on cards
