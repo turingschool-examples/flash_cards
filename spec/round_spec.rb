b51be83c5e7f16acb32eff7f6dc06ee5fd19bc79
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-13 18:09:24 -0500

    Add percent correct test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -212,7 +212,27 @@ describe Round do
       expect(round.number_correct_by_category(:STEM)).to eq 2
       expect(round.number_correct_by_category(:Music)).to eq 0
 
+    end
+  end
 
+  describe '#percent_correct' do
+    it "returns the percent correct out of how many turns taken " do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      card_4 = Card.new("Presto, largo, allegro relate to what property of music?" , "Tempo", :Music)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn_1 = round.take_turn("Juneau")
+      new_turn_2 = round.take_turn("Mars")
+      new_turn_3 = round.take_turn("North north west")
+      new_turn_4 = round.take_turn("Timbre")
+
+      expect(round.turns.length).to eq 4
+      expect(round.number_correct).to eq 3
+
+      expect(round.percent_correct).to eq 75.0
     end
   end
 
