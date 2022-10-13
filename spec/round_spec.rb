5cef91d70a42d31693189c350462cde395b95372
Brady Rohrig <brady.rohrig@gmail.com>
2022-10-13 12:07:55 -0700

    Added turn count test after card change

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -123,6 +123,29 @@ RSpec.describe Round do
         expect(round.current_card).to eq(card_2)
     end
 
+    it 'creates a new turn with the second card' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+        new_turn_2 = round.take_turn("Venus")
+
+        expect(new_turn_2).to be_a(Turn)
+    end
+
+    it 'gives correct turn count after turn 2' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+        new_turn_2 = round.take_turn("Venus")
+
+        expect(round.turns.count).to eq(2)
+    end
 
 
 
