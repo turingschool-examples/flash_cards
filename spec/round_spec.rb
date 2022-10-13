2de9950ac1fd672dccf2af2eddb9a82b4610d44f
Kelsie Porter <krporter22@gmail.com>
2022-10-12 20:13:01 -0600

    Create number_correct_by_category specs

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -151,4 +151,36 @@ RSpec.describe Round do
       expect(round.number_correct).to eq(2)
     end
   end
+
+  describe '#number_correct_by_category' do
+    it 'returns the number of correct turns by each category' do
+      card_1 = Card.new(
+        "What is the capital of Alaska?",
+        "Juneau",
+        :Geography
+      )
+      card_2 = Card.new(
+        "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?",
+        "Mars",
+        :STEM
+      )
+      card_3 = Card.new(
+        "Describe in words the exact direction that is 697.5° clockwise from due north?",
+        "North north west",
+        :STEM
+      )
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      round.take_turn("Juneau")
+      round.take_turn("Venus")
+      round.take_turn("North north west")
+
+      expect(round.number_correct).to eq(2)
+      expect(round.number_correct_by_category(:Geography)).to eq(1)
+      expect(round.number_correct_by_category(:STEM)).to eq(1)
+      expect(round.number_correct_by_category(:Math)).to eq(0)
+    end
+  end
 end
