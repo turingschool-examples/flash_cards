abc8b63071e201b418f67fed83ed4a4f43d919ff
Anhnhi Tran <anhtran811@gmail.com>
2022-10-12 22:22:55 -0600

    Add starts a round test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,43 +4,59 @@ require './lib/deck'
 require './lib/round'
 
 RSpec.describe Round do 
-it 'exists' do
-  card_1 = Card.new(
-    'What is the capital of Alaska?',
-    'Juneau',
-    :Geography
-  )
-  card_2 = Card.new(
-    'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?',
-    'Mars',
-    :STEM
-  )
-  card_3 = Card.new(
-    'Describe in words the exact direction that is 697.5° clockwise from due north?',
-    'North north west',
-    :STEM
-  )
+  it 'exists' do
+    card_1 = Card.new(
+      'What is the capital of Alaska?',
+      'Juneau',
+      :Geography
+    )
+    card_2 = Card.new(
+      'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?',
+      'Mars',
+      :STEM
+    )
+    card_3 = Card.new(
+      'Describe in words the exact direction that is 697.5° clockwise from due north?',
+      'North north west',
+      :STEM
+    )
 
-  expect(card_1).to be_instance_of(Card)
-  expect(card_2).to be_instance_of(Card)
-  expect(card_3).to be_instance_of(Card)
+    expect(card_1).to be_instance_of(Card)
+    expect(card_2).to be_instance_of(Card)
+    expect(card_3).to be_instance_of(Card)
 
-  deck = Deck.new([card_1, card_2, card_3])
-  round = Round.new(deck)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
 
-  expect(deck).to be_instance_of(Deck)
-  expect(round).to be_instance_of(Round)
+    expect(deck).to be_instance_of(Deck)
+    expect(round).to be_instance_of(Round)
+  end
 
+  it 'starts a round' do
+    card_1 = Card.new(
+      'What is the capital of Alaska?',
+      'Juneau',
+      :Geography
+    )
+    card_2 = Card.new(
+      'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?',
+      'Mars',
+      :STEM
+    )
+    card_3 = Card.new(
+      'Describe in words the exact direction that is 697.5° clockwise from due north?',
+      'North north west',
+      :STEM
+    )
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.deck).to be(deck)
   end
 end
 
 
-
-
-
-
-# round.deck
-# #=> #<Deck:0x00007fa160a38ed8...>
 
 # round.turns
 # #=> []
