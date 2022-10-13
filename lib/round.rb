5adc13e08b930e12c96aa2dc00e9be4fd294e24e
Kelsie Porter <krporter22@gmail.com>
2022-10-12 20:52:11 -0600

    Create percent_correct_by_category method and pass associated specs

--- lib/round.rb
+++ lib/round.rb
@@ -37,4 +37,11 @@ class Round
     number_correct / @turns.length.to_f * 100
   end
 
+  def percent_correct_by_category(category)
+    turns_by_cat = @turns.select do |turn|
+      turn.card.category == category
+    end
+    number_correct_by_category(category) / turns_by_cat.length.to_f * 100
+  end
+
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -236,7 +236,7 @@ RSpec.describe Round do
       round.take_turn("Juneau")
       round.take_turn("Venus")
 
-      expect(round.percent_correct_by_category).to eq(100.0)
+      expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
       expect(round.current_card).to eq(card_3)
     end
   end
