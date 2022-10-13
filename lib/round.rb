ccdcc44b05dbc4028f969c822fbb95205593201a
Alastair Lloyd <dex8056@aol.com>
2022-10-13 13:47:52 -0600

    Add turns method

--- lib/round.rb
+++ lib/round.rb
@@ -1,19 +1,30 @@
 class Round
 
   attr_reader         :card,
-                      :guesses,
+                      :turns,
                       :deck,
                       :number_correct,
                       :total_cards,
                       :number_guesses
 
-def initialize(deck)
+ def initialize(deck)
   @deck             = deck
-  @guesses          = []
+  @turns            = []
   @current_card     = 0
-  @number_correct   = 0.0
-  @number_guesses   = 0.0
-  @total_cards      = deck.cards.count
-  @current_guess    = 0
-  @percent_correct  = percent_correct
+  # @number_correct   = 0.0
+  # @number_guesses   = 0.0
+  # @total_cards      = deck.cards.count
+  # @current_guess    = 0
+ end
+
+ def current_card
+    deck.cards[@current_card]
+  end
+
+ def record_turn(turn)
+    @new_turn = Turn.new(guess, current_card)
+    @turns << @new_turn
+    @new_turn
+  end
+
 end
