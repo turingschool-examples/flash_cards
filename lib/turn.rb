7f4b5dd732ffa1581d98de4eb8a0e008165f4d82
Drew Layton <dlayton66@gmail.com>
2022-10-12 22:37:18 -0600

    Finish Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,21 @@
 class Turn
   attr_reader :guess, :card
+
   def initialize(guess, card)
     @guess = guess
     @card = card
   end
+
+  def correct?
+    card.answer == guess
+  end
+
+  def feedback
+    if card.answer == guess
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
+
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -47,14 +47,14 @@ describe Turn do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         turn = Turn.new("Juneau",card)
 
-        expect(turn.feedback).to be "Correct!"
+        expect(turn.feedback).to eq "Correct!"
       end
 
       it 'returns "Incorrect." for an incorrect guess' do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         turn = Turn.new("Alaska City",card)
 
-        expect(turn.feedback).to be "Incorrect."
+        expect(turn.feedback).to eq "Incorrect."
       end
 
     end
