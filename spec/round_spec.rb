d6ec3fa0b13a3021ab3a69a98f5ed1887eb8111a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-13 15:43:02 -0500

    Add take turn feedback test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -119,6 +119,22 @@ describe Round do
       expect(round.turns.count).to eq 2
     end
 
+    it 'saves feedback for each new turn' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn_1 = round.take_turn("Juneau")
+
+      expect(round.turns.last.feedback).to eq "Correct!"
+
+      new_turn_2 = round.take_turn("Juneau")
+
+      expect(round.turns.last.feedback).to eq "Incorrect."
+    end
+
     it 'saves correct guesses' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
