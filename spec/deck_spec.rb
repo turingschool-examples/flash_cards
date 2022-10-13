34b159f9619a8780007fb2b1280755cb208adc99
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-13 14:35:39 -0600

    Identified the tests for the deck class

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,3 +2,27 @@ require './lib/card'
 require './lib/deck.rb'
 
 RSpec.describe Deck do
+
+#Test 1
+#Card 1 -3 all exist
+
+#Test 2
+#Array cards with card 1-3 exists
+
+#Test 3
+#Deck, cards exists
+
+#Test 4
+#Can call on cards
+
+#Test 5
+#Can count the number of cards in deck
+
+#Test 6
+#Can call the cards in category :STEM
+
+#Test 7
+#Can call the cards in category :Geography
+
+#Test 8
+#Can call the cards in category "Pop Culture"
