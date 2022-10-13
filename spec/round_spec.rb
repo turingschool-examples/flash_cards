fabdbdc744e9324e648fdf12659d1aa9d6cde14f
Kelsie Porter <krporter22@gmail.com>
2022-10-12 19:28:07 -0600

    Add additional specs to number_correct spec

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -139,9 +139,11 @@ RSpec.describe Round do
       round.take_turn("Juneau")
 
       expect(round.number_correct).to eq(1)
+      expect(round.current_card).to eq(card_2)
 
       round.take_turn("Homer")
-
+      expect(round.turns.count).to eq(2)
+      expect(round.turns.last.feedback).to eq("Incorrect.")
       expect(round.number_correct).to eq(1)
 
       round.take_turn("North north west")
