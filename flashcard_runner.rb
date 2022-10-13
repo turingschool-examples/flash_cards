699f5d67e7d4aef377f99fa5ea24ce60219170fa
Brady Rohrig <brady.rohrig@gmail.com>
2022-10-13 15:32:06 -0700

    Added questions to runner

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -10,10 +10,21 @@ card_4 = Card.new("What is the rarest blood type?", "AB-", :STEM)
 card_5 = Card.new("Who played Jack in the movie 'Titanic'?", "Leonardo DiCaprio", :PopCulture)
 card_6 = Card.new("What amazing soccer team is based in Seattle, Washington?", "Sounders FC", :Sports)
 
-deck_1 = [card_1, card_2, card_3, card_4, card_5, card_6]
+deck_1 = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
 
 round = Round.new(deck_1)
 
 p "Welcome! You're playing with #{round.deck.count} cards."
 p "-------------------------------------------------------"
-p "This is card number #{(round.turns.length) + 1} out of #{round.deck.count}."
+
+while round.deck.count > 0 do
+
+p "This is card number #{(round.turns.length) + 1} out of #{round.deck.count + round.turns.count}."
+p "Question: #{round.current_card.question}"
+
+answer_1 = gets.chomp
+
+round.take_turn(answer_1)
+
+p round.turns[-1].feedback
+end
