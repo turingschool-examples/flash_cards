d62d4aa94d00e6c3a631a5716d2737a21cb6acee
Anhnhi Tran <anhtran811@gmail.com>
2022-10-12 22:27:55 -0600

    Add turns test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -54,12 +54,32 @@ RSpec.describe Round do
 
     expect(round.deck).to be(deck)
   end
+
+  it 'starts with a card' do
+    card_1 = Card.new(
+      'What is the capital of Alaska?',
+      'Juneau',
+      :Geography
+    )
+    card_2 = Card.new(
+      'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?',
+      'Mars',
+      :STEM
+    )
+    card_3 = Card.new(
+      'Describe in words the exact direction that is 697.5° clockwise from due north?',
+      'North north west',
+      :STEM
+    )
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.turns).to eq([])
+    require 'pry'; binding.pry
+  end
 end
 
-
-
-# round.turns
-# #=> []
 
 # round.current_card
 # #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
