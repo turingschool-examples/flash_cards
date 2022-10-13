2a8f844c371afb30cb41865cb0908c67d42c62fd
Alex Pitzel <pitzelalex@gmail.com>
2022-10-13 17:41:43 -0400

    Refactor indentation

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,18 +4,20 @@ require './lib/deck'
 require './lib/round'
 
 def start
-    # Creates object instances
-    cards = []
+  # Creates object instances
+  cards = []
     
-    cards << Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    cards << Card.new("The Viking spacecraftsent back to Earth photographs and reportsabout the surface of which planet?","Mars", :STEM)
-    cards << Card.new("Describe in words theexact direction that is 697.5° clockwisefrom due north?", "North north west", :STEM)
+  cards << Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  cards << Card.new("The Viking spacecraftsent back to Earth photographs and reportsabout the surface of which planet?","Mars", :STEM)
+  cards << Card.new("Describe in words theexact direction that is 697.5° clockwisefrom due north?", "North north west", :STEM)
 
-    deck = Deck.new(cards)
+  deck = Deck.new(cards)
     
-    Round.new(deck)
+  Round.new(deck)
 end
 
 round = start
-
+    
 puts "Welcome! You're playing with #{round.deck.count} cards."
+puts "-------------------------------------------------"
+puts "This is card number #{1 + round.turns.count} out of #{round.deck.count}"
