43d51750c04423a494e84509e01103ac3ad35fbe
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-13 13:19:33 -0500

    Add stores correct and incorrect tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -101,6 +101,33 @@ describe Round do
       expect(round.turns).to eq [new_turn]
     end
 
+    it 'saves correct guesses' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+
+      expect(round.correct).to include new_turn
+
+    end
+
+    it 'saves incorrect guesses' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Austin")
+
+      expect(round.correct).not_to include new_turn
+      expect(round.incorrect).to include new_turn
+      
+    end
+
   end
 
   describe '#number_correct' do
@@ -118,6 +145,8 @@ describe Round do
     end
 
   end
+
+  describe '#'
 
 
 
