6d0635124668cf617d47061876d7487e7500128e
Anhnhi Tran <anhtran811@gmail.com>
2022-10-12 18:44:39 -0600

    Add count test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -29,24 +29,44 @@ RSpec.describe Deck do
 
   it 'can be a deck of cards' do
 
-  card_1 = Card.new(
-    "What is the capital of Alaska?", 
-    "Juneau", 
-    :Geography)
-  card_2 = Card.new(
-    "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", 
-    "Mars", 
-    :STEM)
-  card_3 = Card.new(
-    "Describe in words the exact direction that is 697.5° clockwise from due north?", 
-    "North north west", 
-    :STEM)
+    card_1 = Card.new(
+      "What is the capital of Alaska?", 
+      "Juneau", 
+      :Geography)
+    card_2 = Card.new(
+      "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", 
+      "Mars", 
+      :STEM)
+    card_3 = Card.new(
+      "Describe in words the exact direction that is 697.5° clockwise from due north?", 
+      "North north west", 
+      :STEM)
 
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
 
-    expect(deck.cards).to eq(cards)
-  
+      expect(deck.cards).to eq(cards)
   end
-  
+
+  it 'can count the number of cards' do
+
+    card_1 = Card.new(
+      "What is the capital of Alaska?", 
+      "Juneau", 
+      :Geography)
+    card_2 = Card.new(
+      "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", 
+      "Mars", 
+      :STEM)
+    card_3 = Card.new(
+      "Describe in words the exact direction that is 697.5° clockwise from due north?", 
+      "North north west", 
+      :STEM)
+
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+
+      expect(deck.count).to be(3)
+  end
+
 end
