27e42eb79def1c67a57c684bd7e18c56788fd5ee
Alex Pitzel <pitzelalex@gmail.com>
2022-10-12 20:48:30 -0400

    Add different amounts of cards to #count test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -15,6 +15,7 @@ describe Deck do
             card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
             card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
             card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+            
             cards = [card_1, card_2, card_3]
             deck = Deck.new(cards)
 
@@ -23,15 +24,20 @@ describe Deck do
     end
 
     describe '#count' do
-        it 'counts number of cards in deck' do
+        it 'counts differnt amounts of cards in decks' do
 
             card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
             card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
             card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-            cards = [card_1, card_2, card_3]
-            deck = Deck.new(cards)
+            card_4 = Card.new("test", "test", :test)
+            
+            cards_1 = [card_1, card_2, card_3]
+            cards_2 = [card_1, card_2, card_3, card_3]
+            deck_1 = Deck.new(cards_1)
+            deck_2 = Deck.new(cards_2)
 
-            expect(deck.count).to eq(3)
+            expect(deck_1.count).to eq(3)
+            expect(deck_2.count).to eq(4)
         end 
     end
 end
