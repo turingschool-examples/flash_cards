61d1ae3939f6f896e1417a7a2e7ebc4df504eaea
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-10-13 15:47:52 -0700

    Cleanup

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Deck do
     expect(deck.count).to eq(3)
   end
 
-  it 'knows how many cards in each category' do
+  it 'knows the cards in each category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe Turn do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
+    expect(turn.card).to eq(card)
     expect(turn.card).to be_a(Card)
   end
 
