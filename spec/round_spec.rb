2c95a4aa68fc3aaaeb2e2428e031f80bb2733717
Alex Pitzel <pitzelalex@gmail.com>
2022-10-12 23:25:21 -0400

    Add #number_correct test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -123,4 +123,21 @@ describe Round do
             expect(round.turns).to eq([new_turn])
         end
     end
+
+    describe '#number_correct' do
+        it 'counts the number of correct answers in a round' do
+
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+
+            round = Round.new(deck)
+            new_turn = round.take_turn("Juneau")
+
+            expect(round.number_correct).to eq(1)
+        end
+    end
 end
