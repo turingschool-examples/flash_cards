724b6c8d12900e1ab1e217a6f72dcf07454506aa
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-12 23:34:15 -0600

    Add cards_in_category method and tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,12 +2,22 @@ class Deck
 
     attr_reader :cards
 
-    def initialize(cards)
+    def initialize(cards) #cards is an array of Card objects
         @cards = cards
     end
 
     def count
         @cards.size
     end
-    
+
+    def cards_in_category(specific_category)
+        arr = []
+        @cards.each do |card|
+            if card.category == specific_category
+                arr << card
+            end
+        end
+        return arr
+    end
+
 end
