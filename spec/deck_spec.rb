a3caf3bb73222ee20f0db2bbe4e8342b3280c9f3
KaraJH <karas.jones@yahoo.com>
2022-10-12 20:37:17 -0600

    Created test for count method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,12 +11,22 @@ RSpec.describe Deck do
     deck = Deck.new(cards)
     expect(deck).to be_instance_of(Deck)
   end
-  
+
   it 'has an array of cards' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     cards = [card_1, card_2]
     deck = Deck.new(cards)
-    expect(deck.card_deck).to eq(cards)
+    expect(deck.cards).to eq(cards)
+  end
+
+  describe '#count' do
+    it 'counts the number of cards in a deck' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      cards = [card_1, card_2]
+      deck = Deck.new(cards)
+      expect(deck.count).to eq(2)
+    end
   end
 end
