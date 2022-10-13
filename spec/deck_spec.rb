6a0b2d6f91678d36907ed20a7341d75f879d5d24
Alex Pitzel <pitzelalex@gmail.com>
2022-10-12 20:21:14 -0400

    Add initialize deck test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,14 @@
 require 'rspec'
 require './lib/turn'
 require './lib/card'
+
+describe Deck do
+    describe '#initialize' do
+        it 'is an instance of deck' do
+        
+            deck = Deck.new
+            expect(deck).to be_instance_of(Deck)
+        end
+        it 'has cards'
+    end
+end
