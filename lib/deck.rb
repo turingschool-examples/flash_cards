fdac65b62756dd5d25280201fa3361545f4f0dc8
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-10-12 17:26:20 -0700

    Add cards in category test and code

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,24 @@
+require './lib/card'
+
 class Deck
-attr_reader :cards
+  attr_reader :cards
   def initialize(cards)
     @cards = cards
   end
+
   def count
     @cards.count
+  end
+
+  def cards_in_category(cat)
+      cat_count = []
+
+     cards.each do |card|
+       if cat == card.category then
+         cat_count << card
+       end
+     end
+      cat_count
   end
 
 end
