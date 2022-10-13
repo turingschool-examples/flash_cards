edea4235628858007ebad462c771a2cb92ca0b3d
Alex Pitzel <pitzelalex@gmail.com>
2022-10-12 22:57:15 -0400

    Add 'stores the new turn in the round' test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -107,5 +107,20 @@ describe Round do
 
             expect(new_turn.correct?).to be true
         end
+
+        it 'stores the new turn in the round' do
+
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+
+            round = Round.new(deck)
+            new_turn = round.take_turn("Juneau")
+
+            expect(round.turns).to eq([card_1])
+        end
     end
 end
