c34d1b363d6cfdb29a4aff6419a8cdf6d2a6d48b
KaraJH <karas.jones@yahoo.com>
2022-10-12 21:11:04 -0600

    Working on cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,12 +2,29 @@
 #should also return cards based on given category
 
 class Deck
-  attr_reader :cards
+
+  attr_reader :card_obj
+
   def initialize(card_objects)
-    @cards = card_objects
+    @card_obj = card_objects
   end
 
   def count
-    @cards.count
+    @card_obj.count
   end
+
+  def cards_in_category(category)
+    if category == @question
+      cards
+    elsif category == @answer
+      cards
+    elsif category == @category
+      cards
+    end
+  end
+  #rspec
+  #expected: #<Card:0x00007fd51b8cbe58
+  #@question="What is the capital of Alaska?", @answer="Juneau",
+  # @category=:Geography>
+  #got: nil
 end
