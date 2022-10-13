9533144ad761f2797f0952a7adf1eba9928f7530
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-13 16:15:00 -0500

    Fix requires

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,4 +1,4 @@
-require './lib/card'
+
 require './lib/deck'
 require 'rspec'
 
@@ -11,7 +11,7 @@ describe Deck do
       expect(deck).to be_a Deck
     end
 
-    it 'has cards' do
+    it 'can have cards' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,6 +1,3 @@
-require './lib/card'
-require './lib/turn'
-require './lib/deck'
 require './lib/round'
 require 'rspec'
 
