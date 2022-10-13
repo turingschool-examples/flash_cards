f5aee116b5e31317dc724cb11df96e5255d9e503
Brady Rohrig <brady.rohrig@gmail.com>
2022-10-12 17:54:16 -0700

    Started round class, passed 2 tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,9 +2,9 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
-require 'pry'; binding.pry
 
-Rspec.describe Round do
+
+RSpec.describe Round do
     it 'exists' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -25,7 +25,7 @@ Rspec.describe Round do
         expect(round.deck).to eq(deck)
     end
 
-    it 'has the ability to tell what turn it is on' do
+    xit 'has the ability to tell what turn it is on' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -35,7 +35,7 @@ Rspec.describe Round do
         expect(round.turns).to eq([])
     end
 
-    it 'has the ability to tell what the current card is' do
+    xit 'has the ability to tell what the current card is' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -43,3 +43,19 @@ Rspec.describe Round do
         round = Round.new(deck)
 
         expect(round.current_card).to eq(card_1)
+    end
+
+    xit 'has the ability to take a new turn' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        turn = Turn.new("Juneau", card_1)
+
+        expect(round.take_turn("Juneau")).to eq(turn)
+    end
+
+    xit 'has method #new_turn which creates a new instance of the Turn class' do
+end
+end
