921cee64cef630021a24db4be885c0a54d12dbda
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-13 15:04:55 -0500

    Add take turns save multiple turns test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -101,6 +101,24 @@ describe Round do
       expect(round.turns).to eq [new_turn]
     end
 
+    it 'can save multiple turns' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      new_turn_1 = round.take_turn("Juneau")
+
+      expect(round.turns).to eq [new_turn_1]
+      expect(round.turns.count).to eq 1
+
+      new_turn_2 = round.take_turn("Juneau")
+
+      expect(round.turns).to eq [new_turn_1, new_turn_2]
+      expect(round.turns.count).to eq 2
+    end
+
     it 'saves correct guesses' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -140,7 +158,7 @@ describe Round do
       expect(round.current_card).to eq card_2
 
       new_turn_2 = round.take_turn("Saturn")
-      
+
       expect(round.current_card).to eq card_3
 
     end
