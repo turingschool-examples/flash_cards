cf5304cda880da64ff9337a91ac3252989b27d5e
Alex Pitzel <pitzelalex@gmail.com>
2022-10-13 16:38:59 -0400

    Add code to create object instances

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,3 +2,12 @@ require './lib/turn'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+
+# Creates object instances
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraftsent back to Earth photographs and reportsabout the surface of which planet?","Mars", :STEM)
+card_3 = Card.new("Describe in words theexact direction that is 697.5° clockwisefrom due north?", "North north west", :STEM)
+cards = [card_1, card_2, card_3]
+deck = Deck.new(cards)
+round = Round.new(deck)
+
