cdcb41eb17518b60835a6afb18b9300310761f0c
Drew Layton <dlayton66@gmail.com>
2022-10-12 22:18:14 -0600

    Fix Turn tests, initialize Turn class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,21 +3,24 @@ require './lib/turn'
 describe Turn do
 
     it 'exists' do
-      turn = Turn.new("Juneau",current_card)
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau",card)
 
       expect(turn).to be_a Turn
     end
 
     it 'has a guess' do
-      turn = Turn.new("Juneau",current_card)
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau",card)
 
       expect(turn.guess).to eq "Juneau"
     end
 
     it 'has a question card' do
-      turn = Turn.new("Juneau",current_card)
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau",card)
 
-      expect(turn.card).to eq current_card
+      expect(turn.card).to eq card
     end
 
     describe '#correct?' do
