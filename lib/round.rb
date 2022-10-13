ec3c1fb9f04a3b692c4d90aba99bf48f4c6b1586
Joe King <j.king92@gmail.com>
2022-10-13 12:08:49 -0700

    Add percent_correct_by_category method

--- lib/round.rb
+++ lib/round.rb
@@ -1,12 +1,15 @@
 class Round
-  attr_reader :deck, :turns, :number_correct
+  attr_reader :deck,
+    :turns,
+    :number_correct,
+    :correct_by_category
   # , :current_turn
 
   def initialize(deck)
     @deck = deck
     @turns = []
     @number_correct = 0
-    # @current_turn = nil
+    # @correct_by_category = []
   end
 
   def current_card
@@ -18,15 +21,35 @@ class Round
     if guess == @deck.cards[0].answer
       @number_correct += 1
     end
+    #put correct in an array
+
+    # if guess == @deck.cards[0].answer
+    #   @correct_by_category.each do |category|
+    #     if
+    #   end
+    # end
     @deck.cards.shift
     @turns << current_turn
 
     return current_turn
   end
 
+  def number_correct_by_category(category_correct)
+    this_category = []
+    @turns.each do |turn|
+      if turn.card.category == category_correct
+        if turn.guess == turn.card.answer
+          this_category << turn
+        end
+      end
+    end
+    this_category.length
+  end
+
   def percent_correct
     100.0 * @number_correct / @turns.length
   end
 end
+
 
 # require "pry"; binding.pry
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -44,6 +44,7 @@ describe Round do
       new_turn = round.take_turn("Juneau")
 
       expect(new_turn).to be_a Turn
+      expect(new_turn.guess == "Juneau").to eq true
     end
 
     it 'can confirm if a question is correct' do
@@ -113,6 +114,7 @@ describe Round do
       round = Round.new(deck)
 
       new_turn = round.take_turn("Juneau")
+      expect(round.turns.count).to eq 1
       turn2 = round.take_turn("Venus")
       expect(round.turns.count).to eq 2
     end
@@ -133,6 +135,21 @@ describe Round do
 
       expect(round.number_correct_by_category(:Geography)).to eq 1
     end
+
+    it 'tracks number correct in a diff given category category' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+      turn2 = round.take_turn("Venus")
+      # require "pry"; binding.pry
+      expect(round.number_correct_by_category(:STEM)).to eq 0
+    end
   end
 
   describe '#percent_correct' do
@@ -148,6 +165,22 @@ describe Round do
       new_turn = round.take_turn("Juneau")
       turn2 = round.take_turn("Venus")
       expect(round.percent_correct).to eq 50.0
+    end
+  end
+
+  describe '#percent_correct_by_category' do
+    it 'tracks percent of the correct answers in a given category' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+      turn2 = round.take_turn("Venus")
+      expect(round.percent_correct_by_category(:Geography)).to eq 100.0
     end
   end
 end
