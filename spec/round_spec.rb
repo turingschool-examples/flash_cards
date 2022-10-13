11e89eb8eaa90057dd2a94f6945649dc1787a973
Alex Pitzel <pitzelalex@gmail.com>
2022-10-12 22:19:38 -0400

    Add #take_turn test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -64,4 +64,20 @@ describe Round do
             expect(round.current_card).to eq(card_1)
         end
     end
+
+    describe '#take_turn'do
+        it 'creates a turn class instance' do
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+
+            round = Round.new(deck)
+            turn = Turn.new("Juneau", card_1)
+
+            expect(Round.take_turn).to eq(turn)
+        end
+    end
 end
