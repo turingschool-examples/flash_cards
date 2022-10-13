12284d3967a05ece97e81ec3199a143ac82ff49c
Samuel Cox <samc1253@gmail.com>
2022-10-12 19:09:24 -0600

    Add method to give the amount of correct answers in a specific category with corresponding test

--- lib/round.rb
+++ lib/round.rb
@@ -5,12 +5,14 @@ require './turn'
 require './deck'
 
 class Round
-    attr_reader :deck, :turns
+    attr_reader :deck, :turns, :number_correct, :turns_taken, :correct_cards
     def initialize(deck)
         @deck = deck
         @turns = []
         @number_correct = 0
         @turns_taken = 0
+        @correct_cards = []
+        @catogory_correct_amount = 0
     end
 
     def current_card
@@ -24,6 +26,7 @@ class Round
         if
             @turns.last.card.answer == @turns.last.guess
             @number_correct += 1
+            @correct_cards.push self.turns.last
         end
         @turns_taken += 1
         @deck.cards.shift
@@ -35,6 +38,17 @@ class Round
         @number_correct
     end
 
+    def number_correct_by_category(category)
 
+        @category_correct_amount = 0
+
+        @correct_cards.each do |correct_card|
+            if
+                correct_card.card.category == category
+                @category_correct_amount += 1
+            end
+        end
+        @category_correct_amount
+    end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -154,4 +154,31 @@ RSpec.describe Round do
         end
     end
 
+    describe '#number_correct_by_category' do
+        it 'returns the amount of questions answered correctly in a specific category' do
+
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+            cards = [card_1, card_2, card_3]
+
+            deck = Deck.new(cards)
+
+            round = Round.new(deck)
+
+            new_turn = round.take_turn("Juneau")
+
+            expect(round.number_correct_by_category(:Geography)).to eq 1
+
+            new_turn = round.take_turn("Mars")
+
+            new_turn = round.take_turn("North north west")
+
+            expect(round.number_correct_by_category(:STEM)).to eq 2
+        end
+    end
+
 end
