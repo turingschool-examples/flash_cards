83d11512535e20f579e8aa1b23a497fcc40b8c3c
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-14 14:55:34 -0600

    Add percent correct by category to round class

---
+++ flashcard_runner.rb
@@ -0,0 +1,4 @@
+puts Welcome! You're playing with 4 cards.
+-------------------------------------------------
+This is card number 1 out of 4.
+Question: What is 5 + 5?
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -97,7 +97,7 @@ RSpec.describe Round do
         expect(round.turns.last.feedback).to eq ("Incorrect.")
         expect(round.number_correct_by_category(:Geography)).to eq (1)
         expect(round.percent_correct).to eq (50.0)
-       
+        expect(round.percent_correct_by_category(:Geography)).to eq (100.0)
         expect(round.current_card).to eq (card_3)
     end
 end
