b533568df3c480eb91156931b168503df2bc938e
Elle Majors <hellolovely@ellemajors>
2022-10-14 08:42:45 -0600

    Returns a card test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe Turn do
     turn = Turn.new("Juneau", card)
 
     expect(turn.card).to eq(card)
+  end
+
+  xit 'returns the card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end  
 end
