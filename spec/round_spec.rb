aa418ac30d18f6836be645d90050a1b011605b05
Anhnhi Tran <anhtran811@gmail.com>
2022-10-13 20:24:34 -0600

    Add next turn with new card

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -155,11 +155,34 @@ RSpec.describe Round do
 
     expect(round.number_correct).to eq(1)
   end
+
+  it 'can be the next turn and have a new card' do
+    card_1 = Card.new(
+      'What is the capital of Alaska?',
+      'Juneau',
+      :Geography
+    )
+    card_2 = Card.new(
+      'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?',
+      'Mars',
+      :STEM
+    )
+    card_3 = Card.new(
+      'Describe in words the exact direction that is 697.5° clockwise from due north?',
+      'North north west',
+      :STEM
+    )
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    
+    new_turn_2 = round.take_turn("Venus")
+
+    expect(round.current_card).to eq(card_2)
+  end
 end
 
 
-# round.number_correct
-# #=> 1
 
 # round.current_card
 # #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
