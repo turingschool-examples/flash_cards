4333013172c07760646e0bfb01378636441ea52b
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-14 14:43:58 -0600

    Wrote test 9 for round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -180,13 +180,39 @@ RSpec.describe Round do
 
         new_turn.class
 
-        expect(new_turn.class)to eq(Turn)
+        expect(new_turn.class).to eq(Turn)
 
     end
 
-
 #Test 9
 #Checks that the guess for the new turn is correct
+
+    it "The guess in new_turn is correct" do
+
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        round.turns
+
+        round.current_card
+
+        new_turn = round.take_turn("Juneau")
+
+        new_turn.correct?
+
+        expect(new_turn.correct?).to eq(True)
+        require 'pry'; binding.pry
+    end
 
 #Test 10
 #Checks that the turns method can return the turn instance for the round
