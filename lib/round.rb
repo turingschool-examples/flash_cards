47b6428d75c948487188876d767598a2c5aa34ae
Alastair Lloyd <dex8056@aol.com>
2022-10-14 06:54:18 -0600

    Make changes to take turn class

--- lib/round.rb
+++ lib/round.rb
@@ -1,34 +1,22 @@
 class Round
+    attr_reader :deck, :turns, :number_correct
 
-  attr_reader         :card,
-                      :deck,
-                      :turns
+    def initialize(deck)
+        @deck = deck
+        @turns = []
+        @number_correct = 0
+    end
 
+    def current_card
+        current_card = @deck.cards
+        current_card[turns.count]
+    end
 
-  def initialize(deck)
-   @deck             = deck
-   @turns            = []
-   @current_card     = 0
-   @number_correct   = 0
-   @number_guesses   = 0
-  end
-
-
-  def current_card
-    deck.cards[@current_card]
-  end
-
-  def turns (turn)
-    @new_turn = Turn.new(guess, current_card)
-    @turns << @new_turn
-    @new_turn
-  end
-
-  def take_turn
-    @new_turn.correct? @number_correct += 1
-    deck.cards << deck.cards[@current_card] && @total_cards += 1
-    @current_card    += 1
-    @number_guesses  += 1
-  end
-
-end
+    def take_turn(guess)
+        new_turn = Turn.new(guess, current_card)
+        @turns << new_turn
+        if new_turn.correct?
+            @number_correct += 1
+        end
+        new_turn
+    end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -26,4 +26,15 @@ RSpec.describe Round do
     expect(round.current_card).to be(card_1)
   end
 
+  it 'records the turn' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    expect(round.take_turn("Juneau")).to change(round.turns.count).by(1)
+  end
+
+
 end
