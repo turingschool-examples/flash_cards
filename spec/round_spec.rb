b0d8c63b42fde73d7ba3bbea87755d4b53a300a8
KaraJH <karas.jones@yahoo.com>
2022-10-14 16:22:54 -0600

    started percent_correct test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -90,5 +90,18 @@ RSpec.describe Round do
 
     end
   end
-
+  describe '#percent_correct' do
+    it ' calculates the percent of correct answers' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
+      round = Round.new(deck)
+      new_turn = round.take_turn("Juneau")
+      expect(new_turn.correct?).to eq(true)
+      expect(round.number_correct).to eq(1)
+      expect(round.percent_correct).to eq(100.00)
+    end
+  end
 end
