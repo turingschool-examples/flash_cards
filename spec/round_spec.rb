1b4c080fb11794439f630f0b8aba9f6b8ca81cc9
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-14 14:40:40 -0600

    Wrote test 8 for round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -155,9 +155,35 @@ RSpec.describe Round do
 
     end
 
-
 #Test 8
-#Checks that new_turn is an instance of Turn class
+#Checks that new_turn is an instance of Turn class with the class method
+
+    it "new_turn.class returns Turn" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        round.turns
+
+        round.current_card
+
+        new_turn = round.take_turn("Juneau")
+
+        new_turn.class
+
+        expect(new_turn.class)to eq(Turn)
+
+    end
+
 
 #Test 9
 #Checks that the guess for the new turn is correct
