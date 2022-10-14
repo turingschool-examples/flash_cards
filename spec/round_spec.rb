911baf86fc3b43842f344809697b991b4ead0b8d
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-14 13:28:48 -0600

    Wrote test 4 for round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -56,10 +56,29 @@ RSpec.describe Round do
         round = Round.new(deck)
 
         expect(round).to be_instance_of(Round)
+
     end
 
 #Test 4
 #Checks that round.deck calls the instance deck
+    it "deck exists within round" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        expect(round.deck).to eq(deck)
+        require 'pry'; binding.pry
+    end
+
 
 #Test 5
 #Checks that turns is a method that pulls up a blank turn?
