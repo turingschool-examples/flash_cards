b4a4c486c67ab7c389a03ff38defdd0f27275429
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-13 20:26:11 -0700

    Add work on take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -4,9 +4,27 @@ class Round
         @deck = deck
     end
 
-    def take_turn(guess_string)
-        @turn = Turn.new(guess_string, card)
+    # def take_turn(guess_string)
+    #     @turn = Turn.new(guess_string, card)
+    # end
+
+    def turns
+        turns = []
     end
+
+    def current_card
+        deck.cards[0]
+    end
+
+    def take_turn(guess)
+        # require 'pry'; binding.pry
+        turn = Turn.new(deck.cards[0], guess)
+        turns << turn
+    
+    end
+
+
+
     # The take_turn method takes a string representing the guess. 
     # take_turn method should create a new Turn object with the appropriate guess and Card. 
     # take_turn method should store this new Turn, as well as return the new Turn from the take_turn method. 
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -23,12 +23,47 @@ RSpec.describe Round do
         round = Round.new(deck)
 
         expect(round.deck).to eq(deck)
+    end
+
+
+    # it can store and call turns
+    it 'stores and calls turns' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        
+        expect(round.turns).to eq([])
+
+    end
+
+    # it can store the current card
+    it 'stores the current card' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        expect(round.current_card).to eq(card_1)
 
 
     end
-    # it can store and call turns
-    # it can store the current card
+
     # it can accept an answer and go to the next card
+    it 'takes an answer argument and calls the next card' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        round.take_turn("Juneau")
+
+        expect(round.take_turn("Juneau")).to eq(Round.turns)
+
+    end
+
     # it is an instance of the Turn class
     # it can store if correct
     # it can recall turns
