f29beb9255765be90107df9222d5da5317d0975d
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-14 07:19:04 -0700

    Make a test for the turns method for the round class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -37,4 +37,20 @@ end
     end
   end
 
+  describe '#turns method' do
+    it 'will tell you show you the turns you have completed' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+
+      deck = Deck.new([card_1, card_2, card_3])
+
+      round = Round.new(deck)
+
+      expect(round.turns).to eq([])
+
+    end
+  end
+
 end
