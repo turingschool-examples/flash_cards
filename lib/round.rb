c0760972436f0716dc7c0895f5f43b345b19970e
KaraJH <karas.jones@yahoo.com>
2022-10-14 15:02:32 -0600

    Added number by category method

--- lib/round.rb
+++ lib/round.rb
@@ -11,20 +11,37 @@ class Round
   end
 
   def current_card
-
     @deck.cards.shift
   end
 
   def take_turn(guess)
-
-    turn = Turn.new(guess, current_card)
+    #turn = Turn.new(guess, current_card)
+    turn = Turn.new(guess, deck.cards[0])
     @turns << turn
       if turn.correct? == true
         @number_correct += 1
       end
     return turn
 
+    def number_correct_by_category(category)
+      @turns.each do |card_turn|
+        if category == card_turn.category
+          @number_correct
+        end
+      end
+      @number_correct
+    end
   end
 
+  #def number_correct_by_category(category)
+    #correct_in_category = 0
+  #  @turns.each do |card_turn|
+  #    if category == card_turn.category
+        #correct_in_category += 1
+  #      @number_correct
+  #    end
+  #  end
+    #return correct_in_category
+  #end
 
 end
