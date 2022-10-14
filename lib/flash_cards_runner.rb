2e0ace17fc209f40722c4f4e897c138f97bf97b8
Samuel Cox <samc1253@gmail.com>
2022-10-13 20:26:29 -0600

    Allow game to run properly

--- lib/flash_cards_runner.rb
+++ lib/flash_cards_runner.rb
@@ -16,24 +16,44 @@ card_4 = Card.new('Who was the first European to set foot in North America?', 'L
 
 card_5 = Card.new('Who has won the most Formula 1 Grands Prix?', 'Lewis Hamilton', :Sports)
 
-deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+cards = [card_1, card_2, card_3, card_4, card_5]
+
+deck = Deck.new(cards)
 
 $round = Round.new(deck)
 
 $round_count = $round.deck.count
 
 def start
+  @categories_played = []
   puts "Welcome! You are playing with #{$round_count} cards."
   puts '--------------------------------------------------'
   $round_count.times do
     puts "This is card number #{$round.card_number} out of #{$round_count}."
     puts "Question: #{$round.current_card.question}"
+
+    unless @categories_played.include?($round.current_card.category)
+
+      @categories_played << $round.current_card.category
+    end
+    
     $round.take_turn(gets.chomp)
     puts $round.turns.last.feedback
+
+
   end
   puts '****** Game over! ******'
-  puts "You had #{$round.number_correct} guesses out of #{$round_count.to_i} for a total score of #{$round.percent_correct.to_i}%."
-  puts
+  puts "You had #{$round.number_correct.to_i} guesses out of #{$round_count.to_i} for a total score of #{$round.percent_correct.to_i}%."
+
+
+  
+  @categories_played.each do |category_played|
+    puts "#{category_played} - #{$round.percent_correct_by_category(category_played)}% correct."
+  end
+
 end
 
+
 start
+
+
