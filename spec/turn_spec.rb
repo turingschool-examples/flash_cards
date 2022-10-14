1191249939fa8815281dbc53045cea1d34c9e8d6
Elle Majors <hellolovely@ellemajors>
2022-10-14 13:57:44 -0600

    Test feedback

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,35 +1,42 @@
-require './lib/turn'
+require 'rspec'
 require './lib/card'
-
+require './lib/turn'
 
 RSpec.describe Turn do
   describe '#initialize' do
-    xit 'has two arguments'
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+    it 'has two arguments'do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      turn = Turn.new("Juneau", card)
 
-    expect(turn).to be_instance_of(Turn)
+      expect(turn).to be_instance_of(Turn)
+    end
   end
 
-  xit 'returns a guess' do
+  it 'returns a guess' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.card).to eq(card)
   end
 
-  xit 'returns the card' do
+  it 'returns the card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn.card).to eq(card)
   end
 
-  xit 'has a true answer' do
+  it 'has a true answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-
+# require 'pry'; binding.pry
     expect(turn.correct?).to be true
   end
 
+  it 'provides feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
 end
