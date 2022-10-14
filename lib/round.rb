6dba8df852236afd99ee2f0c02d2471ac824b5b6
Alastair Lloyd <dex8056@aol.com>
2022-10-14 08:54:02 -0600

    Correct by category method

--- lib/round.rb
+++ lib/round.rb
@@ -1,10 +1,14 @@
 class Round
-    attr_reader :deck, :turns, :number_correct
+    attr_reader :deck,
+                :turns,
+                :number_correct,
+                :correct_by_cat
 
     def initialize(deck)
         @deck = deck
         @turns = []
         @number_correct = 0
+        @correct_by_cat = 0
     end
 
     def current_card
@@ -20,3 +24,21 @@ class Round
         end
         new_turn
     end
+
+    def number_correct_by_category (category)
+      group = []
+      correct_by_cat = 0
+      turns.each do |turn|
+        if turn.card.category == category
+          group << turn
+        end
+      end
+
+      group.each do |card|
+        if card.correct?
+          correct_by_cat += 1
+        end
+      end
+      correct_by_cat
+end
+end
