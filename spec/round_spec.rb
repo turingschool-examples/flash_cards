8e54225e77e9c7172bfcd8b45bb168022e043885
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-14 15:50:16 -0600

    Wrote Test 12 in round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -275,6 +275,32 @@ RSpec.describe Round do
 #Test 12
 #Checks that the current card has changed to the next one"
 
+    it "Current card has been updated to the next one via the take turn method" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        round.turns
+
+        round.current_card
+
+        new_turn = round.take_turn("Juneau")
+
+        round.current_card
+
+        expect(round.current_card).to eq(card_2)
+
+    end
+
 #Test 13
 #Takes another turn and guesses venus"
 
