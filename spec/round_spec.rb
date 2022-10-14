cd9aeb5beb66371b8c859172194113543ef951ae
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-14 14:51:38 -0600

    Wrote Test 10 for round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -210,12 +210,39 @@ RSpec.describe Round do
 
         new_turn.correct?
 
-        expect(new_turn.correct?).to eq(True)
-        require 'pry'; binding.pry
+        expect(new_turn.correct?).to eq(true)
+
     end
 
 #Test 10
-#Checks that the turns method can return the turn instance for the round
+#Checks that the turns method can contains the turns taken
+
+    it "When a new turn is made the turn also needs to be added in the round's @turns" do
+
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        round.turns
+
+        round.current_card
+
+        new_turn = round.take_turn("Juneau")
+
+        round.turns
+
+        expect(round.turns).to eq(turns)
+
+    end
 
 #Test 11
 #Checks that numer correct method can share the number of correct answers in the round
