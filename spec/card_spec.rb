695f81dbe73fa2f035976c2845196f1f88dae08b
Alex Pitzel <pitzelalex@gmail.com>
2022-10-13 21:25:36 -0400

    Refactor card_spec.rb

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,28 +1,32 @@
+require 'rspec'
 require './lib/card'
 
-RSpec.describe Card do
-  it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    expect(card).to be_instance_of(Card)
-  end
+describe Card do
+  describe '#initialize' do
+    it 'exists' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-  it 'has a question' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      expect(card).to be_instance_of(Card)
+    end
 
-    expect(card.question).to eq("What is the capital of Alaska?")
-  end
+    it 'has a question' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-  it 'has an answer' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      expect(card.question).to eq("What is the capital of Alaska?")
+    end
 
-    expect(card.answer).to eq("Juneau")
-  end
+    it 'has an answer' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-  it 'has a category' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      expect(card.answer).to eq("Juneau")
+    end
 
-    expect(card.category).to eq(:Geography)
+    it 'has a category' do
+      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+      expect(card.category).to eq(:Geography)
+    end
   end
   
   it 'can handle different arguments' do
