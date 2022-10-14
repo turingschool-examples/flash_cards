027da46791fa696c3c119e0012a30e39a2705426
Alex Pitzel <pitzelalex@gmail.com>
2022-10-13 20:38:21 -0400

    Add #play_round, #play_turn and #results methods

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -8,8 +8,10 @@ def start
   cards = []
     
   cards << Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  cards << Card.new("The Viking spacecraftsent back to Earth photographs and reportsabout the surface of which planet?","Mars", :STEM)
-  cards << Card.new("Describe in words theexact direction that is 697.5° clockwisefrom due north?", "North north west", :STEM)
+  cards << Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?","Mars", :STEM)
+  cards << Card.new("Describe in words the exact direction that is 697.5° clockwisefrom due north?", "North north west", :STEM)
+  cards << Card.new("Who won the 2021 F1 Drivers Championship?", "Max Verstappen", :Sports)
+  cards << Card.new("What is a group of owls called", "A Parlement", :Animals)
 
   deck = Deck.new(cards)
     
@@ -17,11 +19,35 @@ def start
 end
 
 def play_round
-  round = start
+  @round = start
+  @categories_played = []
+  puts "Welcome! You're playing with #{@round.deck.count} cards."
 
-  puts "Welcome! You're playing with #{round.deck.count} cards."
-  puts "-------------------------------------------------"
-  puts "This is card number #{1 + round.turns.count} out of #{round.deck.count}"
-  puts "Question: #{round.current_card.question}"
+  @round.deck.count.times do
+    def play_turn
+      puts "-------------------------------------------------"
+      puts "This is card number #{1 + @round.turns.count} out of #{@round.deck.count}"
+      puts "Question: #{@round.current_card.question}"
+      
+      unless @categories_played.include?(@round.current_card.category)
+        @categories_played << @round.current_card.category
+      end
+
+      @round.take_turn(gets.chomp)
+      puts @round.turns.last.feedback
+    end
+    play_turn
+  end
+  
+  def results
+    puts "****** Game over! ******"
+    puts "You had #{@round.number_correct} correct guesses out of #{@round.deck.count} for a total score of #{@round.percent_correct.to_i}%"
+
+    @categories_played.each do |category_played|
+      puts "#{category_played} - #{@round.percent_correct_by_category(category_played).to_i}%"
+    end
+  end
+
+  results
 end
 play_round
