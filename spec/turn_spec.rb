8b312d2f7d455641791c75e93575620085ceac59
Elle Majors <hellolovely@ellemajors>
2022-10-14 08:30:58 -0600

    Test returns guess

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,10 +4,16 @@ require './lib/card'
 
 RSpec.describe Turn do
   describe '#initialize' do
-    it 'has two arguments'
+    xit 'has two arguments'
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn).to be_instance_of(Turn)
   end
+
+  xit 'returns a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
 end
