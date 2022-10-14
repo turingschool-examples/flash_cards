deade7d044b31d9993932b9e5aad8661ea8a19ff
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-14 15:36:04 -0600

    Create runner welcome text

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,4 +1,12 @@
-puts Welcome! You're playing with 4 cards.
--------------------------------------------------
-This is card number 1 out of 4.
-Question: What is 5 + 5?
+class Start
+
+  p "Welcome to Flashcards!"
+  p "Please enter your name"
+  name = gets.chomp
+
+  p "Welcome '#{name}' ! You're playing with 4 cards."
+  p "-------------------------------------------------"
+  p "This is card number 1 out of 4."
+  p "Question: What is 5 + 5?"
+  guess = gets.chomp
+end
