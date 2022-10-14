2e84e9b80500c7dfe356f1120e6d974fb608e026
Anhnhi Tran <anhtran811@gmail.com>
2022-10-13 19:59:15 -0600

    Add number correct test

--- lib/round.rb
+++ lib/round.rb
@@ -11,26 +11,20 @@ class Round
   end
 
   def take_turn(guess)
-    turn = Turn.new(guess, current_card) 
-    @turns << turn
+    new_turn = Turn.new(guess, current_card)
+    @turns << new_turn
     @deck.cards.shift
-    turn
+    new_turn
   end
 
-# take_turn - take an guess(string)
-# create new TURN OBJECT with guess and current_card
-# store this new TURN OBJECT (shovel into array)
-# and new TURN OBJET from the take_turn method (final return)
-# when take_turn method is called, round should move onto next card in deck (aka new current card)
-
-
-# method that returns the number of correct guesses
-# the method is called number_correct
-# there are no parameters
-# it returns an integer
-# i have an array of turns
-# in the turn object i can call correct? which will return a boolean
-# count the number of trues
-# hint: there is an enumerable that can count he number of trues
+  def number_correct
+    correct_answer_count = 0
+    turns.each do |turn|
+      if turn.correct? == true
+        correct_answer_count += 1
+      end
+    end
+    correct_answer_count
+  end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -124,28 +124,39 @@ RSpec.describe Round do
 
     expect(round.current_card).to eq(card_1)
 
-    turn = round.take_turn("Juneau")
+    new_turn = round.take_turn("Juneau")
 
-    expect(turn.class).to eq(Turn)
-    expect(round.turns).to eq([turn])
+    expect(new_turn.class).to eq(Turn)
+    expect(new_turn.correct?).to be true
+    expect(round.turns).to eq([new_turn])
     expect(round.current_card).to eq(card_2)
   end
 
+  it 'has a correct number of guesses' do
+    card_1 = Card.new(
+      'What is the capital of Alaska?',
+      'Juneau',
+      :Geography
+    )
+    card_2 = Card.new(
+      'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?',
+      'Mars',
+      :STEM
+    )
+    card_3 = Card.new(
+      'Describe in words the exact direction that is 697.5° clockwise from due north?',
+      'North north west',
+      :STEM
+    )
+
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    expect(round.number_correct).to eq(1)
+  end
 end
 
-
-
-# new_turn = round.take_turn('Juneau')
-# #=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
-
-# new_turn.class
-# #=> Turn
-
-# new_turn.correct?
-# #=> true
-
-# round.turns
-# #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
 
 # round.number_correct
 # #=> 1
