5fa9a98f0dd3e98093d3104064dcec8b65319edf
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-13 19:16:42 -0600

    Add take turn

--- lib/round.rb
+++ lib/round.rb
@@ -1,19 +1,22 @@
 require './lib/turn'
+require './lib/card'
 
 class Round
     attr_reader :deck, 
                 :turns
 
-    def initialize(deck)
+    def initialize(deck, turns = [])
         @deck = deck
-        @turns = []
+        @turns = turns
     end
 
     def current_card
-        @deck.cards[0]
+        @deck.cards[@turns.length]
     end
 
     def take_turn(guess)
         turn = Turn.new(guess, current_card)
+        @turns << turn
+        turn
     end
 end
