06cd743b1a725984d913b64f0a99daa9637d4f59
Elle Majors <hellolovely@ellemajors>
2022-10-14 13:58:40 -0600

    Add feedback method

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,12 +2,19 @@ class Turn
   attr_reader :guess,
               :card
 
-  def initialize(string, card)
-    @guess = string
+  def initialize(guess, card)
+    @guess = guess
     @card = card
   end
 
   def correct?
-    @guess == @card
+    @guess == @card.answer
+  end
+
+  def feedback
+    if @guess == true
+      puts "Correct!"
+    else puts "Incorrect!"
+    end
   end
 end
