f3e39bafad9863f6b8e98733844101a992c0490c
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-13 20:18:57 -0600

    Wrote Test 7

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -110,11 +110,31 @@ RSpec.describe Deck do
       
         expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
 
-
     end
 
 #Test 7
 #Can call the cards in category :Geography
+    it "can call the cards with category :Geography" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    
+        cards_1 = []
+        cards_1.push card_1, card_2, card_3
+
+        deck = Deck.new(cards_1)
+
+
+        deck.cards_in_category(:Geography)
+      
+        expect(deck.cards_in_category(:STEM)).to eq([card_1])
+
+    end
+
+
+
 
 #Test 8
 #Can call the cards in category "Pop Culture"
