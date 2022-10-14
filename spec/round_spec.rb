c3b163e489914797f4619d576c6ee80d364c49fc
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-14 12:19:20 -0700

    Make the test for the take_turn method in the Round class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -69,8 +69,26 @@ end
 
     expect(round.current_card).to eq(card_1)
 
+    end
+
   end
 
+  describe '#take_turn()' do
+    it 'will take a turn guessing what the answer is' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      turn_1 = Turn.new("Juneau", card_1)
+
+      deck = Deck.new([card_1, card_2, card_3])
+
+      round = Round.new(deck)
+
+      new_turn = round.take_turn("Juneau")
+
+      expect(round.take_turn("Juneau")).to eq()
+    end
   end
 
 
