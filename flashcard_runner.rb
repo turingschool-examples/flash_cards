a2ce75cd8913416b25a13c71b8edb6a0a94f2507
Thomas Casady <tscasady@gmail.com>
2022-10-14 17:59:22 -0500

    Replace initial_deck_size variable created in runner with deck.initial_size

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -9,16 +9,16 @@ require './lib/card_generator'
 cards = CardGenerator.new("cards.txt").cards
 deck = Deck.new(cards)
 round = Round.new(deck)
-initial_deck_size = round.deck.count
+
 #move method to round/deck class / refactor whole process
 categories = deck.cards.map {|card| card.category}.uniq
 
-puts "Welcome! You're playing with #{initial_deck_size} cards."
+puts "Welcome! You're playing with #{round.deck.initial_size} cards."
 puts "-------------------------------------------------"
 
 
 until round.deck.count == 0 do
-  puts "This is card number #{round.turns.length + 1} out of #{initial_deck_size}."
+  puts "This is card number #{round.turns.length + 1} out of #{round.deck.initial_size}."
   puts "Question: #{round.current_card.question}"
   guess = gets.chomp
   round.take_turn(guess)
@@ -26,5 +26,5 @@ until round.deck.count == 0 do
 end
 
 puts "****** Game over! ******"
-puts "You had #{round.number_correct} correct guesses out of #{initial_deck_size} for a total score of #{round.percent_correct}."
+puts "You had #{round.number_correct} correct guesses out of #{round.deck.initial_size} for a total score of #{round.percent_correct}."
 categories.each{|category| puts "#{category.to_s} - #{round.percent_correct_by_category(category)}% correct"}
