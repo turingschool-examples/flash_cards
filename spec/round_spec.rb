ec4e494e07e4844430463abf604328616ee34597
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-10-14 11:38:59 -0700

    Add turns taken test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -76,9 +76,24 @@ RSpec.describe Round do
 
     expect(new_turn.correct?).to eq(true)
 
-    new_turn = round.take_turn('bob')
+    new_turn1 = round.take_turn('bob')
 
-    expect(new_turn.correct?).to eq(false)
+    expect(new_turn1.correct?).to eq(false)
+  end
+
+  it 'stores turns taken' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
+
+    expect(round.turns).to eq([new_turn])
+  end
+
+  it 'stores number_correct' do
+    
   end
 
 end
