61b0d451c7a385866c114c97a6d56ccfef67c65f
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-14 16:04:51 -0700

    Make take_turns method work properly and pass it's test

--- lib/round.rb
+++ lib/round.rb
@@ -3,20 +3,29 @@ require './lib/turn'
 require './lib/deck'
 
 class Round
-  attr_reader :deck, :turns
+  attr_reader :deck, :turns, :number_correct
 
   def initialize(deck)
     @deck = deck
     @turns = []
+    @number_correct = 0
+
   end
 
   def current_card
-    deck.cards[0]
+    deck.cards[0] #When looking or "reading" you don't need @
   end
 
   def take_turn(guess)
-    new_turn = Turn.new(guess, deck.cards[0])
+    new_turn = Turn.new(guess, current_card)
+    turns << new_turn
+    deck.cards.shift
+    if new_turn.correct?
+      @number_correct += 1 #When modifying, you must have an @
+    end
     return new_turn
+
+
 
   end
 
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -81,7 +81,7 @@ end
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    
+
 
       deck = Deck.new([card_1, card_2, card_3])
 
@@ -89,9 +89,18 @@ end
 
       new_turn = round.take_turn("Juneau")
 
+
       expect(new_turn).to be_instance_of(Turn)
+
+
     end
   end
+
+  describe ''
+
+
+
+
 
 
 end
