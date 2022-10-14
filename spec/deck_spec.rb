3c3977f00d62f7a11ef3d0ad757bae83106f9d58
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-13 18:36:42 -0700

    Add a test for the .cards_in_category method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -70,6 +70,21 @@ end
 
   end
 
+  describe '#cards_in_category' do
+
+    it 'will call forth all the cards in any category' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      cards = [card_1, card_2, card_3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.cards_in_category(:Geography)).to eq([card_1])
+    end
+  end
+
 
 
 
