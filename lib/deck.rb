d59eca223dddb2b4b65d98a171c3f63d9b5fbba4
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-10-14 18:11:02 -0500

    Update deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,23 @@
+require './lib/card'
+
 class Deck
-  def initialize(card)
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(cat)
+    thing = []
+    @cards.each do |card|
+      if card.category == cat
+        thing << card
+      end
+    end
+    thing
   end
 end
