89a2c56c327f347f30676cfb115f0c3069112ffa
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-13 19:28:49 -0500

    Add percent correct by category test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -261,7 +261,7 @@ describe Round do
   end
 
   describe '#percent_correct_by_category' do
-    xit 'returns the percent correct in a category' do
+    it 'returns the percent correct in a category' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -276,12 +276,15 @@ describe Round do
 
       expect(deck.cards_in_category(:Geography).length).to eq 1
       expect(round.number_correct_by_category(:Geography)).to eq 1
+      expect(percent_correct_by_category(:Geography)).to eq 100.0
 
       expect(deck.cards_in_category(:STEM).length).to eq 2
       expect(round.number_correct_by_category(:STEM)).to eq 1
+      expect(percent_correct_by_category(:STEM)).to eq 50.0
 
       expect(deck.cards_in_category(:Music).length).to eq 1
       expect(round.number_correct_by_category(:Music)).to eq 0
+      expect(percent_correct_by_category(:Music)).to eq 0.0
 
     end
 
