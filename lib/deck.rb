cad6694882c87b4a786d5a6258af3683ff4a6b77
Issa Abdel Mageed <imageed95@gmail.com>
2022-10-13 20:02:11 -0600

    work on deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,18 @@
 class Deck
-attr_reader :guess, :correct?, :card
+  attr_reader :guess, :correct?, :card
+def initialize (cards)
+  @card = cards
+  @count = cards.length
+end
 
-@deck = deck
-
+def cards_category(category)
+  category_collection = []
+  @cards.each do |card|
+    if card.category == category
+      category_collection << card
+    end
+  end
+  
 
 
 
