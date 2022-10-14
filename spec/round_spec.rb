481f5e178d6e082a75d674b3ca7759f804f2425d
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-14 13:36:17 -0600

    Wrote Test 5 in round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -60,7 +60,7 @@ RSpec.describe Round do
     end
 
 #Test 4
-#Checks that round.deck calls the instance deck
+#Checks that round.active_deck calls the instance deck
     it "deck exists within round" do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
@@ -75,13 +75,35 @@ RSpec.describe Round do
 
         round = Round.new(deck)
 
-        expect(round.deck).to eq(deck)
-        require 'pry'; binding.pry
+        expect(round.active_deck).to eq(deck)
+
     end
 
 
 #Test 5
-#Checks that turns is a method that pulls up a blank turn?
+#Checks that round has an attribute, turns with an empty array
+    it "round has attribute turns that is an empty array" do
+
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        round.turns
+
+        expect(round.turns)to eq([])
+        
+    end
+
+
 
 #Test 6
 #Checks that method current card pulls up the first card we make
