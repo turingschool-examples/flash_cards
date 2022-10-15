21dfafe7d11493301640b38c2472d0cd68d2ae3c
Max MacGillivray <mary.macgillivray@gmail.com>
2022-10-15 15:12:06 -0700

    Fix an error in deck.rb

---
+++ lib/deck.rb
@@ -0,0 +1,18 @@
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+  
+  def count
+    self.cards.count
+  end
+  
+  def cards_in_category(category)
+    cat = []
+    @cards.each do |card|
+      cat << card if card.category == category
+    end
+  cat
+  end
+end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,20 +2,21 @@ require 'pry'
 require './lib/card'
 
 RSpec.describe Card do
-    let (:card1) {Card.new("What is the capital of Alaska?", "Juneau", :Geography)}
+  let (:card_1) {Card.new("What is the capital of Alaska?", "Juneau", :Geography)}
+    
   it 'exists' do
-    expect(card1).to be_instance_of(Card)
+    expect(card_1).to be_instance_of(Card)
   end
 
   it 'has a question' do
-    expect(card1.question).to eq("What is the capital of Alaska?")
+    expect(card_1.question).to eq("What is the capital of Alaska?")
   end
 
   it 'has an answer' do
-    expect(card1.answer).to eq("Juneau")
+    expect(card_1.answer).to eq("Juneau")
   end
 
   it 'has a category' do
-    expect(card1.category).to eq(:Geography)
+    expect(card_1.category).to eq(:Geography)
   end
 end
