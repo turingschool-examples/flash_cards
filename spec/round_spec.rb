4ad38e5d38d7ce3d9719c537b9755712bb01bfa4
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-14 22:10:00 -0600

    Wrote test 18 for round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -461,6 +461,35 @@ RSpec.describe Round do
 #Test 18
 #Checks the number of correct answers in the round by category, STEM"
 
+    it "what are the number of correct answers in the round by category STEM" do
+
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        round.turns
+
+        round.current_card
+
+        new_turn = round.take_turn("Juneau")
+
+        new_turn2 = round.take_turn("Venus")
+
+        round.number_correct_by_category(:STEM)
+
+        expect(round.number_correct_by_category(:STEM)).to eq(0)
+
+    end
+
 #Test 19
 #Checks the percentage of correct answers
 
