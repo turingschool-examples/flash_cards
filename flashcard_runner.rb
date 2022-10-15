8cf710046a5ad95df24ba0d3f9d47a0b393bd624
beddings81 <111210465+beddings81@users.noreply.github.com>
2022-10-15 18:38:33 -0500

    Add start method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
@@ -5,13 +6,34 @@ require './lib/round'
 
 
 
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  deck = Deck.new([card_1, card_2, card_3,])
-  round = Round.new(deck)
 
 
-  def start
-    p "Welcome! You're playing with 4 cards"
+class Game
+  attr_reader :card_1, :card_2, :card_3, :card_4, :cards, :deck, :round
+
+  def initialize
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @card_4 = Card.new("What is 5 + 5?", "10", :Math)
+    @cards = [card_1, card_2, card_3, card_4]
+    @deck = Deck.new(cards)
+    @round =Round.new(deck)
   end
+
+
+  def start(card)
+    p "Welcome! You're playing with #{deck.count} cards."
+    p "-----------------------------------------------"
+    #p "This is card number #{cards.count} out of 4."
+    while round.turns.length < 4
+      p "Question: #{round.current_card.question}"
+      guess = gets.chomp
+      round.take_turn(guess)
+      p round.turns.last.feedback
+    end
+  end
+end
+
+new_game = Game.new
+new_game.start(:card_1)
