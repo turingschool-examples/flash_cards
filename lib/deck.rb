ce201fa1bda47e0369877cd0ff3016744a98ada2
Thomas Casady <tscasady@gmail.com>
2022-10-15 12:43:28 -0500

    Feat: add categories and category_data to Deck class.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,9 +1,10 @@
 class Deck
-    attr_reader :cards, :initial_size
+    attr_reader :cards, :initial_size, :category_data
 
     def initialize(cards)
         @cards = cards
         @initial_size = count
+        @category_data = {}
     end
 
     def count
@@ -16,6 +17,10 @@ class Deck
 
     def change_card
         @cards.shift
+    end
+
+    def categories
+        @cards.map {|card| card.category}.uniq
     end
 
     def cards_in_category(category)
