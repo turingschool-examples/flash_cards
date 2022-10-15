accb3d88f037654c4fb4d608867e82ac7542a0af
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-14 22:57:16 -0700

    Debug

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -13,15 +13,15 @@ deck_size = deck.cards.length
 
 round.start
 
-new_turn = round.take_turn(gets.strip)
+new_turn = round.take_turn(gets.strip.downcase)
 puts new_turn.feedback
 puts "This is card number 2 out of #{deck_size}."
 puts "#{deck.cards[0].question}"
-new_turn = round.take_turn(gets.strip)
+new_turn = round.take_turn(gets.strip.downcase)
 puts new_turn.feedback
 puts "This is card number 3 out of #{deck_size}."
 puts "#{deck.cards[0].question}"
-new_turn = round.take_turn(gets.strip)
+new_turn = round.take_turn(gets.strip.downcase)
 puts new_turn.feedback
 
 round.complete
--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,7 @@ class Turn
   end
 
   def correct?
-    @guess == card.answer
+    @guess == card.answer.downcase
   end
 
   def feedback
