419465a487af36db3b68d2421c7c7848da451e48
Thomas Casady <tscasady@gmail.com>
2022-10-14 19:03:00 -0500

    Add current_card test and missing tests to deck.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -12,7 +12,19 @@ RSpec.describe Deck do
             expect(deck).to be_a Deck
         end
 
-        it 'has a count' do
+        it 'has an initial_size' do
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+
+            expect(deck.initial_size).to eq 3
+        end
+    end
+
+    describe '#count' do
+        it 'can count the number of cards left in the deck' do
             card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
             card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
             card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -21,7 +33,21 @@ RSpec.describe Deck do
 
             expect(deck.count).to eq 3
         end
+    end
 
+    describe '#current_card' do
+        it 'can return the fist card in the deck' do
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+
+            expect(deck.current_card).to eq card_1
+        end
+    end
+
+    describe '#cards_in_category' do
         it 'can list the cards in a category' do
             card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
             card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
