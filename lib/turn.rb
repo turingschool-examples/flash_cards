fda811b76a22850c5394ad0d20e951b3a815e0d1
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-14 22:28:43 -0700

    Remove comments

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,3 @@
-# require_relative 'card'
-# lib/card.rb
-# pry into the code
-# instantiate card
-# do research on coding directories and how to interact
-# with the terminal
-# what is the difference between . and .. , why do i need
-# /
-
 class Turn
   attr_reader :guess, :card
 
@@ -15,29 +6,15 @@ class Turn
     @card = card
   end
 
-  # def guess
-  #     @guess
-  # end
-
   def correct?
-    # returns boolean if guess matches answer on card
-    # require 'pry'; binding.pry
-    # @card == @answer
     @guess == card.answer
   end
 
   def feedback
-    # require 'pry'; binding.pry
     if correct?
       'Correct!'
     else
       'Incorrect.'
     end
   end
-
-  # def number_correct_by_category
-  #   require 'pry'; binding.pry
-  # end
-
-
-end
+end 
