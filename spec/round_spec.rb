efe47f496f1c44823ed1f474131564ba18f55a7a
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-14 21:06:09 -0600

    Accidentaly deleted test 16. Fixed it

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -399,7 +399,26 @@ RSpec.describe Round do
 
     it "Shows number of correct answers in the round" do
 
-        cç
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = []
+        cards.push card_1, card_2, card_3
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        round.turns
+
+        round.current_card
+
+        new_turn = round.take_turn("Juneau")
+
+        new_turn2 = round.take_turn("Venus")
 
         round.number_correct
 
@@ -435,7 +454,7 @@ RSpec.describe Round do
 
         round.number_correct_by_category(:Geography)
 
-        expect(round.number_correct_by_category(:Geography)).to eq(1)
+        expect(round.number_correct_by_category).to eq(1)
 
     end
 
