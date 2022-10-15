3460d3ee3585e8c713c758a12b36e63b810618a6
Alex Pitzel <pitzelalex@gmail.com>
2022-10-14 20:34:40 -0400

    Add #build_cards method that passes test

---
+++ lib/card_generator.rb
@@ -0,0 +1,19 @@
+require './lib/card'
+
+class CardGenerator
+  attr_accessor :filename, :cards
+  
+  def initialize(filename)
+    @filename = filename
+    @cards = []
+  end
+
+  def build_cards
+    file = File.read(filename) 
+    raw_cards = file.split(/\n+/)
+    raw_cards.each do |raw_card|
+      raw_card = raw_card.split(',')
+      @cards << Card.new(raw_card[0], raw_card[1], raw_card[2].delete(' ').to_sym)
+    end
+  end
+end
