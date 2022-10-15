ff26bd6081bc9b77f45d1ce9d11b0f73391eb87d
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-15 13:30:53 -0500

    Add next input for turns

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -17,6 +17,7 @@ puts "Welcome! You're playing with #{round.deck.count} cards."
 puts '------------------------------------------------------------'
 
 until round.turns.length == round.deck.count
+  system 'clear'
   puts "This is card number #{round.turns.length + 1} out of #{round.deck.count}"
   puts "Question: #{round.current_card.question}"
   puts "Type your guess:"
@@ -25,11 +26,16 @@ until round.turns.length == round.deck.count
   round.take_turn(guess)
   puts round.turn_feedback
   puts '------------------------------------------------------------'
-
+  if round.turns.length  < round.deck.count
+    puts 'Hit enter for next'
+  else
+    puts 'End Game'
+  end
+  gets.chomp
 end
 
 
-
+system 'clear'
 puts "****** Game Over! ******"
 puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct}%."
 round.deck.category_groups.each do |category, card|
