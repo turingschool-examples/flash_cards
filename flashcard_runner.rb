18ae7dffec77792a020e31c877e6a3b212446fa2
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-15 14:21:33 -0700

    Refactor

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,11 +4,8 @@ require './lib/deck'
 require './lib/round'
 
 card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-card_2 = Card.new(
-  'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM
-)
-card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west',
-                  :STEM)
+card_2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west',:STEM)
 card_4 = Card.new('What is 6 + 5?', '11', :STEM)
 
 deck = Deck.new([card_1, card_2, card_3, card_4])
