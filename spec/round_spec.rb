1f35b44e2afc8abcce59e058ab0034a12d7e622d
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-10-15 15:07:47 -0500

    Add round initilize test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -23,7 +23,7 @@ describe Round do
       expect(round.deck).to eq deck
     end
 
-    it 'has an array for Turns that defaults empty' do
+    it 'has an array for turns that starts empty' do
       card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -31,6 +31,17 @@ describe Round do
       round = Round.new(deck)
 
       expect(round.turns).to eq []
+    end
+
+    it 'has an array for correct and incorrect turns that starts empty' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+
+      expect(round.correct).to eq []
+      expect(round.incorrect).to eq []
     end
 
   end
