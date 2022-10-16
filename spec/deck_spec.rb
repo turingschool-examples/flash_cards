ebe3bfce339e7a412c7ad40df5d12438e1f9c77e
Elle Majors <hellolovely@ellemajors>
2022-10-16 16:50:49 -0600

    Updated test for deck class

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,27 +1,76 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
-require 'pry'
+# require 'pry'
 
 RSpec.describe Deck do
   describe '#initialize' do
-    xit 'has empty deck object' do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    it 'has deck' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+      cards = [card_1, card_2, card_3]
       deck = Deck.new(cards)
 
-      expect(deck).to eq([])
+      expect(deck).to be_a(Deck)
     end
   end
 
 
-  it 'has cards objects' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    deck = Deck.new(cards)
-# require 'pry'; binding.pry
+  it 'has array of cards' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    expect(cards).to be_instance_of([cards_1, card_2, card_3])
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(cards).to eq([card_1, card_2, card_3])
   end
+
+  xit 'has cards objects' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(cards).to include([card_1, card_2, card_3])
+  end
+
+  xit 'returns cards in catagory' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category).to eq(:STEM)
+  end
+
+  xit 'returns cards in category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_catagory).to eq(:Geography)
+  end
+
 end
+
+  # xit 'returns cards in catagory' do
+  #   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #   deck = Deck.new(cards)
+  #
+  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  #
+  #   expect(deck.cards_in_catagory).to eq("")
+  # end
