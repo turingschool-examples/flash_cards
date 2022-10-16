b3604a938e0d5fbb0934cb1a7d62b820c03710ce
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-16 13:38:57 -0600

    Wrote teat 2 for card_generator_spec.rb

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,12 @@
 class CardGenerator
+    attr_reader :filename
 
     def initialize (filename)
         @filename = filename
     end
 
+    def make_cards
+
+        
+    end
 end
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,20 +1,30 @@
 require './lib/card_generator'
+require './lib/deck'
 
-    filename = "cards.txt"
-
+    filename = File.read("cards.txt")
+    file_with = filename.split("\n")
 
 RSpec.describe CardGenerator do
 
 #Test 1
-#See that cards is an array of the cards in the txt file
+#See that cards exists
     it "cards exists" do
 
-cards = CardGenerator.new(filename).cards
+        cards = CardGenerator.new(file_with)
 
-require 'pry'; binding.pry
+        expect(cards).to be_instance_of(CardGenerator)
 
-expect(cards).to be_instance_of(CardGenerator)
+    end
 
+#Test 2
+#See that cards holds the cards in the txt file
+
+    it "cards has all 4 cards" do
+        
+        cards = CardGenerator.new(file_with)
+       require 'pry'; binding.pry 
+
+       expect(cards)to be_instance_of(Card)
     end
 
 end
