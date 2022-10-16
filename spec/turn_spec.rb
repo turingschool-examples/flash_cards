f644d60438b85fee5e0634f76b0551fd6de1906a
Aether Kerstens <philippekerstens@gmail.com>
2022-10-16 10:57:18 -0700

    Create turn spec test

---
+++ spec/turn_spec.rb
@@ -0,0 +1,60 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+  it 'has an answer?' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'has an answer?' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+  it 'has a card' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+  it 'can make a guess' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.guess).to eq("Saturn")
+  end
+
+  it 'can be incorrect' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.correct?).to eq(false)
+  end
+
+  it 'can tell you Incorrect!' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.feedback).to eq("Incorrect!")
+  end
+end
