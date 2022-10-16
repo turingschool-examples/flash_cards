53b854ae88ee03cb1b86f5fc3fb064b8fb736258
Brian Hayes <Brian.hayes1200@gmail.com>
2022-10-16 01:23:29 -0600

    add card number message

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,10 +6,15 @@ require './lib/round'
 card_1 = Card.new("What is Brian's favorite sport?", "Football", :Brian)
 card_2 = Card.new("What is the capital city of Colorado?", "Denver", :Geography)
 card_3 = Card.new("What is 3 + 3?", "6", :Math)
-card_4 = Card.mew("What is Brian's favorite food?", "Chicken parmesan", :Brian)
+card_4 = Card.new("What is Brian's favorite food?", "Chicken parmesan", :Brian)
 deck = Deck.new([card_1, card_2, card_3, card_4])
 round = Round.new(deck)
 
 opening_message = "Welcome! You're playing with #{deck.count} cards."
 puts opening_message
+
+
+card_number_message = "This is card number 1 out of #{deck.count}."
+
+
 
