7bf59e4eb634206655745da04230a2d20fe3c6af
Alex Pitzel <pitzelalex@gmail.com>
2022-10-15 20:31:06 -0400

    Add function to shuffle cards and select 6

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,21 +1,21 @@
 require './lib/card'
 
 class CardGenerator
-  attr_accessor :filename, :cards
+  attr_accessor :filename, :cards, :new_cards
   
   def build_cards
     file = File.read(filename) 
     raw_cards = file.split(/\n+/)
     raw_cards.reject{ |raw_card| raw_card.start_with?("#") }.each do |raw_card|
       raw_card = raw_card.split(',')
-      @cards << Card.new(raw_card[0], raw_card[1], raw_card[2].delete(' ').to_sym)
+      @new_cards << Card.new(raw_card[0], raw_card[1], raw_card[2].delete(' ').to_sym)
     end
   end
 
   def initialize(filename)
     @filename = filename
-    @cards = []
-
+    @new_cards = []
     build_cards
+    @cards = @new_cards.shuffle.first(6)
   end
 end
