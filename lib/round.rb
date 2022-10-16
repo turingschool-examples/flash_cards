6920f1d8569afa41689eafccfb5b5d1b3b9829f0
Drew Layton <dlayton66@gmail.com>
2022-10-15 18:52:46 -0600

    Finish writing and testing round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,4 +1,65 @@
 class Round
-  def initialize
+  attr_reader :deck, :turns
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def take_turn(guess)
+    cur_index = turns.length
+    cur_turn = Turn.new(guess,deck.cards[cur_index])
+    turns << cur_turn
+    cur_turn
+  end
+
+  def current_card
+    deck.cards[turns.length]
+  end
+
+  def number_correct
+    # can't have gotten any correct if no turns have been played
+    if turns.length == 0
+      return 0
+    else
+      num_correct = 0
+      turns.each do |turn|
+        num_correct += 1 if turn.correct?
+      end
+      num_correct
+    end
+  end
+
+  def number_correct_by_category(category)
+    if turns.length == 0
+      return 0
+    else
+      num_correct_by_cat = 0
+      turns.each do |turn|
+        num_correct_by_cat += 1 if turn.correct? && turn.card.category == category
+      end
+      num_correct_by_cat
+    end
+  end
+
+  def percent_correct
+      # all percents rounded to 1 decimal place
+      if turns.length == 0
+        return nil
+      else
+        ((number_correct.to_f / turns.length)*100).round(1)
+      end
+  end
+
+  def percent_correct_by_category(category)
+    if turns.length == 0
+      return nil
+    else
+      num_questions_category = 0
+      turns.each do |turn|
+        num_questions_category += 1 if turn.card.category == category
+      end
+      ((number_correct_by_category(category) / num_questions_category.to_f)*100).round(1)
+    end
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,110 +4,114 @@ require './lib/deck'
 require './lib/round'
 
 describe Round do
-  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  cards = [card_1, card_2, card_3]
-  deck = Deck.new(cards)
-  round = Round.new(deck)
+  before(:each) do
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+  end
 
   it 'exists' do
-    expect(round).to be_a Round
+    expect(@round).to be_a Round
   end
 
   it 'has a deck' do
-    expect(round.deck).to eq deck
+    expect(@round.deck).to eq @deck
   end
 
-  describe '#turns' do
-    it 'returns an array of all past turns' do
-      expect(round.turns).to eq []
+  describe 'turns' do
+    it 'returns the array of all past turns built by take_turn method' do
+      expect(@round.turns).to eq []
 
-      round.take_turn("Juneau")
+      turn_1 = @round.take_turn("Juneau")
 
-      expect(round.turns).to eq [["Juneau",card_1]]
+      expect(@round.turns).to eq [turn_1]
 
-      round.take_turn("Venus")
+      turn_2 = @round.take_turn("Venus")
 
-      expect(round.turns).to eq [["Juneau",card_1],["Venus",card_2]]
+      expect(@round.turns).to eq [turn_1, turn_2]
     end
   end
 
   describe '#take_turn' do
     it 'takes a guess and passes it with the current card to a new turn object' do
+      new_turn = @round.take_turn("Juneau")
 
-# test
-      expect(round.turns).to eq []
-
-      new_turn = round.take_turn("Juneau")
-
-      expect(new_turn).to eq ["Juneau",card_1]
-
-      new_turn = round.take_turn("Venus")
-
-      expect(new_turn).to eq ["Venus",card_2]
+      expect(new_turn).to be_a Turn
+      expect(new_turn.guess).to eq "Juneau"
+      expect(new_turn.card).to eq @card_1
     end
   end
 
   describe '#current_card' do
     it 'returns the current (i.e. first) card' do
-      expect(round.current_card).to eq card_1
+      expect(@round.current_card).to eq @card_1
     end
     it 'returns the next card after a turn' do
-      round.take_turn("Juneau")
-      
-      expect(round.current_card).to eq card_2
+      @round.take_turn("Juneau")
+
+      expect(@round.current_card).to eq @card_2
     end
   end
 
   describe '#number_correct' do
     it 'returns the number of correct answers' do
-      expect(round.number_correct).to be 0
+      expect(@round.number_correct).to be 0
 
-      round.take_turn("Juneau")
+      @round.take_turn("Juneau")
 
-      expect(round.number_correct).to be 1
+      expect(@round.number_correct).to be 1
 
-      round.take_turn("Venus")
+      @round.take_turn("Venus")
 
-      expect(round.number_correct).to be 1
+      expect(@round.number_correct).to be 1
     end
   end
 
   describe '#number_correct_by_category' do
     it 'returns the number of correct answers by category' do
-      round.take_turn("Juneau")
-      round.take_turn("Venus")
+      @round.take_turn("Juneau")
+      @round.take_turn("Venus")
 
-      expect(round.number_correct_by_category(:Geography)).to be 1
-      expect(round.number_correct_by_category(:STEM)).to be 0
+      expect(@round.number_correct_by_category(:Geography)).to be 1
+      expect(@round.number_correct_by_category(:STEM)).to be 0
     end
   end
 
   describe '#percent_correct' do
+    it 'returns nil if no guesses have been played' do
+      expect(@round.percent_correct).to be nil
+    end
+
     it 'returns the percentage of correct guesses' do
-      round.take_turn("Juneau")
-      round.take_turn("Venus")
+      @round.take_turn("Juneau")
+      @round.take_turn("Venus")
 
-      expect(round.percent_correct).to be 50.0
+      expect(@round.percent_correct).to be 50.0
 
-      round.take_turn("North")
+      @round.take_turn("North north west")
 
-      expect(round.percent_correct).to be 33.3
+      expect(@round.percent_correct).to be 66.7
     end
   end
 
   describe '#percent_correct_by_category' do
+    it 'returns nil if no guesses have been played' do
+      expect(@round.percent_correct_by_category(:Geography)).to be nil
+    end
+
     it 'returns the percentage of correct guesses' do
-      round.take_turn("Juneau")
-      round.take_turn("Venus")
+      @round.take_turn("Juneau")
+      @round.take_turn("Venus")
 
-      expect(round.percent_correct_by_category(:Geography)).to be 100.0
-      expect(round.percent_correct_by_category(:STEM)).to be 0.0
+      expect(@round.percent_correct_by_category(:Geography)).to be 100.0
+      expect(@round.percent_correct_by_category(:STEM)).to be 0.0
 
-      round.take_turn("North")
+      @round.take_turn("North north west")
 
-      expect(round.percent_correct_by_category(:Geography)).to be 50.0
+      expect(@round.percent_correct_by_category(:STEM)).to be 50.0
     end
   end
 end
