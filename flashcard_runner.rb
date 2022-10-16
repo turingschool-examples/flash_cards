6961410ebe54c33d539cd07f9180549950d15597
Brian Hayes <Brian.hayes1200@gmail.com>
2022-10-16 01:14:20 -0600

    add opening message

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,10 +3,13 @@ require './lib/card'
 require './lib/turn'
 require './lib/round'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-deck = Deck.new([card_1, card_2, card_3])
+card_1 = Card.new("What is Brian's favorite sport?", "Football", :Brian)
+card_2 = Card.new("What is the capital city of Colorado?", "Denver", :Geography)
+card_3 = Card.new("What is 3 + 3?", "6", :Math)
+card_4 = Card.mew("What is Brian's favorite food?", "Chicken parmesan", :Brian)
+deck = Deck.new([card_1, card_2, card_3, card_4])
 round = Round.new(deck)
 
-puts ""
+opening_message = "Welcome! You're playing with #{deck.count} cards."
+puts opening_message
+
