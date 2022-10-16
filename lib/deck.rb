0cca3c4345b9d645094fd2461376032b6d0bde1a
Elle Majors <hellolovely@ellemajors>
2022-10-16 16:56:31 -0600

    Updated deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,13 @@
 class Deck
-  attr_reader :deck,
-              :cards
+  attr_reader :cards
 
   attr_accessor :cards_in_category
 
-  def initialize(deck, cards)
-    @deck = []
-    @cards = [card_1, card_2, card_3]
+  def initialize(cards)
+    @cards = cards
   end
 
-  def cards_in_category( :STEM, :Geography, "Pop Culture" )
-    @cards_in_category = @cards_in_category
+  def cards_in_category
+    @cards_in_category = @cards + @category[2]
   end
 end
-  # def cards
-  #   @cards = [card_1, card_2, card_3]
-  # end
