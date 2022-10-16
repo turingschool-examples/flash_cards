2754dddf084b03bf23fb297ad7f23e3995732bcb
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-16 12:42:58 -0600

    Wrote test 1 for card_generator_spec.rb

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -5,6 +5,16 @@ require './lib/card_generator'
 
 RSpec.describe CardGenerator do
 
+#Test 1
+#See that cards is an array of the cards in the txt file
+    it "cards exists" do
 
+cards = CardGenerator.new(filename).cards
+
+require 'pry'; binding.pry
+
+expect(cards).to be_instance_of(CardGenerator)
+
+    end
 
 end
