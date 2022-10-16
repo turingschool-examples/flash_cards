6d410722613491125b6b492691ac448cb0e734b4
Drew Layton <dlayton66@gmail.com>
2022-10-16 16:44:26 -0600

    Finalize flashcard_runner.rb to pull cards from file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,6 +2,7 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 
 # card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 # card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -13,6 +14,8 @@ require './lib/round'
 # card_8 = Card.new("Who composed the Windows 95 startup theme?", "Brian Eno", :Music)
 # card_9 = Card.new("What is the best-selling album of all time?", "Thriller", :Music)
 
-deck = Deck.new([card_1,card_2,card_3,card_4,card_5,card_6,card_7,card_8,card_9])
+filename = "cards.txt"
+cards = CardGenerator.new(filename).cards
+deck = Deck.new(cards)
 round = Round.new(deck)
 round.start
