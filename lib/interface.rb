0f5edbdcad9be492dd896dc3c2423f110350bb4c
Thomas Casady <tscasady@gmail.com>
2022-10-15 21:41:24 -0500

    finish interface class and tests.

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,2 +1,28 @@
 class Interface
+  
+  attr_reader :cards, :deck, :round
+
+  def initialize
+    @cards = CardGenerator.new("cards.txt").cards
+    @deck = Deck.new(@cards)
+    @round = Round.new(@deck)
+  end
+
+  def play
+    puts "Welcome! You're playing with #{@round.deck.initial_size} cards."
+    puts "-------------------------------------------------"
+
+
+    until @round.deck.count == 0 do
+      puts "This is card number #{@round.turns.length + 1} out of #{@round.deck.initial_size}."
+      puts "Question: #{@round.current_card.question}"
+      guess = gets.chomp
+      @round.take_turn(guess)
+      puts "#{@round.feedback}\n"
+    end
+
+    puts "****** Game over! ******"
+    puts "You had #{@round.number_correct} correct guess#{@round.number_correct > 1 ? 'es' : ''} out of #{@round.deck.initial_size} for a total score of #{@round.percent_correct}%."
+    @deck.category_data.keys.each {|category| puts "#{category} - #{@round.percent_correct_by_category(category)}% correct"}
+  end
 end
--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -1,1 +1,21 @@
 require './lib/interface'
+
+RSpec.describe Interface do
+  describe '#initialize' do
+    it 'has a card generator' do
+
+      expect(Interface.new().cards).to be_a Array
+    end
+
+    it 'has a deck' do
+      expect(Interface.new().deck).to be_a Deck
+    end
+
+    it 'has a round' do
+      expect(Interface.new().round).to be_a Round
+    end
+  end
+end
+
+
+
