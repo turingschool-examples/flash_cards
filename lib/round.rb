4e6fa30df04603415a5dd6086a9230fcb2d97d1d
Brian Hayes <Brian.hayes1200@gmail.com>
2022-10-16 01:01:23 -0600

    fix take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -5,8 +5,33 @@ class Round
         @turns = []
     end 
 
-
     def current_card
-        deck.cards[0]
+        @current_card = deck.cards[0]
     end 
+
+    def take_turn(guess)
+        @turn = Turn.new(guess, current_card)
+        @turns << @turn
+        deck.cards.shift
+        return @turn 
+    end 
+
+    def number_correct
+        num_correct = []
+         @turns.each do |completed_turn| 
+            if completed_turn.correct? == true
+                num_correct << completed_turn
+            return num_correct.count  
+            end       
+       end  
+    end 
+
+    def number_correct_by_category(category)
+         
+         
+
+    end 
+
+    
+  
 end 
