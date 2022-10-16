b6e56b5cb9d5b8c4b7abf13e6e81ae601558be1c
Christian McCabe <christian.mccabe89@gmail.com>
2022-10-16 11:28:39 -0600

    Initialize Round class and create/pass 'exist' test

---
+++ lib/round.rb
@@ -0,0 +1,17 @@
+class Round
+
+  attr_reader :deck, :turns
+  attr_accessor :number_correct, :count, :total_cards, :cats
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+    @number_correct = 0
+    @count = 1
+    @total_cards = 0
+    @cats = []
+  end
+
+  
+
+end
