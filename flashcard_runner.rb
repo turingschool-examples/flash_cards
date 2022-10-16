75ef9bc0b4bea9a51412f8ab993fbe066d01d639
Lacey Weaver <jlweave89@gmail.com>
2022-10-16 10:02:28 -0600

    Add last push for project

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,7 +3,6 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("What is 5 + 5", "10", :Mathematics)
@@ -18,7 +17,6 @@ def start(deck)
   number_of_cards = deck.count
 
     round.deck.cards.each do |card|
-        # require 'pry'; binding.pry
         puts "This is card number #{number} out of #{number_of_cards}"
         puts "Question: #{card.question}"
         answer = gets.chomp
@@ -35,18 +33,3 @@ def start(deck)
 end
 start(deck_1)
  
- 
-    
-
-
-
-
-
-
-
-# card_4 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-# card_5 = Card.new("What is 10 + 5", "15", :Mathematics)
-# card_6 = Card.new("What is the capital of Colorado?", "Denver", :Geography)
-# cards_2 = [card_4, card_5, card_6]
-# deck_2 = Deck.new(cards_2)
-# round_2 = Round.new(deck_2)
