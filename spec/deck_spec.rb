eeb83491eea6df67b1ee886ee1b783e0820b4448
Elle Majors <hellolovely@ellemajors>
2022-10-16 16:54:28 -0600

    Deleted unusable test code

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -61,16 +61,4 @@ RSpec.describe Deck do
 
     expect(deck.cards_in_catagory).to eq(:Geography)
   end
-
 end
-
-  # xit 'returns cards in catagory' do
-  #   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #   deck = Deck.new(cards)
-  #
-  #   card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  #   card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  #   card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  #
-  #   expect(deck.cards_in_catagory).to eq("")
-  # end
