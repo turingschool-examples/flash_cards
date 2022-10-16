4f574fe7fb01f6595f332b7cf270da9f93075434
Brian Hayes <Brian.hayes1200@gmail.com>
2022-10-16 14:40:22 -0600

    add closing message and results

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -10,8 +10,11 @@ card_4 = Card.new("What is Brian's favorite food?", "Chicken parmesan", :Brian)
 deck = Deck.new([card_1, card_2, card_3, card_4])
 round = Round.new(deck)
 
-opening_message = "Welcome! You're playing with #{deck.count} cards."
+
+ opening_message = "Welcome! You're playing with #{deck.count} cards."
 puts opening_message
+
+
 
 
 card_number_message = "This is card number 1 out of #{deck.count}."
@@ -19,3 +22,48 @@ puts card_number_message
 
 question_message = "Question: #{round.current_card.question}"
 puts question_message
+
+guess = gets.chomp
+new_turn = round.take_turn(guess)
+puts round.turns.last.feedback
+
+
+card_number_message = "This is card number 2 out of 4."
+puts card_number_message
+
+question_message = "Question: #{round.current_card.question}"
+puts question_message
+
+
+guess = gets.chomp
+new_turn = round.take_turn(guess)
+puts round.turns.last.feedback
+
+card_number_message = "This is card number 3 out of 4."
+puts card_number_message
+question_message = "Question: #{round.current_card.question}"
+puts question_message
+
+
+guess = gets.chomp
+new_turn = round.take_turn(guess)
+puts round.turns.last.feedback
+
+card_number_message = "This is card number 4 out of 4."
+puts card_number_message
+
+question_message = "Question: #{round.current_card.question}"
+puts question_message
+
+
+guess = gets.chomp
+new_turn = round.take_turn(guess)
+puts round.turns.last.feedback
+
+closing_message = '****** Game Over! *******'
+puts closing_message
+
+correct_guesses_message = "You had #{round.number_correct} correct guesses out of 4 for a total 
+score of #{round.percent_correct}."
+
+puts correct_guesses_message
