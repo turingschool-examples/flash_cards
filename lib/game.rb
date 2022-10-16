345006acdfa331b8b7a3ecf729b315ebf5bfc74c
Bryan Flanagan <bflanagan78@yahoo.com>
2022-10-16 12:15:34 -0600

    Make game end when card deck is exhausted

--- lib/game.rb
+++ lib/game.rb
@@ -4,9 +4,7 @@ require './lib/deck'
 require './lib/round'
 
 class Game
-  attr_reader :round,
-              :turn,
-              :deck
+  attr_reader :round
 
   def initialize(round)
     @round = round
@@ -28,32 +26,30 @@ class Game
 
     puts "Welcome #{name}! You're playing with #{round.deck.cards.length} cards."
     puts "-------------------------------------------------"
-    
-    if (round.turns.size + 1) < round.deck.cards.length
-      
+    round_number = 1
+    until (round_number - 1) == round.deck.cards.size
       round.deck.cards.each do |card|
-        puts "This is card number #{round.turns.size + 1} out of #{round.deck.cards.length}."
+        puts "This is card number #{round_number} out of #{round.deck.cards.length}."
         puts "Question: #{round.current_card.question}"
         guess = gets.chomp
         turn = round.take_turn(guess)
         puts turn.feedback
+        round_number += 1
       end
-
-    else
       end_game
     end
   end
 
   def end_game
                                                                                           
-    p "     _/_/_/    _/_/    _/      _/  _/_/_/_/        _/_/    _/      _/  _/_/_/_/  _/_/_/    "
-    p "  _/        _/    _/  _/_/  _/_/  _/            _/    _/  _/      _/  _/        _/    _/  " 
-    p " _/  _/_/  _/_/_/_/  _/  _/  _/  _/_/_/        _/    _/  _/      _/  _/_/_/    _/_/_/    "  
-    p "_/    _/  _/    _/  _/      _/  _/            _/    _/    _/  _/    _/        _/    _/  "   
-    p " _/_/_/  _/    _/  _/      _/  _/_/_/_/        _/_/        _/      _/_/_/_/  _/    _/  "
-    p "You had #{round.number_correct} correct guesses out of #{round.turns.length} for a total score of #{round.percent_correct}."
-    p "STEM - #{round.percent_correct_by_category(:STEM)}% correct"
-    p "Turing Staff - #{round.percent_correct_by_category(:TuringStaff)}% correct"
-    p "Pop Culture - #{round.percent_correct_by_category(:PopCulture)}% correct"
+    puts "     _/_/_/    _/_/    _/      _/  _/_/_/_/        _/_/    _/      _/  _/_/_/_/  _/_/_/    "
+    puts "  _/        _/    _/  _/_/  _/_/  _/            _/    _/  _/      _/  _/        _/    _/  " 
+    puts " _/  _/_/  _/_/_/_/  _/  _/  _/  _/_/_/        _/    _/  _/      _/  _/_/_/    _/_/_/    "  
+    puts "_/    _/  _/    _/  _/      _/  _/            _/    _/    _/  _/    _/        _/    _/  "   
+    puts " _/_/_/  _/    _/  _/      _/  _/_/_/_/        _/_/        _/      _/_/_/_/  _/    _/  "
+    puts "You had #{round.number_correct} correct guesses out of #{round.turns.length} for a total score of #{round.percent_correct}."
+    puts "STEM - #{round.percent_correct_by_category(:STEM)}% correct"
+    puts "Turing Staff - #{round.percent_correct_by_category(:TuringStaff)}% correct"
+    puts "Pop Culture - #{round.percent_correct_by_category(:PopCulture)}% correct"
   end 
 end 
