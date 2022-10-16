78caa1a1e2aeeaebb412be37a22459420948b38c
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-16 09:11:03 -0700

    Refactor the round spec test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -12,15 +12,12 @@ RSpec.describe do
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-
     deck = Deck.new([card_1, card_2, card_3])
 
     round = Round.new(deck)
 
     expect(round).to be_instance_of(Round)
-
   end
-
 end
 
   describe '#deck method' do
@@ -29,13 +26,11 @@ end
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-
       deck = Deck.new([card_1, card_2, card_3])
 
       round = Round.new(deck)
 
       expect(round.deck).to eq(deck)
-
     end
   end
 
@@ -45,16 +40,12 @@ end
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-
       deck = Deck.new([card_1, card_2, card_3])
 
       round = Round.new(deck)
 
       expect(round.turns).to eq([])
-
     end
-
-
   end
 
   describe '#current_card' do
@@ -64,15 +55,12 @@ end
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-
     deck = Deck.new([card_1, card_2, card_3])
 
     round = Round.new(deck)
 
     expect(round.current_card).to eq(card_1)
-
     end
-
   end
 
   describe '#take_turn()' do
@@ -81,18 +69,13 @@ end
       card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-
-
       deck = Deck.new([card_1, card_2, card_3])
 
       round = Round.new(deck)
 
       new_turn = round.take_turn("Juneau")
 
-
       expect(new_turn).to be_instance_of(Turn)
-
-
     end
   end
 
@@ -131,7 +114,6 @@ end
       new_turn_3 = round.take_turn("Blues Clues")
 
       expect(round.percent_correct).to eq(33.3)
-
     end
   end
 
@@ -153,16 +135,6 @@ end
 
       expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
       expect(round.percent_correct_by_category(:STEM)).to eq(0.0)
-
     end
-
   end
-
-
-
-
-
-
-
-
 end
