8ed138b3183be6a0e1b2e4339badaa455f618dd9
Thomas Casady <tscasady@gmail.com>
2022-10-16 12:54:22 -0500

    Refactor: change interface.play to call methods the put the welcome message, turns, and end of game message.

--- lib/interface.rb
+++ lib/interface.rb
@@ -9,21 +9,34 @@ class Interface
   end
 
   def play
-    puts "Welcome! You're playing with #{@round.deck.initial_size} cards."
-    puts "Answers are not case sensitive and *~some~* spelling errors are allowed."
-    puts "-------------------------------------------------"
 
+    welcome_message
 
     until @round.deck.count == 0 do
-      puts "This is card number #{@round.turns.length + 1} out of #{@round.deck.initial_size}."
+      play_turn
+    end
+
+    end_game_mesasge
+  end
+
+  def welcome_message
+    puts "Welcome! You're playing with #{@round.deck.initial_size} cards."
+    puts "Answers are not case sensitive and (too many) spelling errors are allowed."
+    puts "-------------------------------------------------"
+  end
+
+  def play_turn
+    puts "This is card number #{@round.turns.length + 1} out of #{@round.deck.initial_size}."
       puts "Question: #{@round.current_card.question}"
       guess = gets.chomp
       @round.take_turn(guess)
       puts "#{@round.feedback}\n"
-    end
+  end
 
+  def end_game_mesasge
     puts "****** Game over! ******"
     puts "You had #{@round.number_correct} correct guess#{@round.number_correct > 1 ? 'es' : ''} out of #{@round.deck.initial_size} for a total score of #{@round.percent_correct}%."
     @deck.category_data.keys.each {|category| puts "#{category} - #{@round.percent_correct_by_category(category)}% correct"}
   end
+
 end
