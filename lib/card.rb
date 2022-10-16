a708361fc81e4902da6de2513932e658198f6d6e
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-10-16 10:10:51 -0700

    Refactor the card class file

--- lib/card.rb
+++ lib/card.rb
@@ -6,17 +6,4 @@ class Card
     @answer = answer
     @category = category
   end
-
-  # def question
-  #   @question
-  # end
-  #
-  # def answer
-  #   @answer
-  # end
-  #
-  # def category
-  #   @category
-  # end
-
 end
