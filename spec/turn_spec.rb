3ed4533ec0d24c0e42dd987c1f6709d52c858737
Jordan Skomal <jskomal@gmail.com>
2022-11-01 16:22:16 -0600

    Add tests to turn spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe Turn do
 
     expect(turn).to be_instance_of(Turn)
   end
+
+  it 'contains a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'contains the correct card card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.card.question).to eq("What is the capital of Alaska?")
+    expect(turn.card.answer).to eq("Juneau")
+    expect(turn.card.category).to eq(:Geography)
+  end
+
+  it 'contains a correct? method that returns true or false if the guess is correct' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    wrong_turn = Turn.new("Washington DC", card)
+    expect(turn.correct?).to eq(true)
+    expect(wrong_turn.correct?).to eq(false)
+  end
+
 end
