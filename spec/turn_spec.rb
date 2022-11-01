da27b3ea9c4549ce72e665a6c447aeca385c8d00
Jordan Skomal <jskomal@gmail.com>
2022-11-01 16:26:19 -0600

    Add test for feedback method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -29,8 +29,18 @@ RSpec.describe Turn do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     wrong_turn = Turn.new("Washington DC", card)
+
     expect(turn.correct?).to eq(true)
     expect(wrong_turn.correct?).to eq(false)
+  end
+
+  it 'contains a feedback method that returns a string based on the correct or incorrect answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    wrong_turn = Turn.new("Washington DC", card)
+
+    expect(turn.feedback).to eq('Correct!')
+    expect(wrong_turn.feedback).to eq('Incorrect.')
   end
 
 end
