bc4165bb9757ade00bce5f291cea76be06dd6530
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-01-27 20:39:01 -0700

    Update deck spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,12 +1,12 @@
 require './lib/card'
 require './lib/deck'
 
-cards = [
-  Card.new("What is my name?", "Rontron", :People),
-  Card.new("Where is the best training track?", "Aurora Reservoir", :Places),
-  Card.new("How long is the Miami UltraSkate?", "24 hours", :SkateEvents),
-  Card.new("Where does the A2A roadskate begin?", "Athens, GA", :SkateEvents)
-]
+card_1 = Card.new("What is my name?", "Rontron", :People)
+card_2 = Card.new("Where is the best training track?", "Aurora Reservoir", :Places)
+card_3 = Card.new("How long is the Miami UltraSkate?", "24 hours", :SkateEvents)
+card_4 = Card.new("Where does the A2A roadskate begin?", "Athens, GA", :SkateEvents)
+
+cards = [card_1, card_2, card_3, card_4]
 
 RSpec.describe Deck do
   it 'exists' do
@@ -15,7 +15,7 @@ RSpec.describe Deck do
     expect(deck).to be_instance_of(Deck)
   end
 
-  it 'holds a deck of cards' do
+  it 'holds a collection of cards' do
     deck = Deck.new(cards)
 
     expect(deck.cards).to eq(cards)
@@ -30,9 +30,9 @@ RSpec.describe Deck do
   it 'can return cards based on a given category' do
     deck = Deck.new(cards)
 
-    expect(deck.cards_in_category(:People)).to eq([cards[0]])
-    expect(deck.cards_in_category(:Places)).to eq([cards[1]])
-    expect(deck.cards_in_category(:SkateEvents)).to eq([cards[2], cards[3]])
+    expect(deck.cards_in_category(:People)).to eq([card_1])
+    expect(deck.cards_in_category(:Places)).to eq([card_2])
+    expect(deck.cards_in_category(:SkateEvents)).to eq([card_3, card_4])
     expect(deck.cards_in_category(:Business)).to eq([])
   end
 end
