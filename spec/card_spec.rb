c04ff838cbe214a90195f24b204e47d9e40b1c98
William Zale <williamzale@gmail.com>
2023-05-08 19:24:03 -0400

    Change single quotations to double

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,22 +1,22 @@
-require './lib/card'
+require "./lib/card"
 
 RSpec.describe Card do
   before(:each) do
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
   end
-  it 'exists' do
+  it "exists" do
     expect(@card).to be_instance_of(Card)
   end
 
-  it 'has a question' do
+  it "has a question" do
     expect(@card.question).to eq("What is the capital of Alaska?")
   end
 
-  it 'has an answer' do
+  it "has an answer" do
     expect(@card.answer).to eq("Juneau")
   end
 
-  it 'has a category' do
+  it "has a category" do
     expect(@card.category).to eq(:Geography)
   end
 end
