7f118a088c97483ba891656f4179be7fd1c2e676
Weston Schutt <wtschutt@gmail.com>
2023-05-08 17:53:54 -0600

    Add test for returning cards by category given

--- spec/deck_speck.rb
+++ spec/deck_speck.rb
@@ -32,4 +32,15 @@ RSpec.describe Deck do
 
     expect(deck.count).to eq 3
   end
+
+  it "can return array of cards in a category" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq([card_2,card_3])
+    expect(deck.cards_in_category(:Geography)).to eq([card_1])
+  end
 end
