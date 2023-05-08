15414aa9cd81477d569f8f3a5a443dc7cf03c57b
William Zale <williamzale@gmail.com>
2023-05-08 19:47:05 -0400

    Add guess, correct?, and feedback methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,4 +4,21 @@ class Turn
     @string = string
     @card = card
   end
+
+  def guess
+    @string
+  end
+
+  def correct?
+    @string == @card.answer
+  end
+
+  def feedback
+    if @string == @card.answer
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
+
 end
