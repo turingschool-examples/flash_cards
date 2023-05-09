81843f370c62166ee31fce0a9a401f510957a850
William Zale <williamzale@gmail.com>
2023-05-08 20:39:36 -0400

    Add test for cards_in_category method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -22,5 +22,9 @@ RSpec.describe Deck do
     expect(@deck.count).to eq(3)
   end
 
-  
+  it "has a cards in category method" do
+    expect(@deck.cards_in_category(:STEM)).to eq([@card_2, @card_3])
+    expect(@deck.cards_in_category(:Geography)).to eq([@card_1])
+    expect(@deck.cards_in_category("Pop Culture")).to eq([])
+  end
 end
