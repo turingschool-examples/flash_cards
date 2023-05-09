d23f74e9418733fe6b770b6b250b85ad8f852ccf
William Zale <williamzale@gmail.com>
2023-05-09 09:31:53 -0400

    Add test for current_card method

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Round do
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     
-    @deck = Deck.new([@card_1, @card2, @card_3])
+    @deck = Deck.new([@card_1, @card_2, @card_3])
     
     @round = Round.new(@deck)
   end
@@ -18,5 +18,11 @@ RSpec.describe Round do
   it "exists with readable attributes" do
     expect(@round).to be_instance_of(Round)
     expect(@round.deck).to eq(@deck)
+    expect(@round.turns).to eq([])
   end
+
+  it "has a current card method" do
+    expect(@round.current_card).to eq(@card_1)
+  end
+
 end
