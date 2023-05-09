76b7986d516f82cc893f082dea7392b6394e0d7e
William Zale <williamzale@gmail.com>
2023-05-08 21:00:00 -0400

    Add test for object instantiation with readable attributes

---
+++ spec/round_spec.rb
@@ -0,0 +1,22 @@
+require "./lib/card"
+require "./lib/turn"
+require "./lib/deck"
+require "./lib/round"
+
+RSpec.describe Round do
+
+  before(:each) do
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    
+    @deck = Deck.new([@card_1, @card2, @card_3])
+    
+    @round = Round.new(@deck)
+  end
+
+  it "exists with readable attributes" do
+    expect(@round).to be_instance_of(Round)
+    expect(@round.deck).to eq(@deck)
+  end
+end
