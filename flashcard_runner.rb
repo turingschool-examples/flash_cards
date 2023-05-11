4dfc20233bcdebeb84d3cfae7d598d9f68f0d4ab
Connor Richmond <richmond.connor@gmail.com>
2023-05-10 23:56:52 -0400

    Move and format cards and flashcard

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,7 +1,7 @@
-require './turn'
-require './card'
-require './deck'
-require './round'
+require './lib/turn'
+require './lib/card'
+require './lib/deck'
+require './lib/round'
 
 card1 = Card.new("What is 5 + 5?", "10", :STEM)
 card2 = Card.new("What is Rachel's favorite animal?", "panda", :Pop_Culture)
