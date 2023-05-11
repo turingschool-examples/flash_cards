57fb0950032f78013f4aeb433bf48aca545d0346
Connor Richmond <richmond.connor@gmail.com>
2023-05-11 00:11:19 -0400

    Update to be able to pull cards from other file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,13 +2,10 @@ require './lib/turn'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 
-card1 = Card.new("What is 5 + 5?", "10", :STEM)
-card2 = Card.new("What is Rachel's favorite animal?", "panda", :Pop_Culture)
-card3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
-card4 = Card.new("What cardboard cutout lives at Turing?", "justin bieber", :Pop_Culture)
-
-cards = [card1, card2, card3, card4]
+filename = "cards.txt"
+cards = CardGenerator.new(filename).cards
 deck = Deck.new(cards)
 round = Round.new(deck)
 
