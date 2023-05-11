1ed164a8ed076d65c56960ea75b9e13decc4b6fd
William Zale <williamzale@gmail.com>
2023-05-11 10:10:56 -0400

    Add overall_correct_percent, start, round, and game_over methods

--- lib/round.rb
+++ lib/round.rb
@@ -31,4 +31,51 @@ class Round
     number_in_category = @turns.select { |turn| turn.card.category == category }.count
     (number_correct / number_in_category.to_f) * 100
   end
+
+  def overall_correct_percent
+    ((number_correct.to_f / @turns.count) * 100).round(2).to_s + "%"
+  end
+
+  def start
+    puts ""
+    puts "Welcome to flashcards!"
+    puts "You're playing with 4 cards."
+    puts "-------------------------------------------------"
+    puts "This is card number 1 out of 4."
+    puts "Question: #{@deck.cards.first.question}"
+    user_answer = gets.chomp
+    new_turn = take_turn(user_answer)
+    p new_turn.feedback
+    round(2)
+  end
+
+  # if number is greater than length of deck array, go to game over
+  # method
+  def round(number)
+    if number > 4
+      game_over
+    else
+      puts ""
+      puts "Welcome to round #{number}!"
+      puts "You're playing with 4 cards."
+      puts "-------------------------------------------------"
+      puts "This is card number #{number} out of 4."
+      puts "Question: #{@deck.cards.first.question}"
+      user_answer = gets.chomp
+      new_turn = take_turn(user_answer)
+      p new_turn.feedback
+      round(number + 1)
+    end
+  end
+
+# if else statement for puts guess vs. guesses based on guesses
+
+  def game_over
+    puts ""
+    puts "****** Game over! ******"
+    puts "You had #{number_correct} correct guesses out of #{@turns.count} for a total score of #{overall_correct_percent}."
+    puts "Geography - #{percent_correct_by_category(:Geography)} correct"
+    puts "STEM - #{percent_correct_by_category(:STEM)} correct"
+    puts "Pets - #{percent_correct_by_category(:Pets)} correct"
+  end
 end
