638b4f633de62ce3492c6bfefc1c96ab859481d8
Victor Sanchez <vsanantone@gmail.com>
2023-08-21 15:05:04 -0500

    add testing for both card & turn spec file

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,16 +1,48 @@
+require './lib/card'
 require './lib/turn'
-require '.lib/card'
 
 RSpec.describe do
- it 'exist' do
-  card = Card.new("What is the capital of Alaska?",
-   "Juneau", :Geography)
-  turn = Turn.new("Juneau", card)
+
+  before(:each) do
+    card = Card.new("What is the capital of Alaska?","Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+  end
+ 
+  it 'exist' do
+    turn = Turn.new("Juneau", card)
+    expect(@turn).to be_instance_of(Turn)
  end
 
- it 'exists' do
-  turn = Turn.new("Juneau", card)
-  expect(turn).to be_instance_of(Turn)
-end
+  it 'has a guess' do
+    turn = Turn.new("Juneau", card)
+    turn.card
+    expect(turn.guess).to eq("Juneau")
+
+    turn = Turn.new("Saturn", card)
+    turn.card
+    expect(turn.guess).to eq("Saturn")
+  end
+
+  it 'can check if guess is correct?' do
+    turn = Turn.new("Juneau", card)
+    turn.card
+    expect(turn.correct?).to eq(true)
+
+    turn = Turn.new("Saturn", card)
+    turn.card
+    expect(turn.correct?).to eq(false)
+
+  end
+
+  it 'gives feedback' do
+    turn = Turn.new("Juneau", card)
+    turn.card
+    expect(turn.feedback).to eq("Correct!")
+
+    turn = Turn.new("Saturn", card)
+    turn.card
+    expect(turn.feedback).to eq("Incorrect")
+
+  end
 
 end
