0bdb0ac255096e4cfca7aafd4724551f3bae6aec
Victor Sanchez <vsanantone@gmail.com>
2023-08-21 17:36:13 -0500

    fix: GitHub issues

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,47 +2,46 @@ require './lib/card'
 require './lib/turn'
 
 RSpec.describe do
-
-  before(:each) do
+  it 'exist' do
     card = Card.new("What is the capital of Alaska?","Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-  end
- 
-  it 'exist' do
-    turn = Turn.new("Juneau", card)
-    expect(@turn).to be_instance_of(Turn)
+
+    expect(turn).to be_instance_of(Turn)
  end
 
   it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?","Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-    turn.card
     expect(turn.guess).to eq("Juneau")
-
-    turn = Turn.new("Saturn", card)
-    turn.card
-    expect(turn.guess).to eq("Saturn")
   end
 
-  it 'can check if guess is correct?' do
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?","Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.card).to eq(card)
+  end
+
+  xit 'can check if guess is correct?' do
+    card = Card.new("What is the capital of Alaska?","Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     turn.card
     expect(turn.correct?).to eq(true)
-
-    turn = Turn.new("Saturn", card)
-    turn.card
-    expect(turn.correct?).to eq(false)
-
+    
   end
 
-  it 'gives feedback' do
+  xit 'gives feedback on correct answers' do
+    card = Card.new("What is the capital of Alaska?","Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     turn.card
     expect(turn.feedback).to eq("Correct!")
-
-    turn = Turn.new("Saturn", card)
-    turn.card
-    expect(turn.feedback).to eq("Incorrect")
-
   end
 
+  it 'gives feedback on incorrect answers' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    turn.card
+    expect(turn.guess).to eq("Saturn")
+    expect(turn.correct?).to eq(false)
+    expect(turn.guess).to eq("Incorrect.")
+  end
 end
