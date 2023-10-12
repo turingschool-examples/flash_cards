ea8cdb4fe9cbcb7a23d3963b46656a7d619798e2
Sam Walker <sgwalker327@gmail.com>
2023-10-12 17:11:49 -0600

    Made Turn class and initialize method as well as associated test

---
+++ spec/turn_spec.rb
@@ -0,0 +1,29 @@
+require './lib/card'
+require './lib/turn'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  xit 'has a question' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card.question).to eq("What is the capital of Alaska?")
+  end
+
+  xit 'has an answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card.answer).to eq("Juneau")
+  end
+
+  xit 'has a category' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card.category).to eq(:Geography)
+  end
+end
