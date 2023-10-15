da96db7493ec74b22fe5226f1b1b3603d291e698
Sam Walker <sgwalker327@gmail.com>
2023-10-15 15:13:23 -0600

    Wrote tests for feedback and correct? turn methods

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -18,4 +18,24 @@ RSpec.describe Turn do
   it 'has a card' do
     expect(@turn.card).to be_an_instance_of(Card)
   end
+
+  it 'is correct' do
+    expect(@turn.correct?).to eq(true)
+  end
+
+  it 'is incorrect' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.correct?).to eq(false)
+  end
+
+  it 'provides feedback that an answer is Incorrect' do
+    expect(@turn.feedback).to eq("Correct!")
+  end
+
+  it 'provides feedback that an answer is Incorrect' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.feedback).to eq("Incorrect.")
+  end
 end
