aab754643f66592b27ea737c490de88a158fe2d3
Sam Walker <sgwalker327@gmail.com>
2023-10-15 15:08:54 -0600

    Create correct and feedback methods for Turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,20 @@
 class Turn
   attr_reader :guess, :card
+  
   def initialize(guess, card)
     @guess = guess
     @card = card
+  end
+
+  def correct?
+    @guess.upcase == @card.answer.upcase
+  end
+
+  def feedback
+    if correct?
+      "Correct!"
+    else
+      "Incorrect."
+    end
   end
 end
