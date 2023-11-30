31355940747194f6ecdc49905509217fad82e8d6
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 15:10:19 -0700

    Require Card class file and create a card object inside te exist test for Turn class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,9 +1,11 @@
 require './lib/turn.rb'
+require './lib/card.rb'
 
 RSpec.describe Turn do
     it 'exists' do
-        turn = turn.new("Juneau", card)
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
 
-        expect(turn).to_be_an_instance_of(Turn)
+        expect(turn).to be_an_instance_of(Turn)
     end
 end
