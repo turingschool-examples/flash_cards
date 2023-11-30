a8dc87d8e3f0c0747a3804883419ef9031293390
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 15:19:10 -0700

    Create the evaluates correct? when true test for Turn Class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Turn do
 
         expect(turn).to be_an_instance_of(Turn)
     end
+
+    it 'evaluates correct? when true' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        turn.correct?
+
+        expect(turn.correct?).to eq (true)
+    end
 end
