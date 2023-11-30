27ca3fbee8bbd764c7770f30b73e340319c0a961
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-30 14:33:09 -0800

    Add code to create correct? method

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,12 @@ class Turn
         @guess = guess
         @card = card
     end
+
+    def correct?
+        if @guess == @card.answer
+            true
+        else
+            false
+        end
+    end
 end
