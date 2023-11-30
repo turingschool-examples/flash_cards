d7de1aa02e14597e593c69bcde9fae490eea9f80
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 15:57:51 -0700

    Create the gives feedback when correct and when incorrect tests for Turn Class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -33,4 +33,24 @@ RSpec.describe Turn do
 
         expect(turn.correct?).to eq (false)
     end
+
+    it 'gives feedback when correct' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        turn.correct?
+        turn.feedback
+
+        expect(turn.feedback).to eq ("Correct!")
+    end
+
+    it 'gives feedback when incorrect' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Denver", card)
+
+        turn.correct?
+        turn.feedback
+
+        expect(turn.feedback).to eq ("Incorrect.")
+    end
 end
