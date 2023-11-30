f50519dec173afc0df5fff385ba48a78d88333a6
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-11-30 16:43:33 -0500

    Create some more tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -25,9 +25,20 @@ RSpec.describe Turn do
     expect(turn.correct?).to eq true
   end
 
+  it "can test if the guess is incorrect" do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.correct?).to eq false
+  end
+
   it "can give feedback" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    expect(turn.feedback).to eq "Correct!"
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn_1 = Turn.new("Juneau", card_1)
+
+    card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn_2 = Turn.new("Saturn", card_2)
+
+    expect(turn_1.feedback).to eq "Correct!"
+    expect(turn_2.feedback).to eq "Incorrect."
   end
 end
