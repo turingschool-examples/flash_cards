e0359aa0c1b714c9715f6a58d053fbebb6d89666
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 15:30:44 -0700

    Create the reads cards test test for Turn Class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe Turn do
         expect(turn).to be_an_instance_of(Turn)
     end
 
+    it 'reads cards' do
+        card0 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn0 = Turn.new("Juneau", card0)
+
+        expect(turn0.card).to eq (card0)
+    end
+
     it 'evaluates correct? when true' do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         turn = Turn.new("Juneau", card)
