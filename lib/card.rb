b3933548bfb4ade6316c21d5a8ddee8571e8a945
Joey Reyes <joeyreyes105@gmail.com>
2023-11-30 14:19:50 -0800

    Update card.rb add attr_reader

--- lib/card.rb
+++ lib/card.rb
@@ -1,22 +1,9 @@
 class Card 
-    attr_reader 
+    attr_reader :question, :answer, :category
 
     def initialize(question, answer, category)
         @question = question 
         @answer = answer 
         @category = category 
     end 
-
-    def question 
-        @question = "What is the capital of Alaska?"
-    end 
-
-    def answer 
-        @answer = "Juneau"
-    end 
-
-    def category
-        @category = :Geography 
-    end 
-
 end     
