d3e0d64b2541b2b617149ebdbc0f8758aa2b2081
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 18:16:58 -0700

    Create the exists test for Round class

---
+++ spec/round_spec.rb
@@ -0,0 +1,16 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+RSpec.describe Round do
+    it 'exists' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        expect(round).to be_an_instance_of Round
+    end
+end
