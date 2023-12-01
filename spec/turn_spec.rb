bdc59e04deabaa424fbcee1bd06bd9b3d7122ffa
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-01 10:48:23 -0500

    Update spaces to make assertions more readable

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,30 +4,35 @@ RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
+    
     expect(turn).to be_instance_of(Turn)
   end
 
   it "has a card" do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
+
     expect(turn.card).to eq card
   end
 
   it "have a guess" do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
+
     expect(turn.guess).to eq "Juneau"
   end
 
   it "can attest is the guess is correct" do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
+
     expect(turn.correct?).to eq true
   end
 
   it "can test if the guess is incorrect" do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
+
     expect(turn.correct?).to eq false
   end
 
