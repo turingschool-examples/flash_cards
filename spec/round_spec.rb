41ce26a63e2356c30b251cd0fd40858971c2e8ed
Joey Reyes <joeyreyes105@gmail.com>
2023-11-30 20:55:59 -0800

    Add round.rb and round_spec.rb

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,7 +1,22 @@
-require 
+require "./lib/round"
+require "./lib/deck"
+require "./lib/turn"
+require "./lib/card"
 
-RSpec.describe Turn do 
+RSpec.describe Turn do
+    before(:each) do
+        @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @turn = Turn.new("Juneau", @card)
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards) 
+    end 
+
     it "exists" do 
-    
+        round = Round.new(deck) 
+
+        expect(round).to be_instance_of(Round)
     end 
 end 
