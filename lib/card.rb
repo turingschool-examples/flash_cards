4ced46e18f2e6249fd2bc165dedfdccac62c56da
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-01 14:50:17 -0500

    Question method

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,14 @@
 class Card
+    attr_reader :question, :answer, :category
+
     def initialize(question, answer, category)
+        @question = question
+        @answer = answer
+        @category = category
+    end
+
+    def Question?
+        "What is the capital of #{}"
     end
 end
 
-class Turn
-end
