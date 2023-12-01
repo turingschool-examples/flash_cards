8964ed3570b251303f6d1b97d4be6b8a8c71d6a0
Dana Howell <howelld115@gmail.com>
2023-11-30 17:58:33 -0700

    Complete iteration 1 adding #feedback and testing for incorrect guesses

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,20 +4,32 @@ require './lib/turn'
 
 RSpec.describe Turn do 
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn1 = Turn.new("Juneau", card1)
 
-    expect(turn).to be_a(Turn)
+    expect(turn1).to be_a(Turn)
   end
 
   it 'has @answer, @card, can #guess, can see if guess is #correct?, give #feedback' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card) 
-     
-    expect(turn.guess).to eq("Juneau")
-    expect(turn.correct?).to be(true)  
-    expect(turn.feedback).to eq("Correct!")
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn1 = Turn.new("Juneau", card1) 
+
+    expect(turn1.guess).to eq("Juneau")
+    expect(turn1.correct?).to be(true)  
+    expect(turn1.feedback).to eq("Correct!")
   end 
+
+  it "can test for incorrect guesses" do
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn1 = Turn.new("Juneau", card1)
+    turn2 = Turn.new("Saturn", card2)
+
+    expect(turn2.card).to eq(card2)
+    expect(turn2.guess).to eq("Saturn")
+    expect(turn2.correct?).to be false
+    expect(turn2.feedback).to eq("Incorrect")
+  end
 end
 
 
