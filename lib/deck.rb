9ae3a28e4db93e2e26956be14e80a8b5b0f274e6
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 18:09:57 -0700

    Create the cards_in_category method for Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,23 @@
+require './lib/card.rb'
+
 class Deck
-    attr_reader :card
+    attr_reader :cards
     
-    def initialize (card)
-        @card = card
+    def initialize (cards)
+        @cards = cards
     end
 
+    def cards_in_category(cat)
+        cards_in_cat = []
+        
+        @cards.each_index do |index|
+           
+            if (@cards[index].category) == cat
+                cards_in_cat << @cards[index]
+            end
+       
+        end
+        
+        cards_in_cat
+    end
 end
