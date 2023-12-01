a68e95b34ad5679194ad84cd7c2cd2f7840b6fe0
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 18:23:15 -0700

    Create the attribute turns and instantiating as an empty array when a round is created

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,13 @@
 require './lib/deck'
 
 class Round
-    attr_accessor :deck
+    attr_accessor :deck,
+                  :turns
 
     def initialize (deck)
         @deck = deck
+        @turns = []
     end
-    
+
 
 end
