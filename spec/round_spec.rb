c7cef378589c5d5cdcf1ba96c80380060d94fa8b
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 18:37:28 -0700

    Create the returns the correct card being played test for Round class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,33 +4,28 @@ require './lib/deck'
 require './lib/round'
 
 RSpec.describe Round do
-    it 'exists' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new([card_1, card_2, card_3])
-        round = Round.new(deck)
+    before(:each) do
+        #setup for tests below
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        @deck = Deck.new([card_1, card_2, card_3])
+        @round = Round.new(deck)
+    end
 
+    it 'exists' do
         expect(round).to be_an_instance_of Round
     end
 
     it 'stores a deck' do 
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new([card_1, card_2, card_3])
-        round = Round.new(deck)
-
         expect(round.deck).to eq (deck)
     end
 
     it 'starts with no turns' do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new([card_1, card_2, card_3])
-        round = Round.new(deck)
-
         expect(round.turns).to eq([])
+    end
+
+    it 'returns the correct card being played' do
+        expect(round.current_card).to eq (card_1)
     end
 end
