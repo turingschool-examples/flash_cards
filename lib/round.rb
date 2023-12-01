93725a1a9eb31750a164a6d6a0e497afc9783e56
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-01 07:45:00 -0800

    Add code to take_turn method to work for additional testing

--- lib/round.rb
+++ lib/round.rb
@@ -16,6 +16,16 @@ class Round
     end
 
     def take_turn(guess)
-        Turn.new(guess, (@deck.cards.first))
+        taken_turn = Turn.new(guess, (@deck.cards.first))
+        @turns << taken_turn
+        @turns[-1]
     end
+
+    # I need to have the take turn method accept a string argument as the guess,
+    # then create a new instance of Turn using that guess and the current card
+    # Once that instance of turn is created, the take_turn method should return
+    # that Turn. At the same time, it needs to add that turn into the @turns array
+    # while also then taking the current card and pushing it to the back of the deck
+    # i might need some separate methods I wasn't thinking I would need to call
+    # within the take turn method
 end
