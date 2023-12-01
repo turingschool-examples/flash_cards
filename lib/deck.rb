27b3c580adf6353efd2a1923977a8ecaa8f34a38
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-30 16:51:48 -0800

    Add code for cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,4 +11,17 @@ class Deck
     def count
         @cards.count
     end
+# for cards in category we will need to dig into the array of cards that we have
+# then identify any cards that fall into the same category as the argument
+# then will need to return all the cards that fit in that category in a new array
+
+    def cards_in_category(category)
+        matched_cards = []
+        @cards.each do |card|
+            if card.category == category
+                matched_cards << card
+            end
+        end
+        matched_cards
+    end
 end
