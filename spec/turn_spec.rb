c4b911133cf18b7d65aa2dd17250e629bce95a5f
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-11-30 18:45:08 -0700

    Create test2

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,9 +5,16 @@ require './lib/turn'
 describe Turn do 
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    it 'exist' do
+    it 'exists' do
        turn = Turn.new("Juneau", card)
 
        expect(turn).to be_instance_of(Turn)
     end 
+    
+    it 'has a guess' do 
+        turn = Turn.new("Juneau", card)
+        
+        expect(turn.guess).to eq("Juneau")
+    end
+
 
