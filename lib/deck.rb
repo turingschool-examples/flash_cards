f21157f77053d6fa3014c4252b27289b4825c5f5
Joey Reyes <joeyreyes105@gmail.com>
2023-11-30 18:25:50 -0800

    Create deck.rb, passed test

---
+++ lib/deck.rb
@@ -0,0 +1,8 @@
+class Deck 
+    attr_reader
+
+    def initialize(cards)
+        @cards = [@card_1, @card_2, @card_3]
+    end 
+
+end 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,8 +1,20 @@
-require 
+require "./lib/deck"
+require "./lib/turn"
+require "./lib/card"
 
 RSpec.describe Deck do 
-    it "exists" do 
+    before(:each) do
+        @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @turn = Turn.new("Juneau", @card)
+    end
     
+    it "exists" do 
+        deck = Deck.new(@cards) 
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        expect(deck).to be_instance_of(Deck) 
     end 
        
 end 
