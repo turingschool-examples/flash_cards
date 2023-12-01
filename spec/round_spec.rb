d4528ca86bb4373a5f8989c08d1e7b99931f5fd9
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 18:51:04 -0700

    create instance variables for the spec file as setup for the tests below

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -9,23 +9,23 @@ RSpec.describe Round do
         @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        @deck = Deck.new([card_1, card_2, card_3])
-        @round = Round.new(deck)
+        @deck = Deck.new([@card_1, @card_2, @card_3])
+        @round = Round.new(@deck)
     end
 
     it 'exists' do
-        expect(round).to be_an_instance_of Round
+        expect(@round).to be_an_instance_of Round
     end
 
     it 'stores a deck' do 
-        expect(round.deck).to eq (deck)
+        expect(@round.deck).to eq (@deck)
     end
 
     it 'starts with no turns' do
-        expect(round.turns).to eq([])
+        expect(@round.turns).to eq([])
     end
 
     it 'returns the correct card being played' do
-        expect(round.current_card).to eq (card_1)
+        expect(@round.current_card).to eq (@card_1)
     end
 end
