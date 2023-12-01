5b46d7328a0a882bc0400d1945eff71ba8aa949d
Joey Reyes <joeyreyes105@gmail.com>
2023-11-30 20:32:58 -0800

    Finish files with all tests passed

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,18 @@
 class Deck 
-    attr_reader
+    attr_reader :cards   
 
     def initialize(cards)
-        @cards = [@card_1, @card_2, @card_3]
+        @cards = cards 
+    end 
+
+    def cards_in_category(category)
+        @cards.select do 
+            |card| card.category == category
+        end  
+    end 
+
+    def count 
+        @cards.count 
     end 
 
 end 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,12 +9,26 @@ RSpec.describe Deck do
     end
     
     it "exists" do 
-        deck = Deck.new(@cards) 
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards) 
+
         expect(deck).to be_instance_of(Deck) 
     end 
+
+    it "has categories" do  
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        expect(deck.cards_in_category(:Geography)).to eq [card_1]
+        expect(deck.cards_in_category(:STEM)).to eq [card_2, card_3]
+        expect(deck.cards_in_category(:RandomCategory)).to eq []
+    end 
+        
        
 end 
