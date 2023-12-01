612ee8c225f947147f0013e66c809c0ba46241d7
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-30 18:21:03 -0700

    Create the stores a deck test for Round class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Round do
 
         expect(round).to be_an_instance_of Round
     end
+
+    it 'stores a deck' do 
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        expect(round.deck).to eq (deck)
+    end
 end
