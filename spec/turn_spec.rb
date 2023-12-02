872d6888822e486327dca2c6b6da847da323c9ed
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-02 14:09:30 -0500

    Add tests for the interaction patterns of turn.card, turn.guess, turn.correct?, turn.feedback

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,31 +1,64 @@
-require "lib/card"
+require "./lib/card"
 require "./lib/turn"
 
 RSpec.describe Turn do
     it 'exists' do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn = Turn.new("Juneu", card)
-        
+        turn = Turn.new("Juneau", card)
+       
+        expect(turn).to be_instance_of(Turn)
+    end
+
+    it 'has a card' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.card).to eq(card)
+    end
+
+    it 'has a guess' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.guess). to eq("Juneau")
+    end
+
+    it 'has a correct guess' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.correct?). to eq(true)
+    end
+
+    it 'provides feedback' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.feedback).to eq("Correct!")
+    end
+end
+
+
 # pry(main)> require './lib/turn'
 # #=> true
 
-pry(main)> require './lib/card'
-#=> true
+# pry(main)> require './lib/card'
+# #=> true
 
-pry(main)> card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
+# pry(main)> card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# #=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
 
-pry(main)> turn = Turn.new("Juneau", card)
-#=> #<Turn:0x00007f99842f0998 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:geography>, @guess="Juneau">
+# pry(main)> turn = Turn.new("Juneau", card)
+# #=> #<Turn:0x00007f99842f0998 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:geography>, @guess="Juneau">
 
-pry(main)> turn.card
-#=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
+# pry(main)> turn.card
+# #=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
 
-pry(main)> turn.guess
-#=> "Juneau"
+# pry(main)> turn.guess
+# => "Juneau"
 
-pry(main)> turn.correct?
-#=> true
+# pry(main)> turn.correct?
+# #=> true
 
-pry(main)> turn.feedback
-#=> "Correct!"
+# pry(main)> turn.feedback
+# #=> "Correct!"
