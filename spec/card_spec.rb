79bf959a9ce4da0621d530a9603fb219b1f07659
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-02 11:30:52 -0800

    add changes to card_spec.rb file

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,9 +1,10 @@
 require './lib/card'
+require 'pry'
 
 RSpec.describe Card do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
+  binding.pry
     expect(card).to be_instance_of(Card)
   end
 
