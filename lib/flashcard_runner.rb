0d995ec6d54878f7197dcd5ca4c986f889c3c5b1
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-01 21:20:05 -0800

    Instantiate necessary classes and card_count variable

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -3,3 +3,33 @@ require './lib/deck.rb'
 require './lib/turn.rb'
 require './lib/round.rb'
 
+# Remember to use .chomp after gets
+
+card_1 = Card.new("What is the first letter of the alphabet?", "A", :English)
+card_2 = Card.new("what is the last letter of the alphabet?", "Z", :English)
+card_3 = Card.new("What is the best kind of food?", "Pizza", :JackTrivia)
+card_4 = Card.new("What kind of car does Jack drive?", "Volkswagen", :JackTrivia)
+
+@cards = [card_1, card_2, card_3, card_4]
+
+@deck = Deck.new(@cards)
+
+@round = Round.new(@deck)
+
+@card_count = 0
+
+def start
+    puts "Welcome! You're playing with #{@cards.count} cards."
+    puts "-------------------------------------------"
+    (@cards.count).times do
+        @card_count += 1
+        puts "This is card number #{@card_count} out of #{@cards.count}."
+        puts @round.current_card.question
+    
+        input1 = gets.chomp
+        @round.take_turn(input1)
+        @round.turns[-1].feedback
+    end
+end
+
+start
