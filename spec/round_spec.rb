e2bfb0d8b6e90caca571bc73f51a0de910286cb0
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-02 08:33:55 -0800

    Add missing test for turns_by_category helper method

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -260,5 +260,29 @@ RSpec.describe Round do
         new_turn3 = round.take_turn("wrong answer")
 
         expect(round.percent_correct_by_category(:STEM)).to eq(50.0)
-    end 
+    end
+    
+    it 'can tell the amount of turns taken by category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        new_turn1 = round.take_turn('Juneau')
+
+        expect(round.turns_by_category(:Geography)).to eq(1)
+
+        new_turn2 = round.take_turn("Mars")
+
+        expect(round.turns_by_category(:STEM)).to eq(1)
+
+        new_turn3 = round.take_turn("wrong answer")
+
+        expect(round.turns_by_category(:STEM)).to eq(2)
+    end
 end
