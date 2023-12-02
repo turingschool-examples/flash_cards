a6710d306f0e616f387023549aea03c65fc6f09a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-02 16:29:27 -0700

    Comment out test execution and assertions

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -9,8 +9,10 @@ RSpec.describe Round do
         @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        @deck = Deck.new([@card_1, @card_2, @card_3])
+        @cards = [@card_1, @card_2, @card_3]
+        @deck = Deck.new(@cards)
         @round = Round.new(@deck)
+        
     end
 
     it 'exists' do
@@ -30,28 +32,34 @@ RSpec.describe Round do
     end
 
     it 'can create a Turn class object and store guess and card' do
-        new_turn = @round.take_turn("Juneau")
+        new_turn_1 = @round.take_turn("Juneau")
+
+        expect(new_turn_1).to be_an_instance_of (Turn)
+        expect(new_turn_1.guess).to eq ("Juneau")
+        expect(new_turn_1.card).to eq (@card_1)
         
-        expect(new_turn).to be_an_instance_of (Turn)
-        expect(new_turn.guess).to eq ("Juneau")
-        expect(new_turn.card).to eq (@card_1)
+        new_turn_2 = @round.take_turn("Venus")
+        expect(new_turn_2).to be_an_instance_of (Turn)
     end
     
     it 'can store each turn in the turns array' do
-        new_turn = @round.take_turn("Juneau")
-        
-        expect(@round.turns).to eq ([new_turn])
-    end
-
-    it 'move on to the next card in the deck and store turns information' do
         new_turn_1 = @round.take_turn("Juneau")
 
+        expect(@round.turns).to eq ([new_turn_1])
+
+        new_turn_2 = @round.take_turn("Venus")
+
+        expect(@round.turns).to eq ([new_turn_1, new_turn_2])
+        
+    end
+
+    it 'stores the guess in the new Turn object and moves on to the next card in the deck' do    
+        new_turn_1 = @round.take_turn("Juneau")
+        
         expect(@round.current_card).to eq (@card_2)
-        expect(turns).to eq ([new_turn_1])
 
-        @round.take_turn("z")
-
+        new_turn_2 = @round.take_turn("Venus")
+        
         expect(@round.current_card).to eq (@card_3)
-        expect(turns).to eq ([new_turn_1])
     end
 end
