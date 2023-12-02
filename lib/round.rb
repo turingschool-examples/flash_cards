a0980a372601cc453148d8da78cdc3e2af3ffec4
Dana Howell <howelld115@gmail.com>
2023-12-02 13:21:32 -0700

    Initializes Round, adds #current_card

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,12 @@
 class Round
-  attr_reader   
+  attr_reader :deck, :turns
   def initialize(deck)
     @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    deck.cards.first
   end
 end
 
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -13,57 +13,42 @@ RSpec.describe Round do
     round = Round.new(deck)
 
     expect(round).to be_a Round
-    expect(card_1).to eq()
   end
-end
 
+  it "have a #current_card which is first in the deck" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
     expect(round.deck).to eq(deck)
     expect(round.turns).to eq([])
+    expect(round.current_card).to eq(card_1)
+  end
+end  
 
-    expect(round.current_card
-#=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
 
-    expect(new_turn = round.take_turn("Juneau")
-#=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
+#     expect(new_turn = round.take_turn("Juneau")).to eq()
+# #=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
 
-    expect(new_turn.class
-#=> Turn
+#     expect(new_turn.class).to 
+# #=> Turn
 
-    expect(new_turn.correct?
-#=> true
+#     expect(new_turn.correct?) to be true
+#     expect(round.turns).to eq()
+# #=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
 
-    expect(round.turns
-#=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
+#     expect(round.number_correct).to eq(1)
+#     expect(round.current_card).to eq(card_2)
+#     expect(round.take_turn("Venus")).to eq()
+# #=> #<Turn:0x00007f972a215b38...>
 
-    expect(round.number_correct
-#=> 1
-
-    expect(round.current_card
-#=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
-
-    expect(round.take_turn("Venus")
-#=> #<Turn:0x00007f972a215b38...>
-
-    expect(round.turns.count
-#=> 2
-
-    expect(round.turns.last.feedback
-#=> "Incorrect."
-
-    expect(round.number_correct
-#=> 1
-
-    expect(round.number_correct_by_category(:Geography)
-#=> 1
-
-    expect(round.number_correct_by_category(:STEM)
-#=> 0
-
-    expect(round.percent_correct
-#=> 50.0
-
-    expect(round.percent_correct_by_category(:Geography)
-#=> 100.0
-
-    expect(round.current_card
-#=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
+#     expect(round.turns.count).to eq(2)
+#     expect(round.turns.last.feedback).to eq("Incorrect")
+#     expect(round.number_correct).to eq(1)
+#     expect(round.number_correct_by_category(:Geography)).to eq(1)
+#     expect(round.number_correct_by_category(:STEM)).to eq(0)
+#     expect(round.percent_correct).to eq(50.0)
+#     expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
+#     expect(round.current_card).to 
+# #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
