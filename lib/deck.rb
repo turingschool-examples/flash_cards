4d9fe3144b69b000644e00830ae7aa62adb621df
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-03 18:55:15 -0500

    Initial commit

---
+++ lib/deck.rb
@@ -0,0 +1,8 @@
+class deck
+    attr_reader :question, :answer, :category
+    
+    def initialize(question, answer, category)
+        @question = question
+        @answer = answer
+        @category = category
+    end
