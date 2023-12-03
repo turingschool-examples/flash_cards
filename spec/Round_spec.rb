443e5aeed04e50700d089cf98663749f9f9a3a5b
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-03 14:41:16 -0800

    add new_turn information to blocks

--- spec/Round_spec.rb
+++ spec/Round_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe Round do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        deck = Deck.new([card_1, card_2, card_3])
+        deck = Deck.new([card_1, card_2, card_3]) 
         round = Round.new(deck)
         new_turn = round.take_turn("Juneau")
        
