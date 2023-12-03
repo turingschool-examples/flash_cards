a813dca1da73c53e25e65f9a9311332e561a768c
Jessica Kohl <kohl.jess45@gmail.com>
2023-12-03 17:52:26 -0600

    listing turns test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -14,5 +14,13 @@ RSpec.describe Round do
         expect(round).to be_instance_of(Round)
     end
 
-    it ""
+    it "lists turns taken so far" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+       expect(round.turns).to eq([])
+    end
 end
