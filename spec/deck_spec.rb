47bf9079b10b38b4c4735b46e5e3dbf082b00ad5
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-12-03 07:12:25 -0500

    Write a test for deck

---
+++ spec/deck_spec.rb
@@ -0,0 +1,25 @@
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+    it 'has a deck' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        expect(deck.cards).to eq(cards)
+        expect(deck.count).to eq(3)
+    end
+
+    it 'has a category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+        expect(deck.cards_in_category(:Geography)).to eq([card_1])
+        expect(deck.cards_in_category("Pop Culture")).to eq([])
+    end
+end
