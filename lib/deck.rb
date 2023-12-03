b2880796fcaa9d2fbd4e8c95f91dd5f013c0a7c5
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-02 22:33:16 -0700

    Filled in more class info.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,14 @@
 class Deck 
-  attr_reader :
+  attr_reader :card_1, :card_2, :card_3
 
-  def initialize()
-    @
+  def initialize(card_1, card_2, card_3)
+    @card_1 = card_1
+    @card_2 = card_2
+    @card_3 = card_3
   end
 
-  def method
-    text
-  end
+  # def method
+  #   text
+  # end
 
 end
