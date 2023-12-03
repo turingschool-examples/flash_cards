604730be200842e5ff6ff10440fe5ffbcc37d27d
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-02 20:35:28 -0700

    Added interaction pattern.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -13,3 +13,42 @@ RSpec.describe Deck do
   end
 
 end
+
+
+
+
+
+# pry(main)> require './lib/card'
+# #=> true
+
+# pry(main)> require './lib/deck'
+# #=> true
+
+# pry(main)> card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+# #=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
+
+# pry(main)> card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+# #=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
+
+# pry(main)> card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+# #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
+
+# pry(main)> cards = [card_1, card_2, card_3]
+
+# pry(main)> deck = Deck.new(cards)
+# #=> #<Deck:0x00007fa160a38ed8...>
+
+# pry(main)> deck.cards
+# #=> [#<Card:0x00007fa16104e160...>, #<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
+
+# pry(main)> deck.count
+# #=> 3
+
+# pry(main)> deck.cards_in_category(:STEM)
+# #=> [#<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
+
+# pry(main)> deck.cards_in_category(:Geography)
+# #=> [#<Card:0x00007fa16104e160...>]
+
+# pry(main)> deck.cards_in_category("Pop Culture")
+# #=> []
