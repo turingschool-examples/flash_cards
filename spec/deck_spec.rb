482affe4fd57e3e1909d2152ecbacf069bc16fe2
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-03 15:44:03 -0700

    Filled in it block templates.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -31,57 +31,30 @@ RSpec.describe Deck do
     expect(deck.count).to eq(3)
   end
 
-  it "" do 
+  it "displays the STEM category" do 
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    expect().to eq()
+    expect(deck.cards_in_category(:STEM)).to eq(card_2, card_3)
   end
 
-  it "" do 
+  it "displays the Geography category" do 
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    expect().to eq()
+    expect(deck.cards_in_category(:Geography)).to eq(card_1)
   end
 
-  it "" do 
+  it "displays the Pop Culture category" do 
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     cards = [card_1, card_2, card_3]
     deck = Deck.new(cards)
-    expect().to eq()
+    expect(deck.cards_in_category("Pop Culture")).to eq([])
   end
-
-  it "" do 
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    expect().to eq()
-  end
-
 end
-
-
-
-
-# pry(main)> deck.cards
-# #=> [#<Card:0x00007fa16104e160...>, #<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
-
-
-
-# pry(main)> deck.cards_in_category(:STEM)
-# #=> [#<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
-
-# pry(main)> deck.cards_in_category(:Geography)
-# #=> [#<Card:0x00007fa16104e160...>]
-
-# pry(main)> deck.cards_in_category("Pop Culture")
-# #=> []
