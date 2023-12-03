66a85367016f15c937bb883df922e59fffc34b17
Hoa Dam <hoangocdam@gmail.com>
2023-12-02 22:46:53 -0800

    test initialize

---
+++ spec/round_spec.rb
@@ -0,0 +1,36 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+require 'rspec'
+
+RSpec.describe Round do
+  let(:card_1) { Card.new("What is the capital of Alaska?", "Juneau", :Geography) }
+  let(:card_2) { Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM) }
+  let(:card_3) { Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM) }
+  let(:deck) { Deck.new([card_1, card_2, card_3]) }
+
+  describe '#initialize' do
+    it 'is an instance of Round' do
+      deck = Deck.new([])
+      round = Round.new(deck)
+
+      expect(round).to be_a Round
+    end
+
+    it 'has a deck' do
+      deck = Deck.new([])
+      round = Round.new(deck)
+
+      expect(round.deck).to be_a (Deck)
+    end
+
+    it 'has turns' do
+      deck = Deck.new([])
+      round = Round.new(deck)
+      expect(round.turns).to eq ([])
+    end
+  end
+
+
+end
