14b500a552652d24e69892368917f810f3dd895b
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-03 10:53:17 -0800

    Clean up spacing and move notes

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,9 +11,6 @@ class Deck
     def count
         @cards.count
     end
-# for cards in category we will need to dig into the array of cards that we have
-# then identify any cards that fall into the same category as the argument
-# then will need to return all the cards that fit in that category in a new array
 
     def cards_in_category(category)
         matched_cards = []
