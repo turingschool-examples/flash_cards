7110c31ef79aef53a658885a17f3c31aecad8b90
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-02 20:03:03 -0700

    Made skeletons.

---
+++ lib/deck.rb
@@ -0,0 +1,12 @@
+class Deck 
+  attr_reader :
+
+  def initialize()
+    @
+  end
+
+  def method
+    text
+  end
+
+end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,15 @@
+require "rspec"
+require "./lib/card"
+require "./lib/turn"
+require "./lib/deck"
+
+RSpec.describe Deck do
+  it "exists" do
+    text
+  end
+
+  it "" do 
+    text 
+  end
+
+end
