df901f2f7235864225342995048cf4fba1e557da
Hoa Dam <hoangocdam@gmail.com>
2023-12-02 22:09:09 -0800

    feat collection_card

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,4 +5,16 @@ class Deck
   def initialize(collection_card)
     @collection_card = collection_card
   end
+
+  def add_card(card)
+    @collection_card << card
+  end
+
+  def count
+    @collection.card.length
+  end
+
+  def cards_in_category(category)
+    @collection_card.find_all {|card| card.category == category}
+  end
 end
