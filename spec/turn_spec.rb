c315cb54e7e6d9749f57f6b7168764070f8da8ec
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-03 15:42:42 -0700

    Fixed spacing.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,50 +3,51 @@ require "./lib/card"
 require "./lib/turn"
 
 RSpec.describe Turn do
-    it "exists" do
-     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     turn = Turn.new("Juneau", card)
-     expect(turn).to be_instance_of(Turn)
-   end
+  it "exists" do
+   card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+   turn = Turn.new("Juneau", card)
+   expect(turn).to be_instance_of(Turn)
+  end
 
-    it "makes a guess" do 
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Juneau", card)
-      expect(turn.guess).to eq("Juneau")
-    end 
-
-    it "is correct" do 
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Juneau", card)
-      expect(turn.correct?).to be(true)
-    end
-
-    it "gives feedback" do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Juneau", card)
-      expect(turn.feedback).to eq("Correct!")
-    end
-
-
-    it "guesses wrong" do 
-      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-      turn = Turn.new("Saturn", card)
-      expect(turn.guess).to eq("Saturn") 
-    end
-
-    it "is false" do
-      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-      turn = Turn.new("Saturn", card)
-      expect(turn.correct?).to eq(false)
-    end
-
-    it "gives incorrect feedback" do
-      card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-      turn = Turn.new("Saturn", card)
-      expect(turn.feedback).to eq("Incorrect.")
-    end
-
+  it "makes a guess" do 
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
   end 
+
+  it "is correct" do 
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to be(true)
+  end
+
+  it "gives feedback" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq("Correct!")
+  end
+
+
+  it "guesses wrong" do 
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.guess).to eq("Saturn") 
+  end
+
+  it "is false" do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.correct?).to eq(false)
+  end
+
+  it "gives incorrect feedback" do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+end
+
+
 
 
 # pry(main)> turn.card
