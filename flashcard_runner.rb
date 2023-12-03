ec1518190c2b9e74b16a1ca8f51a7cf5f9ea5afb
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-02 23:07:23 -0700

    Fix last part of final results, version 1

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,11 +6,11 @@ require './lib/round'
 
 #Setup
 card_0 = Card.new("What is 5 + 5?", "10", :STEM)
-card_1 = Card.new("What is Isaac's favorite animal?", "beaver", :TuringStaff)
-card_2 = Card.new("What is Mike's middle name?", "nobody knows", :TuringStaff)
-card_3 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :TuringStaff)
+card_1 = Card.new("What is Isaac's favorite animal?", "beaver", :Turing_Staff)
+card_2 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
+card_3 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
 cards = [card_0, card_1, card_2, card_3]
-deck = Deck.new(cards)
+deck = Deck.new([card_0, card_1, card_2, card_3])
 round = Round.new(deck)
 
 puts "Welcome! You're playing with #{deck.cards.count} cards."
@@ -28,8 +28,8 @@ cards_total.times do
 end
 
 puts "****** Game over! ******"
-puts "You had #{round.number_correct} correct guesses out of #{round.turns.count} for a total score of #{round.percent_correct}."
+puts "You had #{round.number_correct} correct guesses out of #{round.turns.count} for a total score of #{round.percent_correct}%."
 
-cards_total.times do
-    puts "#{round.turns.card.category} - #{percent_correct_by_category} correct"
-end
+puts "STEM - #{round.percent_correct_by_category(:STEM)}% correct"
+puts "Turing Staff - #{round.percent_correct_by_category(:Turing_Staff)}% correct"
+puts "Pop culture - #{round.percent_correct_by_category(:Pop_Culture)}% correct"
