91eaba4b450c69cd3e756316e0a5685d8c622c33
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-12-03 07:11:13 -0500

    Create a new class for deck

---
+++ lib/deck.rb
@@ -0,0 +1,14 @@
+class Deck
+    attr_reader :cards
+    def initialize(cards)
+        @cards = cards
+    end
+
+    def count 
+        cards.count
+    end
+
+    def cards_in_category(category)
+        cards.select {|card| card.category == category}
+    end
+end
