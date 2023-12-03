c70801992bc0541809e566e7d4b499a0539be113
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-03 00:22:17 -0700

    Create can return an array of the cards classes test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -14,16 +14,21 @@ RSpec.describe Deck do
         expect(@deck).to be_an_instance_of (Deck)    
     end
 
-    it 'stores cards' do
+    it "stores cards" do
         expect(@deck.cards).to eq ([@card_1,@card_2,@card_3])
     end
 
-    it 'can return cards based from its category' do
+    it "can return cards based from its category" do
         expect(@deck.cards_in_category(:STEM)).to eq ([@card_2, @card_3])
     end
 
-    it 'can return an empty array when no cards have been created in the category' do
+    it "can return an empty array when no cards have been created in the category" do
         expect(@deck.cards_in_category("Pop Culture")).to eq ([])
+    end
+
+    it "can return an array of the cards classes" do
+        expect(@deck.categories_in_deck).to eq ([:Geography, :STEM])
+
     end
 
 end
