39c24616eff65691014a59ed7e38b405e4180bba
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-03 01:17:20 -0700

    change to iterate directly over category to avoid use of indexes

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,9 +6,9 @@ require './lib/round'
 
 #Setup
     card_0 = Card.new("What is 5 + 5?", "10", :STEM)
-    card_1 = Card.new("What is Isaac's favorite animal?", "beaver", :Turing_Staff)
-    card_2 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
-    card_3 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+    card_1 = Card.new("What is Rachel's favorite animal?", "beaver", :Turing_Staff)
+    card_2 = Card.new("What is Mike's middle name?", "Red Panda", :Turing_Staff)
+    card_3 = Card.new("What cardboard cutout lives at Turing?", "Justin bieber", :Pop_Culture)
     cards = [card_0, card_1, card_2, card_3]
     deck = Deck.new([card_0, card_1, card_2, card_3])
     round = Round.new(deck)
@@ -33,10 +33,8 @@ end
 puts "****** Game over! ******"
 puts "You had #{round.number_correct} correct guesses out of #{round.turns.count} for a total score of #{round.percent_correct}%."
 
-n = 0
-categories.count.times do
-    puts "#{categories[n]} - #{round.percent_correct_by_category(categories[n])}% correct"
-    n += 1
+categories.each do |category|
+    puts "#{category} - #{round.percent_correct_by_category(category)}% correct"
 end
 
 
