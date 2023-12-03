fbfecaa680b0842d821cf610bc969ca0fd1e65cb
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-02 20:49:56 -0700

    Adjust gets to remove /n with chop

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,7 +6,7 @@ require './lib/round'
 
 #Setup
 card_0 = Card.new("What is 5 + 5?", "10", :STEM)
-card_1 = Card.new("What is Isaac's favorite animal?", "Beaver", :TuringStaff)
+card_1 = Card.new("What is Isaac's favorite animal?", "beaver", :TuringStaff)
 card_2 = Card.new("What is Mike's middle name?", "nobody knows", :TuringStaff)
 card_3 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :TuringStaff)
 cards = [card_0, card_1, card_2, card_3]
@@ -22,14 +22,14 @@ cards_total.times do
     puts" This is card number #{n} out of #{cards_total}.
     Question: #{round.current_card.question}"
 
-    guess = gets
+    guess = gets.chop
     puts round.take_turn(guess).feedback
     n += 1
 end
-
+require 'pry'; binding.pry
 puts "****** Game over! ******"
 puts "You had #{round.number_correct} correct guesses out of #{round.turns.count} for a total score of #{round.percent_correct}."
 
-@round.turn.count do |guess|
-    puts "#{turn.card.category} - #{percent_correct_by_category} correct"
+@round.turns.count do |guess|
+    puts "#{turns.card.category} - #{percent_correct_by_category} correct"
 end
