2aa0da2059e1fd83484547e163972d7a4f5b84c6
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-03 17:03:13 -0700

    Make last modification to turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,19 +4,19 @@ class Turn
     attr_reader :guess, :card
 
     def initialize(guess, card)
-        @guess = guess 
-        @card = card 
+        @guess = guess
+        @card = card
     end
 
     def correct?
          @guess == @card.answer
     end
-    
+
     def feedback
-        if correct? 
+        if correct?
              "correct"
-        else 
+        else
              "incorrect"
         end
-    end 
+    end
 end
