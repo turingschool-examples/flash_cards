98d9736684a7b427dbf3057f525281c38c057276
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-04 17:08:59 -0500

    Make a test for the card count in deck class

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -14,7 +14,7 @@ Rspec.descibe Deck do
         expect(deck.cards).to equal (card1, card2, card3)
     end
 
-    it 'storing cards in a deck'
+    it 'storing cards in a deck' do
         card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -23,8 +23,16 @@ Rspec.descibe Deck do
         deck = Deck.new(cards)
 
         expect(deck.cards).to be (cards)
+    end
 
-    it 'has an amount of cards'
+    it 'has an amount of cards' do
+        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        expect(deck.count).to be (3)
+    end
+
 end
 
 
