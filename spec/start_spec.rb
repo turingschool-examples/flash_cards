1483778c8d8baa92df2689d886e97fc6f0286882
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 14:08:09 -0800

    test: Add test for round

--- spec/start_spec.rb
+++ spec/start_spec.rb
@@ -7,8 +7,34 @@ require 'pry'
 
 RSpec.describe do
     it 'exists' do
-        start = Start.new
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        start = Start.new(round)
 
         expect(start).to be_a(Start)
+    end
+
+    it 'can have a round' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        start = Start.new(round)
+        
+        expect(start.round).to eq(round)
     end
 end
