44e8ee681e93cef0666ac27b0f6a5805efe33298
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-04 10:55:33 -0500

    Create methods for Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,13 @@
 class Deck
     attr_reader :cards
     
-    def initialize(*card_objects)
-        @card_objects = card_objects
+    def initialize(cards)
+        @cards = cards
     end
+
+    def return_cards_based_on_category(category)
+        @cards.select { |card| card.category == category }
+    end
+
+    def cards
+end
