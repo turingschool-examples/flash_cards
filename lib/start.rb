1dcfc9b02229d34db2db57044d9a41520964dca7
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 14:30:16 -0800

    feat: Add start game message to start

--- lib/start.rb
+++ lib/start.rb
@@ -5,9 +5,15 @@ require './lib/round.rb'
 
 class Start
 
-    attr_reader :round
+    attr_reader :round, :deck, :cards
 
     def initialize(round)
         @round = round
+        @deck = @round.deck
+        @cards = @deck.cards
+    end
+
+    def start_game_message
+        p "Welcome! You're playing with #{@cards.count} cards.\n-------------------------------------------"
     end
 end
