391e8b70b32d2a5750040edf7830fbece597da63
Dana Howell <howelld115@gmail.com>
2023-12-03 20:10:37 -0700

    Add test for #take_turn and #number_correct_by_category

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,7 +2,7 @@ require 'pry'
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
-require './lib/round'
+require './lib/practice_round'
 
 RSpec.describe Round do
   before do
@@ -29,41 +29,39 @@ RSpec.describe Round do
     end
   end
 
-  describe '#new_turn' do
+  describe '#take_turn with #new_turn' do
     it 'can take a new_turn' do
       @new_turn = @round.take_turn("Juneau")
       
       expect(@new_turn).to be_a(Turn)
-      expect(@new_turn.correct?).to be true
     end
   end
+  # expect(@new_turn.correct?).to be true
   
   describe '#take_turn' do
-    it 'can take a turn' do
+    it 'can take a second turn' do
       @new_turn = @round.take_turn("Juneau")
-    
-      expect(@round.turns).to eq([@new_turn])
-      expect(@round.number_correct).to q(1)
-      expect(@round.current_card).to eq(card_2)
 
-      @round.take_turn("Venus")
+      expect(@round.turns).to eq([@new_turn])
+      expect(@round.number_correct).to eq(1)
+      expect(@round.current_card).to eq(@card_2)
       
+      @new_turn1 = @round.take_turn("Venus")
+      require 'pry' ; binding.pry
       expect(@round.turns.count).to eq(2)
       expect(@round.turns.last.feedback).to eq("Incorrect")
       expect(@round.number_correct).to eq(1)
-      expect(@round.current_card).to eq(card_3)
+      expect(@round.current_card).to eq(@card_3)
+    end
+  end
 
+  describe '#number_correct_by_category' do
+    it 'can determine #number_correct_by_category' do
+
+    end
+
+    it 'calculates #percent_corrrect and ' do
 
     end
   end
 end  
-
-
-
-    #   expect(@round.number_correct_by_category(:Geography)).to eq(1)
-    #   expect(@round.number_correct_by_category(:STEM)).to eq(0)
-    #   expect(@round.percent_correct).to eq(50.0)
-    #   expect(@round.percent_correct_by_category(:Geography)).to eq(100.0)
-    #   expect(@round.current_card).to 
-    #   #=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
-    # end
