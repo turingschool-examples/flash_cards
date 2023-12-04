9250c479bccf2338015db10610997f1d4746f66c
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-04 17:21:51 -0500

    Add tests for checking if deck has cards in specific category

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -22,7 +22,7 @@ Rspec.descibe Deck do
         cards = [card1, card2, card3]
         deck = Deck.new(cards)
 
-        expect(deck.cards).to be (cards)
+        expect(deck.cards).to equal (cards)
     end
 
     it 'has an amount of cards' do
@@ -30,8 +30,23 @@ Rspec.descibe Deck do
         card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-        expect(deck.count).to be (3)
+        expect(deck.count).to equal (3)
     end
+
+    it 'checks if the deck has cards in the :STEM category' do
+        card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        expect(deck.cards_in_category).to equal (card2, card3)
+    end
+
+    it 'checks if the deck has cards in the :Geography category' do
+        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+        expect(deck.cards_in_category).to equal (card1)
+    end
+
+    it 'checks if the deck has cards in the :Pop culture category' do
 
 end
 
