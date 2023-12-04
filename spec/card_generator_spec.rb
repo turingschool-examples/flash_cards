c8711ef01f219ca7688c4c314382cb418283612b
Hoa Dam <hoangocdam@gmail.com>
2023-12-04 00:18:16 -0800

    feat: read the text file

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -9,6 +9,13 @@ describe CardGenerator do
     end
   end
 
+  describe "#card_data" do
+    it "read the file and return the array of card data" do
+      card_generator = CardGenerator.new("cards.txt")
+
+      expect(card_generator.card_data).to eq ["What is the capital of Alaska?,Juneau,Geography", "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?,Mars,STEM", "Describe in words the exact direction that is 697.5° clockwise from due north?,North north west,STEM", "How many episodes does Friends sitcom have?,236 eps,Movies", "Who is the first Asian actor/actress to win Oscar?,Michelle Yeoh,Movies"]
+    end
+  end
 
 
 end
