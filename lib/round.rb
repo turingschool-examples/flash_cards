568f9776bc1be56c56281dcb45791c7b017251fc
Jessica Kohl <kohl.jess45@gmail.com>
2023-12-03 21:21:30 -0600

    track number correct/incorrect (total and by category), option to give number correct as percentage

--- lib/round.rb
+++ lib/round.rb
@@ -10,6 +10,8 @@ class Round
         @guess = guess
         @turns = []
         @active_card = 0
+        @correct_guesses = 0
+        @incorrect_guesses = 0
     end
 
     def current_card
@@ -20,6 +22,27 @@ class Round
         new_turn = Turn.new(guess, current_card)
         @turns.push(new_turn)
         @active_card = @active_card + 1
+        if new_turn.correct? == true
+            @correct_guesses = @correct_guesses + 1
+        else
+            @incorrect_guesses = @incorrect_guesses + 1
+        end
         new_turn
+    end
+
+    def number_correct
+        @correct_guesses
+    end
+
+    def number_correct_by_category(category)
+        @turns.count {|turn_result| turn_result.correct? && turn_result.card.category == category}
+    end
+
+    def percentage_correct
+        @correct_guesses.to_f / @turns.length.to_f * 100
+    end
+
+    def percent_correct_by_category(category)
+        number_correct_by_category(category) / @turns.count {|turn_result| turn_result.card.category == category}.to_f * 100
     end
 end
