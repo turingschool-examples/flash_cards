a6dc960bbc15f9fdd8636f5389f75069927b76a2
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-04 10:54:58 -0500

    Make two tests, one for cards in a deck, and one for storing cards in a deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,26 @@
 require './lib/card'
 require './lib/deck'
+
+Rspec.descibe Deck do
+    it "is a deck" do
+        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+    end
+
+    it 'storing cards in a deck'
+        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.cards).to be (cards)
+end
+
+
+
