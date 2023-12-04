91118d3d0daf23117bfd6cf44fe3668983ed467c
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2023-12-04 10:32:57 -0700

    added card in category

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,19 @@ class Deck
         @cards = cards
 
     end
+
+    def count
+        cards.count
+    end
+
+    def cards_in_category(category)
+         newarray = []
+        @cards.each do |card|
+            if card.category == category
+                 newarray << card
+            end
+        end 
+        newarray
+        
+    end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -17,6 +17,23 @@ RSpec.describe Deck do
         expect(deck.cards).to eq([card_1, card_2, card_3])
     end
 
+    it 'counts' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        expect(deck.count).to eq(3)
+    end
+
+    it 'returns_cards_in_category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        expect(deck.cards_in_category(:Geography)).to eq([card_1])
+    end
 
 
 
