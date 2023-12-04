61059e4df7d1b7bab5bb2bf3b998927096551546
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 14:17:09 -0800

    test: Add test for start game message

--- spec/start_spec.rb
+++ spec/start_spec.rb
@@ -37,4 +37,20 @@ RSpec.describe do
         
         expect(start.round).to eq(round)
     end
+
+    it 'can give the start message of the game' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        start = Start.new(round)
+
+        expect(start.start_game_message).to eq("Welcome! You're playing with #{@cards.count} cards.", "-------------------------------------------")
+    end
 end
