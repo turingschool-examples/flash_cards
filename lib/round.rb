da3db7494e8ad48ea064021c2b5b2a05a206ec97
Jessica Kohl <kohl.jess45@gmail.com>
2023-12-03 20:06:50 -0600

    current_card and take_turn methods added

--- lib/round.rb
+++ lib/round.rb
@@ -3,9 +3,23 @@ require "./lib/turn"
 require "./lib/deck"
 
 class Round
-    attr_reader :deck
+    attr_reader :deck, :turns, :guess
 
     def initialize(deck)
         @deck = deck
+        @guess = guess
+        @turns = []
+        @active_card = 0
+    end
+
+    def current_card
+        @deck.cards[@active_card]
+    end
+
+    def take_turn(guess)
+        new_turn = Turn.new(guess, current_card)
+        @turns.push(new_turn)
+        @active_card = @active_card + 1
+        new_turn
     end
 end
