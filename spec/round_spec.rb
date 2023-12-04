8da158db657b9215838837a8f672a0408cb685cd
Jessica Kohl <kohl.jess45@gmail.com>
2023-12-03 19:50:42 -0600

    test if guess is correct and that new_turn is of Turn class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -24,13 +24,25 @@ RSpec.describe Round do
        expect(round.turns).to eq([])
     end
 
-    it "states current card" do
+    it "new turn is of Turn class" do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         deck = Deck.new([card_1, card_2, card_3])
         round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
 
-       expect(round.current_card).to eq(card_1)
+       expect(new_turn).to be_instance_of(Turn)
+    end
+
+    it "is guess correct?" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+
+       expect(new_turn.correct?).to eq(true)
     end
 end
