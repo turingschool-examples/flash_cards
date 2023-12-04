e1a10722fd66c2a1ba5815200cc8065a51d1166a
Dana Howell <howelld115@gmail.com>
2023-12-04 15:17:29 -0700

    Creates new and simpler number_correct_by_category and completes #percent_correct

--- lib/practice_round.rb
+++ lib/practice_round.rb
@@ -24,34 +24,58 @@ class Round
   end
 
 
-      #@turns.card.category
-      #I need to iterate on @turns to isolate correct turns.
-        #isolate category
-        #count correct guesses cards with category
+  # will count the return value of iteration. 
   def number_correct_by_category(category)
-    correct_count = 0
-    require 'pry' ; binding.pry
-    @turns.each do |turn|
-      #need to include category somewhere
-      correct_count += 1 if turn.correct?
+    @turns.count do |turn|
+      turn.correct? && turn.card.category == category 
     end
-      correct_count
   end
 
-  # def percent_correct
-  #   @turns / self.number_correct
+  def percent_correct
+    (number_correct.to_f / @turns.count.to_f) * 100
+  end
 
-  #may need to change to float and/or numbers visible after decimal
-  # end
+  #   def percent_correct_by_category
+#     #iterate on @turns 
+#     require 'pry' ; binding.pry
+#     @turns / #self.number_correct_by_category
 
-  # def percent_correct_by_category
-    # iterate on @turns 
-    #@turns / self.number_correct_by_category
+#     may need to change to float and/or numbers visible after decimal
+#   end
 
-    #may need to change to float and/or numbers visible after decimal
-  # end
 end
+
+# end
 
   
     
-  
+      # #
+      # #I need to iterate on @turns to isolate correct turns.
+      #   #isolate category
+      #   #count correct guesses cards with category
+      #   def number_correct_by_category(category)
+      #     correct_categories_count = {}
+      #     @turns.each do |turn|
+      #       # if the turn is correct
+      #       # and has the correct category of category being passed in the argument,
+      #       # and our hash does not already have that category as a key
+      #       # the key/#value pair gets added to our hash with a value of 1
+      #       if turn.correct? && turn.card.category == category && !correct_categories_count.has_key?(category) 
+      #         correct_categories_count[turn.card.category] = 1
+      
+      #       #if turn is correct and the category already exists in our hash, then
+      #       # a number gets added to our hash value for that category
+      #       elsif turn.correct? && turn.card.category == category
+      #         correct_categories_count[turn.card.category] += 1
+      
+      #       # if the category matches the category being passed and the turn is not correct
+      #       # adds the category to the hash with a value of zero
+      #       elsif turn.card.category == category && !turn.correct? #second operator doesn't NEED to be here
+      #         correct_categories_count[category] = 0
+      #       end
+      #     end
+      #     correct_categories_count[category]
+      
+      #       #does the correct_count.include? category
+      #   end
+      # # 
