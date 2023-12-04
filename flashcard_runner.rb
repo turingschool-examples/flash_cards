73cb1bf52adcdaa2feff775e4ba9163c705e7401
Jessica Kohl <kohl.jess45@gmail.com>
2023-12-04 16:47:41 -0600

    creates cards, deck, & round. Also states welcome message.

---
+++ flashcard_runner.rb
@@ -0,0 +1,15 @@
+require "./lib/card"
+require "./lib/turn"
+require "./lib/deck"
+require "./lib/round"
+
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+deck = Deck.new([card_1, card_2, card_3])
+round = Round.new(deck)
+
+puts "Welcome! You're playing with #{deck.count} cards."
+
+
