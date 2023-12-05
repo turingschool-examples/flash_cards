8bace9e6b3e11661cf39955928a292e649190741
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 19:19:43 -0800

    feat: Add read_file to card generator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -2,5 +2,10 @@ class CardGenerator
 
     def initialize(file)
         @file = file
+        @file_data = []
+    end
+
+    def read_file
+        @file_data = File.read(@file).split("\n")
     end
 end
