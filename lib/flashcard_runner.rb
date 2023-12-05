b45a35c5eb6e869865091bf8368ecf82dfc47ec0
Kevin Gonzalez <kevinglz619@gmail.com>
2023-12-05 15:44:23 -0800

    added changes to game set up, set up game over section

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -4,64 +4,126 @@ require './lib/deck'
 require './lib/round'
 require 'pry'
 
-        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        @card_4 = Card.new("What is the capital of California?", "Sacramento", :Geography)
+
+        @card_1 = Card.new("What is 5 + 5?", "10", :STEM)
+        @card_2 = Card.new("What is Rachel's favorite animal?", "beaver", :Turing_Staff)
+        @card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
+        @card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
+        @cards = [@card_1, @card_2, @card_3, @card_4]
         @deck = Deck.new([@card_1, @card_2, @card_3, @card_4])
         @round = Round.new(@deck)
-        @card_counter = 1
-
-        def start
-            puts "Welcome! You're playing with #{@deck.cards.count} cards."
-            puts "-------------------------"
-            puts "This is card number #{@card_counter} out of #{@deck.cards.count}."
-            puts "Question: What is 5 + 5?"
-
-            answer = gets.chomp.to_i
-            if answer == 10
-              puts "Correct!"
-            else puts  "Nope, sorry!"
-                @card_counter += 1
-            end    
-
-            puts "This is card number #{@card_counter} out of #{@deck.cards.count}."
-            puts "Question: What is Rachel's favorite animal?"
-            answer = gets.chomp
-
-        if answer == "lion" || "Lion"
-            
-            puts "Correct!"
-            else puts  "Nope, sorry!"
-                @card_counter += 1
-            end
-
-            puts "This is card number #{@card_counter} out of #{@deck.cards.count}."
-            puts "Question: What is Mike's middle name?"
-            answer = gets.chomp
-
-        if answer == "no one knows." || "No one knows." || "no one knows"
-            puts "Correct!"
-            else puts  "Nope, sorry!"
-                @card_counter += 1
-            end
+        # @deck = deck.cards_in_category
+        cards_total = @deck.cards.count
+        counter = 1
 
 
-        puts "This is card number #{@card_counter} out of #{@deck.cards.count}."
-        puts "Question: What cardboard cutout lives at Turing?"
-    answer = gets.chomp
+        puts "Welcome! You're playing with #{cards_total} cards."
+        puts "-------------------------"
 
-    #Justin Bieber
+        cards_total.times do
+            puts " This is card number #{counter} out of #{cards_total}.
+            Question: #{@round.current_card.question}"
+            guess = gets.chomp 
+            counter += 1
 
-
-
-
+          turn =  @round.take_turn(guess)
+          puts turn.feedback
         end
+       
+        
+    def individual_category_count
+        individual_category = []
+        @cards.each do |card|
+        
+        
+    individual_category << card.category
+        end 
+        individual_category.uniq!
+    end
+    
+    puts "you had #{@round.number_correct} guesses out of #{cards_total} for a total score of #{@round.percent_correct}"
+individual_category_count.each do |category|
+    puts "#{category} - #{@round.percent_correct_by_category(category)}% correct"
+    end
+
+        #guesses correct percentage
+        #guesses correct percentage by category
+
+        
+            
+      #count number of cards for total
+      #account for how many categories
+      #for each card do total, this is card #, question, guess, counter and feedback
+      
+      
+      
+      
+      
+      
+      
+        # def start
+        #     puts "Welcome! You're playing with #{@deck.cards.count} cards."
+        #     puts "-------------------------"
+        #     puts "This is card number #{@card_counter} out of #{@deck.cards.count}."
+        #     puts "Question: What is 5 + 5?"
+
+        #     answer = gets.chomp.to_i
+        #     if answer == 10
+        #       puts "Correct!"
+        #       @correct_counter += 1
+        #     else puts  "Nope, sorry!"
+        #         @card_counter += 1
+        #     end    
+            #.each method on cards array in deck
+            # creates put statement "this is card x out of x"
+            #puts question on card 
+
+    #         puts "This is card number #{@card_counter} out of #{@deck.cards.count}."
+    #         puts "Question: What is Rachel's favorite animal?"
+    #         answer = gets.chomp
+
+    #     if answer == "lion" || "Lion"
+    #         puts "Correct!"
+    #         @correct_counter += 1
+    #         else puts  "Nope, sorry!"
+    #             @card_counter += 1
+    #         end
+
+    #         puts "This is card number #{@card_counter} out of #{@deck.cards.count}."
+    #         puts "Question: What is Mike's middle name?"
+    #         answer = gets.chomp
+
+    #     if answer == "no one knows." || "No one knows." || "no one knows"
+    #         puts "Correct!"
+    #         @correct_counter += 1
+    #         else puts  "Nope, sorry!"
+    #             @card_counter += 1
+    #         end
+
+
+    #     puts "This is card number #{@card_counter} out of #{@deck.cards.count}."
+    #     puts "Question: What cardboard cutout lives at Turing?"
+    #     answer = gets.chomp
+
+    #     if answer == "Justin Bieber" || "justin bieber"
+    #         puts "Correct!"
+    #         @correct_counter += 1
+    #     else puts "Nope, sorry"
+    #         @card_counter += 1
+    #     end
+        
+
+    # #Justin Bieber
+
+
+
+
+    #     end
 
 
 
 
         
 
-        start
+    #     start
     
