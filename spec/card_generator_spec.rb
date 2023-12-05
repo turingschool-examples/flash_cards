57c827b91c63d199e91cdcf993c82f07fcde31f7
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 19:23:56 -0800

    test: Add test for separate data to card generator

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -15,4 +15,11 @@ RSpec.describe CardGenerator do
 
         expect(cg.read_file).to eq(["What is 5 + 5?,10,STEM","What is Rachel's favorite animal?,red panda,Turing Staff","What is Mike's middle name?,nobody knows,Turing Staff","What cardboard cutout lives at Turing?,Justin bieber,PopCulture"])
     end
+
+    it 'can separate file data into separate arrays and portions' do
+        filename = "cards.txt"
+        cg = CardGenerator.new(filename)
+        cg.read_file
+
+        expect(cg.separate_data).to eq([["What is 5 + 5?", "10", "STEM"],["What is Rachel's favorite animal?", "red panda", "Turing Staff"],["What is Mike's middle name?", "nobody knows", "Turing Staff"], ["What cardboard cutout lives at Turing?", "Justin bieber", "PopCulture"]])
 end
