019fd2537c1abe07adcc11a900ff9a2a5be39550
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-05 01:25:07 -0700

    refactor: Split on comma and change of line, and finding cards

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,27 +1,26 @@
 require './card'
 
 class CardGenerator
-    attr_reader :cards, :file_lines
+    attr_reader :cards
 
     
     def initialize (filename)
         @cards = []
-        @file_lines = []
+        file_lines = []
 
-        filename.open
-        @file_lines = File.read("./#{filename}").chop.split(",")
-        
-        # File.readlines("./#{filename}").each do |line|
-        # @file_lines << line.chop.split(",")
-        # end
-        
+        file_lines = File.read("./#{filename}").split(/[,\n]/)      
 
-        # file_lines.each do |index|
-        #     file = file_lines.shift
-        #     card = Card.new((file.first),(file[1]),(file.last))
-        #     @cards << card
-        # end
-        # @cards
+        file_lines.each do |index|
+          question = file_lines[1]
+          file_lines.shift
+          answer = file_lines[1]
+          file_lines.shift
+          category = file_lines[1]
+          card = Card.new(question,answer,category)
+          @cards << card
+         end
+       
+        @cards
     end
     require 'pry'; binding.pry
    
