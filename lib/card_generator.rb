887c6127de47ae3c9fa1fd642ab3c09fd80559e7
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-05 01:00:02 -0700

    feat: Draft reading filename

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,9 +1,28 @@
-require 'cards.txt'
+require './card'
 
 class CardGenerator
-    attr_reader cards
+    attr_reader :cards, :file_lines
 
+    
     def initialize (filename)
-        @cards = filename.split(",").in_slices_of(3)
+        @cards = []
+        @file_lines = []
+
+        filename.open
+        @file_lines = File.read("./#{filename}").chop.split(",")
+        
+        # File.readlines("./#{filename}").each do |line|
+        # @file_lines << line.chop.split(",")
+        # end
+        
+
+        # file_lines.each do |index|
+        #     file = file_lines.shift
+        #     card = Card.new((file.first),(file[1]),(file.last))
+        #     @cards << card
+        # end
+        # @cards
     end
+    require 'pry'; binding.pry
+   
 end
