006d3382138fbe72ed7218e888c94e9cbf90c827
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-04 20:20:38 -0500

    Refactor and clean up code

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,74 +2,54 @@ require "./lib/card"
 require "./lib/turn"
 
 RSpec.describe Turn do
-    it 'checks if turn1 exists' do
-        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn1 = Turn.new("Juneau", card1)
-       
-        expect(turn1).to be_instance_of(Turn)
+    describe 'correct guesses' do
+        it 'checks if turn exists' do
+            card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            turn = Turn.new("Juneau", card)
+        
+            expect(turn).to be_instance_of(Turn)
+            expect(turn.card).to eq(card)
+            expect(turn.guess). to eq("Juneau")
+        end
+
+        it 'checks if card has a correct guess' do
+            card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            turn = Turn.new("Juneau", card)
+
+            expect(turn.correct?). to eq(true)
+        end
+
+        it 'provides feedback for turn guess' do
+            card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            turn = Turn.new("Juneau", card)
+
+            expect(turn.feedback).to eq("Correct!")
+        end
     end
 
-    it 'checks if turn2 exists' do
-        card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-        turn2 = Turn.new("Mercury", card2)
-       
-        expect(turn2).to be_instance_of(Turn)
-    end
+    describe 'incorrect guesses' do
+        it 'checks if turn exists' do
+            card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+            turn = Turn.new("Saturn", card)
+        
+            expect(turn).to be_instance_of(Turn)
+            expect(turn.card).to eq(card)
+            expect(turn.guess). to eq("Saturn")
+        end
 
-    it 'checks if turn1 has a card' do
-        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn1 = Turn.new("Juneau", card1)
+        it 'checks if card has a correct guess' do
+            card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+            turn = Turn.new("Saturn", card)
 
-        expect(turn1.card).to eq(card1)
-    end
+            expect(turn.correct?). to eq(false)
+        end
 
-    it 'checks if turn2 also has a card' do
-        card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-        turn2 = Turn.new("Mercury", card2)
+        it 'provides feedback for turn guess' do
+            card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+            turn = Turn.new("Saturn", card)
 
-        expect(turn2.card).to eq(card2)
-    end
-
-    it 'checks if card1 has an answer' do
-        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn1 = Turn.new("Juneau", card1)
-
-        expect(turn1.guess). to eq("Juneau")
-    end
-
-    it 'checks if card2 also has an answer' do
-        card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-        turn2 = Turn.new("Mercury", card2)
-
-        expect(turn2.guess). to eq("Mercury")
-    end
-
-    it 'checks if card1 has a correct guess' do
-        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn1 = Turn.new("Juneau", card1)
-
-        expect(turn1.correct?). to eq(true)
-    end
-
-        it 'checks if card2 has a correct guess' do
-        card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-        turn2 = Turn.new("Mercury", card2)
-
-        expect(turn2.correct?). to eq(true)
-    end
-
-    it 'provides feedback for turn1 guess' do
-        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn1 = Turn.new("Juneau", card1)
-
-        expect(turn1.feedback).to eq("Correct!")
-    end
-
-        it 'provides feedback for turn2 guess' do
-        card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-        turn2 = Turn.new("Mercury", card2)
-
-        expect(turn2.feedback).to eq("Correct!")
+            expect(turn.feedback).to eq("Incorrect.")
+        end
     end
 end
 
