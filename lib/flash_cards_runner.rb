304f8b01dbdc25ab89a6ffdfc0be154208f17dd3
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-04 21:07:58 -0700

    Create new file

---
+++ lib/flash_cards_runner.rb
@@ -0,0 +1,10 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+require '../runner/flaskcard_runner'
+require './lib/card_generator'
+
+filename = "cards.txt"
+card_generator = CardGenerator.new(cards.txt)
+flashcards = card_generator.generate_cards
