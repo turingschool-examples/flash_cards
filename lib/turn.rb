c053f08a489a68efd89c6cfbfaddb567d6f1e1dd
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-05 02:59:08 -0700

    refactor: Correct lining and spacing

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,10 +14,12 @@ class Turn
     end
 
     def feedback
+       
         if @guess == @card.answer
             "Correct!"
         else
             "Incorrect."
         end
+        
     end
 end
