fe212900282f59c795767bfe417729b3713e94ba
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-05 16:58:18 -0500

    Refactor: remove unnecessary code, add before do test method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,32 +1,25 @@
 require './lib/turn'
 
 RSpec.describe Turn do
+  before(:each) do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @turn = Turn.new("Juneau", @card)
+  end
+
   it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-    
-    expect(turn).to be_instance_of(Turn)
+    expect(@turn).to be_instance_of(Turn)
   end
 
   it "has a card" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.card).to eq card
+    expect(@turn.card).to eq @card
   end
 
   it "have a guess" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.guess).to eq "Juneau"
+    expect(@turn.guess).to eq "Juneau"
   end
 
   it "can attest is the guess is correct" do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    expect(turn.correct?).to eq true
+    expect(@turn.correct?).to eq true
   end
 
   it "can test if the guess is incorrect" do
