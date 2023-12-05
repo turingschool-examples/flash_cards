51d54ae6832f7a71ed24fa83f727b3ac7008c065
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 19:15:20 -0800

    test: Add test for read_file to card generator

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe CardGenerator do
 
         expect(cg).to be_a(CardGenerator)
     end
+
+    it 'can read a text file' do
+        filename = "cards.txt"
+        cg = CardGenerator.new(filename)
+
+        expect(cg.read_file).to eq(["What is 5 + 5?,10,STEM","What is Rachel's favorite animal?,red panda,Turing Staff","What is Mike's middle name?,nobody knows,Turing Staff","What cardboard cutout lives at Turing?,Justin bieber,PopCulture"])
+    end
 end
