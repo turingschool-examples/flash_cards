ebef70f718557197272e0d036e22842445ab8fe5
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 16:31:32 -0800

    test: Add test for game over message to start

--- spec/start_spec.rb
+++ spec/start_spec.rb
@@ -87,4 +87,20 @@ RSpec.describe do
 
         expect(start.category_selector).to eq([:Geography, :STEM])
     end
+
+    it 'can read the game over message' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        start = Start.new(round)
+
+        expect(start.game_over_message).to eq("****** Game over! ******\nYou had 0 correct guesses out of 3 for a total score of 0.0%.\n:Geography - 0.0% correct\n:STEM - 0.0% correct")
+    end
 end
