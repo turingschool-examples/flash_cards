ed6fb6400dfee37f99a5bdc9b96d89625b9fea77
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 19:28:40 -0800

    feat: Add part of separate date method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -3,9 +3,16 @@ class CardGenerator
     def initialize(file)
         @file = file
         @file_data = []
+        @separate_data = []
     end
 
     def read_file
         @file_data = File.read(@file).split("\n")
+    end
+
+    def separate_data
+        @file_data.each do |raw_card_data|
+            @separate_data << raw_card_data.split(",")
+        end
     end
 end
