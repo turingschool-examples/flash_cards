0cd7e2a33e99cc80e80789c585d5a95893cd4603
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-04 21:12:16 -0500

    Change syntax throughout code to try and make tests pass

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,20 @@
 class Deck
-    attr_reader :cards
+    # attr_reader :cards
+    def initialize(card1, card2, card3)
+        @card1 = card1
+        @card2 = card2
+        @card3 = card3
+    end
     
-    def initialize(cards)
-        @cards = cards
-    end
-
-    def return_cards_based_on_category(category)
-        @cards.select { |card| card.category == category }
-    end
-
     def cards
-        @cards
+        [@card1, @card2, @card3]
+    end
+
+    def count
+        cards.length
+    end
+
+    def cards_in_category(category)
+        @cards.select { |card| card.category == category }
     end
 end
