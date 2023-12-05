f93a9e72322de06979f1060abbe0d82670abd1da
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 16:25:19 -0800

    feat: Add game rounds to start

--- lib/start.rb
+++ lib/start.rb
@@ -11,9 +11,22 @@ class Start
         @round = round
         @deck = @round.deck
         @cards = @deck.cards
+        @card_count = 0
     end
 
     def start_game_message
         p "Welcome! You're playing with #{@cards.count} cards.\n-------------------------------------------"
+    end
+
+    def game_rounds
+        (@cards.count).times do
+            @card_count += 1
+            puts "This is card number #{@card_count} out of #{@cards.count}."
+            puts @round.current_card.question
+        
+            input1 = gets.chomp
+            @round.take_turn(input1)
+            @round.turns[-1].feedback
+        end
     end
 end
