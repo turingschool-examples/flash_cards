e0780d0025a57b636fdbe61ad68d8ed102fbf846
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 16:27:24 -0800

    test: Add test for category selector

--- spec/start_spec.rb
+++ spec/start_spec.rb
@@ -72,5 +72,19 @@ RSpec.describe do
         expect(start.game_rounds).to eq("This is card number 1 out of 3.")
     end
 
-    
+    it 'can select unique categories' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        start = Start.new(round)
+
+        expect(start.category_selector).to eq([:Geography, :STEM])
+    end
 end
