1b1930c80283ea9d9fc1bed47f279f3c9e8cadd9
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-04 19:51:35 -0800

    feat: Add create cards method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,9 +1,12 @@
+require './lib/card.rb'
+
 class CardGenerator
 
     def initialize(file)
         @file = file
         @file_data = []
         @separate_data = []
+        @cards = []
     end
 
     def read_file
@@ -15,5 +18,12 @@ class CardGenerator
             @separate_data << raw_card_data.split(",")
         end
         @separate_data
+    end
+
+    def create_cards
+        @separate_data.each do |card_data|
+            @cards << Card.new(card_data[0], card_data[1], card_data[2])
+        end
+        @cards
     end
 end
