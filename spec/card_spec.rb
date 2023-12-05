a25be8510fac20c16dacf19d31e3f156dc3b77b0
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-05 16:56:40 -0500

    Refactor: remove unnecessary code, add before do test method

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,27 +1,23 @@
 require './lib/card'
 
 RSpec.describe Card do
-  it 'exists' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  before(:each) do
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  end
 
-    expect(card).to be_instance_of(Card)
+  it 'exists' do
+    expect(@card).to be_instance_of(Card)
   end
 
   it 'has a question' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.question).to eq("What is the capital of Alaska?")
+    expect(@card.question).to eq("What is the capital of Alaska?")
   end
 
   it 'has an answer' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.answer).to eq("Juneau")
+    expect(@card.answer).to eq("Juneau")
   end
 
   it 'has a category' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card.category).to eq(:Geography)
+    expect(@card.category).to eq(:Geography)
   end
 end
