dec82089cef6cccb835bc5cd2f6ba53af213833d
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-04 21:11:09 -0500

    Syntax to try and make passing tests, so far the last 3 tests do not pass

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,53 +2,62 @@ require './lib/card'
 require './lib/deck'
 require 'pry'
 
-Rspec.descibe Deck do
-    it "is a deck" do
+RSpec.describe Deck do
+    it "checks that deck exists" do
         card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-        cards = [card1, card2, card3]
-        deck = Deck.new(cards)
+        deck = Deck.new(card1, card2, card3)
 
-        expect(deck.cards).to equal (card1, card2, card3)
+        expect(deck).to be_an_instance_of(Deck)
     end
 
-    it 'storing cards in a deck' do
+    it 'stores cards in a deck' do
         card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-        cards = [card1, card2, card3]
-        deck = Deck.new(cards)
+        deck = Deck.new(card1, card2, card3)
 
-        expect(deck.cards).to equal (cards)
+        expect(deck.cards).to eq([card1, card2, card3])
     end
 
     it 'has an amount of cards' do
         card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        deck = Deck.new(card1, card2, card3)
 
-        expect(deck.count).to equal (3)
+        expect(deck.count).to eq(3)
     end
 
     it 'checks if the deck has cards in the :STEM category' do
+        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-        expect(deck.cards_in_category).to equal (card2, card3)
+        deck = Deck.new(card1, card2, card3)
+        expect(deck.cards_in_category).to eq([card2, card3])
     end
 
     it 'checks if the deck has cards in the :Geography category' do
         card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-        expect(deck.cards_in_category).to equal (card1)
+        card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        deck = Deck.new(card1, card2, card3)
+        expect(deck.cards_in_category).to eq([card1])
     end
 
     it 'checks if the deck has cards in the :Pop culture category' do
-
-        expect(deck.cards_in_category).to equal ()
+        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        deck = Deck.new(card1, card2, card3)
+        expect(deck.cards_in_category).to eq([])
     end
 end
 
