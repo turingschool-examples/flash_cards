2698c339438253713591449da367aea274a8921f
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-04 21:14:07 -0500

    Refactor code and delete erroneous code

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,51 +1,12 @@
 require './lib/card'
 
 RSpec.describe Card do
-  it 'checks if card1 exists' do
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  it 'checks if card exists and has attributes' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
-    expect(card1).to be_instance_of(Card)
-  end
-
-  it 'checks if card2 exists' do
-    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-
-    expect(card2).to be_instance_of(Card)
-  end
-
-  it 'checks if card1 has a question' do
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card1.question).to eq("What is the capital of Alaska?")
-  end
-
-  it 'checks if card2 has a question' do
-    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-
-    expect(card2.question).to eq("Which planet is closest to the sun?")
-  end
-
-  it 'checks if card1 has an answer' do
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card1.answer).to eq("Juneau")
-  end
-
-  it 'checks if card2 has an answer' do
-    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-
-    expect(card2.answer).to eq("Mercury")
-  end
-
-  it 'checks if card1 has a category' do
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-    expect(card1.category).to eq(:Geography)
-  end
-
-  it 'checks if card2 has a category' do
-    card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-
-    expect(card2.category).to eq(:STEM)
+    expect(card).to be_instance_of(Card)
+    expect(card.question).to eq("What is the capital of Alaska?")
+    expect(card.answer).to eq("Juneau")
+    expect(card.category).to eq(:Geography)
   end
 end
