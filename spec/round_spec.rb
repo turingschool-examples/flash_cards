2dcdc87fe4a755c009ee1b6bc7fa5a6177d0577f
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-12-05 17:15:48 -0500

    Make the first two tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,3 +2,25 @@ require './lib/card'
 require './lib/turn'
 require './lib/deck'
 require './lib/round'
+
+RSpec.describe Round do
+    it 'checks that a round exists' do
+        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        deck = Deck.new(card1, card2, card3)
+        round = Round.new(deck)
+
+        expect(round).to be_an_instance_of(Round)
+    end
+
+    it 'checks if the round is associated with the deck'
+        card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        deck = Deck.new(card1, card2, card3)
+        round = Round.new(deck)
+
+        expect(round.deck).to eq(deck)
