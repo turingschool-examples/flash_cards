4ade8b8cd8642ec303bff138dfa0de9dd514b5ec
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2023-12-06 06:59:18 -0500

    Modify my file, it can take different cards

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,13 +2,10 @@ require './lib/card'
 require './lib/deck'
 require './lib/turn'
 require './lib/round'
+require './lib/card_generator'
 
-
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
- 
-cards = [card_1, card_2, card_3]
+filename = "cards.txt"
+cards = CardGenerator.new(filename).cards
 
 deck = Deck.new(cards)
 
