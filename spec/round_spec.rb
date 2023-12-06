56e36394759aafc359a6b703652824aa2995bff1
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-05 17:56:09 -0700

    Fill in more it blocks

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -7,94 +7,44 @@ require "./lib/round"
 
 
 RSpec.describe Round do
-  it "exists" do
+  it "returns objects of the expected class" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
+    rounds_deck = round.deck
+
     expect(round).to be_instance_of(Round)
+    expect(rounds_deck).to be_instance_of(Deck)
+    expect(round.current_card).to be_instance_of(Card)
   end
 
-  it "template" do
-    #text
-    expect().to eq()
+  it "shows no turns have been taken" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    
+    expect(round.turns).to eq([])
   end
 
-  it "template" do
-    #text
-    expect().to eq()
-  end
+  it "takes a new turn" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    new_turn = round.take_turn("Juneau")
 
-  it "template" do
-    #text
-    expect().to eq()
-  end
-
-  it "template" do
-    #text
-    expect().to eq()
-  end
-
-  it "template" do
-    #text
-    expect().to eq()
+    expect(new_turn).to be_instance_of(Turn)
+    expect(new_turn.correct?).to be(true)
+    expect(round.turns.count).to eq(1)
   end
 
 end
 
 
 
-pry(main)> round.deck
-#=> #<Deck:0x00007fa160a38ed8...>
 
-pry(main)> round.turns
-#=> []
-
-pry(main)> round.current_card
-#=> #<Card:0x00007fa16104e160 @answer="Juneau", @question="What is the capital of Alaska?", @category=:Geography>
-
-pry(main)> new_turn = round.take_turn("Juneau")
-#=> #<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">
-
-pry(main)> new_turn.class
-#=> Turn
-
-pry(main)> new_turn.correct?
-#=> true
-
-pry(main)> round.turns
-#=> [#<Turn:0x00007f99842f09e8 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>, @guess="Juneau">]
-
-pry(main)> round.number_correct
-#=> 1
-
-pry(main)> round.current_card
-#=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
-
-pry(main)> round.take_turn("Venus")
-#=> #<Turn:0x00007f972a215b38...>
-
-pry(main)> round.turns.count
-#=> 2
-
-pry(main)> round.turns.last.feedback
-#=> "Incorrect."
-
-pry(main)> round.number_correct
-#=> 1
-
-pry(main)> round.number_correct_by_category(:Geography)
-#=> 1
-
-pry(main)> round.number_correct_by_category(:STEM)
-#=> 0
-
-pry(main)> round.percent_correct
-#=> 50.0
-
-pry(main)> round.percent_correct_by_category(:Geography)
-#=> 100.0
-
-pry(main)> round.current_card
-#=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
