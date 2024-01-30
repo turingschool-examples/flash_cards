cf0613444ba64fd47bea8c7eb83cc554ca5d119a
Neil Hendren <neil.hendren@gmail.com>
2024-01-30 12:05:47 -0700

    Corrections from linter/formatter

--- lib/deck.rb
+++ lib/deck.rb
@@ -15,21 +15,20 @@ class Deck
 
   def cards_in_category(category)
     num_of_cards = 0
-    for i in 0..@cards.length - 1
-      card = @cards[i]
+    @cards.each do |card|
       num_of_cards += 1 if card.category == category
     end
     num_of_cards
   end
 end
 
-card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-card_2 = Card.new(
+card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+card2 = Card.new(
   'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM
 )
-card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west',
-                  :STEM)
+card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west',
+                 :STEM)
 
-cards = [card_1, card_2, card_3]
+cards = [card1, card2, card3]
 
-deck = Deck.new(cards)
+deck = Deck.new(cards) # rubocop:disable Lint/UselessAssignment
