c1ec7165d0447dffa12ea1e944bf0528c01fd792
Neil Hendren <neil.hendren@gmail.com>
2024-01-29 21:47:24 -0700

    Begin spec for Turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,1 +1,32 @@
 require './lib/turn.rb'
+require './lib/card.rb'
+
+RSpec.describe Turn do
+    card = Card.new("question", "answer", :category)
+    turn = Turn.new("guess", card)
+    
+    it 'exists' do
+        expect(turn).to be_an_instance_of(Turn)
+    end
+
+    it 'has a card' do
+        expect(turn.card).to be_an_instance_of(Card)
+    end
+
+    it 'has a question' do
+        expect(turn.question).to be_an_instance_of(String)
+    end
+
+    it 'has an answer' do
+        expect(turn.answer).to be_an_instance_of(String)
+    end
+
+    it 'has a guess' do
+        expect(turn.guess).to be_an_instance_of(String)
+    end
+
+    it 'has a category' do
+        expect(turn.category).to be_an_instance_of(Symbol)
+    end
+end
+    
