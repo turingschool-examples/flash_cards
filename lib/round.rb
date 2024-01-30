d9096c4d2cbe47bfc33f1067fab0547974a513af
Neil Hendren <neil.hendren@gmail.com>
2024-01-30 09:39:46 -0700

    Add class and spec for Round

---
+++ lib/round.rb
@@ -0,0 +1,9 @@
+require './lib/card.rb'
+require './lib/turn.rb'
+require './lib/deck.rb'
+
+class Round
+    def initialize
+
+    end
+end
---
+++ spec/round_spec.rb
@@ -0,0 +1,8 @@
+require './lib/card.rb'
+require './lib/turn.rb'
+require './lib/deck.rb'
+require './lib/round.rb'
+
+RSpec.describe Round do
+
+end
