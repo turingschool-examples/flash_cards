a0754f8cf6ec38fd72a74ccf8d7e0dc71d6960d1
Neil Hendren <neil.hendren@gmail.com>
2024-01-29 18:36:44 -0700

    Finish Turn class specifications

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,6 +4,20 @@ class Turn
     def initialize(guess, card)
         @guess = guess
         @card = card
+        @question = @card.question
+        @answer = @card.answer
+        @category = @card.category
     end
-    
+
+    def correct?
+        @guess == @answer
+    end
+
+    def feedback
+        if @guess == @answer
+            "Correct!"
+        else
+            "Incorrect."
+        end
+    end
 end
