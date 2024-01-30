20e4adb7d9b37c6b50969d533f5c033727e79740
Neil Hendren <neil.hendren@gmail.com>
2024-01-30 13:33:38 -0700

    Removes unnecessary testing code

--- lib/deck.rb
+++ lib/deck.rb
@@ -21,14 +21,3 @@ class Deck
     num_of_cards
   end
 end
-
-card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-card2 = Card.new(
-  'The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM
-)
-card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west',
-                 :STEM)
-
-cards = [card1, card2, card3]
-
-deck = Deck.new(cards) # rubocop:disable Lint/UselessAssignment
