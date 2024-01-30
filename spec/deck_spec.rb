b0af1a42d2dfa6a41da308875a3e2093fed09775
Neil Hendren <neil.hendren@gmail.com>
2024-01-30 13:30:34 -0700

    Updated specs to follow convention

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,28 +5,30 @@ require './lib/turn'
 require './lib/deck'
 
 RSpec.describe Deck do
-  cards = [
-    Card.new('Question 1', 'Answer 1', :category1),
-    Card.new('Question 2', 'Answer 2', :category1),
-    Card.new('Question 3', 'Answer 3', :category2)
-  ]
-  deck = Deck.new(cards)
+  before(:each) do
+    @cards = [
+      Card.new('Question 1', 'Answer 1', :category1),
+      Card.new('Question 2', 'Answer 2', :category1),
+      Card.new('Question 3', 'Answer 3', :category2)
+    ]
+    @deck = Deck.new(cards)
+  end
 
   it 'cards property is an array' do
-    expect(deck.cards).to be_an_instance_of(Array)
+    expect(@deck.cards).to be_an_instance_of(Array)
   end
 
   it 'cards array items are Cards' do
-    deck.cards.each do |card|
+    @deck.cards.each do |card|
       expect(card).to be_an_instance_of(Card)
     end
   end
 
   it 'counts number of cards in deck' do
-    expect(deck.count).to eq(3)
+    expect(@deck.count).to eq(3)
   end
 
   it 'counts number of cards in category' do
-    expect(deck.cards_in_category(:category1)).to eq(2)
+    expect(@deck.cards_in_category(:category1)).to eq(2)
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,30 +4,32 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
-  card = Card.new('question', 'answer', :category)
-  turn = Turn.new('guess', card)
+  before(:each) do
+    @card = Card.new('question', 'answer', :category)
+    @turn = Turn.new('guess', card)
+  end
 
   it 'exists' do
-    expect(turn).to be_an_instance_of(Turn)
+    expect(@turn).to be_an_instance_of(Turn)
   end
 
   it 'has a card' do
-    expect(turn.card).to be_an_instance_of(Card)
+    expect(@turn.card).to be_an_instance_of(Card)
   end
 
   it 'has a question' do
-    expect(turn.question).to be_an_instance_of(String)
+    expect(@turn.question).to be_an_instance_of(String)
   end
 
   it 'has an answer' do
-    expect(turn.answer).to be_an_instance_of(String)
+    expect(@turn.answer).to be_an_instance_of(String)
   end
 
   it 'has a guess' do
-    expect(turn.guess).to be_an_instance_of(String)
+    expect(@turn.guess).to be_an_instance_of(String)
   end
 
   it 'has a category' do
-    expect(turn.category).to be_an_instance_of(Symbol)
+    expect(@turn.category).to be_an_instance_of(Symbol)
   end
 end
