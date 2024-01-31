7207ac87cf8bc1fe611cbbbdf2c50a1d149cc429
Neil Hendren <neil.hendren@gmail.com>
2024-01-30 18:14:12 -0700

    Complete methods required for iteration 2

--- lib/round.rb
+++ lib/round.rb
@@ -4,5 +4,53 @@ require './lib/deck'
 
 # Documentation for class Round
 class Round
-  def initialize; end
+  attr_reader :deck, :turns, :turn, :number_correct
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+    @turn = 0
+    @guess = nil
+    @number_correct = 0
+  end
+
+  def current_card
+    @deck.cards[@turn]
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    turns.push(turn)
+    @guess = guess
+    @turn += 1
+    @number_correct += 1 if guess == turn.answer
+    turn
+  end
+
+  def correct?
+    if @guess == current_card.answer
+      true
+    elsif @guess.nil?
+      nil
+    else
+      false
+    end
+  end
+
+  def number_correct_by_category(category)
+    count_correct = 0
+    @turns.each do |turn|
+      count_correct += 1 if turn.category == category && turn.guess == turn.answer
+    end
+    count_correct
+  end
+
+  def percent_correct_by_category(category)
+    count_category = 0
+    @turns.each do |turn|
+      count_category += 1 if turn.category == category
+    end
+    fraction_correct = number_correct_by_category(category).to_f / count_category
+    (fraction_correct * 100.0).round(1)
+  end
 end
