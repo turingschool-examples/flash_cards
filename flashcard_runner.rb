32c024ba72a6bbed43ef593896394bb64812dd71
Neil Hendren <neil.hendren@gmail.com>
2024-01-30 22:40:36 -0700

    Write game loop and show results

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -16,7 +16,37 @@ class Game
   end
 
   def start
-    puts "Welcome! You\'re playing with #{@cards.length} cards."
+    puts "\nWelcome! You're playing with #{@deck.count} cards.\n--------------------------------------\n"
+    quiz_loop
+  end
+
+  def take_input
+    puts "This is card number #{@round.turn + 1} out of #{@deck.count}"
+    puts "Question: #{@round.current_card.question}"
+    guess = gets
+    guess[-1] = '' # remove line break
+    turn = @round.take_turn(guess)
+    puts turn.feedback
+    sleep(1)
+    puts "--------------------------------------\n"
+  end
+
+  def quiz_loop
+    take_input while @round.turn < @deck.count
+    show_results
+  end
+
+  def show_results # rubocop:disable Metrics/AbcSize
+    puts "\n****** Game over! ******\n"
+    puts "\nYou had #{@round.number_correct} correct guesses out of #{@deck.count} for a total score of #{(100.0 * @round.number_correct / @deck.count).round}%."
+    categories = []
+    @deck.cards.each do |card|
+      categories.push(card.category) unless categories.include?(card.category)
+    end
+    categories.each do |category|
+      puts "#{category} - #{@round.percent_correct_by_category(category)}% correct"
+    end
+    puts ''
   end
 end
 
