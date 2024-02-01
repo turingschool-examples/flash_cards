688cef250e6cf3309ca84b68ae3cd8a56327aeb0
Jared Hobson <jaredmhobson@gmail.com>
2024-02-01 15:11:42 -0800

    Add test to check Round class exists

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,8 +1,14 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
 require './lib/round'
 
 RSpec.describe Round do
   it 'exists' do
-    round = Round.new()
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    cards = [card]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
 
     expect(round).to be_instance_of(Round)
   end
