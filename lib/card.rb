30099426a5da3dbada249ccdaf1bf85ae9551390
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-01 15:37:59 -0700

    Card class complete

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,10 @@
 class Card
+  attr_reader :question, :answer, :category
+  
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
 
 end
