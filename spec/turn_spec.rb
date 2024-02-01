b0cf90ec9c6c91d6d5f8201352b199cf9a979f36
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-01 16:11:45 -0700

    Correct? test pass

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -10,4 +10,29 @@ RSpec.describe Turn do
 
     expect(turn).to be_instance_of(Turn)
   end
+  
+  it 'has a guess' do 
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.guess).to eq("Saturn")
+  end
+
+  it 'is correct?' do 
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.correct?).to eq(false)
+  end
+
+  it 'gives feedback' do 
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.correct?).to eq(false)
+  end
+
 end
