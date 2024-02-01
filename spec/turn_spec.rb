a11769de85a72cb034fec6fa87e9cd79a9f97390
Jared Hobson <jaredmhobson@gmail.com>
2024-02-01 15:06:15 -0800

    Add test to check Turn class exists

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,8 +1,10 @@
+require './lib/card'
 require './lib/turn'
 
 RSpec.describe Turn do
   it 'exists' do
-    turn = Turn.new()
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("guess", card)
 
     expect(turn).to be_instance_of(Turn)
   end
