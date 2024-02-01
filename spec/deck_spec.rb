23bb6be7e86c02c4d2e93b29dfdf09ac2a77d353
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-01 16:43:47 -0700

    Fix it block

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,7 +3,7 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
-  it `exist` do 
+  it "exist" do 
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
