9beff5c818f6aab0df26e04454a880cf5b2d98a7
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-01 16:34:37 -0700

    Test for deck ''

---
+++ spec/deck_spec.rb
@@ -0,0 +1,19 @@
+require './lib/card'
+
+require './lib/deck'
+
+RSpec.describe Deck do
+  it `exist` do 
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck).to be_instance_of(Deck)
+  end
+end
