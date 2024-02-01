7954f57e48b455557081df41576c546fc0af9812
Grant Davis <grantdavis303@gmail.com>
2024-02-01 14:29:51 -0700

    Reformatting Card's RSpec File

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,27 +1,33 @@
-require './lib/card'
+# card_spec.rb
+
+require 'rspec'
+require './lib/card.rb'
+
+# Feedback Fomatting
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
 
 RSpec.describe Card do
+
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     expect(card).to be_instance_of(Card)
   end
 
   it 'has a question' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     expect(card.question).to eq("What is the capital of Alaska?")
   end
 
   it 'has an answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     expect(card.answer).to eq("Juneau")
   end
 
   it 'has a category' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     expect(card.category).to eq(:Geography)
   end
+
 end
