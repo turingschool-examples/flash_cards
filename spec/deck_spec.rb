4d337ab430d461e29517264b82b6e88bb98f292d
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-01 16:51:19 -0700

    Add Count test for deck class.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -31,5 +31,19 @@ RSpec.describe Deck do
     expect(deck.cards).to eq(cards)
   end
 
+  it "counts cards" do 
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.counts).to eq(3)
+  end
+
 
 end
