fb2d2a79e23ba21cf825a72c85b4491ac51919ee
Jared Hobson <jaredmhobson@gmail.com>
2024-02-01 14:35:03 -0800

    Create Round class with outline for necessary methods

---
+++ lib/round.rb
@@ -0,0 +1,29 @@
+require './lib/deck'
+
+class Round
+    attr_reader :deck, :turns
+
+    def initialize(deck)
+        @deck = deck
+        @turns = []
+    end
+
+    def current_card
+    end
+
+    def take_turn(guess)
+    end
+
+    def number_correct
+    end
+
+    def number_correct_by_category(category)
+    end
+
+    def percent_correct
+    end
+
+    def percent_correct_by_category(category)
+    end
+
+end
