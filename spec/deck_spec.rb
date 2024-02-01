4b2ac2c89f6278b11aa747ca5961f9fa3ae92d1e
Jared Hobson <jaredmhobson@gmail.com>
2024-02-01 15:09:14 -0800

    Add test to check Deck class exists

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,8 +1,11 @@
 require './lib/deck'
+require './lib/card'
 
 RSpec.describe Deck do
   it 'exists' do
-    deck = Deck.new()
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    cards = [card]
+    deck = Deck.new(cards)
 
     expect(deck).to be_instance_of(Deck)
   end
