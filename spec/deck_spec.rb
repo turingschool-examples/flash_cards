b7c32b3d36ff0f45415d5c2d371aa4f1b0928828
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-01 16:48:23 -0700

    Has cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe Deck do
 
     expect(deck).to be_instance_of(Deck)
   end
+
+  it "has cards" do 
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq(cards)
+  end
+
+
 end
