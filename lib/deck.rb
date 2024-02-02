962a06d545a8da02b2fdb0e79be2f32cd4c6a2bd
Billy Wallace <wallace.billy1@gmail.com>
2024-02-02 12:32:51 -0700

    Create cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,8 +2,11 @@ class Deck
     attr_reader :cards
     
     def initialize(cards)
-        @cards = []
+        @cards = cards
         @count = @cards.count
     end
 
+    def cards_in_category(category)
+        cards.select { |card| card.category == category}
+    end
 end
