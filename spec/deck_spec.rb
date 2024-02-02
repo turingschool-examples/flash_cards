87d076c3e57888976bb43b4fdc086ca9c5b4c2a7
Grant Davis <grantdavis303@gmail.com>
2024-02-02 08:21:09 -0700

    Added second test to check all cards in deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,11 +4,6 @@ require 'rspec'
 require './lib/card.rb'
 require './lib/deck.rb'
 
-# Feedback Fomatting
-RSpec.configure do |config|
-    config.formatter = :documentation
-end
-
 RSpec.describe Deck do
 
     it 'exists' do
@@ -20,7 +15,17 @@ RSpec.describe Deck do
         deck = Deck.new(cards)
 
         expect(deck).to be_instance_of (Deck)
+    end
 
+    it 'displays all cards in the deck' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        expect(deck.cards).to eq (cards)
     end
 
 end
