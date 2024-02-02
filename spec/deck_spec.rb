75d610a8601e3adb5cd0c94edf2dadf581887ee7
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-01 19:55:27 -0700

    Catagory tests pass

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -57,7 +57,35 @@ RSpec.describe Deck do
     deck = Deck.new(cards)
 
     expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
-  end
+    end
+ 
+    it "is seeing the category" do 
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:Geography)).to eq([card_1])
+    end
+ 
+    it "is seeing the category" do 
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category("Pop Culture")).to eq([])
+    end
 
 
 end
