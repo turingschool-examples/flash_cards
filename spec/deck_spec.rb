f8861bfbdb00dd71be670d163ca631f1dd8d7b8f
Billy Wallace <wallace.billy1@gmail.com>
2024-02-02 12:16:33 -0700

    Create test to return cards in specific category

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -42,4 +42,18 @@ RSpec.describe Deck do
 
         expect(cards.count).to eq(3)
     end
+
+    it 'returns cards from a specific category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]  
+
+        deck = Deck.new(cards)
+
+        stem_cards = deck.cards_in_category(:STEM)
+
+        expect(stem_cards).to eq(card_2, card_3)
+    end
 end
