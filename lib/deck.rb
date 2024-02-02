50165e7d2d15c2ee82f0c30cf5416d61000b96f7
Jared Hobson <jaredmhobson@gmail.com>
2024-02-01 17:05:27 -0800

    Add Deck class method cards_in_category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,11 +1,16 @@
+require './lib/card'
+
 class Deck
     attr_reader :cards
 
-    def initialize(cards)
-        @cards = []
+    def initialize(cards = [])
+        @cards = cards
     end
 
-    def cards_in_category(category)
+    def cards_in_category(category_name)
+        @cards.select do |card|
+            card.category == category_name
+        end
     end
 
 end
