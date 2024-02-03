1cb1c241824109ba94ac68950be7c4eb65b382af
Billy Wallace <wallace.billy1@gmail.com>
2024-02-03 14:03:22 -0700

    Update take_turn method with adding current turn into turns array

--- lib/round.rb
+++ lib/round.rb
@@ -11,6 +11,8 @@ class Round
     end
 
     def take_turn(guess)
-        Turn.new(guess, current_card)
+        current_turn = Turn.new(guess, current_card)
+        @turns << current_turn
+        current_turn
     end
 end
