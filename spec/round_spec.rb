6d7a8a49c93b29fc7119178c3854a0c4726ed488
Billy Wallace <wallace.billy1@gmail.com>
2024-02-03 14:02:20 -0700

    Add test for adding the last turn into the turns array

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -98,6 +98,24 @@ RSpec.describe Round do
 
         expect(new_turn.correct?). to be false        
     end
+
+    it 'adds the last turn to the turns array' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+    
+        deck = Deck.new(cards)
+
+        round = Round.new(deck) 
+        
+        expect(round.turns). to eq []
+
+        current_turn = round.take_turn("Juneau")
+
+        expect(round.turns).to eq[current_turn]
+    end
 end
 
 # A Round will be the object that processes responses and records guesses. A Round is initialized with a Deck. The idea is that when we start a Round, the current card is the first in the deck (the first in the Deck’s array of Cards). When we make a guess, the guess is recorded, and the next card in the deck becomes the current card.
