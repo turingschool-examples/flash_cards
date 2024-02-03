20ff10c6631c129881c9300c549fe5038578ea10
Billy Wallace <wallace.billy1@gmail.com>
2024-02-03 12:20:01 -0700

    Add test for empty turns array

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -22,4 +22,32 @@ RSpec.describe Round do
 
         expect(round).to be_a(Round)
     end
+
+    it 'TBD' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+        
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        expect(round.deck).to be deck
+    end
+
+    it 'starts with an empty turns array' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+        
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)
+
+        expect(round.turns).to eq []
+    end
 end
