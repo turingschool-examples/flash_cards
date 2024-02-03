9f151237bfcc4b73a88d36b0f3d28a5e0b3bbe4f
Billy Wallace <wallace.billy1@gmail.com>
2024-02-03 13:27:25 -0700

    Add test for take_turn method

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Round do
         expect(round).to be_a(Round)
     end
 
-    it 'TBD' do
+    it 'has a deck' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -64,4 +64,22 @@ RSpec.describe Round do
 
         expect(round.current_card).to eq card_1
     end
+
+    it 'can take a turn' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+    
+        deck = Deck.new(cards)
+
+        round = Round.new(deck)   
+
+        expect(round.take_turn("Juneau")). to be_a Turn
+    end
 end
+
+# A Round will be the object that processes responses and records guesses. A Round is initialized with a Deck. The idea is that when we start a Round, the current card is the first in the deck (the first in the Deck’s array of Cards). When we make a guess, the guess is recorded, and the next card in the deck becomes the current card.
+
+# The take_turn method is the crux of this problem. The take_turn method takes a string representing the guess. It should create a new Turn object with the appropriate guess and Card. It should store this new Turn, as well as return it from the take_turn method. Also, when the take_turn method is called, the Round should move on to the next card in the deck.
