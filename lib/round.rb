e9b484ed34dfd3150b3e495d01a1ec3653c87cf1
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-02-05 00:52:05 -0700

    Add methods

--- lib/round.rb
+++ lib/round.rb
@@ -4,6 +4,32 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
+    @correct = 0
 
   end
+
+  def current_card
+    @deck.current_card
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(guess, current_card)
+    @turns << turn
+    turn
+  end
+
+  def new_turn
+    correct?
+  end
+
+  def number_correct
+      @turns.each do |turn|
+        turn.correct? == true
+        @correct += 1
+    end
+    @correct
+  end
+
+
+
 end
