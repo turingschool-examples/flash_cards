bd236b29e84065a92090158236084be2a5a3abd8
Billy Wallace <wallace.billy1@gmail.com>
2024-02-05 17:18:08 -0700

    Add test for returning percent correct

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -203,6 +203,23 @@ RSpec.describe Round do
 
         expect(feedback).to eq "Incorrect."
     end
+
+    it 'returns returns what percent of the round that has been answered correctly' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+    
+        deck = Deck.new(cards)
+
+        round = Round.new(deck) 
+
+        turn_1 = round.take_turn("Juneau")
+        turn_2 = round.take_turn("Venus")
+
+        expect(round.percent_correct).to eq 50.0
+    end
 end
 
 
