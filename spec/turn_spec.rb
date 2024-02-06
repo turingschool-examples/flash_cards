0d88aab4fa35ff41d0f76a69fa939a6f4dae843a
NeilTheSeal <neil.hendren@gmail.com>
2024-02-06 13:16:58 -0700

    Add additional tests for Turn spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,7 +6,7 @@ require './lib/card'
 RSpec.describe Turn do
   before(:each) do
     @card = Card.new('question', 'answer', :category)
-    @turn = Turn.new('guess', card)
+    @turn = Turn.new('guess', @card)
   end
 
   it 'exists' do
@@ -19,5 +19,18 @@ RSpec.describe Turn do
 
   it 'has a guess' do
     expect(@turn.guess).to be_an_instance_of(String)
+  end
+
+  it 'has the "guess" attribute that equals the guess input' do
+    expect(@turn.guess).to eq('guess')
+  end
+
+  it 'gives correct feedback when guess is correct' do
+    turn = Turn.new('answer', @card)
+    expect(turn.feedback).to eq('Correct!')
+  end
+
+  it 'gives correct feedback when guess is incorrect' do
+    expect(@turn.feedback).to eq("Incorrect. The correct answer is #{@card.answer}")
   end
 end
