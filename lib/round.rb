cd8beec2c52cc72a6ad3ac8f321bbb2a231e62b3
Steddman Bell <steddfast@gmail.com>
2024-02-05 23:36:20 -0800

    This commit is completion of the number_correct method

--- lib/round.rb
+++ lib/round.rb
@@ -1,12 +1,38 @@
+require 'pry'
 class Round
     attr_reader :deck, :turns
 
     def initialize(deck)
         @deck = deck
         @turns = []
+        
     end
 
     def current_card
-        @deck.cards
+        @deck.cards.first
     end
+
+    def take_turn(guess)
+        turn = Turn.new(guess, current_card) 
+        @turns << turn
+        turn
+    end
+
+    def number_correct
+        # have a turn
+        # have a card
+        @turns.count {|turn| turn.correct?}
+    end
+
+    # def number_correct_by_category
+
+    # end
+
+    # def percent_correct
+
+    # end
+
+    # def percent_correct_by_category
+
+    # end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe Round do
         expect(round.turns).to eq([])
     end
 
-    it 'counts number correct' do
+    it 'returns current card' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -45,6 +45,33 @@ RSpec.describe Round do
         deck = Deck.new(cards)
         round = Round.new(deck)
 
-        expect(round.turns).to eq([])
+
+        expect(round.current_card).to eq(card_1)
+    end
+
+    it 'takes turns' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+
+        expect(new_turn).to be_instance_of(Turn)
+        expect(new_turn.correct?).to eq(true)
+        expect(round.turns).to eq([new_turn])
+    end
+
+    it 'counts number correct' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        cards = [card_1]
+        deck = Deck.new(cards)
+        round = Round.new(deck)
+        new_turn = round.take_turn("Juneau")
+        
+        
+        # require 'pry'; binding.pry
+        expect(round.number_correct).to eq(1)
     end
 end
