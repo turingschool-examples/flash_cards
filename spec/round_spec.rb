92c7ac9ad39ee873748afab690bb97a697ec9f6b
Billy Wallace <wallace.billy1@gmail.com>
2024-02-05 17:28:39 -0700

    Add test for returning what percent of answers in a category are correct

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -220,6 +220,23 @@ RSpec.describe Round do
 
         expect(round.percent_correct).to eq 50.0
     end
+
+    it 'returns returns what percent of answers in a category are correct' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+    
+        deck = Deck.new(cards)
+
+        round = Round.new(deck) 
+
+        turn_1 = round.take_turn("Juneau")
+        turn_2 = round.take_turn("Venus")
+
+        expect(round.percent_correct_by_category(:Geography)).to eq 100.0
+    end
 end
 
 
