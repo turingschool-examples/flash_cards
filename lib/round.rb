2ccfa395b2d2e3dc35b291c0d0acac53a3675e77
Billy Wallace <wallace.billy1@gmail.com>
2024-02-05 17:28:11 -0700

    Create percent_correct_by_category method

--- lib/round.rb
+++ lib/round.rb
@@ -29,4 +29,10 @@ class Round
     def percent_correct
         (@number_correct.to_f / @turns.count) * 100
     end
+
+    def percent_correct_by_category(category)
+        category_correct = @turns.count { |turn| turn.card.category == category && turn.correct?}
+        category_count = @turns.count { |turn| turn.card.category == category}
+        (category_count.to_f / category_correct) * 100
+    end
 end
