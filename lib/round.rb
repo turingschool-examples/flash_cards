f4345045d5c3650417e3f52d0b61ef9149bec3ca
Billy Wallace <wallace.billy1@gmail.com>
2024-02-05 17:17:47 -0700

    Add percent correct method

--- lib/round.rb
+++ lib/round.rb
@@ -18,10 +18,15 @@ class Round
             @number_correct += 1
         else
         end
+        @deck.cards.shift
         current_turn
     end
 
     def number_correct_by_category(category)
         @turns.count { |turn| turn.card.category == category && turn.correct?}
+    end
+
+    def percent_correct
+        (@number_correct.to_f / @turns.count) * 100
     end
 end
