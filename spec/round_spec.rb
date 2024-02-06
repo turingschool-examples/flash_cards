495b2dd47f5b64c0aa06abeb7df2398488923f26
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-06 16:14:28 -0700

    Fix spacing and expects

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -46,21 +46,29 @@ RSpec.describe Round do
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
+
     new_turn = round.take_turn("Juneau")
+
     expect(new_turn).to be_instance_of(Turn)
     expect(new_turn.correct?).to be(true)
     expect(round.turns.count).to eq(1)
   end
 
   it "takes turns" do
-    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    turn = Turn.new("Saturn", card)
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    card_4 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card_4)
+    deck = Deck.new([card_1, card_2, card_3, card_4])
     round = Round.new(deck)
 
-    expect(round.turns).to eq(:turn)
+    expect(round.turns).to eq([])
     expect(round.number_correct).to eq(1)
-    expect(round.current_card).to eq(:card)
-    expect(round.take_turn("Venus")).to eq(:turn)
+    expect(round.current_card).to eq(card_4)
+
+    expect(round.take_turn("Venus")).to eq(turn)
+
     expect(round.turns.count).to eq(2)
     expect(round.turns.last.feedback).to eq("Incorrect.")
     expect(round.number_correct).to eq(1)
