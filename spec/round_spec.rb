a9df6e2b92639d5aa80d880383226ebc0805a0c0
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-07 15:21:54 -0700

    Add new tdd category for getting the math right

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -98,4 +98,29 @@ RSpec.describe Round do
     expect(round.number_correct_by_category(:STEM)).to eq(0)
     expect(round.percent_correct_by_category(:STEM)).to eq(0.0)
   end
+
+  it "ensures we got the math right" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :Geography)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :Geography)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+
+    round.take_turn("Venus")
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+
+    expect(round.percent_correct).to eq(50.0)
+    expect(round.percent_correct_by_category(:Geography)).to eq(50.0)
+    expect(round.current_card).to eq(card_3)
+
+    round.take_turn("Blah")
+
+    expect(round.percent_correct).to eq(33.33)
+    expect(round.number_correct_by_category(:Geography)).to eq(1)
+    expect(round.percent_correct_by_category(:Geography)).to eq(33.33)
+  end
+
 end
