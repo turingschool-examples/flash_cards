2553443d25de507abeafa0052b96de7bad35f23c
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-06 18:25:48 -0700

    number_correct_by_category

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -58,23 +58,34 @@ RSpec.describe Round do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    card_4 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-    turn = Turn.new("Saturn", card_4)
-    deck = Deck.new([card_1, card_2, card_3, card_4])
+    # card_4 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    # turn = Turn.new("Saturn", card_4)
+    deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
 
     new_turn = round.take_turn("Juneau")
 
     expect(round.number_correct).to eq(1)
-    expect(round.current_card).to eq(card_4)
-    expect(round.take_turn("Venus")).to eq(turn)
+    expect(round.current_card).to eq(card_2)
+
+    new_turn = round.take_turn("Venus")
+
     expect(round.turns.count).to eq(2)
     expect(round.turns.last.feedback).to eq("Incorrect.")
     expect(round.number_correct).to eq(1)
   end
 
 
-  it "" do
+  it "finds the number correct by category" do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    round.take_turn("Venus")
+
     expect(round.number_correct_by_category(:Geography)).to eq(1)
     expect(round.number_correct_by_category(:STEM)).to eq(0)
     expect(round.percent_correct).to eq(50.0)
@@ -82,29 +93,3 @@ RSpec.describe Round do
     expect(round.current_card).to eq(:card)
   end
 end
-
-
-
-  # def take_turn(guess)
-  #   turn = Turn.new(guess, current_card)
-  #   if turn.correct?
-  #     @number_correct += 1
-  #   end
-  #   @turns << turn
-  #   turn
-  # end
-
-
-  # # The take_turn method takes a string representing the guess.
-  # def take_turn(guess)
-  #   # It should create a new Turn object with the appropriate guess and Card.
-  #   require 'pry'; binding.pry
-  #   current_card = @deck.cards.first
-  #   turn = Turn.new(guess, current_card)
-  #   # It should store this new Turn...
-  #   @turns << turn
-  #   # When the take_turn method is called, the Round should move on to the next card in the deck.
-  #   @deck.cards.shift
-  #   # ...as well as return it from the take_turn method.
-  #   turn
-  #   end
