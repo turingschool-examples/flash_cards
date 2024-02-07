aa75dc2a3b5c29ee6fc0a83d9eaec81a040ad884
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-07 14:57:51 -0700

    Add round method to percent_correct

--- lib/round.rb
+++ lib/round.rb
@@ -27,7 +27,7 @@ class Round
   end
 
   def percent_correct
-    (@number_correct.to_f / @turns.count) * 100
+    ((@number_correct.to_f / @turns.count) * 100).round(2)
   end
 
   def percent_correct_by_category(category)
