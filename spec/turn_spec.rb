e1393d209feb395e6fa91cb5de99786b36db9b76
Chee Lee <cheeleertr@gmail.com>
2024-03-21 16:12:37 -0500

    create first turn test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,5 +1,10 @@
 require "./lib/turn"
+require "./lib/card"
 
 RSpec.describe Turn do
+ it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
 
+    expect(turn.new).to be_instance_of(Turn)
 end
