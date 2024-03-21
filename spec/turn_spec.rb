f639af22c39b10b8d6af7d0d94db130ca7ff6d28
Zach Bergman <bzach634@gmail.com>
2024-03-21 16:45:15 -0600

    Add assertions for an incorrect guess

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,8 +4,11 @@ require 'pry'
 
 RSpec.describe Turn do
     before(:each) do
-        @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        @turn = Turn.new("Juneau", @card)
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @turn = Turn.new("Juneau", @card_1)
+
+        @card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        @turn_2 = Turn.new("Saturn", @card_2)
     end
 
     it "exists" do
@@ -13,22 +16,26 @@ RSpec.describe Turn do
     end
 
     it "has a card" do  
-        expect(@turn.card).to eq(@card)
+        expect(@turn.card).to eq(@card_1)
+        expect(@turn_2.card).to eq(@card_2)
     end
 
     it "has a guess" do
         expect(@turn.guess).to eq("Juneau")
+        expect(@turn_2.guess).to eq("Saturn")
     end
 
     describe "#correct?" do
         it "returns boolean if guess matches answer on card" do
             expect(@turn.correct?).to be true
+            expect(@turn_2.correct?).to be false
         end
     end
 
     describe "#feedback" do
         it "returns correct or incorrect" do
             expect(@turn.feedback).to eq("Correct!")
+            expect(@turn_2.feedback).to eq("Incorrect.")
         end
     end
 end
