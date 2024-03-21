b8e60b176e070a0b4ba2042efd5956f76861991c
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-21 17:39:30 -0600

    Adds the turn.rb file and Turn class

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+class Turn
+    attr_reader string:, card:
+
+    initialize(string, card)
+        
+
+
+        def correct?
+            if guess == 
+        end
+
+        def feedback 
+            if guess.correct? == true
+                puts "Correct!"
+            else
+                puts "Incorrect."
+            end
+        end
+
+    end 
+
+
+
+end
