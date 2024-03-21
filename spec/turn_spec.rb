11e88eaf1aa82d779cf9bdcdf5f7184b0b704d31
Zach Bergman <bzach634@gmail.com>
2024-03-21 15:00:07 -0600

    add additional attribute assertions

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,10 +5,18 @@ require 'pry'
 RSpec.describe Turn do
     before(:each) do
         @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        @turn = Turn.new("Saturn", @card)
+        @turn = Turn.new("Juneau", @card)
     end
 
     it "exists" do
         expect(@turn).to be_instance_of(Turn)
+    end
+
+    it "has a card" do  
+        expect(@turn.card).to eq(@card)
+    end
+
+    it "has a guess" do
+        expect(@turn.guess).to eq("Juneau")
     end
 end
