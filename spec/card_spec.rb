83b8e19d96b6d63ba702d4b6fce923994b104479
Zach Bergman <bzach634@gmail.com>
2024-03-21 14:44:13 -0600

    add extra assertions to tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -10,19 +10,25 @@ RSpec.describe Card do
 
   it 'has a question' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
     expect(card.question).to eq("What is the capital of Alaska?")
+    expect(card_2.question).to eq("Which planet is closest to the sun?")
   end
 
   it 'has an answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
     expect(card.answer).to eq("Juneau")
+    expect(card_2.answer).to eq("Mercury")
   end
 
   it 'has a category' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
 
     expect(card.category).to eq(:Geography)
+    expect(card_2.category).to eq(:STEM)
   end
 end
