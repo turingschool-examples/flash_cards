2c3dfc6f361450496d10306e17261ec895974745
Zach Bergman <bzach634@gmail.com>
2024-03-22 08:08:37 -0600

    Add cards_in_category method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,12 @@
 class Deck
     attr_reader :cards, :count
-    
+
     def initialize(cards)
         @cards = cards
         @count = @cards.length
+    end
+
+    def cards_in_category(category)
+        @cards.find_all {|card| card if card.category == category}
     end
 end
