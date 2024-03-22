8f2529181c333d65c060d52aed405deda5280a32
Zach Bergman <bzach634@gmail.com>
2024-03-22 07:17:01 -0600

    Change double quotation marks to single - preferred

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -10,29 +10,29 @@ RSpec.describe Turn do
         @turn_2 = Turn.new("Saturn", @card_2)
     end
 
-    it "exists" do
+    it 'exists' do
         expect(@turn_1).to be_instance_of(Turn)
     end
 
-    it "has a card" do  
+    it 'has a card' do  
         expect(@turn_1.card).to eq(@card_1)
         expect(@turn_2.card).to eq(@card_2)
     end
 
-    it "has a guess" do
+    it 'has a guess' do
         expect(@turn_1.guess).to eq("Juneau")
         expect(@turn_2.guess).to eq("Saturn")
     end
 
-    describe "#correct?" do
-        it "returns boolean if guess matches answer on card" do
+    describe '#correct?' do
+        it 'returns boolean if guess matches answer on card' do
             expect(@turn_1.correct?).to be true
             expect(@turn_2.correct?).to be false
         end
     end
 
-    describe "#feedback" do
-        it "returns correct or incorrect" do
+    describe '#feedback' do
+        it 'returns correct or incorrect' do
             expect(@turn_1.feedback).to eq("Correct!")
             expect(@turn_2.feedback).to eq("Incorrect.")
         end
