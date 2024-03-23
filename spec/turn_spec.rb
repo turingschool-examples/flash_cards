5484a254598e8be2b7d9376f7f41568fd9579692
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-23 14:36:35 -0600

    Adds RSpec configure code block

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,1 +1,20 @@
 require './lib/turn.rb'
+
+RSpec.configure do |config|
+      config.formatter = :documentation
+end
+
+RSpec.describe Turn do
+  xit 'exists' do
+    turn = Turn.new("Juneau", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a guess' do
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.guess).to eq("")
+  end
+
+end
