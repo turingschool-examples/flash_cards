95c8f2857e871844951177f7166f19a2ec732f10
Clyde Autin <clyde.autin@gmail.com>
2024-03-23 09:52:26 -0700

    runner now passes exists test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,13 @@
 class Deck
-    
+
+    attr_reader :cards, :count
+
+    def initialize(cards)
+        @cards = cards
+        @count = count
+    end
+
+    def count
+        @count = cards.count
+    end
 end
