eb8ab1f2dd915c5be49f16344db104c7a6694965
Zach Bergman <bzach634@gmail.com>
2024-03-23 12:50:07 -0600

    Add start method to Round

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -3,7 +3,7 @@ require './lib/deck'
 require './lib/turn'
 require './lib/round'
 
-card_1 = Card.new("What is 5 + 5?", "10", :Math)
+card_1 = Card.new("What is 5 + 5?", "10", :STEM)
 card_2 = Card.new("What is Rachel's favorite animal?", "red panda", :Turing_Staff)
 card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
 card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
--- lib/round.rb
+++ lib/round.rb
@@ -5,6 +5,7 @@ class Round
         @deck = deck
         @turns = []
         @number_correct = 0
+        @card_count = 1
     end
 
     def current_card
@@ -15,6 +16,8 @@ class Round
         new_turn = Turn.new(guess, current_card)
         @turns << new_turn
         @number_correct += 1 if new_turn.correct?
+        @card_count += 1
+        p new_turn.feedback
         @deck.rotate_cards
         new_turn
     end
@@ -30,5 +33,21 @@ class Round
     def percent_correct_by_category(category)
         cards_in_category = @turns.select {|turn| turn.card.category == category}.count
         (number_correct_by_category(category).to_f / cards_in_category) * 100
+    end
+
+    def start
+        p "Welcome! You're playing with #{@deck.count} cards."
+        p "-------------------------------------------------"
+        until @card_count > @deck.count
+            p "This is card number #{@card_count} out of #{@deck.count}."
+            p "Question: #{current_card.question}"
+            user_answer = gets.chomp
+            take_turn(user_answer)
+        end
+        p '****** Game over! ******'
+        p "You had #{@number_correct} correct guesses out of #{@deck.count} for a total score of #{percent_correct.to_s.sub(/0+$/, '').sub('.', '')}%."
+        p "STEM - #{percent_correct_by_category(:STEM).to_s.sub(/0+$/, '').sub('.', '')}% correct"
+        p "Turing Staff - #{percent_correct_by_category(:Turing_Staff).to_s.sub(/0+$/, '').sub('.', '')}% correct"
+        p "Pop Culture - #{percent_correct_by_category(:Pop_Culture).to_s.sub(/0+$/, '').sub('.', '')}% correct"
     end
 end
