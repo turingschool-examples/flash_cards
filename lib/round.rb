6d001a2d9a592fea5e04a72f19d19a308f3f3192
Clyde Autin <clyde.autin@gmail.com>
2024-03-23 11:58:44 -0700

    It can now start a turn

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,6 @@
 require './lib/deck'
 require './lib/card'
+require './lib/turn'
 
 class Round
 
@@ -7,6 +8,20 @@ class Round
 
     def initialize(deck)
         @deck = deck
+        @turns = []
+    end
+
+    def turns
+        @turns
+    end
+
+    def current_card
+
+    def take_turn(guess)
+        new_turn = Turn.new(guess, current_card)  # Create a new turn with the current card and the guess
+        @turns << new_turn  # Add the new turn to the list of turns
+        @current_card_index += 1  # Move to the next card for the next turn
+        new_turn  # Return the new turn
     end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe Round do
 
         expect(deck).to be_a(Deck)
     end
+
+    it 'can start a turn' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        my_round = Round.new(deck)
+
+        expect(my_round.turns).to eq([])
+    end
 end
