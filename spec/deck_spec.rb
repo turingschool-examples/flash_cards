fdfaab04fa0738652137a7e58641eef85c1ef3b7
Clyde Autin <clyde.autin@gmail.com>
2024-03-23 09:54:53 -0700

    initial commit

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,9 +1,18 @@
 require "./lib/deck"
+require "./lib/card"
 
 RSpec.configure do |config|
     config.formatter = :documentation
 end
 
 RSpec.describe Deck do
+    it 'exists' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
 
+        expect(deck).to be_a(Deck)
+    end
 end
