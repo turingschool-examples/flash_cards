ab547cb42d5bc2bd15d3e79b83893247011bcad0
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-03-22 20:42:34 -0600

    ACJ first commit complete Card class

---
+++ lib/card.rb
@@ -0,0 +1,13 @@
+class Card
+    
+    attr_reader :question,
+                :answer,
+                :category
+                
+    def initialize(question, answer, category)
+        @question = question
+        @answer = answer
+        @category = category
+    end
+
+end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,9 +1,11 @@
 require './lib/card'
+RSpec.configure do |config|
+  config.formatter = :documentation
 
 RSpec.describe Card do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
+    
     expect(card).to be_instance_of(Card)
   end
 
