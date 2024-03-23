f4ae6567bd3df511c809bddd34b1731aee055c3d
Clyde Autin <clyde.autin@gmail.com>
2024-03-23 10:20:23 -0700

    can now count the number of cards in a category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,5 @@
+require './lib/card.rb'
+
 class Deck
 
     attr_reader :cards, :count
@@ -9,5 +11,9 @@ class Deck
 
     def count
         @count = cards.count
+    end
+
+    def cards_in_category(category)
+        cards_in_category = @cards.count { |card| card.category == category }
     end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe Deck do
 
         expect(deck).to be_a(Deck)
     end
+
+    it 'counts the number of cards in a category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        expect(deck.cards_in_category(:STEM)).to eq(2)
+    end
 end
