d266ccc8a6d71767ef66308330a9d106d95dc579
Zach Bergman <bzach634@gmail.com>
2024-03-22 19:19:32 -0600

    Add current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,12 @@
 class Round
     attr_reader :deck, :turns
-    
+
     def initialize(deck)
         @deck = deck
         @turns = []
+    end
+
+    def current_card
+        @deck.cards[0]
     end
 end
