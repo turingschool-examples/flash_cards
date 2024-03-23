e6c65f08e2c870c7da49bdbb7e9f91cb6dc87a30
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-23 16:03:02 -0600

    Adds first interation of tests for turn_spec.rb file

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,16 +5,53 @@ RSpec.configure do |config|
 end
 
 RSpec.describe Turn do
-  xit 'exists' do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
-    expect(turn.guess).to eq("")
+    expect(turn.guess).to eq("Juneau")
+  end
+
+  it 'has a correct guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to eq true
+  end
+
+  it 'has a incorrect guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Anchorage", card)
+
+    expect(turn.correct?).to eq false
+  end
+
+  it 'has feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq ("Correct!")
+  end
+
+  it 'has correct feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.feedback).to eq ("Correct!")
+  end
+
+  it 'has incorrect feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Anchorage", card)
+
+    expect(turn.feedback).to eq ("Incorrect.")
   end
 
 end
