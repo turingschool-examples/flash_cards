a4a4d63ff64211770d898d311375e31d61695179
Chee Lee <cheeleertr@gmail.com>
2024-03-23 23:04:11 -0500

    Create template for Flashcard_runner.rb

--- runner/flashcard_runner.rb
+++ runner/flashcard_runner.rb
@@ -3,10 +3,11 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-card1 = Card.new()
-card2 = Card.new()
-
-deck = Deck.new()
+card1 = Card.new(["What is 5 + 5?", 10, STEM])
+card2 = Card.new(["What is Rachel's favorite animal?", panda, Turing Staff])
+card3 = Card.new(["What is Mike's middle name?", "nobody knows", Turing Staff])
+card4 = Card.new(["What cardboard cutout lives at Turing?", "Justin Bieber", Pop Culture])
+deck = Deck.new(card1, card2, card3, card4)
 round = Round.new
 
 def start
