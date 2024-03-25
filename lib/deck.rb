58459902f3881c507a47aaea7981a77c440948c7
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-03-24 20:04:53 -0600

    experiment more on decks, stopping point

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,12 +4,13 @@ require './lib/card'
 
 class Deck
 
-	attr_reader :cards,
+	attr_reader :card,
+							:cards,
 
 	def initialize(cards)
 		@cards = cards
+		@card = card
 	end
-
 
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -7,6 +7,18 @@ end
 
 RSpec.describe Deck do
   it 'exists' do
+    deck = Deck.new
+    expect(deck).to be_instance_of(Deck)
+  end
+
+  it 'exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    cards = [card_1]
+    deck = Deck.new(cards)
+    expect(deck).to eq(Deck)
+  end
+
+  xit 'can return a specific card' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -14,14 +26,5 @@ RSpec.describe Deck do
     deck = Deck.new(cards)
     expect(deck).to be_instance_of(Deck)
   end
-
-  it 'can return a specific card' do
-      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-      cards = [card_1, card_2, card_3]
-      deck = Deck.new(cards)
-      expect(deck).to be_instance_of(Deck)
-    end
 
 end
