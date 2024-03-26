e7883e4a9f8ffea28020c7021d5ca5fe65147adb
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-26 15:42:22 -0600

    Add the updated deck.rb and turn.rb file

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,1 +1,17 @@
-g
+require './lib/card'
+
+class Deck
+    attr_reader :cards, :category
+
+    def initialize(cards)
+        @cards = cards
+    end
+    
+    def cards_in_category(category)
+        cards.find_all do |card|
+            card.category == category
+        end
+    end
+
+end
+
