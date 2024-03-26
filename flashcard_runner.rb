68a72ba53bf5f70aa1ab393d15566732afed6417
Chee Lee <cheeleertr@gmail.com>
2024-03-25 20:54:09 -0500

    Fixed flashcard runner class to working, Refactor code

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,22 +1,7 @@
-require './lib/card'
-require './lib/turn'
-require './lib/deck'
-require './lib/round'
-require 'pry'
-
-card1 = Card.new("What is 5 + 5?", "10", :STEM)
-card2 = Card.new("What is Rachel's favorite animal?", "cat", "Turing Staff")
-card3 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff")
-card4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
-cards = [card1, card2, card3, card4]
-deck = Deck.new(cards)
-round = Round.new(deck)
 
 class Flashcard_runner
-    attr_reader :round,
 
-    def initialize(round)
-        @round = round
+    def initialize
         @card_number = 1
         @categories = []
     end
@@ -40,18 +25,6 @@ class Flashcard_runner
             @card_number += 1
         end
 
-    # returns cards but not category
-        # categories = round.deck.cards.select {|card| card.category}
-
-    # trying make variable for category to keep DRY
-        # category = round.turns.card.category
-        # round.turns.each {|turn|
-        #     if category
-        #         categories << category
-        #     end
-        #     }
-
-    # working method after .uniq
         round.turns.each {|turn|
         if turn.card.category
             @categories << turn.card.category
@@ -60,7 +33,8 @@ class Flashcard_runner
         
         categories_uniq = @categories.uniq
         
-        puts "****** Game over! ******\nYou had #{round.number_correct} correct guesses out of #{round.turns.length} for a total score of #{round.percent_correct}."
+        puts "****** Game over! ******"
+        puts "You had #{round.number_correct} correct guesses out of #{round.turns.length} for a total score of #{round.percent_correct}."
 
         category_statement_count = 0
 
@@ -72,30 +46,20 @@ class Flashcard_runner
         end
 end
 
-run_it = Flashcard_runner.new
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+require 'pry'
 
-run_it.start(@round)
+card1 = Card.new("What is 5 + 5?", "10", :STEM)
+card2 = Card.new("What is Rachel's favorite animal?", "cat", "Turing Staff")
+card3 = Card.new("What is Mike's middle name?", "nobody knows", "Turing Staff")
+card4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", "Pop Culture")
+cards = [card1, card2, card3, card4]
+deck = Deck.new(cards)
+round = Round.new(deck)
 
+test_flashcard = Flashcard_runner.new
 
-# def try_again(answer, round)
-#     if answer == "yes"
-#         start(round)
-#     elsif answer == "no"
-#         puts "Hope you had fun."
-#     else 
-#         puts "Invalid answer. Reload to play again."
-#     end
-# end
-
-# loop do
-#     puts "Try Again?
-# Enter 'yes' or 'no'"
-
-# answer = gets.chomp
-#     if answer == "yes"
-#         try_again(answer, round)
-#     else
-#     break
-#     end
-# end
-# end
+test_flashcard.start(round)
