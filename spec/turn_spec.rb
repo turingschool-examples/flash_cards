71f3946f1ef31395659f425f867223057177be6a
Shane Galvin <Sgalvin36@gmail.com>
2024-07-03 16:38:00 -0600

    End of day commit

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,11 +1,16 @@
 require './lib/turn'
+require './lib/card'
 
 RSpec.describe Turn do
     it 'exists' do
         #Check to see a Turn object is created
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn).to be_instance_of(Turn)
     end
 
-    it 'initializes' do
+    xit 'initializes' do
         #Check to see initialization happens by checking each given parameter
         it 'has a guess' do
         end
@@ -14,13 +19,13 @@ RSpec.describe Turn do
         end
     end
 
-    it 'checks the answer' do
+    xit 'checks the answer' do
         #Checks the guess with the answer on the card and sees if they're equal
         #**IF TIME** Provide edge case testing such as correct answer but punctuation 
         #is included or capitalization is wrong
     end
 
-    it 'provides feedback to answer' do
+    xit 'provides feedback to answer' do
         #Provides a response to the user on quality of their answer
     end
 end
