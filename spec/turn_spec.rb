22fd7e850b586c115dd51b0b4a3e7c0fd0cb2674
Shane Galvin <Sgalvin36@gmail.com>
2024-07-03 16:20:25 -0600

    Creates psuedocode for requirements on turn_spec

---
+++ spec/turn_spec.rb
@@ -0,0 +1,26 @@
+require './lib/turn'
+
+RSpec.describe Turn do
+    it 'exists' do
+        #Check to see a Turn object is created
+    end
+
+    it 'initializes' do
+        #Check to see initialization happens by checking each given parameter
+        it 'has a guess' do
+        end
+
+        it 'has a card' do
+        end
+    end
+
+    it 'checks the answer' do
+        #Checks the guess with the answer on the card and sees if they're equal
+        #**IF TIME** Provide edge case testing such as correct answer but punctuation 
+        #is included or capitalization is wrong
+    end
+
+    it 'provides feedback to answer' do
+        #Provides a response to the user on quality of their answer
+    end
+end
