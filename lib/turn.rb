c419a9c6cffe821d31a9413dea616f2dc2f5b08a
Terra Manning <terradmanning@gmail.com>
2024-07-04 08:59:42 -0600

    include draft for  entire code for file, not yet passing corresponding tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,18 +2,28 @@ require './lib/card'
 
 class Turn
   attr_reader :guess, :card
+
   def initialize(guess, card)
     @guess = guess
     @card = card
   end
 
   def correct?
-    if @guess == @card.answer
-     return true
+    if @guess == @answer
+      true
     else
-     return false
+      false
     end
- end
+  end
+
+   def feedback
+    if @guess == @answer 
+      return "Correct!"
+    else 
+      return "Incorrect."
+    end
+  end
+
 end
 
 
