922274c72bd86f9333bd3aa78ac22340ad8edb2f
Candice Cirbo <thecirbos@icloud.com>
2024-07-03 22:11:06 -0600

    Initial commit, methods and test pass

---
+++ lib/deck.rb
@@ -0,0 +1,18 @@
+class Deck
+    attr_reader :cards
+
+    def initialize(cards)
+        @cards = cards
+    end
+
+    def count
+        @cards.count
+    end
+
+    def cards_in_category(category)
+        # require 'pry'; binding.pry
+      @cards.find_all {|card| card.category == category}
+       
+    end
+   
+end
