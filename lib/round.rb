cdc7af3d8c8d6284bc5e7e245fd776950831893e
Candice Cirbo <thecirbos@icloud.com>
2024-07-03 22:28:10 -0600

    adds current_card test and method to the round files

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,15 @@
 class Round
-    attr_reader :deck
+    attr_reader :deck,
+                :turns
 
     def initialize(deck)
         @deck = deck
+        @turns = []
     end
+
+    def current_card
+        @deck.cards.first
+    end
+
+    
 end
