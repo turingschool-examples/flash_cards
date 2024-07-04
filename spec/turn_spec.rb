a15d7e4430492840af118b9048bf509cc10ba9ce
Jim Macur <jim.macur@gmail.com>
2024-07-04 12:16:35 -0600

    Defines card variable

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,9 +3,9 @@ require './lib/turn'
 
 RSpec.describe Turn do
     it 'exists' do
-      turn = Turn.new("Juneau", card)
-  
-      expect(turn).to be_instance_of(Turn)
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+        expect(turn).to be_instance_of(Turn)
     end
 
   end
