81d1ef257fc50de0582232b73450acbe0c557799
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-04 12:07:19 -0400

    Adding methods

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+class Turn
+    attr_reader :guess, :card
+    
+    def initialize(guess, card)
+      @guess = guess
+      @card = card
+    end
+    
+    def correct?
+      @guess == @card.answer
+    end
+    
+    def feedback
+      correct? ? "Correct!" : "Incorrect."
+    end
+  end
