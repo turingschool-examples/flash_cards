6bfd536845436ad8882cd6f9bba2e895f690c376
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-04 19:54:43 -0400

    Add round and round_spec files

---
+++ spec/round_spec.rb
@@ -0,0 +1,11 @@
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
+
+RSpec.describe Round do
+    it 'proves Round exists'
+
+
+    expect(round).to be_a(Round)
+end
