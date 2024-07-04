4482ce189597a7df5ece0b3b9feb70119c0f7dbd
Shane Galvin <Sgalvin36@gmail.com>
2024-07-04 08:55:42 -0600

    Add tests to turn_spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,6 +2,8 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
+#If time try to figure out how to pull common methods out from all tests to simplify code
+
     it 'exists' do
         #Check to see a Turn object is created
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -12,10 +14,15 @@ RSpec.describe Turn do
 
     xit 'initializes' do
         #Check to see initialization happens by checking each given parameter
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
         it 'has a guess' do
+            expect(turn.guess).to eq("Juneau")
         end
 
         it 'has a card' do
+            expect(turn.card).to eq(card)
         end
     end
 
@@ -23,9 +30,17 @@ RSpec.describe Turn do
         #Checks the guess with the answer on the card and sees if they're equal
         #**IF TIME** Provide edge case testing such as correct answer but punctuation 
         #is included or capitalization is wrong
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.correct?).to eq True
     end
 
     xit 'provides feedback to answer' do
         #Provides a response to the user on quality of their answer
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.feedback).to be "Correct!"
     end
 end
