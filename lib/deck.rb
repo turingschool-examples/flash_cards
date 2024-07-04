637f90d7df7e5f65b93315987dff9563c288aa56
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-04 16:47:44 -0400

    Adds the cards in category to only work for one example

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,6 +11,14 @@ class Deck
     end
 
     def cards_in_category(category)
-        return deck.category
+        if @cards[0].category == category
+            return @cards[0]
+        end
     end
+
+    # def cards_in_category(category)
+    #     if cards.any?(category)
+            
+    #     end
+    # end
 end
