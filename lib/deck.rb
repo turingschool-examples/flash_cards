97731ffc5d86837e7803e52e3a8eb8f70ca52274
Kyle Delaney <khdelaney99@gmail.com>
2024-07-04 17:28:59 -0400

    begin work on deck && deck_spec

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,1 +1,11 @@
+require './lib/card'
 require './lib/turn'
+
+class Deck
+    attr_reader :cards,
+                :length
+    def initialize(cards)
+        @cards = cards
+        @count = @cards.length
+    end
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,1 +1,27 @@
+require 'rspec'
 require './lib/deck'
+
+RSpec.describe 'deck' do
+    before(:each) do
+        @deck = Deck.new([
+                            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography),
+                            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM),
+                            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+                        ])
+    end
+    it 'should exist' do
+        expect(@deck).to be_instance_of(Deck)
+    end
+
+    it 'should hold a number of card objects in an array' do
+        expect(@deck.cards).to be_instance_of(Array).and all(be_instance_of(Card))
+    end
+
+    xit 'has an observable length' do
+        #code here
+    end
+
+    xit 'should return cards based on their category' do
+        #code here
+    end
+end
