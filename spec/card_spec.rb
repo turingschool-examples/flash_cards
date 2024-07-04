d6048275da7d07192e8efad403975859ca29f1ca
Candice Cirbo <thecirbos@icloud.com>
2024-07-03 22:08:13 -0600

    Initial commit, all tests pass

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe Card do
     expect(card).to be_instance_of(Card)
   end
 
-  it 'has a question' do
+   it 'has a question' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.question).to eq("What is the capital of Alaska?")
   end
 
-  it 'has an answer' do
+   it 'has an answer' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.answer).to eq("Juneau")
   end
 
-  it 'has a category' do
+   it 'has a category' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.category).to eq(:Geography)
