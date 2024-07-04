3310be482e20c675323c85f4c14607720cee705c
Terra Manning <terradmanning@gmail.com>
2024-07-03 21:33:16 -0600

    Add tests for has a guess and correct or incorect guess

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -10,5 +10,24 @@ RSpec.describe Turn do
     expect(turn).to be_instance_of(Turn)
   end
 
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Janeau", card)
+    expect(turn.guess).to eq("Janeau")
+  end
+
+  xit 'has a correct guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Janeau", card)
+    expect(turn.correct?).to eq(true)
+  end
+
+  it "has an incorrect guess" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Anchorage", card) 
+
+    expect(turn.correct?).to eq(false)
+  end
+
 end
 
