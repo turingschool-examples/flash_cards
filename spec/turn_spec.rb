0c2ddf089404ee97f4817addde45ff48ce673594
Jim Macur <jim.macur@gmail.com>
2024-07-04 11:43:52 -0600

    Add requires and exist test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,3 +1,12 @@
-class Turn
-    
-end
+require './lib/card'
+require './lib/turn'
+
+RSpec.describe Turn do
+    it 'exists' do
+      turn = Turn.new("Juneau", card)
+  
+      expect(turn).to be_instance_of(Turn)
+    end
+
+  end
+  
