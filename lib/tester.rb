788a60aca5f4b4cb3d9ee7f94f39d3c466c65c47
John Hill <jphill1989@gmail.com>
2024-07-03 20:14:34 -0600

    Created the round object, and an initaliaze function for two attributes

--- lib/tester.rb
+++ lib/tester.rb
@@ -20,7 +20,10 @@
 # end
 
 require './lib/card'
+require './lib/turn'
 require './lib/deck'
+require './lib/round'
+
 require 'rspec'
 
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -28,6 +31,7 @@ card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and repo
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 cards = [card_1, card_2, card_3]
 deck = Deck.new(cards) # the array of cards object
+round = Round.new(deck)
 
 
 p deck.cards_in_category(:Geography)
