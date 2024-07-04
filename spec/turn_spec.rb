ae51c5ebac2b35383719cce5a9b388838edeab4e
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-04 12:14:42 -0400

    Add initialize test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,3 +1,11 @@
 require './lib/turn'
 require './lib/card'
 require 'pry'
+
+RSpec.describe Turn do
+    it "initializes with guess and card" do
+        turn = Turn.new("Juneau", card)
+        expect(turn.guess).to eq "Juneau"
+        expect(turn.card).to eq card
+      end
+    end 
