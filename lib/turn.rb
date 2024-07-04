90af5a1059f404ddc042f829b8b065603cf765c7
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-04 12:38:35 -0600

    show turn work

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+class Turn
+
+    attr_reader :guess, :card
+    
+    def initialize(guess, card)
+        @guess = guess
+        @card = card
+    end
+
+    def correct?
+        @guess == @card
+    end
+            
+    def feedback
+        if @guess == @card
+            then "Correct!"
+            else "Incorrect."
+        end
+    end
+
+
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,11 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+    it 'exists' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn).to be_instance_of(Turn)
+    end
+end
