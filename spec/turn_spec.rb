0e089fb59a2f61c072f8271de7cb722db6936edf
Jim Macur <jim.macur@gmail.com>
2024-07-04 19:05:08 -0600

    Test a new turn quess

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe Turn do
         expect(turn).to be_instance_of(Turn)
     end
 
-  end
+    it "has another turn" do
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Dog", card)
+        turn.correct?
+        expect(turn.correct?).to be (false)   
+    end
+
+ end
+  
   
