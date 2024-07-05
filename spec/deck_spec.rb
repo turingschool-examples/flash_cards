483a34a507bb57dcc4af26f0916e0f4114d7c10b
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-05 14:38:19 -0400

    Initial Commit

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,10 @@
 require './lib/card'
 require './lib/deck'
 require 'pry'
+
+RSpec.describe Deck do
+    it 'exists' do 
+        deck = Deck.new(cards)
+        expect(deck).to be_an_instance_of Deck
+    end
+end
