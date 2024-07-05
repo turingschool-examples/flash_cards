841fa44a633a93c7c0f63c390eb451e3389a9ae9
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-04 20:01:44 -0400

    Add a class for Round and a test proving the class exists

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,8 +4,14 @@ require './lib/deck'
 require './lib/round'
 
 RSpec.describe Round do
-    it 'proves Round exists'
+    it 'proves Round exists' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
 
-    expect(round).to be_a(Round)
+        expect(round).to be_a(Round)
+    end 
 end
