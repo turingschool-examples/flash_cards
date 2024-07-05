f94b7a37931d902844f43a996c954b2a2cc726d3
Jim Macur <jim.macur@gmail.com>
2024-07-05 14:53:12 -0600

    Corrects the correct? method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,16 +5,21 @@ RSpec.describe Turn do
     it 'exists' do
         card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         turn = Turn.new("Juneau", card)
+      
         expect(turn).to be_instance_of(Turn)
     end
 
-    it "has another turn" do
+    it "can guess wrong" do
         card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
-        turn = Turn.new("Dog", card)
+        turn = Turn.new("Saturn", card)
         turn.correct?
         expect(turn.correct?).to be (false)   
     end
 
+    it "can guess right" do
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Mercury", card)
+        turn.correct?
+        expect(turn.correct?).to be (true)   
+    end
  end
-  
-  
