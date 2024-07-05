3b7cddd7bfe52a97a58d7080a5954dcf8fea59b8
Terra Manning <terradmanning@gmail.com>
2024-07-05 15:01:04 -0600

    Add before:each and config.formatter to file for setup

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,16 +3,39 @@ require './lib/turn'
 require './lib/deck'
 require 'pry'
 
+RSpec.configure do |config|
+  config.formatter = :documentation
+  end
+
 RSpec.describe Deck do
-  it 'exists' do
+  before(:each) do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    cards = [card_1, card_2, card_3]
+  end
+
+
+
+  it 'exists' do
     deck = Deck.new(@cards)
     expect(deck).to be_instance_of(Deck)
   end
 end
+
+
+
+# cards = [card_1, card_2, card_3]
+
+#   it 'has a new card' do
+#     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+#     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+#     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+#     cards = [card_1, card_2, card_3]
+#     deck = Deck.new(@cards)
+#     expect(deck.cards).to eq([card_1, card_2, card_3])
+#   end
+# end
+
 
   # xit "shows cards" do
   #   cards = [card_1, card_2, card_3]
