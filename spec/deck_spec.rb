70503570104e34f2e2d5b9f4766c02b3c6eb96e7
Terra Manning <terradmanning@gmail.com>
2024-07-06 14:29:11 -0600

    Clean up code for completed deck test. Confirmed pry tests passed.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe Deck do
     expect(deck.count).to eq(3)
   end
 
-  xit "displays the cards by category" do
+  it "displays the cards by category" do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -45,26 +45,8 @@ RSpec.describe Deck do
     expect(deck.cards_in_category(:Geography)).to eq([card_1])
     expect(deck.cards_in_category("Pop Culture")).to eq([])
   end
-
+  
 end
 
 
 
-# cards = [card_1, card_2, card_3]
-
-#   it 'has a new card' do
-#     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#     cards = [card_1, card_2, card_3]
-#     deck = Deck.new(@cards)
-#     expect(deck.cards).to eq([card_1, card_2, card_3])
-#   end
-# end
-
-
-  # xit "shows cards" do
-  #   cards = [card_1, card_2, card_3]
-  #   deck = Deck.new(@cards)
-  #   expect(deck.cards).to eq([card_1, card_2, card_3])
-  # end
