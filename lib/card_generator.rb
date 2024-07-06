2fb377003cfb8ef2dc408e535b5b1e3860ea734a
John Hill <jphill1989@gmail.com>
2024-07-06 10:23:27 -0600

    turned card_generator into a class, and am able to read the tex file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,13 +1,20 @@
 # source https://www.writesoftwarewell.com/how-to-read-file-in-ruby/#:~:text=The%20simplest%20way%20to%20access,file%20%3D%20File.
+require './lib/card.rb'
 
-File.foreach("./lib/card.txt") do |line| 
-  puts line
-  cards_data = line.chomp.split(",")
-  p cards_data
-  # puts line.class
+class CardGenerator
+
+  def initialize(filename = "./lib/card.txt")
+
+    File.foreach(filename) do |line| 
+      # puts line
+      cards_data = line.chomp.split(",")
+      cards_data[2] = cards_data[2].to_sym
+      puts cards_data[2].class
+      card = Card.new(cards_data[0],cards_data[1],cards_data[2])
+      p card
+    end
+  end
+
 end
 
-
-# puts content
-
-# puts content.class
+card_generator = CardGenerator.new
