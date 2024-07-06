1657ee48d0c6d7e2a6fcfb160aae2b4dbf273eae
Jim Macur <jim.macur@gmail.com>
2024-07-05 22:07:11 -0600

    Adds before each

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,14 +3,15 @@ require './lib/deck'
 
 RSpec.describe Deck do
     
-    it 'exists' do
+    before(:each) do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         cards = [card_1, card_2, card_3]
         deck = Deck.new(cards)
-        expect(deck).to be_instance_of(Deck)
     end
     
-
+    it 'exists' do
+        expect(deck).to be_instance_of(Deck)
+    end
 end
