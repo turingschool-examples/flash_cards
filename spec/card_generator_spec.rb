e5f965a093229add3e6543bece53c242b13d57ca
John Hill <jphill1989@gmail.com>
2024-07-06 10:45:28 -0600

    Created functioning tests for card_generator

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -2,11 +2,19 @@ require './lib/card.rb'
 require './lib/card_generator.rb'
 require 'rspec'
 
-RSpec.describe Card do
+RSpec.describe CardGenerator do
   before(:each) do
-    @card_generator = CardGenerator.new()
+    @card_generator = CardGenerator.new("./lib/card.txt")
   end
+
   it 'exsist' do
+    expect(@card_generator).to be_instance_of(CardGenerator)
     
   end
+
+  it 'generates a list of card objects' do
+    expect(@card_generator.cards.class).to eq Array
+    expect(@card_generator.cards[3]).to be_instance_of(Card)
+  end
+
 end
