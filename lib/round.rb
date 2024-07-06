47e0ba5ca326586983ed8c0db1f4a722e97b7d72
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-06 17:01:27 -0600

    Add tests to round_spec.rb

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,7 @@
 class Round
-    
+    attr_reader :deck
+
+    def initialize(deck)
+        @deck = deck
+    end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,12 +1,29 @@
 require './lib/round'
+require './lib/turn'
 require './lib/deck'
 require './lib/card'
 
 RSpec.describe Round do
     before(:each) do
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         @deck = Deck.new([@card_1, @card_2, @card_3])
-
+        @round = Round.new(@deck)
     end
 
+    it 'exists' do
+        expect(@round).to be_instance_of(Round)
+    end
+
+    it 'has deck' do
+        expect(@round.deck).to eq(@deck)
+    end
+
+    it 'has cards in deck' do
+        expect(@deck.cards).to eq([@card_1, @card_2, @card_3])
+    end
+
+    
 
 end
