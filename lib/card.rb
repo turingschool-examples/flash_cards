1a30a839b2c136519eb02ea8cbb5cb781f42d5e4
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-06 16:49:13 -0600

    Remove bug (false assignment) from Card Class

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,7 @@
 class Card
      attr_reader :question, :answer, :category
     
-    def initialize(question, answer, category = "History") 
+    def initialize(question, answer, category) 
         @question = question
         @answer = answer
         @category = category
