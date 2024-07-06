16c6899002d9c95c38bfd51ab2e8079c62b30355
Terra Manning <terradmanning@gmail.com>
2024-07-06 12:02:29 -0600

    Add the code that confirms that incorrect guesses are handled properly

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,13 +6,12 @@ class Turn
   def initialize(guess, card)
     @guess = guess
     @card = card
-    @question = @card.question
+    # @question = @card.question
     @answer = @card.answer
-    @category = @card.category
+    # @category = @card.category
   end
 
   def correct?
-    # return true if @guess = @answer
     return true if @guess = @card.answer
     false
   end
