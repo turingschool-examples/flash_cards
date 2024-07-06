ac676ecfc86d0fcffd11f07f51c5746642273e87
John Hill <jphill1989@gmail.com>
2024-07-06 10:29:44 -0600

    Created CardGenerator class, and its behaving as expected-- need to create test for it

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -2,19 +2,22 @@
 require './lib/card.rb'
 
 class CardGenerator
+  attr_reader :cards
 
   def initialize(filename = "./lib/card.txt")
-
+    @cards = []
     File.foreach(filename) do |line| 
       # puts line
       cards_data = line.chomp.split(",")
       cards_data[2] = cards_data[2].to_sym
       puts cards_data[2].class
+      puts cards_data[2]
       card = Card.new(cards_data[0],cards_data[1],cards_data[2])
-      p card
+      @cards << card
     end
   end
 
 end
 
 card_generator = CardGenerator.new
+puts card_generator.cards
