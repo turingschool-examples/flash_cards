93cdb6b00a96c95ee15e439163dee33f0a99bbef
Kyle Delaney <khdelaney99@gmail.com>
2024-07-05 21:10:59 -0400

    add turns test

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,11 @@
+require './lib/deck'
+require './lib/turn'
+
 class Round
-   
+   attr_reader :deck
     
     def initialize(deck)
         @deck = deck
+        
     end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -6,10 +6,21 @@ RSpec.configure do |config|
     end
 
 
-RSpec.configure 'round' do
-    it 'should exist' do
-        round = Round.new(deck)
+RSpec.describe 'round' do
 
-        expect(round).to be_instance_of Round
+    before(:each) do
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        @deck = Deck.new([@card_1, @card_2, @card_3])
+        @round = Round.new(@deck)
+    end
+    it 'should exist' do
+
+        expect(@round).to be_an_instance_of Round
+    end
+
+    it 'should have an attribute named turns' do
+        expect(@round.turns).to be_an_instance_of Array
     end
 end
