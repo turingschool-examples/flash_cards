73cd1097340575ee22d8da0a22f105367c811f8a
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-06 11:16:19 -0400

    Initial Commit

---
+++ spec/round_spec.rb
@@ -0,0 +1,23 @@
+require './lib/card'
+
+
+require './lib/turn'
+
+
+require './lib/deck'
+
+
+require './lib/round'
+
+
+RSpec.describe Round do 
+    before(:each) do 
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        @cards = [@card_1, @card_2, @card_3]
+        @deck = Deck.new(@cards)
+        @round = Round.new(deck) 
+    end
+
+end
