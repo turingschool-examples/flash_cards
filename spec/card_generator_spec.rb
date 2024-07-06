88e864cb05683801189fe1b40d38503b09ebcf47
John Hill <jphill1989@gmail.com>
2024-07-06 10:39:57 -0600

    Created a single text inside card_generator_spec.rb

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,2 +1,12 @@
 require './lib/card.rb'
 require './lib/card_generator.rb'
+require 'rspec'
+
+RSpec.describe Card do
+  before(:each) do
+    @card_generator = CardGenerator.new()
+  end
+  it 'exsist' do
+    
+  end
+end
