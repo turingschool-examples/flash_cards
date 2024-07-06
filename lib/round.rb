86baf44a471ee143fb4b534e378866ce6f44c6fa
Kyle Delaney <khdelaney99@gmail.com>
2024-07-05 21:12:24 -0400

    add turn attribute to round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -2,10 +2,12 @@ require './lib/deck'
 require './lib/turn'
 
 class Round
-   attr_reader :deck
+    attr_reader :deck,
+                :turns
     
     def initialize(deck)
         @deck = deck
+        @turns = []
         
     end
 end
