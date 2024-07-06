695bbdc4ab84695d2414b30703f1308576d27daf
Jim Macur <jim.macur@gmail.com>
2024-07-06 11:43:11 -0600

    Adds has cards in the deck test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,14 +4,20 @@ require './lib/deck'
 RSpec.describe Deck do
     
     before(:each) do
-        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        cards = [card_1, card_2, card_3]
-        deck = Deck.new(cards)
+        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        @cards = [@card_1, @card_2, @card_3]
+        @deck = Deck.new(@cards)
     end
-    
+
     it 'exists' do
-        expect(deck).to be_instance_of(Deck)
+        expect(@deck).to be_instance_of(Deck)
+        expect(@card_1).to be_instance_of(Card)
+        expect(@deck.cards.class).to eq Array
+    end
+
+    it 'has cards in the deck' do
+        expect(@deck.cards).to eq([@card_1, @card_2, @card_3])
     end
 end
