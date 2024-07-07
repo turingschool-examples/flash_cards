0e888b7f5da9d6e7dc93bcd63c2f130d2f67f041
Terra Manning <terradmanning@gmail.com>
2024-07-07 10:46:35 -0600

    Refactor correct?  method and other small changes

--- lib/round.rb
+++ lib/round.rb
@@ -1,28 +1,43 @@
 require './lib/card'
 
 class Round
-  attr_reader :deck, :turns, :turn, :guess, :new_card, :current_card
+  attr_reader :deck, :turns, :current_card, :turn_counter, :number_correct, :new_turn
 
   def initialize(deck)
-  @deck = deck
-  @turns = []
-  @turn_counter = -1
-  @guess = guess
-  @new_card
-  @current_card
+    @deck = deck
+    @turns = []
+    @turn_counter = 0
+    @current_card = deck.cards[turn_counter]
+    @number_correct = 0
+    @new_turn 
   end
 
-end
+  # def new_turn
+  #   if (@deck.count 0) > @turn_counter
+  #     @turn_counter += 1
+  #     @current_card = @deck.cards[@turn_counter]
+  #   end
+  # end
 
-def new_card
-  if (@deck.count -1) > @turn_counter
+  def take_turn(guess)
+    turn = Turn.new(guess, @current_card)
+    turns.push(turn)
+    @guess = guess
     @turn_counter += 1
-    @current_card = @deck.cards[@turn_counter]
+    @number_correct += 1 if turn.correct?
+    turn
   end
-end
 
-def take_turn(guess)
-  new_turn = Turn.new(guess, @current_card)
-  @turns.push(new_turn)
-  return new_turn
+  def number_correct_by_category 
+
+  end
+  
+  def percent_correct_by_category
+
+  end
+
+  def correct?
+    @guess == @card.answer
+  end
+
 end
