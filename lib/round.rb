1a084da809543dc9cb2abccdaddfeceae61a4070
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-07 14:35:56 -0400

    Add a number_correct method change and as associated test

--- lib/round.rb
+++ lib/round.rb
@@ -2,9 +2,6 @@ class Round
     
     attr_reader :deck,
                 :turns
-                # :current_card,
-
-                
 
     def initialize(deck)
         @deck = deck
@@ -21,7 +18,6 @@ class Round
     def take_turn(guess)
         new_turn = Turn.new(guess, current_card)
         @turns.push(new_turn)
-        # @turns << new_turn
         @current_card_index += 1
         return new_turn
     end
@@ -29,8 +25,9 @@ class Round
 
     def number_correct
         @turns.count do |turn|
-            turn.correct?
+            if turn.correct? == true
             @correct_guesses += 1
+            end
         end
         return @correct_guesses
     end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -76,4 +76,17 @@ RSpec.describe Round do
 
         expect(round.number_correct).to eq(1)
     end
+
+    it 'will add 1 to correct guesses' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        new_turn = round.take_turn("Juneau")
+
+        expect(round.number_correct).to eq(1)
+    end
 end
