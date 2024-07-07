07043dd6c01547dc219bb759b1cac65722683305
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-06 21:32:35 -0400

    Added more methods

--- lib/round.rb
+++ lib/round.rb
@@ -25,15 +25,18 @@ class Round
 
     
 
-    # def number_correct_by_category(category)
-    #     1
-    # end
+    def number_correct_by_category(category)
+        @turns.count { |turn| turn.correct? && turn.card.category == category }
+      end
+   
 
-    # def percent_correct
-    #    50.0
-    # end
+    def percent_correct
+        (@turns.count { |turn| turn.correct? }.to_f / @turns.size) * 100
+    end
 
-    # def percent_correct_by_category(category)
-    #    100.0
-    # end
+    def percent_correct_by_category(category)
+        category_turns = @turns.select { |turn| turn.card.category == category }
+        return 0 if category_turns.empty?
+        (category_turns.count { |turn| turn.correct? }.to_f / category_turns.size) * 100
+    end
 end 
