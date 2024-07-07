d7b3a2244612b9d8ffcda600665e57fe4d42f376
Terra Manning <terradmanning@gmail.com>
2024-07-07 10:45:34 -0600

    Remove bugs

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -6,47 +6,55 @@ require 'pry'
 
 RSpec.configure do |config|
   config.formatter = :documentation
-  end
+end
 
 RSpec.describe Round do
-it 'exists' do
-  card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-  card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-  card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-  deck = Deck.new([@card_1, @card_2, @card_3])
-  round = Round.new(deck)
-  expect(round).to be_instance_of(Round)
+  before(:each) do
+    @card_1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+    @card_2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+    @card_3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+    
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+    @round = Round.new(@deck)
   end
 
-it 'returns an empty array' do
-  card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-  card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-  card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-  deck = Deck.new([@card_1, @card_2, @card_3])
-  round = Round.new(deck)
-  expect(round.turns).to eq([])
-end
+  it 'exists' do
+    expect(@round).to be_an_instance_of(Round)
+  end
 
-it 'show the current card' do
-  card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-  card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-  card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-  deck = Deck.new([@card_1, @card_2, @card_3])
-  round = Round.new(deck)
-  expect(round.current_card).to eq(@card_1)
-end
+  it 'returns an empty array' do
+    expect(@round.turns).to eq([])
+  end
 
-it 'take a new turn' do
-  card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-  card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-  card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-  deck = Deck.new([@card_1, @card_2, @card_3])
-  round = Round.new(deck)
-  new_turn = round.take_turn("Juneau")
-  # expect(new_turn).to eq(turn)
-  # expect(new_turn.class). to eq(Turn)
-  expect(round.turns[0]).to eq(new_turn)
-end
+  it 'show the current card' do 
+    expect(@round.current_card).to eq(@card_1)
+  end
+
+  it 'Makes a turn into a turn object' do
+    new_turn = @round.take_turn("Juneau")
+    
+    expect(new_turn).to be_an_instance_of(Turn)
+  end
+
+  xit 'Shows the new class' do
+    new_turn = round.take_turn("Juneau")
+    new_turn.class
+    
+    expect(new_turn.class).to eq(Turn)
+  end
+
+  xit 'shows if the answer is correct' do
+    new_turn = round.take_turn("Juneau")
+    
+    expect(new_turn.correct?).to eq(True)
+  end
+
+  xit 'shows if the answer is incorrect' do 
+    new_turn = round.take_turn("Wrong Answer")
+    
+    expect(new_turn.correct?).to eq(False)
+  end
+
 
 end
- 
+  
