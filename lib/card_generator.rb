6d6174233789afa0a03baf30ae7f147ca03d1583
Shane Galvin <Sgalvin36@gmail.com>
2024-07-07 14:39:59 -0600

    test: Add four tests for building cards

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,9 +1,24 @@
 
 class CardGenerator
-    attr_reader :filename
-    
+    attr_reader :filename,
+                :file,
+                :data
+
     def initialize(filename)
         @filename = filename
+        @file = set_file
+        @data = get_data
     end
 
+    def set_file
+        @file = File.open(@filename)
+    end
+
+    def get_data
+        @data = file.readlines.map(&:chomp)
+    end
+
+    def cards
+        
+    end
 end
--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -17,4 +17,31 @@ RSpec.describe CardGenerator do
     it 'has existing .txt file' do
         expect(@cards.filename).to eq @filename
     end
+
+    it 'opens the file' do
+        expect(@cards.file).to be_instance_of File
+    end
+
+    it 'reads the data on the file' do
+        expect(@cards.data). to be_instance_of Array
+    end
+
+    it 'splits first string into three string' do
+        expect(@cards.card_data[@cards.data[0]]). to eq ["What is the capital of Alaska?","Juneau","Geography"]
+    end
+
+    xit 'splits last string into three strings' do
+        expect(@cards.card_data[@cards.data[6]]). to eq ["What is the capital of Alaska?","Juneau","Geography"]
+    end
+
+    xit 'create a card using each new string for each attribute' do
+    
+    end
+    
+    xit 'adds new Card into cards array' do
+
+    end
+    xit 'creates a card for each line in the file' do
+        expect(@cards.cards.count). to eq @cards.data.count 
+    end
 end
