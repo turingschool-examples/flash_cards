cc01852888f23e1ce94091c1b824003ba97e5f40
Shane Galvin <Sgalvin36@gmail.com>
2024-07-08 14:43:04 -0600

    refactor: Pull all shared lines out to single instance

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,21 +2,15 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
-#If time try to figure out how to pull common methods out from all tests to simplify code
-
+    before (:all) do
+        @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        @turn = Turn.new("Juneau", @card)
+    end
     it 'exists' do
-        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn = Turn.new("Juneau", card)
-
-        expect(turn).to be_instance_of(Turn)
+        expect(@turn).to be_instance_of(Turn)
     end
 
     describe 'initialize' do
-         before do
-            @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-            @turn = Turn.new("Juneau", @card)
-        end
-
         it 'has a guess' do
             expect(@turn.guess).to eq("Juneau")
         end
@@ -27,12 +21,9 @@ RSpec.describe Turn do
     end
 
     it 'checks the answer' do
-        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn = Turn.new("Juneau", card)
+        expect(@turn.correct?).to eq true
 
-        expect(turn.correct?).to eq true
-
-        turn = Turn.new("Cheboygan", card)
+        turn = Turn.new("Cheboygan", @card)
         expect(turn.correct?).to eq false
     end
 
@@ -42,12 +33,9 @@ RSpec.describe Turn do
     # end
 
     it 'provides feedback to answer' do
-        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn = Turn.new("Juneau", card)
+        expect(@turn.feedback).to eq "Correct!"
 
-        expect(turn.feedback).to eq "Correct!"
-
-        turn = Turn.new("Cheboygan", card)
+        turn = Turn.new("Cheboygan", @card)
         expect(turn.feedback).to eq "Better luck next time"
     end
 end
