e18f02261830cb7005d9c1a0da14766774e8b535
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-07-07 19:52:08 -0600

    fix: subbed integer for @card_index counter

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,18 @@
 class Round
-    attr_reader :deck
+    attr_reader :deck, :turns
 
     def initialize(deck)
         @deck = deck
+        @turns = []
+        @card_index = 0
+    end
+
+    def current_card
+        card = @deck.cards[@card_index]
+        return card  
+    end
+
+    def take_turn(guess)
+
     end
 end
