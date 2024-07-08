a0d9cb139310c2a87b24951c4a9c4f22c1a76215
Terra Manning <terradmanning@gmail.com>
2024-07-07 20:16:32 -0600

    refactor redundant count method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,11 @@
 require './lib/card'
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :count
 
   def initialize(cards)
     @cards = cards
     @count = cards.count
-  end
-
-  def count
-    @cards.count
   end
 
   def cards_in_category(category)
