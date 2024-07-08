c5ee69a2a59b57bf8caa8da47a13709d477b424a
Kyle Delaney <khdelaney99@gmail.com>
2024-07-08 19:46:08 -0400

    refactor deck_spec for readability

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -14,19 +14,25 @@ RSpec.describe 'deck' do
         @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         @deck = Deck.new([@card_1, @card_2, @card_3])
     end
-    it 'should exist' do
-        expect(@deck).to be_instance_of(Deck)
+    describe '#initialize' do
+        it 'should exist' do
+            expect(@deck).to be_instance_of(Deck)
+        end
+
+        it 'should hold a number of card objects in an array' do
+            expect(@deck.cards).to be_instance_of(Array)
+        end
+
+        it 'has an observable length' do
+            expect(@deck.cards.count).to eq 3
+        end
     end
 
-    it 'should hold a number of card objects in an array' do
-        expect(@deck.cards).to be_instance_of(Array)
-    end
+    describe '#cards_in_category' do
 
-    it 'has an observable length' do
-        expect(@deck.cards.count).to eq 3
-    end
+        it 'should return cards based on their category' do
+            expect(@deck.cards_in_category(:STEM)).to eq [@card_2, @card_3]
+        end
 
-    it 'should return cards based on their category' do
-        expect(@deck.cards_in_category(:STEM)).to eq [@card_2, @card_3]
     end
 end
