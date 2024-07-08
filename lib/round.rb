88428d0cfb8da275cbf7cc9937ec6f77a944596e
Candice Cirbo <thecirbos@icloud.com>
2024-07-07 19:27:26 -0600

    refactor: methods in the round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,15 +1,53 @@
 class Round
     attr_reader :deck,
-                :turns
+                :turns,
+                :number_correct                 
 
     def initialize(deck)
         @deck = deck
         @turns = []
+        @number_correct = 0
     end
 
     def current_card
         @deck.cards.first
     end
 
+   
+    def take_turn(guess)
+       new_turn = Turn.new(guess, current_card) 
+       if new_turn.correct?
+            @number_correct += 1
+       end
+       @turns << new_turn
+       @deck.cards.shift
+        new_turn
+    end
+
+    # def number_correct_by_category(category)
+    #     @turns.find_all do |turn|
+    #         (turn.correct?) && (turn.card.category == category)
+    #     end.count
+    # end
+    def number_correct_by_category(category)
+        @turns.count do |turn|
+             turn.correct? && turn.card.category == category
+        end
+    end
+
+    def percent_correct
+        (number_correct.to_f / @turns.count * 100).round(1)
+    end   
+
+   
+    def percent_correct_by_category(category)
+        (number_correct_by_category(category).to_f / @turns.count {|turn| turn.card.category == category} * 100).round(1)
+        # require 'pry'; binding.pry
+    end
+
+    
     
 end
+
+
+  
