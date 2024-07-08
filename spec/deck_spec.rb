0b8fb339f31404e5f5569912181d2f00cfdbe450
Terra Manning <terradmanning@gmail.com>
2024-07-07 20:26:19 -0600

    Refactor code by adding before:each

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -8,42 +8,31 @@ RSpec.configure do |config|
   end
 
 RSpec.describe Deck do
+  before(:each) do
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "East", :STEM)
+
+    @cards = [@card_1, @card_2, @card_3]
+    @deck = Deck.new(@cards)
+  end
+
   it 'exists' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "East", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    expect(deck).to be_instance_of(Deck)
+    expect(@deck).to be_instance_of(Deck)
   end
 
   it 'has cards' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "East", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    expect(deck.cards).to eq(cards)
+    expect(@deck.cards).to eq(@cards)
   end
 
   it "counts cards" do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "East", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    expect(deck.count).to eq(3)
+    expect(@deck.count).to eq(3)
   end
 
   it "displays the cards by category" do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "East", :STEM)
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
-    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
-    expect(deck.cards_in_category(:Geography)).to eq([card_1])
-    expect(deck.cards_in_category("Pop Culture")).to eq([])
+    expect(@deck.cards_in_category(:STEM)).to eq([@card_2, @card_3])
+    expect(@deck.cards_in_category(:Geography)).to eq([@card_1])
+    expect(@deck.cards_in_category("Pop Culture")).to eq([])
   end
 
 end
