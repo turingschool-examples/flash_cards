3685e7a9c8f26d78ad0ab6fceae49afb5dd06e8d
Terra Manning <terradmanning@gmail.com>
2024-07-09 11:30:08 -0600

    test to see if test file was removed

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,41 +6,47 @@ RSpec.configure do |config|
   config.formatter = :documentation
   end
 
-  RSpec.describe Turn do
-    it 'exists' do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Janeau", card)
-      expect(turn).to be_instance_of(Turn)
-    end
-  
-    it 'has a card' do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Janeau", card)
-      expect(turn.card).to eq(card)
-    end
-  
-    it 'has a guess' do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Janeau", card)
-      expect(turn.guess).to eq("Janeau")
-    end
-  
-    it 'has a correct guess' do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Juneau", card)
-      expect(turn.correct?).to eq true
-    end
-  
-    it "gives feedback for correct answer" do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Juneau", card) 
-      expect(turn.feedback).to eq("Correct!")
-    end
-  
-    it "gives feedback for incorrect answer" do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Wrong Answer", card) 
-      expect(turn.feedback).to eq("Incorrect.")
-    end
-  
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Janeau", card)
+
+    expect(turn).to be_instance_of(Turn)
   end
+  
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Janeau", card)
+
+    expect(turn.card).to eq(card)
+  end
+  
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Janeau", card)
+
+    expect(turn.guess).to eq("Janeau")
+  end
+  
+  it 'has a correct guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    expect(turn.correct?).to eq true
+  end
+  
+  it "gives feedback for correct answer" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card) 
+
+    expect(turn.feedback).to eq("Correct!")
+  end
+  
+  it "gives feedback for incorrect answer" do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Wrong Answer", card) 
+    
+    expect(turn.feedback).to eq("Incorrect.")
+  end
+  
+end
