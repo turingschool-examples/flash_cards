592a46072391af0e9216291e2f3b0d633a3ca141
Shane Galvin <Sgalvin36@gmail.com>
2024-07-08 21:58:04 -0600

    refactor: change the randomize class to only call each card once

--- lib/randomize.rb
+++ lib/randomize.rb
@@ -1,15 +1,24 @@
 class Randomize
     attr_reader :deck,
-                :card_amount
+                :card_amount,
+                :card_number,
+                :give_number
 
     def initialize(deck)
         @deck = deck
         @card_amount = @deck.count
+        @card_number = set_card_number(deck)
+        @give_number = 0
     end
 
+    def set_card_number(deck)
+        (1..deck.count).to_a
+    end
+    
     def pick
-        @card_amount -= 1
-        rand(0..@card_amount)
+        @give_number = @card_number.sample
+        @card_number.delete(@give_number)
+        @give_number -= 1
     end
 
 end
--- spec/randomize_spec.rb
+++ spec/randomize_spec.rb
@@ -23,19 +23,24 @@ RSpec.describe Randomize do
         expect(@randomize.card_amount).to eq 7
     end
 
-    it 'picks a random number' do
-        expect(@randomize.pick).to be <= 6
+    it 'creates an array of number of cards' do
+        expect(@randomize.card_number).to eq [1, 2, 3, 4, 5, 6, 7]
+    end
+    
+    xit 'picks a random number' do
+        @randomize.pick
+        expect(@randomize.give_number).to be <= 6
     end
 
-    it 'lowers card_amount by one each time' do
-        #not sure why this is counting the card_amount to 6 instead of 7,
-        #isn't it a new instance of Random?
-        expect(@randomize.card_amount).to eq 6
+    it 'lowers card_number by one each time' do
+        #not sure why this is counting the card_number to 6 instead of 7,
+        #isn't it a new instance of Randomize? if I skip the prior test this one passes
+        expect(@randomize.card_number).to eq [1, 2, 3, 4, 5, 6, 7]
         
         @randomize.pick
-        expect(@randomize.card_amount).to eq 5
+        expect(@randomize.card_number.count).to eq 6
         
         @randomize.pick        
-        expect(@randomize.card_amount).to eq 4
+        expect(@randomize.card_number.count).to eq 5
     end
 end
