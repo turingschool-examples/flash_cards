5f07741d8b9e1cb333fa48dbbbebca5ddd78b9d3
Shane Galvin <Sgalvin36@gmail.com>
2024-07-09 11:38:01 -0600

    test: Add six tests and corresponding code

--- lib/high_score.rb
+++ lib/high_score.rb
@@ -1,6 +1,28 @@
 class HighScore
+    attr_reader :filename,
+                :scores,
+                :current_score,
+                :user_name
 
-    def initialize(round)
+    def initialize(round, filename)
+        @filename = filename
+        @scores = get_scores
+        @current_score = round.number_correct
+        @user_name = "Default John"
+    end
 
+    def get_scores
+        @scores = File.readlines(filename)
+        @scores = @scores.map do |score|
+            score.chomp
+        end
+    end
+
+    def get_user_name(name)
+        @user_name = name
+    end
+
+    def add_score(name, score)
+        scores << "#{name}, #{score}"
     end
 end
--- spec/high_score.rb
+++ spec/high_score.rb
@@ -1,4 +1,8 @@
-require './runner'
+require './lib/high_score'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+require './lib/round'
 
 RSpec.configure do |config|
     config.formatter = :documentation
@@ -6,34 +10,49 @@ end
 
 RSpec.describe HighScore do
     before (:all) do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
+        @cards = [card_1, card_2, card_3]
+        @deck = Deck.new(@cards)
+        @round = Round.new(@deck)
+        @round.take_turn("Juneau")
+        @round.take_turn("Juneau")
+        @round.take_turn("North")
+        @filename = "high_score.txt"
+        @high_score = HighScore.new(@round, @filename)
     end
 
     it 'exists' do
-    
+        expect(@high_score).to be_instance_of HighScore
     end
 
     it 'has a valid filename' do
-
+        expect(@high_score.filename).to eq @filename
     end
 
-    it 'can read the file' do
-
+    it 'reads the data on the file' do
+        expect(@high_score.scores). to be_instance_of Array
     end
 
     it 'has score from completed round' do
-        
+        expect(@high_score.current_score).to eq @round.number_correct
     end
 
     it 'gets the users name' do
-
+        expect(@high_score.get_user_name("Bobby")).to eq @high_score.user_name
     end
 
-    it 'can write the score and name to the file' do
-
+    it 'can add user_name and score to scores array' do
+        @high_score.get_user_name("Bobby")
+        @high_score.add_score(@high_score.user_name, @high_score.current_score)
+        expect(@high_score.scores).to include("Bobby, 2")
     end
 
-    it 'can save the file to be used for other rounds' do
+    xit 'can write the score and name to the file' do
+    end
 
+    xit 'can save the file to be used for other rounds' do
     end
 end
