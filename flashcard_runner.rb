0ac52e285f34157d74e1d2d65d49704649385c14
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-09 10:06:27 -0400

    Add working playthrough appart from the correct category

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -3,10 +3,16 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
+# Create cards
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-deck = Deck.new([card_1, card_2, card_3])
+card_4 = Card.new("What is the three letter airport indicator for Tokyo, Japan (Narita)?", "NRT", :Geography)
+
+# Put cards in a deck
+deck = Deck.new([card_1, card_2, card_3, card_4])
+
+# Start a new round
 round = Round.new(deck)
 
 def start(round)
@@ -23,12 +29,16 @@ def start(round)
 
         guess = gets.chomp
         new_turn = round.take_turn(guess)
-        # require 'pry'; binding.pry
+        
         puts new_turn.feedback
     end 
 
     puts "****** Game Over! ******"
-    puts "You had #{} correct guesses out of #{round.deck.count} for a total score of #{}%."
+    puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct.round(2)}%."
+
+    round.deck.cards.each do |category|
+        puts "#{category} -- #{round.percent_correct_by_category(category)}% correct"
+    end
 end
 
 puts start(round)
