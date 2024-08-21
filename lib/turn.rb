0b20b6a6a6337a62f8b75429f9edc487a79f669f
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-21 15:55:14 -0600

    Initial commit

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+class Turn
+
+end
+
+def guess
+
+end
+
+def correct?
+
+end
+
+def feedback
+
+end
+
+turn_1 = Turn.new(guess, card)
+
+#class objects can become the parameter for the next class instance
