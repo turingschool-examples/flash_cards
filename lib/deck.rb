92b57a9e353fb74dca6c6eced4db0ea8cd16cd82
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-22 16:30:54 -0600

    delete unnecessary code

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,17 +1,14 @@
 
-require 'card.rb'
-
 class Deck
     attr_reader :cards
 
-    def initialize; end
+    def initialize(cards)
+        @cards = cards
+    end
 
-    def cards
-        ObjectSpace.each_object(Card) .to_a
+    def identify_category(category)
+        @cards.select do |card|
+        card.category == category
+        end
     end
 end
-
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-deck = Deck.new(cards)
