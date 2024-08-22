c25b18786e17a857681580a3173d453bcae3019e
Seth Verrill <verrill.seth@gmail.com>
2024-08-22 15:59:22 -0600

    Cleaned up code for readability

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,4 +5,15 @@ class Deck
     @cards = cards
   end
 
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(category)
+      @cards.select do |card|
+        card.category == category 
+      end
+       
+  end
+
 end
