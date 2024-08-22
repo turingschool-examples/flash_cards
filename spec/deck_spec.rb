ba570f78a97882d08b32bd72213a4024214bc2df
Devlin Lynch <devklynch@gmail.com>
2024-08-22 15:07:39 -0600

    feat: Add count to deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,7 +9,26 @@ RSpec.describe Deck do
       card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
       cards = [card_1, card_2, card_3]
       deck = Deck.new(cards)
-      require "pry" ; binding.pry
+      #require "pry" ; binding.pry
       expect(deck).to be_instance_of(Deck)
     end
+
+    it 'has a cards array' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)  
+        expect(deck.cards).to eq(cards)
+    end
+
+    it 'can return the count of cards in the deck' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)  
+        expect(deck.count).to eq(cards.count)
+    end
+
 end
