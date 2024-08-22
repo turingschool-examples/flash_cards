a30f68d120963d69d3abe03051abdd79a5e96862
Devlin Lynch <devklynch@gmail.com>
2024-08-22 16:15:23 -0600

    feat: Add Round method and initialize

---
+++ spec/round_spec.rb
@@ -0,0 +1,26 @@
+require './lib/turn'
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+
+RSpec.describe Round do
+    it 'exists' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+      deck = Deck.new([card_1, card_2, card_3])
+      round = Round.new(deck)
+      #require "pry" ; binding.pry
+      expect(round).to be_instance_of(Round)
+    end
+    
+    it 'has a deck' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        expect(round.deck).to eq(deck)
+    end
+end
