91bb65130b96ef60930f2c3d78b5acf92477a657
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-08-21 19:06:37 -0500

    second class created

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+class Turn
+
+    attr_reader :card
+                :guess
+
+     def initialize (card, guess)
+        @card = card
+        @guess = guess
+     end
+end
+
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,24 +2,12 @@ require './lib/card'
 require './lib/turn'
 
 card_1  = Card.new("What is Dr. Brown's specialty?", "Ultrasonography", "Personal")
-it 'has a question' do
-  card_1 = Card.new("What is Dr. Brown's specialty?", "Ultrasonography", :Personal)
 
-  expect(card.question).to eq("What is Dr. Brown's specialty?")
-end
-it 'has an answer' do
-  card_1 = Card.new("What is Dr. Brown's specialty?", "Ultrasonography", :Personal)
+turn_1 = Turn.new(card_1, "Ultrasonography")
 
-  expect(card.answer).to eq("Ultrasonography")
-end
-
-it 'has a category' do
-  card_1 = Card.new("What is Dr. Brown's specialty?", "Ultrasonography", :Personal)
-
-  expect(card.category).to eq(:Personal)
-end
 
 require 'pry'; binding.pry
+
 
 #RSpec.describe Card do
   #it 'exists' do
