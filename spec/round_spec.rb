27af4ed92b6cd068bdd069106115d4db2c465998
Devlin Lynch <devklynch@gmail.com>
2024-08-22 17:59:36 -0600

    feat: Add current_card method to Round

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -23,4 +23,23 @@ RSpec.describe Round do
 
         expect(round.deck).to eq(deck)
     end
+    it 'has a turn array' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        expect(round.turns).to eq([])
+    end
+
+    it 'has a turn array' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        expect(round.current_card).to eq(card_1)
+    end
 end
