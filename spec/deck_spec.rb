3d19645889197292edd373622434930e5e14995c
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-22 16:08:02 -0600

    add cards variable to make first 2 tests pass

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,8 +4,34 @@ require './lib/deck'
 RSpec.describe Deck do
     it 'exists' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        deck = Deck.new
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
 
-        expect(card_1).to be_instance_of(Card)
+        expect(deck).to be_instance_of(Deck)
     end
+
+    it 'returns cards' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        expect(deck.cards).to eq(cards)
+    end
+
+    it 'identifies_category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        category = card.category
+
+        expect(deck.category).to eq(cards[n].category)
+    end
+
+
 end
