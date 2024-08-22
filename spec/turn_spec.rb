b87764e94d0b0fdbb90daa3e35f70e1d14d495a4
Seth Verrill <verrill.seth@gmail.com>
2024-08-22 11:50:08 -0600

    Write test for each method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -9,6 +9,34 @@ RSpec.describe Turn do
     expect(turn).to be_instance_of(Turn)
   end
 
-  
-  
+  it 'has a guess' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.guess).to eq("Saturn")
+  end
+
+  it 'has a card' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.card).to eq(card)
+  end
+
+  it 'can determine if guess is correct' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.correct?).to eq(false)
+  end
+
+  it 'gives feedback' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn.feedback).to eq("Incorrect!")
+  end
+
 end
+  
+  
