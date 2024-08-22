04d6d2eedf8df1cfce10ad1fc3546c09ac6ea99d
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-08-21 20:56:17 -0500

    another step added and is working

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,4 +12,12 @@ class Turn
      def correct?
         @guess == @card.answer
         end
+
+    def feedback
+        if @guess == @card.answer
+            "Correct!"
+        else
+            "incorrect."
+        end
+    end
 end
