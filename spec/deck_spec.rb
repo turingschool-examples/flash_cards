abee39bc48f9e84367d1af89db0d803935e018ae
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-22 20:17:05 -0600

    modify code to make first 4 tests pass

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Deck do
         expect(deck).to be_instance_of(Deck)
     end
 
-    it 'returns cards' do
+    it 'returns_cards' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -22,16 +22,24 @@ RSpec.describe Deck do
         expect(deck.cards).to eq(cards)
     end
 
+    it 'counts _cards' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        expect(deck.count).to eq(3)
+    end
+# require 'pry';binding.pry
     it 'identifies_category' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         cards = [card_1, card_2, card_3]
         deck = Deck.new(cards)
-        category = card.category
+        card = @cards
 
-        expect(deck.category).to eq(cards[n].category)
+        expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
     end
-
-
 end
