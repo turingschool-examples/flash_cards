0a6712ca5956894204560ea14477ca57f18c6d74
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-23 16:45:48 -0600

    add passing currernt_card method

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,30 @@
 class Round
-    attr_reader :deck
+    attr_reader :deck, :cards 
 
     def initialize(deck)
         @deck = deck
+        @cards = cards
     end
 
     def turns
         @deck = []
     end
+
+    def current_card
+        @deck.cards[0]
+    end
+
+    # while @deck.cards.empty? == false do
+
+    # end
 end
+
+# def take_turn(guess)
+#     new_turn = Tnrn.new(guess,@deck.cards[@turns.count])
+#     @turns << new_turn
+#     new_turn
+# end
+
+# turns.length = 0 (turns.length should go up one every turn)
+
+
