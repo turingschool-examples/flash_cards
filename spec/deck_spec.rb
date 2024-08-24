82affc8c0d4c33411cc63ed8b112159df09f44eb
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-23 21:45:07 -0600

    change category test name

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe Deck do
         expect(deck.count).to eq(3)
     end
 
-    it 'identifies_category' do
+    it 'cards_in_category' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
