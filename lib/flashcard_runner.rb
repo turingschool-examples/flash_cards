7025b6d93585e2c65045f95d3d4711003e7362e9
Wally Wallace <jrvw001@gmail.com>
2024-08-24 14:36:03 -0600

    Add round setup to flashcard_runner.rb

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -4,4 +4,18 @@ require 'round.rb'
 require 'turn.rb'
 
 class Flashcard_runner
+  myCard1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  myCard2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  myCard3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  myCard4 = Card.new("What is objectively the best rodent?", "Capybara", :STEM)
+  
+  cards = [myCard1,myCard2,myCard3,myCard4]
+  
+  deck1 = Deck.new(cards)
+  
+  round1 = Round.new(deck1)
+
+  # Start the round using a new method called start
+  def start
+  end
 end
