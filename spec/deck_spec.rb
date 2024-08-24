0c6cb76f6b4bcf7a9023f4d052fde29920a925a1
Devlin Lynch <devklynch@gmail.com>
2024-08-24 15:31:12 -0600

    test: updated category test for Deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -31,13 +31,14 @@ RSpec.describe Deck do
         expect(deck.count).to eq(cards.count)
     end
 
-    # it 'can give card info by category' do
-    #     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    #     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    #     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    #     cards = [card_1, card_2, card_3]
-    #     deck = Deck.new(cards)  
-    #     expect(deck.cards_in_category(:STEM)).to eq(cards.category)
-    # end
+    it 'can give card info by category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards) 
+        #require "pry" ; binding.pry
+        expect(deck.cards_in_category(:STEM).count).to eq(2)
+    end
 
 end
