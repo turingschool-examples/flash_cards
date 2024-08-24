28fc424eabbd0262c7f893936078929b22ba4f12
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-24 10:32:53 -0600

    modify take turn test according to interaction pattern

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -52,12 +52,14 @@ RSpec.describe Round do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        cards = [card_1, card_2, card_3]
         deck = Deck.new([card_1, card_2, card_3])
         round = Round.new(deck)
-        turn = Turn.new("Juneau", @card)
-
-        expect(round.take_turn("Juneau")).to eq(@turn << turn)
+        turn = Turn.new("Juneau", card_1)
+        turns = []
+        user_guesses = []
+        number_correct = 0
+require 'pry'; binding.pry
+        expect(round.take_turn("Juneau")).to eq(Turn.new("Juneau", card_1))
     end
 
     xit 'says_if_correct' do
@@ -68,7 +70,6 @@ RSpec.describe Round do
         deck = Deck.new([card_1, card_2, card_3])
         round = Round.new(deck)
         turn = Turn.new("Juneau", @card)
-        new_turn = @turn 
         round.deck
 
         round.take_turn("Juneau") == @turn 
