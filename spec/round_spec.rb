c9143af8bf1a2c67901b1f1d93e31120b52c9af8
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-23 21:42:38 -0600

    add 3 new tests

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -48,6 +48,48 @@ RSpec.describe Round do
         expect(round.current_card).to eq(card_1)
     end
 
+    it 'takes_turns' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        turn = Turn.new("Juneau", @card)
+
+        expect(round.take_turn("Juneau")).to eq(@turn << turn)
+    end
+
+    xit 'says_if_correct' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        turn = Turn.new("Juneau", @card)
+        new_turn = @turn 
+        round.deck
+
+        round.take_turn("Juneau") == @turn 
+# require 'pry'; binding.pry
+        expect(new_turn.correct?).to eq(true)
+    end
+
+    xit 'identifies_number_correct' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+        turn = Turn.new("Juneau", @card)
+        current_card = @deck.cards[0]
+
+        expect(round.number_correct).to eq(1)
+
+    end
+
 
 
 # new_turn = round.take_turn
