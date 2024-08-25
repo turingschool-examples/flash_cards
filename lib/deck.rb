43f2bf974ba9898f1458a243bbef3deac27a5bc1
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-24 21:27:27 -0600

    add require relative to pull in card.rb for add card method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,9 +1,11 @@
+# require_relative './card.rb' (may need to add to make method to add cards work)
 
 class Deck
     attr_reader :cards
 
     def initialize(cards)
         @cards = cards
+        
     end
 # require 'pry'; binding.pry
     def count
@@ -15,4 +17,9 @@ class Deck
         category == card.category
         end
     end
+
+    # def add_card(new_card)
+    #     @cards.push(Card.new(question, answer, category)) 
+    # end
+
 end
