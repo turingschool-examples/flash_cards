3ede10bc907df77f536361acfde41e1ae144cf7a
Seth Verrill <verrill.seth@gmail.com>
2024-08-25 12:29:13 -0600

    Add test for correct guess

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -67,17 +67,20 @@ RSpec.describe Round do
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
-    round.take_turn("Juneau")
+    turn = round.take_turn("Juneau")
 
     expect(round.turns.count).to eq(1)
   end
-    
 
+  it 'gives correct on correct' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
 
-
-
-
-
-
+    expect(turn.feedback).to eq("Correct!")
+  end
 
 end
