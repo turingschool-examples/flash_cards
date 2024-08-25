2f9711b94025a7dbc02390b043066591456216de
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-25 12:19:42 -0600

    add number and percent correct by category methods

--- lib/round.rb
+++ lib/round.rb
@@ -2,14 +2,18 @@ require_relative './turn.rb'
 require_relative './card.rb'
 
 class Round
-    attr_reader :deck, :turns, :user_guesses, :number_correct, :card_number
+    attr_reader :deck, :turns, :user_guesses, :number_correct, :card_number, :number_correct_by_category, :percent_correct, :percent_correct_by_category
 
     def initialize(deck)
         @deck = deck
         @turns = []
         @card_number = 0
         @number_correct = 0
+        @number_correct_by_category = 0
         @user_guesses = []
+        @current_card = current_card
+        @percent_correct = 0
+        @percent_correct_by_category = 0
     end
 
     def turns
@@ -21,7 +25,15 @@ class Round
     end
 
     def current_card
-        @deck.cards[@card_number]
+        @current_card = @deck.cards[@card_number]
+    end
+
+    def number_correct_by_category(category)
+        @number_correct_by_category
+    end
+
+    def percent_correct_by_category(category)
+        @percent_correct_by_category
     end
 
     def take_turn(guess)
@@ -29,36 +41,10 @@ class Round
         @turns << new_turn 
         @user_guesses << new_turn.correct? #returns true/false boolean
         @number_correct += 1 if new_turn.correct?
+        @number_correct_by_category += 1 if new_turn.correct?
         @card_number += 1
+        @percent_correct = ((@number_correct.to_f / @turns.length) * 100).round(2)
+        @percent_correct_by_category = ((@number_correct_by_category.to_f / deck.cards.length) * 100).round(2)
         new_turn
     end
-
-    def number_correct_by_category(category)
-        @deck.current_card.category
-    end
 end
-    # def number_correct
-        
-    # end
-
-    # # def last
-
-    # end
-
-    # def feedback
-
-    # end
-
-
-
-# def take_turn(guess)
-#     new_turn = Turn.new(guess,@deck.cards[@turns.count])
-#     @turns << new_turn
-#     new_turn
-# end
-
-# turns.length = 0 (turns.length should go up one every turn)
-
-
-# turn should pull the card and return it to the end of the array
-
