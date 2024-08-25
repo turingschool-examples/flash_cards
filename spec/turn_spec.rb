547476de8d113a7bf5f61cc3b149dd63b83c9bd1
Devlin Lynch <devklynch@gmail.com>
2024-08-24 18:48:51 -0600

    test: cleaning up test formatting for card and turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,19 +5,13 @@ RSpec.describe Turn do
   it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau",card)
-
     expect(turn).to be_instance_of(Turn)
   end
 
-  it 'has a guess' do
+  it 'has a guess and card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.guess). to eq("Juneau")
-  end
-
-  it 'has a valid card' do
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
     expect(turn.card). to eq(card)
   end
 
@@ -37,9 +31,7 @@ RSpec.describe Turn do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.feedback). to eq("Correct!")
-  end
 
-  it 'provides feedback on the test' do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     expect(turn.feedback). to eq("Incorrect.")
