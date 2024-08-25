7e98dff49985533567629d0bfae6cebdc641a4e6
Seth Verrill <verrill.seth@gmail.com>
2024-08-25 12:47:13 -0600

    Add tests to track answers

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -93,4 +93,27 @@ RSpec.describe Round do
 
     expect(turn.feedback).to eq("Incorrect!")
   end
+
+  it 'tracks correct answers' do 
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    turn = round.take_turn("Juneau")
+
+    expect(round.number_correct).to eq (1)
+  end
+
+  it 'tracks incorrect answers' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    turn = round.take_turn("Helena")
+
+    expect(round.number_correct).to eq(0)
+  end
+
 end
