cdfc5e1477e82af90e9c5e28322d9543463fe1f7
Seth Verrill <verrill.seth@gmail.com>
2024-08-25 12:19:26 -0600

    Add class to verify turns array

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -60,6 +60,17 @@ RSpec.describe Round do
     expect(turn.card).to eq(card_1)
     expect(turn.guess).to eq("Juneau")
   end
+
+  it 'adds guess to turns array' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+
+    expect(round.turns.count).to eq(1)
+  end
     
 
 
