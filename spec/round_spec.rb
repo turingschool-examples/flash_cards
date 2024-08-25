001ac389bcf38d9063c21487f484a197081f26a9
Seth Verrill <verrill.seth@gmail.com>
2024-08-25 10:58:01 -0600

    Add test elements to spec file

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -15,6 +15,38 @@ RSpec.describe Round do
     expect(round).to be_instance_of(Round)
   end
 
+  it 'has a deck' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.deck).to eq(deck)
+  end
+
+  it 'has turns' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.turns).to eq([])
+  end
+
+  it 'has a current card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    cards = [card_1]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    expect(round.current_card).to eq(card_1)
+  end
+
+
 
 
 
