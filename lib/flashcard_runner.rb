4dda6f88a538b9ec46854ba6cba770b98731e83d
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-25 12:18:06 -0600

    delete unnecessary game class and add start method

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -3,25 +3,46 @@ require_relative './turn.rb'
 require_relative './round.rb'
 require_relative './deck.rb'
 
-class Game
-    def initialize_game
-        @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-        @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-        @deck = Deck.new(@cards)
-        @turn = Turn.new("Juneau", @card_1)
-        @round = Round.new(@deck)
-    end
 
-    def welcome
-        "Welcome! You're playing with #{@cards.length} cards."
-        puts @round.welcome
-    end
+@card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+@card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+@card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+@cards = [@card_1, @card_2, @card_3]
+@deck = Deck.new(@cards)
+@round = Round.new(@deck)
 
-    def play_game
-        
-    end
+
+def start 
+    puts "Welcome! You're playing with #{@cards.length} cards."
+    puts "----------------------------------------------------------"
+    puts "This is card number #{@round.card_number} out of #{@cards.length}."
+    puts "Question: #{@round.current_card.question}"
+    user_input = gets.chomp
+    puts user_input
 end
+
+# gets.chomp
+
+start
+# class Game
+#     def initialize_game
+#         @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+#         @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+#         @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+#         @deck = Deck.new(@cards)
+#         @turn = Turn.new("Juneau", @card_1)
+#         @round = Round.new(@deck)
+#     end
+
+#     def welcome
+#         
+#         puts @round.welcome
+#     end
+
+#     def play_game
+        
+#     end
+# end
 
 
 # @cards = [@card_1, @card_2, @card_3]
