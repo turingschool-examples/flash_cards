a6f87cf3bfcdf4c3f46598657dd5c078f5c3a46d
Seth Verrill <verrill.seth@gmail.com>
2024-08-25 10:57:27 -0600

    Add code to pass test to turn method

--- lib/round.rb
+++ lib/round.rb
@@ -1,7 +1,23 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck,
+              :turns,
+              :current_card
 
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @current_card = deck.cards
   end
+
+  def current_card
+    @deck.cards[@turns.count]
+  end
+
+  def take_turn(guess)
+    turn = Turn.new(@current_card, guess)
+    @turns << new_turn
+    turn
+    end
+
+
 end
