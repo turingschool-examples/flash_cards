2717580e1a99d262856c98641e25e7712bbd10ec
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-08-25 18:06:41 -0500

    cards and turns added for fun

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,7 +11,15 @@ card2 = Card.new("What does Dr. Reeve love?", "Poms", "Personal")
 
 card3 = Card.new("What vein is best to place and IV catheter?", "Cephalic", "Medical")
 
-cards = [card1, card2, card3]
+card4 = Card.new("What species is Solensia for?", "Felines", "Medical")
+
+card5 = Card.new("How is Leptospirosis most commonly transmitted?", "Urine", "Medical")
+
+card6 Card.new("What is a common anti-inflammatory for cats?", "Onsior", "Medical")
+
+card7 Card.new("What behavior do Natasha's favorite appointments have?", "Aggressive", "Personal")
+
+cards = [card1, card2, card3, card4, card5, card6, card7]
 
 deck = Deck.new(cards)
 
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -3,24 +3,25 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-
 card1  = Card.new("What is Dr. Brown's specialty?", "Ultrasonography", "Personal")
-
-
 card2 = Card.new("What does Dr. Reeve love?", "Poms", "Personal")
-
-
 card3 = Card.new("What vein is best to place and IV catheter?", "Cephalic", "Medical")
+card4 = Card.new("What species is Solensia for?", "Felines", "Medical")
+card5 = Card.new("How is Leptospirosis most commonly transmitted?", "Urine", "Medical")
+card6 Card.new("What is a common anti-inflammatory for cats?", "Onsior", "Medical")
+card7 Card.new("What behavior do Natasha's favorite appointments have?", "Aggressive", "Personal")
+
+cards = [card1, card2, card3, card4, card5, card6, card7]
 
 turn1 = Turn.new(card1, "Chemo")
-
 turn2 = Turn.new(card2, "Poms")
-
 turn3 = Turn.new(card3, "Saphenous")
+turn4 = Turn.new(card4, "Felines")
+turn5 = Turn.new(card5, "Blood")
+turn6 = Turn.new(card6, "Onsior")
+turn7 = Turn.new(card7, "Friendly")
 
-turns = [turn1, turn2, turn3]
-
-cards = [card1, card2, card3]
+turns = [turn1, turn2, turn3, turn4, turn5, turn6, turn7]
 
 deck = Deck.new(cards)
 
