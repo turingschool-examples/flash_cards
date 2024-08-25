c896643a7a8f47445013bd02606bd317c78cfa2f
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-24 21:28:51 -0600

    add number correct by category test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Round do
         @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         @cards = [@card_1, @card_2, @card_3]
+        turn = Turn.new("Juneau", @card_1)
         @deck = Deck.new(@cards)
         @turn = Turn.new("Juneau", @card_1)
         @round = Round.new(@deck)
@@ -58,5 +59,11 @@ RSpec.describe Round do
         # require 'pry'; binding.pry
 
         expect(@round.number_correct).to eq(1)
+    end
+
+    xit 'identifies_number_correct_by_category' do
+        new_turn = Turn.new("Venus", @card_2)
+
+        expect(@round.number_correct_by_category(:Geography).to eq(1))
     end
 end
