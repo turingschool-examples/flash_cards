f704ccfcbb93fbec916e7e94da026bba69c4b453
Seth Verrill <verrill.seth@gmail.com>
2024-08-25 12:07:19 -0600

    Rework test to be more robust

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -39,13 +39,28 @@ RSpec.describe Round do
 
   it 'has a current card' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    cards = [card_1]
-    deck = Deck.new(cards)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
 
     expect(round.current_card).to eq(card_1)
   end
 
+  it 'has a new turn with guess and card' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    turn = round.take_turn("Juneau")
+
+    expect(turn).to be_instance_of(Turn)
+    expect(turn.card).to eq(card_1)
+    expect(turn.guess).to eq("Juneau")
+  end
+    
 
 
 
