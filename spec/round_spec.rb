6324cfa06827b70cf8bc46d1d8832a687decc191
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-08-25 15:38:38 -0700

    Updating Comments

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Round do
     #Danielle was kind enough to share this introduction to the test below.
     #Rather than having to repeat each instance, this allows each test to instantiate the card(s) and deck
     before(:each) do
-      @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography) #info given for card 1 while creating a new flashcard
       @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
       @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-      @cards  = [@card_1, @card_2, @card_3]
-      @deck   = Deck.new(@cards)
-      @round  = Round.new(@deck)
+      @cards  = [@card_1, @card_2, @card_3] #creating a cards object that contains all info from cards 1/2/3
+      @deck   = Deck.new(@cards) #instantiating a new deck
+      @round  = Round.new(@deck) #instantiating a new round - as to not repeat any information gained from previous
     end
       
     it 'has a Round' do
@@ -41,7 +41,7 @@ RSpec.describe Round do
     end
 
     it 'counts the number correct'do
-        round.take_turn("Juneau")
+        @round.take_turn("Juneau")
         expect(@round.correct?).to eq true
         expect(@round.number_correct).to eq(1)
 end
@@ -66,7 +66,18 @@ end
         expect(@round.number_correct_by_category(:STEM)).to eq(0)
     end
     
+    it 'Percent Complete' do
+        expect(@round.percent_correct).to eq(50.0)
+    end
+
+    it 'Count Percent Correct by Category' do
+        expect(@round.percent_correct_by_category(:Geography)).to eq(100.0)
+    end
     
+    it 'Shows the final card'
+        @round.take_turn("Jeans")
+        @round.take_turn("Mars")
+        expect(@round.current_card).to eq(@card_3)
 end
 
 
