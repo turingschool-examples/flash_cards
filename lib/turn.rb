500feb0e7193c601a6679ce3532a4165b5866499
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-24 18:18:19 -0600

    add == method to resolve testing issue

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,15 +7,19 @@ class Turn
     end
 
     def correct?
-        return true if guess.downcase == card.answer.downcase
+        return true if @guess.downcase == @card.answer.downcase
         false
     end
 
     def feedback
-        if guess.downcase == card.answer.downcase
+        if @guess.downcase == @card.answer.downcase
         "Correct!"
         else
         "Incorrect."
         end
+    end
+
+    def ==(other_turn)
+        self.guess == other_turn.guess && self.card == other_turn.card
     end
 end
