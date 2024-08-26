dc3825685f18f3f3029f1aae14e8451f1ea88aac
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-26 17:19:42 -0600

    fix bugs in correct? and feedback methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,6 @@
 class Turn
     attr_reader :card, :guess
+    attr_writer :card, :guess
 
     def initialize(guess, card)
         @guess = guess
@@ -7,19 +8,15 @@ class Turn
     end
 
     def correct?
-        return true if @guess.downcase == @card.answer.downcase
+        return true if card.answer.downcase == guess.downcase 
         false
     end
 
     def feedback
-        if @guess.downcase == @card.answer.downcase
+        if card.answer.downcase == guess.downcase
         "Correct!"
         else
         "Incorrect."
         end
-    end
-
-    def ==(other_turn)
-        self.guess == other_turn.guess && self.card == other_turn.card
     end
 end
