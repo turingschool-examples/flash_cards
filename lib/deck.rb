a139e1ab42a19619cb513f53fa1418ea336f2e82
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-26 17:38:23 -0600

    fix bug in add new card feature

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,11 +1,11 @@
 # require_relative './card.rb' (may need to add to make method to add cards work)
 
 class Deck
-    attr_reader :cards
+    attr_reader :card, :cards
+    attr_writer :cards
 
     def initialize(cards)
         @cards = cards
-        
     end
 # require 'pry'; binding.pry
     def count
@@ -18,8 +18,8 @@ class Deck
         end
     end
 
-    # def add_card(new_card)
-    #     @cards.push(Card.new(question, answer, category)) 
-    # end
+    def add_card(new_card)
+        @cards << new_card
+    end
 
 end
