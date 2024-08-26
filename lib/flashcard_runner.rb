0532586574f7ca7ac695d934857acc942e3f8898
Danielle Cardona <danielle.cardona23@gmail.com>
2024-08-26 17:37:44 -0600

    add new turn method and game play enumerable

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -7,50 +7,30 @@ require_relative './deck.rb'
 @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+# @card = Card.new(@question, @answer, @category)
 @cards = [@card_1, @card_2, @card_3]
 @deck = Deck.new(@cards)
+@turn = Turn.new(@guess, @card)
 @round = Round.new(@deck)
 
-
-def start 
-    puts "Welcome! You're playing with #{@cards.length} cards."
-    puts "----------------------------------------------------------"
-    puts "This is card number #{@round.card_number} out of #{@cards.length}."
-    puts "Question: #{@round.current_card.question}"
-    user_input = gets.chomp
-    puts user_input
+# require 'pry'; binding.pry
+def start
+        @round.deck
+        @round.turns
+        puts "Welcome! You're playing with #{@cards.length} cards."
+        puts "----------------------------------------------------------"
+    @round.deck.cards.length.times do
+        puts "This is card number #{@round.turn_number} out of #{@cards.length}."
+        puts "Question: #{@round.current_card.question}"
+        user_input = gets.chomp
+        new_turn = @round.take_turn(user_input) 
+        # require 'pry'; binding.pry
+        puts new_turn.feedback
+    end
+    puts "****** Game over! ******"
+    require 'pry'; binding.pry
+    puts "You had #{@round.number_correct} correct guesses out of #{@cards.length} for a total score of #{@round.percent_correct}."
+    puts
 end
 
-# gets.chomp
-
 start
-# class Game
-#     def initialize_game
-#         @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-#         @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#         @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#         @deck = Deck.new(@cards)
-#         @turn = Turn.new("Juneau", @card_1)
-#         @round = Round.new(@deck)
-#     end
-
-#     def welcome
-#         
-#         puts @round.welcome
-#     end
-
-#     def play_game
-        
-#     end
-# end
-
-
-# @cards = [@card_1, @card_2, @card_3]
-# @turns = []
-#         @user_guesses = []
-#         @number_correct = 0
-#         @card_number = 0
-
-# puts "---------------------------------------------------------------"
-# puts "This is card number #{@card.number} out of #{@cards.length} cards."
-
