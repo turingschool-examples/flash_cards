f31460ba28fd054a5c01510661e755fe8c093daa
Brody Nikolaos <brodynichols777@gmail.com>
2024-08-27 18:46:32 -0500

    Feat: Added text prompt for runner.rb

--- lib/runner.rb
+++ lib/runner.rb
@@ -3,8 +3,16 @@ require './lib/deck.rb'
 require './lib/round.rb'
 require './lib/turn.rb'
 
+puts "Welcome to Brody's flashcard game!"
+puts "--------------------------------------"
+puts "press P to start or Q to quit!"
+
+get
+
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 deck = Deck.new([card_1, card_2, card_3])
 round = Round.new(deck)
+
+game.play
