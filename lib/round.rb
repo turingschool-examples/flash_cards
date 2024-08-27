b5355bb4b3a864749775087fce9fb70fa2ab823b
Brody Nikolaos <brodynichols777@gmail.com>
2024-08-27 15:46:11 -0500

    Feat: Added turn class

---
+++ lib/round.rb
@@ -0,0 +1,11 @@
+class Round
+    attr_reader :guess, :turn
+
+    def initialize(guess, turn)
+        @guess = guess
+        @turn = turn
+    end
+
+
+    
+end
--- spec/Round_spec.rb
+++ spec/Round_spec.rb
@@ -5,8 +5,23 @@ require './lib/round'
 
 Respec.describe Round do
     it 'describes Round class' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
         round = Round.new(deck)
 
         expect(round.deck). to eq(Round)
         expect(round.turns).to eq([])
+        expect(round.current_card).to eq(card_1)
     end
+
+    it 'starts a new round' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+
+        expect
+
+end
