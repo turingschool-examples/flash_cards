ec9be6128dbb097e5121cf9cd54f3a661f651924
Wally Wallace <jrvw001@gmail.com>
2024-08-26 18:08:36 -0600

    test multi card decks and resolve defect with that scenario

--- lib/card_reader.rb
+++ lib/card_reader.rb
@@ -7,8 +7,8 @@ class Card_reader
     @cards = []
   end
 
-  def create_deck()
-    cards_as_string = @deck_file.split('\n')
+  def create_deck
+    cards_as_string = @deck_file.split("\n")
     cards_as_string.each do |card_as_string|
       card_as_array = card_as_string.split(',')
       card = Card.new(card_as_array[0],card_as_array[1],card_as_array[2].intern)
@@ -19,11 +19,3 @@ class Card_reader
   end
 
 end
-# myCard1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# myCard2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-# myCard3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-# myCard4 = Card.new("What is objectively the best rodent?", "Capybara", :STEM)
-
-# cards = [myCard1,myCard2,myCard3,myCard4]
-
-# deck1 = Deck.new(cards)
--- spec/card_reader_spec.rb
+++ spec/card_reader_spec.rb
@@ -29,5 +29,20 @@ it 'creates a deck with one card' do
 end
 
 #read multiple card deck
-#build a deck to be able to pass to round
+it 'creates a deck with four cards' do
+  deck_file = './lib/resources/deck2.txt'
+  card_reader1 = Card_reader.new(deck_file)
+  deck1 = card_reader1.create_deck
+
+  expect(deck1).to be_instance_of(Deck)
+  expect(deck1.count).to eq(4)
+  expect(deck1.cards[0]).to be_instance_of(Card)
+  expect(deck1.cards[0].question).to eq("What is the capital of Alaska?")
+  expect(deck1.cards[0].answer).to eq("Juneau")
+  expect(deck1.cards[0].category).to eq(:Geography)
+  expect(deck1.cards[3]).to be_instance_of(Card)
+  expect(deck1.cards[3].question).to eq("What is objectively the best rodent?")
+  expect(deck1.cards[3].answer).to eq("Capybara")
+  expect(deck1.cards[3].category).to eq(:STEM)
+  end
 end
