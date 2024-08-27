c6a07f7ca644516f26ea376c6792c4c5f0aff6ee
Wally Wallace <jrvw001@gmail.com>
2024-08-26 18:29:17 -0600

    implement card_reader into flashcard_runner

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -7,30 +7,32 @@ require './card_reader'
 class Flashcard_runner
 
   def self.start()
-    myCard1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    myCard2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    myCard3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-    myCard4 = Card.new("What is objectively the best rodent?", "Capybara", :STEM)
+    # myCard1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    # myCard2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    # myCard3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    # myCard4 = Card.new("What is objectively the best rodent?", "Capybara", :STEM)
     
-    cards = [myCard1,myCard2,myCard3,myCard4]
-    
-    deck1 = Deck.new(cards)
-    
+    # cards = [myCard1,myCard2,myCard3,myCard4]
+    deck_file = './resources/deck2.txt'
+    card_reader = Card_reader.new(deck_file)
+    # deck1 = Deck.new(cards)
+    deck1 = card_reader.create_deck
+
     round1 = Round.new(deck1)
-    puts "\n\n\n\n\nWelcome! You're playing with #{cards.length} cards.
+    puts "\n\n\n\n\nWelcome! You're playing with #{deck1.count} cards.
 -------------------------------------------------"
 
     round1.deck.cards.each do |card|
-      puts "This is card number #{round1.turns.length + 1} out of #{cards.length}\nQuestion: #{round1.current_card.question}"
+      puts "This is card number #{round1.turns.length + 1} out of #{deck1.count}\nQuestion: #{round1.current_card.question}"
       round1.take_turn(gets.chomp)
     end
 
     puts "****** Game over! ******
-You had #{round1.number_correct} correct guesses out of #{cards.length} for a total score of #{round1.percent_correct}%."
+You had #{round1.number_correct} correct guesses out of #{deck1.count} for a total score of #{round1.percent_correct}%."
 
 categories = []
 
-  cards.each do |card|
+  deck1.cards.each do |card|
     if !categories.include?(card.category)
       categories << card.category
     end
