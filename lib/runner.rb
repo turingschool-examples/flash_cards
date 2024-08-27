5835472242e855a5dc7217ff246cc003362d783e
Brody Nikolaos <brodynichols777@gmail.com>
2024-08-27 18:37:11 -0500

    Feat: Added objects to runner.rb

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,10 @@
-require './lib/card'
+require './lib/card.rb'
+require './lib/deck.rb'
+require './lib/round.rb'
+require './lib/turn.rb'
 
-card = Card.new
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+deck = Deck.new([card_1, card_2, card_3])
+round = Round.new(deck)
