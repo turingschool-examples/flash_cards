9f20c2c9c9a6337347fc437a9a4f7f54c611391b
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 16:28:06 -0600

    Create class skeleton

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+class Turn
+
+    def initialize(string, card)
+
+    end
+
+    def guess
+
+    end
+
+    def card
+
+    end
+
+    def correct?
+
+    end
+
+    def feedback
+        
+    end
+end
