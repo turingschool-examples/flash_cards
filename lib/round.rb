ad079e59289a716fd72528efb584768ecd411920
Jaren Garman <jarengarman@protonmail.com>
2024-12-03 18:59:06 -0500

    Added current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -2,9 +2,15 @@
 
 # Where the main game is played. Starts with a deck, tracks statistics.
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns
 
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @deck_index = 0
+  end
+
+  def current_card
+    @deck.cards[@deck_index]
   end
 end
