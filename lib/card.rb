fba2c01f1f9fa6bb8006f36e731e14b6a2b28eec
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 16:23:09 -0600

    Add answer attribute

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,8 @@
 class Card
-    attr_reader :question
+    attr_reader :question, :answer
 
     def initialize(question, answer, category)
         @question = question
+        @answer = answer
     end
 end
