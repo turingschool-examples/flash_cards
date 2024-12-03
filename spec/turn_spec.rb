bf42bcec69848c0d33d67a1bf40b8006eb7b0fc8
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 16:46:47 -0600

    Add guess and correct? tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -16,4 +16,18 @@ describe Turn do
 
         expect(turn.card).to eq(card)
     end
+
+    it 'has a guess' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.guess).to eq("Juneau")
+    end
+
+    it 'can identify a correct answer' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+
+        expect(turn.correct?).to eq(true)
+    end
 end
