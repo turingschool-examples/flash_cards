5765b6ee272d72017108aeb7d51af622bb3898d7
Jaren Garman <jarengarman@protonmail.com>
2024-12-03 17:57:38 -0500

    Added comments to Turn for clarity

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Takes a card and guess as input. Checks guess, then provides feedback.
+# Takes a card and guess as input. Checks guess against the answer on the card, then provides feedback.
 class Turn
   attr_reader :guess, :card
 
@@ -10,10 +10,12 @@ class Turn
   end
 
   def correct?
+    # Is the guess equal to the answer on the card? Returns true or false
     @guess == card.answer
   end
 
   def feedback
+    # Instead of reinventing the wheel, this if statement calls the correct? method we defined earlier
     if correct?
       'Correct!'
     else
