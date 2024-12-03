c49daa381f85e2d603fcdf3fa39474c77e366c31
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 17:02:28 -0600

    Update exists test for new Deck args

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,7 +4,12 @@ require './lib/deck'
 
 describe Deck do
     it 'exists' do
-        deck = Deck.new
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
 
         expect(deck).to be_a(Deck)
     end
