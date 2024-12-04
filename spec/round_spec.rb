2b7e90cb636ac5dcf32f67c80b9f491da71d97bb
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 19:29:02 -0600

    Add stores all previous taken turns test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -114,4 +114,18 @@ describe Round do
         expect(round.current_card).to eq(card_2)
     end
 
+    it 'stores all previous taken turns' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        new_turn = round.take_turn("Juneau")
+        new_turn_2 = round.take_turn("Venus")
+
+        expect(round.turns.count).to eq(2)
+    end
+
 end
