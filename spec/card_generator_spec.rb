b088a10fdf7eaf96bbb3da1483ff4258cf5b3a0a
Jaren Garman <jarengarman@protonmail.com>
2024-12-04 09:57:03 -0500

    Reorganized and expanded card_generator_spec

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,18 +1,54 @@
 # frozen_string_literal: true
 
 require_relative '../lib/card_generator'
+require_relative '../lib/deck'
 require 'rspec'
 
 RSpec.describe CardGenerator do
-  it 'exists' do
-    cards = CardGenerator.new('cards.txt')
+  describe 'Initialization' do
+    it 'is an instance of CardGenerator' do
+      cards = CardGenerator.new('cards.txt')
 
-    expect(cards).to be_instance_of(CardGenerator)
+      expect(cards).to be_instance_of(CardGenerator)
+    end
   end
 
-  it 'holds cards' do
-    cards = CardGenerator.new('cards.txt')
+  describe 'Hold and count cards' do
+    it 'can hold cards' do
+      cards = CardGenerator.new('cards.txt').cards
+      deck = Deck.new(cards)
 
-    expect(cards.cards[0]).to be_instance_of(Card)
+      expect(deck).to be_instance_of(Deck)
+    end
+
+    it 'can count cards' do
+      cards = CardGenerator.new('cards.txt').cards
+      deck = Deck.new(cards)
+
+      expect(deck.count).to eq(4)
+    end
+  end
+
+  describe 'Find cards by category' do
+    it 'can find cards by category-1' do
+      cards = CardGenerator.new('cards.txt').cards
+      deck = Deck.new(cards)
+
+      expect(deck.cards_in_category('STEM')).to eq([cards[0]])
+    end
+
+    it 'can find cards by category-2' do
+      cards = CardGenerator.new('cards.txt').cards
+      deck = Deck.new(cards)
+
+      expect(deck.cards_in_category('Geography')).to eq([])
+    end
+
+    it 'can find cards by category-3' do
+      cards = CardGenerator.new('cards.txt').cards
+      deck = Deck.new(cards)
+
+      expect(deck.cards_in_category('Turing Staff')).to eq([cards[1], cards[2]])
+    end
   end
 end
