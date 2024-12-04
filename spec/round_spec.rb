817a382e481c50c2100aa503ac76b616c432a407
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 19:42:25 -0600

    Add percent correct test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -145,4 +145,18 @@ describe Round do
         expect(round.number_correct_by_category(:STEM)).to eq(0)
     end
 
+    it 'knows the percent of correctly answered questions' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        new_turn = round.take_turn("Juneau")
+        new_turn_2 = round.take_turn("Venus")
+
+        expect(round.percent_correct).to eq(50)
+    end
+
 end
