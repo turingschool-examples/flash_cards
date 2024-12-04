bd8b6c9a63dccda599fb7b85a89280399c598b8d
Will Fox <william.macdonald.fox@gmail.com>
2024-12-03 19:53:32 -0500

    test: updated tests, all tests passing, iteration 1 complete

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,22 +4,32 @@ require './lib/card'
 
 RSpec.describe Turn do
   before(:each) do
-    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    @turn = Turn.new("Juneau", @card)
-  end 
+      @card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      @card2 = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+      @turn1 = Turn.new("Juneau", @card1)
+      @turn2 = Turn.new("Saturn", @card2)
+    end 
 
     it 'exists' do 
-      expect(@turn).to be_a(Turn)
+      expect(@turn1).to be_a(Turn)
     end
 
-    it 'holds a card object' do # redundant, unnecessary?
-      expect(@turn.card).to eq(@card) 
+    it 'holds a card object' do 
+      expect(@turn1.card).to eq(@card1) 
     end
 
     describe 'instance methods' do 
+        it 'can check if the guess is correct' do 
+            expect(@turn1.correct?).to eq(true)
+        end
 
-        it 'can check if a turns guess is correct' do 
-            expect(@turn.correct?).to eq(true)
+        it 'can check if the guess is incorrect' do
+            expect(@turn2.correct?).to eq(false)
+        end
+
+        it 'can give feedback on both correct and incorrect guesses' do
+            expect(@turn1.feedback).to eq("Correct!")
+            expect(@turn2.feedback).to eq("Incorrect.")
         end
     end
 end
