6b952b9a271b2d14388efe059b47d3bdc107d338
Jaren Garman <jarengarman@protonmail.com>
2024-12-04 09:25:00 -0500

    Reorganized deck_spec for readability

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,63 +5,69 @@ require_relative '../lib/deck'
 require 'rspec'
 
 RSpec.describe Deck do
-  it 'exists' do
-    card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-    card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+  describe 'Initialization' do
+    it 'is an instance of Deck' do
+      card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+      card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+      card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
 
-    expect(deck).to be_instance_of(Deck)
+      expect(deck).to be_instance_of(Deck)
+    end
   end
 
-  it 'can hold cards' do
-    card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-    card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+  describe 'Hold and count cards' do
+    it 'can hold cards' do
+      card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+      card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+      card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
 
-    expect(deck.cards).to eq(cards)
+      expect(deck.cards).to eq(cards)
+    end
+
+    it 'can count cards' do
+      card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+      card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+      card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(deck.count).to eq(3)
+    end
   end
 
-  it 'can be counted' do
-    card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-    card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+  describe 'Find cards by category' do
+    it 'can find cards by category-1' do
+      card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+      card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+      card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
 
-    expect(deck.count).to eq(3)
-  end
+      expect(deck.cards_in_category(:STEM)).to eq([card2, card3])
+    end
 
-  it 'can find cards by category' do
-    card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-    card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+    it 'can find cards by category-2' do
+      card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+      card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+      card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
 
-    expect(deck.cards_in_category(:STEM)).to eq([card2, card3])
-  end
+      expect(deck.cards_in_category(:Geography)).to eq([card1])
+    end
 
-  it 'can find cards by category 2' do
-    card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-    card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+    it 'can find cards by category-3' do
+      card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+      card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
+      card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
 
-    expect(deck.cards_in_category(:Geography)).to eq([card1])
-  end
-
-  it 'can find cards by category 3' do
-    card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-    card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expect(deck.cards_in_category('Pop Culture')).to eq([])
+      expect(deck.cards_in_category('Pop Culture')).to eq([])
+    end
   end
 end
