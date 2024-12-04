e79ae61343261a528636f3f890e5e87e1cc8232a
ontruster74 <ontruster74@gmail.com>
2024-12-03 21:04:02 -0700

    Add incorrect guess tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -13,7 +13,7 @@ describe Turn do
   it 'exists and is initalized' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
-    expect(turn).to be_a(Turn)
+    expect(turn).to be_instance_of(Turn)
   end
 
   it 'has a card attr_reader' do
@@ -38,6 +38,45 @@ describe Turn do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.feedback).to eq("Correct!")
+  end
+end
+
+describe Card do
+  it 'exists and is initialized' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    expect(card).to be_instance_of(Card)
+  end
+end
+
+describe Turn do
+  it 'exists and is initalized' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn).to be_instance_of(Turn)
+  end
+
+  it 'has a card attr_reader' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.card).to eq(card)
+  end
+
+  it 'has a guess attr_reader' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.guess).to eq("Saturn")
+  end
+
+  it 'has a working correct? method' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.correct?).to eq(false)
+  end
+
+  it 'has a working feedback method' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+    expect(turn.feedback).to eq("Incorrect.")
   end
 end
 
