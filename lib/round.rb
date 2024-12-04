a3b1a99fcecfe80079d1b27d26b08ef9b5b9860e
Will Fox <william.macdonald.fox@gmail.com>
2024-12-04 10:09:12 -0500

    feat: add take_turn method to round class

--- lib/round.rb
+++ lib/round.rb
@@ -1,14 +1,21 @@
 require 'pry'
 
 class Round
-    attr_reader :deck, :turn
+    attr_reader :deck, :turns
 
     def initialize(deck)
         @deck = deck 
-        @turn = []
+        @turns = []
     end
 
     def current_card
         @deck.cards.first
     end 
+
+    def take_turn(guess)
+        new_turn = Turn.new(guess, current_card)
+        @turns << new_turn
+        @deck.cards.shift 
+        new_turn 
+    end
 end
