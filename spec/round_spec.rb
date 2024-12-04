014e2c5b72ddba4d03759a293f4be8c276395930
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 18:56:23 -0600

    Change exists tag to require deck arg

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -6,7 +6,12 @@ require './lib/round'
 
 describe Round do
     it 'exists' do
-        round = Round.new
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        
+        round = Round.new(deck)
 
         expect(round).to be_a(Round)
     end
