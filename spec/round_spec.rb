6ee0b687bb782d88ee44e37e86ecb3dcb728cb55
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 19:35:00 -0600

    Add correct by category test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -130,4 +130,19 @@ describe Round do
         expect(round.number_correct).to eq(1)
     end
 
+    it 'knows how many turns were correct in a givin category' do 
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        new_turn = round.take_turn("Juneau")
+        new_turn_2 = round.take_turn("Venus")
+
+        expect(number_correct_by_category(:Geography)).to eq(1)
+        expect(number_correct_by_category(:Geography)).to eq(0)
+    end
+
 end
