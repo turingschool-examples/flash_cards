548ebc1dae738344e6b67132f7c271923b243718
Beverly Green <greenbev000@gmail.com>
2024-12-03 18:28:21 -0600

    added card class

---
+++ lib/card.rb
@@ -0,0 +1,8 @@
+class Card
+    attr_reader :question, :answer, :category
+    def initialize(card)
+        @question = question
+        @answer = answer
+        @category = category
+    end
+end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,4 +1,5 @@
-require './lib/card'
+require 'rspec'
+require './lib/card.rb'
 
 RSpec.describe Card do
   it 'exists' do
