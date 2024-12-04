2190cb07813b944cef9d125b8bbab449c2de45b8
Hannah Gray <hannah7gray@gmail.com>
2024-12-04 14:43:41 -0700

    Added an extra Turn class somehow

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    @guess.downcase == @card.answer.downcase
+  end
+
+  def feedback
+    correct? ? "Correct!" : "Incorrect."
+  end
+end
