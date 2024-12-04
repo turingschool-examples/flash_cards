9ea9fdc628f7736c173d2b4af88e260ab8478455
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 19:05:12 -0600

    Add can create turns test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -49,4 +49,17 @@ describe Round do
         expect(round.current_card).to eq(card_1)
     end
 
+    it 'can create turns' do 
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        new_turn = round.take_turn("Juneau")
+
+        expect(new_turn).to be_a(Turn)
+    end
+
 end
