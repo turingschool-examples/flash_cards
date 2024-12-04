839fadc8df13140993ee3778a926f327088aac9f
Will Fox <william.macdonald.fox@gmail.com>
2024-12-04 13:59:35 -0500

    feat: add correct_count method

--- lib/round.rb
+++ lib/round.rb
@@ -18,4 +18,19 @@ class Round
         @deck.cards.shift 
         new_turn 
     end
+
+    def number_correct
+        correct_count = 0
+
+        @turns.each do |turn|
+          if turn.correct?
+            correct_count += 1
+          end 
+        end
+        correct_count
+    end
+
+    # def number_correct_by_category
+
+    # end
 end
