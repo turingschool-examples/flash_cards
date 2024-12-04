20da7fa3800cce11ad6c6d4b6aaacc5af13ef9ef
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 19:02:04 -0600

    Add current_card test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -38,4 +38,15 @@ describe Round do
         expect(round.turns).to eq([])
     end
 
+    it 'can get the current card' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        expect(round.current_card).to eq(card_1)
+    end
+
 end
