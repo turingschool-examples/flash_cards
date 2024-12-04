3eaa08c787881e6a1fe9e063699286e578212a46
Will Fox <william.macdonald.fox@gmail.com>
2024-12-03 22:11:49 -0500

    feat: add deck class with methods

---
+++ lib/deck.rb
@@ -0,0 +1,17 @@
+class Deck 
+    attr_reader :cards
+
+    def initialize(cards) 
+        @cards = cards
+    end
+
+    def cards_in_category(category)
+        @cards.select do |card|
+        card.category == category 
+        end
+    end
+
+    def count
+        @cards.count
+    end
+end
