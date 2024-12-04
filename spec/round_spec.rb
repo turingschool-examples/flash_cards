009dca753c3cce8b8cd3073d0b9cf4e84030cfa5
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 18:58:05 -0600

    Add has a deck test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -10,9 +10,21 @@ describe Round do
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         deck = Deck.new([card_1, card_2, card_3])
-        
+
         round = Round.new(deck)
 
         expect(round).to be_a(Round)
     end
+
+    it 'has a deck' do 
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        expect(round.deck).to eq(deck)
+    end
+
 end
