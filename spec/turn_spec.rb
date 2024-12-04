3ea5f44b95a6342b71cea775fb0176f6c7ecd9a3
Chase Phillips <chase.thomas.phillips@gmail.com>
2024-12-04 15:18:01 -0500

    updated turn spec

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -18,5 +18,14 @@ RSpec.describe Turn do
   it 'has a card' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Boston" , card)
+    expect(turn.card).to eq(card)
   end
+
+  it 'is checking answer' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Boston" , card)
+    expect(turn.correct?).to eq(true)
+  end
+
+  
 end
