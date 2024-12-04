ded87f7749ece7cf4e321cc629c970957cade782
Will Fox <william.macdonald.fox@gmail.com>
2024-12-04 17:34:18 -0500

    feat: add percentage methods, complete iteration 2, may go back to refactor

--- lib/round.rb
+++ lib/round.rb
@@ -19,6 +19,14 @@ class Round
         new_turn 
     end
 
+    def turn_count #refactored these names for clarity and consistency, I believe this is an example of Law of Demeter 
+        @turns.count #previously was def count, @rounds.turns.count (in spec file)
+    end
+
+    def last_turn #previously was def last, @round.turns.last (in spec file)
+        @turns.last
+    end
+
     def number_correct
         correct_count = 0
 
@@ -30,11 +38,28 @@ class Round
         correct_count
     end
 
-    def turn_count #refactored these names for clarity and consistency, I believe this is an example of Law of Demeter 
-        @turns.count #previously was def count, @rounds.turns.count (in spec file)
+    def correct_by_category(category)
+        correct_by_cat_count = 0
+
+        @turns.each do |turn|
+            if turn.correct? && turn.card.category == category
+                correct_by_cat_count += 1
+            end
+        end
+        correct_by_cat_count
     end
 
-    def last_turn #previously was def last, @round.turns.last (in spec file)
-        @turns.last
+    def percent_correct
+       (number_correct / turn_count.to_f) * 100
+    end
+
+    def percent_correct_by_category(category)
+        count_by_cat = 0
+        @turns.each do |turn|
+            if turn.card.category == category 
+                count_by_cat += 1
+            end
+        end
+        (correct_by_category(category) / count_by_cat.to_f) * 100
     end
 end
