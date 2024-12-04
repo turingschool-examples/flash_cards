d1b16021f1dd5c66f8ca99fa6843c52ff3fa2edc
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-03 22:51:01 -0600

    Create Card class and basic attributes

---
+++ lib/card.rb
@@ -0,0 +1,11 @@
+#card.rb - defines the Card class
+
+class Card
+    # Permit member variables to be accessed by methods
+    attr_reader :category, :question, :answer
+
+    #Initialize method
+
+    #Other methods
+
+end
