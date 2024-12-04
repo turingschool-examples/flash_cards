9e8b7bb372c5d403880d29ad30461a8290b5d4b6
Sebastian McKee <sebmcke@gmail.com>
2024-12-04 13:02:57 -0600

    Change runner file to utilize updated deck.count method

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -9,22 +9,20 @@ def start
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
     deck = Deck.new([card_1,card_2,card_3])
-    deck_size = deck.count
-
     round = Round.new(deck)
 
-    puts "Welcome! You're playing with #{deck_size} cards."
+    puts "Welcome! You're playing with #{deck.count} cards."
     puts "-------------------------------------------------"
 
     (1..deck.count).each do |round_num|
-        puts "This is card #{round_num} out of #{deck_size}"
+        puts "This is card #{round_num} out of #{deck.count}"
         puts "Question: #{round.current_card.question}"
         turn_result = round.take_turn(gets.chomp)
         puts turn_result.feedback
     end
 
     puts "****** Game over! ******"
-    puts "You had #{round.number_correct} out of #{deck_size} for a total score of #{round.percent_correct}"
+    puts "You had #{round.number_correct} out of #{deck.count} for a total score of #{round.percent_correct}"
     round.deck.categories.each do |category|
         puts "#{category} - #{round.percent_correct_by_category(category)}% correct"
     end
