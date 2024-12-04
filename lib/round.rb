f17683076b4c7956fe0e715dd9cd46667c9d950e
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 19:14:11 -0600

    Change take_turn method to store turns

--- lib/round.rb
+++ lib/round.rb
@@ -12,6 +12,8 @@ class Round
     end
 
     def take_turn(guess)
-        Turn.new(guess, current_card)
+        turn = Turn.new(guess, current_card)
+        turns << turn
+        turn
     end
 end
