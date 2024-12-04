2257abd02d2b59b191e1167be5b71661f99aef7b
Sebastian McKee <sebmcke@gmail.com>
2024-12-03 19:26:28 -0600

    Add can cycle through deck test

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -101,4 +101,17 @@ describe Round do
         expect(round.number_correct).to eq(1)
     end
 
+    it 'can cycle through a deck' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+
+        round = Round.new(deck)
+
+        new_turn = round.take_turn("Juneau")
+
+        expect(round.current_card).to eq(card_2)
+    end
+
 end
