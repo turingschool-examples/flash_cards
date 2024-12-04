c237a335c5a35eeba56e84e233c1e1c860df9b5f
Sebastian McKee <sebmcke@gmail.com>
2024-12-04 14:11:14 -0600

    Change runner file to use CardGenerator class

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -2,13 +2,11 @@ require './lib/round'
 require './lib/card'
 require './lib/turn'
 require './lib/deck'
+require './lib/card_generator'
 
 def start
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
-    deck = Deck.new([card_1,card_2,card_3])
+    cards = CardGenerator.new("cards.txt").cards
+    deck = Deck.new(cards)
     round = Round.new(deck)
 
     puts "Welcome! You're playing with #{deck.count} cards."
