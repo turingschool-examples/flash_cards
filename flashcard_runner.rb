5a7e86c44c2b13093be645f4139c6425fcdfa300
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-05 17:23:54 -0600

    Update flashcard_runner.rb to fully replicate iteration 3 requirements

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -10,9 +10,9 @@ require 'pry'
 
 #Create 4 cards and build a deck
 card_1 = Card.new("What is 5 + 5?", "10", :STEM)        #NOTE: need .to_s for this kind of guess...add this later!
-card_2 = Card.new("What is Rachel's favorite animal?", "Meerkat", :Trivia)  #Why is "\'" not needed?
-card_3 = Card.new("What is Mike's middle name?", "Nobody knows", :Trivia)
-card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Trivia)
+card_2 = Card.new("What is Rachel's favorite animal?", "Meerkat", :Turing_staff)  #Why is "\'" not needed?
+card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_staff)
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Pop_culture)
 
 deck = Deck.new([card_1, card_2, card_3, card_4])       #Alternate: deck = Deck.new([]), then add cards with my other method
 
