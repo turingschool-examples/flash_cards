fef7a67ad1a8b9e97a4be3b76eea35635e779590
Beverly Green <greenbev000@gmail.com>
2024-12-05 16:56:22 -0600

    Fixed cards error and working on cards_in_category method

---
+++ lib/deck.rb
@@ -0,0 +1,14 @@
+require './lib/card'
+
+class Deck
+    attr_reader :cards, :count, :cards_in_category
+    def initialize(card_1, card_2, card_3)
+        @cards = [card_1, card_2, card_3]
+    end
+   def count
+    @cards.count
+   end
+   def cards_in_category
+    
+   end
+end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,24 @@
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+    it 'exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new(card_1, card_2, card_3)
+
+    expect(card_1).to be_instance_of(Card)
+    expect(card_2).to be_instance_of(Card)
+    expect(card_3).to be_instance_of(Card)
+    expect(deck).to be_instance_of(Deck)
+    end
+    it 'counts number of cards' do
+    deck = Deck.new(card_1, card_2, card_3)
+    
+    expect(deck.count).to eq(3)
+    end
+    it 'can list cards in category' do
+        
+    end
+end
