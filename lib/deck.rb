589d0aba397497c5204c551ce3cfcd75eb614954
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-04 21:47:48 -0600

    Add cards_in_category() method

--- lib/deck.rb
+++ lib/deck.rb
@@ -13,5 +13,18 @@ class Deck
         cards.length()
         #Alternate: cards.count(), though this one can do other things too...
     end
+
+    def cards_in_category(category_to_match)
+        #Return array (sub-array) of card objects which have category equal to category_to_match
+        card_sublist = []
     
+        @cards.each do |card|
+            if card.category == category_to_match
+                card_sublist << card
+            end
+        end
+    
+        return card_sublist
+    end
+
 end
