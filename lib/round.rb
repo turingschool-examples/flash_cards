90bb3e3d2e3f035a7ed24f4a8362bff3b7982089
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-04 22:32:32 -0600

    Fix functionality issues  in Round methods

--- lib/round.rb
+++ lib/round.rb
@@ -2,6 +2,7 @@
 
 class Round
     attr_reader :deck, :turns, :current_card
+    #Note: current_card_index does not need to be externally accessed
 
     def initialize(deck)
         @deck = deck
@@ -9,7 +10,9 @@ class Round
         #Track the current card in the deck via an integer index.  May be unneeded later.
         @current_card_index = 0
 
-        @current_card = deck[current_card_index]
+        # @current_card = deck[@current_card_index]
+        @current_card = deck.cards[@current_card_index]
+
         @turns = []
     end
 
@@ -23,9 +26,9 @@ class Round
 
         #Advance to the next card in the deck before returning
         @current_card_index += 1
-        @current_card = deck[current_card_index]
+        @current_card = deck.cards[@current_card_index]
 
-        return @current_card
+        return new_turn
     end
 
 
