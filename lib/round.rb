d76074ea582c65ae92dac3c6ce6574febb3a3fc3
Mark Kendall <mkendall.tutor@gmail.com>
2024-12-05 17:20:35 -0600

    Add count_turns() method and correct percent_correct*() methods in Round class

--- lib/round.rb
+++ lib/round.rb
@@ -24,6 +24,9 @@ class Round
         new_turn = Turn.new(guess, @current_card)
         @turns << new_turn
 
+        #Provide feedback to user
+        new_turn.feedback()
+
         #Advance to the next card in the deck before returning
         @current_card_index += 1
         @current_card = deck.cards[@current_card_index]
@@ -68,12 +71,12 @@ class Round
         #Note: I typically express percents as a decimal (math), but my guess is this project wants out of 100
         #Another note: it appears it wants one decimal of precision for rounding, hence the round(1) below
         #Yet another note: I had to force an int -> float conversion even though I thought the * 100.0 would imply it...
-        (number_correct() / turns.length.to_f * 100.0).round(1)
+        (number_correct() / count_turns().to_f * 100.0).round(1)
     end
 
     def percent_correct_by_category(category)
         #Similar observation to number_correct_by_category() method before...
-        (number_correct_by_category(category) / turns.length.to_f * 100.0).round(1)
+        (number_correct_by_category(category) / count_turns(category).to_f * 100.0).round(1)
     end
 
     def start()
@@ -84,6 +87,34 @@ class Round
 
         #QUESTION: where to build the taking turns logic?  Here?  Or the runner file?
 
+    end
+
+    def finish()
+        #Complete the round and print out results
+        puts "****** Game Over! ******"
+        puts "You had #{number_correct()} correct guesses out of #{deck.count()} for a total score of #{percent_correct()}%."
+        
+        #Hard part: figure out what categories even exist.  Hacky workaround for now, manually run the known categoeies based on runner file (ugh).
+        puts "STEM - #{percent_correct_by_category(:STEM)}% correct"
+        puts "Turing Staff - #{percent_correct_by_category(:Turing_staff)}% correct"
+        puts "Pop Culture - #{percent_correct_by_category(:Pop_culture)}% correct"
+    end
+
+    def count_turns(category_to_count = nil)
+        #Count the total number of turns for a given category (or just overall total if not specified)
+        #Note: could count total in deck, but this is more flexible as it could be called mid-game for real-time status!
+        #Question: is it good practice to group these together, or make two methods?  This one is more flexible, but 'beefier'
+        if category_to_count == nil
+            return turns.length
+        else
+            total = 0
+            turns.each do |turn|
+                if turn.card.category == category_to_count
+                    total += 1
+                end
+            end
+            return total
+        end
     end
 
 end
