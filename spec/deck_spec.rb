914ff17e377f91055c84df73e1a7f3211c6dc745
ontruster74 <ontruster74@gmail.com>
2024-12-04 18:15:38 -0700

    remove unnecessary tests

---
+++ spec/deck_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require '../lib/card'
+require '../lib/deck'
+
+describe Deck do
+  it 'exists and is initialized properly' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    expect(deck).to be_a(Deck)
+  end
+
+  
+end
