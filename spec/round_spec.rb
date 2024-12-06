806ba4d153a617b17af9381907578f28ef5cd523
ontruster74 <ontruster74@gmail.com>
2024-12-05 19:23:48 -0700

    fix test syntax

---
+++ spec/round_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require './lib/card'
+require './lib/turn'
+require './lib/round'
+require './lib/deck'
+
+describe Round do
+  it 'exists and is initialized properly' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round).to be_a(Round)
+  end
+
+  it 'has a deck attr_reader' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.deck).to eq(deck)
+  end
+
+  it 'has a turns attr_reader' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+    expect(round.turns).to eq([])
+  end
+end
