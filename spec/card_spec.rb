985b2c19163fe967472c469ff25b9b9c901cbf59
Jaren Garman <jarengarman@protonmail.com>
2024-12-06 18:37:37 -0500

    Refactored using memoized class methods

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,30 +4,24 @@ require_relative '../lib/card'
 require 'rspec'
 
 describe Card do
+  let(:card) { Card.new('What is the capital of Alaska?', 'Juneau', :Geography) }
+
   describe 'Initialization' do
     it 'is an instance of Card' do
-      card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-
       expect(card).to be_instance_of(Card)
     end
   end
 
   describe 'Store and retrieve data' do
     it 'has a question' do
-      card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-
       expect(card.question).to eq('What is the capital of Alaska?')
     end
 
     it 'has an answer' do
-      card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-
       expect(card.answer).to eq('Juneau')
     end
 
     it 'has a category' do
-      card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-
       expect(card.category).to eq(:Geography)
     end
   end
