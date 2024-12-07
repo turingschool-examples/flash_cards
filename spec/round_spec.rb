6dcd36a41d2c2427a5130d10cadba4293ddc4e25
Jaren Garman <jarengarman@protonmail.com>
2024-12-07 18:12:07 -0500

    Refactor to comply with rspec style guide

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -6,126 +6,143 @@ require_relative '../lib/card'
 require 'rspec'
 
 describe Round do
-  let(:first_card) { Card.new('What is the capital of Alaska?', 'Juneau', :Geography) }
-  let(:second_card) { Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM) }
-  let(:third_card) { Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM) }
-  let(:cards) { [first_card, second_card, third_card] }
+  let(:cards) do
+    [
+      Card.new('What is the capital of Alaska?', 'Juneau', :Geography),
+      Card.new('Who is the CEO of SpaceX?', 'Elon Musk', :STEM),
+      Card.new('Describe in words the exact direction of 697.5° clockwise from due north?', 'North north west', :STEM)
+    ]
+  end
   let(:deck) { Deck.new(cards) }
-  let(:round) { Round.new(deck) }
+  let(:round) { described_class.new(deck) }
 
-  describe 'Initialize' do
-    it 'is an instance of Round' do
-      expect(round).to be_instance_of(Round)
-    end
+  describe '#initialize' do
+    subject(:round) { described_class.new(deck) }
 
-    it 'has a deck' do
+    it { is_expected.to be_instance_of(described_class) }
+  end
+
+  describe '#deck' do
+    subject(:deck) { Deck.new(cards) }
+
+    it { is_expected.not_to be_nil }
+
+    it 'returns the deck' do
       expect(round.deck).to eq(deck)
     end
   end
 
-  describe 'Turns' do
-    it 'has turns' do
-      expect(round.turns).to eq([])
-    end
+  describe '#turns' do
+    subject(:turns) { round.turns }
 
-    describe 'Store and track turns' do
-      it 'can store turns' do
-        new_turn = round.take_turn('Juneau')
+    it { is_expected.not_to be_nil }
 
-        expect(round.turns).to eq([new_turn])
-      end
-
-      it 'can track amount of turns' do
-        round.take_turn('Juneau')
-        round.take_turn('Venus')
-
-        expect(round.turns.count).to eq(2)
+    context 'when no turns have been taken' do
+      it 'returns empty array' do
+        expect(turns).to eq([])
       end
     end
 
-    describe 'Move to next turn' do
-      it 'starts on the first card' do
-        expect(round.current_card).to eq(first_card)
-      end
-
-      it 'can take a turn' do
-        new_turn = round.take_turn('Juneau')
-
-        expect(new_turn).to be_instance_of(Turn)
-      end
-
-      it 'can move to the next card' do
-        round.take_turn('Juneau')
-
-        expect(round.current_card).to eq(second_card)
-      end
-
-      it 'can move to last card' do
+    context 'when turns have been taken' do
+      it 'returns array of turns' do
         round.take_turn('Juneau')
         round.take_turn('Venus')
 
-        expect(round.current_card).to eq(third_card)
+        expect(turns.all?(Turn)).to be true
+      end
+    end
+
+    describe '.count' do
+      subject(:count) { round.turns.count }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns amount of turns' do
+        round.take_turn('Juneau')
+        round.take_turn('Venus')
+
+        expect(count).to eq(2)
       end
     end
   end
 
-  describe 'Track statistics' do
-    it 'can be correct' do
-      new_turn = round.take_turn('Juneau')
+  describe '#current_card' do
+    subject(:current_card) { round.current_card }
 
-      expect(new_turn.correct?).to be true
+    it { is_expected.not_to be_nil }
+
+    context 'when no turns have been taken' do
+      it 'returns first card' do
+        expect(current_card).to eq(deck.cards[0])
+      end
     end
 
-    it 'can give feedback' do
+    context 'when one turn has been taken' do
+      it 'returns second card' do
+        round.take_turn('Juneau')
+        expect(current_card).to eq(deck.cards[1])
+      end
+    end
+
+    context 'when two turns have been taken' do
+      it 'returns third card' do
+        round.take_turn('Juneau')
+        round.take_turn('Venus')
+
+        expect(current_card).to eq(deck.cards[2])
+      end
+    end
+  end
+
+  describe '#number_correct' do
+    subject(:number_correct) { round.number_correct }
+
+    it { is_expected.not_to be_nil }
+
+    it 'returns amount of correct answers' do
       round.take_turn('Juneau')
       round.take_turn('Venus')
 
-      expect(round.turns.last.feedback).to eq('Incorrect.')
+      expect(number_correct).to eq(1)
     end
+  end
 
-    describe 'Number correct' do
-      it 'can track amount of correct answers' do
-        round.take_turn('Juneau')
+  describe '#number_correct_by_category' do
+    subject(:number_correct_by_category) { round.number_correct_by_category(:Geography) }
 
-        expect(round.number_correct).to eq(1)
-      end
+    it { is_expected.not_to be_nil }
 
-      it 'does not include incorrect answers' do
-        round.take_turn('Juneau')
-        round.take_turn('Venus')
+    it 'returns number correct by category' do
+      round.take_turn('Juneau')
+      round.take_turn('Venus')
 
-        expect(round.number_correct).to eq(1)
-      end
-
-      it 'can track number correct by category-1' do
-        round.take_turn('Juneau')
-        round.take_turn('Venus')
-
-        expect(round.number_correct_by_category(:Geography)).to eq(1)
-      end
-
-      it 'can track number correct by category-2' do
-        round.take_turn('Juneau')
-        round.take_turn('Venus')
-
-        expect(round.number_correct_by_category(:STEM)).to eq(0)
-      end
+      expect(number_correct_by_category).to eq(1)
     end
+  end
 
-    describe 'Percent correct' do
-      it 'can track percent correct' do
-        round.take_turn('Juneau')
-        round.take_turn('Venus')
+  describe '#percent_correct' do
+    subject(:percent_correct) { round.percent_correct }
 
-        expect(round.percent_correct).to eq(50.0)
-      end
+    it { is_expected.not_to be_nil }
 
-      it 'can track percent correct by category' do
-        round.take_turn('Juneau')
-        round.take_turn('Venus')
+    it 'returns percent correct' do
+      round.take_turn('Juneau')
+      round.take_turn('Venus')
 
-        expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
-      end
+      expect(percent_correct).to eq(50.0)
+    end
+  end
+
+  describe '#percent_correct_by_category' do
+    subject(:percent_correct_by_category) { round.percent_correct_by_category(:Geography) }
+
+    it { is_expected.not_to be_nil }
+
+    it 'returns percent correct by category' do
+      round.take_turn('Juneau')
+      round.take_turn('Venus')
+
+      expect(percent_correct_by_category).to eq(100.0)
     end
   end
 end
