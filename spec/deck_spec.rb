f7f8dd73d5961efec90c6cb1a974b744e7a93644
Jaren Garman <jarengarman@protonmail.com>
2024-12-06 22:47:29 -0500

    Cleaned up formatting

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -48,10 +48,9 @@ describe Deck do
 
     context 'when category is not present' do
       it 'returns empty array' do
-        first_card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-        second_card = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :Fantasy)
-        third_card = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :Fantasy)
-        cards = [first_card, second_card, third_card]
+        cards = [Card.new('What is the capital of Alaska?', 'Juneau', :Geography),
+                 Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :Fantasy),
+                 Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :Fantasy)]
         deck = described_class.new(cards)
 
         expect(deck.cards_in_category(:STEM)).to eq([])
@@ -68,10 +67,9 @@ describe Deck do
 
     context 'when category is a string' do
       it 'returns array of category strings' do
-        first_card = Card.new('What is the capital of Alaska?', 'Juneau', 'Geography')
-        second_card = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', 'STEM')
-        third_card = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', 'STEM')
-        cards = [first_card, second_card, third_card]
+        cards = [Card.new('What is the capital of Alaska?', 'Juneau', 'Geography'),
+                 Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', 'STEM'),
+                 Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', 'STEM')]
         deck = described_class.new(cards)
 
         expect(deck.categories).to eq(%w[Geography STEM])
