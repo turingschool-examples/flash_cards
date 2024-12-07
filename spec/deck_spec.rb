f9cfc82a9831adf8ffc62fa02ecf6f9e2357c5f5
Jaren Garman <jarengarman@protonmail.com>
2024-12-07 18:09:43 -0500

    Formatting to avoid line length being too long

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,8 +6,10 @@ require 'rspec'
 
 describe Deck do
   let(:first_card) { Card.new('What is the capital of Alaska?', 'Juneau', :Geography) }
-  let(:second_card) { Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM) }
-  let(:third_card) { Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM) }
+  let(:second_card) { Card.new('Who is the CEO of SpaceX?', 'Elon Musk', :STEM) }
+  let(:third_card) do
+    Card.new('Describe in words the exact direction of 697.5° clockwise from due north?', 'North north west', :STEM)
+  end
   let(:cards) { [first_card, second_card, third_card] }
   let(:deck) { described_class.new(cards) }
 
@@ -40,6 +42,8 @@ describe Deck do
   describe '#cards-in-category' do
     subject(:category) { deck.cards_in_category(:STEM) }
 
+    it { is_expected.not_to be_nil }
+
     context 'when category is present' do
       it 'returns array of cards in category' do
         expect(deck.cards_in_category(:STEM)).to eq([second_card, third_card])
@@ -49,8 +53,8 @@ describe Deck do
     context 'when category is not present' do
       it 'returns empty array' do
         cards = [Card.new('What is the capital of Alaska?', 'Juneau', :Geography),
-                 Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :Fantasy),
-                 Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :Fantasy)]
+                 Card.new('Who is the CEO of SpaceX?', 'Elon Musk', :Fantasy),
+                 Card.new('Some question?', 'North north west', :Fantasy)]
         deck = described_class.new(cards)
 
         expect(deck.cards_in_category(:STEM)).to eq([])
@@ -59,17 +63,21 @@ describe Deck do
   end
 
   describe '#categories' do
+    subject(:categories) { deck.categories }
+
+    it { is_expected.not_to be_nil }
+
     context 'when category is a symbol' do
       it 'returns array of category symbols' do
-        expect(deck.categories).to eq(%i[Geography STEM])
+        expect(categories).to eq(%i[Geography STEM])
       end
     end
 
     context 'when category is a string' do
       it 'returns array of category strings' do
         cards = [Card.new('What is the capital of Alaska?', 'Juneau', 'Geography'),
-                 Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', 'STEM'),
-                 Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', 'STEM')]
+                 Card.new('Who is the CEO of SpaceX?', 'Elon Musk', 'STEM'),
+                 Card.new('Some question?', 'North north west', 'STEM')]
         deck = described_class.new(cards)
 
         expect(deck.categories).to eq(%w[Geography STEM])
