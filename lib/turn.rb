d8182dea412d3635b0a14cd48b06f43721ab673b
Donte Handy <donte.handy@gmail.com>
2024-12-07 15:13:32 -0400

    complete iteration 1 without explantions

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,28 +6,33 @@ class Turn #creates a Turn class
     @card = card #set the card to the card parameter
   end
 
-    def card #creates a method called card
-        @card #returns the card
-    end
+  def card #creates a method called card
+    @card #returns the card
+  end
 
-    def guess #creates a method called guess
-        @string #returns the string entered by the user
-    end
+  def guess #creates a method called guess
+    @string #returns the string entered by the user
+  end
 
-    def correct? #creates a method called correct?
-        @string.downcase == @card.answer.downcase #returns true if the string entered by the user is equal to the answer of the card
-        #? makes the method return a boolean value
-        #.downcase makes the string entered by the user and the answer of the card lowercase
-        #== compares the string entered by the user and the answer of the card
-        #.answer accesses the answer attribute of the card *update initialize to include answer?*
-        #.downcase makes the answer of the card lowercase
-    end
+  def correct? #creates a method called correct?
+    @string.downcase == @card.answer.downcase #returns true if the string entered by the user is equal to the answer of the card
+    #? makes the method return a boolean value
+    #.downcase makes the string entered by the user and the answer of the card lowercase
+    #== compares the string entered by the user and the answer of the card
+    #.answer accesses the answer attribute of the card *update initialize to include answer?*
+    #.downcase makes the answer of the card lowercase
+  end
 
-    def feedback #creates a method called feedback
-        correct? ? "Correct!" : "Incorrect." #returns "Correct!" if the string entered by the user is equal to the answer of the card
-        #? makes the method return a boolean value
-        #: ? ? makes the method return a boolean value and represents an if/else statement
-        # : is used to separate the if/else statement
-    end
+  def feedback #creates a method called feedback
+    correct? ? "Correct!" : "Incorrect." #returns "Correct!" if the string entered by the user is equal to the answer of the card
+    #? makes the method return a boolean value
+    #: ? ? makes the method return a boolean value and represents an if/else statement
+    # : is used to separate the if/else statement
+  end
 end
+
+#[:STEM] placed in the code to help identify the different parts of the code
+# for example after creating and instance and adding a string and card
+# the code would look like this:    @turn = Turn.new("Juneau", @card) #[:STEM]
+# the purpose of this is to help identify the different parts of the code
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,48 +1,37 @@
-require 'rspec' #links the rspec gem to the turn_spec.rb file
-require 'pry' # This is required to use Pry
-require_relative '../lib/turn' #links the turn.rb file to the turn_spec.rb file
-require_relative '../lib/card' #links the card.rb file to the turn_spec.rb file
+require_relative '../lib/card'
+require_relative '../lib/turn'
+require 'rspec'
+require 'pry'
 
-RSpec.describe Turn do #rspec syntax to describe the Turn class
+RSpec.describe Turn do
   before(:each) do
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    #creates a new instance of the Card class with the following parameters
     @turn = Turn.new("Juneau", @card)
-    #creates a new instance of the Turn class with the following parameters
   end
 
   it 'exists' do
     expect(@turn).to be_instance_of(Turn)
   end
 
-  it 'has a guess' do
-    binding.pry # Add this line to inspect the state
-    expect(@turn.guess).to eq("Juneau")
-  end
-
   it 'has a card' do
     expect(@turn.card).to eq(@card)
   end
 
-  it 'can check if the guess is correct' do
+  it 'has a guess' do
+    expect(@turn.guess).to eq("Juneau")
+  end
+
+  it 'is correct' do
     expect(@turn.correct?).to be true
   end
 
-  it 'gives feedback' do
+  it 'gives correct feedback' do
     expect(@turn.feedback).to eq("Correct!")
   end
 
-  context 'when the guess is incorrect' do
-    before(:each) do
-      @turn = Turn.new("Anchorage", @card)
-    end
-
-    it 'can check if the guess is incorrect' do
-      expect(@turn.correct?).to be false
-    end
-
-    it 'gives feedback' do
-      expect(@turn.feedback).to eq("Incorrect.")
-    end
+  it 'handles incorrect guesses' do
+    incorrect_turn = Turn.new("Anchorage", @card)
+    expect(incorrect_turn.correct?).to be false
+    expect(incorrect_turn.feedback).to eq("Incorrect.")
   end
 end
