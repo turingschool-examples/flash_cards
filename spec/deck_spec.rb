6c81d1c1cb37efe034f1faad1aa855b0727c38e4
Jaren Garman <jarengarman@protonmail.com>
2024-12-06 21:50:18 -0500

    Refactor to comply with rspec style guide

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,51 +9,73 @@ describe Deck do
   let(:second_card) { Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM) }
   let(:third_card) { Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM) }
   let(:cards) { [first_card, second_card, third_card] }
-  let(:deck) { Deck.new(cards) }
+  let(:deck) { described_class.new(cards) }
 
-  describe 'Initialization' do
-    it 'is an instance of Deck' do
-      expect(deck).to be_instance_of(Deck)
+  describe '#initialize' do
+    subject(:deck) { described_class.new(cards) }
+
+    it { is_expected.to be_instance_of(described_class) }
+  end
+
+  describe '#cards' do
+    subject(:deck_cards) { deck.cards }
+
+    it { is_expected.not_to be_nil }
+
+    it 'returns array of cards' do
+      expect(deck_cards).to eq([first_card, second_card, third_card])
     end
   end
 
-  describe 'Hold and count cards' do
-    it 'can hold cards' do
-      expect(deck.cards).to eq(cards)
-    end
+  describe '#count' do
+    subject(:count) { deck.count }
 
-    it 'can count cards' do
+    it { is_expected.not_to be_nil }
+
+    it 'returns card count' do
       expect(deck.count).to eq(3)
     end
   end
 
-  describe 'Find cards by category' do
-    it 'can find cards by category-1' do
-      expect(deck.cards_in_category(:STEM)).to eq([second_card, third_card])
+  describe '#cards-in-category' do
+    subject(:category) { deck.cards_in_category(:STEM) }
+
+    context 'when category is present' do
+      it 'returns array of cards in category' do
+        expect(deck.cards_in_category(:STEM)).to eq([second_card, third_card])
+      end
     end
 
-    it 'can find cards by category-2' do
-      expect(deck.cards_in_category(:Geography)).to eq([first_card])
-    end
+    context 'when category is not present' do
+      it 'returns empty array' do
+        first_card = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
+        second_card = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :Fantasy)
+        third_card = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :Fantasy)
+        cards = [first_card, second_card, third_card]
+        deck = described_class.new(cards)
 
-    it 'can find cards by category-3' do
-      expect(deck.cards_in_category('Pop Culture')).to eq([])
+        expect(deck.cards_in_category(:STEM)).to eq([])
+      end
     end
   end
 
-  describe 'List categories' do
-    it 'can return a list of categories-symbols' do
-      expect(deck.categories).to eq(%i[Geography STEM])
+  describe '#categories' do
+    context 'when category is a symbol' do
+      it 'returns array of category symbols' do
+        expect(deck.categories).to eq(%i[Geography STEM])
+      end
     end
 
-    it 'can return a list of categories-strings' do
-      first_card = Card.new('What is the capital of Alaska?', 'Juneau', 'Geography')
-      second_card = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', 'STEM')
-      third_card = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', 'STEM')
-      cards = [first_card, second_card, third_card]
-      deck = Deck.new(cards)
+    context 'when category is a string' do
+      it 'returns array of category strings' do
+        first_card = Card.new('What is the capital of Alaska?', 'Juneau', 'Geography')
+        second_card = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', 'STEM')
+        third_card = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', 'STEM')
+        cards = [first_card, second_card, third_card]
+        deck = described_class.new(cards)
 
-      expect(deck.categories).to eq(%w[Geography STEM])
+        expect(deck.categories).to eq(%w[Geography STEM])
+      end
     end
   end
 end
