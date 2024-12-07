bdae1233f83bbcd98a86a1184ec6d8a2da0231b0
Donte Handy <donte.handy@gmail.com>
2024-12-07 10:57:53 -0400

    complete iteration one

---
+++ lib/card.rb
@@ -0,0 +1,12 @@
+require 'pry' 
+
+class Card
+  attr_reader :question, :answer, :category
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+end
+
