bb0b7554c2f38d75b4110ac082e9c4e2c3c3c5d4
Jaren Garman <jarengarman@protonmail.com>
2024-12-08 16:52:50 -0500

    refactor: avoid unnecessary if/else statements

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,32 +1,24 @@
 # frozen_string_literal: true
 
 require_relative 'card'
-# Reads cards from a file
+# Create cards from a file
 class CardGenerator
   attr_reader :cards
 
-  def initialize(filename) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
+  def initialize(filename) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     @cards = []
     File.open(filename, 'r').each_line do |line|
-      next if line.empty? || line.start_with?('#')
+      next if line.empty? || line.start_with?('#') || line.split(',').size < 2
 
       card_array = line.split(',')
-      @cards << if card_array.size == 2
-                  Card.new(card_array[0], card_array[1].chomp)
-                elsif card_array.size == 3
-                  Card.new(card_array[0], card_array[1], card_array[2].chomp)
-                elsif card_array.size >= 4
-                  answers = []
-                  index = 2
-                  (card_array.size - 2).times do
-                    answers << card_array[index]
-                    index += 1
-                  end
-                  answers.last.chomp!
-                  Card.new(card_array[0], card_array[1], answers)
-                else
-                  next
-                end
+      answers = []
+      index = 2
+      (card_array.size - 2).times do
+        answers << card_array[index]
+        index += 1
+      end
+      answers&.last&.chomp!
+      @cards << Card.new(card_array[0], card_array[1], answers)
     end
   end
 end
