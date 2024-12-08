c1ceec2778e23b74334b0753f15bb0c523e90c97
Beverly Green <greenbev000@gmail.com>
2024-12-08 12:10:40 -0600

    Updated current_card method and take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -2,42 +2,44 @@ class Round
     attr_reader :deck, :turns, :current_card, :take_turn
     def initialize(deck)
         @deck = deck
-        @turns = turns
+        @turns = []
+        @turns_taken = 0
         @current_card = current_card
         @take_turn = take_turn
     end
     def current_card
-        turns = []
-        cards.first do |card|
-            turns << card.take_turn
-        end
-        cards.find(2) do |card|
-            turns << card.take_turn
-        end
-        cards.last do |card|
-            turns << card.take_turn
+        @deck.cards[@turns_taken]
+        # if turns == []
+        #     first_card = deck.map{|deck|deck.drop(card_2, card_3)}
+        #     first_card.take_turn
+        # elsif #need second condition here
+        #     second_card = deck.map{|deck|deck.drop(card_1, card_3)}
+        #     second_card.take_turn
+        # third_card = deck.map{|deck|deck.drop(card_1, card_2)}
+        #     third_card.take_turn
         end
     end
     def take_turn(guess)
+        new_turn = Turn.new(guess, current_card)
+        @turns << new_turn
+        if new_turn.correct?
+            @number_correct += 1
+            @turns_taken += 1
+            new_turn
+        end
+    end
         #records appropriate guess and card to 'turns' method
         #round should move onto next card in the deck
-    end
-    # def turns
-    #     turns_taken = []
-    #     turns_taken << first_card
-    #     turns_taken << second_card
-    #     turns_taken << third_card
-
-    #     #this is a list of all the turns taken this round. They are added into an array after the turn is taken
     # end
+    def turns
+        #turns = deck.map do {|deck| deck.take_turn}
+    end
     def correct?
-        #checks if guess matches answer
+        if guess == answer
+        end
     end
     def number_correct
-        #counts num of correct answers
-    end
-    def number_correct
-        #how many were correct
+        correct_answers = []
     end
     def number_correct_by_category(category)
         #match which card categories were given correct response
