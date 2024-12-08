a0eade489a5186eca5053eb7b2d85b79fdade8fd
Jaren Garman <jarengarman@protonmail.com>
2024-12-08 14:19:30 -0500

    refactor: update to new card format with multiple answers

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -5,15 +5,25 @@ require_relative 'card'
 class CardGenerator
   attr_reader :cards
 
-  def initialize(filename) # rubocop:disable Metrics/MethodLength
+  def initialize(filename) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
     @cards = []
     File.open(filename, 'r') do |file|
       file.each_line do |card|
         card_array = card.split(',')
-        @cards << if card_array.size == 3
+        @cards << case card_array.size
+                  when 2
+                    Card.new(card_array[0], card_array[1].chomp)
+                  when 3
                     Card.new(card_array[0], card_array[1], card_array[2].chomp)
-                  else
-                    Card.new(card_array[0], card_array[1], card_array[2], card_array[3].chomp)
+                  when 4..100
+                    answers = []
+                    index = 2
+                    (card_array.size - 2).times do
+                      answers << card_array[index]
+                      index += 1
+                    end
+                    answers.last.chomp!
+                    Card.new(card_array[0], card_array[1], answers)
                   end
       end
     end
