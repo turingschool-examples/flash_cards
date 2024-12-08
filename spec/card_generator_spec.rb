aefa793057df9bce5a25269ed49bef6473c9f5fa
Jaren Garman <jarengarman@protonmail.com>
2024-12-07 22:03:54 -0500

    Added tests for alternate answer

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -5,10 +5,10 @@ require_relative '../lib/deck'
 require 'rspec'
 
 describe CardGenerator do
-  let(:deck) { Deck.new(cards) }
+  let(:cards) { described_class.new('cards.txt').cards }
 
   describe '#initialize' do
-    subject(:cards) { described_class.new('cards.txt') }
+    subject(:card_generator) { described_class.new('cards.txt') }
 
     it { is_expected.to be_instance_of(described_class) }
   end
@@ -20,6 +20,24 @@ describe CardGenerator do
 
     it 'returns array of cards' do
       expect(cards.all?(Card)).to be true
+    end
+  end
+
+  describe 'parses alternate answers' do
+    context 'when alternate answer is not given' do
+      it 'does not pass alternate answer' do
+        expect(cards[2].alternate_answer).to be_nil
+      end
+    end
+
+    context 'when alternate answer is given' do
+      subject(:alternate_answer) { cards[0].alternate_answer }
+
+      it { is_expected.not_to be_nil }
+
+      it 'passes alternate answer' do
+        expect(alternate_answer).to eq 'Ten'
+      end
     end
   end
 end
