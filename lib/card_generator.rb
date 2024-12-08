a75a146e16c2335442f187ede8a43d103c721c87
Jaren Garman <jarengarman@protonmail.com>
2024-12-08 14:29:22 -0500

    refactor: replace case/when with if/elsif to allow use of >=

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -10,12 +10,11 @@ class CardGenerator
     File.open(filename, 'r') do |file|
       file.each_line do |card|
         card_array = card.split(',')
-        @cards << case card_array.size
-                  when 2
+        @cards << if card_array.size == 2
                     Card.new(card_array[0], card_array[1].chomp)
-                  when 3
+                  elsif card_array.size == 3
                     Card.new(card_array[0], card_array[1], card_array[2].chomp)
-                  when 4..100
+                  elsif card_array.size >= 4
                     answers = []
                     index = 2
                     (card_array.size - 2).times do
@@ -24,6 +23,8 @@ class CardGenerator
                     end
                     answers.last.chomp!
                     Card.new(card_array[0], card_array[1], answers)
+                  else
+                    next
                   end
       end
     end
