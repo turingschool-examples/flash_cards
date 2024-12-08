e622f5431456fcec05e956231a98f961c3434d8c
Jaren Garman <jarengarman@protonmail.com>
2024-12-08 16:38:48 -0500

    refactor: call each_line directly on the file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -7,28 +7,26 @@ class CardGenerator
 
   def initialize(filename) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
     @cards = []
-    File.open(filename, 'r') do |file|
-      file.each_line do |line|
-        next if line.empty? || line.start_with?('#')
+    File.open(filename, 'r').each_line do |line|
+      next if line.empty? || line.start_with?('#')
 
-        card_array = line.split(',')
-        @cards << if card_array.size == 2
-                    Card.new(card_array[0], card_array[1].chomp)
-                  elsif card_array.size == 3
-                    Card.new(card_array[0], card_array[1], card_array[2].chomp)
-                  elsif card_array.size >= 4
-                    answers = []
-                    index = 2
-                    (card_array.size - 2).times do
-                      answers << card_array[index]
-                      index += 1
-                    end
-                    answers.last.chomp!
-                    Card.new(card_array[0], card_array[1], answers)
-                  else
-                    next
+      card_array = line.split(',')
+      @cards << if card_array.size == 2
+                  Card.new(card_array[0], card_array[1].chomp)
+                elsif card_array.size == 3
+                  Card.new(card_array[0], card_array[1], card_array[2].chomp)
+                elsif card_array.size >= 4
+                  answers = []
+                  index = 2
+                  (card_array.size - 2).times do
+                    answers << card_array[index]
+                    index += 1
                   end
-      end
+                  answers.last.chomp!
+                  Card.new(card_array[0], card_array[1], answers)
+                else
+                  next
+                end
     end
   end
 end
