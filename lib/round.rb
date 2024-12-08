6d77f3dec0843b63e1ccb841e337ff0615022db3
Beverly Green <greenbev000@gmail.com>
2024-12-08 13:47:39 -0600

    Modifed take_turn method and current_card method

--- lib/round.rb
+++ lib/round.rb
@@ -1,53 +1,31 @@
 class Round
-    attr_reader :deck, :turns, :current_card, :take_turn
+    attr_reader :deck, :turns, :turns_taken, :number_correct 
     def initialize(deck)
         @deck = deck
         @turns = []
         @turns_taken = 0
-        @current_card = current_card
-        @take_turn = take_turn
+        @number_correct = 0
     end
     def current_card
         @deck.cards[@turns_taken]
-        # if turns == []
-        #     first_card = deck.map{|deck|deck.drop(card_2, card_3)}
-        #     first_card.take_turn
-        # elsif #need second condition here
-        #     second_card = deck.map{|deck|deck.drop(card_1, card_3)}
-        #     second_card.take_turn
-        # third_card = deck.map{|deck|deck.drop(card_1, card_2)}
-        #     third_card.take_turn
-        end
     end
     def take_turn(guess)
         new_turn = Turn.new(guess, current_card)
         @turns << new_turn
         if new_turn.correct?
             @number_correct += 1
-            @turns_taken += 1
-            new_turn
+        @turns_taken += 1
+        new_turn
         end
-    end
-        #records appropriate guess and card to 'turns' method
-        #round should move onto next card in the deck
-    # end
-    def turns
-        #turns = deck.map do {|deck| deck.take_turn}
-    end
-    def correct?
-        if guess == answer
-        end
-    end
-    def number_correct
-        correct_answers = []
     end
     def number_correct_by_category(category)
-        #match which card categories were given correct response
+        @turns.count 
+        turn.card.category == category && turn.correct?
     end
     def percent_correct
-        #will give back percent of correct answers
+        (@number_correct/@turns) *100
     end
     def percent_correct_by_category(category)
-        #explanatory
+        #(number_correct_by_category(category)/
     end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Deck do
 
     expect(deck).to be_instance_of(Deck)
     end
-   it 'counts number of cards' do
+   it'counts number of cards' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
