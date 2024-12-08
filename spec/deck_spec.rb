2ff204f657efe86ffda89b72f7c24ecea1f58a2f
Chase Phillips <chase.thomas.phillips@gmail.com>
2024-12-08 11:17:26 -0500

    cleaned up test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -7,10 +7,8 @@ RSpec.describe Deck do
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-#=> #<Card:0x00007fa160a62e90 @answer="Mars", @question="The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", @category=:STEM>
 
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-#=> #<Card:0x00007fa161a136f0 @answer="North north west", @question="Describe in words the exact direction that is 697.5° clockwise from due north?", @category=:STEM>
 
 cards = [card_1, card_2, card_3]
 deck = Deck.new(cards)
