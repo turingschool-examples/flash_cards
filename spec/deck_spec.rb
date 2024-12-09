cdb5a1d12f3ef6a933d5e0231d43e3d67de63bf3
Patrick Little <little.patrick@outlook.com>
2024-12-09 08:35:30 -0700

    first two passed

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,33 +5,31 @@ require_relative '../lib/round'
 require 'pry'
 
 
- RSpec.describe deck do
-      xit 'exists' do
-            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-            
-            cards = [card_1, card_2, card_3]
-            deck = Deck.new(cards)
-
-            expect(deck).to be_instance_of(Deck)
-      
+RSpec.describe Deck do
+      it 'exists' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+    
+        expect(deck).to be_instance_of(Deck)
       end
-
+    
       it 'has cards' do
-
-            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-            
-            cards = [card_1, card_2, card_3]
-            deck = Deck.new(cards)
-            
-            expect(deck.cards).to eq(cards)
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        
+        expect(deck.cards).to eq(cards)
       end
-
-      it 'category' do
-
+    
+      xit 'category' do
+        
       end
-
-end
+    end
+    
