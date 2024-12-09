19ba94b6261af999758b15982285fac746f606c2
Patrick Little <little.patrick@outlook.com>
2024-12-09 08:26:24 -0700

    rspec test for if deck has cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -18,11 +18,19 @@ require 'pry'
       
       end
 
-      it 'has a question' do
+      it 'has cards' do
 
+            card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+            card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+            card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+            
+            cards = [card_1, card_2, card_3]
+            deck = Deck.new(cards)
+            
+            expect(deck.cards).to eq(cards)
       end
 
-      it 'has an answer' do
+      it 'category' do
 
       end
 
