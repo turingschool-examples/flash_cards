1bfb922a1f2abfdd2fbd730050e4e69028752582
Jaren Garman <jarengarman@protonmail.com>
2024-12-08 21:05:02 -0500

    refactor: replace times loop and unnecessary variables with a simpler solution

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -5,20 +5,14 @@ require_relative 'card'
 class CardGenerator
   attr_reader :cards
 
-  def initialize(filename) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+  def initialize(filename)
     @cards = []
     File.open(filename, 'r').each_line do |line|
       next if line.empty? || line.start_with?('#') || line.split(',').size < 2
 
       card_array = line.split(',')
-      answers = []
-      index = 2
-      (card_array.size - 2).times do
-        answers << card_array[index]
-        index += 1
-      end
-      answers&.last&.chomp!
-      @cards << Card.new(card_array[0], card_array[1], answers)
+      card_array.last.chomp!
+      @cards << Card.new(card_array.shift, card_array.shift, card_array)
     end
   end
 end
