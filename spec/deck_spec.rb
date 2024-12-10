8cf1806c54dea62f4093ba37929b8523f3ade7db
ontruster74 <ontruster74@gmail.com>
2024-12-10 09:30:40 -0700

    reword tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,7 +3,7 @@ require './lib/card'
 require './lib/deck'
 
 describe Deck do
-  it 'exists and is initialized properly' do
+  it 'exists' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
@@ -12,7 +12,7 @@ describe Deck do
     expect(deck).to be_a(Deck)
   end
 
-  it 'has a working cards attr_reader' do
+  it 'contains an array of cards' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
@@ -21,7 +21,7 @@ describe Deck do
     expect(deck.cards).to eq(cards)
   end
 
-  it 'has a working count attr_reader' do
+  it 'tracks the number of cards it contains' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
@@ -30,7 +30,7 @@ describe Deck do
     expect(deck.count).to eq(3)
   end
 
-  it 'has a working cards_in_category method (STEM cards)' do
+  it 'tracks which cards are in a given category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
@@ -39,7 +39,7 @@ describe Deck do
     expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
   end
 
-  it 'has a working cards_in_category method (Geography cards)' do
+  it 'tracks which cards are in another given category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
@@ -48,7 +48,7 @@ describe Deck do
     expect(deck.cards_in_category(:Geography)).to eq([card_1])
   end
 
-  it 'has a working cards_in_category method (unincluded topic cards)' do
+  it 'tracks which cards are in a third given category' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5 degrees clockwise from due north?", "North north west", :STEM)
