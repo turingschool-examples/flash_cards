ebb0f8d40291bba8728a037da191789aaa44048d
ontruster74 <ontruster74@gmail.com>
2024-12-10 09:31:10 -0700

    reword tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,63 +3,64 @@ require './lib/turn'
 require './lib/card'
 
 describe Turn do
-  it 'exists and is initalized properly' do
+  it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn).to be_instance_of(Turn)
   end
 
-  it 'has a card attr_reader' do
+  it 'contains a card object' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.card).to eq(card)
   end
 
-  it 'has a guess attr_reader' do
+  it 'contains a guess string' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.guess).to eq("Juneau")
   end
 
-  it 'has a working correct? method' do
+  it 'tracks whether the guess is correct' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.correct?).to eq(true)
   end
 
-  it 'has a working feedback method' do
+  it 'can display feedback on the guess' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
     expect(turn.feedback).to eq("Correct!")
   end
 end
 
+# repeat the tests above with a different object
 describe Turn do
-  it 'exists and is initalized' do
+  it 'exists' do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     expect(turn).to be_instance_of(Turn)
   end
 
-  it 'has a card attr_reader' do
+  it 'contains a card object' do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     expect(turn.card).to eq(card)
   end
 
-  it 'has a guess attr_reader' do
+  it 'contains a guess string' do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     expect(turn.guess).to eq("Saturn")
   end
 
-  it 'has a working correct? method' do
+  it 'tracks whether the guess is correct' do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     expect(turn.correct?).to eq(false)
   end
 
-  it 'has a working feedback method' do
+  it 'can display feedback on the guess' do
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     expect(turn.feedback).to eq("Incorrect.")
