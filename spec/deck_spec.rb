d7747ff402a835a676249c16d3095eda8a5a3f15
Logan Sauer <ldsauer2@gmail.com>
2024-12-09 20:45:33 -0700

    this finished up my deck spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,25 +1,30 @@
 require "./lib/card"
 require "./lib/deck"
+require "./lib/turn"
 
 RSpec.describe Deck do 
-    it 'exists' do
-        deck = Deck.new
-
-        expect(deck).to be_an_instance_of(Deck)
-    end
-
-    it 'has cards' do
-        deck = Deck.new
-
-        expect(deck.add_cards).to eq([])
-    end
-
-    xit 'counts cards in the deck' do
-        deck = Deck.new
+    it 'exists and has cards' do
         
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        expect(deck).to be_instance_of(Deck)
+        expect(deck.cards).to eq(cards)
         expect(deck.count).to eq(3)
     end
 
-    xit 'has a category' cards.each do |card|
-        
+    it 'has a category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        expect(deck.cards[2].category).to eq(:STEM)
+    end
 end
