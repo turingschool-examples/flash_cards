2c53ba5c86307d685506e734dee9b0d9eda56121
ontruster74 <ontruster74@gmail.com>
2024-12-10 09:30:04 -0700

    add comments

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -4,14 +4,17 @@ require './lib/round'
 require './lib/turn'
 require './lib/card_generator'
 
+# main function definition
 def start(round_var)
+  # declare preliminary variables
   deck_var = round_var.deck
   cards_var = deck_var.cards
-  turns_var = round_var.turns
 
+  # opening messages
   p "Welcome! You're playing with #{cards_var.length} cards."
   p "--------------------------------------------------"
 
+  # main quiz gameplay loop
   round_var.deck.cards.each { |card|
     p "This is card #{(round_var.index + 1)} out of #{cards_var.length}"
     p "Question: #{round_var.current_card.question}"
@@ -20,9 +23,11 @@ def start(round_var)
     new_turn.feedback
   }
 
+  # closing messages
   p "*** Game over! ***"
   p "You had #{round_var.number_correct} correct guesses out of #{cards_var.length} for a total score of #{round_var.percent_correct}%."
   
+  # sort cards by category
   categories = []
   cards_var.each {|card|
     if !categories.include?(card.category)
@@ -30,15 +35,17 @@ def start(round_var)
     end
   }
 
+  # display percent correct by category
   categories.each {|category|
     p "#{category} - #{round_var.percent_correct_by_category(category)}% correct"
   }
-
 end
 
+# card_generator text file integration
 filepath = "cards.txt"
 gen = Card_Generator.new(filepath)
 deck = Deck.new(gen.cards)
 round = Round.new(deck)
 
+# main function call for CLI
 start(round)
