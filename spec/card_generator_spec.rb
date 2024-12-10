636de7f95d973878713568c31c061a37c761a694
ontruster74 <ontruster74@gmail.com>
2024-12-10 09:31:31 -0700

    reword tests

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -3,13 +3,13 @@ require './lib/card_generator'
 require './lib/card'
 
 describe Card_Generator do
-  it 'is initialized properly' do
+  it 'exists' do
     filepath = "./cards.txt"
     gen = Card_Generator.new(filepath)
     expect(gen).to be_a(Card_Generator)
   end
 
-  it 'has a working cards reader' do
+  it 'contains an array of cards' do
     filepath = "./cards.txt"
     gen = Card_Generator.new(filepath)
     card_array = [gen.cards[0], gen.cards[1], gen.cards[2], gen.cards[3]]
