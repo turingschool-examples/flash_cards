f77e69c3594089e8f837d27cab1764353f28efdd
Patrick Little <little.patrick@outlook.com>
2024-12-10 07:26:34 -0700

    update the turn tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,5 +5,52 @@ require './lib/round.rb'
 require 'pry'
 
 RSpec.describe Turn do
- 
+    it "exists" do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+    
+        expect(turn).to be_a(Turn)
+      end
+    
+      it "has a card" do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+    
+        expect(turn.card).to eq(card)
+      end
+    
+      it "has a guess" do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+    
+        expect(turn.guess).to eq("Juneau")
+      end
+    
+      it "can check if a guess is correct" do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+    
+        expect(turn.correct?).to eq(true)
+      end
+    
+      it "can check if a guess is incorrect" do
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Saturn", card)
+    
+        expect(turn.correct?).to eq(false)
+      end
+    
+      it "provides feedback for a correct guess" do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau", card)
+    
+        expect(turn.feedback).to eq("Correct!")
+      end
+    
+      it "provides feedback for an incorrect guess" do
+        card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+        turn = Turn.new("Saturn", card)
+    
+        expect(turn.feedback).to eq("Incorrect.")
+      end
   end
