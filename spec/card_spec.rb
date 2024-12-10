2338437fa4cf2071c387868e13e11a1561ca43e3
ontruster74 <ontruster74@gmail.com>
2024-12-10 09:30:24 -0700

    reword tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,25 +2,25 @@ require 'rspec'
 require './lib/card'
 
 describe Card do
-  it 'exists and is initialized properly' do
+  it 'exists' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card).to be_instance_of(Card)
   end
 
-  it 'has a question' do
+  it 'contains a question string' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.question).to eq("What is the capital of Alaska?")
   end
 
-  it 'has an answer' do
+  it 'contains an answer string' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.answer).to eq("Juneau")
   end
 
-  it 'has a category' do
+  it 'contains a category symbol' do
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     expect(card.category).to eq(:Geography)
