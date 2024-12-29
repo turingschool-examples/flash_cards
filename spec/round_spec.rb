690f6c8b560c1288130258efa66b6a70776df5c6
Donte Handy <donte.handy@gmail.com>
2024-12-28 20:30:52 -0400

    test: add RSpec tests for Round functionality including start, question display, feedback, and scoring

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -64,7 +64,33 @@ RSpec.describe Round do
     expect(@round.percent_correct_by_category(:Geography)).to eq(100.0)
     expect(@round.percent_correct_by_category(:STEM)).to eq(100.0)
   end
+
+  it 'can start the round and display welcome message' do
+    allow(@round).to receive(:puts)
+    allow(@round).to receive(:gets).and_return("Juneau", "Mars", "North north west")
+    expect { @round.start }.to output(/Welcome! You're playing with 3 cards./).to_stdout
+  end
+
+  it 'can display questions and take guesses' do
+    allow(@round).to receive(:puts)
+    allow(@round).to receive(:gets).and_return("Juneau", "Mars", "North north west")
+    expect { @round.start }.to output(/Question: What is the capital of Alaska?/).to_stdout
+    expect { @round.start }.to output(/Question: The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?/).to_stdout
+    expect { @round.start }.to output(/Question: Describe in words the exact direction that is 697.5° clockwise from due north?/).to_stdout
+  end
+
+  it 'can display feedback for each guess' do
+    allow(@round).to receive(:puts)
+    allow(@round).to receive(:gets).and_return("Juneau", "Venus", "North north west")
+    expect { @round.start }.to output(/Correct!/).to_stdout
+    expect { @round.start }.to output(/Incorrect./).to_stdout
+  end
+
+  it 'can display final score and percent correct by category' do
+    allow(@round).to receive(:puts)
+    allow(@round).to receive(:gets).and_return("Juneau", "Mars", "North north west")
+    expect { @round.start }.to output(/You had 3 correct guesses out of 3 for a total score of 100%./).to_stdout
+    expect { @round.start }.to output(/Geography - 100% correct/).to_stdout
+    expect { @round.start }.to output(/STEM - 100% correct/).to_stdout
+  end
 end
-
-
-
