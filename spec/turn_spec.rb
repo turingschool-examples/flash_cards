349a14dacfd65a84f9cfa36166e05be9d3ac3249
Maia Bard <Maiakbard@gmail.com>
2025-02-04 20:56:14 -0700

    Add test to file

---
+++ spec/turn_spec.rb
@@ -0,0 +1,30 @@
+require './lib/turn'
+require './lib/card'
+require 'rspec'
+
+describe Turn do
+    it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn).to be_a Turn
+    end
+
+    it 'has a guess'do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq("Juneau")
+    end
+
+    it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.card).to eq(card)
+    end
+
+    it 'should return true if correct' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to eq(true)
+    end
+end
+
