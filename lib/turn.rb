0492ac6d924f3f32a7967ce253073f37ba8dc018
Katya Weicht <katya.weicht@gmail.com>
2025-02-05 14:04:00 -0700

    add method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,11 @@
 class Turn
-    attr_reader :string, :card
+    attr_reader :guess, :card
     def initialize(string,card)
         @guess = string
         @card = card
-
+    end
+    
+    def correct?
 
     end
 end
