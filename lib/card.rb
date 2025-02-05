a9fc555da0c04f3e052f598887bce09acbdda6cd
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-05 15:45:47 -0600

    Inititialize Card class and add an att_reader

--- lib/card.rb
+++ lib/card.rb
@@ -1,23 +1,11 @@
 class Card
         attr_reader :question, :answer, :category
 
-        def initalize(question, answer, category) 
+        def initialize(question, answer, category) 
         @question = question
         @answer = answer 
         @category = category
         end
-
-    def add_question(question)
-    @question << question 
-    end 
-
-    def add_answer(answer)
-    @answer << answer 
-    end
-
-    def add_category(category)
-    @category << answer
-    end
 end
 
 
