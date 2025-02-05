9bf6ec4cf2e89f9504d88baa69952bb6f5b38a63
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-02-05 16:48:42 -0700

    A boolean

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,15 @@
 class Turn
-    attr_reader :answer, :card, :guess
+    attr_reader :answer, :card, :guess, :correct
 
     def initialize(answer, card)
         @card = card
         @answer = answer
         @guess = "Juneau"
-
+        @correct = true
     end
-
+    def correct?
+        correct = true 
+    end
 end
    
 
