1bf55011db8c5905a34e46eb08423886ff5223fd
Katya Weicht <katya.weicht@gmail.com>
2025-02-05 19:59:32 -0700

    Add methods and add testing for incorrect guesses

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,9 +3,24 @@ class Turn
     def initialize(string,card)
         @guess = string
         @card = card
+        
     end
-    
-    def correct?
 
+    def correct?
+        if 
+            true
+        else
+            false
+        end
     end
+
+    def feedback
+        if
+            "Correct!"
+        else
+            "Incorrect."
+        end
+    end
+
+    
 end
