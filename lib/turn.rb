4aa028768b97cc070a63ded5e15b86f43c3b37b2
Maia Bard <Maiakbard@gmail.com>
2025-02-05 18:30:50 -0700

    Add code to file

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,24 @@
 class Turn
     attr_reader :guess, :card
 
-    def initialize(answer, card)
-        @guess = answer
+    def initialize(guess, card)
+        @guess = guess
         @card = card
     end
 
     def correct?
+        @guess == @card.answer
+    end
+
+    def feedback
+        if correct? == true
+        return "Correct!"
+
+        else 
+        return "Incorrect."
+        end 
+       
+    
     end
 end
+
