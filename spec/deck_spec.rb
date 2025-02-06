b20bb9aa1e809d803bf9d3c29544c01ada5af3a5
Brady Blair <bblair321@yahoo.comm>
2025-02-06 10:59:38 -0800

    Finished writing the rspec tests for deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,30 +5,31 @@ require './lib/deck'
 RSpec.describe Deck do
     # This test is checking to see if card returns the question, answer and category of card
     # It's also
-    it 'exists' do
-        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-        turn = Turn.new("Juneau",card)
+    it 'get a card' do
+        card = Card.new(:question, :answer, :category)
         deck = Deck.new(card)
     
         expect(deck).to be_instance_of(Deck)
       end
-    # it 'makes a guess' do
-    #     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    #     turn = Turn.new("Juneau",card)
+    it 'Is there cards in the deck' do
+        card = Card.new(:question, :answer, :category)
+        deck = Deck.new([Card])
     
-    #     expect(turn.guess).to eq("Juneau")
-    # end
-    # it 'checks if the guess is correct' do
-    #     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    #     turn = Turn.new("Juneau",card)
-    
-    #     expect(turn.correct?).to eq(true)
-    # end
-    # it 'checks for feedback' do
-    #     card= Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    #     turn = Turn.new("Juneau",card)
+        expect(deck.cards).to eq([Card])
+    end
+    it 'check for count' do
+        card_1 = Card.new(:question, :answer, :category)
+        card_2 = Card.new(:question, :answer, :category)
+        card_3 = Card.new(:question, :answer, :category)
+        deck = Deck.new([card_1,card_2,card_3])
 
-    #     expect(turn.feedback).to eq("Correct!")
-    # end
-   
+        expect(deck.count).to eq(3) 
+    end
+    it 'check for category' do
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        deck = Deck.new([card])
+
+        expect(deck.cards_in_category(:Geography)).to eq([card])
+    end
+
 end
