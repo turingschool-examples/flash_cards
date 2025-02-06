4b708a3b36ae2f1beccb8a9ac8359b3bf9f20ddf
Brady Blair <bblair321@yahoo.comm>
2025-02-06 10:06:44 -0800

    Had to go and fix my card class. I hard coded the variables and I removed my manually defined variables and used a attr_reader instead

--- lib/card.rb
+++ lib/card.rb
@@ -1,16 +1,8 @@
 class Card
+    attr_reader :question, :answer, :category
     def initialize(question, answer, category)
         @question = question
         @answer = answer
         @category = category
-    end
-    def question
-        @question = "#{question}"
-    end
-    def answer
-        @answer = "Juneau"
-    end
-    def category
-        @category = :Geography
     end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,11 +6,12 @@ RSpec.describe Deck do
     # This test is checking to see if card returns the question, answer and category of card
     # It's also
     it 'exists' do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Juneau",card)
-  
-      expect(turn).to be_instance_of(Deck)
-    end
+        card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        turn = Turn.new("Juneau",card)
+        deck = Deck.new(card)
+    
+        expect(deck).to be_instance_of(Deck)
+      end
     # it 'makes a guess' do
     #     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     #     turn = Turn.new("Juneau",card)
@@ -30,4 +31,4 @@ RSpec.describe Deck do
     #     expect(turn.feedback).to eq("Correct!")
     # end
    
-end    
+end
