39f53ba12d9a694ff6c6b1d3e2f1184b86cb92d1
Katya Weicht <katya.weicht@gmail.com>
2025-02-06 12:01:51 -0700

    Add class and methods

---
+++ lib/deck.rb
@@ -0,0 +1,16 @@
+class Deck
+    attr_reader :cards
+    def initialize(cards)
+        @cards = cards
+        
+    end
+    def count
+        cards.count
+    end
+
+    def cards_in_category(category)
+        cards.find_all do |card|
+            card.category == category
+        end
+    end
+end
