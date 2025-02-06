98c701d82492eeed1f25e9bd32a81b95f947bdcc
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-02-06 12:47:19 -0600

    Complete correct code for turn.rb

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+class Turn 
+    attr_reader :guess, :card
+    def initialize(string, card)
+        @guess = string
+        @card = card
+    end
+
+    def correct?
+        if @guess == card.answer
+            true 
+        else 
+            false 
+        end
+    end
+
+    def feedback
+        if @guess == card.answer
+            "Corret!"
+        else 
+            "Incorrect."
+        end
+    end
+
+end
