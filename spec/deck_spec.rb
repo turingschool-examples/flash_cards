e092c45d6d9fbbe335d20fc44f0787e6da8ca677
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-02-07 13:08:52 -0700

    make count test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Deck do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
         cards = [card_1, card_2, card_3]
         deck = Deck.new(cards)
     end
@@ -15,11 +14,29 @@ RSpec.describe Deck do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-
         cards = [card_1, card_2, card_3]
         deck = Deck.new(cards)
     
+    expect(deck.cards).to be(cards)
+    end
 
-    expect(deck.cards).to eq(cards)
+    it "can count" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+    
+        expect(deck.count).to eq(3)
+    end
+
+    it "has categorys" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+
     end
 end
