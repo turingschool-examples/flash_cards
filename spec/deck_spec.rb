66f43f568c57feb86b4765b01559d94f3f557896
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-02-06 17:34:50 -0700

    Verify the first commit

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,7 +1,8 @@
 require './lib/card.rb'
 require './lib/deck.rb'
+require "pry"
 
-RSpec describe Deck do
+RSpec.describe Deck do
     it "exist" do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -9,6 +10,16 @@ RSpec describe Deck do
 
         cards = [card_1, card_2, card_3]
         deck = Deck.new(cards)
+    end
+    it "has cards" do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+    
+
+    expect(deck.cards).to eq(cards)
     end
 end
