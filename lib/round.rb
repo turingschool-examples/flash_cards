8e5c5b8e01ddd8e8ddea1833ca845a339feb6fe0
Katya Weicht <katya.weicht@gmail.com>
2025-02-06 23:12:11 -0700

    Add current_card  method

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,16 @@
 class Round
-    
+    attr_reader :deck, :turns, :current_card
+    def initialize(deck)
+        @deck = deck
+        @turns = []
+        #require 'pry'; binding.pry
+        @current_card = 
+        
+    end
+    def take_turn(string)
+
+    end
+
 end
+
+
