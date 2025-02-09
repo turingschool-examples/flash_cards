14822c989839b520a198e7df6ade1d7ae5e9c204
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-08 22:14:11 -0500

    finish Iteration 2

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,44 @@
-#Round class
-#Initialize deck
-#define current card
-#define take turn
-#define number of correct guesses
-#define number of correct by category
-#define percent correct
-#define percent correct by category
+class Round
+    attr_reader :deck, :turns
+
+    def initialize(deck)
+        @deck = deck
+        @turns = []
+        @current_card_index = 0
+    end
+
+    def current_card
+        @deck.cards[@current_card_index] #selects the card
+    end
+
+    def take_turn(guess)
+        current_card = @deck.cards[@current_card_index]
+        new_turn = Turn.new(guess, current_card) #uses the guess and current card as a turn
+
+        @turns << new_turn
+        @current_card_index += 1 #do I need more here?
+        #adding if @current_card_index < @decks.cards.length - 1
+        new_turn #I have to print the new turn
+    end
+
+    def number_correct #number correct per turn
+        @turns.count{ |turn| turn.correct? }
+    end
+
+    def number_correct_by_category(category)
+        @turns.count {|turn| turn.card.category == category && turn.correct? }
+    end
+
+    def percent_correct
+        return 0.0 if @turns.empty? #no division by 0
+        correct_count = @turns.count { |turn| turn.correct? } #this could be wrong
+        (correct_count.to_f / @turns.length * 100).round(1) #converting to float for division
+    end
+
+    def percent_correct_by_category(category)
+        category_turns = @turns.select {|turn| turn.card.category == category}
+        return 0.0 if @turns.empty?
+        correct_in_category = category_turns.count {|turn| turn.correct?}
+        (correct_in_category.to_f / category_turns.length * 100).round(1)
+    end
+end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -3,7 +3,7 @@ require './lib/turn'
 require './lib/deck'
 require './lib/round'
 
-Rspec.describe Round do
+RSpec.describe Round do
     it 'exists' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -14,7 +14,28 @@ Rspec.describe Round do
         expect(round).to be_a(Round)
     end
 
-    it 'start with the first card in the deck' do
+    it 'has a deck' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        expect(round.deck).to eq(deck)
+    end
+
+    it 'defaults as no turns' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        expect(round.turns).to eq([])
+
+    end
+
+    it 'has a current card' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -52,5 +73,36 @@ Rspec.describe Round do
         expect(round.number_correct).to eq(1)
     end
 
+    it 'counts correct answers by category' do
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        round.take_turn("Juneau")
+        round.take_turn("Mercury")
+
+        expect(round.number_correct_by_category(:Geography)).to eq(1)
+        expect(round.number_correct_by_category(:STEM)).to eq(0)
+    end
+
+    it 'calculates percentages' do #as a whole and by category
+        card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+        card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+        deck = Deck.new([card_1, card_2, card_3])
+        round = Round.new(deck)
+
+        round.take_turn("Juneau")
+        round.take_turn("Mercury")
+
+        expect(round.percent_correct).to eq(50.0)
+        expect(round.percent_correct_by_category(:Geography)).to eq(100.0)
+        expect(round.percent_correct_by_category(:STEM)).to eq(0.0)
+    end
+
 
 end
+
+#Would like to check this in pry during session with Alec
