264881c2ad84ed6cb267aa9e1868e32cbb6ec3e7
Zach Johnson <zachj5551@gmail.com>
2025-02-08 19:01:42 -0600

    add turn file

---
+++ lib/card.rb
@@ -0,0 +1,20 @@
+class Card
+    def initialize (question, answer, category)
+        @question = question
+        @answer = answer
+        @category = category
+    end
+
+    def question 
+        @question
+    end
+
+    def answer
+        @answer
+    end
+
+    def category
+        @category
+    end
+end
+
---
+++ spec/turn_spec.rb
@@ -0,0 +1,13 @@
+require './lib/turn'
+# require'pry';binding.pry
+require './lib/card'
+
+RSpec.describe Turn do
+  it 'exists' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn).to be_instance_of(Turn)
+  end
+end
+
+  
