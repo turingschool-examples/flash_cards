1c295f6d09eaeb6a86ea1e0f066fce3be3342f9e
Zach Johnson <zachj5551@gmail.com>
2025-02-09 14:50:13 -0600

    fix capitol S in Rspec

---
+++ lib/deck.rb
@@ -0,0 +1,10 @@
+class Deck
+    attr_reader :deck
+
+    def initialize (cards)
+        @cards = []
+    end
+
+
+
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,12 +1,17 @@
 require './lib/card'
 require './lib/deck'
 
-Rspec.describe Deck do
+RSpec.describe Deck do
     it 'exists' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         cards = [card_1, card_2, card_3]
         deck = Deck.new(cards)
+        expect(deck).to be_a(Deck)
     end
+
+    # it 'has an array of cards' do
+
+
 end
