36f4d110e2aa27d3a33dbd13a490cbf633d5575f
Zach Johnson <zachj5551@gmail.com>
2025-02-09 12:00:34 -0600

    replace methods with attr reader

--- lib/card.rb
+++ lib/card.rb
@@ -1,20 +1,10 @@
 class Card
+    attr_reader :question, :answer, :category
+
     def initialize (question, answer, category)
         @question = question
         @answer = answer
         @category = category
-    end
-
-    def question 
-        @question
-    end
-
-    def answer
-        @answer
-    end
-
-    def category
-        @category
     end
 end
 
