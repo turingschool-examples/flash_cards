cc6b252dc58e5776921eab3d1c77247fc55e4beb
Katya Weicht <katya.weicht@gmail.com>
2025-02-09 16:34:51 -0700

    Finish iteration three

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,7 +7,7 @@ require 'pry'
 
 def start_game
 
-    card_1 = Card.new("what is 5 + 5", 10, :STEM)
+    card_1 = Card.new("what is 5 + 5", "10", :STEM)
     card_2 = Card.new("what is Rachel's favorite animal?", "panda", :Turing_Staff)
     card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Pop_Culture)
     card_4 = Card.new("what cardboard cutout lives at Turing?", "Justin Bieber", :Turing_Staff)
@@ -16,7 +16,6 @@ def start_game
     deck = Deck.new(cards)
 
     round = Round.new(deck)
-    # binding.pry
 
     puts "Welcome to the flashcards game! You are playing with #{cards.length} cards."
 
@@ -29,8 +28,19 @@ def start_game
         puts "Question #{round.current_card.question}"
         guess = gets.chomp
         round.take_turn(guess)
+       # binding.pry
+        puts round.turns.last.feedback
     end
-   
+    
+
+    puts "****** GAME OVER ******"
+
+    puts "You had #{round.number_correct} guesses out of 4 for a total score of #{round.percent_correct}%"
+
+    puts " STEM - #{round.percent_correct_by_category(:STEM)}% correct"
+    puts " Turing_Staff - #{round.percent_correct_by_category(:Turing_Staff)}% correct"
+    puts " Pop_Culture - #{round.percent_correct_by_category(:Pop_Culture)}% correct"
+
 
 end
 
