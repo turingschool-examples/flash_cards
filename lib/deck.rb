b109aea1f6569c84b42a92d0d3998eea2d498e49
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-02-08 19:27:06 -0700

    finish deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,11 +1,24 @@
 class Deck
-    attr_reader :cards
+    attr_reader :cards, :category
 
     def initialize(cards)
-        @cards = cards    
+        @cards = cards 
+        @category = category
     end
     def count
-        @cards = 3
+        @cards.count
 
+    end
+    def cards_in_category(category)
+        @cards.find_all do |card|
+            card.category == category
+        end
+        # cards_in_cat =[]
+        # @cards.each do |card|
+        #     if card.category == category
+        #         cards_in_cat << card
+        #     end
+        # end
+        # cards_in_cat
     end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -36,6 +36,9 @@ RSpec.describe Deck do
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
         cards = [card_1, card_2, card_3]
         deck = Deck.new(cards)
+        deck.cards_in_category(:STEM)
+        deck.cards_in_category(:Geography)
+        deck.cards_in_category("Pop Culture")
 
 
     end
