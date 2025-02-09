e6ed45dde8c17e3f5f5795d09e77d8baf9deb3b8
Brady Blair <bblair321@yahoo.comm>
2025-02-09 11:05:47 -0800

    Have most of iteration 2 working

--- lib/round.rb
+++ lib/round.rb
@@ -7,5 +7,36 @@ class Round
         # current_card is returning the first card in deck
         @current_card = deck.cards.first
     end
+    def take_turn(guess) # this is creating the take_turn method and is taking the argument guess from the Card
+        # current_card is returning the current card in the deck
+        # making a new turn instance that gets stored in @turns and moves to the next turn
+        new_turn = Turn.new(guess, current_card)
+        @turns << new_turn
+        new_turn
+    end
+    def number_correct
+        # uses the @turns array 
+        @turns.count {|turn| turn.correct?}
+        # {|turn| turn.correct?} this is checking to see if each Turn was correct
+    end
+    def number_correct_by_category(category)
+        # counts the number of correct asnwers in categories
+        @turns.count {|turn| turn.correct? && turn.card.category == category}
+        # this is doing the same things as above pretty much excpet it's also getting the category
+    end
+    def percent_correct
+        total_turns = @turns.length
+        correct_turns = @turns.count {|turn| turn.correct?}
+        return 0 if total_turns == 0
+    end
+    def percent_correct_by_category
+        # Get the total number of turns for the given category
+        total_turns_in_category = @turns.count { |turn| turn.card.category == category }
+  
+        # Get the number of correct turns in that category
+        correct_turns_in_category = @turns.count { |turn| turn.correct? && turn.card.category == category }
+
+        return 0 if total_turns_in_category == 0 
+    end
 
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -4,27 +4,26 @@ require './lib/deck'
 require './lib/round'
 
 RSpec.describe Round do
-    it 'get a round' do
+    xit 'get a round' do
         card = Card.new(:question, :answer, :category)
         deck = Deck.new(card)
         round = Round.new(deck)
     
         expect(round).to be_instance_of(Round)
       end
-    it 'Is turns returning an empty array' do
+    xit 'Is turns returning an empty array' do
         deck = ('Deck')
         round = Round.new(deck)
 
         expect(round.turns).to eq([])
     end
-    # it 'check for count' do
-    #     card_1 = Card.new(:question, :answer, :category)
-    #     card_2 = Card.new(:question, :answer, :category)
-    #     card_3 = Card.new(:question, :answer, :category)
-    #     deck = Deck.new([card_1,card_2,card_3])
+     it 'check for number correct' do
+         card_1 = Card.new("What is the capital of Alaska?", "Juneau", "Geography")
+         round = Round.new
+         round.take_turn("Juneau", card_1) # Correct
 
-    #     expect(deck.count).to eq(3) 
-    # end
+         expect(round.number_correct).to eq(1) 
+     end
     # it 'check for category' do
     #     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     #     deck = Deck.new([card])
