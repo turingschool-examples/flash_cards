36c6c8b31f824974896204cb4666e7a7298e384d
Zach Johnson <zachj5551@gmail.com>
2025-02-09 12:58:50 -0600

    add feeback method and test

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,4 +5,20 @@ class Turn
         @guess = guess
         @card = card
     end
+
+    def correct?
+        if guess = card.answer
+            true
+        else
+            false
+        end
+    end
+
+    def feedback
+        if correct? == true
+            'Correct!'
+        else
+            'Incorrect!'
+        end
+    end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,6 +8,28 @@ RSpec.describe Turn do
     turn = Turn.new("Juneau", card)
     expect(turn).to be_instance_of(Turn)
   end
-end
 
-  
+  it 'has a card' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.card).to eq(card)
+  end
+
+  it 'has a guess' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.guess).to eq('Juneau')
+  end
+
+  it 'can say if correct' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.correct?).to eq(true)
+  end
+
+  it 'gives feedback' do
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    expect(turn.feedback).to eq('Correct!')
+  end
+end
