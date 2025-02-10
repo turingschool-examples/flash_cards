8c3509b18070e22da5cc81ed2320de8a3d0c563d
Katya Weicht <katya.weicht@gmail.com>
2025-02-10 14:56:28 -0700

    Finish testing for card_generator class

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,13 +7,6 @@ require 'pry'
 
 
 def start_game
-
-    # card_1 = Card.new("what is 5 + 5", "10", :STEM)
-    # card_2 = Card.new("what is Rachel's favorite animal?", "panda", :Turing_Staff)
-    # card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_Staff)
-    # card_4 = Card.new("what cardboard cutout lives at Turing?", "Justin Bieber", :Pop_Culture)
-
-    # cards = [card_1, card_2, card_3, card_4]
     filename = "./cards.txt"
 
     cards = CardGenerator.new(filename).cards
@@ -35,6 +28,7 @@ def start_game
         guess = gets.chomp
         round.take_turn(guess)
         puts round.turns.last.feedback
+        puts
     end
     
 
@@ -42,10 +36,10 @@ def start_game
 
     puts "You had #{round.number_correct} guesses out of 4 for a total score of #{round.percent_correct}%"
 
-    puts " STEM - #{round.percent_correct_by_category("STEM\n")}% correct"
-    puts " Turing_Staff - #{round.percent_correct_by_category("Turing_Staff\n")}% correct"
-    puts " Pop_Culture - #{round.percent_correct_by_category("Pop_Culture\n")}% correct"
-    #binding.pry
+    puts "STEM - #{round.percent_correct_by_category("STEM\n")}% correct."
+    puts "Turing_Staff - #{round.percent_correct_by_category("Turing_Staff\n")}% correct."
+    puts "Pop_Culture - #{round.percent_correct_by_category("Pop_Culture\n")}% correct."
+    
 
 
 end
--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,4 +1,4 @@
-
+require './lib/card'
 class CardGenerator
     def initialize(filename)
         @filename = filename
