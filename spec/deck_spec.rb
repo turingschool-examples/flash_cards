c98dc3aa019f867b2ee580d06ba7458e4911cfd8
Zach Johnson <zachj5551@gmail.com>
2025-02-09 21:36:59 -0600

    add existence of round test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe Deck do
         expect(deck.cards_in_category(:Geography)).to eq([card_1])
     end
 
-    it 'counts the cards in STEM category' do
+    it 'counts the cards in Geography category' do
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
         card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
