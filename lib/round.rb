9b2745efca807622935096de9043b0b221fbd935
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-09 18:17:25 -0600

    .

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,18 @@
 class Round
-    attr_reader :deck, :turns, :current_card
+    attr_reader :deck, :turns, :current_card, :current_card_index
     def initialize(deck)
         @deck = deck
         @turns = []
-        @current_card = deck.cards.first
+        @current_card_index = 0 
+        @current_card = deck.cards[@current_card_index]
         
     end 
+    def take_turn(guess)
+        turn = Turn.new(guess, @current_card) 
+        @turns << turn
+        @current_card_index += 1
+        @current_card = deck.cards[@current_card_index]
+        turn
+
+    end
+end
