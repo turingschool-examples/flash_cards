fe41ff631316ee06bb1dc1a306a0ed6fe18ac121
Zach Johnson <zachj5551@gmail.com>
2025-02-09 21:02:29 -0600

    add card category sorting method

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,14 @@ class Deck
     def count
         cards.count
     end
+
+    def cards_in_category(filtered_category)
+        cards_by_category = []
+        cards.each do |card|
+            if card.category == filtered_category
+                cards_by_category << card
+            end
+        end
+        cards_by_category
+    end
 end
